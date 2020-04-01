97be9d2b95b165cd99df7e798f82ca5ca13e0ba2
zakhap <zhs2104@columbia.edu>
2020-04-01 15:42:47 -0400

    Invite-related Route Unit Tests (#432)

--- test/unit/api/invites.spec.ts
+++ test/unit/api/invites.spec.ts
@@ -26,7 +26,7 @@ describe('Invite Tests', () => {
     let adminAddress;
     let userJWT;
     let userAddress;
-    const userEmail = 'hello@commonwealth.im';
+    const userEmail = 'zak@commonwealth.im';
 
     before(async () => {
       let res = await modelUtils.createAndVerifyAddress({ chain });
@@ -55,48 +55,124 @@ describe('Invite Tests', () => {
           author_chain: chain,
           address: adminAddress,
         });
-      console.dir(res.body);
       expect(res.body.status).to.be.equal('Success');
+      expect(res.body.result.community_id).to.be.equal(community);
+      expect(res.body.result.invited_email).to.be.equal(userEmail);
+      expect(res.body.result.used).to.be.false;
     });
 
     it('/createInvite as user', async () => {
+      const res = await modelUtils.createAndVerifyAddress({ chain });
+      const newUserAddress = res.address;
+      const newUserEmail = 'zak2@commonwealth.im';
+      const newUserJWT = jwt.sign({ id: res.user_id, email: newUserEmail }, JWT_SECRET);
 
-      const res = await chai.request(app)
+      const invite = await chai.request(app)
         .post('/api/createInvite')
         .set('Accept', 'application/json')
-        .send({});
-    });
-
-    it('/getInvites', async () => {
-      const res = await chai.request(app)
-      .post('/api/createInvite')
-      .set('Accept', 'application/json')
-      .send({});
-
+        .send({
+          jwt: userJWT,
+          invitedEmail: newUserEmail,
+          community,
+          author_chain: chain,
+          address: userAddress,
+        });
+      expect(invite.body.status).to.be.equal('Success');
+      expect(invite.body.result.community_id).to.be.equal(community);
+      expect(invite.body.result.invited_email).to.be.equal(newUserEmail);
+      expect(invite.body.result.used).to.be.false;
     });
 
     it('/acceptInvite', async () => {
+      const invite = await chai.request(app)
+        .post('/api/createInvite')
+        .set('Accept', 'application/json')
+        .send({
+          jwt: adminJWT,
+          invitedEmail: userEmail,
+          community,
+          author_chain: chain,
+          address: adminAddress,
+        });
+      expect(invite.body.status).to.be.equal('Success');
       const res = await chai.request(app)
-      .post('/api/createInvite')
-      .set('Accept', 'application/json')
-      .send({});
-
+        .post('/api/acceptInvite')
+        .set('Accept', 'application/json')
+        .send({
+          jwt: userJWT,
+          reject: false,
+          inviteCode: invite.body.result.id,
+          address: userAddress,
+        });
+      expect(res.body.status).to.be.equal('Success');
+      expect(res.body.result.updatedCode.community_id).to.be.equal(community);
+      expect(res.body.result.updatedCode.invited_email).to.be.equal(userEmail);
+      expect(res.body.result.updatedCode.used).to.be.true;
+      expect(res.body.result.membership.active).to.be.true;
+      expect(res.body.result.membership.community).to.be.equal(community);
     });
   });
 
   describe('Invite Link Tests', () => {
-    it('/createInviteLink', async () => {
+    const community = 'staking';
+    const chain = 'ethereum';
+    let adminJWT;
+    let adminAddress;
+    let userJWT;
+    let userAddress;
+    const userEmail = 'zak@commonwealth.im';
+    let inviteCodeId;
 
+    before(async () => {
+      let res = await modelUtils.createAndVerifyAddress({ chain });
+      adminAddress = res.address;
+      adminJWT = jwt.sign({ id: res.user_id, email: res.email }, JWT_SECRET);
+      const isAdmin = await modelUtils.assignAdmin(res.address_id, community);
+      res = await modelUtils.createAndVerifyAddress({ chain });
+      userAddress = res.address;
+      userJWT = jwt.sign({ id: res.user_id, email: userEmail }, JWT_SECRET);
+      expect(adminAddress).to.not.be.null;
+      expect(adminJWT).to.not.be.null;
+      expect(isAdmin).to.not.be.null;
+      expect(userAddress).to.not.be.null;
+      expect(userEmail).to.not.be.null;
+      expect(userJWT).to.not.be.null;
     });
 
-    it('/getInviteLinks', async () => {
-
+    it('/createInviteLink as admin', async () => {
+      const res = await chai.request(app)
+        .post('/api/createInviteLink')
+        .set('Accept', 'application/json')
+        .send({
+          community_id: community,
+          time: 'none',
+          uses: 'none',
+          jwt: adminJWT,
+        });
+      expect(res.body.status).to.be.equal('Success');
+      expect(res.body.result.community_id).to.be.equal(community);
+      expect(res.body.result.time_limit).to.be.equal('none');
+      expect(res.body.result.multi_use).to.be.null;
+      expect(res.body.result.active).to.be.true;
+      inviteCodeId = res.body.result.id;
     });
 
-    it('/acceptInviteLink', async () => {
-
+    it('/createInviteLink as user', async () => {
+      const res = await chai.request(app)
+        .post('/api/createInviteLink')
+        .set('Accept', 'application/json')
+        .send({
+          community_id: community,
+          time: 'none',
+          uses: 'none',
+          jwt: userJWT,
+        });
+      expect(res.body.status).to.be.equal('Success');
+      expect(res.body.result.community_id).to.be.equal(community);
+      expect(res.body.result.time_limit).to.be.equal('none');
+      expect(res.body.result.multi_use).to.be.null;
+      expect(res.body.result.active).to.be.true;
+      inviteCodeId = res.body.result.id;
     });
-
   });
-
 });
