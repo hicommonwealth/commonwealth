af80e3963a2e80183587df1e012a84fd9a25b318
zakhap <zhs2104@columbia.edu>
2020-04-14 13:28:18 -0400

    Complete first pass at migration.

--- server/migrations/20200414143124-add-privacy-features-to-threads.js
+++ server/migrations/20200414143124-add-privacy-features-to-threads.js
@@ -2,6 +2,18 @@
 
 module.exports = {
   up: (queryInterface, DataTypes) => {
+    await queryInterface.createTable('read_only_roles_threads', {
+      id: { type: DataTypes.INTEGER, allowNull: false },
+      thread_id: { type: DataTypes.INTEGER, allowNull: false },
+      created_at: { type: DataTypes.DATE, allowNull: false },
+      updated_at: { type: DataTypes.DATE, allowNull: false },
+    });
+    await queryInterface.createTable('private_threads_roles', {
+      id: { type: DataTypes.INTEGER, allowNull: false },
+      thread_id: { type: DataTypes.INTEGER, allowNull: false },
+      created_at: { type: DataTypes.DATE, allowNull: false },
+      updated_at: { type: DataTypes.DATE, allowNull: false },
+    });
     await queryInterface.addColumn('private', {
       type: DataTypes.BOOLEAN,
       allowNull: false,
@@ -12,16 +24,11 @@ module.exports = {
       allowNull: false,
       defaultValue: false
     });
-    await queryInterface.createTable('read_only_roles_threads', {
-      thread_id: {},
-      
-    });
-    await queryInterface.createTable('private_threads_roles', {
-
-    });
   },
 
   down: (queryInterface, DataTypes) => {
+    await queryInterface.dropTable('read_only_roles_threads');
+    await queryInterface.dropTable('private_threads_roles');
     await queryInterface.deleteColumn('private', {
       type: DataTypes.BOOLEAN,
       allowNull: false,
@@ -32,7 +39,6 @@ module.exports = {
       allowNull: false,
       defaultValue: false
     });
-    await queryInterface.dropTable('read_only_roles_threads');
-    await queryInterface.dropTable('private_threads_roles');
+
   }
 };
