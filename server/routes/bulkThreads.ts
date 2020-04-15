c9a3d1ae900c92987ee3dae8f7d4b14076d64f61
zakhap <zhs2104@columbia.edu>
2020-04-15 13:00:27 -0400

    Filer out private threads from bulkThreads.

--- server/routes/bulkThreads.ts
+++ server/routes/bulkThreads.ts
@@ -3,11 +3,33 @@ import lookupCommunityIsVisibleToUser from '../util/lookupCommunityIsVisibleToUs
 import { UserRequest } from '../types';
 
 const bulkThreads = async (models, req: UserRequest, res: Response, next: NextFunction) => {
+  const { Op } = models.sequelize;
   const [chain, community] = await lookupCommunityIsVisibleToUser(models, req.query, req.user, next);
 
-  const threads = await models.OffchainThread.findAll({
-    where: community ? { community: community.id }
-      : chain ? { chain: chain.id } : {},
+  // const userAddresses = req.user.getAddresses().filter((address) => !!address.verified);
+  const userAddresses = req.user.getAddresses();
+  const userAddressIds = Array.from(userAddresses.map((address) => address.id));
+  console.dir(userAddresses);
+  console.dir(userAddressIds);
+  console.dir(community);
+  const roles = await models.Role.findAll({
+    where: {
+      address_id: { [Op.in]: userAddressIds },
+    }
+  });
+
+  console.dir(roles);
+
+  const publicThreads = await models.OffchainThread.findAll({
+    where:
+      community
+        ? {
+          community: community.id,
+          private: false,
+        } : chain ? {
+          chain: chain.id,
+          private: false,
+        } : {},
     include: [
       models.Address,
       {
@@ -21,6 +43,25 @@ const bulkThreads = async (models, req: UserRequest, res: Response, next: NextFu
     ],
     order: [['created_at', 'DESC']],
   });
+
+  const threads = publicThreads;
+
+  // const threads = await models.OffchainThread.findAll({
+  //   where: community ? { community: community.id }
+  //     : chain ? { chain: chain.id } : {},
+  //   include: [
+  //     models.Address,
+  //     {
+  //       model: models.OffchainTag,
+  //       as: 'tags',
+  //       through: {
+  //         model: models.TaggedThread,
+  //         as: 'taggedThreads',
+  //       },
+  //     },
+  //   ],
+  //   order: [['created_at', 'DESC']],
+  // });
   return res.json({ status: 'Success', result: threads.map((c) => c.toJSON()) });
 };
 
