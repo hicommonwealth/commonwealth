48eaed839e30d8810738229f5ac8feff7ea5942c
zakhap <zhs2104@columbia.edu>
2020-04-20 16:40:22 -0400

    Add two fail tests.

--- test/unit/api/threads.spec.ts
+++ test/unit/api/threads.spec.ts
@@ -23,16 +23,25 @@ describe('Thread Tests', () => {
     const chain = 'ethereum';
     let adminJWT;
     let adminAddress;
+    let userJWT;
+    let userAddress;
     let thread;
 
     before(async () => {
-      const res = await modelUtils.createAndVerifyAddress({ chain });
+      let res = await modelUtils.createAndVerifyAddress({ chain });
       adminAddress = res.address;
       adminJWT = jwt.sign({ id: res.user_id, email: res.email }, JWT_SECRET);
       const isAdmin = await modelUtils.assignAdmin(res.address_id, chain);
       expect(adminAddress).to.not.be.null;
       expect(adminJWT).to.not.be.null;
       expect(isAdmin).to.not.be.null;
+
+      res = await modelUtils.createAndVerifyAddress({ chain });
+      userAddress = res.address;
+      userJWT = jwt.sign({ id: res.user_id, email: res.email }, JWT_SECRET);
+      expect(userAddress).to.not.be.null;
+      expect(userJWT).to.not.be.null;
+
       const res2 = await modelUtils.createThread({
         chain,
         address: adminAddress,
@@ -158,6 +167,31 @@ describe('Thread Tests', () => {
       expect(res.body.result.private).to.be.equal(false);
       expect(res.body).to.not.be.null;
       expect(res.body.status).to.be.equal('Success');
+    });
+
+    it('User should fail to edit admin\'s post', async () => {
+      const thread_id = thread.id;
+      const kind = thread.kind;
+      const body = thread.body;
+      const recentEdit : any = { timestamp: moment(), body };
+      const versionHistory = JSON.stringify(recentEdit);
+      const readOnly = false;
+      const privacy = true;
+      const res = await chai.request(app)
+        .post('/api/editThread')
+        .set('Accept', 'application/json')
+        .send({
+          'thread_id': thread_id,
+          'kind': kind,
+          'body': encodeURIComponent(body),
+          'version_history': versionHistory,
+          'attachments[]': null,
+          'privacy': privacy,
+          'read_only': readOnly,
+          'jwt': userJWT,
+        });
+      expect(res.body.error).to.not.be.null;
+      expect(res.status).to.be.equal(500);
     });
   });
 });
