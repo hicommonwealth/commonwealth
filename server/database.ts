caa3066296c303f6ec83af3743e36545dea56677
Jake Naviasky <jake@commonwealth.im>
2020-04-21 12:59:31 -0400

    Add test-events command and remove db logging.

--- server/database.ts
+++ server/database.ts
@@ -7,7 +7,7 @@ import { DATABASE_URI } from './config';
 const sequelize = new Sequelize(DATABASE_URI, {
   // disable string operators (https://github.com/sequelize/sequelize/issues/8417)
   operatorsAliases: false,
-  logging: (process.env.NODE_ENV === 'test') ? false : (msg) => { console.log(msg); },
+  logging: (process.env.NODE_ENV === 'test') ? false : (msg) => { },
   dialectOptions: {
     requestTimeout: 10000
   },
