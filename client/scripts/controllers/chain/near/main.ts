689e3dc5fb6aa0c5c546f9c72cc45f20f553be35
Jake Naviasky <jake@commonwealth.im>
2020-04-22 15:34:26 -0400

    Move chain adapter server loading to super.init call.

--- client/scripts/controllers/chain/near/main.ts
+++ client/scripts/controllers/chain/near/main.ts
@@ -14,21 +14,14 @@ export default class Near extends IChainAdapter<NearToken, any> {
   private _loaded: boolean = false;
   get loaded() { return this._loaded; }
 
-  private _serverLoaded: boolean = false;
-  get serverLoaded() { return this._serverLoaded; }
-
-  public init = async (onServerLoaded?) => {
+  public async init(onServerLoaded?) {
     console.log(`Starting ${this.meta.chain.id} on node: ${this.meta.url}`);
     this.chain = new NearChain(this.app);
-    this.app.chainModuleReady.next(true);
     this.accounts = new NearAccounts(this.app);
-    await this.app.threads.refreshAll(this.id, null, true);
-    await this.app.comments.refreshAll(this.id, null, true);
-    await this.app.reactions.refreshAll(this.id, null, true);
-    this._serverLoaded = true;
-    if (onServerLoaded) await onServerLoaded();
 
-    await this.chain.init(this.meta);
+    await super.init(async () => {
+      await this.chain.init(this.meta);
+    }, onServerLoaded);
     await this.accounts.init(this.chain);
 
     this._loaded = true;
