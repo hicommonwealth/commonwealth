81f06721e4090546180c5b424b1e5209c7b5e0ef
Raymond Zhong <raykyri@gmail.com>
2020-04-22 14:19:46 -0700

    fix import

--- server/webhookNotifier.ts
+++ server/webhookNotifier.ts
@@ -1,5 +1,5 @@
 import request from 'superagent';
-import { NotificationCategories } from 'types';
+import { NotificationCategories } from '../shared/types';
 
 export interface WebhookContent {
   notificationCategory: string;
