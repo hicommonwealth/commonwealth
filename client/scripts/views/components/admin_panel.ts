12d1d71a252cc69349b5e8ae9dade65d7515e17c
zakhap <zhs2104@columbia.edu>
2020-04-23 10:51:09 -0400

    Create admin panel component.

---
+++ client/scripts/views/components/admin_panel.ts
@@ -0,0 +1,64 @@
+import m from 'mithril';
+import $ from 'jquery';
+import { OffchainThread, OffchainTag } from 'models';
+import { Button, Classes, Dialog, Icon, Icons, Tag, TagInput } from 'construct-ui';
+import app from 'state';
+
+interface ITagEditorAttrs {
+  thread: OffchainThread;
+  onChangeHandler: Function;
+}
+
+const CommunityMetadata: m.Component = {
+  view: (vnode) => {
+    return m(TagInput);
+  }
+};
+
+const ChainMetadata: m.Component = {
+  view: (vnode) => {
+    return m(TagInput);
+  }
+};
+
+const Panel: m.Component = {
+  view: (vnode) => {
+    return m('.Panel', [
+
+    ]);
+  }
+};
+
+const AdminPanel: m.Component<{}, {isOpen: boolean}> = {
+  oninit: (vnode) => {
+    vnode.state.isOpen = false;
+  },
+  oncreate: (vnode) => {
+  },
+  view: (vnode) => {
+    return m('AdminPanel', [
+      m('a', {
+        href: '#',
+        onclick: (e) => { e.preventDefault(); vnode.state.isOpen = true; }
+      }, [
+        'Manage Community'
+      ]),
+      m(Dialog, {
+        autofocus: true,
+        basic: false,
+        closeOnEscapeKey: true,
+        closeOnOutsideClick: true,
+        content: m(Panel),
+        hasBackdrop: true,
+        isOpen: vnode.state.isOpen,
+        inline: false,
+        onClose: () => { vnode.state.isOpen = false; },
+        title:'Manage Community',
+        transitionDuration: 200,
+        footer: m('div'),
+      }),
+    ]);
+  },
+};
+
+export default AdminPanel;
