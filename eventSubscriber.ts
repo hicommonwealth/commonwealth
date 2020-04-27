41049f9f8817206b22241ff83a687a65e8b9dc4a
Drew Stone <drewstone329@gmail.com>
2020-04-27 16:27:37 -0600

    Adds flag for event logging

--- eventSubscriber.ts
+++ eventSubscriber.ts
@@ -2,11 +2,11 @@ import subscribeEdgewareEvents from './shared/events/edgeware/index';
 import { IEventHandler, CWEvent } from './shared/events/interfaces';
 
 const url = process.env.NODE_URL || undefined;
-
+const DEV = process.env.NODE_ENV !== 'production';
 class StandaloneSubstrateEventHandler extends IEventHandler {
   public async handle(event: CWEvent) {
     // just prints the event
-    console.log(`Received event: ${JSON.stringify(event, null, 2)}`);
+    if (DEV) console.log(`Received event: ${JSON.stringify(event, null, 2)}`);
   }
 }
 
