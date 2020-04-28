af613bf81e7bebf288195a0cd6bde95dfecbdc9e
Graham Johnson <graham@commonwealth.im>
2020-04-28 13:49:02 -0400

    Implement Quill checklist functionality

--- client/scripts/views/components/markdown_formatted_text.ts
+++ client/scripts/views/components/markdown_formatted_text.ts
@@ -140,6 +140,13 @@ function applyBlockFormatters(text, hideFormatting) {
       formatMany: (text) => m('ul', m('ul', m('ul', text))),
       formatOne: (text, match) =>
         m('li', applyInlineFormatters(text.replace(match, ''), hideFormatting)),
+    }, {
+      pattern: /^\[([ x])\] /,
+      formatMany: (text) => m('ul.checklist', text),
+      formatOne: (text, match) => {
+        debugger
+        return m(`li${match.includes('x') ? '.checked' : '.unchecked'}`, applyInlineFormatters(text.replace(match, ''), hideFormatting));
+      }
     }];
 
     // Lines which don't match any of the above groups are assigned an
