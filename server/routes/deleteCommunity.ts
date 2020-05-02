d9ec50c52e11685a8d3d0a1544b04d59990a6b7f
Raymond Zhong <raykyri@gmail.com>
2020-05-02 15:10:01 -0700

    filter on verified

--- server/routes/deleteCommunity.ts
+++ server/routes/deleteCommunity.ts
@@ -16,7 +16,7 @@ const deleteCommunity = async (models, req: UserRequest, res: Response, next: Ne
     const community = await models.OffchainCommunity.findOne({
       where: { id: req.body.community_id },
     });
-    if (userOwnedAddresses.map((addr) => addr.id).indexOf(community.creator_id) === -1) {
+    if (userOwnedAddresses.filter((addr) => addr.verified).map((addr) => addr.id).indexOf(community.creator_id) === -1) {
       return next(new Error('Only the original creator can delete this community'));
     }
     const communityTags = await community.getTags();
--- server/routes/editThread.ts
+++ server/routes/editThread.ts
@@ -44,7 +44,7 @@ const editThread = async (models, req: UserRequest, res: Response, next: NextFun
     const thread = await models.OffchainThread.findOne({
       where: { id: thread_id },
     });
-    if (userOwnedAddresses.map((addr) => addr.id).indexOf(thread.author_id) === -1) {
+    if (userOwnedAddresses.filter((addr) => addr.verified).map((addr) => addr.id).indexOf(thread.author_id) === -1) {
       return next(new Error('Not owned by this user'));
     }
     const arr = thread.version_history;
