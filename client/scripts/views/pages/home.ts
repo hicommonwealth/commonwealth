1c6fd2381475d091c17fe86e446d930501ee6320
gdjohnson <graham@commonwealth.im>
2020-05-14 20:01:25 -0400

    Construct UI: Homepage Icons (#195)
    * Add requisite components to homepage
    
    * Styling
    
    * Separate Pseudoheader out into separate component
    
    * Clean up imports, add types to mithril
    
    * Separate out Pseudoheader

--- client/scripts/views/pages/home.ts
+++ client/scripts/views/pages/home.ts
@@ -2,16 +2,14 @@ import 'pages/home.scss';
 
 import { default as m } from 'mithril';
 import HomepageCommunities from 'views/components/homepage_communities';
+import PseudoHeader from 'views/components/pseudoheader';
 
 const HomePage : m.Component<{}, {}> = {
-  view: (vnode) => {
+  view: (vnode: m.VnodeDOM) => {
     return m('.HomePage', [
       m('.home-content', [
         m('.container', [
-          m('h2.lead-title', 'On-chain communities'),
-          m('p.lead-description', [
-            'Forums, profiles, and voting for decentralized organizations',
-          ]),
+          m(PseudoHeader),
           m(HomepageCommunities),
         ]),
       ]),
