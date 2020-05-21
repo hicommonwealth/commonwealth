3a52047db8f78e060e2513bf594e7a3c615cdbae
Graham Johnson <graham@commonwealth.im>
2020-05-21 13:34:12 -0400

    Switch 404 to Construct empty state page

--- client/scripts/views/pages/404.ts
+++ client/scripts/views/pages/404.ts
@@ -2,29 +2,19 @@ import 'pages/404.scss';
 
 import { default as m } from 'mithril';
 import { default as mixpanel } from 'mixpanel-browser';
+import { EmptyState, Icon, Icons } from 'construct-ui';
 
 const PageNotFound: m.Component<{}> = {
   oncreate: (vnode) => {
     mixpanel.track('PageVisit', { 'Page Name': '404Page' });
   },
   view: (vnode) => {
-    return m('.PageNotFound', [
-      m('.home-header', [
-        m('.container', [
-          m('.row.row-narrow', [
-            m('.col-sm-2.col-md-1', [
-              m('h2', '404'),
-            ]),
-            m('.col-sm-10.col-md-11', [
-              m('.info', [
-                m('p', 'The page you were looking for could not be found. '),
-                m('p', 'If it is not visible to the public, you may need to log in.'),
-              ]),
-            ]),
-          ]),
-        ]),
-      ]),
-    ]);
+    return m(EmptyState, {
+      class: '.PageNotFound',
+      icon: Icons.X_OCTAGON,
+      header: 'The page you were looking for could not be found.',
+      content: 'If it is not visible to the public, you may need to log in.'
+    });
   }
 };
 
