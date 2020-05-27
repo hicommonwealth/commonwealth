d0493ecc98f31b8a962dbce5121e86c678068249
Raymond Zhong <raykyri@gmail.com>
2020-05-27 00:49:22 -0700

    Add migration to remove ETH chain from frontend

---
+++ server/migrations/20200527073233-drop-eth-chain.js
@@ -0,0 +1,11 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.sequelize.query('DELETE FROM "ChainNodes" WHERE chain = \'ethereum\';');
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.sequelize.query('INSERT INTO "ChainNodes" (chain, url) VALUES (\'ethereum\', \'wss://mainnet.infura.io/ws\');');
+  }
+};
