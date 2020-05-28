b8451e3c835460fbacfb265bc20ac38cd0985bba
Mir Nawaz <inbox.mirnawaz@gmail.com>
2020-05-28 21:44:46 +0500

    staking ui global statistics and separate controller for statistics

--- client/scripts/controllers/chain/substrate/staking.ts
+++ client/scripts/controllers/chain/substrate/staking.ts
@@ -3,15 +3,20 @@ import BN from 'bn.js';
 import { IApp } from 'state';
 import { Vec } from '@polkadot/types';
 import { ApiRx } from '@polkadot/api';
+import { formatNumber } from '@polkadot/util';
 import { Observable, combineLatest, of } from 'rxjs';
 import { map, flatMap, auditTime, switchMap } from 'rxjs/operators';
-import { EraIndex, AccountId, Exposure, SessionIndex } from '@polkadot/types/interfaces';
+import { EraIndex, AccountId, Exposure, SessionIndex, EraRewardPoints } from '@polkadot/types/interfaces';
+import { InterfaceTypes } from '@polkadot/types/types';
 import { DeriveStakingValidators, DeriveStakingElected, DeriveSessionProgress } from '@polkadot/api-derive/types';
-import SubstrateAccounts, { IValidatorInfo, IValidators } from './account';
+import SubstrateAccounts, { IValidators } from './account';
 
 class SubstrateStaking extends SubstrateAccounts {
   constructor(app: IApp) {
     super(app);
+  }
+  public createType<K extends keyof InterfaceTypes>(type: K, ...params: any[]): InterfaceTypes[K] {
+    return this._Chain.registry.createType(type, ...params);
   }
   public get validatorCount(): Observable<SessionIndex> {
     return this._Chain.query((api: ApiRx) => api.query.staking.validatorCount())
@@ -27,31 +32,25 @@ class SubstrateStaking extends SubstrateAccounts {
         of(api),
         api.derive.staking.validators(),
         api.query.staking.currentEra(),
-        api.derive.staking.electedInfo(),
-        api.derive.staking.stashes()
+        api.derive.staking.stashes(),
+        api.derive.staking.currentPoints()
       )),
 
       // fetch balances alongside validators
       flatMap((
-        [api, { nextElected, validators: currentSet }, era, electedInfo, allStashes]:
-        [ApiRx, DeriveStakingValidators, EraIndex, DeriveStakingElected, any]
+        [api, { nextElected, validators: currentSet }, era, allStashes, queryPoints]:
+        [ApiRx, DeriveStakingValidators, EraIndex, AccountId[], EraRewardPoints]
       ) => {
+        const eraPoints: Record<string, string> = {};
+        const entries = [...queryPoints.individual.entries()]
+          .map(([accountId, points]) => [accountId.toString(), formatNumber(points)]);
+        entries.forEach(([accountId, points]): void => {
+          eraPoints[accountId] = points;
+        });
+
         // set of not yet but future validators
         const waiting = allStashes.filter((v) => !currentSet.includes(v));
         const toBeElected = nextElected.filter((v) => !currentSet.includes(v));
-        const validatorsInfo: IValidatorInfo = {};
-
-        electedInfo.info.forEach(({ accountId, validatorPrefs }) => {
-          const commissionPer = (validatorPrefs.commission.unwrap() || new BN(0)).toNumber() / 10_000_000;
-          const isCommission = !!validatorPrefs.commission;
-          const key = accountId.toString();
-          const details = {
-            commissionPer,
-            isCommission
-          };
-          validatorsInfo[key] = details;
-          return details;
-        });
 
         // Different runtimes call for different access to stakers: old vs. new
         const stakersCall = (api.query.staking.stakers) ? api.query.staking.stakers : api.query.staking.erasStakers;
@@ -64,15 +63,17 @@ class SubstrateStaking extends SubstrateAccounts {
           stakersCall.multi(currentSet.map((elt) => stakersCallArgs(elt.toString()))),
           api.query.staking.bonded.multi(toBeElected.map((elt) => elt.toString())),
           stakersCall.multi(toBeElected.map((elt) => stakersCallArgs(elt.toString()))),
-          of(validatorsInfo),
-          of(waiting)
+          of(waiting),
+          of(eraPoints)
         );
       }),
       auditTime(100),
       map(([
-        currentSet, toBeElected, controllers, exposures, nextUpControllers, nextUpExposures, validatorsInfo, waiting
+        currentSet, toBeElected, controllers, exposures,
+        nextUpControllers, nextUpExposures, waiting, eraPoints
       ] : [
-        AccountId[], AccountId[], Vec<AccountId>, Exposure[], Vec<AccountId>, Exposure[], IValidatorInfo, Uint32Array[]
+        AccountId[], AccountId[], Vec<AccountId>, Exposure[],
+        Vec<AccountId>, Exposure[], Uint32Array[], Record<string, string>
       ]) => {
         const result: IValidators = {};
         for (let i = 0; i < currentSet.length; ++i) {
@@ -82,9 +83,7 @@ class SubstrateStaking extends SubstrateAccounts {
             controller: controllers[i].toString(),
             isElected: true,
             isWaiting: false,
-            commissionPer: validatorsInfo[key]
-              ? validatorsInfo[key].commissionPer
-              : 0
+            eraPoints: eraPoints[key]
           };
         }
 
@@ -96,9 +95,7 @@ class SubstrateStaking extends SubstrateAccounts {
             controller: nextUpControllers[i].toString(),
             isElected: false,
             isWaiting: false,
-            commissionPer: validatorsInfo[key]
-              ? validatorsInfo[key].commissionPer
-              : 0
+            eraPoints: eraPoints[key]
           };
         }
         // add set of next elected
@@ -109,9 +106,7 @@ class SubstrateStaking extends SubstrateAccounts {
             controller: nextUpControllers[i].toString(),
             isElected: false,
             isWaiting: false,
-            commissionPer: validatorsInfo[key]
-              ? validatorsInfo[key].commissionPer
-              : 0
+            eraPoints: eraPoints[key]
           };
         }
         // add set of waiting validators
@@ -119,10 +114,10 @@ class SubstrateStaking extends SubstrateAccounts {
           const key = waiting[i].toString();
           result[key] = {
             exposure: null,
-            controller: 'controller',
+            controller: null,
             isElected: false,
             isWaiting: true,
-            commissionPer: 0
+            eraPoints: eraPoints[key]
           };
         }
 
@@ -130,16 +125,13 @@ class SubstrateStaking extends SubstrateAccounts {
       }),
     );
   }
-
   public info(address: string): Observable<any> {
-    // console.log(' getinfo  ', address);
     return this._Chain.query((api: ApiRx) => api.derive.accounts.info(address))
       .pipe(
         map((info) => info)
       );
   }
   public query(address: string): Observable<any> {
-    // console.log(' getinfo  ', address);
     return this._Chain.query((api: ApiRx) => api.derive.staking.query(address))
       .pipe(
         map((info) => info)
