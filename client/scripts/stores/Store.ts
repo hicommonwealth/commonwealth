b4ef5d1db4e7775979c03feea59c84561b2d3ea4
Jake Naviasky <jake@commonwealth.im>
2020-05-28 14:05:26 -0400

    Remove observable and update call from stores.

--- client/scripts/stores/Store.ts
+++ client/scripts/stores/Store.ts
@@ -1,17 +1,8 @@
-// TODO: we can make this even better if we have T implement a StoreKey mixin,
-import { BehaviorSubject, Observable } from 'rxjs';
-import { filter } from 'rxjs/operators';
-import { IStoreUpdate, UpdateType } from './interfaces';
-
-// then we can look up items by key, prevent duplicates from being inserted, etc
 abstract class Store<T> {
   protected _store: T[] = [];
 
-  private _subject: BehaviorSubject<IStoreUpdate<T> | null> = new BehaviorSubject(null);
-
   public add(item: T): Store<T> {
     this._store.push(item);
-    this._subject.next({ item, updateType: UpdateType.Add });
     return this;
   }
 
@@ -22,26 +13,15 @@ abstract class Store<T> {
       return this;
     }
     this._store.splice(index, 1);
-    this._subject.next({ item, updateType: UpdateType.Remove });
-    return this;
-  }
-
-  public update(item: T): Store<T> {
-    this._subject.next({ item, updateType: UpdateType.Update });
     return this;
   }
 
   public clear() {
     this._store = [];
-    this._subject = new BehaviorSubject(null);
   }
 
   public getAll(): T[] {
     return this._store;
-  }
-
-  public getObservable(): Observable<IStoreUpdate<T>> {
-    return this._subject.asObservable().pipe(filter((s) => s !== null));
   }
 }
 
