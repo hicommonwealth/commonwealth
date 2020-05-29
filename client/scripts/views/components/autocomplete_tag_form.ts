27b9285902ed0be92c96464e3ed6abaae8a65ea4
Graham Johnson <graham@commonwealth.im>
2020-05-29 12:05:17 -0400

    New tag form up and running

--- client/scripts/views/components/autocomplete_tag_form.ts
+++ client/scripts/views/components/autocomplete_tag_form.ts
@@ -30,18 +30,23 @@ const AutoCompleteTagForm: m.Component<IAutoCompleteTagFormAttrs, IAutoCompleteT
       });
     };
 
+    const manuallyClosePopover = () => {
+      const button = document.getElementsByClassName('cui-popover-trigger-active')[0]
+      if (button) (button as HTMLButtonElement).click();
+    };
+
     const selectTag = (e: Event) => {
       const { innerText } = (e.target as HTMLElement);
       const tag = vnode.attrs.tags.filter((t) => t.name === innerText.slice(2))[0];
       vnode.state.selectedTag = tag;
       vnode.attrs.updateFormData(tag);
-      console.log(vnode.state.selectedTag);
-      m.redraw();
+      manuallyClosePopover();
     };
 
     const featuredTags = vnode.attrs.featuredTags.map((tag, idx) => TagItem(tag, idx, selectTag));
 
     return m(SelectList, {
+      closeOnSelect: false,
       emptyContent: m('.no-matching-tags', { onclick: selectTag }, 'No matching tags found'),
       initialContent: featuredTags,
       itemRender: TagItem,
