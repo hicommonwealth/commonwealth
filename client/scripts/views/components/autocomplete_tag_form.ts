453fb19d381ed6c27a87da13d11cef6030f02dc3
Graham Johnson <graham@commonwealth.im>
2020-05-29 15:32:46 -0400

    Restructuring ListSelector to display all tags

--- client/scripts/views/components/autocomplete_tag_form.ts
+++ client/scripts/views/components/autocomplete_tag_form.ts
@@ -1,7 +1,7 @@
 import 'components/autocomplete_tag_form.scss';
 
 import { default as m } from 'mithril';
-import { SelectList, ListItem, Colors, Button, Icons } from 'construct-ui';
+import { SelectList, ListItem, Colors, Button, Icons, List } from 'construct-ui';
 import { OffchainTag } from 'client/scripts/models';
 
 
@@ -21,43 +21,43 @@ interface IAutoCompleteTagFormState {
 
 const AutoCompleteTagForm: m.Component<IAutoCompleteTagFormAttrs, IAutoCompleteTagFormState> = {
   view: (vnode) => {
-    const { defaultActiveIndex, tags, updateFormData } = vnode.attrs;
-    const TagItem = (tag: OffchainTag, index?: number, onclick?: (e: Event) => void) => {
-      return m(ListItem, {
-        allowOnContentClick: true,
+    const { defaultActiveIndex, featuredTags, tags, updateFormData } = vnode.attrs;
+
+    const itemListRender = (items: any[]) => {
+      const listItem = (tag) => m(ListItem, {
         contentLeft: m('.tagItem', `# ${tag.name}`),
-        key: index,
-        selected: vnode.state.selectedTag && vnode.state.selectedTag.name === tag.name,
-        onclick
+        selected: vnode.state.selectedTag && vnode.state.selectedTag.name === tag.name,    
       });
+      return m(List, [
+        featuredTags.map((tag) => listItem(tag)),
+        m('.divider-line', '---'),
+        tags.map((tag) => listItem(tag))
+      ]);
     };
 
-    const manuallyClosePopover = () => {
-      const button = document.getElementsByClassName('tag-selection-drop-menu')[0];
-      if (button) (button as HTMLButtonElement).click();
-    };
+    // const manuallyClosePopover = () => {
+    //   const button = document.getElementsByClassName('tag-selection-drop-menu')[0];
+    //   if (button) (button as HTMLButtonElement).click();
+    // };
 
     const selectTag = (e: Event) => {
       const { innerText } = (e.target as HTMLElement);
       const tag = tags.filter((t) => t.name === innerText.slice(2))[0];
       vnode.state.selectedTag = tag;
       updateFormData(tag);
-      manuallyClosePopover();
     };
 
-    const featuredTags = vnode.attrs.featuredTags.map((tag, idx) => TagItem(tag, idx, selectTag));
+    // const featuredTags = vnode.attrs.featuredTags.map((tag, idx) => TagItem(tag, idx, selectTag));
 
     return m(SelectList, {
-      closeOnSelect: false,
       defaultActiveIndex,
       emptyContent: m('.no-matching-tags', { onclick: selectTag }, 'No matching tags found'),
-      initialContent: featuredTags,
-      itemRender: TagItem,
-      items: tags,
+      itemListRender,
+      itemRender: () => null,
+      items: featuredTags.concat(tags),
       onSelect: (item: OffchainTag) => {
         vnode.state.selectedTag = item;
         updateFormData(item);
-        manuallyClosePopover();
       },
       trigger: m(Button, {
         align: 'left',
