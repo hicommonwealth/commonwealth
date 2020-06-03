03bca9e0906cacc92d3b0da5910726b38f64bc16
Jake Naviasky <jake@commonwealth.im>
2020-06-03 12:06:09 -0400

    Recompile types for truffle-v5.

--- scripts/build-contracts.sh
+++ scripts/build-contracts.sh
@@ -1,6 +1,6 @@
 truffle compile &&
-./node_modules/typechain/dist/cli/cli.js --target=ethers --outDir contracts 'contractbase/eth/build/contracts/ERC*.json'
-./node_modules/typechain/dist/cli/cli.js --target=ethers --outDir contracts/MolochV2 'contractbase/eth/build/contracts/Moloch2.json'
-./node_modules/typechain/dist/cli/cli.js --target=ethers --outDir contracts/MolochV2 'contractbase/eth/build/contracts/GuildBank2.json'
-./node_modules/typechain/dist/cli/cli.js --target=ethers --outDir contracts/MolochV1 'contractbase/eth/build/contracts/Moloch1.json'
-./node_modules/typechain/dist/cli/cli.js --target=ethers --outDir contracts/MolochV1 'contractbase/eth/build/contracts/GuildBank1.json'
+./node_modules/typechain/dist/cli/cli.js --target=truffle-v5 --outDir contracts 'build/contracts/ERC*.json'
+./node_modules/typechain/dist/cli/cli.js --target=truffle-v5 --outDir contracts/MolochV2 'build/contracts/Moloch2.json'
+./node_modules/typechain/dist/cli/cli.js --target=truffle-v5 --outDir contracts/MolochV2 'build/contracts/GuildBank2.json'
+./node_modules/typechain/dist/cli/cli.js --target=truffle-v5 --outDir contracts/MolochV1 'build/contracts/Moloch1.json'
+./node_modules/typechain/dist/cli/cli.js --target=truffle-v5 --outDir contracts/MolochV1 'build/contracts/GuildBank1.json'
