3f27e153ac7c74d5f932990c07613f35ae5dea1c
Graham Johnson <graham@commonwealth.im>
2020-06-03 18:00:14 -0400

    Rm joins table up

---
+++ server/migrations/20200603210325-remove-tagged-threads-joins-table.js
@@ -0,0 +1,17 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Memberships');
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+  }
+};
