b5a840fcd423942f4a4bcde9a0a62efcb8638592
Jake Naviasky <jake@commonwealth.im>
2020-06-04 13:26:43 -0400

    Flatten typescript output structure.

--- eth/build-contracts.sh
+++ eth/build-contracts.sh
@@ -1,6 +1,2 @@
 truffle compile &&
-../node_modules/typechain/dist/cli/cli.js --target=truffle-v5 --outDir './types' './build/contracts/ERC*.json'
-../node_modules/typechain/dist/cli/cli.js --target=truffle-v5 --outDir './types/MolochV2' './build/contracts/Moloch2.json'
-../node_modules/typechain/dist/cli/cli.js --target=truffle-v5 --outDir './types/MolochV2' './build/contracts/GuildBank2.json'
-../node_modules/typechain/dist/cli/cli.js --target=truffle-v5 --outDir './types/MolochV1' './build/contracts/Moloch1.json'
-../node_modules/typechain/dist/cli/cli.js --target=truffle-v5 --outDir './types/MolochV1' './build/contracts/GuildBank1.json'
+../node_modules/typechain/dist/cli/cli.js --target=truffle-v5 --outDir './types' './build/contracts/*.json'
