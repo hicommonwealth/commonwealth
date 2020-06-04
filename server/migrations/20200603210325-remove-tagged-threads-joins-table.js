d10588ea01c624e843a3c0f22edac7713c476b6c
Graham Johnson <graham@commonwealth.im>
2020-06-04 15:25:39 -0400

    Finish taggedThreads migration

--- server/migrations/20200603210325-remove-tagged-threads-joins-table.js
+++ server/migrations/20200603210325-remove-tagged-threads-joins-table.js
@@ -1,17 +1,26 @@
+/* eslint-disable quotes */
 'use strict';
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Memberships');
+    return queryInterface.dropTable('TaggedThreads');
   },
 
-  down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.dropTable('users');
-    */
+  down: async (queryInterface, DataTypes) => {
+    await queryInterface.createTable('TaggedThreads', {
+      id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true, allowNull: false },
+      tag_id: { type: DataTypes.INTEGER, allowNull: false },
+      thread_id: { type: DataTypes.INTEGER, allowNull: false },
+      created_at: { type: DataTypes.DATE, allowNull: false },
+      updated_at: { type: DataTypes.DATE, allowNull: false },
+    });
+    const [threads] = await queryInterface.sequelize.query(`SELECT * FROM "OffchainThreads";`);
+    await Promise.all(threads.map(async (thread) => {
+      const { id, tag_id } = thread;
+      if (tag_id) {
+        const query = `INSERT INTO "TaggedThreads" (tag_id, thread_id, created_at, updated_at) VALUES (${tag_id}, ${id}, NOW(), NOW())`;
+        await queryInterface.sequelize.query(query);
+      }
+    }));
   }
 };
