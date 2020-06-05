e9d8b6aa2ecf02738be8cdab9db507f1164986b5
Jake Naviasky <jake@commonwealth.im>
2020-06-05 13:19:11 -0400

    Stub out labeler and implement titler.

--- shared/events/moloch/filters/labeler.ts
+++ shared/events/moloch/filters/labeler.ts
@@ -1,5 +1,11 @@
 import { LabelerFilter, IEventLabel } from '../../interfaces';
-import { IMolochEventData } from '../types';
+import { IMolochEventData, MolochEventKind } from '../types';
+
+function fmtAddr(addr : string) {
+  if (!addr) return;
+  if (addr.length < 16) return addr;
+  return `${addr.slice(0, 7)}…${addr.slice(addr.length - 3)}`;
+}
 
 /**
  * This a labeler function, which takes event data and describes it in "plain english",
@@ -10,7 +16,42 @@ const labelMolochEvent: LabelerFilter = (
   chainId: string,
   data: IMolochEventData,
 ): IEventLabel => {
-  return null;
+  switch (data.kind) {
+    case MolochEventKind.SubmitProposal: return {
+      heading: 'Proposal Submitted',
+      label: '',
+    };
+    case MolochEventKind.SubmitVote: return {
+      heading: 'Vote Submitted',
+      label: '',
+    };
+    case MolochEventKind.ProcessProposal: return {
+      heading: 'Proposal Processed',
+      label: '',
+    };
+    case MolochEventKind.Ragequit: return {
+      heading: 'Member Ragequit',
+      label: '',
+    };
+    case MolochEventKind.Abort: return {
+      heading: 'Proposal Aborted',
+      label: '',
+    };
+    case MolochEventKind.UpdateDelegateKey: return {
+      heading: 'Delegate Key Updated',
+      label: '',
+    };
+    // this event should never appear
+    case MolochEventKind.SummonComplete: return {
+      heading: 'Summon Complete',
+      label: `Moloch is summoned, by user ${fmtAddr(data.summoner)} with ${data.shares}!`,
+    };
+    default: {
+      // ensure exhaustive matching -- gives ts error if missing cases
+      const _exhaustiveMatch: never = data;
+      throw new Error('unknown event type');
+    }
+  }
 };
 
 export default labelMolochEvent;
--- shared/events/moloch/filters/titler.ts
+++ shared/events/moloch/filters/titler.ts
@@ -7,7 +7,41 @@ import { IEventTitle, TitlerFilter } from '../../interfaces';
  * on the client to present a list of subscriptions that a user might want to subscribe to.
  */
 const titlerFunc: TitlerFilter = (kind: MolochEventKind): IEventTitle => {
-  return null;
+  switch (kind) {
+    case MolochEventKind.SubmitProposal: return {
+      title: 'Proposal Submitted',
+      description: 'A new proposal is submitted.',
+    };
+    case MolochEventKind.SubmitVote: return {
+      title: 'Vote Submitted',
+      description: 'A proposal is voted on.',
+    };
+    case MolochEventKind.ProcessProposal: return {
+      title: 'Proposal Processed',
+      description: 'A proposal is completed and processed.',
+    };
+    case MolochEventKind.Ragequit: return {
+      title: 'Member Ragequit',
+      description: 'A member ragequits.',
+    };
+    case MolochEventKind.Abort: return {
+      title: 'Proposal Aborted',
+      description: 'A proposal is aborted by its applicant.',
+    };
+    case MolochEventKind.UpdateDelegateKey: return {
+      title: 'Delegate Key Updated',
+      description: 'A member updates their delegate key.',
+    };
+    case MolochEventKind.SummonComplete: return {
+      title: 'Summon Complete',
+      description: 'The contract is summoned (never emitted).',
+    };
+    default: {
+      // ensure exhaustive matching -- gives ts error if missing cases
+      const _exhaustiveMatch: never = kind;
+      throw new Error('unknown event type');
+    }
+  }
 };
 
 export default titlerFunc;
