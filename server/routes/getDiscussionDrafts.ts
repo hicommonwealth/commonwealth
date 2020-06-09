2da08936e253fd78703093f338bfc20f00aa4a75
Graham Johnson <graham@commonwealth.im>
2020-06-09 11:45:30 -0400

    Add route to grab user drafts

---
+++ server/routes/getDiscussionDrafts.ts
@@ -0,0 +1,18 @@
+import { Request, Response, NextFunction } from 'express';
+import { factory, formatFilename } from '../../shared/logging';
+import lookupAddressIsOwnedByUser from '../util/lookupAddressIsOwnedByUser';
+const log = factory.getLogger(formatFilename(__filename));
+
+const getDiscussionDrafts = async (models, req: Request, res: Response, next: NextFunction) => {
+  const author = await lookupAddressIsOwnedByUser(models, req, next);
+
+  const drafts = await models.DiscussionDraft.findAll({
+    where: {
+      author_id: author.id,
+    }
+  });
+
+  return res.json({ status: 'Success', result: drafts });
+};
+
+export default getDiscussionDrafts;
