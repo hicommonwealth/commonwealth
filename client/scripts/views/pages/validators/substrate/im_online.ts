d8694ad818c77fbaa8dfc1a156cff4ecd1340b4d
Furqan Ahmed <nblogist1@gmail.com>
2020-06-13 23:37:52 +0500

    Add descriptions on-hover over icons in validator list.

--- client/scripts/views/pages/validators/substrate/im_online.ts
+++ client/scripts/views/pages/validators/substrate/im_online.ts
@@ -1,5 +1,5 @@
 import m from 'mithril';
-import { Icons, Icon } from 'construct-ui';
+import { Icons, Icon, Tooltip } from 'construct-ui';
 import { u32 } from '@polkadot/types';
 
 interface ImOnlineAttrs {
@@ -11,17 +11,28 @@ interface ImOnlineAttrs {
 
 const ImOnline: m.Component<ImOnlineAttrs, {}> = {
   view: (vnode) => {
-    return m('td.val-im-online',
-      m('span.im-online-icons', [
-        vnode.attrs.toBeElected
-    && m(Icon, { name: Icons.ARROW_LEFT_CIRCLE, size: 'sm' }),
-        vnode.attrs.isOnline
-    && m(Icon, { name: Icons.WIFI, size: 'sm' }),
-        vnode.attrs.hasMessage
-    && m(Icon, { name: Icons.MESSAGE_SQUARE, size: 'sm' }),
-        vnode.attrs.blockCount
-    && m('label.block-count', vnode.attrs.blockCount)
-      ]));
+    return m(
+      "td.val-im-online",
+      m("span.im-online-icons", [
+        vnode.attrs.toBeElected &&
+        m(Tooltip, {
+          trigger: m(Icon, { name: Icons.ARROW_LEFT_CIRCLE, size: "sm" }),
+          content: m("div", "Validator is to be Elected"),
+        }),
+        vnode.attrs.isOnline &&
+          m(Tooltip, {
+            trigger: m(Icon, { name: Icons.WIFI, size: "sm" }),
+            content: m("div", "Validator is Online!"),
+          }),
+        vnode.attrs.hasMessage &&
+          m(Tooltip, {
+            trigger: m(Icon, { name: Icons.MESSAGE_SQUARE, size: "sm" }),
+            content: m("div", "New Message!"),
+          }),
+        vnode.attrs.blockCount &&
+          m("label.block-count", vnode.attrs.blockCount),
+      ])
+    );
   },
 };
 
