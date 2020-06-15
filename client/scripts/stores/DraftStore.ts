947419ea82ea8cc50869ded98a7471ca4cde2585
Graham Johnson <graham@commonwealth.im>
2020-06-15 12:37:48 -0400

    Set up DraftStore, first pass

--- client/scripts/stores/DraftStore.ts
+++ client/scripts/stores/DraftStore.ts
@@ -1,32 +1,32 @@
 import IdStore from './IdStore';
-import { OffchainTag } from '../models';
+import { DiscussionDraft } from '../models';
 import { byAscendingCreationDate } from '../helpers';
 
 // TODO: Differentiate between tags associated with a chain, and tags associated with a community
-class DraftStore extends IdStore<OffchainTag> {
-  private _storeCommunity: { [identifier: string]: Array<OffchainTag> } = {};
+class TagStore extends IdStore<DiscussionDraft> {
+  private _storeCommunity: { [identifier: string]: Array<DiscussionDraft> } = {};
 
-  public add(tag: OffchainTag) {
+  public add(draft: DiscussionDraft) {
     // TODO: Remove this once we start enforcing an ordering in stores
-    super.add(tag);
+    super.add(draft);
     this.getAll().sort(byAscendingCreationDate);
-    const parentEntity = tag.communityId ? tag.communityId : tag.chainId;
+    const parentEntity = draft.community ? draft.community : draft.chain;
     if (!this._storeCommunity[parentEntity]) {
       this._storeCommunity[parentEntity] = [];
     }
-    this._storeCommunity[parentEntity].push(tag);
+    this._storeCommunity[parentEntity].push(draft);
     this._storeCommunity[parentEntity].sort(byAscendingCreationDate);
     return this;
   }
 
-  public remove(tag: OffchainTag) {
-    super.remove(tag);
-    const parentEntity = tag.communityId ? tag.communityId : tag.chainId;
+  public remove(draft: DiscussionDraft) {
+    super.remove(draft);
+    const parentEntity = draft.community ? draft.community : draft.chain;
     const communityStore = this._storeCommunity[parentEntity];
-    const matchingTag = communityStore.filter((t) => t.id === tag.id)[0];
-    const proposalIndex = communityStore.indexOf(matchingTag);
+    const matchingDraft = communityStore.filter((t) => t.id === draft.id)[0];
+    const proposalIndex = communityStore.indexOf(matchingDraft);
     if (proposalIndex === -1) {
-      throw new Error('Tag not in proposals store');
+      throw new Error('Draft not in store');
     }
     communityStore.splice(proposalIndex, 1);
     return this;
@@ -37,13 +37,13 @@ class DraftStore extends IdStore<OffchainTag> {
     this._storeCommunity = {};
   }
 
-  public getByCommunity(communityId): Array<OffchainTag> {
+  public getByCommunity(communityId): Array<DiscussionDraft> {
     return this._storeCommunity[communityId] || [];
   }
 
-  public getByName(name, communityId): OffchainTag {
+  public getByName(name, communityId): DiscussionDraft {
     return this.getByCommunity(communityId).find((t) => t.name === name);
   }
 }
 
-export default DraftStore;
+export default TagStore;
