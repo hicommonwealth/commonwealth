cd745e757460fedb93b073cd0832a0c7c61410aa
Raymond Zhong <raykyri@gmail.com>
2020-06-16 14:12:57 -0700

    Add clear tags migration

---
+++ server/migrations/20200616210948-clear-tags.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.sequelize.query(`
+DELETE FROM "OffchainTags" WHERE community_id != 'internal' OR community_id IS NULL
+`);
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return new Promise((resolve, reject) => resolve());
+  }
+};
