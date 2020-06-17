5ad11d94d37e6ef68b696ef7a199fe52e314d9bc
zakhap <zhs2104@columbia.edu>
2020-06-17 17:00:00 -0400

    Have updateEmail send verification sign in email to new email input

--- server/routes/updateEmail.ts
+++ server/routes/updateEmail.ts
@@ -1,5 +1,9 @@
 import { Request, Response, NextFunction } from 'express';
-import { factory, formatFilename } from '../../shared/logging';
+import { factory, formatFilename } from 'shared/logging';
+import moment from 'moment';
+import { LOGIN_RATE_LIMIT_MINS, SERVER_URL, SENDGRID_API_KEY } from 'server/config';
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(SENDGRID_API_KEY);
 
 const log = factory.getLogger(formatFilename(__filename));
 
@@ -27,13 +31,43 @@ const updateEmail = async (models, req: Request, res: Response, next: NextFuncti
   });
   if (!user) return next(new Error(Errors.NoUser));
 
-  user.email = email;
-  user.emailVerified = null;
-  await user.save();
+  // ensure no more than 3 tokens have been created in the last 5 minutes
+  const recentTokens = await models.LoginToken.findAndCountAll({
+    where: {
+      email,
+      created_at: {
+        $gte: moment().subtract(LOGIN_RATE_LIMIT_MINS, 'minutes').toDate()
+      }
+    }
+  });
+  if (recentTokens.count >= LOGIN_RATE_LIMIT_MINS) {
+    return res.json({
+      status: 'Error',
+      message: `You've tried to update your email several times! Try again in ${LOGIN_RATE_LIMIT_MINS} minutes`,
+    });
+  }
 
-  // TODO: Send new verification email
-
-  return res.json({ status: 'Success', result: user.toJSON() });
+  // create and email the token
+  const path = '/';
+  const tokenObj = await models.LoginToken.createForEmail(email, path);
+  const loginLink = `${SERVER_URL}/api/finishLogin?token=${tokenObj.token}&email=${encodeURIComponent(email)}`;
+  const msg = {
+    to: email,
+    from: 'Commonwealth <no-reply@commonwealth.im>',
+    templateId: 'd-2b00abbf123e4b5981784d17151e86be', // TODO: Set this to Verification Template, not Sign In Template
+    dynamic_template_data: {
+      loginLink,
+    },
+  };
+  sgMail.send(msg).then(async (result) => {
+    user.email = email;
+    user.emailVerified = null;
+    await user.save();
+    res.json({ status: 'Success', result: user.toJSON() });
+  }).catch((e) => {
+    log.error(`Could not send authentication email: ${loginLink}`);
+    res.status(500).json({ error: 'Could not send login email', message: e.message });
+  });
 };
 
 export default updateEmail;
