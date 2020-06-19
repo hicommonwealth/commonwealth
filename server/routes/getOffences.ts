08ae57964c07a4a6706828ba1d3dd1b5b7c822be
Mir Nawaz <inbox.mirnawaz@gmail.com>
2020-06-20 00:12:08 +0500

    Calculate total # of offences across all validators over a specific interval of time.

--- server/routes/getOffences.ts
+++ server/routes/getOffences.ts
@@ -4,14 +4,21 @@ import { factory, formatFilename } from '../../shared/logging';
 const log = factory.getLogger(formatFilename(__filename));
 
 const getOffences = async (models, req: Request, res: Response, next: NextFunction) => {
-  const { chain } = req.query;
+  const { chain, startDate, endDate } = req.query;
+
   if (!chain) return next(new Error('Cannot find chain id'));
 
+  const where: any = {
+    '$ChainEventType.chain$': chain,
+    '$ChainEventType.event_name$': 'offence'
+  };
+  if (startDate && endDate) {
+    where.created_at = {
+      $between: [startDate, endDate]
+    };
+  }
   const invites = await models.ChainEvent.findAll({
-    where: {
-      '$ChainEventType.chain$': chain,
-      '$ChainEventType.event_name$': 'offence',
-    },
+    where,
     include: [
       { model: models.ChainEventType }
     ]
