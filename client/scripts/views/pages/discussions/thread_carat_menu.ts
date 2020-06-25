b32d103bf9457bb2c4836a8437a1a90741a31adf
zakhap <zhs2104@columbia.edu>
2020-06-25 16:17:29 -0400

    Auto-Subscribe user to New Threads when joining community (#433)
    * modify carat menu to never delete subscriptions, just enable/disable after creation
    
    * Create NewThread subscription on /createRole (for joining public community) and /acceptInvite (for joingin private community)
    
    * Add expect subscription checks to role generating tests that also return subscriptions.
    
    * Add role check to /accepTinvite route test to make it more comprehensive.

--- client/scripts/views/pages/discussions/thread_carat_menu.ts
+++ client/scripts/views/pages/discussions/thread_carat_menu.ts
@@ -18,8 +18,12 @@ export const ThreadSubscriptionButton: m.Component<{ proposal: OffchainThread }>
     return m(MenuItem, {
       onclick: (e) => {
         e.preventDefault();
-        if (notificationSubscription) {
-          app.user.notifications.deleteSubscription(notificationSubscription).then(() => {
+        if (notificationSubscription && notificationSubscription.isActive) {
+          app.user.notifications.disableSubscriptions([notificationSubscription]).then(() => {
+            m.redraw();
+          });
+        } else if (notificationSubscription) { // subscription, but not active
+          app.user.notifications.enableSubscriptions([notificationSubscription]).then(() => {
             m.redraw();
           });
         } else {
@@ -28,8 +32,8 @@ export const ThreadSubscriptionButton: m.Component<{ proposal: OffchainThread }>
           });
         }
       },
-      label: notificationSubscription ? 'Turn off notifications' : 'Turn on notifications',
-      iconLeft: notificationSubscription ? Icons.VOLUME_X : Icons.VOLUME_2,
+      label: notificationSubscription?.isActive ? 'Turn off notifications' : 'Turn on notifications',
+      iconLeft: notificationSubscription?.isActive ? Icons.VOLUME_X : Icons.VOLUME_2,
     });
   },
 };
