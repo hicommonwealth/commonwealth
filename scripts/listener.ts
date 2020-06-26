5e821ce2f4b0e81919c1070c773a215e5a08c6fc
Jake Naviasky <jake@commonwealth.im>
2020-06-26 16:15:52 -0400

    Setup build.

--- scripts/listener.ts
+++ scripts/listener.ts
@@ -1,12 +1,13 @@
 import subscribeMolochEvents, { createMolochApi } from '../src/moloch/index';
+import subscribeSubstrateEvents, { createSubstrateApi, createSubstrateProvider } from '../src/substrate/index';
 import { IEventHandler, CWEvent } from '../src/interfaces';
 
-const chain = process.env.NODE_CHAIN || 'moloch-local';
-const network = process.env.ETH_NETWORK || 'ws://localhost:9545';
+const chain = process.env.NODE_CHAIN || 'edgeware';
+const network = process.env.ETH_NETWORK || 'ws://mainnet1.edgewa.re:9944';
 const contractAddress = '0x9561C133DD8580860B6b7E504bC5Aa500f0f06a7';
 
 const DEV = process.env.NODE_ENV !== 'production';
-class StandaloneMolochEventHandler extends IEventHandler {
+class StandaloneEventHandler extends IEventHandler {
   public async handle(event: CWEvent): Promise<any> {
     // just prints the event
     if (DEV) console.log(`Received event: ${JSON.stringify(event, null, 2)}`);
@@ -15,12 +16,13 @@ class StandaloneMolochEventHandler extends IEventHandler {
 
 const skipCatchup = false;
 
-createMolochApi(network, 1, contractAddress).then((api) => {
-  subscribeMolochEvents({
+createSubstrateProvider(network).then((provider) => {
+  return createSubstrateApi(provider, true).isReady;
+}).then((api) => {
+  subscribeSubstrateEvents({
     chain,
     api,
-    contractVersion: 1,
-    handlers: [ new StandaloneMolochEventHandler() ],
+    handlers: [ new StandaloneEventHandler() ],
     skipCatchup,
   });
-});
+})
--- src/logging.ts
+++ src/logging.ts
@@ -18,7 +18,7 @@ export const formatFilename = (name) => {
   return t[t.length - 1];
 };
 
-export const factory = LFService.createNamedLoggerFactory('LoggerFactory', options);
+export const factory = LFService.createNamedLoggerFactory('ChainEvents', options);
 
 const control = getLogControl();
 
