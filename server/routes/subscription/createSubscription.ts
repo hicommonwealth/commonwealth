126925a8eb502070531678ebd3a29e1471bd4926
zakhap <zhs2104@columbia.edu>
2020-07-09 14:55:37 -0400

    Fix most of the tests

--- server/routes/subscription/createSubscription.ts
+++ server/routes/subscription/createSubscription.ts
@@ -19,18 +19,19 @@ export default async (models, req: Request, res: Response, next: NextFunction) =
     return next(new Error(Errors.InvalidNotificationCategory));
   }
 
-  let args;
+
+  let obj;
   const parsed_object_id = req.body.object_id.split(/-|_/);
   const p_id = parsed_object_id[1];
   const p_entity = parsed_object_id[0];
-  switch (category) {
+  switch (category.name) {
     case 'new-thread-creation': {
       const chains = await models.Chain.findAll();
       const chainsIds = chains.map((c) => c.id);
-      if (chainsIds.includes(req.body.object_id)) {
-        args = { chain_id: req.body.object_id };
+      if (chainsIds.includes(p_entity)) {
+        obj = { chain_id: p_entity };
       } else {
-        args = { community_id: req.body.object_id };
+        obj = { community_id: p_entity };
       }
       break;
     }
@@ -39,17 +40,17 @@ export default async (models, req: Request, res: Response, next: NextFunction) =
         const thread = await models.OffchainThread.findOne({ where: { id: Number(p_id), } });
         if (!thread) return next(new Error(Errors.NoThread));
         if (thread.chain) {
-          args = { offchain_thread_id: Number(p_id), chain_id: thread.chain, };
+          obj = { offchain_thread_id: Number(p_id), chain_id: thread.chain, };
         } else if (thread.community) {
-          args = { offchain_thread_id: Number(p_id), community_id: thread.community, };
+          obj = { offchain_thread_id: Number(p_id), community_id: thread.community, };
         }
       } else if (p_entity === 'comment') {
         const comment = await models.OffchainComment.findOne({ where: { id: Number(p_id), } });
         if (!comment) return next(new Error(Errors.NoComment));
         if (comment.chain) {
-          args = { offchain_comment_id: Number(p_id), chain_id: comment.chain, };
+          obj = { offchain_comment_id: Number(p_id), chain_id: comment.chain, };
         } else if (comment.community) {
-          args = { offchain_comment_id: Number(p_id), community_id: comment.community, };
+          obj = { offchain_comment_id: Number(p_id), community_id: comment.community, };
         }
       } else {
         return next(new Error(Errors.NoCommentOrReactionEntity));
@@ -58,18 +59,19 @@ export default async (models, req: Request, res: Response, next: NextFunction) =
     case 'new-mention':
       return next(new Error(Errors.NoMentions));
     case 'chain-event':
-      args = { chain_id: p_entity, chain_event_type_id: req.body.object_id };
+      obj = { chain_id: p_entity, chain_event_type_id: req.body.object_id };
       break;
     default:
       return next(new Error(Errors.InvalidNotificationCategory));
   }
+  console.dir(obj);
 
   const subscription = await models.Subscription.create({
     subscriber_id: req.user.id,
     category_id: req.body.category,
     object_id: req.body.object_id,
     is_active: !!req.body.is_active,
-    ...args,
+    ...obj,
   });
 
   return res.json({ status: 'Success', result: subscription.toJSON() });
