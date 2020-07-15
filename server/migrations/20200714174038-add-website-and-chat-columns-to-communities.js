9e45d1109687828c016fe38f784bfe7d5d7d65ad
Graham Johnson <graham@commonwealth.im>
2020-07-15 11:46:05 -0700

    Add transaction to migration

--- server/migrations/20200714174038-add-website-and-chat-columns-to-communities.js
+++ server/migrations/20200714174038-add-website-and-chat-columns-to-communities.js
@@ -2,72 +2,84 @@
 
 module.exports = {
   up: async (queryInterface, DataTypes) => {
-    await queryInterface.addColumn(
-      'OffchainCommunities',
-      'website',
-      {
-        type: DataTypes.STRING,
-        allowNull: true,
-      }
-    );
-    await queryInterface.addColumn(
-      'OffchainCommunities',
-      'chat',
-      {
-        type: DataTypes.STRING,
-        allowNull: true,
-      }
-    );
-    await queryInterface.addColumn(
-      'Chains',
-      'website',
-      {
-        type: DataTypes.STRING,
-        allowNull: true,
-      }
-    );
-    await queryInterface.addColumn(
-      'Chains',
-      'chat',
-      {
-        type: DataTypes.STRING,
-        allowNull: true,
-      }
-    );
+    return queryInterface.sequelize.transaction(async (t) => {
+      await queryInterface.addColumn(
+        'OffchainCommunities',
+        'website',
+        {
+          type: DataTypes.STRING,
+          allowNull: true,
+        },
+        { transaction: t }
+      );
+      await queryInterface.addColumn(
+        'OffchainCommunities',
+        'chat',
+        {
+          type: DataTypes.STRING,
+          allowNull: true,
+        },
+        { transaction: t }
+      );
+      await queryInterface.addColumn(
+        'Chains',
+        'website',
+        {
+          type: DataTypes.STRING,
+          allowNull: true,
+        },
+        { transaction: t }
+      );
+      await queryInterface.addColumn(
+        'Chains',
+        'chat',
+        {
+          type: DataTypes.STRING,
+          allowNull: true,
+        },
+        { transaction: t }
+      );
+    });
   },
 
   down: async (queryInterface, DataTypes) => {
-    await queryInterface.removeColumn(
-      'OffchainCommunities',
-      'website',
-      {
-        type: DataTypes.STRING,
-        allowNull: true,
-      }
-    );
-    await queryInterface.removeColumn(
-      'OffchainCommunities',
-      'chat',
-      {
-        type: DataTypes.STRING,
-        allowNull: true,
-      }
-    );
-    await queryInterface.addColumn(
-      'Chains',
-      'website',
-      {
-        type: DataTypes.STRING,
-        allowNull: true,
-      }
-    );
-    await queryInterface.addColumn(
-      'Chains',
-      'chat',
-      {
-        type: DataTypes.STRING,
-        allowNull: true,
-      }
-    );
+    return queryInterface.sequelize.transaction(async (t) => {
+      await queryInterface.removeColumn(
+        'OffchainCommunities',
+        'website',
+        {
+          type: DataTypes.STRING,
+          allowNull: true,
+        },
+        { transaction: t }
+      );
+      await queryInterface.removeColumn(
+        'OffchainCommunities',
+        'chat',
+        {
+          type: DataTypes.STRING,
+          allowNull: true,
+        },
+        { transaction: t }
+      );
+      await queryInterface.addColumn(
+        'Chains',
+        'website',
+        {
+          type: DataTypes.STRING,
+          allowNull: true,
+        },
+        { transaction: t }
+      );
+      await queryInterface.addColumn(
+        'Chains',
+        'chat',
+        {
+          type: DataTypes.STRING,
+          allowNull: true,
+        },
+        { transaction: t }
+      );
+    });
   }
 };
