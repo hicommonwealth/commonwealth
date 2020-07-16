88acee7d637cc59f28e929eb8d6026d7c0421c26
zakhap <zhs2104@columbia.edu>
2020-07-16 11:46:17 -0400

    Modify viewSubscriptions route with new association parameters.

--- server/routes/viewSubscriptions.ts
+++ server/routes/viewSubscriptions.ts
@@ -12,8 +12,35 @@ export default async (models, req: Request, res: Response, next: NextFunction) =
     return next(new Error(Errors.NotLoggedIn));
   }
 
+  const associationParams: any = [{
+    model: models.Chain,
+    required: false,
+    as: 'Chain',
+  }, {
+    model: models.OffchainCommunity,
+    required: false,
+    as: 'OffchainCommunity',
+  }, {
+    model: models.OffchainThread,
+    required: false,
+    as: 'OffchainThread',
+  }, {
+    model: models.OffchainComment,
+    required: false,
+    as: 'OffchainComment',
+  }, {
+    model: models.ChainEventType,
+    required: false,
+    as: 'ChainEventType',
+  }, {
+    model: models.ChainEntity,
+    required: false,
+    as: 'ChainEntity',
+  }];
+
   const subscriptions = await models.Subscription.findAll({
     where: { subscriber_id: req.user.id },
+    include: [ ...associationParams ],
   });
   return res.json({ status: 'Success', result: subscriptions.map((s) => s.toJSON()) });
 };
