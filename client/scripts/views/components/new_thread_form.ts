03616864fad35807d07772b13026616262592618
Raymond Zhong <raykyri@gmail.com>
2020-07-17 15:54:48 -0700

    fix has-drafts css class

--- client/scripts/views/components/new_thread_form.ts
+++ client/scripts/views/components/new_thread_form.ts
@@ -233,7 +233,8 @@ export const NewThreadForm: m.Component<{
     const { newType, saving } = vnode.state;
 
     return m('.NewThreadForm', {
-      class: `${newType === PostType.Link ? 'link-post' : ''} ${discussionDrafts.length > 0 ? 'has-drafts' : ''} `
+      class: `${newType === PostType.Link ? 'link-post' : ''} `
+        + `${newType !== PostType.Link && discussionDrafts.length > 0 ? 'has-drafts' : ''} `
         + `${isModal ? 'is-modal' : ''}`,
       oncreate: (vvnode) => {
         $(vvnode.dom).find('.cui-input input').prop('autocomplete', 'off').focus();
