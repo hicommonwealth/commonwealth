5448d47e14b9b54710a5f784865b85f416f61767
Mir Nawaz <inbox.mirnawaz@gmail.com>
2020-07-18 20:06:53 +0500

    manage staking WIP

--- client/scripts/views/pages/manage_staking/substrate/new_nominator.ts
+++ client/scripts/views/pages/manage_staking/substrate/new_nominator.ts
@@ -1,53 +1,77 @@
 import m from 'mithril';
 import app from 'state';
-import { formatAddressShort } from 'helpers';
-import Substrate from 'controllers/chain/substrate/main';
 import { makeDynamicComponent } from 'models/mithril';
-import User from 'views/components/widgets/user';
-import { IValidators, SubstrateAccount } from 'controllers/chain/substrate/account';
-import { ICosmosValidator } from 'controllers/chain/cosmos/account';
-import { StakingLedger } from '@polkadot/types/interfaces';
-import StashAccountForm from 'views/pages/validators/substrate/stash_form';
-import ControllerAccountForm from 'views/pages/validators/substrate/controller_form';
 import Bond from 'views/pages/manage_staking/substrate/bond';
 
-interface NewNominatorState {
-  dynamic: {
-  }
+const MAX_STEP = 2;
+const MIN_STEP = 1;
+
+interface NewNominatorState { dynamic: {} }
+
+interface NewNominatorAttrs {}
+
+interface IModel {
+  error: boolean,
+  payload: object,
+  step: number,
+  onChange(payload: any, noError: boolean): void,
+  next(): void,
+  bond(): void,
 }
 
-interface NewNominatorAttrs {
-}
-
-const model = {
+const model: IModel = {
   error: true,
   payload: {},
-  onChange: (payload, noError: boolean) => {
-    console.log(noError);
+  step: MIN_STEP,
+  onChange: (payload: object, noError: boolean) => {
     model.payload = payload;
     model.error = !noError;
+  },
+  next: () => {
+    if (model.step < MAX_STEP)
+      model.step = ++model.step;
+  },
+  bond: () => {
+
   }
 };
 
 const NewNominator = makeDynamicComponent<NewNominatorAttrs, NewNominatorState>({
+  oncreate: () => {
+    model.step = MIN_STEP;
+  },
   getObservables: () => ({
     groupKey: app.chain.class.toString()
   }),
   view: (vnode) => {
     return m('.NewNominator', [
       m('.compact-modal-title.center-lg', [
-        m('h5', [ 'Step 1 of 2' ]),
+        m('h5', [ `Step ${model.step} of ${MAX_STEP}` ]),
         m('h3', [ 'Setup Nominator' ]),
       ]),
       m('.compact-modal-body',
-        m(Bond, {
-          onChange: model.onChange
-        }),
-        m('div.center-lg.padding-t-10',
-          m('button.cui-button.cui-align-center.cui-primary', {
-            disabled: model.error,
-            onclick: () => {},
-          }, 'Next')))
+        model.step === MIN_STEP
+        && m('span.first-step', [
+          m(Bond, {
+            onChange: model.onChange
+          }),
+          m('div.center-lg.padding-t-10',
+            m('button.cui-button.cui-align-center.cui-primary', {
+            // disabled: model.error,
+              onclick: model.next,
+            }, 'Next'))
+        ]),
+        model.step === MAX_STEP
+        && m('span.second-step', [
+          // m(Bond, {
+          //   onChange: model.onChange
+          // }),
+          m('div.center-lg.padding-t-10',
+            m('button.cui-button.cui-align-center.cui-primary', {
+            // disabled: model.error,
+              onclick: model.bond,
+            }, 'Bond & Nominate'))
+        ]))
     ]);
   },
 });
