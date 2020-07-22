b48aefadf7203b054c5db48d6cee34d4a2e09436
Raymond Zhong <raykyri@gmail.com>
2020-07-21 20:41:11 -0700

    select address modal

--- client/scripts/views/modals/select_address_modal.ts
+++ client/scripts/views/modals/select_address_modal.ts
@@ -76,7 +76,7 @@ const SelectAddressModal: m.Component<{}, { selectedIndex: number, loading: bool
 
     return m('.SelectAddressModal', [
       m('.compact-modal-title', [
-        m('h3', 'Addresses'),
+        m('h3', 'Manage linked addresses'),
       ]),
       m('.compact-modal-body', [
         m('.select-address-options', [
