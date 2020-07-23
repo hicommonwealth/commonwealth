e0ea2c2f4d84b2fc923f7dbf88908e91ff48726e
Jake Naviasky <jake@commonwealth.im>
2020-07-23 16:49:50 -0400

    Export types at top level.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 export * from './interfaces';
 export * as MolochEvents from './moloch/index';
+export * as MolochTypes from './moloch/types';
 export * as SubstrateEvents from './substrate/index';
+export * as SubstrateTypes from './substrate/types';
