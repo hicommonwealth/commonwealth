57901b99c9526fa05719c2ea5e048f01bb0e97e6
Raymond Zhong <raykyri@gmail.com>
2020-07-23 16:38:09 -0700

    prevent scroll position from being lost for back button

--- client/scripts/views/pages/loading.ts
+++ client/scripts/views/pages/loading.ts
@@ -1,10 +1,19 @@
 import 'pages/loading.scss';
 
+import $ from 'jquery';
 import m from 'mithril';
 import { Spinner } from 'construct-ui';
 import Sublayout from 'views/sublayout';
 
 const LoadingPage: m.Component<{ message?: string }> = {
+  // hack: prevent a smaller scrollHeight from resetting the previous page's scroll position
+  oninit: (vnode) => {
+    const h1 = $('html')[0].scrollHeight;
+    $('html').css('min-height', h1);
+  },
+  onremove: (vnode) => {
+    $('html').css('min-height', 'initial');
+  },
   view: (vnode) => {
     return m(Sublayout, {
       class: 'LoadingPage',
