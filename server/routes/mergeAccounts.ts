f4925172e52d40aea59e85ef7650a277f2f06549
zakhap <zhs2104@columbia.edu>
2020-07-24 14:36:23 -0400

    Handle merging of comments, threads, and roles. Not handling Offchain Profile or Abandoned Address.

--- server/routes/mergeAccounts.ts
+++ server/routes/mergeAccounts.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from 'express';
 import { Op } from 'sequelize';
 import { factory, formatFilename } from '../../shared/logging';
+import role from '../models/role';
 const log = factory.getLogger(formatFilename(__filename));
 
 export const Errors = {
@@ -70,10 +71,59 @@ const mergeAccounts = async (models, req: Request, res: Response, next: NextFunc
     });
 
     // Transfer Threads
+    await Promise.all(
+        threadsToBeMerged.map((thread) => {
+            return thread.update({
+                address_id: addressToBeOwner.id,
+            });
+        })
+    );
 
     // Transfer Comments
+    await Promise.all(
+        commentsToBeMerged.map((comment) => {
+            return comment.update({
+                address_id: addressToBeOwner.id,
+            });
+        })
+    )
 
-    // Transfer Roles (Delete role if address already has role of greater permission)
+    // Transfer Roles
+    await Promise.all(
+        rolesToBeMerged.map((role) => {
+            return role.update({
+                address_id: addressToBeOwner.id,
+            });
+        })
+    );
+    // Prune Roles
+    const allRoles = await models.Role.findAll({
+        where: {
+            address_id: addressToBeOwner.id,
+        },
+    });
+    for (let i=0; i<allRoles.length-1; i++) {
+        const role1 = allRoles[i];
+        for (let j=i+1; j<allRoles.length; j++) {
+            const role2 = allRoles[j];
+            // compare
+            const compare = role1.chain_id
+                ? role1.chain_id === role2.chain_id
+                : role1.community_id === role2.community_id
+            if (compare) {
+                    // destroy the model with the lowest permission
+                    if (role1.permission === 'admin'
+                        || (role1.permission === 'moderator' && role2.permission !== 'admin')
+                    ) {
+                        await role2.destroy();
+                    } else if (role2.permission === 'admin'
+                        || (role2.permission === 'moderator' && role1.permission !== 'admin')
+                    ) {
+                        await role1.destroy();
+                    }
+                }
+        }
+    }
 
     // TODO: What to do with the old Offchain Profile?
     // Keep Address and Offchain Profile in DB, but unassociate with User?
