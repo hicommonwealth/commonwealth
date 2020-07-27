c3d14d466e73064bc224821c6ef08c64230ddae1
Jake Naviasky <jake@commonwealth.im>
2020-07-27 16:27:31 -0400

    Write migrate identities script.

--- server/scripts/migrateIdentities.ts
+++ server/scripts/migrateIdentities.ts
@@ -4,14 +4,78 @@
  * from the chain, writing the results back into the database.
  */
 
-import { SubstrateTypes } from '@commonwealth/chain-events';
+import { SubstrateTypes, SubstrateEvents } from '@commonwealth/chain-events';
+import { OffchainProfileAttributes, OffchainProfileInstance } from '../models/offchain_profile';
 
-export async function migrateIdentities(models, chain?: string) {
-  // TODO: query all valid chains for identity migrations, or query nodes for provided
+import { factory, formatFilename } from '../../shared/logging';
+const log = factory.getLogger(formatFilename(__filename));
 
-  // TODO: query all addresses
+export async function migrateIdentities(models, chain?: string): Promise<OffchainProfileAttributes[]> {
+  // 1. fetch the node and url of supported/selected chains
+  log.info('Fetching node info for identity migrations...');
+  const nodes = [];
+  if (!chain) {
+    // query one node for each supported chain
+    for (const supportedChain of SubstrateTypes.EventChains) {
+      // eslint-disable-next-line no-await-in-loop
+      nodes.push(await models.ChainNode.findOne({
+        where: { chain: supportedChain }
+      }));
+    }
+  } else {
+    // query one node for provided chain
+    nodes.push(await models.ChainNode.findOne({
+      where: { chain }
+    }));
+  }
+  if (!nodes) {
+    throw new Error('no nodes found for identity migration');
+  }
 
-  // TODO: connect to chain and query all identities
+  // 2. for each node, fetch and migrate identities
+  const updatedProfiles: OffchainProfileAttributes[] = [];
+  for (const node of nodes) {
+    // 2a. query all available addresses in the db on the chain
+    log.info(`Querying all profiles on chain ${node.chain}...`);
+    const profiles: OffchainProfileInstance[] = await models.OffchainProfile.findAll({
+      include: [{
+        model: this._models.Address,
+        where: {
+          chain: node.chain,
+        },
+        required: true,
+      }]
+    });
+    const addresses = profiles.map((p) => p.Address.address);
 
-  // TODO: write identities back to db
+    // 2b. connect to chain and query all identities of found addresses
+    log.info(`Fetching identities on chain ${node.chain} at url ${node.url}...`);
+    let nodeUrl = node.url;
+    const hasProtocol = nodeUrl.indexOf('wss://') !== -1 || nodeUrl.indexOf('ws://') !== -1;
+    nodeUrl = hasProtocol ? nodeUrl.split('://')[1] : nodeUrl;
+    const isInsecureProtocol = nodeUrl.indexOf('kusama-rpc.polkadot.io') === -1
+      && nodeUrl.indexOf('rpc.polkadot.io') === -1;
+    const protocol = isInsecureProtocol ? 'ws://' : 'wss://';
+    if (nodeUrl.indexOf(':9944') !== -1) {
+      nodeUrl = isInsecureProtocol ? nodeUrl : nodeUrl.split(':9944')[0];
+    }
+    nodeUrl = protocol + nodeUrl;
+    const provider = await SubstrateEvents.createProvider(nodeUrl);
+    const api = await SubstrateEvents.createApi(provider, node.chain).isReady;
+    const fetcher = new SubstrateEvents.StorageFetcher(api);
+    const identityEvents = await fetcher.fetchIdentities(addresses);
+
+    // 2c. write the found identities back to db
+    log.info(`Writing identities for chain ${node.chain} back to db...`);
+    for (const identityEvent of identityEvents) {
+      const profile = profiles.find((p) => p.Address.address === identityEvent.data.who);
+      if (profile) {
+        profile.identity = identityEvent.data.displayName;
+        await profile.save();
+        log.debug(`Discovered name '${profile.identity}' for ${profile.Address.address}!`);
+        updatedProfiles.push(profile);
+      }
+    }
+  }
+  return updatedProfiles;
 }
