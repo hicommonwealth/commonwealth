a5a82cc8dfbbd947688e9bd9f658df22123e1b4e
zakhap <zhs2104@columbia.edu>
2020-07-29 19:10:33 -0400

    Migration identifying all orphaned subscriptions, throwing foreign key constrain on delete.

--- server/migrations/20200729212319-prune-orphaned-subscriptions.js
+++ server/migrations/20200729212319-prune-orphaned-subscriptions.js
@@ -1,23 +1,41 @@
+
 'use strict';
 
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
-    */
+  up: async (queryInterface, Sequelize) => {
+    const subscriptions = await queryInterface.sequelize.query(`SELECT * FROM "Subscriptions";`);
+    console.dir(subscriptions[0].length);
+    const threads = await queryInterface.sequelize.query(`SELECT * FROM "OffchainThreads" WHERE deleted_at IS NULL;`);
+    const threadIds = threads[0].map((t) => t.id);
+    console.dir(threads[0].length);
+    console.dir(threadIds.length);
+    const comments = await queryInterface.sequelize.query(`SELECT * FROM "OffchainComments" WHERE deleted_at IS NULL;`);
+    const commentIds = comments[0].map((c) => c.id);
+    console.dir(comments[0].length);
+    await Promise.all(subscriptions[0].map((s) => {
+      if (s.category_id === 'new-comment-creation' || s.category_id === 'new-reaction') {
+        if (s.offchain_thread_id) {
+          // console.dir(threadIds.includes(s.offchain_thread_id));
+          if (threadIds.includes(s.offchain_thread_id)) return;
+          else {
+            console.dir(`DELETING SUBSCRIPTION #${s.id}`);
+            queryInterface.sequelize.query(`DELETE FROM "Subscriptions" WHERE id=${s.id};`);
+          }
+        } else if (s.offchain_comment_id) {
+          console.dir(commentIds.includes(s.offchain_comment_id));
+          if (commentIds.includes(s.offchain_comment_id)) return;
+          else {
+            console.dir(`DELETING SUBSCRIPTION #${s.id}`);
+            queryInterface.sequelize.query(`DELETE FROM "Subscriptions" WHERE id=${s.id};`);
+          }
+        }
+      } else {
+        return;
+      }
+    }));
   },
 
   down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
 
-      Example:
-      return queryInterface.dropTable('users');
-    */
   }
 };
