18551244b8444b9b95a3935e56be17170d783e4d
zakhap <zhs2104@columbia.edu>
2020-07-30 21:22:11 -0400

    Get Kusama + Polkadot + Edgeware chain notification settings all working :muscle:

--- client/scripts/helpers/chain_notification_types.ts
+++ client/scripts/helpers/chain_notification_types.ts
@@ -51,3 +51,111 @@ export const EdgewareChainNotificationTypes = {
         'edgeware-vote-delegated',
     ],
 };
+
+export const PolkdotChainNotificationTypes = {
+    Council: [
+        'polkadot-collective-approved',
+        'polkadot-collective-disapproved',
+        'polkadot-collective-executed',
+        'polkadot-collective-member-executed',
+        'polkadot-collective-proposed',
+        'polkadot-collective-voted',
+        'polkadot-election-candidacy-submitted',
+        'polkadot-election-empty-term',
+        'polkadot-election-member-kicked',
+        'polkadot-election-member-renounced',
+        'polkadot-election-new-term',
+    ],
+    Democracy: [
+        'polkadot-democracy-cancelled',
+        'polkadot-democracy-executed',
+        'polkadot-democracy-not-passed',
+        'polkadot-democracy-passed',
+        'polkadot-democracy-proposed',
+        'polkadot-democracy-started',
+        'polkadot-democracy-tabled',
+    ],
+    Preimage: [
+        'polkadot-preimage-invalid',
+        'polkadot-preimage-missing',
+        'polkadot-preimage-noted',
+        'polkadot-preimage-reaped',
+        'polkadot-preimage-used',
+    ],
+    // Signaling: [
+    //     'polkadot-signaling-commit-started',
+    //     'polkadot-signaling-new-proposal',
+    //     'polkadot-signaling-voting-completed',
+    //     'polkadot-signaling-voting-started',
+    // ],
+    // Treasury: [
+    //     'polkadot-treasury-awarded',
+    //     'polkadot-treasury-proposed',
+    //     'polkadot-treasury-rejected',
+    //     'polkadot-treasury-reward-minting',
+    //     'polkadot-treasury-reward-minting-v2'
+    // ],
+    Validator: [
+        'polkadot-bonded',
+        'polkadot-reward',
+        'polkadot-slash',
+        'polkadot-unbonded',
+    ],
+    Vote: [
+        'polkadot-vote-delegated',
+    ],
+};
+
+export const KusamaChainNotificationTypes = {
+    Council: [
+        'kusama-collective-approved',
+        'kusama-collective-disapproved',
+        'kusama-collective-executed',
+        'kusama-collective-member-executed',
+        'kusama-collective-proposed',
+        'kusama-collective-voted',
+        'kusama-election-candidacy-submitted',
+        'kusama-election-empty-term',
+        'kusama-election-member-kicked',
+        'kusama-election-member-renounced',
+        'kusama-election-new-term',
+    ],
+    Democracy: [
+        'kusama-democracy-cancelled',
+        'kusama-democracy-executed',
+        'kusama-democracy-not-passed',
+        'kusama-democracy-passed',
+        'kusama-democracy-proposed',
+        'kusama-democracy-started',
+        'kusama-democracy-tabled',
+    ],
+    Preimage: [
+        'kusama-preimage-invalid',
+        'kusama-preimage-missing',
+        'kusama-preimage-noted',
+        'kusama-preimage-reaped',
+        'kusama-preimage-used',
+    ],
+    // Signaling: [
+    //     'kusama-signaling-commit-started',
+    //     'kusama-signaling-new-proposal',
+    //     'kusama-signaling-voting-completed',
+    //     'kusama-signaling-voting-started',
+    // ],
+    // Treasury: [
+    //     'kusama-treasury-awarded',
+    //     'kusama-treasury-proposed',
+    //     'kusama-treasury-rejected',
+    //     'kusama-treasury-reward-minting',
+    //     'kusama-treasury-reward-minting-v2'
+    // ],
+    Validator: [
+        'kusama-bonded',
+        'kusama-reward',
+        'kusama-slash',
+        'kusama-unbonded',
+    ],
+    Vote: [
+        'kusama-vote-delegated',
+    ],
+};
--- client/scripts/views/pages/notification-settings/chain-event-settings.ts
+++ client/scripts/views/pages/notification-settings/chain-event-settings.ts
@@ -12,184 +12,225 @@ import {
 } from '@commonwealth/chain-events';
 
 import Sublayout from 'views/sublayout';
-import { EdgewareChainNotificationTypes } from 'helpers/chain_notification_types';
+import { EdgewareChainNotificationTypes, KusamaChainNotificationTypes, PolkdotChainNotificationTypes } from 'helpers/chain_notification_types';
+
+
+interface IIndividualEventSubscriptionsState {
+  chain: string;
+  eventKinds: IChainEventKind[];
+  allSupportedChains: string[];
+  isSubscribedAll: boolean;
+  isEmailAll: boolean;
+}
+
+const IndividualEventSubscriptions: m.Component<{}, IIndividualEventSubscriptionsState> = {
+  view: (vnode) => {
+    let titler;
+    if (vnode.state.chain === 'edgeware' || vnode.state.chain === 'edgeware-local') {
+      titler = SubstrateEvents.Title;
+      vnode.state.eventKinds = SubstrateTypes.EventKinds;
+    } else {
+      titler = null;
+      vnode.state.eventKinds = [];
+    }   
+
+    const supportedChains = app.loginStatusLoaded
+    ? app.config.chains.getAll()
+      .filter((c) => vnode.state.allSupportedChains.includes(c.id))
+      .sort((a, b) => a.id.localeCompare(b.id))
+    : [];
+
+    return [
+      supportedChains.length > 0 && vnode.state.eventKinds.length > 0 && titler
+      ? vnode.state.eventKinds.map((kind) => m(
+        EventSubscriptionRow,
+        { chain: vnode.state.chain, kind, titler, key: kind },
+      ))
+      : m('No events available on this chain.'),
+    ]
+  },
+};
 
 interface IEventSubscriptionRowAttrs {
     chain: string;
     kind: IChainEventKind;
     titler: TitlerFilter;
-  }
+}
   
-  const EventSubscriptionRow: m.Component<IEventSubscriptionRowAttrs, {}> = {
-    view: (vnode) => {
-      const { chain, kind } = vnode.attrs;
-      const { title, description } = vnode.attrs.titler(kind);
-      const objectId = `${chain}-${kind}`;
-      const subscription = app.loginStatusLoaded && app.user.notifications.subscriptions
-        .find((sub) => sub.category === NotificationCategories.ChainEvent
-          && sub.objectId === objectId);
-      return m('tr.EventSubscriptionRow', [
-        m('td', `${title}`),
-        app.loginStatusLoaded && m('td', [
-          m(Checkbox, {
-            checked: subscription && subscription.isActive,
-            size: 'lg',
-            onchange: async (e) => {
-              e.preventDefault();
-              if (subscription && subscription.isActive) {
-                await app.user.notifications.disableSubscriptions([ subscription ]);
-              } else if (subscription && !subscription.isActive) {
-                await app.user.notifications.enableSubscriptions([ subscription ]);
-              } else {
-                await app.user.notifications.subscribe(NotificationCategories.ChainEvent, objectId);
-              }
-              m.redraw();
+const EventSubscriptionRow: m.Component<IEventSubscriptionRowAttrs, {}> = {
+  view: (vnode) => {
+    const { chain, kind } = vnode.attrs;
+    const { title, description } = vnode.attrs.titler(kind);
+    const objectId = `${chain}-${kind}`;
+    const subscription = app.loginStatusLoaded && app.user.notifications.subscriptions
+      .find((sub) => sub.category === NotificationCategories.ChainEvent
+        && sub.objectId === objectId);
+    return m('tr.EventSubscriptionRow', [
+      m('td', `${title}`),
+      app.loginStatusLoaded && m('td', [
+        m(Checkbox, {
+          checked: subscription && subscription.isActive,
+          size: 'lg',
+          onchange: async (e) => {
+            e.preventDefault();
+            if (subscription && subscription.isActive) {
+              await app.user.notifications.disableSubscriptions([ subscription ]);
+            } else if (subscription && !subscription.isActive) {
+              await app.user.notifications.enableSubscriptions([ subscription ]);
+            } else {
+              await app.user.notifications.subscribe(NotificationCategories.ChainEvent, objectId);
             }
-          }),
-        ]),
-        m('td', [
-          m(Checkbox, {
-            disabled: !subscription?.isActive,
-            checked: subscription?.isActive && subscription?.immediateEmail,
-            size: 'lg',
-            onchange: async (e) => {
-              e.preventDefault();
-              if (subscription && subscription.immediateEmail) {
-                await app.user.notifications.disableImmediateEmails([ subscription ]);
-              } else {
-                await app.user.notifications.enableImmediateEmails([ subscription ]);
-              }
-              m.redraw();
+            m.redraw();
+          }
+        }),
+      ]),
+      m('td', [
+        m(Checkbox, {
+          disabled: !subscription?.isActive,
+          checked: subscription?.isActive && subscription?.immediateEmail,
+          size: 'lg',
+          onchange: async (e) => {
+            e.preventDefault();
+            if (subscription && subscription.immediateEmail) {
+              await app.user.notifications.disableImmediateEmails([ subscription ]);
+            } else {
+              await app.user.notifications.enableImmediateEmails([ subscription ]);
             }
-          }),
-        ]),
-      ]);
-    }
-  };
+            m.redraw();
+          }
+        }),
+      ]),
+    ]);
+  }
+};
 
 interface IEventSubscriptionTypeRowAttrs {
     title: string;
     notificationTypeArray: string[];
   }
   
-  const EventSubscriptionTypeRow: m.Component<IEventSubscriptionTypeRowAttrs> = {
-    view: (vnode) => {
-      const { title, notificationTypeArray, } = vnode.attrs;
-      const subscriptions = app.loginStatusLoaded && app.user.notifications.subscriptions.filter((s) => {
-        return (
-          s.category === NotificationCategories.ChainEvent
-          && notificationTypeArray.includes(s.objectId)
-        );
-      });
-      const everySubscriptionActive = subscriptions.every((s) => s.isActive);
-      const someSubscriptionsActive = subscriptions.some((s) => s.isActive);
-      const everySubscriptionEmail = subscriptions.every((s) => s.immediateEmail)
-      const someSubscriptionsEmail = subscriptions.some((s) => s.immediateEmail);
-      const allSubscriptionsCreated = subscriptions.length === notificationTypeArray.length;
-  
-      return m('tr.EventSubscriptionTypeRow', [
-        m('td', { class: 'bold' }, title),
-        m('td', [
-          m(Checkbox, {
-            checked: allSubscriptionsCreated && everySubscriptionActive,
-            indeterminate: !everySubscriptionActive && someSubscriptionsActive,
-            size: 'lg',
-            onchange: async (e) => {
-              e.preventDefault();
-              if (allSubscriptionsCreated && everySubscriptionActive) {
-                await app.user.notifications.disableSubscriptions(subscriptions);
-              } else if (allSubscriptionsCreated && !someSubscriptionsActive) {
-                await app.user.notifications.enableSubscriptions(subscriptions);
-              } else {
-                await Promise.all(
-                  notificationTypeArray.map((obj) => {
-                    return app.user.notifications.subscribe(NotificationCategories.ChainEvent, obj);
-                  })
-                )
-              }
-              m.redraw();
+const EventSubscriptionTypeRow: m.Component<IEventSubscriptionTypeRowAttrs> = {
+  view: (vnode) => {
+    const { title, notificationTypeArray, } = vnode.attrs;
+    const subscriptions = app.user.notifications.subscriptions.filter((s) => {
+      return (
+        s.category === NotificationCategories.ChainEvent
+        && notificationTypeArray.includes(s.objectId)
+      );
+    });
+    const everySubscriptionActive = subscriptions.every((s) => s.isActive);
+    const someSubscriptionsActive = subscriptions.some((s) => s.isActive);
+    const everySubscriptionEmail = subscriptions.every((s) => s.immediateEmail)
+    const someSubscriptionsEmail = subscriptions.some((s) => s.immediateEmail);
+    const allSubscriptionsCreated = subscriptions.length === notificationTypeArray.length;
+    return m('tr.EventSubscriptionTypeRow', [
+      m('td', { class: 'bold' }, title),
+      m('td', [
+        m(Checkbox, {
+          checked: allSubscriptionsCreated && everySubscriptionActive,
+          indeterminate: !everySubscriptionActive && someSubscriptionsActive,
+          size: 'lg',
+          onchange: async (e) => {
+            e.preventDefault();
+            if (allSubscriptionsCreated && everySubscriptionActive) {
+              await app.user.notifications.disableSubscriptions(subscriptions);
+            } else if (allSubscriptionsCreated && !someSubscriptionsActive) {
+              await app.user.notifications.enableSubscriptions(subscriptions);
+            } else {
+              await Promise.all(
+                notificationTypeArray.map((obj) => {
+                  return app.user.notifications.subscribe(NotificationCategories.ChainEvent, obj);
+                })
+              )
             }
-          })
-        ]),
-        m('td', [
-          m(Checkbox, {
-            disabled: !everySubscriptionActive || !allSubscriptionsCreated,
-            checked:  everySubscriptionEmail,
-            indeterminate: !everySubscriptionEmail && someSubscriptionsEmail,
-            size: 'lg',
-            onchange: async (e) => {
-              e.preventDefault();
-              if (everySubscriptionEmail) {
-                await app.user.notifications.disableImmediateEmails(subscriptions);
-              } else {
-                await app.user.notifications.enableImmediateEmails(subscriptions);
-              }
-              m.redraw();
+            m.redraw();
+          }
+        })
+      ]),
+      m('td', [
+        m(Checkbox, {
+          disabled: !everySubscriptionActive || !allSubscriptionsCreated,
+          checked:  everySubscriptionEmail && allSubscriptionsCreated,
+          indeterminate: !everySubscriptionEmail && someSubscriptionsEmail,
+          size: 'lg',
+          onchange: async (e) => {
+            e.preventDefault();
+            if (everySubscriptionEmail) {
+              await app.user.notifications.disableImmediateEmails(subscriptions);
+            } else {
+              await app.user.notifications.enableImmediateEmails(subscriptions);
             }
-          }),
-        ]),
-      ]);
-    }
-  };
-
-interface IEventSubscriptionState {
-    chain: string;
-    eventKinds: IChainEventKind[];
-    allSupportedChains: string[];
-    isSubscribedAll: boolean;
-    isEmailAll: boolean;
+            m.redraw();
+          }
+        }),
+      ]),
+    ]);
   }
+};
+
+const EdgewareChainEvents: m.Component = {
+  view: (vnode) =>{
+    return [
+      m(EventSubscriptionTypeRow, { title: 'Council events', notificationTypeArray: EdgewareChainNotificationTypes.Council, }),
+      m(EventSubscriptionTypeRow, { title: 'Democracy events', notificationTypeArray: EdgewareChainNotificationTypes.Democracy, }),
+      m(EventSubscriptionTypeRow, { title: 'Preimage events', notificationTypeArray: EdgewareChainNotificationTypes.Preimage, }),
+      m(EventSubscriptionTypeRow, { title: 'Signaling events', notificationTypeArray: EdgewareChainNotificationTypes.Signaling, }),
+      m(EventSubscriptionTypeRow, { title: 'Treasury events', notificationTypeArray: EdgewareChainNotificationTypes.Treasury, }),
+      m(EventSubscriptionTypeRow, { title: 'Validator events', notificationTypeArray: EdgewareChainNotificationTypes.Validator, }),
+      m(EventSubscriptionTypeRow, { title: 'Vote events', notificationTypeArray: EdgewareChainNotificationTypes.Vote, }),
+    ];
+  }
+};
+
+const KusamaChainEvents: m.Component = {
+  view: (vnode) =>{
+    return [
+      m(EventSubscriptionTypeRow, { title: 'Council events', notificationTypeArray: KusamaChainNotificationTypes.Council, }),
+      m(EventSubscriptionTypeRow, { title: 'Democracy events', notificationTypeArray: KusamaChainNotificationTypes.Democracy, }),
+      m(EventSubscriptionTypeRow, { title: 'Preimage events', notificationTypeArray: KusamaChainNotificationTypes.Preimage, }),
+      // m(EventSubscriptionTypeRow, { title: 'Treasury events', notificationTypeArray: KusamaChainNotificationTypes.Treasury, }),
+      // m(EventSubscriptionTypeRow, { title: 'Signaling events', notificationTypeArray: KusamaChainNotificationTypes.Signaling, }),
+      m(EventSubscriptionTypeRow, { title: 'Validator events', notificationTypeArray: KusamaChainNotificationTypes.Validator, }),
+      m(EventSubscriptionTypeRow, { title: 'Vote events', notificationTypeArray: KusamaChainNotificationTypes.Vote, }),
+    ];
+  }
+};
+
+const PolkadotChainEvents: m.Component = {
+  view: (vnode) => {
+    return [
+      m(EventSubscriptionTypeRow, { title: 'Council events', notificationTypeArray: PolkdotChainNotificationTypes.Council, }),
+      m(EventSubscriptionTypeRow, { title: 'Democracy events', notificationTypeArray: PolkdotChainNotificationTypes.Democracy, }),
+      m(EventSubscriptionTypeRow, { title: 'Preimage events', notificationTypeArray: PolkdotChainNotificationTypes.Preimage, }),
+      // m(EventSubscriptionTypeRow, { title: 'Signaling events', notificationTypeArray: PolkdotChainNotificationTypes.Signaling, }),
+      // m(EventSubscriptionTypeRow, { title: 'Treasury events', notificationTypeArray: PolkdotChainNotificationTypes.Treasury, }),
+      m(EventSubscriptionTypeRow, { title: 'Validator events', notificationTypeArray: PolkdotChainNotificationTypes.Validator, }),
+      m(EventSubscriptionTypeRow, { title: 'Vote events', notificationTypeArray: PolkdotChainNotificationTypes.Vote, }),
+    ]
+  }
+};
+
+
   
-  const EventSubscriptions: m.Component<{chain: ChainInfo}, IEventSubscriptionState> = {
-    oninit: (vnode) => {
-      vnode.state.chain = vnode.attrs.chain.id;
-      vnode.state.eventKinds = SubstrateTypes.EventKinds;
-      vnode.state.allSupportedChains = EventSupportingChains.sort();
-      vnode.state.isSubscribedAll = false;
-      vnode.state.isEmailAll = false;
-    },
-    view: (vnode) => {
-        // For debugging: individual EventSubscriptionRows
-        // let titler;
-        // if (vnode.state.chain === 'edgeware' || vnode.state.chain === 'edgeware-local') {
-        //     titler = SubstrateEvents.Title;
-        //     vnode.state.eventKinds = SubstrateTypes.EventKinds;
-        // } else {
-        //     titler = null;
-        //     vnode.state.eventKinds = [];
-        // }   
-        // const supportedChains = app.loginStatusLoaded
-        // ? app.config.chains.getAll()
-        //     .filter((c) => vnode.state.allSupportedChains.includes(c.id))
-        //     .sort((a, b) => a.id.localeCompare(b.id))
-        // : [];
-        return m('.EventSubscriptions', [
-            m('h2', vnode.attrs.chain.name),
-            m(Table, {}, [
-                m('tr', [
-                    m('th', null),
-                    m('th', 'In app'),
-                    m('th', 'By email'),
-                ]),
-                m(EventSubscriptionTypeRow, { title: 'Council events', notificationTypeArray: EdgewareChainNotificationTypes.Council, }),
-                m(EventSubscriptionTypeRow, { title: 'Democracy events', notificationTypeArray: EdgewareChainNotificationTypes.Democracy, }),
-                m(EventSubscriptionTypeRow, { title: 'Preimage events', notificationTypeArray: EdgewareChainNotificationTypes.Preimage, }),
-                m(EventSubscriptionTypeRow, { title: 'Treasury events', notificationTypeArray: EdgewareChainNotificationTypes.Treasury, }),
-                m(EventSubscriptionTypeRow, { title: 'Signaling events', notificationTypeArray: EdgewareChainNotificationTypes.Signaling, }),
-                m(EventSubscriptionTypeRow, { title: 'Validator events', notificationTypeArray: EdgewareChainNotificationTypes.Validator, }),
-                m(EventSubscriptionTypeRow, { title: 'Vote events', notificationTypeArray: EdgewareChainNotificationTypes.Vote, }),
-        
-                // List all event kinds in the UI. Good for debugging.
-                // supportedChains.length > 0 && vnode.state.eventKinds.length > 0 && titler
-                //   ? vnode.state.eventKinds.map((kind) => m(
-                //     EventSubscriptionRow,
-                //     { chain: vnode.state.chain, kind, titler, key: kind },
-                //   ))
-                //   : m('No events available on this chain.'),
-            ]),
-        ]);
-    }
-  };
+const EventSubscriptions: m.Component<{chain: ChainInfo}> = {
+  view: (vnode) => {
+    const { chain } = vnode.attrs;
+    console.dir(chain);
+    return m('.EventSubscriptions', [
+      m('h2', vnode.attrs.chain.name),
+      m(Table, {}, [
+        m('tr', [
+            m('th', null),
+            m('th', 'In app'),
+            m('th', 'By email'),
+        ]),
+        (chain.id === 'edgeware') && m(EdgewareChainEvents),
+        (chain.id === 'kusama') && m(KusamaChainEvents),
+        (chain.id === 'polkadot') && m(PolkadotChainEvents),
+      ]),
+    ]);
+  }
+};
   
 const ChainNotificationManagementPage: m.Component<{chains: ChainInfo[],}, { selectedChain: ChainInfo,}> = {
     oninit: (vnode) => {
@@ -201,6 +242,8 @@ const ChainNotificationManagementPage: m.Component<{chains: ChainInfo[],}, { sel
       const { chains } = vnode.attrs;
       const chainIds = chains.map((c) => c.id);
       if (chains.length < 1) return;
+      const validChains = ['edgeware', 'polkadot', 'kusama'];
+      const filteredChains = chains.filter((c) => validChains.includes(c.id)).sort((a,b) => (a.id > b.id) ? 1 : -1);
       return m('ChainNotificationManagementPage', [
         m('h2', 'Subscribe to Chain Events'),
         m(SelectList, {
@@ -217,7 +260,7 @@ const ChainNotificationManagementPage: m.Component<{chains: ChainInfo[],}, { sel
               selected: (vnode.state.selectedChain === chain),
             });
           },
-          items: chains,
+          items: filteredChains,
           trigger: m(Button, {
             align: 'left',
             compact: true,
@@ -231,12 +274,8 @@ const ChainNotificationManagementPage: m.Component<{chains: ChainInfo[],}, { sel
             m.redraw();
           }
         }),
-        chains.filter((c) => c.id === 'edgeware').map((chain) => {
-          return [
-            m(EventSubscriptions, {
-              chain: vnode.state.selectedChain,
-            }),
-          ];
+        m(EventSubscriptions, {
+          chain: vnode.state.selectedChain,
         }),
       ]);
     },
