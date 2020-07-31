4858027e64a5a37e77583a94deb0a5b01bee3773
zakhap <zhs2104@columbia.edu>
2020-07-31 15:21:28 -0400

    Add to /setPrivacy to router.

--- server/router.ts
+++ server/router.ts
@@ -90,6 +90,7 @@ import updateTags from './routes/updateTags';
 import editTag from './routes/editTag';
 import deleteTag from './routes/deleteTag';
 import bulkTags from './routes/bulkTags';
+import setPrivacy from './routes/setPrivacy';
 
 import edgewareLockdropLookup from './routes/getEdgewareLockdropLookup';
 import edgewareLockdropStats from './routes/getEdgewareLockdropStats';
@@ -147,6 +148,8 @@ function setupRouter(app, models, viewCountCache: ViewCountCache) {
   router.get('/bulkThreads', bulkThreads.bind(this, models));
 
   router.get('/profile', getProfile.bind(this, models));
+  
+  router.post('/setPrivacy', passport.authenticate('jwt', { session: false }), setPrivacy.bind(this, models));
 
 
   // offchain discussion drafts
