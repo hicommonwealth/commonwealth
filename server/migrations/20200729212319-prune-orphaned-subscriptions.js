469e83bed4f0a2554645a4409f1ba5c6eedb409d
zakhap <zhs2104@columbia.edu>
2020-08-03 10:16:39 -0400

    Transactionalize up migration and add return promise to functionally null down migration.

--- server/migrations/20200729212319-prune-orphaned-subscriptions.js
+++ server/migrations/20200729212319-prune-orphaned-subscriptions.js
@@ -3,33 +3,35 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    const subscriptions = await queryInterface.sequelize.query(`SELECT * FROM "Subscriptions";`);
-    const threads = await queryInterface.sequelize.query(`SELECT * FROM "OffchainThreads" WHERE deleted_at IS NULL;`);
-    const threadIds = threads[0].map((t) => t.id);
-    const comments = await queryInterface.sequelize.query(`SELECT * FROM "OffchainComments" WHERE deleted_at IS NULL;`);
-    const commentIds = comments[0].map((c) => c.id);
-    await Promise.all(subscriptions[0].map((s) => {
-      if (s.category_id === 'new-comment-creation' || s.category_id === 'new-reaction') {
-        if (s.offchain_thread_id) {
-          if (threadIds.includes(s.offchain_thread_id)) return;
-          else {
-            queryInterface.sequelize.query(`DELETE FROM "Notifications" WHERE subscription_id=${s.id}; DELETE FROM "Subscriptions" WHERE id=${s.id};`);
+    return queryInterface.sequelize.transaction(async (t) => {
+      const subscriptions = await queryInterface.sequelize.query(`SELECT * FROM "Subscriptions";`, { transaction: t });
+      const threads = await queryInterface.sequelize.query(`SELECT * FROM "OffchainThreads" WHERE deleted_at IS NULL;`, { transaction: t });
+      const threadIds = threads[0].map((t) => t.id);
+      const comments = await queryInterface.sequelize.query(`SELECT * FROM "OffchainComments" WHERE deleted_at IS NULL;`, { transaction: t });
+      const commentIds = comments[0].map((c) => c.id);
+      await Promise.all(subscriptions[0].map((s) => {
+        if (s.category_id === 'new-comment-creation' || s.category_id === 'new-reaction') {
+          if (s.offchain_thread_id) {
+            if (threadIds.includes(s.offchain_thread_id)) return;
+            else {
+              queryInterface.sequelize.query(`DELETE FROM "Notifications" WHERE subscription_id=${s.id}; DELETE FROM "Subscriptions" WHERE id=${s.id};`, { transaction: t });
+            }
+          } else if (s.offchain_comment_id) {
+            if (commentIds.includes(s.offchain_comment_id)) return;
+            else {
+              queryInterface.sequelize.query(`DELETE FROM "Notifications" WHERE subscription_id=${s.id}; DELETE FROM "Subscriptions" WHERE id=${s.id};`, { transaction: t });
+            }
+          } else if (!s.offchain_comment_id && !s.offchain_thread_id && !s.chain_id && !s.community_id) {
+            queryInterface.sequelize.query(`DELETE FROM "Notifications" WHERE subscription_id=${s.id}; DELETE FROM "Subscriptions" WHERE id=${s.id};`, { transaction: t });
           }
-        } else if (s.offchain_comment_id) {
-          if (commentIds.includes(s.offchain_comment_id)) return;
-          else {
-            queryInterface.sequelize.query(`DELETE FROM "Notifications" WHERE subscription_id=${s.id}; DELETE FROM "Subscriptions" WHERE id=${s.id};`);
-          }
-        } else if (!s.offchain_comment_id && !s.offchain_thread_id && !s.chain_id && !s.community_id) {
-          queryInterface.sequelize.query(`DELETE FROM "Notifications" WHERE subscription_id=${s.id}; DELETE FROM "Subscriptions" WHERE id=${s.id};`);
+        } else {
+          return;
         }
-      } else {
-        return;
-      }
-    }));
+      }));
+    });
   },
 
   down: (queryInterface, Sequelize) => {
-
+    return new Promise((resolve, reject) => resolve());
   }
 };
