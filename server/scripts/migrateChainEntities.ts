1e6c53b0d4b6bc1c8d253f5637fdff2ab43fffe5
Jake Naviasky <jake@commonwealth.im>
2020-08-03 18:10:17 -0400

    Refactor api init code, refactor yarn run commands for chain events and chain identities. (#533)

---
+++ server/scripts/migrateChainEntities.ts
@@ -0,0 +1,51 @@
+/**
+ * This script "migrates" identities from the chain into the database, by first
+ * querying all addresses, and then attempting to fetch corresponding identities
+ * from the chain, writing the results back into the database.
+ */
+
+import _ from 'underscore';
+import { SubstrateTypes, SubstrateEvents } from '@commonwealth/chain-events';
+
+import MigrationHandler from '../eventHandlers/migration';
+import EntityArchivalHandler from '../eventHandlers/entityArchival';
+
+import { factory, formatFilename } from '../../shared/logging';
+import { constructSubstrateUrl } from '../../shared/substrate';
+const log = factory.getLogger(formatFilename(__filename));
+
+export default async function (models, chain?: string): Promise<void> {
+  // 1. fetch the node and url of supported/selected chains
+  log.info('Fetching node info for chain entity migrations...');
+  const chains = !chain ? SubstrateTypes.EventChains : [ chain ];
+  // query one node for each supported chain
+  const nodes = (await Promise.all(chains.map((c) => {
+    return models.ChainNode.findOne({ where: { chain: c } });
+  }))).filter((n) => !!n);
+  if (!nodes) {
+    throw new Error('no nodes found for chain entity migration');
+  }
+
+  // 2. for each node, fetch and migrate chain entities
+  for (const node of nodes) {
+    const migrationHandler = new MigrationHandler(models, node.chain);
+    const entityArchivalHandler = new EntityArchivalHandler(models, node.chain);
+
+    const nodeUrl = constructSubstrateUrl(node.url);
+    const provider = await SubstrateEvents.createProvider(nodeUrl);
+    const api = await SubstrateEvents.createApi(provider, node.chain).isReady;
+
+    // fetch all events and run through handlers in sequence then exit
+    const fetcher = new SubstrateEvents.StorageFetcher(api);
+    const events = await fetcher.fetch();
+    await Promise.all(events.map(async (event) => {
+      try {
+        // eslint-disable-next-line no-await-in-loop
+        const dbEvent = await migrationHandler.handle(event);
+        await entityArchivalHandler.handle(event, dbEvent);
+      } catch (e) {
+        log.error(`Event handle failure: ${e.message}`);
+      }
+    }));
+  }
+}
