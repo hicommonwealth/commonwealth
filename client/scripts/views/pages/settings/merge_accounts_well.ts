71ba5a7a6a1e217752b318ea54e65bfc5386c640
zakhap <zhs2104@columbia.edu>
2020-08-05 17:01:30 -0400

    Factor out selector component

--- client/scripts/views/pages/settings/merge_accounts_well.ts
+++ client/scripts/views/pages/settings/merge_accounts_well.ts
@@ -10,6 +10,48 @@ import SettingsController from 'controllers/app/settings';
 import { selectNode, initChain } from 'app';
 import { AddressInfo } from 'models';
 import { Icon, Icons, Button, Input, SelectList, ListItem } from 'construct-ui';
+import SelectAddressModal from '../../modals/select_address_modal';
+
+interface IAddressSelectListAttrs {
+  addresses: AddressInfo[];
+  onSelect: Function;
+  label: string;
+}
+
+const AddressSelectList: m.Component<IAddressSelectListAttrs, { selectedAddress: AddressInfo, }> = {
+  view: (vnode) => {
+    return m(SelectList, {
+      class: 'TagSelector',
+      filterable: false,
+      checkmark: false,
+      closeOnSelect: true,
+      itemRender: (address: AddressInfo) => {
+        return m(ListItem, {
+          class: 'account',
+          label: [
+            m(Icon, { name: Icons.USER, }),
+            m('span', address.address),
+          ],
+          selected: (vnode.state.selectedAddress === address),
+        });
+      },
+      items: vnode.attrs.addresses,
+      onSelect: (address: AddressInfo) => { vnode.attrs.onSelect(address); vnode.state.selectedAddress = address; m.redraw(); },
+      trigger: m(Button, {
+        align: 'left',
+        class: 'tag-selection-drop-menu',
+        compact: true,
+        iconRight: Icons.CHEVRON_DOWN,
+        label: vnode.state.selectedAddress
+          ? [
+            // m(Icon, { name: Icons.CHEVRON_DOWN, }),
+            m('span.tag-name', vnode.state.selectedAddress.address)
+          ]
+          : vnode.attrs.label,
+      }),
+    });
+  },
+};
 
 const MergeAccountsWell: m.Component<{}, {address1: AddressInfo; address2: AddressInfo;}> = {
   oninit: (vnode) => {
@@ -20,36 +62,17 @@ const MergeAccountsWell: m.Component<{}, {address1: AddressInfo; address2: Addre
     return m('.MergeAccountsWell', [
       m('h4', 'Merge Addresses'),
       m('p', 'Warning: This will merge all of your threads, comments, reactions, and roles from the first address to the second address. This is not reversible.'),
-      m(SelectList, {
-        class: 'TagSelector',
-        filterable: false,
-        checkmark: false,
-        closeOnSelect: true,
-        itemRender: (address: AddressInfo) => {
-          return m(ListItem, {
-            class: 'account',
-            label: [
-              m(Icon, { name: Icons.USER, }),
-              m('span', address.address),
-            ],
-            selected: (vnode.state.address1 === address),
-          });
-        },
-        items: addresses,
-        onSelect: (address: AddressInfo) => { vnode.state.address1 = address; m.redraw(); console.dir(vnode.state.address1); },
-        trigger: m(Button, {
-          align: 'left',
-          class: 'tag-selection-drop-menu',
-          compact: true,
-          iconRight: Icons.CHEVRON_DOWN,
-          label: vnode.state.address1
-            ? [
-              // m(Icon, { name: Icons.CHEVRON_DOWN, }),
-              m('span.tag-name', vnode.state.address1.address)
-            ]
-            : '',
-        }),
-      })
+      m(AddressSelectList, {
+        label: 'Select address to be merged',
+        addresses,
+        onSelect: (address: AddressInfo) => { vnode.state.address1 = address; m.redraw(); }
+      }),
+      m(Icon, { name: Icons.ARROW_RIGHT }),
+      m(AddressSelectList, {
+        label: 'Select final address to own all content',
+        addresses,
+        onSelect: (address: AddressInfo) => { vnode.state.address2 = address; m.redraw(); }
+      }),
     ]);
   }
 };
