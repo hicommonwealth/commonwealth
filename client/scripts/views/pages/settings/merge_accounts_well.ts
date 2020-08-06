1694c2ab6252d85ec227ace821341b6d64c8263a
zakhap <zhs2104@columbia.edu>
2020-08-06 10:30:59 -0400

    Change addresses to account objects in custom selects.

--- client/scripts/views/pages/settings/merge_accounts_well.ts
+++ client/scripts/views/pages/settings/merge_accounts_well.ts
@@ -8,43 +8,51 @@ import { DropdownFormField, RadioSelectorFormField } from 'views/components/form
 import { notifySuccess } from 'controllers/app/notifications';
 import SettingsController from 'controllers/app/settings';
 import { selectNode, initChain } from 'app';
-import { AddressInfo } from 'models';
+import { AddressInfo, Account } from 'models';
 import { Icon, Icons, Button, Input, SelectList, ListItem } from 'construct-ui';
 import SelectAddressModal from '../../modals/select_address_modal';
+import { getSignatureForMessage } from '../../modals/message_signing_modal';
 
-interface IAddressSelectListAttrs {
-  addresses: AddressInfo[];
+interface IAccountSelectListAttrs {
+  accounts: Account<any>[];
   onSelect: Function;
   label: string;
 }
 
-const AddressSelectList: m.Component<IAddressSelectListAttrs, { selectedAddress: AddressInfo, }> = {
+const AccountSelectList: m.Component<IAccountSelectListAttrs, { selectedAccount: Account<any>, }> = {
   view: (vnode) => {
     return m(SelectList, {
-      class: 'TagSelector',
+      class: 'AccountSelector',
       filterable: false,
       checkmark: false,
       closeOnSelect: true,
-      itemRender: (address: AddressInfo) => {
+      itemRender: (account: Account<any>) => {
         return m(ListItem, {
           class: 'account',
           label: [
             m(Icon, { name: Icons.USER, }),
-            m('span', address.address),
+            account.profile.name && m('span', `${account.profile.name}:`),
+            m('span', account.address),
           ],
-          selected: (vnode.state.selectedAddress === address),
+          selected: (vnode.state.selectedAccount === account),
         });
       },
-      items: vnode.attrs.addresses,
-      onSelect: (address: AddressInfo) => { vnode.attrs.onSelect(address); vnode.state.selectedAddress = address; m.redraw(); },
+      items: vnode.attrs.accounts,
+      onSelect: (account: Account<any>) => {
+        vnode.attrs.onSelect(account);
+        vnode.state.selectedAccount = account;
+        m.redraw();
+      },
       trigger: m(Button, {
         align: 'left',
         class: 'tag-selection-drop-menu',
         compact: true,
         iconRight: Icons.CHEVRON_DOWN,
-        label: vnode.state.selectedAddress
+        label: vnode.state.selectedAccount
           ? [
-            m('span.tag-name', vnode.state.selectedAddress.address)
+            m('span.tag-name',
+              vnode.state.selectedAccount.profile.name
+              || vnode.state.selectedAccount.address),
           ]
           : vnode.attrs.label,
       }),
@@ -52,24 +60,32 @@ const AddressSelectList: m.Component<IAddressSelectListAttrs, { selectedAddress:
   },
 };
 
-const MergeAccountsWell: m.Component<{}, {address1: AddressInfo; address2: AddressInfo;}> = {
+const MergeAccountsWell: m.Component<{}, {address1: Account<any>; address2: Account<any>;}> = {
   view: (vnode) => {
     const addresses = app.user.addresses;
+    const accounts = app.user.activeAccounts;
 
     return m('.MergeAccountsWell', [
       m('h4', 'Merge Addresses'),
       m('p', 'Warning: This will merge all of your threads, comments, reactions, and roles from the first address to the second address. This is not reversible.'),
-      m(AddressSelectList, {
+      m(AccountSelectList, {
         label: 'Select address to be merged',
-        addresses,
-        onSelect: (address: AddressInfo) => { vnode.state.address1 = address; m.redraw(); }
+        accounts,
+        onSelect: (account: Account<any>) => { vnode.state.address1 = account; m.redraw(); }
       }),
       m(Icon, { name: Icons.ARROW_RIGHT }),
-      m(AddressSelectList, {
+      m(AccountSelectList, {
         label: 'Select final address to own all content',
-        addresses,
-        onSelect: (address: AddressInfo) => { vnode.state.address2 = address; m.redraw(); }
+        accounts: accounts.filter((a) => a !== vnode.state.address1),
+        onSelect: (account: Account<any>) => { vnode.state.address2 = account; m.redraw(); }
       }),
+      m(Button, {
+        label: 'Confirm?',
+        onclick: () => {
+          // const account = app.user.
+          // const signatureData = await getSignatureForMessage(vnode.state.address1);
+        },
+      })
     ]);
   }
 };
