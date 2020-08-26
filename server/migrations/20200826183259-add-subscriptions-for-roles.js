5e37dc8932908291900bc7217b9f9aa62d62e556
zakhap <zhs2104@columbia.edu>
2020-08-26 12:21:56 -0700

    Migration mostly working :muscle: lots of queries and checks doing what they expect

--- server/migrations/20200826183259-add-subscriptions-for-roles.js
+++ server/migrations/20200826183259-add-subscriptions-for-roles.js
@@ -11,30 +11,47 @@ module.exports = {
         const { id, } = user;
         const resAddresses = await queryInterface.sequelize.query(`SELECT * FROM "Addresses" WHERE user_id=${id};`, { transaction: t, });
         const addresses = resAddresses[0];
-        console.log('addresses', addresses);
+        // console.log('addresses', addresses.length);
         const userRoles = [];
 
-        // await Promise.all(addresses.map(async (address) => {
-        //   const addressRoles = await queryInterface.sequelize.query(`SELECT * FROM "Roles" WHERE address_id=${address.id};`, { transaction: t, });
-        //   addressRoles[0].forEach((r) => userRoles.push(r));
-        // }));
+        await Promise.all(addresses.map(async (address) => {
+          const addressRoles = await queryInterface.sequelize.query(`SELECT * FROM "Roles" WHERE address_id=${address.id};`, { transaction: t, });
+          // console.log('roles', addressRoles[0].length);
+          addressRoles[0].forEach((r) => userRoles.push(r));
+        }));
 
         // console.log('User Roles', userRoles);
 
-        // const communityIds = [];
-        // const chainIds = [];
+        const subscriptions = await queryInterface.sequelize.query(`SELECT * FROM "Subscriptions" WHERE subscriber_id=${id};`);
+        const existingSubscriptions = [];
+        subscriptions[0].forEach((s) => {
+          if (!existingSubscriptions.includes(s.object_id)) {
+            existingSubscriptions.push(s.object_id);
+          }
+        });
+        // console.log('existingSubscriptions', existingSubscriptions);
 
-        // userRoles.forEach((r) => {
-        //   if (r.offchain_community_id && !communityIds.includes(r.offchain_community_id)) {
-        //     communityIds.push(r.offchain_community_id);
-        //   } else if (r.chain_id && !chainIds.includes(r.chain_id)) {
-        //     chainIds.push(r.chain_id);
-        //   }
-        // });
+        const communityIds = [];
+        const chainIds = [];
+
+        userRoles.forEach((r) => {
+          if (r.offchain_community_id && !communityIds.includes(r.offchain_community_id) && !existingSubscriptions.includes(r.offchain_community_id)) {
+            communityIds.push(r.offchain_community_id);
+          } else if (r.chain_id && !chainIds.includes(r.chain_id) && !existingSubscriptions.includes(r.chain_id)) {
+            chainIds.push(r.chain_id);
+          }
+        });
 
         // console.log("Community Ids", communityIds);
         // console.log("Chain Ids", chainIds);
 
+        if (communityIds.length + chainIds.length > 0) {
+          console.log('user id', id);
+          console.log('userRoles', userRoles);
+          console.log('subscriptions', existingSubscriptions);
+          console.log('community Ids', communityIds);
+          console.log('chain ids', chainIds);
+        }
         // await Promise.all(communityIds.map(async (communityId) => {
         //   await queryInterface.sequelize.query(`INSERT INTO "Subscriptions" (subscriber_id, category_id, object_id, chain_id, community_id) VALUES (${id}, 'new-thread-creation', '${communityId}', NULL, '${communityId}');`)
         // }));
@@ -47,6 +64,9 @@ module.exports = {
   },
 
   down: (queryInterface, DataTypes) => {
+    return queryInterface.sequelize.transaction(async (t) => {
+      await Promise.all([]);
+    });
     /*
       Add reverting commands here.
       Return a promise to correctly handle asynchronicity.
