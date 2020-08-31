50e7edac48e48b7406c24d0e2e98ec8fd1da2281
q <q@qs-MacBook-Pro-13.home>
2020-08-31 16:35:28 -0700

    Allow community_info_module to load invite modal button on chain community to admins/mods

--- client/scripts/views/components/sidebar/community_info_module.ts
+++ client/scripts/views/components/sidebar/community_info_module.ts
@@ -25,6 +25,7 @@ const CommunityInfoModule: m.Component<{ communityName: string, communityDescrip
 
     const meta = app.chain ? app.chain.meta.chain : app.community.meta;
     const { name, description, website, chat, telegram, github } = meta;
+    console.log(isAdmin)
 
     return m('.CommunityInfoModule.SidebarModule', [
       m('.community-name', [
@@ -38,19 +39,17 @@ const CommunityInfoModule: m.Component<{ communityName: string, communityDescrip
           m(Icon, { name: Icons.SETTINGS }),
         ]),
         // TODO: get this working for chains
-        !app.chain
-          && app.community
-          && app.user?.activeAccount
-          && (app.community.meta.invitesEnabled
-              || app.user.isAdminOrModOfEntity({ community: app.activeCommunityId() }))
+        (app.community?.meta.invitesEnabled
+              || isAdmin)
           && m('.community-info-action', {
             onclick: (e) => {
               e.preventDefault();
+              const data = app.activeCommunityId()
+                ? { communityInfo: app.community.meta }
+                : { chainInfo: app.chain.meta.chain }
               app.modals.create({
                 modal: CreateInviteModal,
-                data: {
-                  communityInfo: app.community.meta,
-                },
+                data,
               });
             },
           }, [
