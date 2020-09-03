7f0d3bd3700fc9d7fb51f479cdba1651d0e6e37b
zakhap <zhs2104@columbia.edu>
2020-09-03 11:20:22 -0700

    Instantiate and fill in migration, untested

---
+++ server/migrations/20200903181622-add-chain-property-to-invite-links.js
@@ -0,0 +1,23 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.sequelize.transaction((t) => {
+      return Promise.all([
+        queryInterface.addColumn('InviteLinks', 'chain_id', {
+          type: DataTypes.STRING, allowNull: true,
+        }, { transaction: t }),
+        queryInterface.changeColumn('InviteLinks', 'community_id', { type: DataTypes.STRING, allowNull: true }, { transaction: t })
+      ]);
+    });
+
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.sequelize.transaction((t) => {
+      return Promise.all([
+        queryInterface.removeColumn('InviteLinks', 'chain_id', { transaction: t }),
+      ]);
+    });
+  }
+};
