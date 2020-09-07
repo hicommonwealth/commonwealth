b6a636337c877a0e1bc3b5e589560f131343efa2
Syed Hussain Mehdi <mehdi@seedcode.io>
2020-09-07 11:08:21 +0500

    Select component changed to SelectList for ui consistancy

--- client/scripts/views/pages/staking_calculator/stakingcalculator.ts
+++ client/scripts/views/pages/staking_calculator/stakingcalculator.ts
@@ -9,7 +9,7 @@ import $ from 'jquery';
 import Substrate from 'controllers/chain/substrate/main';
 import { SubstrateAccount, IValidators } from 'controllers/chain/substrate/account';
 import { DeriveSessionProgress } from '@polkadot/api-derive/types';
-import { Grid, Col, Button, Input, Form, FormGroup, PopoverMenu, MenuItem, Icon, Icons, Tag, Switch, Select } from 'construct-ui';
+import { Grid, Col, Button, Input, Form, FormGroup, PopoverMenu, MenuItem, Icon, Icons, Tag, Switch, Select, SelectList, ListItem } from 'construct-ui';
 
 
 interface IPreHeaderState {
@@ -19,60 +19,27 @@ interface IPreHeaderState {
   },
 }
 
+class AssetInfo {
+  name: string
+  sym: string
+}
 
-const StakingCalculatorPage: m.Component<{}> = {
+const assets_list: AssetInfo[] = [
+  { name: "Edgeware", sym: "EDG" },
+  { name: "Kusama", sym: "KSM" },
+  { name: "SomethingElse", sym: "STE" },
+]
+
+const StakingCalculatorPage: m.Component<{}, { selected_asset: AssetInfo, selected_rate: number }> = {
+
   oncreate: (vnode) => {
     mixpanel.track('PageVisit', { 'Page Name': 'StakingCalculatorPage' });
+    vnode.state.selected_rate = 13.3;
+    vnode.state.selected_asset = assets_list[0];
   },
   view: (vnode) => {
     if (!app.chain || !app.chain.loaded) return m(PageLoading, { message: 'Connecting to chain...', title: 'Staking Calculator' });
 
-    const ctrl = {
-      selected_val: "Edgeware",
-      selected_sym: "EDG",
-      opts: ["Edgeware", "Kusama", "SomethingElse"],
-      opts_sym: ["EDG", "KSM", "STE"],
-      doSomething: function () {
-        console.log("Selected: " + ctrl.selected_val)
-        ctrl.selected_sym = ctrl.opts_sym[ctrl.opts.indexOf(ctrl.selected_val)]
-        console.log('selected_sym ', ctrl.selected_sym)
-        $('#stake-sym-tag').text(ctrl.selected_sym);
-        let totalStaked = (app.chain as Substrate).chain.coins(0);
-        
-        const totalbalance = (app.chain as Substrate).chain.totalbalance;
-        const staked = `${(totalStaked.muln(10000).div(totalbalance).toNumber() / 100).toFixed(2)}%`;
-
-        // const { validators, sessionInfo } = vnode.state.dynamic;
-
-        // Object.entries(validators).forEach(([_stash, { exposure, isElected }]) => {
-        //   const valStake = (app.chain as Substrate).chain.coins(exposure?.total.toBn())
-        //   || (app.chain as Substrate).chain.coins(0);
-        //   totalStaked = (app.chain as Substrate).chain.coins(totalStaked.asBN.add(valStake.asBN));
-    
-        //   // count total nominators
-        //   const others = exposure?.others || [];
-        //   others.forEach((indv) => {
-        //     const nominator = indv.who.toString();
-        //     if (!nominators.includes(nominator)) {
-        //       nominators.push(nominator);
-        //     }
-        //   });
-        //   // count elected and waiting validators
-        //   if (isElected) {
-        //     elected++;
-        //   } else {
-        //     waiting++;
-        //   }
-        // });
-        // const qq = app.chain as Substrate;
-        // debugger;
-      },
-      doSomething1: function () {
-        console.log("CurrentVal: " + ctrl.selected_rate)
-        $('#rate-val').text(ctrl.selected_rate + '%');
-      },
-      selected_rate: 13.3
-    }
     return m(Sublayout, {
       class: 'StakingCalculatorPage',
       title: 'Staking Calulator',
@@ -82,15 +49,48 @@ const StakingCalculatorPage: m.Component<{}> = {
         class: 'staking_calc_wrpr'
       }, [m(".title_div", [m("h4", "Staking Calculator")],
 
-        m(".select-asset", [m("label", "SELECT ASSET"), m(Select, {
-          onchange: (e: Event) => {
-            ctrl.selected_val = (e.target as any).value
-            ctrl.doSomething();
+        m(".select-asset", [m("label", "SELECT ASSET"),
+        //  m(Select, {
+        //   onchange: (e: Event) => {
+        //     ctrl.selected_val = (e.target as any).value
+        //     ctrl.doSomething();
+        //   },
+        //   id: 'id-asset-select',
+        //   options: ctrl.opts,
+        //   defaultValue: ctrl.selected_val
+        // })
+
+
+        m(SelectList, {
+          class: 'AssetSelectList',
+          filterable: false,
+          checkmark: false,
+          emptyContent: null,
+          inputAttrs: {
+            class: 'AssetSelectRow',
           },
-          id: 'id-asset-select',
-          options: ctrl.opts,
-          defaultValue: ctrl.selected_val
-        })]),
+          itemRender: (item: AssetInfo) => {
+            return m(ListItem, {
+              label: item.name,
+              selected: (vnode.state.selected_asset === item),
+            });
+          },
+          items: assets_list,
+          trigger: m(Button, {
+            align: 'left',
+            compact: true,
+            iconRight: Icons.CHEVRON_DOWN,
+            label: vnode.state.selected_asset
+              ? vnode.state.selected_asset.name
+              : 'Select Asset',
+          }),
+          onSelect: (item: AssetInfo) => {
+            vnode.state.selected_asset = item;
+            m.redraw();
+          }
+        })
+
+        ]),
 
         m(".select-asset", [m("label", "ENTER STAKING AMOUNT"), m(Input, {
           fluid: true,
@@ -100,8 +100,7 @@ const StakingCalculatorPage: m.Component<{}> = {
           onclick: (e) => {
             e.stopPropagation();
           },
-
-          contentRight: m(Tag, { label: ctrl.selected_sym, id: 'stake-sym-tag' })
+          contentRight: m(Tag, { label: vnode.state.selected_asset ? vnode.state.selected_asset.sym : "--", id: 'stake-sym-tag' })
         })
         ]),
         m(".select-asset", [m("label", "ENTER STAKING LENGTH"), m(Input, {
@@ -115,17 +114,16 @@ const StakingCalculatorPage: m.Component<{}> = {
           contentRight: m(Tag, { "label": "Days" })
         })
         ]),
-        m(".select-asset", [m("label", "STAKING RATE", [m("#rate-val", ctrl.selected_rate + '%')]), m(Input, {
+        m(".select-asset", [m("label", "STAKING RATE", [m("#rate-val", (vnode.state.selected_rate ? +vnode.state.selected_rate : 0) + '%')]), m(Input, {
           fluid: true,
           name: 'stake_rate',
           type: 'range',
           min: "0.01",
           max: "100",
           step: "0.01",
-          defaultValue: ctrl.selected_rate.toString(),
+          defaultValue: (vnode.state.selected_rate ? vnode.state.selected_rate : 0).toString(),
           oninput: (e) => {
-            ctrl.selected_rate = e.target.value;
-            ctrl.doSomething1();
+            vnode.state.selected_rate = e.target.value;
           }
         })
         ]),
@@ -155,9 +153,9 @@ const StakingCalculatorPage: m.Component<{}> = {
       m(Grid, {
         class: 'staking_calc_wrpr'
       }, [m(".title_div", [m("h5", "Returns")]),
-        m(".returns_content_div",[m("strong","1 Day @ 0.071%"),m("p","0.098 EDG"),m("span","($0.70)")]),
-        m(".returns_content_div .borderleft_right",[m("strong","1 Month @ 0.071%"),m("p","0.098 EDG"),m("span","($0.70)")]),
-        m(".returns_content_div",[m("strong","1 Year @ 0.071%"),m("p","0.098 EDG"),m("span","($0.70)")])
+      m(".returns_content_div", [m("strong", "1 Day @ 0.071%"), m("p", "0.098 EDG"), m("span", "($0.70)")]),
+      m(".returns_content_div .borderleft_right", [m("strong", "1 Month @ 0.071%"), m("p", "0.098 EDG"), m("span", "($0.70)")]),
+      m(".returns_content_div", [m("strong", "1 Year @ 0.071%"), m("p", "0.098 EDG"), m("span", "($0.70)")])
       ]
       )
     ]);
