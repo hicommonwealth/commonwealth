210d71b3daf85c5f7fb17276833bb93fd69dd94c
Furqan A <35498810+nblogist@users.noreply.github.com>
2020-09-14 21:39:21 +0500

    Review Changes for #555 #556 #594 (#606)
    * Fixes for unifying repeated logic and making return consisitent #556 #594
    
    * Few Bug Fixes #556
    
    * Fixes #594 #556
    
    * Added routes tests #556
    
    * Not procviding start and end  date now returns 30 days of data for:
    /getRewards
    /getExposureOverTime
    
    * Not procviding start and end  date now returns 30 days of data for:
    /getRewards
    /getExposureOverTime
    
    * Added stash_id parameter for getRewards
    
    * Minor Changes in routes
    Removed unnecessary join in /getExposureOverTime
    Defaulted to 30 days data if start & end dates aren't in /getOffences
    Returned a REST-ful response to empty array in /getOffences
    Renamed stash_id  parameter to stash, to keep it consistent with others
    
    * modified getExposureOverTime.ts to fit for graphs requirements
    
    * Added getImOnline event
    
    * Slight modifications to fit for graphs

---
+++ server/routes/getExposureOverTime.ts
@@ -0,0 +1,152 @@
+import Sequelize from 'sequelize';
+import { Exposure, BlockNumber, AccountId, IndividualExposure } from '@polkadot/types/interfaces';
+import { Request, Response, NextFunction } from 'express';
+import { Errors } from './getOffences';
+import { type } from 'jquery';
+const Op = Sequelize.Op;
+
+interface IEventData {
+  stash: AccountId;
+  exposure: Exposure;
+  block: BlockNumber;
+}
+
+// eslint-disable-next-line prefer-const
+let validators: { [key: string]: { [block: string]: any } } = {};
+
+// COMMON
+const getStakeOverTime = async (models, req: Request, res: Response, next: NextFunction) => {
+  const { stash } = req.query;
+  let { startDate, endDate } = req.query;
+  const { chain } = req.query;
+  // const tmp_chain: string = chain as unknown as string;
+  // chain = tmp_chain.toLowerCase();
+  if (!chain) return next(new Error(Errors.ChainIdNotFound));
+  const chainInfo = await models.Chain.findOne({ where: { id: chain } });
+  if (!chainInfo) return next(new Error(Errors.InvalidChain));
+
+  // if start and end date isn't given, we set it for 30 days for now
+  if (typeof startDate === 'undefined' || typeof endDate === 'undefined') {
+    endDate = new Date();
+    startDate = new Date();
+    endDate = endDate.toISOString(); // 2020-08-08T12:46:32.276Z FORMAT // today's date
+    startDate.setDate(startDate.getDate() - 30);
+    startDate = startDate.toISOString(); // 2020-08-08T12:46:32.276Z FORMAT // 30 days ago date
+  }
+
+  const where: any = {};
+
+  // if stash is given
+  if (stash) where['$HistoricalValidatorStatistic.stash$'] = stash;
+
+  if (startDate && endDate) {
+    where.created_at = {
+      [Op.between]: [startDate, endDate]
+    };
+  }
+
+  const stakeOverTime = await models.HistoricalValidatorStatistic.findAll({
+    where,
+    order: [
+      ['created_at', 'ASC']
+    ],
+    attributes: ['stash', 'exposure', 'block'],
+  });
+  return stakeOverTime;
+};
+
+const getTotalStakeOverTime = async (models, req: Request, res: Response, next: NextFunction) => {
+  const stakeOverTime = await getStakeOverTime(models, req, res, next);
+  validators = {};
+
+  stakeOverTime.forEach((stake) => {
+    const event_data: IEventData = stake.dataValues;
+    const key = event_data.stash.toString();
+
+    // eslint-disable-next-line no-prototype-builtins
+    if (validators.hasOwnProperty(key)) {
+      validators[key][event_data.block.toString()] = Number(event_data.exposure.total);
+    } else {
+      validators[key] = {};
+      validators[key][event_data.block.toString()] = Number(event_data.exposure.total);
+    }
+  });
+  return res.json({ status: 'Success', result: { validators: validators || [] } });
+};
+
+const getOwnStakeOverTime = async (models, req: Request, res: Response, next: NextFunction) => {
+  const stakeOverTime = await getStakeOverTime(models, req, res, next);
+  validators = {};
+
+  stakeOverTime.forEach((stake) => {
+    const event_data: IEventData = stake.dataValues;
+    const key = event_data.stash.toString();
+
+    // eslint-disable-next-line no-prototype-builtins
+    if (validators.hasOwnProperty(key)) {
+      validators[key][event_data.block.toString()] = Number(event_data.exposure.own);
+    } else {
+      validators[key] = {};
+      validators[key][event_data.block.toString()] = Number(event_data.exposure.own);
+    }
+  });
+
+  return res.json({ status: 'Success', result: { validators: validators || [] } });
+};
+
+const getOtherStakeOverTime = async (models, req: Request, res: Response, next: NextFunction) => {
+  const stakeOverTime = await getStakeOverTime(models, req, res, next);
+  const { onlyValue } = req.query;
+  validators = {};
+
+  stakeOverTime.forEach((stake) => {
+    const event_data: IEventData = stake.dataValues;
+    const key = event_data.stash.toString();
+
+    // eslint-disable-next-line no-prototype-builtins
+    if (validators.hasOwnProperty(key)) {
+      let nominatorValue: any = event_data.exposure.others;
+      // eslint-disable-next-line max-len
+      if (onlyValue) nominatorValue = event_data.exposure.others.map((q) => typeof (q.value) === 'string' ? parseInt(q.value, 16) : Number(q.value))
+        .reduce((a: number, b: number) => a + b);
+      validators[key][event_data.block.toString()] = nominatorValue;
+    } else {
+      validators[key] = {};
+      let nominatorValue: any = event_data.exposure.others;
+      // eslint-disable-next-line max-len
+      if (onlyValue) nominatorValue = event_data.exposure.others.map((q) => typeof (q.value) === 'string' ? parseInt(q.value, 16) : Number(q.value))
+        .reduce((a: number, b: number) => a + b);
+      validators[key][event_data.block.toString()] = nominatorValue;
+    }
+  });
+
+  return res.json({ status: 'Success', result: { validators: validators || [] } });
+};
+
+const getNominatorsOverTime = async (models, req: Request, res: Response, next: NextFunction) => {
+  const stakeOverTime = await getStakeOverTime(models, req, res, next);
+  const { onlyValue } = req.query;
+  validators = {};
+
+  stakeOverTime.forEach((stake) => {
+    const event_data: IEventData = stake.dataValues;
+    const key = event_data.stash.toString();
+
+    // eslint-disable-next-line no-prototype-builtins
+    if (validators.hasOwnProperty(key)) {
+      let nominatorValue: any = event_data.exposure.others.map((nominator) => nominator.who);
+      if (onlyValue) nominatorValue = nominatorValue.length.toString();
+      validators[key][event_data.block.toString()] = nominatorValue;
+    } else {
+      validators[key] = {};
+      let nominatorValue: any = event_data.exposure.others.map((nominator) => nominator.who);
+      // eslint-disable-next-line max-len
+      if (onlyValue) nominatorValue = nominatorValue.length.toString();
+      validators[key][event_data.block.toString()] = nominatorValue;
+    }
+  });
+
+  return res.json({ status: 'Success', result: { nominators: validators || [] } });
+};
+
+export { getTotalStakeOverTime, getOwnStakeOverTime, getOtherStakeOverTime, getNominatorsOverTime };
--- server/routes/getRewards.ts
+++ server/routes/getRewards.ts
@@ -18,7 +18,7 @@ interface IEventData {
 }
 
 const getRewards = async (models, req: Request, res: Response, next: NextFunction) => {
-  const { chain, stash_id } = req.query;
+  const { chain, stash } = req.query;
   let { startDate, endDate } = req.query;
 
   if (!chain) return next(new Error(Errors.ChainIdNotFound));
@@ -37,11 +37,13 @@ const getRewards = async (models, req: Request, res: Response, next: NextFunctio
     return next(new Error(Errors.InvalidChain));
   }
 
+  // if start and end date isn't given, we set it for 30 days for now
   if (typeof startDate === 'undefined' || typeof endDate === 'undefined') {
-    startDate = new Date();
-    startDate.setFullYear(startDate.getFullYear() - 1);
-    startDate = new Date(startDate);
     endDate = new Date();
+    startDate = new Date();
+    endDate = endDate.toISOString(); // 2020-08-08T12:46:32.276Z FORMAT // today's date
+    startDate.setDate(startDate.getDate() - 30);
+    startDate = startDate.toISOString(); // 2020-08-08T12:46:32.276Z FORMAT // 30 days ago date
   }
 
   const rewards = await models.ChainEvent.findAll({
@@ -61,14 +63,6 @@ const getRewards = async (models, req: Request, res: Response, next: NextFunctio
   });
 
   const validators: { [key: string]: any[] } = {};
-  // No rewards
-  if (!rewards.length)
-    return res.json({
-      status: 'Success',
-      result: {
-        validators
-      }
-    });
 
   rewards.forEach((reward) => {
     const event_data: IEventData = reward.dataValues.event_data;
@@ -82,12 +76,9 @@ const getRewards = async (models, req: Request, res: Response, next: NextFunctio
     }
   });
 
-  return res.json({
-    status: 'Success',
-    result: {
-      validators
-    }
-  });
+  if (stash)
+    return res.json({ status: 'Success', result: { validator: validators[stash] || [] } });
+  return res.json({ status: 'Success', result: { validators: validators || [] } });
 };
 
 export default getRewards;
