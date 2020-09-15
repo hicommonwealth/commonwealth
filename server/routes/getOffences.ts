ec5c57670d46f3112be606daaf076ac9f364480a
Syed Hussain Mehdi <mehdi@seedcode.io>
2020-09-15 16:41:42 +0500

    getOffence on stackId and response fix

--- server/routes/getOffences.ts
+++ server/routes/getOffences.ts
@@ -10,6 +10,13 @@ export const Errors = {
   NoRecordsFound: 'No records found',
 };
 
+interface ISomeOfflineEventData {
+  kind: string;
+  sessionIndex: number;
+  validators: Array<string>;
+}
+
+
 const getOffences = async (models, req: Request, res: Response, next: NextFunction) => {
   const { chain, stash } = req.query;
   let { startDate, endDate } = req.query;
@@ -31,24 +38,43 @@ const getOffences = async (models, req: Request, res: Response, next: NextFuncti
     startDate.setDate(startDate.getDate() - 30);
     startDate = startDate.toISOString(); // 2020-08-08T12:46:32.276Z FORMAT // 30 days ago date
   }
-
   const where: any = {
-    '$ChainEventType.chain$': chain,
-    '$ChainEventType.event_name$': 'offence'
+    chain_event_type_id: `${chain}-some-offline`
   };
+
+  if (stash) {
+    where.event_data = {
+      [Op.like]: `%${stash}%`
+    }
+  }
+
   if (startDate && endDate) {
     where.created_at = {
       [Op.between]: [startDate, endDate]
     };
   }
   const offences = await models.ChainEvent.findAll({
-    where,
-    include: [
-      { model: models.ChainEventType }
-    ]
+    where
   });
 
-  return res.json({ status: 'Success', result: { validatorOffences: offences || [] } });
+  const validators: { [key: string]: any[] } = {};
+
+  offences.forEach((ofc) => {
+    const event_data: ISomeOfflineEventData = ofc.dataValues.event_data;
+
+    const keys = event_data.validators || chain;
+    keys.array.forEach(key => {
+      if (key in validators) {
+        if (validators.hasOwnProperty(key)) {
+          validators[key].push(ofc.block_number);
+        }
+      } else {
+        validators[key] = [ofc.block_number];
+      }
+    });
+  });
+
+  return res.json({ status: 'Success', result: { validatorOffences: validators || [] } });
 };
 
 export default getOffences;
