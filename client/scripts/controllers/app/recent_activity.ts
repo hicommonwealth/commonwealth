212f9807d57d602b1045e91522f29d1d0c6712ac
Suspended Reason <suspendedreason@gmail.com>
2020-09-17 12:30:06 -0400

    More progress on store + controller hookup

--- client/scripts/controllers/app/recent_activity.ts
+++ client/scripts/controllers/app/recent_activity.ts
@@ -1,65 +1,38 @@
 import RecentActivityStore from 'client/scripts/stores/ActivityStore';
 import { OffchainThread, AddressInfo } from 'models';
 class RecentActivityController {
-    private _store = new RecentActivityStore();
+  private _store = new RecentActivityStore();
 
-    public get store() { return this._store; }
+  public get store() { return this._store; }
 
-    private _initialized = false;
+  private _initialized = false;
 
-    public get initialized() { return this._initialized; }
+  public get initialized() { return this._initialized; }
 
-    public addThreads(threads: OffchainThread[]) {
-        // TODO: Switch over to adding to store, rather than activeThreads
-        const activeThreads = {};
-        threads.forEach((thread) => {
-            const entity = thread.community || thread.chain;
-            if (activeThreads[entity]) {
-              activeThreads[entity][thread.id] = thread;
-            } else if (!activeThreads[entity]) {
-              const addr = {};
-              addr[thread.id] = thread;
-              activeThreads[entity] = addr;
-            }
-          });
-    }
+  public addThreads(threads: OffchainThread[]) {
+    threads.forEach((thread) => this._store.addThread(thread));
+  }
 
-    public addAddresses(addresses: AddressInfo) {
+  public addAddresses(addresses: AddressInfo) {
 
-    }
+  }
 
-    public addAddressesFromActivity(activity: any[]) {
-        // TODO: Switch over to adding to store, rather than activeAddresses
-        const activeAddresses = {};
-        activity.sort((a, b) => (b.updated_at || b.created_at) - (a.updated_at || a.created_at));
-        activity.forEach((item) => {
-            const entity = item.community || item.chain;
-            if (activeAddresses[entity]) {
-              if (activeAddresses[entity][item.address_id]) {
-                activeAddresses[entity][item.address_id]['postCount'] += 1;
-              } else {
-                activeAddresses[entity][item.address_id] = {
-                  'postCount': 1,
-                  'addressInfo': [item.Address.chain, item.Address.address]
-                };
-              }
-            } else if (!activeAddresses[entity]) {
-              activeAddresses[entity] = {};
-              activeAddresses[entity][item.address_id] = {
-                'postCount': 1,
-                'addressInfo': [item.Address.chain, item.Address.address]
-              };
-            }
-          });
-    }
+  public addAddressesFromActivity(activity: any[]) {
+    // TODO: Switch over to adding to store, rather than activeAddresses
+    activity.sort((a, b) => (b.updated_at || b.created_at) - (a.updated_at || a.created_at));
+    activity.forEach((item) => {
+      const parentEntity = item.community || item.chain;
+      this._store.addAddress(item.Address, parentEntity);
+    });
+  }
 
-    public getThreadsByCommunity(community: string) {
+  public getThreadsByCommunity(community: string) {
 
-    }
+  }
 
-    public getAddressesByCommunity(community: string) {
-        
-    }
+  public getAddressesByCommunity(community: string) {
+    
+  }
 }
 
 export default RecentActivityController;
--- client/scripts/stores/ActivityStore.ts
+++ client/scripts/stores/ActivityStore.ts
@@ -1,10 +1,14 @@
-import IdStore from './IdStore';
-import { AddressInfo, DiscussionDraft, OffchainThread } from '../models';
+import { AddressInfo, OffchainThread } from '../models';
 import { byAscendingCreationDate } from '../helpers';
+
+interface IRecentActivity {
+  postCount: number;
+  addressInfo: AddressInfo;
+}
 
 class RecentActivityStore {
   private _threadsByCommunity: { [identifier: string]: Array<OffchainThread> } = {};
-  private _addressesByCommunity: { [identifier: string]: Array<AddressInfo> } = {};
+  private _addressesByCommunity: { [identifier: string]: any } = {};
 
   public addThread(thread: OffchainThread) {
     const parentEntity = thread.community || thread.chain;
@@ -16,10 +20,38 @@ class RecentActivityStore {
     return this;
   }
 
-  public remove(thread: OffchainThread) {
+  public removeThread(thread: OffchainThread) {
     const parentEntity = thread.community || thread.chain;
     const communityStore = this._threadsByCommunity[parentEntity];
     const matchingthread = communityStore.filter((t) => t.id === thread.id)[0];
+    const proposalIndex = communityStore.indexOf(matchingthread);
+    if (proposalIndex === -1) {
+      throw new Error('thread not in store');
+    }
+    communityStore.splice(proposalIndex, 1);
+    return this;
+  }
+
+  public addAddress(address: any, parentEntity: string) {
+    const { id, chain } = address;
+    const communityStore = this._addressesByCommunity[parentEntity];
+    if (!communityStore) {
+      this._addressesByCommunity[parentEntity] = [];
+    }
+    if (!communityStore[id]) {
+      const addressInfo = new AddressInfo(id, address, chain, null);
+      const postCount = 1;
+      communityStore[id] = { addressInfo, postCount };
+    } else {
+      communityStore[id]['postCount'] += 1;
+    }
+    communityStore.sort(byAscendingCreationDate);
+    return this;
+  }
+
+  public removeAddress(address: AddressInfo, parentEntity: string) {
+    const communityStore = this._addressesByCommunity[parentEntity];
+    const matchingthread = communityStore.filter((t) => t.id === address.id)[0];
     const proposalIndex = communityStore.indexOf(matchingthread);
     if (proposalIndex === -1) {
       throw new Error('thread not in store');
