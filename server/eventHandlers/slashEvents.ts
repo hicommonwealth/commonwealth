06ded84aaf7c330f118b69b1260eb33980e8e78d
Zohaib Khan <zohaib@seedcode.io>
2020-09-23 14:31:33 +0500

    modified slash reward calculation

--- server/eventHandlers/slashEvents.ts
+++ server/eventHandlers/slashEvents.ts
@@ -1,5 +1,7 @@
 import { IEventHandler, CWEvent, IChainEventData, SubstrateTypes } from '@commonwealth/chain-events';
 import getLast30DaysStats  from './computeStats'
+import Sequelize from 'sequelize';
+const Op = Sequelize.Op;
 
 
 export default class extends IEventHandler {
@@ -21,38 +23,82 @@ export default class extends IEventHandler {
     const newSlashEventData = event.data;
 
     // 2) Get relevant data from DB for processing.
-    // Get last created validator's record from 'HistoricalValidatorStatistic' table. as slash event contains validator's AccountID.
-    const latestValidatorStat = await this._models.HistoricalValidatorStatistic.findOne({
+    // Check for the stash id whether it is in validator table or not.
+    let validatorStash = "";
+    let newExposure;
+
+    const validatorRecord = await this._models.Validator.findOne({
       where: {
-        stash: newSlashEventData.validator
+        [Op.and]: [{ stash: newSlashEventData.validator }, { state: 'Active' }]
+      },
+    });
+    let validator = JSON.parse(JSON.stringify(validatorRecord));
+
+    if (validator){
+      validatorStash = validator.stash;
+    } else {
+      let activeExposures = [];
+      let slashedNominators = {};
+
+      for (let valid of Object.keys(activeExposures)) {
+        activeExposures[valid].other.forEach(nominator => {
+          if (nominator.who === newSlashEventData.validator){
+            slashedNominators[valid] = activeExposures[valid];
+          }
+        })
+      }
+
+      if (Object.keys(slashedNominators).length === 1){
+        validatorStash = Object.keys(slashedNominators)[0];
+        newExposure = slashedNominators[validatorStash];
+        newExposure.total = (Number(newExposure.total) - Number(newSlashEventData.amount)).toString()
+  
+        newExposure.other.map((nominator) =>{
+          if (nominator.who === newSlashEventData.validator){
+            nominator.value = (Number(nominator.value) - Number(newSlashEventData.amount))
+            return nominator;
+          }
+        })
+
+      } else if (Object.keys(slashedNominators).length > 1){
+        // pass
+        // TODO: once finalized how we'll apply slash on the validator's exposure then modify the logic based to decided criteria.
+        // Multiple nominators presence need to be computed based on final discussion.
+      }
+    }
+
+    // Get last created validator's record from 'HistoricalValidatorStatistic' table. as slash event contains validator's AccountID.
+    const latestValidatorStats = await this._models.HistoricalValidatorStatistic.findOne({
+      where: {
+        stash: validatorStash
       },
       order: [
         ['created_at', 'DESC']
       ]
     });
-    if (!latestValidatorStat) {
-      return dbEvent;
-    }
+    let latestValidator = JSON.parse(JSON.stringify(latestValidatorStats));
 
-    let validator = JSON.parse(JSON.stringify(latestValidatorStat));
+    if ( newExposure ) {
+      latestValidator.exposure = newExposure;
+    } else {
+      latestValidator.exposure.own = (Number(latestValidator.exposure.own) - Number(newSlashEventData.amount)).toString();
+      latestValidator.exposure.total = (Number(latestValidator.exposure.total) - Number(newSlashEventData.amount)).toString();
+    }
 
     // Added Last 30 days Slash count and averages for a validator.
     const [thirtyDaysAvg, thirtyDaysCount] = await getLast30DaysStats(this._chain, newSlashEventData.kind, newSlashEventData.validator);
     validator.slashesStats = {count: thirtyDaysCount, avg: thirtyDaysAvg }
 
     // 3) Modify exposures for validators based of slash balance.
-    // TODO: once finalized how we'll apply slash on the validator's exposure then modify the logic based to decided criteria.
-    validator.exposure.own = (Number(validator.exposure.own) - Number(newSlashEventData.amount)).toString();
-    validator.exposure.total = (Number(validator.exposure.total) - Number(newSlashEventData.amount)).toString();
-    validator.block = event.blockNumber.toString();
-    validator.eventType = newSlashEventData.kind;
-    validator.apr = 0; // TODO: APR to be computed over here
-    validator.created_at = new Date().toISOString();
-    validator.updated_at = new Date().toISOString();
-    delete validator.id;
+    latestValidator.block = event.blockNumber.toString();
+    latestValidator.eventType = newSlashEventData.kind;
+    latestValidator.apr = 0; // TODO: APR to be computed over here
+    latestValidator.created_at = new Date().toISOString();
+    latestValidator.updated_at = new Date().toISOString();
+    delete latestValidator.id;
 
     // 4) create/update event data in database.
-    await this._models.HistoricalValidatorStatistic.create( validator );
+    await this._models.HistoricalValidatorStatistic.create( latestValidator );
 
     return dbEvent;
   }
