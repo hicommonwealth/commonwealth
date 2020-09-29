f2876c1ac2556d4ae874eac73216611a44023f60
zakhap <zhs2104@columbia.edu>
2020-09-29 14:28:35 -0700

    Add more migration removal of unnecessary tables that are unused.

--- server/migrations/20200929202011-remove-privacy-from-threads.js
+++ server/migrations/20200929202011-remove-privacy-from-threads.js
@@ -3,15 +3,33 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.sequelize.transaction(async (t) => {
-      return queryInterface.removeColumn(
+      await queryInterface.removeColumn(
         'OffchainThreads', 'private', { transaction: t },
       );
+      await queryInterface.dropTable('read_only_roles_threads', { transaction: t });
+      await queryInterface.dropTable('private_threads_roles', { transaction: t });
     });
   },
 
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.addColumn(
-      'OffchainThreads', 'private', { type: Sequelize.BOOLEAN, allowNull: false, defaultValue: false },
-    );
+  down: (queryInterface, DataTypes) => {
+    return queryInterface.sequelize.transaction(async (t) => {
+      await queryInterface.addColumn(
+        'OffchainThreads', 'private',
+        { type: DataTypes.BOOLEAN, allowNull: false, defaultValue: false },
+        { transaction: t },
+      );
+      await queryInterface.createTable('read_only_roles_threads', {
+        id: { type: DataTypes.INTEGER, allowNull: false },
+        thread_id: { type: DataTypes.INTEGER, allowNull: false },
+        created_at: { type: DataTypes.DATE, allowNull: false },
+        updated_at: { type: DataTypes.DATE, allowNull: false },
+      }, { transaction: t });
+      await queryInterface.createTable('private_threads_roles', {
+        id: { type: DataTypes.INTEGER, allowNull: false },
+        thread_id: { type: DataTypes.INTEGER, allowNull: false },
+        created_at: { type: DataTypes.DATE, allowNull: false },
+        updated_at: { type: DataTypes.DATE, allowNull: false },
+      }, { transaction: t });
+    });
   }
 };
