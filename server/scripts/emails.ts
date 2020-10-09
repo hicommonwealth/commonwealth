7a21f649b176a0fc6f25777914a75d4d6686d8f9
Raymond Zhong <raykyri@gmail.com>
2020-10-09 01:50:31 -0700

    Add chain event notification emails

--- server/scripts/emails.ts
+++ server/scripts/emails.ts
@@ -1,8 +1,9 @@
 import Sequelize from 'sequelize';
 import moment from 'moment';
 import {
-  SubstrateTypes, MolochTypes,
-  SubstrateEvents, MolochEvents, IEventLabel, IEventTitle, IChainEventData } from '@commonwealth/chain-events';
+  SubstrateTypes, MolochTypes, SubstrateEvents, MolochEvents,
+  IEventLabel, IEventTitle, IChainEventData, chainSupportedBy
+} from '@commonwealth/chain-events';
 
 import { SENDGRID_API_KEY, SERVER_URL } from '../config';
 import { factory, formatFilename } from '../../shared/logging';
@@ -36,7 +37,25 @@ export const createImmediateNotificationEmailObject = async (notification_data, 
         (notification_data as IChainEventNotificationData).chainEvent.event_data,
       );
     }
-    // TODO: send chain event immediate notification email
+    if (!chainEventLabel) return;
+
+    const subject = `${chainEventLabel.heading} event on ${notification_data.chainEventType?.chain}`;
+
+    return {
+      from: 'Commonwealth <no-reply@commonwealth.im>',
+      to: null,
+      bcc: null,
+      subject,
+      templateId: DynamicTemplate.ImmediateEmailNotification,
+      dynamic_template_data: {
+        notification: {
+          chainId: notification_data.chainEventType?.chain,
+          blockNumber: notification_data.chainEvent?.blockNumber,
+          label: chainEventLabel.heading,
+          path: `https://commonwealth.im${chainEventLabel.linkUrl}`,
+        }
+      }
+    };
   } else {
     if (category_id === NotificationCategories.NewReaction || category_id === NotificationCategories.ThreadEdit) return;
     const [
@@ -64,27 +83,50 @@ export const createImmediateNotificationEmailObject = async (notification_data, 
 };
 
 const createNotificationDigestEmailObject = async (user, notifications, models) => {
-  console.log(6);
   const emailObjArray = await Promise.all(notifications.map(async (n) => {
     const s = await n.getSubscription();
-    console.log(s);
-    console.log(n);
-    console.log(n.notification_data);
     const { category_id } = s;
-    const notification_data = JSON.parse(n.notification_data);
 
-    console.log(7);
-    if (notification_data.chain_event) {
-      // TODO: implement chain event
+    if (n.chain_event_id) {
+      const chainEvent = await models.ChainEvent.findOne({
+        where: { id: n.chain_event_id }
+      });
+      if (!chainEvent) return {};
+
+      let label;
+      if (chainSupportedBy(s.chain_id, SubstrateTypes.EventChains)) {
+        label = SubstrateEvents.Label(
+          chainEvent.blockNumber,
+          s.chain_id,
+          chainEvent.event_data,
+        );
+      } else if (chainSupportedBy(s.chain_id, MolochTypes.EventChains)) {
+        label = MolochEvents.Label(
+          chainEvent.blockNumber,
+          s.chain_id,
+          chainEvent.event_data,
+        );
+      }
+      if (!label) return {};
+
+      const path = `https://commonwealth.im/${s.chain_id}/notifications`;
+      let createdAt = moment(n.created_at).fromNow();
+      if (createdAt === 'a day ago') createdAt = `${moment(Date.now()).diff(n.created_at, 'hours')} hours ago`;
+      return {
+        chainId: s.chain_id,
+        blockNumber: chainEvent.blockNumber,
+        label: label.heading,
+        path: `https://commonwealth.im${label.linkUrl}`,
+        createdAt,
+      }
     } else {
-      console.log(8);
+      const notification_data = JSON.parse(n.notification_data);
       const [
         emailSubjectLine, subjectCopy, actionCopy, objectCopy, communityCopy, excerpt, path
       ] = await getForumNotificationCopy(models, notification_data as IPostNotificationData, category_id);
 
       let createdAt = moment(n.created_at).fromNow();
       if (createdAt === 'a day ago') createdAt = `${moment(Date.now()).diff(n.created_at, 'hours')} hours ago`;
-      console.log(9);
       return {
         author: subjectCopy,
         action: actionCopy,
@@ -97,7 +139,6 @@ const createNotificationDigestEmailObject = async (user, notifications, models) 
     }
   }));
 
-  console.log(10);
   // construct email
   return {
     from: 'Commonwealth <no-reply@commonwealth.im>',
@@ -149,7 +190,7 @@ export const sendBatchedNotificationEmails = async (models): Promise<number> => 
           where: { subscriber_id: user.id },
         }],
         where: {
-          is_read: false,
+          // is_read: false,
           created_at: { [Op.gt]: last24hours },
         },
         order: [
@@ -160,6 +201,7 @@ export const sendBatchedNotificationEmails = async (models): Promise<number> => 
 
       // send notification email
       try {
+        console.log(`producing digest for ${user.email}`);
         const emailObject = await createNotificationDigestEmailObject(user, notifications, models);
         emailObject.to = process.env.NODE_ENV === 'development' ? 'raymond@commonwealth.im' : user.email;
         emailObject.bcc = 'raymond+bcc@commonwealth.im';
@@ -167,8 +209,7 @@ export const sendBatchedNotificationEmails = async (models): Promise<number> => 
         console.log(`sending batch notification email to ${user.email}`);
         await sgMail.send(emailObject);
       } catch (e) {
-        console.log('Failed to send batch notification email', e?.response?.body?.errors);
-        console.log(log.error(e));
+        console.log('Failed to send batch notification email', e);
       }
     }));
     return 0;
