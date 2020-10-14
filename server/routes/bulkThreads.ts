f348a00dc2bd9340e68cbd54f14603b63b13dc56
Suspended Reason <suspendedreason@gmail.com>
2020-10-14 12:02:12 -0500

    Add cutoff switch

--- server/routes/bulkThreads.ts
+++ server/routes/bulkThreads.ts
@@ -12,52 +12,65 @@ const bulkThreads = async (models, req: Request, res: Response, next: NextFuncti
   const [chain, community] = await lookupCommunityIsVisibleToUser(models, req.query, req.user, next);
 
   // Threads
-  const whereOptions = community
-    ? `community = :community`
-    : `chain = :chain AND root_id LIKE 'discussion%'`;
+  let threads;
+  if (req.body.cutoffDate) {
+    const whereOptions = community
+      ? `community = :community`
+      : `chain = :chain AND root_id LIKE 'discussion%'`;
 
-  const replacements = community
-    ? { community: community.id }
-    : { chain: chain.id };
+    const replacements = community
+      ? { community: community.id }
+      : { chain: chain.id };
 
-  replacements['created_at'] = req.body.cutoffDate;
+    replacements['created_at'] = req.body.cutoffDate;
 
-  const query = `
-    SELECT t.id
-      FROM "OffchainThreads" AS t
-      WHERE id in (
-        SELECT CAST(TRIM('discussion_' FROM root_id) AS int)
-        FROM (
-          SELECT root_id, created_at, id
+    const query = `
+      SELECT t.id
+        FROM "OffchainThreads" AS t
+        WHERE id in (
+          SELECT CAST(TRIM('discussion_' FROM root_id) AS int)
           FROM (
-            SELECT root_id, MAX(created_at) as created_at 
-            FROM "OffchainComments" 
-            WHERE ${whereOptions}
-              AND created_at < :created_at
-              AND deleted_at IS NULL
-            GROUP BY root_id) grouped_comments
-          ORDER BY created_at DESC LIMIT 20
-        ) ordered_comments
-      );`;
+            SELECT root_id, created_at, id
+            FROM (
+              SELECT root_id, MAX(created_at) as created_at 
+              FROM "OffchainComments" 
+              WHERE ${whereOptions}
+                AND created_at < :created_at
+                AND deleted_at IS NULL
+              GROUP BY root_id) grouped_comments
+            ORDER BY created_at DESC LIMIT 20
+          ) ordered_comments
+        );`;
 
-  let threadIds;
-  try {
-    threadIds = await models.sequelize.query(query, {
-      replacements,
-      type: QueryTypes.SELECT
+    let threadIds;
+    try {
+      threadIds = await models.sequelize.query(query, {
+        replacements,
+        type: QueryTypes.SELECT
+      });
+    } catch (e) {
+      console.log(e);
+    }
+
+    threads = await models.OffchainThread.findAll({
+      where: {
+        id: {
+          [Op.in]: threadIds.map((id) => id.id)
+        }
+      },
+      include: [ models.Address, { model: models.OffchainTopic, as: 'topic' } ],
     });
-  } catch (e) {
-    console.log(e);
-  }
+  } else {
+    const allThreadsQuery = (community)
+      ? { community: community.id, }
+      : { chain: chain.id, };
 
-  const threads = await models.OffchainThread.findAll({
-    where: {
-      id: {
-        [Op.in]: threadIds.map((id) => id.id)
-      }
-    },
-    include: [ models.Address, { model: models.OffchainTopic, as: 'topic' } ],
-  });
+    threads = await models.OffchainThread.findAll({
+      where: allThreadsQuery,
+      include: [ models.Address, { model: models.OffchainTopic, as: 'topic' } ],
+      order: [['created_at', 'DESC']],
+    });
+  }
 
   const userAddresses = await req.user.getAddresses();
   const userAddressIds = Array.from(userAddresses.filter((addr) => !!addr.verified).map((addr) => addr.id));
@@ -70,7 +83,7 @@ const bulkThreads = async (models, req: Request, res: Response, next: NextFuncti
 
   const adminRoles = roles.filter((r) => r.permission === 'admin' || r.permission === 'moderator');
 
-  const filteredThreads = await allThreads.filter((thread) => {
+  const filteredThreads = await threads.filter((thread) => {
     if (userAddressIds.includes(thread.address_id)) {
       return true;
     } else if (adminRoles.length > 0) {
