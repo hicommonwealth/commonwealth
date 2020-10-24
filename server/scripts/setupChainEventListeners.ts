8ca4969bde964a49dba923bff989b30f0fce16ec
Syed Hussain Mehdi <mehdi@seedcode.io>
2020-10-24 15:14:37 +0500

    Disabled new eventListeners setupChainEventListeners.ts

--- server/scripts/setupChainEventListeners.ts
+++ server/scripts/setupChainEventListeners.ts
@@ -10,13 +10,13 @@ import EventStorageHandler from '../eventHandlers/storage';
 import EventNotificationHandler from '../eventHandlers/notifications';
 import EntityArchivalHandler from '../eventHandlers/entityArchival';
 import IdentityHandler from '../eventHandlers/identity';
-import NewSessionHandler from '../eventHandlers/newSessionEvents';
-import RewardHandler from '../eventHandlers/rewardEvents';
-import SlashHandler from '../eventHandlers/slashEvents';
+// import NewSessionHandler from '../eventHandlers/newSessionEvents';
+// import RewardHandler from '../eventHandlers/rewardEvents';
+// import SlashHandler from '../eventHandlers/slashEvents';
 import BondHandler from '../eventHandlers/bondEvents';
-import ImOnlineHandler from '../eventHandlers/imOnlineEvents';
-import OffenceHandler from '../eventHandlers/offenceEvents';
-import HeartbeatHandler from '../eventHandlers/heartbeatEvents';
+// import ImOnlineHandler from '../eventHandlers/imOnlineEvents';
+// import OffenceHandler from '../eventHandlers/offenceEvents';
+// import HeartbeatHandler from '../eventHandlers/heartbeatEvents';
 import { sequelize } from '../database';
 import { constructSubstrateUrl } from '../../shared/substrate';
 import { factory, formatFilename } from '../../shared/logging';
@@ -78,24 +78,24 @@ const setupChainEventListeners = async (
     const storageHandler = new EventStorageHandler(models, node.chain);
     const notificationHandler = new EventNotificationHandler(models, wss);
     const entityArchivalHandler = new EntityArchivalHandler(models, node.chain, wss);
-    const newSessionHandler = new NewSessionHandler(models);
-    const rewardHandler = new RewardHandler(models, node.chain);
-    const slashHandler = new SlashHandler(models, node.chain);
+    // const newSessionHandler = new NewSessionHandler(models);
+    // const rewardHandler = new RewardHandler(models, node.chain);
+    // const slashHandler = new SlashHandler(models, node.chain);
     const bondHandler = new BondHandler(models);
-    const imOnlineHandler = new ImOnlineHandler(models);
-    const offenceHandler = new OffenceHandler(models, node.chain);
-    const heartbeatHandler = new HeartbeatHandler(models, node.chain);
+    // const imOnlineHandler = new ImOnlineHandler(models);
+    // const offenceHandler = new OffenceHandler(models, node.chain);
+    // const heartbeatHandler = new HeartbeatHandler(models, node.chain);
     const identityHandler = new IdentityHandler(models, node.chain);
-    const handlers: IEventHandler[] = [ storageHandler, 
+    const handlers: IEventHandler[] = [ storageHandler,
       notificationHandler,
       entityArchivalHandler,
-      newSessionHandler,
-      rewardHandler,
-      slashHandler,
+      // newSessionHandler,
+      // rewardHandler,
+      // slashHandler,
       bondHandler,
-      imOnlineHandler,
-      offenceHandler,
-      heartbeatHandler
+      // imOnlineHandler,
+      // offenceHandler,
+      // heartbeatHandler
     ];
     let subscriber: IEventSubscriber<any, any>;
     if (SubstrateTypes.EventChains.includes(node.chain)) {
