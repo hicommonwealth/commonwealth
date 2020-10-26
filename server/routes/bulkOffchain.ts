ef26a0d71526729c548523fc8dc7314b370314d7
Suspended Reason <suspendedreason@gmail.com>
2020-10-26 13:38:34 -0500

    Ensure posts without topics or comments are fetched in bulk calls

--- server/routes/bulkOffchain.ts
+++ server/routes/bulkOffchain.ts
@@ -61,13 +61,13 @@ const bulkOffchain = async (models, req: Request, res: Response, next: NextFunct
       topics.description AS topic_description, topics.chain_id AS topic_chain,
       topics.community_id AS topic_community
     FROM "Addresses" AS addr
-    INNER JOIN (
+    RIGHT JOIN (
       SELECT t.id AS thread_id, t.title AS thread_title, t.address_id,
         t.created_at AS thread_created, t.community AS thread_community,
         t.chain AS thread_chain, t.version_history, t.read_only, t.body,
         t.url, t.pinned, t.topic_id, t.kind
       FROM "OffchainThreads" t
-      INNER JOIN (
+      LEFT JOIN (
         SELECT root_id, MAX(created_at) AS comm_created_at
         FROM "OffchainComments"
         WHERE ${whereOptions}
@@ -76,10 +76,10 @@ const bulkOffchain = async (models, req: Request, res: Response, next: NextFunct
       ON CAST(TRIM('discussion_' FROM c.root_id) AS int) = t.id
       WHERE t.deleted_at IS NULL
       AND t.pinned = false
-      ORDER BY c.comm_created_at DESC LIMIT ${20 - pinnedThreads.length}
+      ORDER BY COALESCE(c.comm_created_at, t.created_at) DESC LIMIT ${20 - pinnedThreads.length}
     ) threads
     ON threads.address_id = addr.id
-    INNER JOIN "OffchainTopics" topics
+    LEFT JOIN "OffchainTopics" topics
     ON threads.topic_id = topics.id`;
 
   const preprocessedThreads = await models.sequelize.query(query, {
--- server/routes/bulkThreads.ts
+++ server/routes/bulkThreads.ts
@@ -38,13 +38,13 @@ const bulkThreads = async (models, req: Request, res: Response, next: NextFuncti
         topics.description AS topic_description, topics.chain_id AS topic_chain,
         topics.community_id AS topic_community
       FROM "Addresses" AS addr
-      INNER JOIN (
+      RIGHT JOIN (
         SELECT t.id AS thread_id, t.title AS thread_title, t.address_id,
           t.created_at AS thread_created, t.community AS thread_community,
           t.chain AS thread_chain, t.version_history, t.read_only, t.body,
           t.url, t.pinned, t.topic_id, t.kind
         FROM "OffchainThreads" t
-        INNER JOIN (
+        LEFT JOIN (
           SELECT root_id, MAX(created_at) AS comm_created_at
           FROM "OffchainComments"
           WHERE ${commentOptions}
@@ -56,10 +56,10 @@ const bulkThreads = async (models, req: Request, res: Response, next: NextFuncti
         WHERE t.deleted_at IS NULL
           ${threadOptions}
           AND t.pinned = false
-        ORDER BY c.comm_created_at DESC LIMIT 20
-      ) threads
+          ORDER BY COALESCE(c.comm_created_at, t.created_at) DESC LIMIT 20
+        ) threads
       ON threads.address_id = addr.id
-      INNER JOIN "OffchainTopics" topics
+      LEFT JOIN "OffchainTopics" topics
       ON threads.topic_id = topics.id`;
 
     let preprocessedThreads;
