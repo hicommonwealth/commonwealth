77e1b0c2bd96caeda3f1e9cf6e3f79a2e3e3f93f
zakhap <zhs2104@columbia.edu>
2020-10-27 12:34:06 -0700

    Make governance controller compile.

--- client/scripts/controllers/chain/ethereum/marlin/governance.ts
+++ client/scripts/controllers/chain/ethereum/marlin/governance.ts
@@ -1,71 +1,71 @@
-// import BN from 'bn.js';
-// import EthDater from 'ethereum-block-by-date';
+import BN from 'bn.js';
+import EthDater from 'ethereum-block-by-date';
 
-// import { ProposalModule, ITXModalData, ChainEntity, IChainModule } from 'models';
+import { ProposalModule, ITXModalData, ChainEntity, IChainModule } from 'models';
 
-// import { ERC20Token, EthereumCoin } from 'adapters/chain/ethereum/types';
-// import { IMarlinProposalResponse } from 'adapters/chain/marlin/types';
-// import { EntityRefreshOption } from 'controllers/server/chain_entities';
+import { ERC20Token, EthereumCoin } from 'adapters/chain/ethereum/types';
+import { IMarlinProposalResponse } from 'adapters/chain/marlin/types';
+import { EntityRefreshOption } from 'controllers/server/chain_entities';
 
-// import { MolochEvents } from '@commonwealth/chain-events';
+import { MolochEvents } from '@commonwealth/chain-events';
 
-// import { IApp } from 'state';
-
-
-// import Marlin from './adapter';
-// import MarlinAPI from './api';
-// import MarlinProposal from './proposal';
-// // import MarlinHolder from './holders';
-
-// export default class MarlinGovernance extends ProposalModule<
-// MarlinAPI,
-// IMarlinProposalResponse,
-// MarlinProposal
-// > {
-// //   // MEMBERS
-// //   private _proposalCount: BN;
-// //   private _votingPeriodLength: BN;
-// //   private _periodDuration: BN;
-// //   private _minimumThreshold: BN;
-// //   private _totalSupply: BN;
-
-// //   private _api: MarlinAPI;
-// //   // private _Holders: MarlinHolders;
+import { IApp } from 'state';
 
 
-// //   // GETTERS
-// //   public get proposalCount() { return this._proposalCount; }
-// //   public get votingPeriodLength() { return this._votingPeriodLength; }
-// //   public get periodDuration() { return this._periodDuration; }
-// //   public get minimumThreshold() { return this._minimumThreshold; }
-// //   public get totalSupply() { return this._totalSupply; }
+import Marlin from './adapter';
+import MarlinAPI from './api';
+import MarlinProposal from './proposal';
+// import MarlinHolder from './holders';
 
-// //   public get api() { return this._api; }
-// //   public get usingServerChainEntities() { return this._usingServerChainEntities; }
+export default class MarlinGovernance extends ProposalModule<
+MarlinAPI,
+IMarlinProposalResponse,
+MarlinProposal
+> {
+//   // MEMBERS
+//   private _proposalCount: BN;
+//   private _votingPeriodLength: BN;
+//   private _periodDuration: BN;
+//   private _minimumThreshold: BN;
+//   private _totalSupply: BN;
+
+//   private _api: MarlinAPI;
+//   // private _Holders: MarlinHolders;
 
 
-// //   // INIT / DEINIT
-//   constructor(app: IApp, private _usingServerChainEntities = false) {
-//     super(app
-//       // , (e) => new MarlinProposal(this._Holders, this, e)
-//     );
+//   // GETTERS
+//   public get proposalCount() { return this._proposalCount; }
+//   public get votingPeriodLength() { return this._votingPeriodLength; }
+//   public get periodDuration() { return this._periodDuration; }
+//   public get minimumThreshold() { return this._minimumThreshold; }
+//   public get totalSupply() { return this._totalSupply; }
+
+//   public get api() { return this._api; }
+//   public get usingServerChainEntities() { return this._usingServerChainEntities; }
+
+
+//   // INIT / DEINIT
+  constructor(app: IApp, private _usingServerChainEntities = false) {
+    super(app
+      // , (e) => new MarlinProposal(this._Holders, this, e)
+    );
+  }
+
+  public async init() {
+
+    // this._totalSharesRequested = new BN((await this._api.Contract.totalSharesRequested()).toString(), 10);
+    // this._totalShares = new BN((await this._api.Contract.totalShares()).toString(), 10);
+    // this._gracePeriod = new BN((await this._api.Contract.gracePeriodLength()).toString(), 10);
+    // this._abortWindow = new BN((await this._api.Contract.abortWindow()).toString(), 10);
+  }
+
+//   public deinit() {
+//     this.store.clear();
 //   }
 
-//   public async init() {
-
-//     // this._totalSharesRequested = new BN((await this._api.Contract.totalSharesRequested()).toString(), 10);
-//     // this._totalShares = new BN((await this._api.Contract.totalShares()).toString(), 10);
-//     // this._gracePeriod = new BN((await this._api.Contract.gracePeriodLength()).toString(), 10);
-//     // this._abortWindow = new BN((await this._api.Contract.abortWindow()).toString(), 10);
-//   }
-
-// //   public deinit() {
-// //     this.store.clear();
-// //   }
-
-//   public createTx(...args: any[]): ITXModalData {
-//     throw new Error('Method not implemented.');
-//   }
+  public createTx(...args: any[]): ITXModalData {
+    throw new Error('Method not implemented.');
+  }
 
 
-// }
+}
