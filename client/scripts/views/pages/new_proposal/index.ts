4574125a1c7da36ff7611ce1d58400a074257454
zakhap <zhs2104@columbia.edu>
2020-11-05 16:53:50 -0800

    Add marlin as acceptable type for newproposal page

--- client/scripts/views/pages/new_proposal/index.ts
+++ client/scripts/views/pages/new_proposal/index.ts
@@ -7,14 +7,14 @@ import app from 'state';
 import Sublayout from 'views/sublayout';
 import PageLoading from 'views/pages/loading';
 import { ProposalType, proposalSlugToClass, proposalSlugToFriendlyName } from 'identifiers';
-import { ChainBase, ProposalModule } from 'models';
+import { ChainBase, ChainNetwork, ProposalModule } from 'models';
 import NewProposalForm from 'views/pages/new_proposal/new_proposal_form';
 
 async function loadCmd(type: string) {
   if (!app || !app.chain || !app.chain.loaded) {
     throw new Error('secondary loading cmd called before chain load');
   }
-  if (app.chain.base !== ChainBase.Substrate) {
+  if (app.chain.base !== ChainBase.Substrate || app.chain.network !== ChainNetwork.Marlin) {
     return;
   }
   const c = proposalSlugToClass().get(type);
@@ -35,6 +35,7 @@ const NewProposalPage: m.Component<{ type }, { typeEnum, titlePre }> = {
 
       // check if module is still initializing
       const c = proposalSlugToClass().get(vnode.state.typeEnum) as ProposalModule<any, any, any>;
+      console.log('proposal module', c);
       if (!c.disabled && !c.initialized) {
         if (!c.initializing) loadCmd(vnode.state.typeEnum);
         return m(PageLoading, { narrow: true, showNewProposalButton: true });
