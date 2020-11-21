8fa5314c9b901fb5e9d3d0afce16298d8fcb9805
Raymond Zhong <raykyri@gmail.com>
2020-11-20 16:19:02 -0800

    only collapse notification count on mobile

--- client/scripts/views/components/header/notifications_menu.ts
+++ client/scripts/views/components/header/notifications_menu.ts
@@ -80,7 +80,10 @@ const NotificationsMenu: m.Component<{ small?: boolean }> = {
         class: `NotificationsMenuButton ${unreadNotifications > 0 ? 'has-notifications' : 'no-notifications'}`,
         label: [
           m(Icon, { name: Icons.BELL }),
-          m('.notification-count', unreadNotifications > 9 ? '∞' : unreadNotifications),
+          m('.notification-count', [
+            m('span.hidden-xs', unreadNotifications),
+            m('span.visible-xs', unreadNotifications > 9 ? '∞' : unreadNotifications),
+          ]),
         ],
         size: small ? 'sm' : 'default',
         compact: true,
