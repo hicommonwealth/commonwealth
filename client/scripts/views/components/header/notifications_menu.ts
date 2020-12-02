187a3a6eebc8ce4b67a52b5677add833e32ad067
Raymond Zhong <raykyri@gmail.com>
2020-12-02 13:05:08 -0800

    Split notifications into discussion notifs and chain notifs

--- client/scripts/views/components/header/notifications_menu.ts
+++ client/scripts/views/components/header/notifications_menu.ts
@@ -63,26 +63,37 @@ const NotificationButtons: m.Component<{}> = {
   }
 };
 
-const NotificationsMenu: m.Component<{ small?: boolean }> = {
+const NotificationsMenu: m.Component<{ small?: boolean }, { selectedChainEvents: boolean }> = {
   view: (vnode) => {
     // TODO: Add helper directly on controller
     const { small } = vnode.attrs;
-    const notifications = app.user.notifications
-      ? app.user.notifications.notifications.sort((a, b) => b.createdAt.unix() - a.createdAt.unix())
-      : [];
-    const sortedNotifications = sortNotifications(notifications).reverse();
-    const unreadNotifications = sortedNotifications.filter((n) => !n[0].isRead).length;
+    const notifications = app.user.notifications ? app.user.notifications.notifications : [];
+    const filteredNotifications = vnode.state.selectedChainEvents
+      ? notifications.filter((n) => n.chainEvent)
+      : notifications.filter((n) => !n.chainEvent);
+    const sortedFilteredNotifications = sortNotifications(filteredNotifications).reverse();
+
+    const unreadNotifications = notifications.filter((n) => !n.isRead);
+    const unreadNotificationsCount = unreadNotifications.length;
+    const unreadFilteredNotificationsCount = filteredNotifications.filter((n) => !n.isRead).length;
+    const chainNotificationsCount = vnode.state.selectedChainEvents
+      ? unreadFilteredNotificationsCount
+      : unreadNotificationsCount - unreadFilteredNotificationsCount;
+    const discussionNotificationsCount = vnode.state.selectedChainEvents
+      ? unreadNotificationsCount - unreadFilteredNotificationsCount
+      : unreadFilteredNotificationsCount;
+
     return m(PopoverMenu, {
       hasArrow: false,
       transitionDuration: 0,
       hoverCloseDelay: 0,
       trigger: m(Button, {
-        class: `NotificationsMenuButton ${unreadNotifications > 0 ? 'has-notifications' : 'no-notifications'}`,
+        class: `NotificationsMenuButton ${unreadNotificationsCount > 0 ? 'has-notifications' : 'no-notifications'}`,
         label: [
           m(Icon, { name: Icons.BELL }),
           m('.notification-count', [
-            m('span.hidden-xs', unreadNotifications),
-            m('span.visible-xs', unreadNotifications > 9 ? '∞' : unreadNotifications),
+            m('span.hidden-xs', unreadNotificationsCount),
+            m('span.visible-xs', unreadNotificationsCount > 9 ? '∞' : unreadNotificationsCount),
           ]),
         ],
         size: small ? 'sm' : 'default',
@@ -97,17 +108,43 @@ const NotificationsMenu: m.Component<{ small?: boolean }> = {
       },
       class: 'NotificationsMenu',
       content: [
+        m(ButtonGroup, {
+          class: 'NotificationsTypeSelectorButtons',
+          fluid: true,
+          basic: true,
+        }, [
+          m(Button, {
+            label: discussionNotificationsCount ? `Discussions (${discussionNotificationsCount})` : 'Discussions',
+            active: !vnode.state.selectedChainEvents,
+            onclick: (e) => {
+              e.preventDefault();
+              e.stopPropagation();
+              vnode.state.selectedChainEvents = false;
+            }
+          }),
+          m(Button, {
+            label: chainNotificationsCount ? `Chain events (${chainNotificationsCount})` : 'Chain events',
+            active: !!vnode.state.selectedChainEvents,
+            onclick: (e) => {
+              e.preventDefault();
+              e.stopPropagation();
+              vnode.state.selectedChainEvents = true;
+            }
+          }),
+        ]),
         m('.notification-list', [
-          notifications.length > 0
+          sortedFilteredNotifications.length > 0
             ? m(Infinite, {
               maxPages: 1, // prevents rollover/repeat
-              pageData: () => sortedNotifications.slice(0, MAX_NOTIFS), // limit the number of rows shown here
-              key: notifications.length,
+              pageData: () => sortedFilteredNotifications.slice(0, MAX_NOTIFS), // limit the number of rows shown here
+              key: (vnode.state.selectedChainEvents ? 'chain-' : 'discussion-') + sortedFilteredNotifications.length,
               item: (data, opts, index) => {
                 return m(NotificationRow, { notifications: data });
               },
             })
-            : m('li.no-notifications', 'No Notifications'),
+            : m('li.no-notifications', [
+              vnode.state.selectedChainEvents ? 'No chain notifications' : 'No discussion notifications'
+            ]),
         ]),
         m(NotificationButtons),
       ]
