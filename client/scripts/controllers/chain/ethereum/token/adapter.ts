20ec6f017a6e416318219efe376f83e5909ba454
zakhap <zhs2104@columbia.edu>
2020-12-06 14:46:25 -0800

    Create Token controller dir, first pass at adapter.ts

---
+++ client/scripts/controllers/chain/ethereum/token/adapter.ts
@@ -0,0 +1,57 @@
+import { EthereumCoin } from 'adapters/chain/ethereum/types';
+
+import EthWebWalletController from 'controllers/app/eth_web_wallet';
+import EthereumAccount from 'controllers/chain/ethereum/account';
+import EthereumAccounts from 'controllers/chain/ethereum/accounts';
+import { ChainBase, ChainClass, IChainAdapter, NodeInfo } from 'models';
+
+import { setActiveAccount } from 'controllers/app/login';
+import ChainEntityController from 'controllers/server/chain_entities';
+import { IApp } from 'state';
+
+import EthereumChain from '../chain';
+
+export default class Token extends IChainAdapter<EthereumCoin, EthereumAccount> {
+  public readonly base = ChainBase.Ethereum;
+  public readonly class;
+  public readonly contractAddress;
+
+  public chain: EthereumChain;
+  public accounts: EthereumAccounts;
+
+  public readonly webWallet: EthWebWalletController = new EthWebWalletController();
+  public readonly chainEntities = new ChainEntityController();
+
+  constructor(meta: NodeInfo, app: IApp) {
+    super(meta, app);
+    this.chain = new EthereumChain(this.app);
+    this.accounts = new EthereumAccounts(this.app);
+    this.class = meta.chain.network;
+    this.contractAddress = meta.address;
+  }
+
+  public async initApi() {
+    await this.chain.resetApi(this.meta);
+    await this.chain.initMetadata();
+    await this.accounts.init(this.chain);
+
+    if (this.webWallet) {
+      await this.webWallet.enable();
+      await this.webWallet.web3.givenProvider.on('accountsChanged', async (accounts) => {
+        const updatedAddress = this.app.user.activeAccounts.find((addr) => addr.address === accounts[0]);
+        await setActiveAccount(updatedAddress);
+      });
+    }
+
+    await this.chain.initEventLoop();
+    await super.initApi();
+  }
+
+  public async deinit() {
+    await super.deinit();
+    this.accounts.deinit();
+    this.chain.deinitMetadata();
+    this.chain.deinitEventLoop();
+    this.chain.deinitApi();
+  }
+}
