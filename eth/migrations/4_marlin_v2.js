4fead6f5b42b95eab0ca785662c3912f10b894cd
zakhap <zhs2104@columbia.edu>
2020-12-08 16:14:06 -0800

    Fix truffle migration, contracts launch successfully

--- eth/migrations/4_marlin_v2.js
+++ eth/migrations/4_marlin_v2.js
@@ -7,7 +7,7 @@ module.exports = async function (deployer, network, accounts) {
   // Marlin Contracts
   // accounts[0] is initial comp holder, admin of timelock, and guardian of GovernorAlpha
   const compGaurdian = accounts[0]
-  await  deployer.deploy(MPond, compGaurdian);
+  await  deployer.deploy(MPond, compGaurdian, accounts[1]);
   const mpond = await MPond.deployed();
   await  deployer.deploy(Timelock, compGaurdian, 172800); // 172800 is 2 days in seconds, which is the minimum delay for the contract
   const timelock = await Timelock.deployed();
