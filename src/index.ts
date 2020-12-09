dc0fba5316d2069a0f23ed89c498a9c34b5a48c1
zakhap <zhs2104@columbia.edu>
2020-12-09 13:03:53 -0800

    Export marlin via marlin/index.ts, and import into src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 export * from './interfaces';
 export * as MolochEvents from './moloch/index';
 export * as MolochTypes from './moloch/types';
+export * as MarlinEvents from './marlin/index';
+export * as MarlinTypes from './marlin/types';
 export * as SubstrateEvents from './substrate/index';
 export * as SubstrateTypes from './substrate/types';
