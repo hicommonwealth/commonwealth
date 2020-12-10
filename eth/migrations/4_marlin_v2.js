e712f9b0ff22a0950f21db7961c379fd37b26717
zakhap <zhs2104@columbia.edu>
2020-12-10 15:25:57 -0800

    compGuardian to mpondGuardian

--- eth/migrations/4_marlin_v2.js
+++ eth/migrations/4_marlin_v2.js
@@ -5,12 +5,12 @@ const Timelock = artifacts.require('Timelock');
 // eslint-disable-next-line func-names
 module.exports = async function (deployer, network, accounts) {
   // Marlin Contracts
-  // accounts[0] is initial comp holder, admin of timelock, and guardian of GovernorAlpha
-  const compGaurdian = accounts[0]
-  await  deployer.deploy(MPond, compGaurdian, accounts[1]);
+  // accounts[0] is initial mpondGuardian, admin of timelock, and guardian of GovernorAlpha
+  const mpondGuardian = accounts[0]
+  await  deployer.deploy(MPond, mpondGuardian, accounts[1]);
   const mpond = await MPond.deployed();
-  await  deployer.deploy(Timelock, compGaurdian, 172800); // 172800 is 2 days in seconds, which is the minimum delay for the contract
+  await  deployer.deploy(Timelock, mpondGuardian, 172800); // 172800 is 2 days in seconds, which is the minimum delay for the contract
   const timelock = await Timelock.deployed();
-  await deployer.deploy(GovernorAlpha, timelock.address, mpond.address, compGaurdian);
+  await deployer.deploy(GovernorAlpha, timelock.address, mpond.address, mpondGuardian);
   const governorAlpha = GovernorAlpha.deployed();
 };
