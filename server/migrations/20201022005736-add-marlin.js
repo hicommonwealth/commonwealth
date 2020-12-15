632d2119224b77d7c53dd4c3bbd0975deb3adff5
zakhap <zhs2104@columbia.edu>
2020-12-14 16:38:07 -0800

    Update marlin migration to pull constants from @commonwealth/chain-events rather than hardcoded in the migraiton itself

--- server/migrations/20201022005736-add-marlin.js
+++ server/migrations/20201022005736-add-marlin.js
@@ -1,31 +1,8 @@
 'use strict';
+import { MarlinTypes } from '@commonwealth/chain-events';
+
 const SequelizeLib = require('sequelize');
 const Op = SequelizeLib.Op;
-
-// TODO: if we can use typescript in migrations, we can simply get these
-//   from the shared/events/edgeware/types file.
-
-const MarlinEventKinds = {
-  // MPond Events
-  Approval: 'approval',
-  DelegateChanged: 'delegate-changed',
-  DelegateVotesChanged: 'delegate-votes-changed',
-  Transfer: 'transfer',
-  // GovernorAlpha Events
-  ProposalExecuted: 'proposal-executed',
-  ProposalCreated: 'proposal-created',
-  ProposalCanceled: 'proposal-canceled',
-  ProposalQueued: 'proposal-queued',
-  VoteCast: 'vote-cast',
-  // Timelock Events
-  CancelTransaction: 'cancel-transaction',
-  ExecuteTransaction: 'execute-transactions',
-  NewAdmin: 'new-admin',
-  NewDelay: 'new-delay',
-  NewPendingAdmin: 'new-pending-admin',
-  QueueTransaction: 'queue-transaction',
-};
-
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
@@ -51,7 +28,7 @@ module.exports = {
         chain,
         event_name,
       });
-      const marlinObjs = Object.values(MarlinEventKinds).map((s) => buildObject(s, 'marlin'));
+      const marlinObjs = Object.values(MarlinTypes.EventKinds).map((s) => buildObject(s, 'marlin'));
 
       // TODO: somehow switch this on for testing purposes?
       return queryInterface.bulkInsert(
