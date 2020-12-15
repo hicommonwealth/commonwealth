0278aa68c52496a140f3561d3eb18e3ff592b907
zakhap <zhs2104@columbia.edu>
2020-12-15 00:00:23 -0800

    Add testnet migration

--- server/migrations/20201022005736-add-marlin.js
+++ server/migrations/20201022005736-add-marlin.js
@@ -1,8 +1,28 @@
 'use strict';
-import { MarlinTypes } from '@commonwealth/chain-events';
 
 const SequelizeLib = require('sequelize');
 const Op = SequelizeLib.Op;
+
+const MarlinEventKinds = {
+  // MPond Events
+  Approval: 'approval',
+  DelegateChanged: 'delegate-changed',
+  DelegateVotesChanged: 'delegate-votes-changed',
+  Transfer: 'transfer',
+  // GovernorAlpha Events
+  ProposalExecuted: 'proposal-executed',
+  ProposalCreated: 'proposal-created',
+  ProposalCanceled: 'proposal-canceled',
+  ProposalQueued: 'proposal-queued',
+  VoteCast: 'vote-cast',
+  // Timelock Events
+  CancelTransaction: 'cancel-transaction',
+  ExecuteTransaction: 'execute-transactions',
+  NewAdmin: 'new-admin',
+  NewDelay: 'new-delay',
+  NewPendingAdmin: 'new-pending-admin',
+  QueueTransaction: 'queue-transaction',
+};
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
@@ -10,25 +30,40 @@ module.exports = {
       await queryInterface.bulkInsert('Chains', [{
         id: 'marlin',
         symbol: 'LIN',
-        name: 'Marlin',
+        name: 'Marlin (Mainnet)',
         icon_url: '/static/img/protocols/lin.png',
         type: 'dao',
         network: 'marlin',
         active: true,
-      }], { transaction: t });
+      }, {
+        id: 'marlin-testnet',
+        symbol: 'TESTLIN',
+        name: 'Marlin (Ropsten)',
+        icon_url: '/static/img/protocols/lin.png',
+        type: 'dao',
+        network: 'marlin-testnet',
+        active: true,
+      }
+      ], { transaction: t });
 
       await queryInterface.bulkInsert('ChainNodes', [{
         chain: 'marlin',
+        url: 'wss://mainnet.infura.io/ws/v3/90de850aff68424ab8e7321017406586',
+        address: '0xEa2923b099b4B588FdFAD47201d747e3b9599A5f', // POND Contract Address
+      },
+      {
+        chain: 'marlin-testnet',
         url: 'wss://ropsten.infura.io/ws/v3/90de850aff68424ab8e7321017406586',
         address: '0xEa2923b099b4B588FdFAD47201d747e3b9599A5f', // POND Contract Address
-      }], { transaction: t });
+      }
+      ], { transaction: t });
 
       const buildObject = (event_name, chain) => ({
         id: `${chain}-${event_name}`,
         chain,
         event_name,
       });
-      const marlinObjs = Object.values(MarlinTypes.EventKinds).map((s) => buildObject(s, 'marlin'));
+      const marlinObjs = Object.values(MarlinEventKinds).map((s) => buildObject(s, 'marlin'));
 
       // TODO: somehow switch this on for testing purposes?
       return queryInterface.bulkInsert(
@@ -50,6 +85,14 @@ module.exports = {
       await queryInterface.bulkDelete('ChainEventTypes', { chain: 'marlin' }, { transaction: t });
       await queryInterface.bulkDelete('ChainNodes', { chain: 'marlin' }, { transaction: t });
       await queryInterface.bulkDelete('Chains', { id: ['marlin'] }, { transaction: t });
+
+      await queryInterface.bulkDelete('OffchainReactions', { chain: 'marlin-testnet' }, { transaction: t });
+      await queryInterface.bulkDelete('OffchainComments', { chain: 'marlin-testnet' }, { transaction: t });
+      await queryInterface.bulkDelete('OffchainThreads', { chain: 'marlin-testnet' }, { transaction: t });
+      await queryInterface.bulkDelete('Addresses', { chain: 'marlin-testnet' }, { transaction: t });
+      await queryInterface.bulkDelete('ChainEventTypes', { chain: 'marlin-testnet' }, { transaction: t });
+      await queryInterface.bulkDelete('ChainNodes', { chain: 'marlin-testnet' }, { transaction: t });
+      await queryInterface.bulkDelete('Chains', { id: ['marlin-testnet'] }, { transaction: t });
     });
   }
 };
