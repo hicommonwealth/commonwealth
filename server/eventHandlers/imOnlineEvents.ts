28ead48f6359996a3ab32aa443959c4d8d661f7a
Zohaib Khan <zohaib@seedcode.io>
2020-12-24 16:09:18 +0500

    modified event handlers according to enrichers

--- server/eventHandlers/imOnlineEvents.ts
+++ server/eventHandlers/imOnlineEvents.ts
@@ -1,5 +1,7 @@
 import { IEventHandler, CWEvent, IChainEventData, SubstrateTypes } from '@commonwealth/chain-events';
 import { sequelize } from '../database';
+import Sequelize from 'sequelize';
+const Op = Sequelize.Op;
 
 
 const uptimePercent = (noOfTrues: number, noOfFalse: number, currentEventType: number) => {
@@ -23,17 +25,26 @@ export default class extends IEventHandler {
 
   /**
     Event handler to store ImOnline information of validators details in DB.
-    Sample Payload:
+    Sample Payload - all-good :
                     {
                       data: {
-                        kind: 'all-good'/'some-offline',
-                        sessionIndex: Number,
+                        kind: 'all-good',
                         validators: [
                           'nYST5VF8q99P8P2xVUBH829YxfHUSUXN9cuCLNmyPuTbo74',
                           ...
                         ]
                       },
                       blockNumber: 800
+                    }
+                    
+    Sample Payload - some-offline:
+                    {
+                      "kind": "some-offline",
+                      "validators": [
+                        "[\"mv8WxQF82gB65Ug4RTzF7g8g854fHrGQj6FetuoX2FUTyKf\",{\"total\":\"0x00000000000e3ac49ae4a139b7cfa6de\",\"own\":\"0x00000000000100c2d4f9deef529358f4\",\"others\":[{\"who\":\"nn1kbW7MAdKLYSNCNDUSmZ2aAxcsqAXii4kbUK5TEqP2CSd\",\"value\":\"0x00000000000060b5f1fc8d42339ea968\"},{\"who\":\"kmX4JrFRdUepcqh4WKERBMQAbT6mS8UPgQpQAfmvJ9134KU\",\"value\":\"0x00000000000060b5f1eac9c8f238033b\"},{\"who\":\"jnE6pYL9RDfFAohjDasMUTfn9fFc9aMa1tAxp3wYL6QL1eS\",\"value\":\"0x00000000000000002d65f6bd5e7033eb\"},{\"who\":\"ivVni3ZZxWV6p4rYpiZidb5iZp5Kti5jHozg5jZSyrn8Q2n\",\"value\":\"0x00000000000000f34f056bdde5ee73d0\"},{\"who\":\"jYueRWWePWx63D75ShmoV6PWU5UnxMFX1vLM5NRahYwUBgU\",\"value\":\"0x00000000000427e0cc8802db46ed2f84\"},{\"who\":\"iHNihG3VgrQHd4hHtUH9EfwQQmhpmFXVRw9bzKFiZokL3D3\",\"value\":\"0x00000000000427e0cc8802db46ed2f84\"},{\"who\":\"jboHgKhnqJbU2MowSpzYCCzima3rHBomwh2y88Tn7kC37B3\",\"value\":\"0x00000000000427e0cc8802ed6d2c9a84\"}]}]",
+                        ...
+                      ],
+                      "sessionIndex": 31392
                     }
   */
   public async handle(event: CWEvent < IChainEventData >, dbEvent) {
@@ -42,7 +53,24 @@ export default class extends IEventHandler {
       && event.data.kind !== SubstrateTypes.EventKind.SomeOffline) {
       return dbEvent;
     }
+
     const imOnlineEventData = event.data;
+    let eventValidatorsList: string | any[];
+    if (event.data.kind === SubstrateTypes.EventKind.SomeOffline) {
+      eventValidatorsList = imOnlineEventData.validators?.map(validator=>JSON.parse(validator)[0]);
+    }else{
+      eventValidatorsList = imOnlineEventData.validators;
+    }
+
+    // Get all the records for all validators from database those are in new-session's active and waiting list.
+    const existingValidators = await this._models.Validator.findAll({
+      where: {
+        stash: {
+          [Op.in]: eventValidatorsList
+        }
+      }
+    });
+    if (!existingValidators || existingValidators.length === 0) return dbEvent;
 
     // 2) Get relevant data from DB for processing.
     /*
@@ -59,7 +87,7 @@ export default class extends IEventHandler {
           FROM public."HistoricalValidatorStatistic" as groupTable GROUP by groupTable.stash
           ) joinTable
         ON joinTable.stash = partitionTable.stash
-        WHERE partitionTable.stash IN ('${imOnlineEventData.validators.join("','")}')
+        WHERE partitionTable.stash IN ('${eventValidatorsList.join("','")}')
         )  as validatorQuery
       WHERE  validatorQuery.row_number = 1
     `;
