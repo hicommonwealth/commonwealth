11b866b21201c40dc6fb62eba3dfca94f8b8cfcc
Raymond Zhong <raykyri@gmail.com>
2020-12-27 21:37:12 -0800

    Add keplr web wallet module

---
+++ client/scripts/controllers/app/keplr_web_wallet.ts
@@ -0,0 +1,43 @@
+import { SigningCosmosClient } from '@cosmjs/launchpad';
+import app from 'state';
+
+declare let window: any;
+
+class KeplrWebWalletController {
+  // GETTERS/SETTERS
+  private _offlineSigner: any;
+  private _accounts: any[]; // Todo Typecasting...
+  private _enabled: boolean;
+
+  public get available() {
+    if (!window.getOfflineSigner || !window.keplr) return false;
+    if (!window.keplr?.experimentalSuggestChain) return alert('Please update to a more recent version of Keplr');
+    return true;
+  }
+  public get enabled() {
+    return this.available && this._enabled;
+  }
+  public get accounts() {
+    return this._accounts || [];
+  }
+
+  // ACTIONS
+  public async enable() {
+    console.log('Attempting to enable Keplr web wallet');
+
+    // get the chain id to enable
+    if (!app.chain?.id || !app.chain?.meta?.chain?.id) return;
+    const chainId = app.chain.meta.chain.id === 'straightedge' ? 'straightedge-2'
+      : app.chain.meta.chain.id === 'cosmos' ? 'cosmoshub-3'
+        : null;
+    if (!chainId) return;
+
+    // enable
+    await window.keplr.enable(chainId);
+    this._offlineSigner = window.getOfflineSigner(chainId);
+    this._accounts = await this._offlineSigner.getAccounts();
+    this._enabled = true;
+  }
+}
+
+export default KeplrWebWalletController;
