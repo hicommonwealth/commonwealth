35f144280ee1fde4a7d862da7159920de6aab51c
Raymond Zhong <raykyri@gmail.com>
2020-12-31 22:28:58 -0800

    Fix crash on empty comment

--- shared/utils.ts
+++ shared/utils.ts
@@ -55,7 +55,8 @@ export const preprocessQuillDeltaForRendering = (nodes) => {
   const lines = [];
   for (const node of nodes) {
     if (typeof node.insert === 'string') {
-      node.insert.match(/[^\n]+\n?|\n/g).forEach((line) => {
+      const matches = node.insert.match(/[^\n]+\n?|\n/g);
+      (matches || []).forEach((line) => {
         lines.push({ attributes: node.attributes, insert: line });
       });
     } else {
