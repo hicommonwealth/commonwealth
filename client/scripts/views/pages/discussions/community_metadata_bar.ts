d36a914a7280d0734316bb9d370ca93f5b9ac1bc
Raymond Zhong <raykyri@gmail.com>
2021-01-05 13:50:24 -0800

    More clean up

--- client/scripts/views/pages/discussions/community_metadata_bar.ts
+++ client/scripts/views/pages/discussions/community_metadata_bar.ts
@@ -1,105 +1,79 @@
 import 'pages/discussions/community_metadata_bar.scss';
 
-import { slugify } from 'helpers';
+import { slugify, removeUrlPrefix } from 'helpers';
 import m from 'mithril';
+import { Icon, Icons } from 'construct-ui';
 
 import app from 'state';
 import { AddressInfo, AbridgedThread } from 'models';
 import User from 'views/components/widgets/user';
-import CommunityInfoModule from 'views/components/sidebar/community_info_module';
 import SubscriptionButton from 'views/components/sidebar/subscription_button';
-
-export const MostActiveUser: m.Component<{ user: AddressInfo, activityCount: number }, {}> = {
-  view: (vnode) => {
-    const { user, activityCount } = vnode.attrs;
-    const profile = app.profiles.getProfile(user.chain, user.address);
-    return m('.MostActiveUser', [
-      m(User, {
-        user: profile,
-        avatarSize: 24,
-        linkify: true,
-        popover: true,
-      }),
-      m('.activity-count', activityCount)
-    ]);
-  }
-};
-
-export const MostActiveThread: m.Component<{ thread: AbridgedThread }> = {
-  view: (vnode) => {
-    const { thread } = vnode.attrs;
-    return m('.MostActiveThread', [
-      m('.active-thread', [
-        m('a', {
-          href: '#',
-          onclick: (e) => {
-            e.preventDefault();
-            m.route.set(`/${app.activeId()}/proposal/${thread.slug}/${thread.identifier}-`
-              + `${slugify(thread.title)}`);
-          }
-        }, thread.title),
-      ]),
-      m(User, {
-        user: new AddressInfo(null, thread.address, thread.authorChain, null),
-        linkify: true,
-      }),
-    ]);
-  }
-};
-
-export const ListingSidebar: m.Component<{ entity: string }> = {
-  view: (vnode) => {
-    const activeAddresses = app.recentActivity.getMostActiveUsers();
-    // const activeThreads = app.recentActivity.getMostActiveThreads(entity);
-    return m('.ListingSidebar.forum-container.proposal-sidebar', [
-      m(CommunityInfoModule),
-      activeAddresses.length > 0
-      && m('.user-activity', [
-        m('.user-activity-header', 'Active members'),
-        m('.active-members', activeAddresses.map((user) => {
-          return m(MostActiveUser, {
-            user: user.info,
-            activityCount: user.count
-          });
-        })),
-      ]),
-      // m('.forum-activity', [
-      //   m('.forum-activity-header', 'Active threads'),
-      //   m('.active-threads', activeThreads.map((thread) => {
-      //     return m(MostActiveThread, { thread });
-      //   }))
-      // ]),
-      m('.admins-mods', [
-        m('.admins-mods-header', 'Admins and moderators'),
-        (app.chain || app.community) && m('.active-members', [
-          (app.chain ? app.chain.meta.chain : app.community.meta).adminsAndMods.map((role) => {
-            return m(User, {
-              user: new AddressInfo(null, role.address, role.address_chain, null),
-              avatarSize: 24,
-              linkify: true,
-              popover: true,
-            });
-          }),
-        ]),
-      ]),
-      m(SubscriptionButton),
-    ]);
-  }
-};
+import ManageCommunityModal from 'views/modals/manage_community_modal';
 
 const CommunityMetadataBar: m.Component<{ entity: string }> = {
   view: (vnode) => {
-    const activeAddresses = app.recentActivity.getMostActiveUsers();
+    // const activeAddresses = app.recentActivity.getMostActiveUsers();
     // const activeThreads = app.recentActivity.getMostActiveThreads(entity);
     // available: thread.title, thread.address, thread.authorChain, ...
     //   m.route.set(`/${app.activeId()}/proposal/${thread.slug}/${thread.identifier}-${slugify(thread.title)}`);
+
+    if (!app.chain && !app.community) return;
+    const isAdmin = app.user.isRoleOfCommunity({
+      role: 'admin',
+      chain: app.activeChainId(),
+      community: app.activeCommunityId()
+    });
+
+    const meta = app.chain ? app.chain.meta.chain : app.community.meta;
+    const { name, description, website, chat, telegram, github, introTitle, introText } = meta;
+
     return m('.CommunityMetadataBar', [
       m('h2', [
-        'Edgeware is a next-generation smart contract platform.',
+        introTitle || name,
       ]),
       m('p', [
-        'Use this forum to discuss projects, make proposals, and petition the community.',
+        introText || 'Use this forum to discuss projects, make proposals, and share with the community.',
       ]),
+      (website || chat || telegram || github || isAdmin) && m('p', [
+        website && m('.community-info', [
+          m(Icon, { name: Icons.GLOBE }),
+          m('a.community-info-text', {
+            target: '_blank',
+            href: website
+          }, removeUrlPrefix(website)),
+        ]),
+        chat && m('.community-info', [
+          m(Icon, { name: Icons.MESSAGE_SQUARE }),
+          m('a.community-info-text', {
+            target: '_blank',
+            href: chat
+          }, removeUrlPrefix(chat)),
+        ]),
+        telegram && m('.community-info', [
+          m(Icon, { name: Icons.SEND }),
+          m('a.community-info-text', {
+            target: '_blank',
+            href: telegram
+          }, removeUrlPrefix(telegram)),
+        ]),
+        github && m('.community-info', [
+          m(Icon, { name: Icons.GITHUB }),
+          m('a.community-info-text', {
+            target: '_blank',
+            href: github
+          }, removeUrlPrefix(github)),
+        ]),
+        isAdmin && m('.community-info', [
+          m('a.community-info-text', {
+            href: '#',
+            onclick: (e) => {
+              e.preventDefault();
+              app.modals.create({ modal: ManageCommunityModal });
+            }
+          }, 'Manage community'),
+        ]),
+      ])
+
       // activeAddresses.length > 0
       // && m('.user-activity', [
       //   m('.user-activity-header', 'Active members'),
