f58e09bd1782df12be1fc9c1f717adb4bbda50c1
Yameen Malik <65854545+YameenMalik@users.noreply.github.com>
2021-01-05 22:19:12 +0500

    fixed uptime query

--- server/eventHandlers/imOnlineEvents.ts
+++ server/eventHandlers/imOnlineEvents.ts
@@ -1,6 +1,6 @@
 import { IEventHandler, CWEvent, IChainEventData, SubstrateTypes } from '@commonwealth/chain-events';
-import { sequelize } from '../database';
 import Sequelize from 'sequelize';
+import { sequelize } from '../database';
 const Op = Sequelize.Op;
 
 
@@ -8,9 +8,9 @@ const uptimePercent = (noOfTrues: number, noOfFalse: number, currentEventType: n
   /*
     This formula is used to calculate the uptime percentage of the validators based on previous sessions' uptime.
     upTimePercentage = ((No. isOnline True(s) + Current Execution Mode [1 for AllGood, 0 for SomeOffline]) /
-                        (No. isOnline True(s) + No. isOnline False(s) - 1 + 1)) * 100
+                        (No. isOnline True(s) + No. isOnline False(s))) * 100
   */
-  const upTimePercentage = (((noOfTrues + currentEventType) / (noOfTrues + noOfFalse - 1 + 1)) * 100)
+  const upTimePercentage = (((noOfTrues + currentEventType) / (noOfTrues + noOfFalse)) * 100)
     .toFixed(2);
 
   return upTimePercentage;
@@ -22,7 +22,6 @@ export default class extends IEventHandler {
   ) {
     super();
   }
-  
   public async handle(event: CWEvent < IChainEventData >, dbEvent) {
     // 1) if other event type ignore and do nothing.
     if (event.data.kind !== SubstrateTypes.EventKind.AllGood
@@ -33,8 +32,8 @@ export default class extends IEventHandler {
     const imOnlineEventData = event.data;
     let eventValidatorsList: string | any[];
     if (event.data.kind === SubstrateTypes.EventKind.SomeOffline) {
-      eventValidatorsList = imOnlineEventData.validators?.map(validator=>JSON.parse(validator)[0]);
-    }else{
+      eventValidatorsList = imOnlineEventData.validators?.map((validator) => JSON.parse(validator)[0]);
+    } else {
       eventValidatorsList = imOnlineEventData.validators;
     }
 
@@ -50,8 +49,10 @@ export default class extends IEventHandler {
 
     // 2) Get relevant data from DB for processing.
     /*
-      This query will return the last created record for validators in 'HistoricalValidatorStatistic' table and return the data for each validator with there onlineCount and offlineCount counts iff any.
-      since all new and active validators records has been created by new-session event handler, it'll return the the last created records of them.
+      This query will return the last created record for validators in 'HistoricalValidatorStatistic' 
+      table and return the data for each validator with there onlineCount and offlineCount counts if any.
+      since all new and active validators records has been created by new-session event handler, 
+      it'll return the the last created records of them.
     */
     const rawQuery = `
       SELECT *
@@ -59,8 +60,10 @@ export default class extends IEventHandler {
         SELECT * ,ROW_NUMBER() OVER( PARTITION BY partitionTable.stash ORDER BY created_at DESC ) 
         FROM public."HistoricalValidatorStatistic" as partitionTable
         JOIN( 
-          SELECT stash, SUM(case when "isOnline" then 1 else 0 end) as "onlineCount", SUM(case when "isOnline"  then 0 else 1 end) as "offlineCount" 
-          FROM public."HistoricalValidatorStatistic" as groupTable GROUP by groupTable.stash
+          SELECT stash, SUM(case when "isOnline" then 1 else 0 end) as "onlineCount", 
+          SUM(case when "isOnline"  then 0 else 1 end) as "offlineCount" 
+          FROM public."HistoricalValidatorStatistic" as groupTable 
+          where "eventType" in ('all-good', 'some-offline') GROUP by groupTable.stash
           ) joinTable
         ON joinTable.stash = partitionTable.stash
         WHERE partitionTable.stash IN ('${eventValidatorsList.join("','")}')
@@ -74,14 +77,22 @@ export default class extends IEventHandler {
     switch (imOnlineEventData.kind) {
       case SubstrateTypes.EventKind.AllGood: {
         validatorsList.forEach((validator: any) => {
-          validator.uptime = uptimePercent(Number(validator.onlineCount), Number(validator.offlineCount), 1).toString();  // 1 for AllGood event
+          validator.uptime = uptimePercent(
+            Number(validator.onlineCount),
+            Number(validator.offlineCount),
+            1 // 1 for AllGood event
+          ).toString();
           validator.isOnline = true;
         });
         break;
       }
       case SubstrateTypes.EventKind.SomeOffline: {
         validatorsList.forEach((validator: any) => {
-          validator.uptime = uptimePercent(Number(validator.onlineCount), Number(validator.offlineCount), 0).toString();  // 0 for SomeOffline event
+          validator.uptime = uptimePercent(
+            Number(validator.onlineCount),
+            Number(validator.offlineCount),
+            0  // 0 for SomeOffline event
+          ).toString();
           validator.isOnline = false;
         });
         break;
