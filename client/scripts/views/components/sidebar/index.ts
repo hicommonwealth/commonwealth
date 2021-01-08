0ff59fb5df612f313d3fd5473ef4d844c50bc186
Raymond Zhong <raykyri@gmail.com>
2021-01-07 17:43:41 -0800

    Members page

--- client/scripts/views/components/sidebar/index.ts
+++ client/scripts/views/components/sidebar/index.ts
@@ -95,6 +95,7 @@ const OffchainNavigationModule: m.Component<{ sidebarTopic: number }, { dragulaI
     const onDiscussionsPage = (p) => p === `/${app.activeId()}` || p === `/${app.activeId()}/`
       || p.startsWith(`/${app.activeId()}/proposal/discussion/`);
     const onSearchPage = (p) => p.startsWith(`/${app.activeId()}/search`);
+    const onMembersPage = (p) => p.startsWith(`/${app.activeId()}/members`);
     const onChatPage = (p) => p === `/${app.activeId()}/chat`;
 
     return m('.OffchainNavigationModule.SidebarModule', [
@@ -139,6 +140,16 @@ const OffchainNavigationModule: m.Component<{ sidebarTopic: number }, { dragulaI
             m.route.set(`/${app.activeId()}/search`);
           },
           contentLeft: m(Icon, { name: Icons.SEARCH }),
+        }),
+        m(ListItem, {
+          active: onMembersPage(m.route.get())
+            && (app.chain ? app.chain.serverLoaded : app.community ? app.community.serverLoaded : true),
+          label: 'Members',
+          onclick: (e) => {
+            e.preventDefault();
+            m.route.set(`/${app.activeId()}/members`);
+          },
+          contentLeft: m(Icon, { name: Icons.USERS }),
         }),
         // m(ListItem, {
         //   active: onChatPage(m.route.get()),
