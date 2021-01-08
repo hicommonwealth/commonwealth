d89045adc9014a69e39e1952c8b4224f447210a7
Suspended Reason <graham@commonwealth.im>
2021-01-07 19:12:38 -0600

    Remove unnecessary try/catch structure

--- server/routes/addEditors.ts
+++ server/routes/addEditors.ts
@@ -26,121 +26,113 @@ const addEditors = async (models, req: Request, res: Response, next: NextFunctio
   const [chain, community] = await lookupCommunityIsVisibleToUser(models, req.body, req.user, next);
   const author = await lookupAddressIsOwnedByUser(models, req, next);
 
-  try {
-    const userOwnedAddressIds = await (req.user as any).getAddresses()
-      .filter((addr) => !!addr.verified).map((addr) => addr.id);
-    const thread = await models.OffchainThread.findOne({
-      where: {
-        id: thread_id,
-        address_id: { [Op.in]: userOwnedAddressIds },
-      },
-    });
-    if (!thread) return next(new Error(Errors.InvalidThread));
+  const userOwnedAddressIds = await (req.user as any).getAddresses()
+    .filter((addr) => !!addr.verified).map((addr) => addr.id);
+  const thread = await models.OffchainThread.findOne({
+    where: {
+      id: thread_id,
+      address_id: { [Op.in]: userOwnedAddressIds },
+    },
+  });
+  if (!thread) return next(new Error(Errors.InvalidThread));
 
-    const collaborators = await Promise.all(Object.values(editors).map((editor: any) => {
-      return models.Address.findOne({
-        where: { id: editor.id },
-        include: [ models.Role, models.User ]
+  const collaborators = await Promise.all(Object.values(editors).map((editor: any) => {
+    return models.Address.findOne({
+      where: { id: editor.id },
+      include: [ models.Role, models.User ]
+    });
+  }));
+  // if any of collab array is null, throw error?
+  // if () return next(new Error(Errors.InvalidEditor));
+
+  // Ensure collaborators have community permissions
+  if (collaborators?.length > 0) {
+    await Promise.all(collaborators.map(async (collaborator) => {
+      if (community) {
+        const isMember = collaborator.Roles
+          .find((role) => role.offchain_community_id === community.id);
+        if (!isMember) return next(new Error(Errors.InvalidEditor));
+      } else if (chain) {
+        const isMember = collaborator.Roles
+          .find((role) => role.chain_id === chain.id);
+        if (!isMember) return next(new Error(Errors.InvalidEditor));
+      }
+      const collaboration = await models.SharingPermission.findOrCreate({
+        where: {
+          offchain_thread_id: thread.id,
+          address_id: collaborator.id
+        }
+      });
+
+      // auto-subscribe collaborator to comments & reactions
+      await models.Subscription.create({
+        subscriber_id: collaborator.User.id,
+        category_id: NotificationCategories.NewComment,
+        object_id: `discussion_${thread.id}`,
+        offchain_thread_id: thread.id,
+        community_id: thread.community || null,
+        chain_id: thread.chain || null,
+        is_active: true,
+      });
+      await models.Subscription.create({
+        subscriber_id: req.user.id,
+        category_id: NotificationCategories.NewReaction,
+        object_id: `discussion_${thread.id}`,
+        offchain_thread_id: thread.id,
+        community_id: thread.community || null,
+        chain_id: thread.chain || null,
+        is_active: true,
       });
     }));
-    // if any of collab array is null, throw error?
-    // if () return next(new Error(Errors.InvalidEditor));
-
-    // Ensure collaborators have community permissions
-    if (collaborators?.length > 0) {
-      await Promise.all(collaborators.map(async (collaborator) => {
-        if (community) {
-          const isMember = collaborator.Roles
-            .find((role) => role.offchain_community_id === community.id);
-          if (!isMember) return next(new Error(Errors.InvalidEditor));
-        } else if (chain) {
-          const isMember = collaborator.Roles
-            .find((role) => role.chain_id === chain.id);
-          if (!isMember) return next(new Error(Errors.InvalidEditor));
-        }
-        const collaboration = await models.SharingPermission.findOrCreate({
-          where: {
-            offchain_thread_id: thread.id,
-            address_id: collaborator.id
-          }
-        });
-
-        // auto-subscribe collaborator to comments & reactions
-        try {
-          await models.Subscription.create({
-            subscriber_id: collaborator.User.id,
-            category_id: NotificationCategories.NewComment,
-            object_id: `discussion_${thread.id}`,
-            offchain_thread_id: thread.id,
-            community_id: thread.community || null,
-            chain_id: thread.chain || null,
-            is_active: true,
-          });
-          await models.Subscription.create({
-            subscriber_id: req.user.id,
-            category_id: NotificationCategories.NewReaction,
-            object_id: `discussion_${thread.id}`,
-            offchain_thread_id: thread.id,
-            community_id: thread.community || null,
-            chain_id: thread.chain || null,
-            is_active: true,
-          });
-        } catch (err) {
-          return next(new Error(err));
-        }
-      }));
-    } else {
-      return next(new Error(Errors.InvalidEditor));
-    }
-
-    await thread.save();
-    const finalThread = await models.OffchainThread.findOne({
-      where: { id: thread.id },
-      include: [
-        models.OffchainAttachment,
-        { model: models.Address, as: 'Address' },
-        { model: models.OffchainTopic, as: 'topic' },
-        { model: models.Address, through: models.SharingPermission, as: 'collaborator' },
-      ],
-    });
-
-    // TODO: Build and test notifications
-    if (collaborators?.length > 0) await Promise.all(collaborators.map(async (collaborator) => {
-      if (!collaborator.User) return; // some Addresses may be missing users, e.g. if the user removed the address
-
-      await models.Subscription.emitNotifications(
-        models,
-        NotificationCategories.NewCollaboration,
-        `user-${collaborator.User.id}`,
-        {
-          created_at: new Date(),
-          root_id: Number(finalThread.id),
-          root_type: ProposalType.OffchainThread,
-          root_title: finalThread.title,
-          comment_text: finalThread.body,
-          chain_id: finalThread.chain,
-          community_id: finalThread.community,
-          author_address: finalThread.Address.address,
-          author_chain: finalThread.Address.chain,
-        },
-        {
-          user: finalThread.Address.address,
-          url: getProposalUrl('discussion', finalThread),
-          title: req.body.title,
-          bodyUrl: req.body.url,
-          chain: finalThread.chain,
-          community: finalThread.community,
-          body: finalThread.body,
-        },
-        req.wss,
-        [ finalThread.Address.address ],
-      );
-    }));
-
-    return res.json({ status: 'Success' });
-  } catch (e) {
-    return next(new Error(e));
+  } else {
+    return next(new Error(Errors.InvalidEditor));
   }
+
+  await thread.save();
+  const finalThread = await models.OffchainThread.findOne({
+    where: { id: thread.id },
+    include: [
+      models.OffchainAttachment,
+      { model: models.Address, as: 'Address' },
+      { model: models.OffchainTopic, as: 'topic' },
+      { model: models.Address, through: models.SharingPermission, as: 'collaborator' },
+    ],
+  });
+
+  // TODO: Build and test notifications
+  if (collaborators?.length > 0) await Promise.all(collaborators.map(async (collaborator) => {
+    if (!collaborator.User) return; // some Addresses may be missing users, e.g. if the user removed the address
+
+    await models.Subscription.emitNotifications(
+      models,
+      NotificationCategories.NewCollaboration,
+      `user-${collaborator.User.id}`,
+      {
+        created_at: new Date(),
+        root_id: +finalThread.id,
+        root_type: ProposalType.OffchainThread,
+        root_title: finalThread.title,
+        comment_text: finalThread.body,
+        chain_id: finalThread.chain,
+        community_id: finalThread.community,
+        author_address: finalThread.Address.address,
+        author_chain: finalThread.Address.chain,
+      },
+      {
+        user: finalThread.Address.address,
+        url: getProposalUrl('discussion', finalThread),
+        title: req.body.title,
+        bodyUrl: req.body.url,
+        chain: finalThread.chain,
+        community: finalThread.community,
+        body: finalThread.body,
+      },
+      req.wss,
+      [ finalThread.Address.address ],
+    );
+  }));
+
+  return res.json({ status: 'Success' });
 };
 
 export default addEditors;
--- server/routes/deleteEditor.ts
+++ server/routes/deleteEditor.ts
@@ -22,76 +22,72 @@ const deleteEditor = async (models, req: Request, res: Response, next: NextFunct
   const [chain, community] = await lookupCommunityIsVisibleToUser(models, req.body, req.user, next);
   const author = await lookupAddressIsOwnedByUser(models, req, next);
 
-  try {
-    const userOwnedAddressIds = await (req.user as any).getAddresses()
-      .filter((addr) => !!addr.verified).map((addr) => addr.id);
-    const thread = await models.OffchainThread.findOne({
-      where: {
-        id: thread_id,
-        address_id: { [Op.in]: userOwnedAddressIds },
-      },
-    });
-    if (!thread) return next(new Error(Errors.InvalidThread));
+  const userOwnedAddressIds = await (req.user as any).getAddresses()
+    .filter((addr) => !!addr.verified).map((addr) => addr.id);
+  const thread = await models.OffchainThread.findOne({
+    where: {
+      id: thread_id,
+      address_id: { [Op.in]: userOwnedAddressIds },
+    },
+  });
+  if (!thread) return next(new Error(Errors.InvalidThread));
 
-    const address = await models.Address.find({
-      where: {
-        chain: req.body.editor_chain,
-        address: req.body.editor_address,
-      }
-    });
-
-    const collaboration = await models.SharingPermission.findOne({
-      where: {
-        offchain_thread_id: thread.id,
-        address_id: address.id
-      }
-    });
-
-    await collaboration.destroy();
-
-    // TODO: Delete subscriptions
-
-    let commentSubscription;
-    let reactionSubscription;
-    try {
-      commentSubscription = await models.Subscription.findOne({
-        subscriber_id: address.user_id,
-        category_id: NotificationCategories.NewComment,
-        object_id: `discussion_${thread.id}`,
-        offchain_thread_id: thread.id,
-        community_id: thread.community || null,
-        chain_id: thread.chain || null,
-        is_active: true,
-      });
-      reactionSubscription = await models.Subscription.create({
-        subscriber_id: address.user_id,
-        category_id: NotificationCategories.NewReaction,
-        object_id: `discussion_${thread.id}`,
-        offchain_thread_id: thread.id,
-        community_id: thread.community || null,
-        chain_id: thread.chain || null,
-        is_active: true,
-      });
-      await commentSubscription.destroy();
-      await reactionSubscription.destroy();
-    } catch (err) {
-      return next(new Error(err));
+  const address = await models.Address.find({
+    where: {
+      chain: req.body.editor_chain,
+      address: req.body.editor_address,
     }
+  });
 
-    const finalThread = await models.OffchainThread.findOne({
-      where: { id: thread.id },
-      include: [
-        models.Address,
-        models.OffchainAttachment,
-        { model: models.OffchainTopic, as: 'topic' },
-        { model: models.Address, through: models.SharingPermission, as: 'collaborator' },
-      ],
+  const collaboration = await models.SharingPermission.findOne({
+    where: {
+      offchain_thread_id: thread.id,
+      address_id: address.id
+    }
+  });
+
+  await collaboration.destroy();
+
+  // TODO: Delete subscriptions
+
+  let commentSubscription;
+  let reactionSubscription;
+  try {
+    commentSubscription = await models.Subscription.findOne({
+      subscriber_id: address.user_id,
+      category_id: NotificationCategories.NewComment,
+      object_id: `discussion_${thread.id}`,
+      offchain_thread_id: thread.id,
+      community_id: thread.community || null,
+      chain_id: thread.chain || null,
+      is_active: true,
     });
-
-    return res.json({ status: 'Success', result: finalThread.toJSON() });
-  } catch (e) {
-    return next(new Error(e));
+    reactionSubscription = await models.Subscription.findOne({
+      subscriber_id: address.user_id,
+      category_id: NotificationCategories.NewReaction,
+      object_id: `discussion_${thread.id}`,
+      offchain_thread_id: thread.id,
+      community_id: thread.community || null,
+      chain_id: thread.chain || null,
+      is_active: true,
+    });
+    await commentSubscription.destroy();
+    await reactionSubscription.destroy();
+  } catch (err) {
+    return next(new Error('Removing editor subscriptions failed'));
   }
+
+  const finalThread = await models.OffchainThread.findOne({
+    where: { id: thread.id },
+    include: [
+      { model: models.Address, as: 'Address' },
+      models.OffchainAttachment,
+      { model: models.OffchainTopic, as: 'topic' },
+      { model: models.Address, through: models.SharingPermission, as: 'collaborator' },
+    ],
+  });
+
+  return res.json({ status: 'Success', result: finalThread.toJSON() });
 };
 
 export default deleteEditor;
