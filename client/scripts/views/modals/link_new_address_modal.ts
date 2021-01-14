6243053e5047b91c5b864120453b7144c0ce0560
Raymond Zhong <raykyri@gmail.com>
2021-01-13 16:21:12 -0800

    Only check address status if we are currently logged in

--- client/scripts/views/modals/link_new_address_modal.ts
+++ client/scripts/views/modals/link_new_address_modal.ts
@@ -60,23 +60,27 @@ const EthereumLinkAccountItem: m.Component<{
       onclick: async (e) => {
         e.preventDefault();
         vnode.state.linking = true;
-        const { result } = await $.post(`${app.serverUrl()}/getAddressStatus`, {
-          address: address.toLowerCase(),
-          chain: app.activeChainId(),
-          jwt: app.user.jwt,
-        });
 
-        if (result.exists) {
-          if (result.belongsToUser) {
-            notifyInfo('This address is already linked to your current account.');
-            vnode.state.linking = false;
-            return;
-          } else {
-            const modalMsg = 'This address is currently linked to another account. Continue?';
-            const confirmed = await confirmationModalWithText(modalMsg)();
-            if (!confirmed) {
+        // check address status if currently logged in
+        if (app.isLoggedIn()) {
+          const { result } = await $.post(`${app.serverUrl()}/getAddressStatus`, {
+            address: address.toLowerCase(),
+            chain: app.activeChainId(),
+            jwt: app.user.jwt,
+          });
+
+          if (result.exists) {
+            if (result.belongsToUser) {
+              notifyInfo('This address is already linked to your current account.');
               vnode.state.linking = false;
               return;
+            } else {
+              const modalMsg = 'This address is currently linked to another account. Continue?';
+              const confirmed = await confirmationModalWithText(modalMsg)();
+              if (!confirmed) {
+                vnode.state.linking = false;
+                return;
+              }
             }
           }
         }
