831b91c8f7c7982317cf24af0e52c741d703244d
gdjohnson <graham@commonwealth.im>
2021-02-03 23:19:43 -0600

    Ensure thread form topics update properly from drafts & localStorage (#1072)
    * Update tag names to topic
    
    * Ensure form updates from drafts and localStorage
    
    * Switch selectedTopic variable off state

--- client/scripts/views/components/new_thread_form.ts
+++ client/scripts/views/components/new_thread_form.ts
@@ -13,7 +13,6 @@ import {
 } from 'construct-ui';
 
 import app from 'state';
-import { link } from 'helpers';
 import { detectURL, parseMentionsForServer } from 'helpers/threads';
 import { OffchainTopic, OffchainThreadKind, CommunityInfo, NodeInfo } from 'models';
 import { updateLastVisited } from 'controllers/app/login';
@@ -289,13 +288,12 @@ export const loadDraft = async (dom, state, draft) => {
   state.form.threadTitle = draft.title;
 
   localStorage.setItem(`${app.activeId()}-new-discussion-storedTitle`, state.form.threadTitle);
-  state.activeTopic = draft.tag;
-  state.form.topicName = draft.tag;
+  state.activeTopic = draft.topic;
+  state.form.topicName = draft.topic;
   state.fromDraft = draft.id;
   if (state.quillEditorState?.alteredText) {
     state.quillEditorState.alteredText = false;
   }
-  m.redraw();
 };
 
 // export const cancelDraft = async (state) => {
@@ -377,7 +375,7 @@ export const NewThreadForm: m.Component<{
       }
       localStorage.removeItem(`${app.activeId()}-new-discussion-storedTitle`);
       localStorage.removeItem(`${app.activeId()}-new-discussion-storedText`);
-      localStorage.removeItem(`${app.activeId()}-active-tag`);
+      localStorage.removeItem(`${app.activeId()}-active-topic`);
       localStorage.removeItem(`${app.activeId()}-post-type`);
     }
   },
@@ -391,7 +389,7 @@ export const NewThreadForm: m.Component<{
     }
 
     const updateTopicState = (topicName: string, topicId?: number) => {
-      localStorage.setItem(`${app.activeId()}-active-tag`, topicName);
+      localStorage.setItem(`${app.activeId()}-active-topic`, topicName);
       vnode.state.activeTopic = topicName;
       vnode.state.form.topicName = topicName;
       vnode.state.form.topicId = topicId;
@@ -430,7 +428,7 @@ export const NewThreadForm: m.Component<{
         localStorage.removeItem(`${app.activeId()}-new-link-storedTitle`);
         localStorage.removeItem(`${app.activeId()}-new-link-storedLink`);
       }
-      localStorage.removeItem(`${app.activeId()}-active-tag`);
+      localStorage.removeItem(`${app.activeId()}-active-topic`);
       localStorage.removeItem(`${app.activeId()}-post-type`);
     };
 
@@ -512,7 +510,7 @@ export const NewThreadForm: m.Component<{
           hasTopics
             ? m(FormGroup, { span: { xs: 12, sm: 5 }, order: 1 }, [
               m(TopicSelector, {
-                defaultTopic: vnode.state.activeTopic || localStorage.getItem(`${app.activeId()}-active-tag`),
+                defaultTopic: vnode.state.activeTopic || localStorage.getItem(`${app.activeId()}-active-topic`),
                 topics: app.topics.getByCommunity(app.activeId()),
                 featuredTopics: app.topics.getByCommunity(app.activeId())
                   .filter((ele) => activeEntityInfo.featuredTopics.includes(`${ele.id}`)),
@@ -617,7 +615,7 @@ export const NewThreadForm: m.Component<{
               m(TopicSelector, {
                 defaultTopic: (vnode.state.activeTopic === false || vnode.state.activeTopic)
                   ? vnode.state.activeTopic
-                  : localStorage.getItem(`${app.activeId()}-active-tag`),
+                  : localStorage.getItem(`${app.activeId()}-active-topic`),
                 topics: app.topics.getByCommunity(app.activeId()),
                 featuredTopics: app.topics.getByCommunity(app.activeId())
                   .filter((ele) => activeEntityInfo.featuredTopics.includes(`${ele.id}`)),
@@ -693,8 +691,7 @@ export const NewThreadForm: m.Component<{
               tabindex: 4
             }),
             m(Button, {
-              disabled: !author || saving || vnode.state.uploadsInProgress > 0
-                || (fromDraft && !vnode.state.quillEditorState?.alteredText),
+              disabled: !author || saving || vnode.state.uploadsInProgress > 0,
               intent: 'none',
               rounded: true,
               onclick: async (e) => {
@@ -767,6 +764,7 @@ export const NewThreadForm: m.Component<{
             onclick: (e) => {
               const parent = $(e.target).closest('.NewThreadForm');
               loadDraft(parent, vnode.state, draft);
+              m.redraw();
             },
             contentRight: [
               fromDraft === draft.id
