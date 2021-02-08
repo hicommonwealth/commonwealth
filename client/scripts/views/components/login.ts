c8a49767972ee670cb27d8c92e5e99b7b72f75fc
Raymond Zhong <raykyri@gmail.com>
2021-02-08 11:39:47 -0800

    Update Magic login flow for clarity

--- client/scripts/views/components/login.ts
+++ client/scripts/views/components/login.ts
@@ -41,7 +41,7 @@ const Login: m.Component<{}, {
       }
     }, [
       m(Form, { gutter: 10 }, [
-        m(FormGroup, { span: 9 }, [
+        !vnode.state.showMagicLoginPrompt && m(FormGroup, { span: 9 }, [
           m(Input, {
             fluid: true,
             name: 'email',
@@ -50,13 +50,12 @@ const Login: m.Component<{}, {
             onclick: (e) => {
               e.stopPropagation();
             },
-            disabled: vnode.state.showMagicLoginPrompt,
             oncreate: (vvnode) => {
               $(vvnode.dom).focus();
             }
           }),
         ]),
-        m(FormGroup, { span: 3 }, [
+        !vnode.state.showMagicLoginPrompt && m(FormGroup, { span: 3 }, [
           m(Button, {
             intent: 'primary',
             fluid: true,
@@ -132,35 +131,12 @@ const Login: m.Component<{}, {
         'Check your email to continue.'
       ]) : vnode.state.showMagicLoginPrompt ? m('.login-magic-prompt', [
         m('p', [
-          'Commonwealth requires a crypto address to participate in discussions. Generate one for ',
-          m('strong', vnode.state.showMagicLoginPromptEmail),
-          '?',
+          'Do you already have a crypto wallet?',
         ]),
         m('.login-magic-prompt-buttons', [
           m(Button, {
-            label: 'Yes, generate an address for me',
+            label: 'Yes, I have a wallet',
             intent: 'primary',
-            rounded: true,
-            compact: true,
-            disabled: vnode.state.disabled,
-            onclick: async (e) => {
-              e.preventDefault();
-              try {
-                vnode.state.disabled = true;
-                await loginWithMagicLink(vnode.state.showMagicLoginPromptEmail);
-                vnode.state.disabled = false;
-                // do not redirect -- just close modal
-                exitWithMagicLoginComplete();
-              } catch (err) {
-                vnode.state.disabled = false;
-                vnode.state.failure = true;
-                vnode.state.error = err.message;
-              }
-            },
-          }),
-          m(Button, {
-            label: 'No, I’ll connect an address later',
-            intent: 'none',
             rounded: true,
             compact: true,
             disabled: vnode.state.disabled,
@@ -189,6 +165,32 @@ const Login: m.Component<{}, {
               m.redraw();
             },
           }),
+          m(Button, {
+            label: 'No, I need an address',
+            intent: 'primary',
+            rounded: true,
+            compact: true,
+            disabled: vnode.state.disabled,
+            onclick: async (e) => {
+              e.preventDefault();
+              try {
+                vnode.state.disabled = true;
+                await loginWithMagicLink(vnode.state.showMagicLoginPromptEmail);
+                vnode.state.disabled = false;
+                // do not redirect -- just close modal
+                exitWithMagicLoginComplete();
+              } catch (err) {
+                vnode.state.disabled = false;
+                vnode.state.failure = true;
+                vnode.state.error = err.message;
+              }
+            },
+          }),
+        ]),
+        m('p', [
+          'If you don’t, we’ll generate an address that you can use to log in from ',
+          m('strong', vnode.state.showMagicLoginPromptEmail),
+          '.'
         ]),
       ]) : '',
       !vnode.state.showMagicLoginPrompt && [
