7ee5f1f9892109e8fb1c8c39800373ddf8bab8a5
Suspended Reason <graham@commonwealth.im>
2021-02-08 10:21:48 -0600

    Add author to migration

--- server/migrations/20210203200024-add-title-to-chain-entities.js
+++ server/migrations/20210203200024-add-title-to-chain-entities.js
@@ -2,24 +2,52 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.addColumn(
-      'ChainEntities',
-      'title',
-      {
-        type: Sequelize.TEXT,
-        allowNull: true,
-      }
-    );
+    return queryInterface.sequelize.transaction((t) => {
+      return Promise.all([
+        queryInterface.addColumn(
+          'ChainEntities',
+          'title',
+          {
+            type: Sequelize.TEXT,
+            allowNull: true,
+          },
+          { transaction: t }
+        ),
+        queryInterface.addColumn(
+          'ChainEntities',
+          'author',
+          {
+            type: Sequelize.STRING,
+            allowNull: true,
+          },
+          { transaction: t }
+        )
+      ]);
+    });
   },
 
   down: (queryInterface, Sequelize) => {
-    return queryInterface.removeColumn(
-      'ChainEntities',
-      'title',
-      {
-        type: Sequelize.TEXT,
-        allowNull: true,
-      }
-    );
+    return queryInterface.sequelize.transaction((t) => {
+      return Promise.all([
+        queryInterface.removeColumn(
+          'ChainEntities',
+          'title',
+          {
+            type: Sequelize.TEXT,
+            allowNull: true,
+          },
+          { transaction: t }
+        ),
+        queryInterface.removeColumn(
+          'ChainEntities',
+          'author',
+          {
+            type: Sequelize.STRING,
+            allowNull: true,
+          },
+          { transaction: t }
+        )
+      ]);
+    });
   }
 };
