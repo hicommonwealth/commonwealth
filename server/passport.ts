71cff41fa3cdb46d994945215ca8cdfc54a65226
Jake Naviasky <jake@commonwealth.im>
2021-02-17 14:59:46 -0500

    Add polkadot magic support. (#1092)
    * Add polkadot magic login for edgeware only.
    
    * Add magic support for all polkadot chains.
    
    * ignore missing chain/community in startEmailLogin
    
    Co-authored-by: Raymond Zhong <raykyri@gmail.com>

--- server/passport.ts
+++ server/passport.ts
@@ -2,6 +2,9 @@ import passport from 'passport';
 import passportGithub from 'passport-github';
 import passportJWT from 'passport-jwt';
 import { Request } from 'express';
+import request from 'superagent';
+
+import { encodeAddress } from '@polkadot/util-crypto';
 
 import { Magic, MagicUserMetadata } from '@magic-sdk/admin';
 import { Strategy as MagicStrategy } from 'passport-magic';
@@ -12,7 +15,7 @@ const log = factory.getLogger(formatFilename(__filename));
 
 
 import {
-  JWT_SECRET, GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET, GITHUB_OAUTH_CALLBACK, MAGIC_API_KEY, MAGIC_SUPPORTED_CHAINS,
+  JWT_SECRET, GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET, GITHUB_OAUTH_CALLBACK, MAGIC_API_KEY, MAGIC_SUPPORTED_BASES,
   MAGIC_DEFAULT_CHAIN
 } from './config';
 import { NotificationCategories } from '../shared/types';
@@ -56,8 +59,13 @@ function setupPassport(models) {
         [ chain, community, error ] = await lookupCommunityIsVisibleToUser(models, req.body, req.user);
         if (error) return cb(error);
       }
-      const registrationChain: string = chain ? chain.id : community?.default_chain
-        ? community?.default_chain : MAGIC_DEFAULT_CHAIN;
+      let registrationChain;
+      if (chain?.id) {
+        registrationChain = chain;
+      } else {
+        const chainId = community?.default_chain || MAGIC_DEFAULT_CHAIN;
+        registrationChain = await models.Chain.findOne({ where: { id: chainId } });
+      }
 
       // fetch user data from magic backend
       let userMetadata: MagicUserMetadata;
@@ -80,11 +88,38 @@ function setupPassport(models) {
       });
 
       // unsupported chain -- client should send through old email flow
-      if (!existingUser && (!registrationChain || !MAGIC_SUPPORTED_CHAINS.includes(registrationChain))) {
+      if (!existingUser && (!registrationChain?.base || !MAGIC_SUPPORTED_BASES.includes(registrationChain.base))) {
         return cb(new Error('Unsupported magic chain.'));
       }
 
       if (!existingUser) {
+        // ensure all eth addresses are lowercase
+        let address = userMetadata.publicAddress.toLowerCase();
+
+        // if on polkadot chain, retrieve the address for the user
+        if (registrationChain.base === 'substrate') {
+          try {
+            const polkadotResp = await request
+              // eslint-disable-next-line max-len
+              .get(`https://api.magic.link/v1/admin/auth/user/public/address/get?issuer=did:ethr:${userMetadata.publicAddress}`)
+              .set('X-Magic-Secret-key', MAGIC_API_KEY)
+              .accept('json');
+            if (polkadotResp.body?.status !== 'ok') {
+              throw new Error(polkadotResp.body?.message || 'Failed to fetch polkadot address');
+            }
+            const polkadotAddress = polkadotResp.body?.data?.public_address;
+
+            // convert to chain-specific address based on ss58 prefix
+            if (registrationChain.ss58_prefix) {
+              address = encodeAddress(polkadotAddress, registrationChain.ss58_prefix);
+            } else {
+              address = polkadotAddress;
+            }
+          } catch (err) {
+            return cb(new Error(err.message));
+          }
+        }
+
         const result = await sequelize.transaction(async (t) => {
           // create new user and unverified address if doesn't exist
           const newUser = await models.User.create({
@@ -96,8 +131,8 @@ function setupPassport(models) {
 
           // TODO: use non-default chain in certain cases?
           const newAddress = await models.Address.create({
-            address: userMetadata.publicAddress.toLowerCase(), // ensure all eth addresses are lowercase
-            chain: registrationChain,
+            address,
+            chain: registrationChain.id,
             verification_token: 'MAGIC',
             verification_token_expires: null,
             verified: new Date(), // trust addresses from magic
@@ -134,7 +169,6 @@ function setupPassport(models) {
 
           return newUser;
         });
-        console.log(`Created user: ${JSON.stringify(result)}`);
 
         // re-fetch user to include address object
         // TODO: simplify this without doing a refetch
