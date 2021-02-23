e05df594f48a28badf802057e2bd71e1a11d7249
Suspended Reason <graham@commonwealth.im>
2021-02-23 10:02:26 -0600

    Prevent initial version history from being counted as an edit

--- server/util/getLastEdited.ts
+++ server/util/getLastEdited.ts
@@ -1,6 +1,6 @@
 export const getLastEdited = (post) => {
   let lastEdited;
-  if (post.version_history && post.version_history.length > 0) {
+  if (post.version_history && post.version_history.length > 1) {
     try {
       const latestVersion = JSON.parse(post.version_history[0]);
       lastEdited = latestVersion ? latestVersion.timestamp : null;
