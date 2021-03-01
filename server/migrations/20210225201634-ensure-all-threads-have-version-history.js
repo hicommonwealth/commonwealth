9bc7beb8f64fbd0527ac3ecd0cfb6995fe72eb3c
Suspended Reason <graham@commonwealth.im>
2021-03-01 10:27:01 -0600

    Second pass, migration up

--- server/migrations/20210225201634-ensure-all-threads-have-version-history.js
+++ server/migrations/20210225201634-ensure-all-threads-have-version-history.js
@@ -4,19 +4,16 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     const query = `SELECT * FROM "OffchainThreads" WHERE version_history IS NULL OR version_history='{}'`;
     const threads = await queryInterface.sequelize.query(query);
-    return queryInterface.sequelize.transaction((t) => {
-      return Promise.all(threads.forEach(async (thread, i) => {
-        const firstVersion = {
-          timestamp: thread.createdAt,
-          body: decodeURIComponent(thread.body)
-        };
-        if (i < 2) console.log(firstVersion);
-        const version_history = [ JSON.stringify(firstVersion) ];
-        if (i === 0) console.log(version_history);
-        const update = `UPDATE "OffchainThreads" SET version_history=${version_history} WHERE id=${thread.id}`;
-        return queryInterface.sequelize.query(update, { transaction: t });
-      }));
-    });
+    return Promise.all(threads[0].map(async (thread, i) => {
+      const firstVersion = {
+        timestamp: thread.created_at,
+        body: decodeURIComponent(thread.body)
+      };
+      const version_history = [ JSON.stringify(firstVersion) ];
+      if (i === 0) console.log(version_history);
+      const update = `UPDATE "OffchainThreads" SET version_history=${version_history} WHERE id=${thread.id}`;
+      return queryInterface.sequelize.query(update, { transaction: t });
+    }));
   },
 
   down: (queryInterface, Sequelize) => {
