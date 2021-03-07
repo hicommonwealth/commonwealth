9491b0b7b7551b043b2651d8c2c8fb5f840b4295
dillchen <dillon@commonwealth.im>
2021-03-06 23:57:19 -0500

    add dialectOption ssl

--- server/database.ts
+++ server/database.ts
@@ -12,7 +12,8 @@ export const sequelize = new Sequelize(DATABASE_URI, {
   operatorsAliases: false,
   logging: (process.env.NODE_ENV === 'test') ? false : (msg) => { log.trace(msg); },
   dialectOptions: {
-    requestTimeout: 10000
+    requestTimeout: 10000,
+    ssl: true
   },
   pool: {
     max: 10,
