a916dd73d48405d049c916a718012df7af8b3fe8
Dillon Chen <dillonchen13@gmail.com>
2021-03-07 06:28:54 +0000

    Revert "Dill.database (#1115)" (#1116)
    This reverts commit 27e88cdccd88ab9f6c4621ec15bb48c6e5147785.

--- server/config.ts
+++ server/config.ts
@@ -42,9 +42,7 @@ export const GITHUB_OAUTH_CALLBACK = (process.env.NODE_ENV === 'production'
 export const DATABASE_URI =
       (!process.env.DATABASE_URL || process.env.NODE_ENV === 'development') ?
       'postgresql://commonwealth:edgeware@localhost/commonwealth' :
-        (process.env.NODE_ENV === 'commonwealth') ? 
-          process.env.DATABASE_URL : 
-          `${process.env.DATABASE_URL}?sslmode=require`;
+      process.env.DATABASE_URL;
 
 // limit logins in the last 5 minutes
 // increased because of chain waitlist registrations
--- server/database.ts
+++ server/database.ts
@@ -13,9 +13,6 @@ export const sequelize = new Sequelize(DATABASE_URI, {
   logging: (process.env.NODE_ENV === 'test') ? false : (msg) => { log.trace(msg); },
   dialectOptions: {
     requestTimeout: 10000,
-    ssl: {
-      rejectUnauthorized: false,
-    }
   },
   pool: {
     max: 10,
