6aa0ea54b51020062ea01e70a6ec9074713af1f7
Raymond Zhong <raykyri@gmail.com>
2021-03-07 01:31:46 -0800

    Tell Sequelize to require ssl, accept unauthorized certs

--- server/database.ts
+++ server/database.ts
@@ -13,6 +13,8 @@ export const sequelize = new Sequelize(DATABASE_URI, {
   logging: (process.env.NODE_ENV === 'test') ? false : (msg) => { log.trace(msg); },
   dialectOptions: {
     requestTimeout: 10000,
+    ssl: true,
+    rejectUnauthorized: false,
   },
   pool: {
     max: 10,
