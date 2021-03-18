94daece54a28fcc866b2de953017cc8b7955a4e1
Raymond Zhong <raykyri@gmail.com>
2021-03-17 22:06:26 -0700

    Add more indexes to migration

--- server/migrations/20210318044656-create-optimization-indexes.js
+++ server/migrations/20210318044656-create-optimization-indexes.js
@@ -3,6 +3,9 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.sequelize.transaction(async (t) => {
+      await queryInterface.addIndex('OffchainThreads', { fields: ['created_at'] }, { transaction: t });
+      await queryInterface.addIndex('OffchainThreads', { fields: ['updated_at'] }, { transaction: t });
+
       await queryInterface.addIndex('OffchainThreads', { fields: ['chain', 'created_at'] }, { transaction: t });
       await queryInterface.addIndex('OffchainThreads', { fields: ['community', 'created_at'] }, { transaction: t });
       await queryInterface.addIndex('OffchainThreads', { fields: ['chain', 'updated_at'] }, { transaction: t });
@@ -25,23 +28,26 @@ module.exports = {
 
   down: (queryInterface, Sequelize) => {
     return queryInterface.sequelize.transaction(async (t) => {
-      await queryInterface.removeIndex('Roles', 'offchain_threads_chain_created_at', { transaction: t });
-      await queryInterface.removeIndex('Roles', 'offchain_threads_community_created_at', { transaction: t });
-      await queryInterface.removeIndex('Roles', 'offchain_threads_chain_updated_at', { transaction: t });
-      await queryInterface.removeIndex('Roles', 'offchain_threads_community_updated_at', { transaction: t });
-      await queryInterface.removeIndex('Roles', 'offchain_threads_chain_pinned', { transaction: t });
-      await queryInterface.removeIndex('Roles', 'offchain_threads_community_pinned', { transaction: t });
+      await queryInterface.removeIndex('OffchainThreads', 'offchain_threads_created_at', { transaction: t });
+      await queryInterface.removeIndex('OffchainThreads', 'offchain_threads_updated_at', { transaction: t });
 
-      await queryInterface.removeIndex('Roles', 'offchain_comments_chain_created_at', { transaction: t });
-      await queryInterface.removeIndex('Roles', 'offchain_comments_community_created_at', { transaction: t });
-      await queryInterface.removeIndex('Roles', 'offchain_comments_chain_updated_at', { transaction: t });
-      await queryInterface.removeIndex('Roles', 'offchain_comments_community_updated_at', { transaction: t });
-      await queryInterface.removeIndex('Roles', 'offchain_comments_root_id', { transaction: t });
+      await queryInterface.removeIndex('OffchainThreads', 'offchain_threads_chain_created_at', { transaction: t });
+      await queryInterface.removeIndex('OffchainThreads', 'offchain_threads_community_created_at', { transaction: t });
+      await queryInterface.removeIndex('OffchainThreads', 'offchain_threads_chain_updated_at', { transaction: t });
+      await queryInterface.removeIndex('OffchainThreads', 'offchain_threads_community_updated_at', { transaction: t });
+      await queryInterface.removeIndex('OffchainThreads', 'offchain_threads_chain_pinned', { transaction: t });
+      await queryInterface.removeIndex('OffchainThreads', 'offchain_threads_community_pinned', { transaction: t });
 
-      await queryInterface.removeIndex('Roles', 'offchain_reactions_chain_thread_id', { transaction: t });
-      await queryInterface.removeIndex('Roles', 'offchain_reactions_community_thread_id', { transaction: t });
-      await queryInterface.removeIndex('Roles', 'offchain_reactions_chain_comment_id', { transaction: t });
-      await queryInterface.removeIndex('Roles', 'offchain_reactions_community_comment_id', { transaction: t });
+      await queryInterface.removeIndex('OffchainComments', 'offchain_comments_chain_created_at', { transaction: t });
+      await queryInterface.removeIndex('OffchainComments', 'offchain_comments_community_created_at', { transaction: t });
+      await queryInterface.removeIndex('OffchainComments', 'offchain_comments_chain_updated_at', { transaction: t });
+      await queryInterface.removeIndex('OffchainComments', 'offchain_comments_community_updated_at', { transaction: t });
+      await queryInterface.removeIndex('OffchainComments', 'offchain_comments_root_id', { transaction: t });
+
+      await queryInterface.removeIndex('OffchainReactions', 'offchain_reactions_chain_thread_id', { transaction: t });
+      await queryInterface.removeIndex('OffchainReactions', 'offchain_reactions_community_thread_id', { transaction: t });
+      await queryInterface.removeIndex('OffchainReactions', 'offchain_reactions_chain_comment_id', { transaction: t });
+      await queryInterface.removeIndex('OffchainReactions', 'offchain_reactions_community_comment_id', { transaction: t });
     });
   }
 };
