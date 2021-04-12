5515d3965827c0421fd834efdc3779dc98bfcf4f
Suspended Reason <graham@commonwealth.im>
2021-04-12 13:03:25 -0500

    Split over previews into sorted, filtered, organized list

--- client/scripts/views/components/search_bar.ts
+++ client/scripts/views/components/search_bar.ts
@@ -2,16 +2,24 @@ import 'pages/search.scss';
 
 import m from 'mithril';
 import _, { capitalize } from 'lodash';
-import { Input, List, ListItem, QueryList } from 'construct-ui';
+import { ControlGroup, Input, List, ListItem } from 'construct-ui';
 import {
   searchMentionableAddresses,
   searchDiscussions,
   searchChainsAndCommunities,
+  CommunityIcon,
+  DiscussionIcon,
+  MemberIcon,
 } from 'helpers/search';
 import getTokenLists from 'views/pages/home/token_lists';
 import app from 'state';
 import { notifyError } from 'controllers/app/notifications';
-import { getCommunityResult, getDiscussionResult, getMemberResult } from '../pages/search';
+import { Profile, AddressInfo } from 'models';
+import moment from 'moment';
+import MarkdownFormattedText from './markdown_formatted_text';
+import QuillFormattedText from './quill_formatted_text';
+import { CommunityLabel } from './sidebar/community_selector';
+import User, { UserBlock } from './widgets/user';
 
 export enum SearchPrefix {
   COMMUNITY = 'in:',
@@ -117,6 +125,195 @@ export const search = async (searchTerm: string, params: SearchParams, vnode) =>
   })();
 };
 
+// TODO: Linkification of users, tokens, comms results
+export const getMemberPreview = (addr, searchTerm) => {
+  const profile: Profile = app.profiles.getProfile(addr.chain, addr.address);
+  const userLink = `/${m.route.param('scope') || addr.chain}/account/${addr.address}?base=${addr.chain}`;
+  // TODO: Display longer or even full addresses
+  return m(ListItem, {
+    contentLeft: m(MemberIcon),
+    label: m('a.search-results-item', [
+      m(UserBlock, {
+        user: profile,
+        searchTerm,
+        avatarSize: 36,
+      }),
+    ]),
+    onclick: (e) => {
+      m.route.set(userLink);
+    }
+  });
+};
+
+export const getCommunityPreview = (community) => {
+  if (community.contentType === ContentType.Token) {
+    return m(ListItem, {
+      contentLeft: m(CommunityIcon),
+      label: m('a.search-results-item', [
+        m('img', {
+          src: community.logoURI,
+          height: '36px',
+          width: '36px'
+        }),
+        m('span', community.name)
+      ]),
+      onclick: (e) => {
+        // TODO: Linkification of tokens
+        m.route.set('/');
+      }
+    });
+  } else if (community.contentType === ContentType.Chain
+    || community.contentType === ContentType.Community) {
+    return m(ListItem, {
+      contentLeft: m(CommunityIcon),
+      label: m('a.search-results-item', [
+        m(CommunityLabel, {
+          community,
+          size: 36,
+        })
+      ]),
+      onclick: (e) => {
+        m.route.set(community.id ? `/${community.id}` : '/');
+      }
+    });
+  }
+};
+
+export const getDiscussionPreview = (thread, searchTerm) => {
+  // TODO: Separate threads, proposals, and comments
+  const activeId = app.activeId();
+  const proposalId = thread.proposalid;
+  return m(ListItem, {
+    onclick: (e) => {
+      m.route.set((thread.type === 'thread')
+        ? `/${activeId}/proposal/discussion/${proposalId}`
+        : `/${activeId}/proposal/${proposalId.split('_')[0]}/${proposalId.split('_')[1]}`);
+    },
+    label: m('a.search-results-item', [
+      thread.type === 'thread' ? [
+        m('.search-results-thread-title', [
+          decodeURIComponent(thread.title),
+        ]),
+        m('.search-results-thread-subtitle', [
+          m('span.created-at', moment(thread.created_at).fromNow()),
+          m(User, { user: new AddressInfo(thread.address_id, thread.address, thread.address_chain, null) }),
+        ]),
+        m('.search-results-thread-body', [
+          (() => {
+            try {
+              const doc = JSON.parse(decodeURIComponent(thread.body));
+              if (!doc.ops) throw new Error();
+              return m(QuillFormattedText, {
+                doc,
+                hideFormatting: true,
+                collapse: true,
+                searchTerm,
+              });
+            } catch (e) {
+              const doc = decodeURIComponent(thread.body);
+              return m(MarkdownFormattedText, {
+                doc,
+                hideFormatting: true,
+                collapse: true,
+                searchTerm,
+              });
+            }
+          })(),
+        ])
+      ] : [
+        m('.search-results-thread-title', [
+          'Comment on ',
+          decodeURIComponent(thread.title),
+        ]),
+        m('.search-results-thread-subtitle', [
+          m('span.created-at', moment(thread.created_at).fromNow()),
+          m(User, { user: new AddressInfo(thread.address_id, thread.address, thread.address_chain, null) }),
+        ]),
+        m('.search-results-comment', [
+          (() => {
+            try {
+              const doc = JSON.parse(decodeURIComponent(thread.body));
+              if (!doc.ops) throw new Error();
+              return m(QuillFormattedText, {
+                doc,
+                hideFormatting: true,
+                collapse: true,
+                searchTerm,
+              });
+            } catch (e) {
+              const doc = decodeURIComponent(thread.body);
+              return m(MarkdownFormattedText, {
+                doc,
+                hideFormatting: true,
+                collapse: true,
+                searchTerm,
+              });
+            }
+          })(),
+        ]),
+      ]
+    ]),
+    contentLeft: m(DiscussionIcon)
+  });
+};
+
+const getContentTypeOrdering = (content: any) => {
+  let type;
+  switch (content.contentType) {
+    case ContentType.Thread:
+      type = 1;
+      break;
+    case ContentType.Comment:
+      type = 2;
+      break;
+    case ContentType.Community:
+      type = 3;
+      break;
+    case ContentType.Token:
+      type = 4;
+      break;
+    case ContentType.Member:
+      type = 5;
+      break;
+    default:
+      type = 6;
+      break;
+  }
+  return type;
+};
+
+const getResultsPreview = (results, searchTerm) => {
+  const placement = {};
+  const sortedResults = results.sort((a, b) => {
+    // TODO: Token-sorting approach
+    // Some users are not verified; we give them a default date of 1900
+    const aCreatedAt = moment(a.created_at || a.createdAt || a.verified || '1900-01-01T:00:00:00Z');
+    const bCreatedAt = moment(b.created_at || b.createdAt || b.verified || '1900-01-01T:00:00:00Z');
+    return bCreatedAt.diff(aCreatedAt);
+  })
+    .slice(0, 10)
+    .sort((a, b) => {
+      return getContentTypeOrdering(b) - getContentTypeOrdering(a);
+    });
+  const organizedResults = [];
+  sortedResults.forEach((item) => {
+    if (!placement[item.contentType]) {
+      const headerEle = m(ListItem, { label: capitalize(item.ContentType) });
+      organizedResults.push(headerEle);
+      placement[item.contentType] = true;
+    }
+    const resultRow = item.searchType === SearchType.Discussion
+      ? getDiscussionPreview(item, searchTerm)
+      : item.searchType === SearchType.Member
+        ? getMemberPreview(item, searchTerm)
+        : item.searchType === SearchType.Community
+          ? getCommunityPreview(item)
+          : null;
+    organizedResults.push(m(ListItem, { label: resultRow }));
+  });
+  return organizedResults;
+};
+
 const SearchBar : m.Component<{}, {
   results: any[],
   searchLoading: boolean,
@@ -142,7 +339,9 @@ const SearchBar : m.Component<{}, {
 
     const { fromPrefix, inPrefix, focused, searchTerm } = vnode.state;
     console.log(vnode.state.results);
-    return m(vnode.state.focused ? '.SearchBar.focused' : '.SearchBar', [
+    return m(ControlGroup, {
+      class: vnode.state.focused ? 'SearchBar focused' : 'SearchBar'
+    }, [
       m(Input, {
         placeholder: 'Type to search...',
         autofocus: true,
@@ -166,6 +365,10 @@ const SearchBar : m.Component<{}, {
             vnode.state.searchModified = true;
           }
           vnode.state.searchTerm = e.target.value?.toLowerCase();
+          if (e.target.value?.length > 2) {
+            // TODO: Hook up community from & address in params
+            search(vnode.state.searchTerm, {}, vnode);
+          }
         },
         onkeyup: (e) => {
           if (e.key === 'Enter') {
@@ -184,24 +387,11 @@ const SearchBar : m.Component<{}, {
             vnode.state.searchModified = false;
             m.route.set(`/${app.activeId()}/search?q=${params}}`);
           }
-          if (e.target.value?.length > 3) {
-            // TODO: Hook up community from & address in params
-            search(vnode.state.searchTerm, {}, vnode);
-          }
         },
       }),
-      m(List, app.searchCache.map((item) => {
-        console.log(item);
-        const resultRow = item.searchType === SearchType.Discussion
-          ? getDiscussionResult(item, searchTerm)
-          : item.searchType === SearchType.Member
-            ? getMemberResult(item, searchTerm)
-            : item.searchType === SearchType.Community
-              ? getCommunityResult(item)
-              : null;
-        console.log(resultRow);
-        return m(ListItem, { label: resultRow });
-      }))
+      // TODO: break out custom dropdown result renderers
+      // TODO: Addrs are showing twice
+      m(List, getResultsPreview(app.searchCache, searchTerm))
     ]);
   }
 };
