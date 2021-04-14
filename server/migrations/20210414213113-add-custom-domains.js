d5b94ca7a37bb5f1435f45c70c2a26219d7c856a
Raymond Zhong <raykyri@gmail.com>
2021-04-14 14:37:55 -0700

    Add custom domains to database, suppress https redirect for local testing domains

---
+++ server/migrations/20210414213113-add-custom-domains.js
@@ -0,0 +1,49 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.sequelize.transaction(async (t) => {
+      await queryInterface.addColumn(
+        'OffchainCommunities',
+        'customDomain',
+        {
+          type: Sequelize.STRING,
+          allowNull: true,
+        },
+        { transaction: t }
+      );
+      await queryInterface.addColumn(
+        'Chains',
+        'customDomain',
+        {
+          type: Sequelize.STRING,
+          allowNull: true,
+        },
+        { transaction: t }
+      );
+    });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.sequelize.transaction(async (t) => {
+      await queryInterface.removeColumn(
+        'OffchainCommunities',
+        'customDomain',
+        {
+          type: Sequelize.STRING,
+          allowNull: true,
+        },
+        { transaction: t }
+      );
+      await queryInterface.removeColumn(
+        'Chains',
+        'customDomain',
+        {
+          type: Sequelize.STRING,
+          allowNull: true,
+        },
+        { transaction: t }
+      );
+    });
+  }
+};
