30f67761fcac4d69709822e699350a3a80e665b0
Jake Naviasky <jake@commonwealth.im>
2021-04-21 14:21:19 -0400

    Add heartbeat removal migration.

---
+++ server/migrations/20210421181754-remove-heartbeat-events.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.sequelize.query(`
+      DELETE FROM "ChainEvents" WHERE "ChainEvents".chain_event_type_id LIKE '%heartbeat-received';`);
+  },
+
+  down: (queryInterface, Sequelize) => {
+    // CANNOT REVERSE
+  }
+};
