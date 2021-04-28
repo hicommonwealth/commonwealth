719ab0f9500667b9cb6ad3aea623bd043ca691e3
James Truong <jamestruong.consult@gmail.com>
2021-04-28 22:31:55 +1000

    [james.bug-substrate-address-checksum] fix offchain profile copy on joining community

--- server/routes/linkExistingAddressToChain.ts
+++ server/routes/linkExistingAddressToChain.ts
@@ -50,6 +50,12 @@ const linkExistingAddressToChain = async (models, req: Request, res: Response, n
     return next(new Error(Errors.NotVerifiedAddressOrUser));
   }
 
+  const originalProfile = await models.OffchainProfile.findOne({
+    where: { address_id: originalAddress.id }
+  });
+
+  const profileData = originalProfile && originalProfile.data ? originalProfile.data : null;
+
   // check if the original address's token is expired. refer edge case 1)
   let verificationToken = originalAddress.verification_token;
   let verificationTokenExpires = originalAddress.verification_token_expires;
@@ -106,6 +112,25 @@ const linkExistingAddressToChain = async (models, req: Request, res: Response, n
       });
 
       addressId = newObj.id;
+    }
+
+    const existingProfile = await models.OffchainProfile.findOne({
+      where: { address_id: addressId }
+    });
+
+    if (existingProfile) {
+      await models.OffchainProfile.update({
+        data: profileData,
+      }, {
+        where: {
+          address_id: addressId
+        }
+      });
+    } else {
+      await models.OffchainProfile.create({
+        address_id: addressId,
+        data: profileData,
+      });
     }
 
     const ownedAddresses = await models.Address.findAll({
