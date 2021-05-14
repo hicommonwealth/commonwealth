da5c7f17b1b14013e0dda26a4994259e5f79d340
fstar1129 <hector1990112950@hotmail.com>
2021-05-14 00:39:21 -0700

    add viewVote component

--- client/scripts/views/pages/view_snapshot_proposal/index.ts
+++ client/scripts/views/pages/view_snapshot_proposal/index.ts
@@ -23,7 +23,7 @@ const ProposalHeader: m.Component<{
     // Original posters have full editorial control, while added collaborators
     // merely have access to the body and title
 		
-		const proposalLink = `/${app.activeId()}/snapshot-proposal/${proposal.ipfsHash}`;
+    const proposalLink = `/${app.activeId()}/snapshot-proposal/${proposal.ipfsHash}`;
 
     return m('.ProposalHeader', {
       class: `proposal-snapshot`
@@ -32,17 +32,17 @@ const ProposalHeader: m.Component<{
         m('.proposal-top-left', [
           m('.proposal-title', [
             m(ProposalHeaderTitle, { proposal })
-        	]),
+          ]),
           m('.proposal-body-meta', [
-						m(ProposalBodyCreated, { item: proposal, link: proposalLink }),
-						m(ProposalBodyLastEdited, { item: proposal }),
-						m(ProposalBodyAuthor, { item: proposal })
-        	]),
+            m(ProposalBodyCreated, { item: proposal, link: proposalLink }),
+	          m(ProposalBodyLastEdited, { item: proposal }),
+	          m(ProposalBodyAuthor, { item: proposal })
+          ]),
       	]),
-			]),
+      ]),
       m('.proposal-content', [
         m('.proposal-content-left', [
-					m(ProposalBodyText, { item: proposal })
+	        m(ProposalBodyText, { item: proposal })
         ]),
       ]),
     ]);
@@ -50,25 +50,81 @@ const ProposalHeader: m.Component<{
 };
 
 interface Vote {
-	voterAddress: string,
-	choice: string,
-	timestamp: string
+  voterAddress: string,
+  choice: string,
+  timestamp: string
+}
+
+const VoteRow: m.Component<{
+  vote: Vote
+}> = {
+  view: (vnode) => {
+    return m('.ViewRow', [
+      m('.row-left', vnode.attrs.vote.voterAddress),
+      m('.row-right', vnode.attrs.vote.choice)
+    ]);
+  }
 }
 
 // Should complete it after mocking up.
 const VoteView: m.Component<{
-	votes: Vote[]
-}> = {
-	view: (vnode) => {
-		return m('h1', {}, "Votes");
-	}
+  votes: Vote[],
+  voteCount: number
+}, {}> = {
+  view: (vnode) => {
+    const { votes, voteCount } = vnode.attrs;
+    let voteListing = [];
+
+    voteListing.push(m('.vote-group-wrap', votes
+      .map((vote) => m(VoteRow, { vote }))));
+
+    return m('.VoteView', [
+      m('h1', [
+        'Votes',
+        m('.vote-count', `${voteCount}`)
+      ]),
+      voteListing
+    ]);
+  }
 }
 
 const ViewProposalPage: m.Component<{
   identifier: string,
 }, {
-	proposal: SnapshotProposal
+  proposal: SnapshotProposal,
+  votes: Vote[],
+  voteCount: number
 }> = {
+  oninit: (vnode) => {
+    vnode.state.votes = [];
+    vnode.state.voteCount = 0;
+
+    const allProposals: SnapshotProposal[] = app.snapshot.proposalStore.getAll();
+    vnode.state.proposal = allProposals.filter(proposal => proposal.ipfsHash === vnode.attrs.identifier)[0];
+
+    if (vnode.state.proposal) {
+      $.get(`https://hub.snapshot.page/api/${app.chain?.meta.chain.snapshot}/proposal/${vnode.state.proposal.ipfsHash}`).then((response) => {
+        if (response.status !== 'Success') {
+          var i = 0;
+          let votes: Vote[] = [];
+          for (const key in response) {
+            let vote: Vote = {
+              voterAddress: '',
+              choice: '',
+              timestamp: '',
+            };
+            vote.voterAddress = key,
+            vote.timestamp = response[key].msg.timestamp;
+            vote.choice = vnode.state.proposal.choices[response[key].msg.payload.choice - 1];
+            votes.push(vote);
+          }
+          vnode.state.voteCount = votes.length;
+          vnode.state.votes = votes.slice(0, 40);
+          m.redraw();
+        }
+      });
+    }
+  },
   oncreate: (vnode) => {
     mixpanel.track('PageVisit', { 'Page Name': 'ViewSnapShotProposalPage' });
     mixpanel.track('Proposal Funnel', {
@@ -79,36 +135,12 @@ const ViewProposalPage: m.Component<{
     });
   },
   view: (vnode) => {
-		const allProposals: SnapshotProposal[] = app.snapshot.proposalStore.getAll(); app.chain.meta.chain.snapshot
-		vnode.state.proposal = allProposals.filter(proposal => proposal.ipfsHash === vnode.attrs.identifier)[0];
-		let votes: Vote[] = [];
-
-		if (vnode.state.proposal) {
-			$.get(`https://hub.snapshot.page/api/${app.chain?.meta.chain.snapshot}/proposal/${vnode.state.proposal.ipfsHash}`).then((response) => {
-				if (response.status !== 'Success') {
-					for (const key in response) {
-						let vote: Vote = {
-							voterAddress: '',
-							choice: '',
-							timestamp: '',
-						};
-						vote.voterAddress = key,
-						vote.timestamp = response[key].msg.timestamp;
-						vote.choice = vnode.state.proposal.choices[response[key].msg.payload.choice - 1];
-						votes.push(vote);
-					}
-				}
-			});
-		}
-
     return m(Sublayout, { class: 'ViewProposalPage', title: "Snapshot Proposal" }, [
       m(ProposalHeader, {
         proposal: vnode.state.proposal,
       }),
-			m('.PinnedDivider', m('hr')),
-			m(VoteView, {
-				votes
-			})
+      m('.PinnedDivider', m('hr')),
+      m(VoteView, { votes: vnode.state.votes, voteCount: vnode.state.voteCount })
     ]);
   }
 };
