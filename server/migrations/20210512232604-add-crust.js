7a754ab91095fda0ede66f7f033e3ac6259268af
zakhap <zhs2104@columbia.edu>
2021-05-14 14:22:17 -0700

    Fix migration

--- server/migrations/20210512232604-add-crust.js
+++ server/migrations/20210512232604-add-crust.js
@@ -1,44 +1,5 @@
 'use strict';
 
-const SubstrateEventKinds = {
-  Slash: 'slash',
-  Reward: 'reward',
-  Bonded: 'bonded',
-  Unbonded: 'unbonded',
-
-  VoteDelegated: 'vote-delegated',
-  DemocracyProposed: 'democracy-proposed',
-  DemocracyTabled: 'democracy-tabled',
-  DemocracyStarted: 'democracy-started',
-  DemocracyPassed: 'democracy-passed',
-  DemocracyNotPassed: 'democracy-not-passed',
-  DemocracyCancelled: 'democracy-cancelled',
-  DemocracyExecuted: 'democracy-executed',
-
-  PreimageNoted: 'preimage-noted',
-  PreimageUsed: 'preimage-used',
-  PreimageInvalid: 'preimage-invalid',
-  PreimageMissing: 'preimage-missing',
-  PreimageReaped: 'preimage-reaped',
-
-  TreasuryProposed: 'treasury-proposed',
-  TreasuryAwarded: 'treasury-awarded',
-  TreasuryRejected: 'treasury-rejected',
-
-  ElectionNewTerm: 'election-new-term',
-  ElectionEmptyTerm: 'election-empty-term',
-  ElectionCandidacySubmitted: 'election-candidacy-submitted',
-  ElectionMemberKicked: 'election-member-kicked',
-  ElectionMemberRenounced: 'election-member-renounced',
-
-  CollectiveProposed: 'collective-proposed',
-  CollectiveVoted: 'collective-voted',
-  CollectiveApproved: 'collective-approved',
-  CollectiveDisapproved: 'collective-disapproved',
-  CollectiveExecuted: 'collective-executed',
-  CollectiveMemberExecuted: 'collective-member-executed',
-};
-
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.sequelize.transaction(async (t) => {
@@ -49,6 +10,7 @@ module.exports = {
         icon_url: '/static/img/protocols/dot.png', // @TODO: Add crust
         type: 'chain',
         network: 'crust',
+        base: 'substrate',
         active: true,
         description: 'A Decentralized Cloud Blockchain Technology on Polkadot',
         telegram: 'https://t.me/CrustNetwork',
@@ -62,21 +24,6 @@ module.exports = {
         url: 'wss://api.crust.network/',
       }], { transaction: t });
 
-      const buildObject = (event_name, chain) => ({
-        id: `${chain}-${event_name}`,
-        chain,
-        event_name,
-      });
-      const crustObjs = Object.values(SubstrateEventKinds).map((s) => buildObject(s, 'stafi'));
-
-      // TODO: somehow switch this on for testing purposes?
-      return queryInterface.bulkInsert(
-        'ChainEventTypes',
-        [
-          ...crustObjs,
-        ],
-        { transaction: t }
-      );
     });
   },
 
