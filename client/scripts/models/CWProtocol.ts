f1391c3be3287cd4b3a65a7aa540ae55d1138118
ace-contributor <acecontributor@gmail.com>
2021-05-17 09:48:51 -0500

    replace dummy data with real chain data

--- client/scripts/models/CWProtocol.ts
+++ client/scripts/models/CWProtocol.ts
@@ -85,8 +85,4 @@ export class CWProtocol {
   public setProjects(_projects: CWProject[]) {
     this.projects = _projects;
   }
-
-  // public setProjects(_projects: CWProject[]) {
-  //   this.projects = _projects;
-  // }
 }
