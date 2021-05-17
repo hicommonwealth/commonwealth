1dbbee482d00f236979348da59abe3957a79b4b6
Suspended Reason <graham@commonwealth.im>
2021-05-17 14:23:10 -0500

    Add footer links, reformat mithril code

--- client/scripts/views/pages/landing/index.ts
+++ client/scripts/views/pages/landing/index.ts
@@ -5,6 +5,7 @@ import Glide from '@glidejs/glide';
 
 import app, { LoginState } from 'state';
 
+import Sublayout from 'views/sublayout';
 import HeaderLandingPage from './landing_page_header';
 import FooterLandingPage from './landing_page_footer';
 import JoinCommonWealthSection from './landing_page_pre_footer';
@@ -17,7 +18,6 @@ import ChainsCrowdfundingComponent from './crowdfunding_card_section';
 // Logged In Homepage View
 import 'pages/landing/index.scss';
 
-import Sublayout from 'views/sublayout';
 import CommunityCards from './community_cards';
 
 interface Chain {
@@ -102,235 +102,237 @@ const LandingPage: m.Component<{}, IState> = {
   view: (vnode) => {
     if (app.loginState !== LoginState.LoggedIn) {
       return m('.LandingPage', { class: 'bg-primary' }, [
-      m(
-        'div',
-        { class: 'absolute w-screen z-20' },
-        m(HeaderLandingPage, {
-          navs: [
-            { text: 'Why Commonwealth?', redirectTo: '/whyCommonWealth' },
+        m(
+          'div',
+          { class: 'absolute w-screen z-20' },
+          m(HeaderLandingPage, {
+            navs: [
+              { text: 'Why Commonwealth?', redirectTo: '/whyCommonWealth' },
             // { text: 'Use Cases', redirectTo: '/whyCommonWealth' },
             // { text: 'Crowdfunding', redirectTo: '/whyCommonWealth' },
             // { text: 'Developers' },
-          ],
-        })
-      ),
-      m(TokensCommunityComponent, { chains: vnode.state.chains }),
-      m(TokensChainsComponent, {
-        oncreateSlider: () => {
-          const glide = new (Glide as any)('.glide', {
-            type: 'carousel',
-            focusAt: 'center',
-            perView: 3,
-            gap: 40,
-            autoplay: 3000,
-            hoverpause: true,
-            peek: {
-              before: 100,
-              after: 100,
-            },
-            breakpoints: {
-              1024: {
-                perView: 2,
-                gap: 40,
+            ],
+          })
+        ),
+        m(TokensCommunityComponent, { chains: vnode.state.chains }),
+        m(TokensChainsComponent, {
+          oncreateSlider: () => {
+            const glide = new (Glide as any)('.glide', {
+              type: 'carousel',
+              focusAt: 'center',
+              perView: 3,
+              gap: 40,
+              autoplay: 3000,
+              hoverpause: true,
+              peek: {
+                before: 100,
+                after: 100,
               },
-              768: {
-                perView: 2,
-                gap: 20,
-              },
-              640: {
-                perView: 1,
-                gap: 16,
-                peek: {
-                  before: 50,
-                  after: 50,
+              breakpoints: {
+                1024: {
+                  perView: 2,
+                  gap: 40,
+                },
+                768: {
+                  perView: 2,
+                  gap: 20,
+                },
+                640: {
+                  perView: 1,
+                  gap: 16,
+                  peek: {
+                    before: 50,
+                    after: 50,
+                  },
                 },
               },
-            },
-          });
-          glide.mount();
-        },
-        chains: vnode.state.chains,
-      }),
-      m(TokensCreatorComponent, {
-        creators: [
-          {
-            button: {
-              id: 'first-section-button1',
-            },
-            texts: {
-              title: ' On-chain notifications ',
-              text:
+            });
+            glide.mount();
+          },
+          chains: vnode.state.chains,
+        }),
+        m(TokensCreatorComponent, {
+          creators: [
+            {
+              button: {
+                id: 'first-section-button1',
+              },
+              texts: {
+                title: ' On-chain notifications ',
+                text:
                 ' Your token holders can stay up-to-date on chain events like votes and large transfers ',
+              },
+              card: {
+                id: 'tab-codepen',
+                imgSrc: 'static/img/tab1.svg',
+                imgAlt: '',
+              },
             },
-            card: {
-              id: 'tab-codepen',
-              imgSrc: 'static/img/tab1.svg',
-              imgAlt: '',
-            },
-          },
-          {
-            button: {
-              id: 'first-section-button2',
-            },
-            texts: {
-              title: ' Off-chain polling & On-chain voting ',
-              text:
+            {
+              button: {
+                id: 'first-section-button2',
+              },
+              texts: {
+                title: ' Off-chain polling & On-chain voting ',
+                text:
                 ' Whether your community uses Snapshot, Comp Governance, or native layer 1. Access everything from one place. ',
+              },
+              card: {
+                id: 'tab2-codepen',
+                imgSrc: 'static/img/tab2.svg',
+                imgAlt: '',
+              },
             },
-            card: {
-              id: 'tab2-codepen',
-              imgSrc: 'static/img/tab2.svg',
-              imgAlt: '',
-            },
-          },
-          {
-            button: {
-              id: 'first-section-button3',
-            },
-            texts: {
-              title: ' Crowdfunding protocols ',
-              text:
+            {
+              button: {
+                id: 'first-section-button3',
+              },
+              texts: {
+                title: ' Crowdfunding protocols ',
+                text:
                 ' Fund new tokens and community initiatives with Kickstarter-like raises from a thread ',
+              },
+              card: {
+                id: 'tab3-codepen',
+                imgSrc: 'static/img/tab3.svg',
+                imgAlt: '',
+              },
             },
-            card: {
-              id: 'tab3-codepen',
-              imgSrc: 'static/img/tab3.svg',
-              imgAlt: '',
-            },
-          },
-          {
-            button: {
-              id: 'first-section-button4',
-            },
-            texts: {
-              title: ' A rich forum experience ',
-              text:
+            {
+              button: {
+                id: 'first-section-button4',
+              },
+              texts: {
+                title: ' A rich forum experience ',
+                text:
                 ' Discuss memes and important topics alike in a Discourse-style forum. Enhance your posts with built in Markdown and fun reactions. ',
+              },
+              card: {
+                id: 'tab4-codepen',
+                imgSrc: 'static/img/tab4.svg',
+                imgAlt: '',
+              },
             },
-            card: {
-              id: 'tab4-codepen',
-              imgSrc: 'static/img/tab4.svg',
-              imgAlt: '',
-            },
-          },
-        ],
-      }),
-      m(TokenHoldersComponent, {
-        holders: [
-          {
-            img: 'static/img/circleCrowd.svg',
-            alt: '',
-            title: 'Your community is here.',
-            text:
+          ],
+        }),
+        m(TokenHoldersComponent, {
+          holders: [
+            {
+              img: 'static/img/circleCrowd.svg',
+              alt: '',
+              title: 'Your community is here.',
+              text:
               ' Stop bouncing between 10 tabs at once - everything you need to know about your token is here. ',
-          },
-          {
-            img: 'static/img/1stButtonToken.svg',
-            alt: '',
-            title: 'Claim your token',
-            text:
+            },
+            {
+              img: 'static/img/1stButtonToken.svg',
+              alt: '',
+              title: 'Claim your token',
+              text:
               ' We generate pages for your favorite community and address from real-time chain actvity. Claim yours. ',
-          },
-          {
-            img: 'static/img/bell.svg',
-            alt: '',
-            title: 'Stay updated',
-            text:
+            },
+            {
+              img: 'static/img/bell.svg',
+              alt: '',
+              title: 'Stay updated',
+              text:
               ' Be the first to know when community events are happening with in-app, email, and mobile push notiications. ',
-          },
-          {
-            img: 'static/img/calendar.svg',
-            alt: '',
-            title: 'Participate in events.',
-            text:
+            },
+            {
+              img: 'static/img/calendar.svg',
+              alt: '',
+              title: 'Participate in events.',
+              text:
               ' Participate in events like upcoming votes, new projects and community initiatives. ',
-          },
-        ],
-      }),
-      m(ChainsCrowdfundingComponent, {
-        chains: [
-          {
-            button: {
-              id: 'second-section-button1',
             },
-            texts: {
-              title: 'Fund new projects',
-              text:
+          ],
+        }),
+        m(ChainsCrowdfundingComponent, {
+          chains: [
+            {
+              button: {
+                id: 'second-section-button1',
+              },
+              texts: {
+                title: 'Fund new projects',
+                text:
                 'Anyone from within your community can easily turn a conversation thread into a Kickstarter-like campaign ',
+              },
+              card: {
+                id: 'tab-card',
+                imgSrc: 'static/img/card1.png',
+                imgAlt: '',
+              },
             },
-            card: {
-              id: 'tab-card',
-              imgSrc: 'static/img/card1.png',
-              imgAlt: '',
-            },
-          },
-          {
-            button: {
-              id: 'second-section-button2',
-            },
-            texts: {
-              title: 'Create Community Endowments',
-              text:
+            {
+              button: {
+                id: 'second-section-button2',
+              },
+              texts: {
+                title: 'Create Community Endowments',
+                text:
                 ' Lets you pool funds with other like minded folks and fund interesting projects within your community or across the web',
+              },
+              card: {
+                id: 'tab2-card',
+                imgSrc: 'static/img/card2.png',
+                imgAlt: '',
+              },
             },
-            card: {
-              id: 'tab2-card',
-              imgSrc: 'static/img/card2.png',
-              imgAlt: '',
-            },
-          },
-          {
-            button: {
-              id: 'second-section-button3',
-            },
-            texts: {
-              title: 'Launch New Tokens',
-              text:
+            {
+              button: {
+                id: 'second-section-button3',
+              },
+              texts: {
+                title: 'Launch New Tokens',
+                text:
                 ' Use a project to raise funds for a new tokenize DeFi or NFT token and optionally plug in an allowlist for KYC compliance ',
+              },
+              card: {
+                id: 'tab3-card',
+                imgSrc: 'static/img/card3.png',
+                imgAlt: '',
+              },
             },
-            card: {
-              id: 'tab3-card',
-              imgSrc: 'static/img/card3.png',
-              imgAlt: '',
-            },
-          },
-        ],
-      }),
-      m(JoinCommonWealthSection),
-      m(FooterLandingPage, {
-        list: [
-          { text: 'Why Commonwealth?', redirectTo: '/whyCommonWealth' },
-          // { text:  'Use Cases' },
-          // { text:  'Crowdfunding' },
-          // { text:  'Developers' },
-          { text:  'Privacy', redirectTo: '/privacy' },
-          { text:  'Terms', redirectTo:  '/terms'}
-        ],
-      }),
-      m('script', {
-        src:
+          ],
+        }),
+        m(JoinCommonWealthSection),
+        m(FooterLandingPage, {
+          list: [
+            { text: 'Why Commonwealth?', redirectTo: '/whyCommonWealth' },
+            // { text:  'Use Cases' },
+            // { text:  'Crowdfunding' },
+            // { text:  'Developers' },
+            { text:  'Privacy', redirectTo: '/privacy' },
+            { text:  'Terms', redirectTo:  '/terms' },
+            { text: 'Jobs', redirectTo: 'https://angel.co/company/commonwealth-labs/jobs' },
+            { text: 'Blog', redirectTo: 'https://commonwealth.im/' }
+          ],
+        }),
+        m('script', {
+          src:
           'https://cdnjs.cloudflare.com/ajax/libs/Glide.js/3.2.0/glide.min.js',
-        integrity:
+          integrity:
           'sha512-IkLiryZhI6G4pnA3bBZzYCT9Ewk87U4DGEOz+TnRD3MrKqaUitt+ssHgn2X/sxoM7FxCP/ROUp6wcxjH/GcI5Q==',
-        crossorigin: 'anonymous',
-      }),
-      m('link', {
-        rel: 'stylesheet',
-        href:
+          crossorigin: 'anonymous',
+        }),
+        m('link', {
+          rel: 'stylesheet',
+          href:
           'https://cdnjs.cloudflare.com/ajax/libs/Glide.js/3.2.0/css/glide.core.min.css',
-        integrity:
+          integrity:
           'sha512-YQlbvfX5C6Ym6fTUSZ9GZpyB3F92hmQAZTO5YjciedwAaGRI9ccNs4iw2QTCJiSPheUQZomZKHQtuwbHkA9lgw==',
-        crossorigin: 'anonymous',
-      }),
-      m('link', {
-        rel: 'stylesheet',
-        href:
+          crossorigin: 'anonymous',
+        }),
+        m('link', {
+          rel: 'stylesheet',
+          href:
           'https://cdnjs.cloudflare.com/ajax/libs/Glide.js/3.2.0/css/glide.theme.min.css',
-        integrity:
+          integrity:
           'sha512-wCwx+DYp8LDIaTem/rpXubV/C1WiNRsEVqoztV0NZm8tiTvsUeSlA/Uz02VTGSiqfzAHD4RnqVoevMcRZgYEcQ==',
-        crossorigin: 'anonymous',
-      }),
-      ]); 
+          crossorigin: 'anonymous',
+        }),
+      ]);
     } else {
       return m(Sublayout, {
         class: 'Homepage',
@@ -357,6 +359,6 @@ const LandingPage: m.Component<{}, IState> = {
       ]);
     }
   }
-}
+};
 
 export default LandingPage;
--- client/scripts/views/pages/landing/landing_page_footer.ts
+++ client/scripts/views/pages/landing/landing_page_footer.ts
@@ -6,58 +6,36 @@ interface IState {
 }
 
 const FooterLandingPage: m.Component<IState, IState> = {
-  oninit: (vnode) => {
-    vnode.state.list = vnode.attrs.list;
-  },
   view: (vnode) => {
     const redirectClick = (route) => {
       m.route.set(route);
     };
 
-    return m(
-      'footer',
-      { class: 'bg-footer bg-cover py-10' },
-      m(
-        'div',
-        {
-          class:
-            'mt-8 container mx-auto md:flex md:flex-row md:justify-between md:items-start',
-        },
-        [
-          m(
-            'div',
-            m('img', {
-              class: 'w-60',
-              src: 'static/img/logo.svg',
-              alt: 'Commonwealth',
-            })
-          ),
-          m(
-            'div',
-            m(
-              'nav',
-              { class: 'mt-10 md:mt-0 w-64' },
-              m('ul', { class: 'flex flex-wrap flex-col h-32' }, [
-                vnode.state.list.map((item) => {
-                  return m(
-                    'li.FooterNavsLinks',
-                    { class: 'mb-2' },
-                    m(
-                      'a',
-                      {
-                        class: 'text-gray-500',
-                        onclick: () => redirectClick(item.redirectTo),
-                      },
-                      item.text
-                    )
-                  );
-                }),
-              ])
-            )
-          ),
-        ]
-      )
-    );
+    return m('footer', { class: 'bg-footer bg-cover py-10' },
+      m('div', { class: 'mt-8 container mx-auto md:flex md:flex-row md:justify-between md:items-start' }, [
+        m('div', [
+          m('img', {
+            class: 'w-60',
+            src: 'static/img/logo.svg',
+            alt: 'Commonwealth',
+          })
+        ]),
+        m('div', [
+          m('nav',
+            { class: 'mt-10 md:mt-0 w-64' },
+            m('ul', { class: 'flex flex-wrap flex-col h-32' }, [
+              vnode.attrs.list.map((item) => {
+                return m('li.FooterNavsLinks', { class: 'mb-2' }, [
+                  m('a', {
+                    class: 'text-gray-500',
+                    onclick: () => redirectClick(item.redirectTo),
+                  },
+                  item.text)
+                ]);
+              }),
+            ]))
+        ]),
+      ]));
   },
 };
 
