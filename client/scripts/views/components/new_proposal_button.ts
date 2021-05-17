1e7129016f2e8dc03d6a66865eaced5cb559daf4
ace-contributor <acecontributor@gmail.com>
2021-05-17 08:07:23 -0500

    enable new project

--- client/scripts/views/components/new_proposal_button.ts
+++ client/scripts/views/components/new_proposal_button.ts
@@ -66,7 +66,7 @@ const getNewProjectMenu = () => {
     m(MenuDivider),
     [
       m(MenuItem, {
-        onclick: (e) => m.route.set(`/${app.community.id}/new/project`),
+        onclick: (e) => m.route.set(`/${app.activeChainId()}/new/project`),
         label: 'New Project'
       }),
     ],
@@ -110,36 +110,36 @@ const NewProposalButton: m.Component<{
     if (!app.chain && !app.community) return;
     if (!app.activeId()) return;
 
-    // if (app.community && app.community.id === 'cw-protocol') {      
-    //   return m(ButtonGroup, {
-    //     class: 'NewProposalButton',
-    //   }, [
-    //     m(PopoverMenu, {
-    //       class: 'new-proposal-button-popover',
-    //       transitionDuration: 0,
-    //       hoverCloseDelay: 0,
-    //       hasArrow: false,
-    //       trigger: m(Button, {
-    //         disabled: !app.user.activeAccount
-    //           || ((app.chain as Token).isToken && !(app.chain as Token).hasToken),
-    //         label: 'New thread',
-    //       }),
-    //       position: 'bottom-end',
-    //       closeOnContentClick: true,
-    //       menuAttrs: {
-    //         align: 'left',
-    //       },
-    //       content: getNewProjectMenu(),
-    //     }),
-    //     m(Button, {
-    //       disabled: !app.user.activeAccount
-    //         || ((app.chain as Token).isToken && !(app.chain as Token).hasToken),
-    //       iconLeft: Icons.EDIT,
-    //       fluid,
-    //       onclick: () => app.modals.create({ modal: NewThreadModal }),
-    //     }),
-    //   ]);
-    // }
+    if (app.chain.network && app.chain.network.includes('CMN')) {
+      return m(ButtonGroup, {
+        class: 'NewProposalButton',
+      }, [
+        m(PopoverMenu, {
+          class: 'new-proposal-button-popover',
+          transitionDuration: 0,
+          hoverCloseDelay: 0,
+          hasArrow: false,
+          trigger: m(Button, {
+            disabled: !app.user.activeAccount
+              || ((app.chain as Token).isToken && !(app.chain as Token).hasToken),
+            label: 'New thread',
+          }),
+          position: 'bottom-end',
+          closeOnContentClick: true,
+          menuAttrs: {
+            align: 'left',
+          },
+          content: getNewProjectMenu(),
+        }),
+        m(Button, {
+          disabled: !app.user.activeAccount
+            || ((app.chain as Token).isToken && !(app.chain as Token).hasToken),
+          iconLeft: Icons.EDIT,
+          fluid,
+          onclick: () => app.modals.create({ modal: NewThreadModal }),
+        }),
+      ]);
+    }
 
     // just a button for communities, or chains without governance
     if (app.community || threadOnly) {
