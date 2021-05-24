af9f01b6594aea581f7d931a61b8f07d3aba5e98
ace-contributor <acecontributor@gmail.com>
2021-05-24 00:03:52 -0500

    implement updated contract

--- client/scripts/app.ts
+++ client/scripts/app.ts
@@ -208,7 +208,6 @@ export async function selectNode(n?: NodeInfo, deferred = false): Promise<boolea
   {
     return;
   }
-
   if ((Object.values(ChainNetwork) as any).indexOf(n.chain.network) === -1) {
     throw new Error('invalid chain');
   }
