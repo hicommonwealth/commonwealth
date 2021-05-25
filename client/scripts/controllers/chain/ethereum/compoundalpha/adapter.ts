a901c26ec89b8eb0c5bb7dc46eb444340e2f8627
zakhap <zhs2104@columbia.edu>
2021-05-24 17:14:58 -0700

    Adapter now same standard as all new ETH controllers

--- client/scripts/controllers/chain/ethereum/compoundalpha/adapter.ts
+++ client/scripts/controllers/chain/ethereum/compoundalpha/adapter.ts
@@ -1,12 +1,11 @@
 import { EthereumCoin } from 'adapters/chain/ethereum/types';
+// Factories?
+import { UniFactory } from 'UniFactory';
 
-import EthWebWalletController from 'controllers/app/eth_web_wallet';
 import EthereumAccount from 'controllers/chain/ethereum/account';
 import EthereumAccounts from 'controllers/chain/ethereum/accounts';
-import EthereumChain from 'controllers/chain/ethereum/chain';
 import { ChainBase, ChainClass, IChainAdapter, ChainEntity, ChainEvent, NodeInfo } from 'models';
 
-import { setActiveAccount } from 'controllers/app/login';
 import ChainEntityController from 'controllers/server/chain_entities';
 import { IApp } from 'state';
 
@@ -15,9 +14,8 @@ import { CompoundalphaTypes } from '@commonwealth/chain-events';
 import CompoundalphaAPI from './api';
 import CompoundalphaChain from './chain';
 import CompoundalphaGovernance from './governance';
-import CompoundalphaProposal from './proposal';
+import CompoundalphaProposal from './proposal'; // TODO: Remove if unused?
 import CompoundalphaHolders from './holders';
-import Token from '../token/adapter';
 
 export default class Compoundalpha extends IChainAdapter<EthereumCoin, EthereumAccount> {
   public readonly base = ChainBase.Ethereum;
@@ -26,7 +24,6 @@ export default class Compoundalpha extends IChainAdapter<EthereumCoin, EthereumA
   public accounts: EthereumAccounts;
   public compoundalphaAccounts:  CompoundalphaHolders;
   public governance: CompoundalphaGovernance;
-  public readonly webWallet: EthWebWalletController = new EthWebWalletController();
   public readonly chainEntities = new ChainEntityController();
 
   constructor(meta: NodeInfo, app: IApp) {
@@ -54,28 +51,20 @@ export default class Compoundalpha extends IChainAdapter<EthereumCoin, EthereumA
   public async initApi() {
     await this.chain.resetApi(this.meta);
     await this.chain.initMetadata();
-    await this.webWallet.enable().catch((e) => console.error(e));
 
-    const activeAddress: string = await this.webWallet.accounts[0];
     const mpondContractAddress = this.meta.address;
-    const governorAlphaContractAddress = '0x777992c2E4EDF704e49680468a9299C6679e37F6';
-    const api = new CompoundalphaAPI();
+    const governorAlphaContractAddress = ''; // TODO: Add support for multiple addresses in meta.address[]
+    const api = new CompoundalphaAPI(
+      UniFactory.connect,
+      this.meta.address,
+      governorAlphaContractAddress,
+      this.chain.api.currentProvider as any,
+    );
     await api.init().catch((e) => {
       this._failed = true;
       notifyError('Please change your Metamask network');
     });
     this.chain.compoundalphaApi = api;
-
-    if ((window as any).ethereum || (window as any).web3) {
-      if (!this.webWallet.enabled) await this.webWallet.enable().catch((e) => console.error(e));
-
-      await this.webWallet.web3.givenProvider.on('accountsChanged', (accounts) => {
-        const updatedAddress = this.app.user.activeAccounts.find((addr) => addr.address === accounts[0]);
-        setActiveAccount(updatedAddress);
-        api.updateSigner(accounts[0]);
-      });
-    }
-
     await this.compoundalphaAccounts.init(api);
     this.block.height = await api.Provider.getBlockNumber(); // TODO: Fix the global eth block height setting
     await super.initApi();
