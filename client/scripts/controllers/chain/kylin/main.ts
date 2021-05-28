34328794721586b0250466201c21c5a62582f862
zakhap <zhs2104@columbia.edu>
2021-05-28 16:40:14 -0700

    Add main controller and adapter spec, spec from their generated, not finalized

---
+++ client/scripts/controllers/chain/kylin/main.ts
@@ -0,0 +1,16 @@
+import { ChainClass, NodeInfo } from 'models';
+import { IApp } from 'state';
+import KylinSpec from 'adapters/chain/kylin/spec';
+import Substrate from '../substrate/main';
+
+class Crust extends Substrate {
+  constructor(n: NodeInfo, app: IApp) {
+    super(n, app, ChainClass.Kylin);
+  }
+
+  public async initApi() {
+    await super.initApi(KylinSpec);
+  }
+}
+
+export default Crust;
