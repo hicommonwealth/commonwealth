451a6b908c3666784dd4d6983f1021d496aad3ac
Jake Naviasky <jake@commonwealth.im>
2021-06-09 12:22:50 -0400

    Factor out magic auth into separate route for mobile.

--- server/passport.ts
+++ server/passport.ts
@@ -2,21 +2,16 @@ import passport from 'passport';
 import passportGithub from 'passport-github';
 import passportJWT from 'passport-jwt';
 import { Request } from 'express';
-import request from 'superagent';
 
-import { encodeAddress } from '@polkadot/util-crypto';
-
-import { Magic, MagicUserMetadata } from '@magic-sdk/admin';
+import { Magic } from '@magic-sdk/admin';
 import { Strategy as MagicStrategy } from 'passport-magic';
 
-import { sequelize } from './database';
+import { authenticateMagicLink } from './util/magicLink';
 import { factory, formatFilename } from '../shared/logging';
 const log = factory.getLogger(formatFilename(__filename));
 
-
 import {
-  JWT_SECRET, GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET, GITHUB_OAUTH_CALLBACK, MAGIC_API_KEY, MAGIC_SUPPORTED_BASES,
-  MAGIC_DEFAULT_CHAIN
+  JWT_SECRET, GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET, GITHUB_OAUTH_CALLBACK, MAGIC_DEFAULT_CHAIN
 } from './config';
 import { NotificationCategories } from '../shared/types';
 import lookupCommunityIsVisibleToUser from './util/lookupCommunityIsVisibleToUser';
@@ -25,7 +20,7 @@ const GithubStrategy = passportGithub.Strategy;
 const JWTStrategy = passportJWT.Strategy;
 const ExtractJWT = passportJWT.ExtractJwt;
 
-function setupPassport(models) {
+function setupPassport(models, magic?: Magic) {
   passport.use(new JWTStrategy({
     jwtFromRequest: ExtractJWT.fromExtractors([
       ExtractJWT.fromBodyField('jwt'),
@@ -49,9 +44,8 @@ function setupPassport(models) {
   }));
 
   // allow magic login if configured with key
-  if (MAGIC_API_KEY) {
+  if (magic) {
     // TODO: verify we are in a community that supports magic login
-    const magic = new Magic(MAGIC_API_KEY);
     passport.use(new MagicStrategy({ passReqToCallback: true }, async (req, user, cb) => {
       // determine login location
       let chain, community, error;
@@ -67,188 +61,23 @@ function setupPassport(models) {
         registrationChain = await models.Chain.findOne({ where: { id: chainId } });
       }
 
-      // fetch user data from magic backend
-      let userMetadata: MagicUserMetadata;
-      try {
-        userMetadata = await magic.users.getMetadataByIssuer(user.issuer);
-      } catch (e) {
-        return cb(new Error('Magic fetch failed.'));
+      const result = await authenticateMagicLink(
+        models,
+        magic,
+        user,
+        registrationChain,
+        { chain: req.body.chain, community: req.body.community },
+      );
+      if (result.error) {
+        return cb(new Error(result.error));
       }
-
-      // check if this is a new signup or a login
-      const existingUser = await models.User.scope('withPrivateData').findOne({
-        where: {
-          email: userMetadata.email,
-        },
-        include: [{
-          model: models.Address,
-          where: { is_magic: true },
-          required: false,
-        }],
-      });
-
-      // unsupported chain -- client should send through old email flow
-      if (!existingUser && (!registrationChain?.base || !MAGIC_SUPPORTED_BASES.includes(registrationChain.base))) {
-        return cb(new Error('Unsupported magic chain.'));
+      if (result.user) {
+        return cb(null, result.user, { message: result.message });
       }
-
-      if (!existingUser) {
-        const ethAddress = userMetadata.publicAddress;
-        let polkadotAddress;
-
-        // always retrieve the polkadot address for the user regardless of chain
-        try {
-          const polkadotResp = await request
-            // eslint-disable-next-line max-len
-            .get(`https://api.magic.link/v1/admin/auth/user/public/address/get?issuer=did:ethr:${userMetadata.publicAddress}`)
-            .set('X-Magic-Secret-key', MAGIC_API_KEY)
-            .accept('json');
-          if (polkadotResp.body?.status !== 'ok') {
-            throw new Error(polkadotResp.body?.message || 'Failed to fetch polkadot address');
-          }
-          const polkadotRespAddress = polkadotResp.body?.data?.public_address;
-
-          // convert to chain-specific address based on ss58 prefix, if we are on a specific
-          // polkadot chain. otherwise, encode to edgeware.
-          if (registrationChain.ss58_prefix) {
-            polkadotAddress = encodeAddress(polkadotRespAddress, registrationChain.ss58_prefix);
-          } else {
-            polkadotAddress = encodeAddress(polkadotRespAddress, 7); // edgeware SS58 prefix
-          }
-        } catch (err) {
-          return cb(new Error(err.message));
-        }
-
-        const result = await sequelize.transaction(async (t) => {
-          // create new user and unverified address if doesn't exist
-          const newUser = await models.User.create({
-            email: userMetadata.email,
-            emailVerified: true,
-            magicIssuer: userMetadata.issuer,
-            lastMagicLoginAt: user.claim.iat,
-          }, { transaction: t });
-
-          // create an address on their selected chain
-          let newAddress;
-          if (registrationChain.base === 'substrate') {
-            newAddress = await models.Address.create({
-              address: polkadotAddress,
-              chain: registrationChain.id,
-              verification_token: 'MAGIC',
-              verification_token_expires: null,
-              verified: new Date(), // trust addresses from magic
-              last_active: new Date(),
-              user_id: newUser.id,
-              is_magic: true,
-            }, { transaction: t });
-
-            // if they selected a substrate chain, create an additional address on ethereum
-            // and auto-add them to the eth forum
-            const ethAddressInstance = await models.Address.create({
-              address: ethAddress,
-              chain: 'ethereum',
-              verification_token: 'MAGIC',
-              verification_token_expires: null,
-              verified: new Date(), // trust addresses from magic
-              last_active: new Date(),
-              user_id: newUser.id,
-              is_magic: true,
-            }, { transaction: t });
-
-            await models.Role.create({
-              address_id: ethAddressInstance.id,
-              chain_id: 'ethereum',
-              permission: 'member',
-            });
-          } else {
-            newAddress = await models.Address.create({
-              address: ethAddress,
-              chain: registrationChain.id,
-              verification_token: 'MAGIC',
-              verification_token_expires: null,
-              verified: new Date(), // trust addresses from magic
-              last_active: new Date(),
-              user_id: newUser.id,
-              is_magic: true,
-            }, { transaction: t });
-
-            // if they selected an eth chain, create an additional address on edgeware
-            // and auto-add them to the forum
-            const edgewareAddressInstance = await models.Address.create({
-              address: polkadotAddress,
-              chain: 'edgeware',
-              verification_token: 'MAGIC',
-              verification_token_expires: null,
-              verified: new Date(), // trust addresses from magic
-              last_active: new Date(),
-              user_id: newUser.id,
-              is_magic: true,
-            }, { transaction: t });
-
-            await models.Role.create({
-              address_id: edgewareAddressInstance.id,
-              chain_id: 'edgeware',
-              permission: 'member',
-            }, { transaction: t });
-          }
-
-          if (req.body.chain || req.body.community) await models.Role.create(req.body.community ? {
-            address_id: newAddress.id,
-            offchain_community_id: req.body.community,
-            permission: 'member',
-          } : {
-            address_id: newAddress.id,
-            chain_id: req.body.chain,
-            permission: 'member',
-          }, { transaction: t });
-
-          // Automatically create subscription to their own mentions
-          await models.Subscription.create({
-            subscriber_id: newUser.id,
-            category_id: NotificationCategories.NewMention,
-            object_id: `user-${newUser.id}`,
-            is_active: true,
-          }, { transaction: t });
-
-          // Automatically create a subscription to collaborations
-          await models.Subscription.create({
-            subscriber_id: newUser.id,
-            category_id: NotificationCategories.NewCollaboration,
-            object_id: `user-${newUser.id}`,
-            is_active: true,
-          }, { transaction: t });
-
-          return newUser;
-        });
-
-        // re-fetch user to include address object
-        // TODO: simplify this without doing a refetch
-        const newUser = await models.User.findOne({
-          where: {
-            id: result.id,
-          },
-          include: [ models.Address ],
-        });
-        return cb(null, newUser);
-      } else if (existingUser.Addresses) {
-        // login user if they registered via magic
-        if (user.claim.iat <= existingUser.lastMagicLoginAt) {
-          console.log('Replay attack detected.');
-          return cb(null, null, {
-            message: `Replay attack detected for user ${user.issuer}}.`,
-          });
-        }
-        existingUser.lastMagicLoginAt = user.claim.iat;
-        await existingUser.save();
-        console.log(`Found existing user: ${JSON.stringify(existingUser)}`);
-        return cb(null, existingUser);
-      } else {
-        // error if email exists but not registered with magic
-        console.log('User already registered with old method.');
-        return cb(null, null, {
-          message: `Email for user ${user.issuer} already registered`
-        });
+      if (result.message) {
+        return cb(null, null, { message: result.message });
       }
+      return cb(new Error('Unknown magic link failure.'));
     }));
   }
 
