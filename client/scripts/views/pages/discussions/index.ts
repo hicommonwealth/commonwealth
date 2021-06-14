525aea7f0a3fa64d25ed11dc59948acf3e5d94c7
Suspended Reason <graham@commonwealth.im>
2021-06-14 11:11:24 -0500

    Fix build error

--- client/scripts/views/pages/discussions/index.ts
+++ client/scripts/views/pages/discussions/index.ts
@@ -90,9 +90,9 @@ export const CommunityOptionsPopover: m.Component<{ isAdmin: boolean, isMod: boo
           onclick: (e) => m.route.set(`/${app.activeId()}/analytics`),
         }),
       ],
-    }),
+    });
   }
-}
+};
 
 const DiscussionStagesBar: m.Component<{ topic: string, stage: string }, {}> = {
   view: (vnode) => {
