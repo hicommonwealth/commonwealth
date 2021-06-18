c26c65df1b6ba54dbadcfbba9d4bed169eec70ed
Jake Naviasky <jake@commonwealth.im>
2021-06-18 17:06:14 -0400

    Fix snapshot dependency inclusion.

--- client/scripts/helpers/snapshot_utils/snapshot_client.ts
+++
@@ -1,6 +0,0 @@
-import Client from '@snapshot-labs/snapshot.js/src/client';
-
-const hubUrl = process.env.SNAPSHOT_HUB_URL || 'https://testnet.snapshot.org';
-const snapshotClient = new Client(hubUrl);
-
-export default snapshotClient;
