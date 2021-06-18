faabbb814fc4cbf4a97102194014060bfe217928
Jake Naviasky <jake@commonwealth.im>
2021-06-18 14:37:04 -0400

    Bump versions and add dydx ropsten migration.

--- server/migrations/20210601151012-add-aave-chain.js
+++ server/migrations/20210601151012-add-aave-chain.js
@@ -14,15 +14,15 @@ module.exports = {
         active: true,
         collapsed_on_homepage: false,
       }, {
-        id: 'aave-testnet',
-        symbol: 'TESTAAVE',
-        name: 'Aave Testnet (Kovan)',
-        icon_url: '/static/img/protocols/aave.png',
+        id: 'dydx-ropsten',
+        network: 'aave',
+        symbol: 'DYDX',
+        name: 'DYDX (ropsten)',
+        icon_url: '/static/img/protocols/dydx.jpg',
+        active: true,
         type: 'dao',
-        network: 'aave-testnet',
         base: 'ethereum',
-        active: false,
-        collapsed_on_homepage: true,
+        collapsed_on_homepage: false,
       }
       ], { transaction: t });
 
@@ -32,9 +32,9 @@ module.exports = {
         address: '0xEC568fffba86c094cf06b22134B23074DFE2252c', // Governance
       },
       {
-        chain: 'aave-testnet',
-        url: 'wss://kovan.infura.io/ws',
-        address: '0xc2ebab3bac8f2f5028f5c7317027a41ebfca31d2', // Governance
+        chain: 'dydx-ropsten',
+        url: 'wss://ropsten.infura.io/ws',
+        address: '0x6938240Ba19cB8a614444156244b658f650c8D5c', // Governance
       }
       ], { transaction: t });
     });
@@ -50,13 +50,13 @@ module.exports = {
       await queryInterface.bulkDelete('ChainNodes', { chain: 'aave' }, { transaction: t });
       await queryInterface.bulkDelete('Chains', { id: ['aave'] }, { transaction: t });
 
-      await queryInterface.bulkDelete('OffchainReactions', { chain: 'aave-testnet' }, { transaction: t });
-      await queryInterface.bulkDelete('OffchainComments', { chain: 'aave-testnet' }, { transaction: t });
-      await queryInterface.bulkDelete('OffchainThreads', { chain: 'aave-testnet' }, { transaction: t });
-      await queryInterface.bulkDelete('Addresses', { chain: 'aave-testnet' }, { transaction: t });
-      await queryInterface.bulkDelete('ChainEventTypes', { chain: 'aave-testnet' }, { transaction: t });
-      await queryInterface.bulkDelete('ChainNodes', { chain: 'aave-testnet' }, { transaction: t });
-      await queryInterface.bulkDelete('Chains', { id: ['aave-testnet'] }, { transaction: t });
+      await queryInterface.bulkDelete('OffchainReactions', { chain: 'dydx-ropsten' }, { transaction: t });
+      await queryInterface.bulkDelete('OffchainComments', { chain: 'dydx-ropsten' }, { transaction: t });
+      await queryInterface.bulkDelete('OffchainThreads', { chain: 'dydx-ropsten' }, { transaction: t });
+      await queryInterface.bulkDelete('Addresses', { chain: 'dydx-ropsten' }, { transaction: t });
+      await queryInterface.bulkDelete('ChainEventTypes', { chain: 'dydx-ropsten' }, { transaction: t });
+      await queryInterface.bulkDelete('ChainNodes', { chain: 'dydx-ropsten' }, { transaction: t });
+      await queryInterface.bulkDelete('Chains', { id: ['dydx-ropsten'] }, { transaction: t });
     });
   }
 };
