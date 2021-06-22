fe1b1b9ae524c68b76712dbec618dd50fd84c09f
James Truong <jamestruong.consult@gmail.com>
2021-06-22 22:08:05 +1000

    encode substrate address on the server side

--- server/routes/verifyAddress.ts
+++ server/routes/verifyAddress.ts
@@ -1,6 +1,8 @@
 import { Request, Response, NextFunction } from 'express';
 import { factory, formatFilename } from '../../shared/logging';
 import { DynamicTemplate } from '../../shared/types';
+import AddressSwapper from '../util/addressSwapper';
+
 const sgMail = require('@sendgrid/mail');
 const log = factory.getLogger(formatFilename(__filename));
 
@@ -34,8 +36,12 @@ const verifyAddress = async (models, req: Request, res: Response, next: NextFunc
     return next(new Error(Errors.InvalidChain));
   }
 
+  const encodedAddress = chain.base === 'substrate'
+    ? AddressSwapper({ address: req.body.address, currentPrefix: chain.ss58_prefix })
+    : req.body.address;
+
   const existingAddress = await models.Address.scope('withPrivateData').findOne({
-    where: { chain: req.body.chain, address: req.body.address }
+    where: { chain: req.body.chain, address: encodedAddress }
   });
   if (!existingAddress) {
     return next(new Error(Errors.AddressNF));
@@ -90,7 +96,7 @@ const verifyAddress = async (models, req: Request, res: Response, next: NextFunc
     } else {
       // if user isn't logged in, log them in now
       const newAddress = await models.Address.findOne({
-        where: { chain: req.body.chain, address: req.body.address },
+        where: { chain: req.body.chain, address: encodedAddress },
       });
       const user = await models.User.scope('withPrivateData').findOne({
         where: { id: newAddress.user_id },
