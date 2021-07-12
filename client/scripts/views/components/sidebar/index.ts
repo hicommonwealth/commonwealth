12ea63e4c436e63a3bc9bd0c15d18cf00b107b50
Suspended Reason <graham@commonwealth.im>
2021-07-12 14:56:30 -0500

    Switch off oninit lifecycle; move submit proposal button to new proposal button

--- client/scripts/views/components/sidebar/index.ts
+++ client/scripts/views/components/sidebar/index.ts
@@ -177,11 +177,6 @@ export const OnchainNavigationModule: m.Component<{}, {}> = {
     const showCommonwealthMenuOptions = app.chain?.network === ChainNetwork.Commonwealth;
 
     const showMarlinOptions = app.user.activeAccount && app.chain?.network === ChainNetwork.Marlin;
-    const showSubmitSnapshotProposalOptions = app.user.activeAccount && app.chain?.meta.chain.snapshot && 
-      (app.chain?.network === ChainNetwork.Yearn ||
-        app.chain?.network === ChainNetwork.Fei ||
-        app.chain?.network === ChainNetwork.Sushi) ||
-        app.chain?.network === ChainNetwork.Demo;
 
     const onSnapshotProposal = (p) => p.startsWith(`/${app.activeId()}/snapshot-proposals`);
     const onProposalPage = (p) => (
@@ -395,15 +390,6 @@ export const OnchainNavigationModule: m.Component<{}, {}> = {
           e.preventDefault();
           m.route.set(`/${app.activeChainId()}/snapshot-proposals/${app.chain.meta.chain.snapshot}`);
         },
-      }),
-      showSubmitSnapshotProposalOptions && m(Button, {
-        fluid: true,
-        rounded: true,
-        onclick: (e) => {
-          e.preventDefault();
-          m.route.set(`/${app.activeChainId()}/new/snapshot-proposal/${app.chain.meta.chain.snapshot}`);
-        },
-        label: 'Submit a Proposal',
       }),
       showCommonwealthMenuOptions && m(Button, {
         fluid: true,
