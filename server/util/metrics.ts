2d533dfb9fe809295b40ad00b889214e4e4ffc24
Jake Naviasky <jake@commonwealth.im>
2021-07-29 14:24:17 -0400

    Revert "Skip metrics creation if env vars undefined."
    This reverts commit 656f98cd7ec38eab3940a8a8877ab6057086cc92.

--- server/util/metrics.ts
+++ server/util/metrics.ts
@@ -3,7 +3,7 @@ const { HG_API_KEY, STATSD_URL } = process.env;
 
 let statsDInstance;
 export const getStatsDInstance = () => {
-  if (!statsDInstance && HG_API_KEY && STATSD_URL) {
+  if (!statsDInstance) {
     statsDInstance = new StatsD({ host: STATSD_URL, prefix: HG_API_KEY });
   }
   return statsDInstance;
