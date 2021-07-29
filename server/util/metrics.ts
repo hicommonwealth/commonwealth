656f98cd7ec38eab3940a8a8877ab6057086cc92
Jake Naviasky <jake@commonwealth.im>
2021-07-29 13:53:12 -0400

    Skip metrics creation if env vars undefined.

--- server/util/metrics.ts
+++ server/util/metrics.ts
@@ -3,7 +3,7 @@ const { HG_API_KEY, STATSD_URL } = process.env;
 
 let statsDInstance;
 export const getStatsDInstance = () => {
-  if (!statsDInstance) {
+  if (!statsDInstance && HG_API_KEY && STATSD_URL) {
     statsDInstance = new StatsD({ host: STATSD_URL, prefix: HG_API_KEY });
   }
   return statsDInstance;
