b09272bf3f9d9bcde3fac88a72bef6df86afecd8
Raymond Zhong <raykyri@users.noreply.github.com>
2021-07-30 13:15:26 -0700

    General custom domain support (#1266)
    * Cross-domain Github login
    
    When logging into Github from a custom domain, redirect the user back to that domain.
    
    Configure edgeware to use a custom domain at `gov2.edgewa.re`.
    
    Add an entry to /etc/hosts:
    ```
    127.0.0.1 gov2.edgewa.re
    ```
    
    Run a local SSL proxy:
    ```
    npm install -g local-ssl-proxy
    local-ssl-proxy --source 443 --target 8080
    ```
    
    Go to gov2.edgewa.re and log into Github from that domain and note
    that you are redirected back to the domain. Note that this PR disables
    the ability for users to set custom domains.
    
    * Fix email login for alt domains
    
    * alternate finishOAuthLogin flow for github logins, to use a token to claim a session from a subdomain
    
    * add finishOAuthLogin to handle post-OAuth redirects. pass a token to the subdomain for the user to claim a session
    
    * associate socialaccount with logintoken before redirect
    
    * Remove dead code
    
    * Add comment
    
    * Add comment
    
    * Don't redirect to https for hardcoded custom domains on dev (because we are using local ssh proxies now)

---
+++ server/migrations/20210727013558-add-domain-to-login-tokens.js
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.addColumn('LoginTokens', 'domain', {
+      type: Sequelize.STRING,
+      allowNull: true
+    });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.removeColumn('LoginTokens', 'domain');
+  }
+};
