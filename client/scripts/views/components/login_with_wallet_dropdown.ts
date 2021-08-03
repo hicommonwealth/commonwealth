a7de6ab85da0a3e0ceebcf93f5f6c99ee173a398
raymondz <raykyri@gmail.com>
2021-08-02 22:10:20 -0700

    Fix login from non-community page

--- client/scripts/views/components/login_with_wallet_dropdown.ts
+++ client/scripts/views/components/login_with_wallet_dropdown.ts
@@ -64,7 +64,11 @@ const LoginWithWalletDropdown: m.Component<{
         onclick: (e) => {
           $('.Login').trigger('modalexit');
           const defaultChainId = baseToNetwork(base);
-          navigateToSubpage('/web3login', web3loginParams);
+          if (app.activeId()) {
+            navigateToSubpage('/web3login', web3loginParams);
+          } else {
+            m.route.set(`${defaultChainId}/web3login`, web3loginParams);
+          }
           app.modals.lazyCreate('link_new_address_modal', {
             loggingInWithAddress,
             joiningChain,
