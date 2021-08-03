9ab57fd10edf18a88bfd053f4ba6edae2acccab3
zakhap <zhs2104@columbia.edu>
2021-08-03 11:08:26 -0400

    Gut snapshot

--- client/scripts/views/pages/view_proposal/index.ts
+++ client/scripts/views/pages/view_proposal/index.ts
@@ -179,14 +179,6 @@ const ProposalHeader: m.Component<{
                     && m(ProposalBodyDeleteMenuItem, { item: proposal }),
                   (isAuthor || isAdmin)
                     && m(ProposalHeaderPrivacyMenuItems, { proposal, getSetGlobalEditingStatus }),
-                  (isAuthor || isAdmin) && (app.chain?.meta.chain.snapshot !== null)
-                    && m(MenuItem, {
-                      onclick: (e) => {
-                        m.route.set(`/${app.activeChainId()}/new/snapshot-proposal/${app.chain.meta.chain.snapshot}`
-                        + `?fromProposalType=${proposal.slug}&fromProposalId=${proposal.id}`);
-                      },
-                      label: 'Snapshot proposal from thread',
-                    }),
                   (isAuthor || isAdmin)
                     && m(MenuDivider),
                   m(ThreadSubscriptionMenuItem, { proposal: proposal as OffchainThread }),
