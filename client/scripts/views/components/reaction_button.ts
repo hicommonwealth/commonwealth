80f9a52cdf0f34ddc632d6d231bef376428d12c5
Suspended Reason <graham@commonwealth.im>
2021-08-26 14:44:45 -0500

    REnable reaction button on token comms, copy fix, rm duplicate form field

--- client/scripts/views/components/reaction_button.ts
+++ client/scripts/views/components/reaction_button.ts
@@ -34,10 +34,7 @@ const ReactionButton: m.Component<{
     if (type === ReactionType.Like) likes = reactions.filter((r) => r.reaction === 'like');
     if (type === ReactionType.Dislike) dislikes = reactions.filter((r) => r.reaction === 'dislike');
 
-    const isCommunity = !!app.activeCommunityId();
-
-    const disabled = vnode.state.loading
-      || (!isCommunity && (app.chain as Token).isToken && !(app.chain as Token).hasToken);
+    const disabled = vnode.state.loading;
     const activeAddress = app.user.activeAccount?.address;
     const rxn = reactions.find((r) => r.reaction && r.author === activeAddress);
     const hasReacted : boolean = !!rxn;
