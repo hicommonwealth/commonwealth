354da68cd7595f03fda67397118f10d6b3520684
Suspended Reason <graham@commonwealth.im>
2021-09-08 17:06:52 -0500

    Card, topic, and sidebar fixes

--- client/scripts/views/components/sidebar/index.ts
+++ client/scripts/views/components/sidebar/index.ts
@@ -610,14 +610,14 @@ const Sidebar: m.Component<{ hideQuickSwitcher? }, {}> = {
         m('br'),
         app.isLoggedIn() && (app.chain || app.community) && m(SubscriptionButton),
         app.chain && m(ChainStatusModule),
+        app.isCustomDomain()
+        && m('a', {
+          class: 'PoweredBy',
+          onclick: (e) => {
+            window.open('https://commonwealth.im/');
+          },
+        }),
       ]),
-      app.isCustomDomain()
-      && m('a', {
-        class: 'PoweredBy',
-        onclick: (e) => {
-          window.open('https://commonwealth.im/');
-        },
-      }),
     ];
   },
 };
