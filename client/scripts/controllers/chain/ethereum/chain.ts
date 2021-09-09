e6fc5c65245d52faf3cef5727a7e68d0eba6c805
Jake Naviasky <jake@commonwealth.im>
2021-09-09 10:41:51 -0400

    Swap infura account back.

--- client/scripts/controllers/chain/ethereum/chain.ts
+++ client/scripts/controllers/chain/ethereum/chain.ts
@@ -12,7 +12,7 @@ import {
 import { EthereumCoin } from 'adapters/chain/ethereum/types';
 import EthereumAccount from './account';
 
-export const INFURA_ID = process.env.INFURA_ID || 'b19b8175e688448ead43a0ab5f03438a';
+export const INFURA_ID = process.env.INFURA_API_KEY || '90de850aff68424ab8e7321017406586';
 const ETHEREUM_BLOCK_TIME = 15;
 export interface IEthereumTXData extends ITXData {
   chainId: string;
