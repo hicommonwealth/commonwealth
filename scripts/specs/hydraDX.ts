0f8a8e95269e90ef4e5ffac6b3455883d68315fd
zakhap <zhs2104@columbia.edu>
2021-09-20 13:38:40 -0700

    Bump polkadot versions and hydradx spec

--- scripts/specs/hydraDX.ts
+++ scripts/specs/hydraDX.ts
@@ -1,45 +1,50 @@
 export const HydraDXSpec = {
   types: {
-    Fee: {
-      numerator: 'u32',
-      denominator: 'u32',
+    AssetPair: {
+      asset_in: 'AssetId',
+      asset_out: 'AssetId',
+    },
+    Amount: 'i128',
+    AmountOf: 'Amount',
+    Address: 'AccountId',
+    OrmlAccountData: {
+      free: 'Balance',
+      frozen: 'Balance',
+      reserved: 'Balance',
+    },
+    BalanceInfo: {
+      amount: 'Balance',
+      assetId: 'AssetId',
     },
     Chain: {
       genesisHash: 'Vec<u8>',
       lastBlockHash: 'Vec<u8>',
     },
-    Price: 'Balance',
-    Amount: 'i128',
-    Address: 'AccountId',
-    AmountOf: 'Amount',
-    AssetPair: {
-      asset_in: 'AssetId',
-      asset_out: 'AssetId',
-    },
+    CurrencyId: 'AssetId',
+    CurrencyIdOf: 'AssetId',
     Intention: {
       who: 'AccountId',
+      asset_sell: 'AssetId',
+      asset_buy: 'AssetId',
       amount: 'Balance',
       discount: 'bool',
-      asset_buy: 'AssetId',
-      asset_sell: 'AssetId',
       sell_or_buy: 'IntentionType',
     },
-    CurrencyId: 'AssetId',
-    OrderedSet: 'Vec<AssetId>',
-    BalanceInfo: {
-      amount: 'Balance',
-      assetId: 'AssetId',
-    },
     IntentionId: 'Hash',
-    CurrencyIdOf: 'AssetId',
-    LookupSource: 'AccountId',
     IntentionType: {
       _enum: ['SELL', 'BUY'],
     },
-    OrmlAccountData: {
-      free: 'Balance',
-      frozen: 'Balance',
-      reserved: 'Balance',
+    LookupSource: 'AccountId',
+    OrderedSet: 'Vec<AssetId>',
+    Price: 'Balance',
+    Fee: {
+      numerator: 'u32',
+      denominator: 'u32',
+    },
+  },
+  alias: {
+    tokens: {
+      AccountData: 'OrmlAccountData',
     },
   },
 };
