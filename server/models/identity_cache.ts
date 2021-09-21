0a0be46ee53a504b8c4612303288abd7cdd1306c
Timothee Legros <62490329+timolegros@users.noreply.github.com>
2021-09-21 11:44:39 -0600

    identity cache error fix (#1433)
    * identity cache error fix
    
    * remove test exclusion

--- server/models/identity_cache.ts
+++ server/models/identity_cache.ts
@@ -26,6 +26,9 @@ export default (
     { timestamps: false }
   );
 
+  // we don't define a primary key so sequelize assumes a primary key on column "id" so this removes that assumption
+  IdentityCache.removeAttribute('id');
+
   IdentityCache.associate = (models) => {
     models.IdentityCache.belongsTo(models.Chain, { foreignKey: 'chain', targetKey: 'id' });
   };
