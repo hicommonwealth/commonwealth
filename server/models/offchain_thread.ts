1b1611a6d9d7e4f9e668cf73e7c49dee043991fb
Suspended Reason <graham@commonwealth.im>
2021-10-11 12:47:41 -0500

    Set up linkedthreads table

--- server/models/offchain_thread.ts
+++ server/models/offchain_thread.ts
@@ -37,69 +37,93 @@ export interface OffchainThreadAttributes {
   Chain?: ChainAttributes;
   OffchainCommunity?: OffchainCommunityAttributes;
   Address?: AddressAttributes;
-  OffchainAttachments?: OffchainAttachmentAttributes[] | OffchainAttachmentAttributes['id'][];
+  OffchainAttachments?:
+    | OffchainAttachmentAttributes[]
+    | OffchainAttachmentAttributes['id'][];
   ChainEntity?: ChainEntityAttributes;
-  collaborators?: AddressAttributes[]
+  collaborators?: AddressAttributes[];
 }
 
-export interface OffchainThreadInstance extends Model<OffchainThreadAttributes>, OffchainThreadAttributes {
+export interface OffchainThreadInstance
+  extends Model<OffchainThreadAttributes>,
+    OffchainThreadAttributes {
   // no mixins used
 }
 
-export type OffchainThreadModelStatic = ModelStatic<OffchainThreadInstance>
+export type OffchainThreadModelStatic = ModelStatic<OffchainThreadInstance>;
 
 export default (
   sequelize: Sequelize.Sequelize,
-  dataTypes: typeof DataTypes,
+  dataTypes: typeof DataTypes
 ): OffchainThreadModelStatic => {
-  const OffchainThread = <OffchainThreadModelStatic>sequelize.define('OffchainThread', {
-    id: { type: dataTypes.INTEGER, autoIncrement: true, primaryKey: true },
-    address_id: { type: dataTypes.INTEGER, allowNull: false },
-    title: { type: dataTypes.TEXT, allowNull: false },
-    body: { type: dataTypes.TEXT, allowNull: true },
-    plaintext: { type: dataTypes.TEXT, allowNull: true },
-    kind: { type: dataTypes.TEXT, allowNull: false },
-    stage: { type: dataTypes.TEXT, allowNull: false, defaultValue: 'discussion' },
-    url: { type: dataTypes.TEXT, allowNull: true },
-    topic_id: { type: dataTypes.INTEGER, allowNull: true },
-    pinned: { type: dataTypes.BOOLEAN, defaultValue: false, allowNull: false },
-    chain: { type: dataTypes.STRING, allowNull: true },
-    community: { type: dataTypes.STRING, allowNull: true },
-    read_only: { type: dataTypes.BOOLEAN, allowNull: false, defaultValue: false },
-    version_history: { type: dataTypes.ARRAY(dataTypes.TEXT), defaultValue: [], allowNull: false },
-    snapshot_proposal: { type: dataTypes.STRING(48), allowNull: true },
+  const OffchainThread = <OffchainThreadModelStatic>sequelize.define(
+    'OffchainThread',
+    {
+      id: { type: dataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+      address_id: { type: dataTypes.INTEGER, allowNull: false },
+      title: { type: dataTypes.TEXT, allowNull: false },
+      body: { type: dataTypes.TEXT, allowNull: true },
+      plaintext: { type: dataTypes.TEXT, allowNull: true },
+      kind: { type: dataTypes.TEXT, allowNull: false },
+      stage: {
+        type: dataTypes.TEXT,
+        allowNull: false,
+        defaultValue: 'discussion',
+      },
+      url: { type: dataTypes.TEXT, allowNull: true },
+      topic_id: { type: dataTypes.INTEGER, allowNull: true },
+      pinned: {
+        type: dataTypes.BOOLEAN,
+        defaultValue: false,
+        allowNull: false,
+      },
+      chain: { type: dataTypes.STRING, allowNull: true },
+      community: { type: dataTypes.STRING, allowNull: true },
+      read_only: {
+        type: dataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
+      },
+      version_history: {
+        type: dataTypes.ARRAY(dataTypes.TEXT),
+        defaultValue: [],
+        allowNull: false,
+      },
+      snapshot_proposal: { type: dataTypes.STRING(48), allowNull: true },
 
-    offchain_voting_options: { type: dataTypes.STRING },
-    offchain_voting_ends_at: { type: dataTypes.DATE, allowNull: true },
-    offchain_voting_votes: { type: dataTypes.INTEGER, allowNull: true },
+      offchain_voting_options: { type: dataTypes.STRING },
+      offchain_voting_ends_at: { type: dataTypes.DATE, allowNull: true },
+      offchain_voting_votes: { type: dataTypes.INTEGER, allowNull: true },
 
-    created_at: { type: dataTypes.DATE, allowNull: false },
-    updated_at: { type: dataTypes.DATE, allowNull: false },
-    deleted_at: { type: dataTypes.DATE, allowNull: true },
-  }, {
-    timestamps: true,
-    createdAt: 'created_at',
-    updatedAt: 'updated_at',
-    deletedAt: 'deleted_at',
-    underscored: true,
-    tableName: 'OffchainThreads',
-    paranoid: true,
-    indexes: [
-      { fields: ['address_id'] },
-      { fields: ['chain'] },
-      { fields: ['community'] },
-      { fields: ['chain', 'created_at'] },
-      { fields: ['community', 'created_at'] },
-      { fields: ['chain', 'updated_at'] },
-      { fields: ['community', 'updated_at'] },
-      { fields: ['chain', 'pinned'] },
-      { fields: ['community', 'pinned'] },
-      { fields: ['chain', 'offchain_voting_ends_at'] },
-      { fields: ['community', 'offchain_voting_ends_at'] },
-      { fields: ['chain', 'offchain_voting_votes'] },
-      { fields: ['community', 'offchain_voting_votes'] },
-    ],
-  });
+      created_at: { type: dataTypes.DATE, allowNull: false },
+      updated_at: { type: dataTypes.DATE, allowNull: false },
+      deleted_at: { type: dataTypes.DATE, allowNull: true },
+    },
+    {
+      timestamps: true,
+      createdAt: 'created_at',
+      updatedAt: 'updated_at',
+      deletedAt: 'deleted_at',
+      underscored: true,
+      tableName: 'OffchainThreads',
+      paranoid: true,
+      indexes: [
+        { fields: ['address_id'] },
+        { fields: ['chain'] },
+        { fields: ['community'] },
+        { fields: ['chain', 'created_at'] },
+        { fields: ['community', 'created_at'] },
+        { fields: ['chain', 'updated_at'] },
+        { fields: ['community', 'updated_at'] },
+        { fields: ['chain', 'pinned'] },
+        { fields: ['community', 'pinned'] },
+        { fields: ['chain', 'offchain_voting_ends_at'] },
+        { fields: ['community', 'offchain_voting_ends_at'] },
+        { fields: ['chain', 'offchain_voting_votes'] },
+        { fields: ['community', 'offchain_voting_votes'] },
+      ],
+    }
+  );
 
   OffchainThread.associate = (models) => {
     models.OffchainThread.belongsTo(models.Chain, {
@@ -108,12 +132,12 @@ export default (
     });
     models.OffchainThread.belongsTo(models.OffchainCommunity, {
       foreignKey: 'community',
-      targetKey: 'id'
+      targetKey: 'id',
     });
     models.OffchainThread.belongsTo(models.Address, {
       as: 'Address',
       foreignKey: 'address_id',
-      targetKey: 'id'
+      targetKey: 'id',
     });
     models.OffchainThread.hasMany(models.OffchainAttachment, {
       foreignKey: 'attachment_id',
@@ -132,11 +156,11 @@ export default (
     });
     models.OffchainThread.belongsToMany(models.Address, {
       through: models.Collaboration,
-      as: 'collaborators'
+      as: 'collaborators',
     });
     models.OffchainThread.hasMany(models.OffchainReaction, {
       foreignKey: 'thread_id',
-      as: 'reactions'
+      as: 'reactions',
     });
     models.OffchainThread.hasMany(models.Collaboration);
     models.OffchainThread.hasMany(models.ChainEntity, {
@@ -146,6 +170,12 @@ export default (
     models.OffchainThread.hasMany(models.OffchainVote, {
       foreignKey: 'thread_id',
       constraints: false,
+    });
+    models.OffchainThread.hasMany(models.LinkedThread, {
+      foreignKey: 'linking_thread',
+    });
+    models.OffchainThread.hasMany(models.LinkedThread, {
+      foreignKey: 'linked_thread',
     });
   };
 
