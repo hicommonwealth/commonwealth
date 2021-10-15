dcb40fc57e995dc5fe85642f14a5b71cffa897e5
Adam Lawrence <adam.ad.lawrence@gmail.com>
2021-10-15 12:38:27 -0400

    fix - match test data with default db data

--- server/util/identityFetchCache.ts
+++ server/util/identityFetchCache.ts
@@ -6,10 +6,8 @@ import JobRunner from './cacheJobRunner';
 import { factory, formatFilename } from '../../shared/logging';
 const log = factory.getLogger(formatFilename(__filename));
 
-
-
 // list of identities to fetch
-type CacheT = { [chain: string]: string[] }
+type CacheT = { [chain: string]: string[] };
 
 export class IdentityFetchCacheNew {
   public async add(chain: string, address: string) {
@@ -26,12 +24,18 @@ export default class IdentityFetchCache extends JobRunner<CacheT> {
     super({}, _jobTimeS);
   }
 
-  public async start(models, fetchers: { [chain: string]: SubstrateEvents.StorageFetcher }) {
+  public async start(
+    models,
+    fetchers: { [chain: string]: SubstrateEvents.StorageFetcher }
+  ) {
     this._models = models;
     this._fetchers = fetchers;
     await this.access(async (c) => {
       // write empty arrays for all available chains
-      Object.keys(fetchers).reduce((res, chain) => Object.assign(c, { [chain]: [] }), c);
+      Object.keys(fetchers).reduce(
+        (res, chain) => Object.assign(c, { [chain]: [] }),
+        c
+      );
     });
 
     // kick off job
@@ -50,22 +54,26 @@ export default class IdentityFetchCache extends JobRunner<CacheT> {
     // for each chain, clear cache and fetch all events
     // TODO: part of this can be removed from the job proper, the only atomic piece needed
     //   is copying and clearing the address list for each chain
-    await Promise.all(Object.keys(c).map(async (chain) => {
-      try {
-        // fetch all identities for the chain
-        const identityEvents = await this._fetchers[chain].fetchIdentities(c[chain]);
+    await Promise.all(
+      Object.keys(c).map(async (chain) => {
+        try {
+          // fetch all identities for the chain
+          const identityEvents = await this._fetchers[chain].fetchIdentities(
+            c[chain]
+          );
 
-        // write the found identities back to db using the event handler
-        log.info(`Writing identities for chain ${chain} back to db...`);
-        const handler = new IdentityEventHandler(this._models, chain);
-        await Promise.all(identityEvents.map((e) => handler.handle(e, null)));
+          // write the found identities back to db using the event handler
+          log.info(`Writing identities for chain ${chain} back to db...`);
+          const handler = new IdentityEventHandler(this._models, chain);
+          await Promise.all(identityEvents.map((e) => handler.handle(e, null)));
 
-        // clear the cache for this chain
-        c[chain].splice(0, c[chain].length);
-        log.debug(`Succeeded in updating identities for ${chain}.`);
-      } catch (e) {
-        log.error(`Failed to update identities for ${chain}: ${e.message}!`);
-      }
-    }));
+          // clear the cache for this chain
+          c[chain].splice(0, c[chain].length);
+          log.debug(`Succeeded in updating identities for ${chain}.`);
+        } catch (e) {
+          log.error(`Failed to update identities for ${chain}: ${e.message}!`);
+        }
+      })
+    );
   }
 }
--- test/unit/api/identityCache.spec.ts
+++ test/unit/api/identityCache.spec.ts
@@ -4,15 +4,20 @@ import models from '../../../server/database';
 
 describe('New identity cache tests', () => {
   it('Should add an identity to the cache', async () => {
-    const cache = new IdentityFetchCacheNew();
-    await cache.add('polkadot', '0x30DB748Fc0E4667CD6494f208de453464cf314A5')
+    let cache;
+    try {
+      cache = new IdentityFetchCacheNew();
+    } catch (error) {
+      console.log(error);
+    }
+    await cache.add('edgeware', '0x30DB748Fc0E4667CD6494f208de453464cf314A5');
     const result = await models.IdentityCache.findOne({
       where: {
-        "chain": 'polkadot',
-        "address": '0x30DB748Fc0E4667CD6494f208de453464cf314A5'
-      }
-    })
-    assert(result.chain === 'polkadot');
+        chain: 'edgeware',
+        address: '0x30DB748Fc0E4667CD6494f208de453464cf314A5',
+      },
+    });
+    assert(result.chain === 'edgeware');
     assert(result.address === '0x30DB748Fc0E4667CD6494f208de453464cf314A5');
-  })
-})
+  });
+});
