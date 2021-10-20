aa48882e0b17752c886a14913a91b1f86b5e7f1c
Jake Naviasky <jake@commonwealth.im>
2021-10-20 11:23:50 -0400

    Support GovernorBravo. (#1461)
    * feat - import bravo contracts, initial deployment script
    
    * feat - deploy script to make proposal
    
    * Change token network to erc20 and update app init.
    
    * Remove EventChains and related logic and support network field.
    
    * Remove dead code.
    
    * Move chain enums to shared.
    
    * Revert "Remove dead code."
    
    This reverts commit efe2fe04651cc473719ba83b28a7c4ed65225197.
    
    * Fix build.
    
    * Add new Compound contracts.
    
    * Create deployOz.ts
    
    * Support GovBravoCompat layer.
    
    * Remove eth artifacts and cache.
    
    * Remove mocked immutable func.
    
    * Bravo 'works'.
    
    * Implement Alpha v Bravo and MPond v ERC20Votes discovery.
    
    * Add abstain + easier getters for features.
    
    * Re-add queue and execute buttons.
    
    * Clean up labeling in emails.
    
    * Fix notifications and queued state.
    
    * Remove yalc chain events.
    
    * Add proposal created toast.
    
    * Remove commented out code.
    
    Co-authored-by: Adam Lawrence <adam.ad.lawrence@gmail.com>
    Co-authored-by: zakhap <zhs2104@columbia.edu>

--- client/scripts/controllers/chain/ethereum/compound/api.ts
+++ client/scripts/controllers/chain/ethereum/compound/api.ts
@@ -1,48 +1,113 @@
-import { GovernorAlpha, MPond, MPond__factory } from 'eth/types';
+import {
+  GovernorCompatibilityBravo,
+  GovernorCompatibilityBravo__factory,
+  GovernorAlpha,
+  GovernorAlpha__factory,
+  MPond,
+  ERC20VotesComp,
+  MPond__factory,
+  ERC20VotesComp__factory,
+} from 'eth/types';
+import { Web3Provider, ExternalProvider } from '@ethersproject/providers';
 import { utils } from 'ethers';
 
 import ContractApi from 'controllers/chain/ethereum/contractApi';
 
-export default class CompoundAPI extends ContractApi<GovernorAlpha> {
-  private _Token: MPond;
+export default class CompoundAPI implements ContractApi<GovernorAlpha | GovernorCompatibilityBravo> {
+  public readonly gasLimit: number = 3000000;
+
+  public readonly contractAddress: string;
+  private _Contract: GovernorAlpha | GovernorCompatibilityBravo;
+  public get Contract() { return this._Contract; }
+  public readonly Provider: Web3Provider;
+
+  private _Token: MPond | ERC20VotesComp | undefined;
   public get Token() { return this._Token; }
 
-  // private _Timelock: Timelock;
-  // public get Timelock() { return this._Timelock; }
+  private _isAlpha: boolean;
+  private _isTokenMPond: boolean;
+  public isGovAlpha(c: GovernorAlpha | GovernorCompatibilityBravo): c is GovernorAlpha {
+    return this._isAlpha;
+  }
+  public isTokenMPond(t: MPond | ERC20VotesComp | undefined): t is MPond {
+    return this._isTokenMPond;
+  }
+
+  constructor(
+    _factory: any,
+    contractAddress: string,
+    web3Provider: ExternalProvider
+  ) {
+    this.contractAddress = contractAddress;
+    this.Provider = new Web3Provider(web3Provider);
+    // 12s minute polling interval (default is 4s)
+    this.Provider.pollingInterval = 12000;
+  }
 
   public async init(tokenName: string) {
-    await super.init();
+    // only Alpha has a guardian -- use to distinguish between types of governance contract
+    try {
+      this._Contract = GovernorAlpha__factory.connect(this.contractAddress, this.Provider);
+      await this.Contract.guardian();
+      this._isAlpha = true;
+      console.log(`Found GovAlpha contract at ${this.Contract.address}`);
+    } catch (e) {
+      this._isAlpha = false;
+      console.log(`Found non-GovAlpha Compound contract at ${this.Contract.address}, using GovernorCompatibilityBravo`);
+      this._Contract = GovernorCompatibilityBravo__factory.connect(this.contractAddress, this.Provider);
+    }
+    // TODO: use `quorumVotes` to distinguish between raw IGovernor and BravoCompat, for later support
+
+    // fetch token and derive token type
     // i.e. "uni" or "MPond" -- should refer to a call
-    if (tokenName) {
-      try {
-        // ABI hack to call arbitrarily named functions on GovAlpha contracts
-        const ABI = [
-          {
-            'inputs': [],
-            'name': tokenName,
-            'outputs': [
-              {
-                'name': '',
-                'type': 'address'
-              }
-            ],
-            'stateMutability': 'view',
-            'type': 'function'
-          }
-        ];
-        const iface = new utils.Interface(JSON.stringify(ABI));
-        const data = iface.encodeFunctionData(tokenName);
-        const resultData = await this.Contract.provider.call({ to: this.Contract.address, data });
-        const tokenAddress = utils.getAddress(Buffer.from(utils.stripZeros(resultData)).toString('hex'));
-        this._Token = MPond__factory.connect(tokenAddress, this.Contract.signer || this.Contract.provider);
-      } catch (err) {
-        console.error(`Could not fetch token ${tokenName}: ${err.message}`);
-      }
-    } else {
-      console.error('No token name found!');
+    let tokenAddress: string;
+
+    // `.token()` is the way to fetch the token address on GovBravo/related contracts,
+    // but since we have no guarantee we're on Bravo, we do it manually via the below ABI hack
+    if (!tokenName) {
+      tokenName = 'token';
     }
 
-    // const timelockAddress = await this.Contract.timelock();
-    // this._Timelock = Timelock__factory.connect(timelockAddress, this.Contract.signer || this.Contract.provider);
+    // attempt to query token via a provided function call name
+    try {
+      // ABI hack to call arbitrarily named functions on GovAlpha contracts
+      const ABI = [
+        {
+          'inputs': [],
+          'name': tokenName,
+          'outputs': [
+            {
+              'name': '',
+              'type': 'address'
+            }
+          ],
+          'stateMutability': 'view',
+          'type': 'function'
+        }
+      ];
+      const iface = new utils.Interface(JSON.stringify(ABI));
+      const data = iface.encodeFunctionData(tokenName);
+      const resultData = await this.Contract.provider.call({ to: this.Contract.address, data });
+      tokenAddress = utils.getAddress(Buffer.from(utils.stripZeros(resultData)).toString('hex'));
+    } catch (err) {
+      console.error(`Could not fetch token ${tokenName}: ${err.message}`);
+      return
+    }
+
+    // query token and determine token capabilities (MPond vs ERC20VotesComp)
+    if (tokenAddress) {
+      this._Token = MPond__factory.connect(tokenAddress, this.Contract.signer || this.Contract.provider);
+      try {
+        await this._Token.admin();
+        this._isTokenMPond = true;
+        console.log(`Found MPond-like Comp token at ${tokenAddress}`);
+      } catch (e) {
+        this._isTokenMPond = false;
+        console.log(`Found non-MPond Comp token at ${tokenAddress}, using ERC20VotesComp`);
+        this._Token = ERC20VotesComp__factory.connect(tokenAddress, this.Contract.signer || this.Contract.provider);
+      }
+    } else {
+      console.warn('No token contract found! Continuing...');
+    }
   }
 }
--- shared/eth/types/factories/IERC1820Registry__factory.ts
+++ shared/eth/types/factories/IERC1820Registry__factory.ts
@@ -18,185 +18,23 @@ export class IERC1820Registry__factory {
 
 const _abi = [
   {
-    constant: false,
-    inputs: [
-      {
-        name: "account",
-        type: "address",
-      },
-      {
-        name: "interfaceHash",
-        type: "bytes32",
-      },
-      {
-        name: "implementer",
-        type: "address",
-      },
-    ],
-    name: "setInterfaceImplementer",
-    outputs: [],
-    payable: false,
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-  {
-    constant: true,
-    inputs: [
-      {
-        name: "account",
-        type: "address",
-      },
-    ],
-    name: "getManager",
-    outputs: [
-      {
-        name: "",
-        type: "address",
-      },
-    ],
-    payable: false,
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    constant: false,
-    inputs: [
-      {
-        name: "account",
-        type: "address",
-      },
-      {
-        name: "newManager",
-        type: "address",
-      },
-    ],
-    name: "setManager",
-    outputs: [],
-    payable: false,
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-  {
-    constant: true,
-    inputs: [
-      {
-        name: "interfaceName",
-        type: "string",
-      },
-    ],
-    name: "interfaceHash",
-    outputs: [
-      {
-        name: "",
-        type: "bytes32",
-      },
-    ],
-    payable: false,
-    stateMutability: "pure",
-    type: "function",
-  },
-  {
-    constant: false,
-    inputs: [
-      {
-        name: "account",
-        type: "address",
-      },
-      {
-        name: "interfaceId",
-        type: "bytes4",
-      },
-    ],
-    name: "updateERC165Cache",
-    outputs: [],
-    payable: false,
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-  {
-    constant: true,
-    inputs: [
-      {
-        name: "account",
-        type: "address",
-      },
-      {
-        name: "interfaceHash",
-        type: "bytes32",
-      },
-    ],
-    name: "getInterfaceImplementer",
-    outputs: [
-      {
-        name: "",
-        type: "address",
-      },
-    ],
-    payable: false,
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    constant: true,
-    inputs: [
-      {
-        name: "account",
-        type: "address",
-      },
-      {
-        name: "interfaceId",
-        type: "bytes4",
-      },
-    ],
-    name: "implementsERC165InterfaceNoCache",
-    outputs: [
-      {
-        name: "",
-        type: "bool",
-      },
-    ],
-    payable: false,
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    constant: true,
-    inputs: [
-      {
-        name: "account",
-        type: "address",
-      },
-      {
-        name: "interfaceId",
-        type: "bytes4",
-      },
-    ],
-    name: "implementsERC165Interface",
-    outputs: [
-      {
-        name: "",
-        type: "bool",
-      },
-    ],
-    payable: false,
-    stateMutability: "view",
-    type: "function",
-  },
-  {
     anonymous: false,
     inputs: [
       {
         indexed: true,
+        internalType: "address",
         name: "account",
         type: "address",
       },
       {
         indexed: true,
+        internalType: "bytes32",
         name: "interfaceHash",
         type: "bytes32",
       },
       {
         indexed: true,
+        internalType: "address",
         name: "implementer",
         type: "address",
       },
@@ -209,16 +47,203 @@ const _abi = [
     inputs: [
       {
         indexed: true,
+        internalType: "address",
         name: "account",
         type: "address",
       },
       {
         indexed: true,
+        internalType: "address",
         name: "newManager",
         type: "address",
       },
     ],
     name: "ManagerChanged",
     type: "event",
+  },
+  {
+    constant: true,
+    inputs: [
+      {
+        internalType: "address",
+        name: "account",
+        type: "address",
+      },
+      {
+        internalType: "bytes32",
+        name: "interfaceHash",
+        type: "bytes32",
+      },
+    ],
+    name: "getInterfaceImplementer",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [
+      {
+        internalType: "address",
+        name: "account",
+        type: "address",
+      },
+    ],
+    name: "getManager",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [
+      {
+        internalType: "address",
+        name: "account",
+        type: "address",
+      },
+      {
+        internalType: "bytes4",
+        name: "interfaceId",
+        type: "bytes4",
+      },
+    ],
+    name: "implementsERC165Interface",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "",
+        type: "bool",
+      },
+    ],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [
+      {
+        internalType: "address",
+        name: "account",
+        type: "address",
+      },
+      {
+        internalType: "bytes4",
+        name: "interfaceId",
+        type: "bytes4",
+      },
+    ],
+    name: "implementsERC165InterfaceNoCache",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "",
+        type: "bool",
+      },
+    ],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [
+      {
+        internalType: "string",
+        name: "interfaceName",
+        type: "string",
+      },
+    ],
+    name: "interfaceHash",
+    outputs: [
+      {
+        internalType: "bytes32",
+        name: "",
+        type: "bytes32",
+      },
+    ],
+    payable: false,
+    stateMutability: "pure",
+    type: "function",
+  },
+  {
+    constant: false,
+    inputs: [
+      {
+        internalType: "address",
+        name: "account",
+        type: "address",
+      },
+      {
+        internalType: "bytes32",
+        name: "interfaceHash",
+        type: "bytes32",
+      },
+      {
+        internalType: "address",
+        name: "implementer",
+        type: "address",
+      },
+    ],
+    name: "setInterfaceImplementer",
+    outputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    constant: false,
+    inputs: [
+      {
+        internalType: "address",
+        name: "account",
+        type: "address",
+      },
+      {
+        internalType: "address",
+        name: "newManager",
+        type: "address",
+      },
+    ],
+    name: "setManager",
+    outputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    constant: false,
+    inputs: [
+      {
+        internalType: "address",
+        name: "account",
+        type: "address",
+      },
+      {
+        internalType: "bytes4",
+        name: "interfaceId",
+        type: "bytes4",
+      },
+    ],
+    name: "updateERC165Cache",
+    outputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function",
   },
 ];
