fc3799e14142d9783a5d2832f89f5a42ab559ed7
zakhap <zhs2104@columbia.edu>
2021-10-26 13:19:11 +0100

    Add migration

---
+++ server/migrations/20211025130133-add-notional.js
@@ -0,0 +1,52 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.sequelize.transaction(async (t) => {
+      // Add Notional
+      await queryInterface.bulkInsert(
+        'Chains',
+        [
+          {
+            id: 'notional',
+            symbol: 'NOTE',
+            name: 'Notional',
+            type: 'dao',
+            network: 'compound',
+            base: 'ethereum',
+            snapshot: 'notional.eth',
+            active: true,
+          },
+        ],
+        { transaction: t }
+      );
+
+      await queryInterface.bulkInsert(
+        'ChainNodes',
+        [
+          {
+            chain: 'notional',
+            url: 'wss://mainnet.infura.io/ws',
+            address: '0x086b4ecD75c494dD36641195E89c25373E06d7cB', // GovernorAlpha
+          },
+        ],
+        { transaction: t }
+      );
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.sequelize.transaction(async (t) => {
+      await queryInterface.bulkDelete(
+        'ChainNodes',
+        { chain: 'notional' },
+        { transaction: t }
+      );
+      await queryInterface.bulkDelete(
+        'Chains',
+        { id: 'notional' },
+        { transaction: t }
+      );
+    });
+  }
+};
