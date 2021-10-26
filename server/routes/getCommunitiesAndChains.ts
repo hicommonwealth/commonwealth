7ea59855925f89f47f80f72923c341030d8b4328
Adam Lawrence <adam.ad.lawrence@gmail.com>
2021-10-26 13:28:46 -0400

    Lint getCommunitiesAndChains

--- server/routes/getCommunitiesAndChains.ts
+++ server/routes/getCommunitiesAndChains.ts
@@ -7,40 +7,57 @@ const { Op } = Sequelize;
 
 const DEFAULT_SEARCH_LIMIT = 100;
 
-const getCommunitiesAndChains = async (models: DB, req: Request, res: Response, next: NextFunction) => {
+const getCommunitiesAndChains = async (
+  models: DB,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   const { user } = req;
   const { searchTerm, limit } = req.query;
   const params = {
-    limit: limit || DEFAULT_SEARCH_LIMIT
+    limit: limit || DEFAULT_SEARCH_LIMIT,
   };
+
   if (searchTerm) {
     params['where'] = { name: { [Op.iLike]: `%${searchTerm}%` } };
   }
-  const chains: (ChainInstance | OffchainCommunityInstance)[] = await models.Chain.findAll(params);
+
+  const chains: (ChainInstance | OffchainCommunityInstance)[] =
+    await models.Chain.findAll(params);
+
   const communities = await models.OffchainCommunity.findAll(params);
   let userAddressIds;
   let userRoles;
   if (user) {
     const addresses = await user.getAddresses();
-    userAddressIds = addresses.filter((addr) => !!addr.verified).map((addr) => addr.id);
+    userAddressIds = addresses
+      .filter((addr) => !!addr.verified)
+      .map((addr) => addr.id);
     userRoles = await models.Role.findAll({
       where: {
         address_id: userAddressIds,
       },
     });
   }
+
   const visibleCommunities = communities.filter((community) => {
     if (!community.privacy_enabled) {
       return true;
     } else {
       if (!user) return false;
-      const userMembership = userRoles.find((role) => role.offchain_community_id === community.id);
+      const userMembership = userRoles.find(
+        (role) => role.offchain_community_id === community.id
+      );
       return !!userMembership;
     }
   });
   const chainsAndCommunities = chains.concat(visibleCommunities);
 
-  return res.json({ status: 'Success', result: chainsAndCommunities.map((p) => p.toJSON()) });
+  return res.json({
+    status: 'Success',
+    result: chainsAndCommunities.map((p) => p.toJSON()),
+  });
 };
 
 export default getCommunitiesAndChains;
