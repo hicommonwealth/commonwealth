9ace75994aebad2b765baecf431b6f17778ccdea
Adam Lawrence <adam.ad.lawrence@gmail.com>
2021-10-29 11:57:40 -0400

    Added small test to make sure we keep sending 400s

---
+++ test/unit/api/api.spec.ts
@@ -0,0 +1,21 @@
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import 'chai/register-should';
+import app from '../../../server-test';
+
+chai.use(chaiHttp);
+const { expect } = chai;
+
+describe('API Tests', () => {
+  describe('API Tests', () => {
+    it('Should receive a 400 error when requesting nonexistent chain', async () => {
+      const res = await chai
+        .request(app)
+        .get('/api/bulkOffchain?chain=nonexistentchain')
+        .set('Accept', 'application/json');
+      expect(res.status).to.be.equal(400);
+      expect(res.body).to.not.be.null;
+      expect(res.body.result).to.not.be.null;
+    });
+  });
+});
