64d0319c9caf9685de12b609e742f26246c57b97
Adam Lawrence <adam.ad.lawrence@gmail.com>
2021-11-02 17:22:31 +0000

    debug - catch test error

--- test/unit/api/api.spec.ts
+++ test/unit/api/api.spec.ts
@@ -9,11 +9,15 @@ const { expect } = chai;
 describe('API Tests', () => {
   describe('API Tests', () => {
     it('Should receive a 400 error when requesting nonexistent chain', async () => {
-      const res = await chai
-        .request(app)
-        .get('/api/bulkOffchain?chain=nonexistentchain')
-        .set('Accept', 'application/json');
-      console.log('res.status', res.status);
+      let res;
+      try {
+        res = await chai
+          .request(app)
+          .get('/api/bulkOffchain?chain=nonexistentchain')
+          .set('Accept', 'application/json');
+      } catch (error) {
+        console.log(error);
+      }
       expect(res.status).to.be.equal(400);
       expect(res.body).to.not.be.null;
       expect(res.body.result).to.not.be.null;
