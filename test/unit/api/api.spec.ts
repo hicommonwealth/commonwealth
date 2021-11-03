51a36f0f8dee9d3ae17dced036f84a04f7c8942e
Adam Lawrence <adam.ad.lawrence@gmail.com>
2021-11-03 14:19:31 +0000

    debug - try increasing timeout

--- test/unit/api/api.spec.ts
+++ test/unit/api/api.spec.ts
@@ -6,25 +6,23 @@ import app from '../../../server-test';
 chai.use(chaiHttp);
 const { expect } = chai;
 
-describe('API Tests', () => {
-  describe('API Tests', () => {
-    it('Should receive a 400 error when requesting nonexistent chain', async () => {
-      let res;
-      console.log(
-        'trying test: Should receive a 400 error when requesting nonexistent chain'
-      );
-      try {
-        res = await chai
-          .request(app)
-          .get('/api/bulkOffchain?chain=nonexistentchain')
-          .set('Accept', 'application/json');
-      } catch (error) {
-        console.log('error encountered', error);
-      }
-      console.log('res result', res);
-      expect(res.status).to.be.equal(400);
-      expect(res.body).to.not.be.null;
-      expect(res.body.result).to.not.be.null;
-    });
-  });
+describe('API Tests', function () {
+  it('Should receive a 400 error when requesting nonexistent chain', async () => {
+    let res;
+    console.log(
+      'trying test: Should receive a 400 error when requesting nonexistent chain'
+    );
+    try {
+      res = await chai
+        .request(app)
+        .get('/api/bulkOffchain?chain=nonexistentchain')
+        .set('Accept', 'application/json');
+    } catch (error) {
+      console.log('error encountered', error);
+    }
+    console.log('res result', res);
+    expect(res.status).to.be.equal(400);
+    expect(res.body).to.not.be.null;
+    expect(res.body.result).to.not.be.null;
+  }).timeout(50000);
 });
