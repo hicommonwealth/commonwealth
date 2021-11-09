e20f5f986ae27cf0c47487af40915d2ae3f61b46
timolegros <timothee@ualberta.ca>
2021-11-09 07:47:38 -0700

    remove unnecessary logs

--- server/eventHandlers/storage.ts
+++ server/eventHandlers/storage.ts
@@ -79,7 +79,6 @@ export default class extends IEventHandler {
     // eslint-disable-next-line @typescript-eslint/no-shadow
     const log = factory.getLogger(addPrefix(__filename, [event.network, event.chain]));
     const chain = event.chain || this._chain;
-    log.info(`>>>>>>>>>>>>>>>>>>>>>>>>>>>DEUBG: ${chain}, event chain: ${event.chain}, class chain: ${this._chain}`);
 
     event = this.truncateEvent(event);
     const shouldSkip = await this._shouldSkip(event);
