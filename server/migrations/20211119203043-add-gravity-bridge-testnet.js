c070008af8aa096242392dfac3a70dc8907fda24
zakhap <zhs2104@columbia.edu>
2021-11-19 14:06:15 -0700

    Update name, remove rpc endpoint

--- server/migrations/20211119203043-add-gravity-bridge-testnet.js
+++ server/migrations/20211119203043-add-gravity-bridge-testnet.js
@@ -5,8 +5,8 @@ module.exports = {
     return queryInterface.sequelize.transaction(async (t) => {
       await queryInterface.bulkInsert('Chains', [{
         id: 'gravity-bridge-testnet',
-        symbol: 'GRAVITY-TEST',
-        name: 'Gravity Bridge',
+        symbol: 'GRAV',
+        name: 'Cosmos Gravity Bridge',
         type: 'chain',
         network: 'gravity',
         base: 'cosmos',
@@ -17,7 +17,7 @@ module.exports = {
 
       await queryInterface.bulkInsert('ChainNodes', [{
         chain: 'gravity-bridge-testnet',
-        url: 'http://chainripper-2.althea.net/',
+        url: '',
       }], { transaction: t });
     });
   },
