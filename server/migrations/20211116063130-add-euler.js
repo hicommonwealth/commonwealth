d5c2a99e2d17b38c8fb4c359d8a725b01c3f62fa
zakhap <zhs2104@columbia.edu>
2021-11-23 14:31:41 -0700

    Add eth_chain_id and set tokenName to 'token' for OZ/Bravo

--- server/migrations/20211116063130-add-euler.js
+++ server/migrations/20211116063130-add-euler.js
@@ -26,9 +26,10 @@ module.exports = {
         [
           {
             chain: 'euler-finance',
+            eth_chain_id: 3,
             url: 'wss://eth-ropsten.alchemyapi.io/v2/2xXT2xx5AvA3GFTev3j_nB9LzWdmxPk7',
             address: '0xD2B07E433e4F9dda20E4a523b2A60c23c7b1344C', // Testnet OZ gov contract
-            token_name: 'EUL', // for alpha/bravo
+            token_name: 'token', // for bravo/oz
           },
         ],
         { transaction: t }
