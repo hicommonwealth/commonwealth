7ec9fa4257e776f16245427055c77bb684087f9e
Adam Lawrence <adam.ad.lawrence@gmail.com>
2021-12-09 15:28:20 -0500

    Update 20211207195840-deprecate-offchain.js

--- server/migrations/20211207195840-deprecate-offchain.js
+++ server/migrations/20211207195840-deprecate-offchain.js
@@ -1,57 +1,50 @@
 'use strict';
 
-const community = 'harvest';
+const communities = [
+  'clcg-commonwealth',
+  'dtrade',
+  'decent',
+  'webb',
+  'agency',
+  'kabocha',
+  'cmn-protocol',
+  'polkaswapcommunity',
+  'stargaze',
+  'cabindao',
+  'ion',
+  'nippon-powder',
+  'endaoment',
+  'poof-cash',
+  'pontem-network',
+  'ubeswap',
+  'euler-finance',
+  'be-your-own-bank',
+  'antimatter-dao',
+  'longhorndao',
+  'magicklu',
+  'burnt-finance',
+  'element-finance',
+  'druside',
+  'sentinel',
+  'gopixel',
+  'anmol',
+  'harvest',
+  'impactmarket',
+  'immunefi',
+  'infinity',
+  'arvex28',
+  '222',
+  'digm-dao',
+  'usemate',
+  'qnt-dao',
+  'knoxedge',
+  'likecoin',
+  'pfeilstorch',
+  'redacted-cartel'
+];
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-    Steps for deprecating offchain communities
-    */
-    const offchainCommunitiesToKeep = [
-      'clcg-commonwealth',
-      'dtrade',
-      'decent',
-      'webb',
-      'agency',
-      'kabocha',
-      'cmn-protocol',
-      'polkaswapcommunity',
-      'stargaze',
-      'cabindao',
-      'ion',
-      'nippon-powder',
-      'endaoment',
-      'poof-cash',
-      'pontem-network',
-      'ubeswap',
-      'euler-finance',
-      'be-your-own-bank',
-      'antimatter-dao',
-      'longhorndao',
-      'magicklu',
-      'burnt-finance',
-      'element-finance',
-      'druside',
-      'sentinel',
-      'gopixel',
-      'anmol',
-      'beta-finance',
-      'harvest',
-      'blockhub-dao',
-      'impactmarket',
-      'immunefi',
-      'infinity',
-      'arvex28',
-      '222',
-      'digm-dao',
-      'usemate',
-      'qnt-dao',
-      'knoxedge',
-      'likecoin',
-      'pfeilstorch',
-      'redacted-cartel'
-    ];
-
     const offchainToRemove = [
       'agoric-community',
       'bauhaus-cw',
@@ -90,11 +83,12 @@ module.exports = {
       'yzs-stash',
     ]
 
-
-
-    return queryInterface.sequelize.transaction(async (t) => {
-      // Copy offchain community to Chains, with appropriate fields filled-in
-
+    /**
+     * Copy offchain community to Chains, with appropriate fields filled-in
+     * @param {string} community
+     * @returns null
+     */
+    const fromOffchainToChain = async (t, community) => {
       // Get rest of the info
       const chain = await queryInterface.sequelize.query(
         `SELECT id, name, icon_url, description FROM "OffchainCommunities" WHERE id='${community}'`,
@@ -103,6 +97,7 @@ module.exports = {
 
       // Make sure it exists
       if (chain[0].length === 0) {
+        console.log("community not found", community)
         return;
       }
 
@@ -112,13 +107,10 @@ module.exports = {
       const name = info['name']
       const icon_url = info['icon_url']
       const description = info['description'].substring(0, 200)
+      const { website, discord, telegram } = info;
 
-      console.log(id)
-      console.log(name)
-      console.log(icon_url)
-      console.log(description)
-      console.log()
 
+      // Create new rows
       const chainObject = {
         id,
         name,
@@ -130,8 +122,10 @@ module.exports = {
         type: 'chain',
         network: 'ethereum',
         base: 'ethereum',
+        website,
+        discord,
+        telegram
       }
-
       const chainNodeObject = {
         chain: id,
         url: 'wss://eth-mainnet.alchemyapi.io/v2/cNC4XfxR7biwO2bfIO5aKcs9EMPxTQfr/',
@@ -139,56 +133,69 @@ module.exports = {
         address: null,
       }
 
-      // Write complete rows to Chains table
-      await queryInterface.bulkInsert('Chains', [chainObject], { transaction: t });
-      await queryInterface.bulkInsert('ChainNodes', [chainNodeObject], { transaction: t })
+      try {
+        // Write complete rows to Chains table
+        await queryInterface.bulkInsert('Chains', [chainObject], { transaction: t, ignoreDuplicates: true });
+        await queryInterface.bulkInsert('ChainNodes', [chainNodeObject], { transaction: t, ignoreDuplicates: true })
+        console.log("successfully ported over community", community)
+      } catch (error) {
+        console.log("error porting over community", community, error.errors)
+        // console.log(error)
+      }
+    }
 
-      // Merge offchain ids into chain column
+    return queryInterface.sequelize.transaction(async (t) => {
+
+      for (let i = 0; i < communities.length; i++) {
+        await fromOffchainToChain(t, communities[i])
+      }
+
+      // // Merge offchain ids into chain column
       await queryInterface.sequelize.query(
         `UPDATE "Roles" SET chain_id = offchain_community_id WHERE chain_id IS NULL;`,
         { transaction: t },
       );
-      await queryInterface.sequelize.query(
-        `UPDATE "InviteCodes" SET chain_id = community_id WHERE chain_id IS NULL;`,
-        { transaction: t },
-      );
-      await queryInterface.sequelize.query(
-        `UPDATE "InviteLinks" SET chain_id = community_id WHERE chain_id IS NULL;`,
-        { transaction: t },
-      );
-      await queryInterface.sequelize.query(
-        `UPDATE "OffchainComments" SET chain = community WHERE chain IS NULL;`,
-        { transaction: t },
-      );
-      await queryInterface.sequelize.query(
-        `UPDATE "OffchainReactions" SET chain = community WHERE chain IS NULL;`,
-        { transaction: t },
-      );
-      await queryInterface.sequelize.query(
-        `UPDATE "OffchainTopics" SET chain_id = community_id WHERE chain_id IS NULL;`,
-        { transaction: t },
-      );
-      await queryInterface.sequelize.query(
-        `UPDATE "OffchainViewCounts" SET chain = community WHERE chain IS NULL;`,
-        { transaction: t },
-      );
-      await queryInterface.sequelize.query(
-        `UPDATE "OffchainVotes" SET chain = community WHERE chain IS NULL;`,
-        { transaction: t },
-      );
-      await queryInterface.sequelize.query(
-        `UPDATE "StarredCommunity" SET chain = community WHERE chain IS NULL;`,
-        { transaction: t },
-      );
-      await queryInterface.sequelize.query(
-        `UPDATE "Subscriptions" SET chain_id = community_id WHERE chain_id IS NULL;`,
-        { transaction: t },
+      // await queryInterface.sequelize.query(
+      //   `UPDATE "InviteCodes" SET chain_id = community_id WHERE chain_id IS NULL;`,
+      //   { transaction: t },
+      // );
+      // await queryInterface.sequelize.query(
+      //   `UPDATE "InviteLinks" SET chain_id = community_id WHERE chain_id IS NULL;`,
+      //   { transaction: t },
+      // );
+      // await queryInterface.sequelize.query(
+      //   `UPDATE "OffchainComments" SET chain = community WHERE chain IS NULL;`,
+      //   { transaction: t },
+      // );
+      // await queryInterface.sequelize.query(
+      //   `UPDATE "OffchainReactions" SET chain = community WHERE chain IS NULL;`,
+      //   { transaction: t },
+      // );
+      // await queryInterface.sequelize.query(
+      //   `UPDATE "OffchainTopics" SET chain_id = community_id WHERE chain_id IS NULL;`,
+      //   { transaction: t },
+      // );
+      // await queryInterface.sequelize.query(
+      //   `UPDATE "OffchainViewCounts" SET chain = community WHERE chain IS NULL;`,
+      //   { transaction: t },
+      // );
+      // await queryInterface.sequelize.query(
+      //   `UPDATE "OffchainVotes" SET chain = community WHERE chain IS NULL;`,
+      //   { transaction: t },
+      // );
+      // await queryInterface.sequelize.query(
+      //   `UPDATE "StarredCommunity" SET chain = community WHERE chain IS NULL;`,
+      //   { transaction: t },
+      // );
+      // await queryInterface.sequelize.query(
+      //   `UPDATE "Subscriptions" SET chain_id = community_id WHERE chain_id IS NULL;`,
+      //   { transaction: t },
 
-      );
-      await queryInterface.sequelize.query(
-        `UPDATE "Webhooks" SET chain_id = offchain_community_id WHERE chain_id IS NULL;`,
-        { transaction: t },
-      );
+      // );
+      // await queryInterface.sequelize.query(
+      //   `UPDATE "Webhooks" SET chain_id = offchain_community_id WHERE chain_id IS NULL;`,
+      //   { transaction: t },
+      // );
 
       // TODO: Add fkey constraints to DB
 
@@ -197,8 +204,14 @@ module.exports = {
   },
   down: (queryInterface, Sequelize) => {
     return queryInterface.sequelize.transaction(async (t) => {
-      await queryInterface.bulkDelete('ChainNodes', { chain: community }, { transaction: t });
-      await queryInterface.bulkDelete('Chains', { id: community }, { transaction: t });
+      for (let i = 0; i < communities.length; i++) {
+        try {
+          await queryInterface.bulkDelete('ChainNodes', { chain: communities[i] }, { transaction: t });
+          await queryInterface.bulkDelete('Chains', { id: communities[i] }, { transaction: t });
+        } catch (error) {
+          console.log("error deleting community", communities[i], error)
+        }
+      }
     });
   }
 };
