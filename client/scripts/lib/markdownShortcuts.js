3cbec6e915a6458121c6678e280287e1db9c4bfd
Suspended Reason <graham@commonwealth.im>
2021-12-14 12:48:27 -0600

    Rm import

--- client/scripts/lib/markdownShortcuts.js
+++ client/scripts/lib/markdownShortcuts.js
@@ -32,7 +32,6 @@
  // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  // THE SOFTWARE.
  //
- import Quill from 'quill';
 
  export class MarkdownShortcuts {
    constructor (quill, options) {
