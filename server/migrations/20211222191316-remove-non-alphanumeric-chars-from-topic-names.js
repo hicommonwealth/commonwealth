85a6deb1bf9a8191755b2d776551b34162979d12
Suspended Reason <graham@commonwealth.im>
2021-12-22 18:21:35 -0600

    Rm unnecessary escape char

--- server/migrations/20211222191316-remove-non-alphanumeric-chars-from-topic-names.js
+++ server/migrations/20211222191316-remove-non-alphanumeric-chars-from-topic-names.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    const prohibitedChars = '["<>%{}|\\/^/`]';
+    const prohibitedChars = '["<>%{}|\\/^`]';
     const query = `UPDATE "OffchainTopics" SET name=REGEXP_REPLACE(name, '${prohibitedChars}', '', 'g');`;
     queryInterface.sequelize.query(query);
   },
