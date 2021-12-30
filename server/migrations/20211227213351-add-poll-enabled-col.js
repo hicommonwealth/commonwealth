f5e93ab02fd3e8df748fbaa37bc5b659f5d82341
Suspended Reason <graham@commonwealth.im>
2021-12-30 11:55:37 -0600

    Consolidate migrations

--- server/migrations/20211227213351-add-poll-enabled-col.js
+++ server/migrations/20211227213351-add-poll-enabled-col.js
@@ -2,13 +2,20 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.addColumn(
-      'OffchainThreads',
-      'offchain_voting_enabled',
-      {
-        type: Sequelize.BOOLEAN,
-      }
-    );
+    return queryInterface.sequelize.transaction(async (t) => {
+      await queryInterface.addColumn(
+        'OffchainThreads',
+        'offchain_voting_enabled',
+        { type: Sequelize.BOOLEAN },
+        { transaction: t }
+      );
+      await queryInterface.sequelize.query(`
+        UPDATE "OffchainThreads" 
+        SET offchain_voting_enabled = true 
+        WHERE offchain_voting_ends_at IS NOT NULL`,
+        { transaction: t }
+      );
+    });
   },
 
   down: async (queryInterface, Sequelize) => {
