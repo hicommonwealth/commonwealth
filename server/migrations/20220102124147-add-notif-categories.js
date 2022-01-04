75676c39cac069c5125bdef3d6bcfb110776dcad
timolegros <timothee@ualberta.ca>
2022-01-03 22:17:25 -0700

    notif migration fix

--- server/migrations/20220102124147-add-notif-categories.js
+++ server/migrations/20220102124147-add-notif-categories.js
@@ -2,40 +2,38 @@
 
 module.exports = {
     up: async (queryInterface, Sequelize) => {
-        return queryInterface.sequelize.transaction(async (t) => {
-            await queryInterface.bulkInsert('NotificationCategories', [
-                {
-                    name: 'thread-edit',
-                    description: 'A thread is edited',
-                    created_at: new Date(),
-                    updated_at: new Date(),
-                },
-                {
-                    name: 'comment-edit',
-                    description: 'A comment is edited',
-                    created_at: new Date(),
-                    updated_at: new Date(),
-                },
-                {
-                    name: 'new-role-creation',
-                    description: 'A new role is created',
-                    created_at: new Date(),
-                    updated_at: new Date(),
-                },
-                {
-                    name: 'entity-event',
-                    description: 'An entity event has occurred',
-                    created_at: new Date(),
-                    updated_at: new Date(),
-                }], {transaction: t});
-        });
+        return queryInterface.bulkInsert('NotificationCategories', [
+            {
+                name: 'thread-edit',
+                description: 'A thread is edited',
+                created_at: new Date(),
+                updated_at: new Date(),
+            },
+            {
+                name: 'comment-edit',
+                description: 'A comment is edited',
+                created_at: new Date(),
+                updated_at: new Date(),
+            },
+            {
+                name: 'new-role-creation',
+                description: 'A new role is created',
+                created_at: new Date(),
+                updated_at: new Date(),
+            },
+            {
+                name: 'entity-event',
+                description: 'An entity event has occurred',
+                created_at: new Date(),
+                updated_at: new Date(),
+            }]);
     },
 
     down: async (queryInterface, Sequelize) => {
         return queryInterface.sequelize.transaction(async (t) => {
             await queryInterface.bulkDelete('NotificationCategories', {
                 name: ['thread-edit', 'new-role-creation', 'comment-edit', 'entity-event']
-            }, { transaction: t });
+            }, {transaction: t});
         });
     }
 };
