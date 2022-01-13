e608dae51252e3e28075ca4953bedb099c08ed14
zakhap <zhs2104@columbia.edu>
2022-01-12 16:24:59 -0800

    Clean up code and fix style

--- client/scripts/views/modals/offchain_voting_modal.ts
+++ client/scripts/views/modals/offchain_voting_modal.ts
@@ -12,35 +12,21 @@ const OffchainVotingModal : m.Component<{ votes: OffchainVote[] }, {}> = {
   view: (vnode) => {
     const { votes } = vnode.attrs;
     if (!votes || votes.length === 0) return;
-    var csvRows = [];
-    votes.forEach(vote => {
-      let displayName;
-      let profile = app.profiles.getProfile(vote.author_chain, vote.address);
-      if(profile)
-        displayName = profile.displayName;
-      else
-        displayName = formatAddressShort(
-          vote.address,
-          vote.author_chain,
-          false
-        );
-      csvRows.push([displayName, vote.option]);
-    });
+    const csvRows = [];
+    votes.forEach((vote) => csvRows.push([vote.address, vote.option]));
 
     return m('.OffchainVotingModal', [
       m('.compact-modal-title', [
         m('h3', 'Votes'),
         m(CompactModalExitButton),
       ]),
-      m('.compact-modal-body', 
+      m('.compact-modal-body',
       [
-        m('a', {
-          style: { 'cursor': 'pointer' },
-          'onclick': (e) => {
+        m('a.download-link', {
+          onclick: (e) => {
             e.preventDefault();
-            let csvContent = "data:text/csv;charset=utf-8," 
-              + csvRows.map(e => e.join(",")).join("\n");
-            var encodedUri = encodeURI(csvContent);
+            const csvContent = `data:text/csv;charset=utf-8,${csvRows.map((e) => e.join(",")).join("\n")}`;
+            const encodedUri = encodeURI(csvContent);
             window.open(encodedUri);
           }
         }, 'Download all votes as CSV'),
