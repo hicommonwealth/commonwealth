569916c5bcc1deaaf65321edbb5ca4753c6c01a1
Gabriel Duquette <gabrielduquette@gmail.com>
2022-01-14 16:03:28 -0600

    fix button class helper, remove shadow from disabled button

--- client/scripts/views/components/component_kit/helpers.ts
+++ client/scripts/views/components/component_kit/helpers.ts
@@ -6,7 +6,9 @@ export const getButtonClasses = (
   styleProps: ButtonStyleProps | EngagementButtonStyleProps
 ): string =>
   `${componentType} ${Object.entries(styleProps)
-    .map(([key, value]) =>
-      key === 'disabled' ? (value === true ? 'disabled' : '') : value
+    .filter(([key, value]) => key && value) // filters out keys that don't have values
+    .map(
+      ([key, value]) =>
+        key === 'disabled' ? (value === true ? 'disabled' : null) : value // returns disabled string instead of boolean
     )
     .join(' ')}`;
