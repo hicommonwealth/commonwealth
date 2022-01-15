49fce63c75b5296ef5b2f903a8f0a416316d7498
timolegros <timothee@ualberta.ca>
2022-01-14 22:06:19 -0700

    fix

--- server/routes/viewNotifications.ts
+++ server/routes/viewNotifications.ts
@@ -35,7 +35,7 @@ export default async (
 
   const notificationParams: any = {
     model: models.NotificationsRead,
-    required: true,
+    required: false,
     include: [
       {
         model: models.Notification,
