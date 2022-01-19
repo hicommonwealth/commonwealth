29fa83941a1e09fb9dcfb5f08493bd7a7d8f55de
Suspended Reason <graham@commonwealth.im>
2022-01-19 11:30:37 -0600

    Card base

---
+++ client/scripts/views/components/crowdfund/crowdfund_card.ts
@@ -0,0 +1,24 @@
+// import 'components/crowdfund/crowdfund_card.scss';
+
+import m from 'mithril';
+
+interface CrowdfundCardAttrs {
+
+}
+
+interface CrowdfundCardState {
+    
+}
+
+const CrowdfundCard: m.Component<
+  CrowdfundCardAttrs,
+  CrowdfundCardState
+> = {
+  view: (vnode) => {
+    return m('.CrowdfundCard', {
+
+    }, [
+
+    ]);
+  }
+}
