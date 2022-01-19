90e38ae76854644c837191fd2796aef23c51c05a
timolegros <timothee@ualberta.ca>
2022-01-19 16:28:36 -0700

    fix

--- server/scripts/setupServer.ts
+++ server/scripts/setupServer.ts
@@ -1,15 +1,16 @@
-import http from 'http';
+import * as http from 'http';
 import { Express } from 'express-serve-static-core';
 import { DEFAULT_PORT } from '../config';
 import { factory, formatFilename } from '../../shared/logging';
 import { setupWebSocketServer } from '../socket';
+import {DB} from "../database";
 const log = factory.getLogger(formatFilename(__filename));
 
-const setupServer = (app: Express) => {
+const setupServer = (app: Express, models: DB) => {
   const port = process.env.PORT || DEFAULT_PORT;
   app.set('port', port);
   const server = http.createServer(app);
-  setupWebSocketServer(server);
+  setupWebSocketServer(server, models);
 
   const onError = (error) => {
     if (error.syscall !== 'listen') {
