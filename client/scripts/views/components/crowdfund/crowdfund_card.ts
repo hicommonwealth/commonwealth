d1e2857488005ac7df7529d7e98dcbf65528f226
Suspended Reason <graham@commonwealth.im>
2022-01-19 13:44:38 -0600

    Spread sizes for page

--- client/scripts/views/components/crowdfund/crowdfund_card.ts
+++ client/scripts/views/components/crowdfund/crowdfund_card.ts
@@ -3,8 +3,14 @@ import 'components/crowdfund/crowdfund_card.scss';
 import m from 'mithril';
 import { AnonymousUser } from '../widgets/user';
 
+enum CrowdfundCardSize {
+  Small,
+  Medium,
+  Large
+}
 interface CrowdfundCardAttrs {
   crowdfund;
+  size: CrowdfundCardSize;
 }
 
 interface CrowdfundCardState {
@@ -16,7 +22,7 @@ enum DummyCrowdfundData {
     + 'Sit ullamcorper tortor pretium amet eget leo. Venenatis id risus at mollis '
     + 'orci sapien integer id non eget.',
   CrowdfundBlockCount = '16K',
-  CrowdfundCompletionPercentage = .32
+  CrowdfundCompletionPercent = 0.32
 }
 
 const DummyChainIcon: m.Component<{ chain, onclick, size: number }> = {
@@ -33,36 +39,77 @@ const DummyChainIcon: m.Component<{ chain, onclick, size: number }> = {
   }
 };
 
+const CrowdfundHeaderPanel: m.Component<{ iconSize }> = {
+  view: (vnode) => {
+    const { iconSize } = vnode.attrs;
+    return m('.CrowdfundInfoPanel', [
+      m(DummyChainIcon, {
+        chain: null,
+        onclick: null,
+        size: iconSize
+      })
+    ]);
+  }
+}
+
+const CrowdfundCompletionBar: m.Component<{ completionPercentage: number }> = {
+  view: (vnode) => {
+    const completionPercentage = vnode.attrs;
+    return m('.CrowdfundCompletionBar', [
+      m('.completed-percentage', {
+        style: `width: ${completionPercentage * 400}px`
+      }),
+    ]);
+  }
+}
+const CrowdfundInfoPanel: m.Component<{ crowdfund, avatarSize: number }> = {
+  view: (vnode) => {
+    const { crowdfund, avatarSize } = vnode.attrs;
+    return m('.cf-info-panel', [
+      m('.cf-info-header', [
+        m('h3.cf-title', DummyCrowdfundData.CrowdfundTitle),
+        m('.cf-block-count', `${DummyCrowdfundData.CrowdfundBlockCount} Blocks`)
+      ]),
+      m('.cf-info-body', DummyCrowdfundData.CrowdfundDescription),
+      m('.cf-info-footer', [
+        m(AnonymousUser, { // dummy user
+            avatarSize,
+            distinguishingKey: '123',
+        })
+      ])
+    ]);
+  }
+}
+
 const CrowdfundCard: m.Component<
   CrowdfundCardAttrs,
   CrowdfundCardState
 > = {
   view: (vnode) => {
-    return m('.CrowdfundCard', {
+    const { crowdfund, size } = vnode.attrs;
 
-    }, [
-      m('.cf-header-panel', [
-        m(DummyChainIcon, { chain: null, onclick: null, size: 45 })
-      ]),
-      m('.cf-completion-bar', [
-        m('.completed-percentage', {
-          style: `width: ${DummyCrowdfundData.CrowdfundCompletionPercentage * 400}px`
-        }),
-      ]),
-      m('.cf-info-panel', [
-        m('.cf-info-header', [
-          m('h3.cf-title', DummyCrowdfundData.CrowdfundTitle),
-          m('.cf-block-count', `${DummyCrowdfundData.CrowdfundBlockCount} Blocks`)
-        ]),
-        m('.cf-info-body', DummyCrowdfundData.CrowdfundDescription),
-        m('.cf-info-footer', [
-          m(AnonymousUser, {
-              avatarSize: 20,
-              distinguishingKey: '123',
-          })
-        ])
-      ])
-    ]);
+    const CrowdfundCardLarge = m('.CrowdfundCard',
+      { class: 'large' },
+      [
+        m(CrowdfundHeaderPanel, { iconSize: 20 }),
+        m(CrowdfundCompletionBar, { completionPercentage: (DummyCrowdfundData.CrowdfundCompletionPercent as number) }),
+        m(CrowdfundInfoPanel, { crowdfund, avatarSize: 45 })
+      ]
+    );
+
+    const CrowdfundCardMedium;
+
+    const CrowdfundCardSmall;
+
+    switch (size) {
+      case CrowdfundCardSize.Large:
+        return CrowdfundCardLarge;
+      case CrowdfundCardSize.Medium:
+        return CrowdfundCardMedium;
+      case CrowdfundCardSize.Small:
+        return CrowdfundCardSmall;
+      default:
+    }
   }
 }
 
