0a78f9d298c3819566b3ae492a73c284d162ca74
zakhap <zhs2104@columbia.edu>
2022-01-27 18:33:00 -0800

    Add query to viewActivity route.

--- server/routes/viewActivity.ts
+++ server/routes/viewActivity.ts
@@ -19,9 +19,15 @@ export default async (
   }
   const { id } = req.user;
 
-  const query = ``; 
+  const query = `SELECT * FROM "Notifications" n 
+                WHERE n.chain_id IN(SELECT r.chain_id from "Roles" r 
+                    inner join "Addresses" a ON r.address_id = a.id
+                    WHERE a.user_id = $id)
+                ORDER BY n.created_at DESC
+                LIMIT 50`;
 
   const notifications = await sequelize.query(query);
+  console.log(notifications);
 
-  return res.json({ status: 'Success', result: notifications.map((n) => n.toJSON()) });
+  return res.json({ status: 'Success', result: notifications });
 };
