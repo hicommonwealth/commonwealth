896b542cbf572083879b2ac28be50e633af1c405
Jake Naviasky <jake@commonwealth.im>
2022-02-04 15:14:32 -0500

    Use snapshot.js to compute vote totals.

--- client/scripts/helpers/snapshot_utils.ts
+++ client/scripts/helpers/snapshot_utils.ts
@@ -144,7 +144,10 @@ export interface SnapshotProposal {
   state: string;
   scores: number[];
   scores_total: number;
-  strategies?: string;
+  strategies?: Array<{
+    name: string;
+    params: any;
+  }>;
 }
 
 export interface SnapshotProposalVote {
@@ -214,20 +217,6 @@ export async function getScore(space: SnapshotSpace, address: string) {
 
 /* Single Choice Voting */
 
-// Returns the total amount of the results
-export function sumOfResultsBalance(votes: SnapshotProposalVote[]) {
-  return votes.reduce((a, b: any) => a + b.power, 0);
-}
-
-//  Returns an array with the results for each choice
-export function resultsByVoteBalance(proposal: SnapshotProposal, votes: SnapshotProposalVote[]) {
-  return proposal.choices.map((choice, i) =>
-    votes
-      .filter((vote: any) => vote.choice === i + 1)
-      .reduce((a, b: any) => a + b.power, 0)
-  );
-}
-
 export async function getResults(space: SnapshotSpace, proposal: SnapshotProposal) {
   const { default: Snapshot } = await import('@snapshot-labs/snapshot.js');
   try {
@@ -235,14 +224,15 @@ export async function getResults(space: SnapshotSpace, proposal: SnapshotProposa
 
     // const voters = votes.map(vote => vote.voter);
     // const provider = Snapshot.utils.getProvider(space.network);
-    const strategies = space.strategies;
+    const strategies = proposal.strategies ?? space.strategies;
+    console.log(strategies);
     if (proposal.state !== 'pending') {
       const scores = await Snapshot.utils.getScores(
         space.id,
         strategies,
         space.network,
-        (votes).map((vote) => vote.voter),
-        proposal.snapshot,
+        votes.map((vote) => vote.voter),
+        parseInt(proposal.snapshot, 10),
         // provider,
       );
 
@@ -250,20 +240,19 @@ export async function getResults(space: SnapshotSpace, proposal: SnapshotProposa
           vote.scores = strategies.map(
             (strategy, i) => scores[i][vote.voter] || 0
           );
-          vote.power = vote.scores.reduce((a, b: any) => a + b, 0);
+          vote.balance = vote.scores.reduce((a, b: any) => a + b, 0);
           return vote;
         })
-        .sort((a, b) => b.power - a.power)
-        .filter(vote => vote.power > 0);
+        .sort((a, b) => b.balance - a.balance)
+        .filter(vote => vote.balance > 0);
     }
 
     /* Get results */
-    const results = proposal.scores_total ? {
-      resultsByVoteBalance: proposal.scores,
-      sumOfResultsBalance: proposal.scores_total,
-    } : {
-      resultsByVoteBalance: resultsByVoteBalance(proposal, votes),
-      sumOfResultsBalance: sumOfResultsBalance(votes),
+    const votingClass = new Snapshot.utils.voting[proposal.type](proposal, votes, strategies);
+    const results = {
+      resultsByVoteBalance: votingClass.resultsByVoteBalance(),
+      resultsByStrategyScore: votingClass.resultsByStrategyScore(),
+      sumOfResultsBalance: votingClass.sumOfResultsBalance()
     };
 
     return { votes, results };
