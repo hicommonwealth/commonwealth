324790beec95f4d3b9a0b7914b48574a9049678a
Gabriel Duquette <gabrielduquette@gmail.com>
2022-02-09 15:03:18 -0600

    converted but not optimally so

--- client/scripts/views/pages/create_community/offchain_community_form_test.tsx
+++ client/scripts/views/pages/create_community/offchain_community_form_test.tsx
@@ -1,7 +1,7 @@
 /* @jsx m */
 
 import m from 'mithril';
-import { Table, Button } from 'construct-ui';
+import { Table } from 'construct-ui';
 import $ from 'jquery';
 
 import 'pages/create_community_test.scss';
@@ -17,14 +17,13 @@ import {
 } from 'views/components/metadata_rows_test';
 import { baseToNetwork } from 'views/components/login_with_wallet_dropdown';
 import {
-  ChainFormState,
-  initChainForm,
+  // ChainFormState,
+  // initChainForm,
   defaultChainRows,
 } from './chain_input_rows_test';
+import { CWButton } from '../../components/component_kit/cw_button';
 
-type OffchainFormAttrs = Record<string, unknown>;
-
-interface OffchainFormStateAttributes extends ChainFormState {
+export class OffchainFormTest implements m.ClassComponent {
   id: string;
   name: string;
   base: ChainBase;
@@ -33,157 +32,161 @@ interface OffchainFormStateAttributes extends ChainFormState {
   loading: boolean;
   symbol: string;
   error: string;
-}
+  description: string;
+  icon_url: string;
+  website: string;
+  discord: string;
+  element: string;
+  telegram: string;
+  github: string;
+  uploadInProgress: boolean;
 
-const OffchainFormState: OffchainFormStateAttributes = {
-  id: '',
-  name: '',
-  base: ChainBase.Ethereum,
-  saving: false,
-  loaded: false,
-  loading: false,
-  symbol: 'XYZ',
-  error: '',
-  description: '',
-  icon_url: '',
-  website: '',
-  discord: '',
-  element: '',
-  telegram: '',
-  github: '',
-  uploadInProgress: false
-}
+  oninit() {
+    this.id = '';
+    this.name = '';
+    this.base = ChainBase.Ethereum;
+    this.saving = false;
+    this.loaded = false;
+    this.loading = false;
+    this.symbol = 'XYZ';
+    this.error = '';
+    this.description = '';
+    this.icon_url = '';
+    this.website = '';
+    this.discord = '';
+    this.element = '';
+    this.telegram = '';
+    this.github = '';
+    this.uploadInProgress = false;
+  }
 
-export class OffchainFormTest implements m.ClassComponent<OffchainFormAttrs> {
-  view(vnode) {
-    return m('.CommunityMetadataManagementTable', [
-      m(
-        Table,
-        {
-          bordered: false,
-          interactive: false,
-          striped: false,
-          class: 'metadata-management-table',
-        },
-        [
-          m(InputPropertyRow, {
-            title: 'Name',
-            defaultValue: OffchainFormState.name,
-            onChangeHandler: (v) => {
-              OffchainFormState.name = v;
-              OffchainFormState.id = slugify(v);
-            },
-          }),
-          m(InputPropertyRow, {
-            title: 'ID',
-            defaultValue: OffchainFormState.id,
-            value: OffchainFormState.id,
-            onChangeHandler: (v) => {
-              OffchainFormState.id = v;
-            },
-          }),
-          m(InputPropertyRow, {
-            title: 'Symbol',
-            defaultValue: OffchainFormState.symbol,
-            onChangeHandler: (v) => {
-              OffchainFormState.symbol = v;
-            },
-          }),
-          m(SelectPropertyRow, {
-            title: 'Base Chain',
-            options: ['cosmos', 'ethereum', 'near'],
-            value: OffchainFormState.base,
-            onchange: (value) => {
-              OffchainFormState.base = value;
-            },
-          }),
-          ...defaultChainRows(this),
-        ]
-      ),
-      m(Button, {
-        class: 'mt-3',
-        label: 'Save changes',
-        intent: 'primary',
-        disabled: OffchainFormState.saving,
-        onclick: async () => {
-          const {
-            id,
-            name,
-            description,
-            icon_url,
-            website,
-            discord,
-            element,
-            telegram,
-            github,
-            symbol,
-          } = OffchainFormState;
-
-          OffchainFormState.saving = true;
-          const additionalArgs: {
-            eth_chain_id?: number;
-            node_url?: string;
-            bech32_prefix?: string;
-          } = {};
-
-          // defaults to be overridden when chain is no longer "offchain" type
-          switch (OffchainFormState.base) {
-            case ChainBase.CosmosSDK: {
-              additionalArgs.node_url = 'https://rpc-osmosis.keplr.app';
-              additionalArgs.bech32_prefix = 'osmo';
-              break;
-            }
-            case ChainBase.NEAR: {
-              additionalArgs.node_url = 'https://rpc.mainnet.near.org';
-              break;
-            }
-            case ChainBase.Solana: {
-              additionalArgs.node_url = 'https://api.mainnet-beta.solana.com';
-              break;
-            }
-            case ChainBase.Substrate: {
-              additionalArgs.node_url = 'wss://mainnet.edgewa.re';
-              break;
-            }
-            case ChainBase.Ethereum:
-            default: {
-              additionalArgs.eth_chain_id = 1;
-              additionalArgs.node_url =
-                'wss://eth-mainnet.alchemyapi.io/v2/cNC4XfxR7biwO2bfIO5aKcs9EMPxTQfr';
-              break;
-            }
-          }
-          try {
-            const res = await $.post(`${app.serverUrl()}/createChain`, {
-              address: '',
+  view() {
+    return (
+      <div class="CommunityMetadataManagementTable">
+        <Table
+          bordered={false}
+          interactive={false}
+          striped={false}
+          class="metadata-management-table"
+        >
+          <InputPropertyRow
+            title="Name"
+            defaultValue={this.name}
+            onChangeHandler={(v) => {
+              this.name = v;
+              this.id = slugify(v);
+            }}
+          />
+          <InputPropertyRow
+            title="ID"
+            defaultValue={this.id}
+            value={this.id}
+            onChangeHandler={(v) => {
+              this.id = v;
+            }}
+          />
+          <InputPropertyRow
+            title="Symbol"
+            defaultValue={this.symbol}
+            onChangeHandler={(v) => {
+              this.symbol = v;
+            }}
+          />
+          <SelectPropertyRow
+            title="Base Chain"
+            options={['cosmos', 'ethereum', 'near']}
+            value={this.base}
+            onchange={(value) => {
+              this.base = value;
+            }}
+          />
+        </Table>
+        {...defaultChainRows(this)}
+        <CWButton
+          class="mt-3"
+          label="Save changes"
+          buttonType="primary"
+          disabled={this.saving}
+          onclick={async () => {
+            const {
               id,
               name,
               description,
               icon_url,
-              symbol,
               website,
               discord,
               element,
               telegram,
               github,
-              jwt: app.user.jwt,
-              type: ChainType.Offchain,
-              base: OffchainFormState.base,
-              network: baseToNetwork(OffchainFormState.base),
-              ...additionalArgs,
-            });
-            await initAppState(false);
-            m.route.set(`/${res.result.chain?.id}`);
-          } catch (err) {
-            notifyError(
-              err.responseJSON?.error ||
-                'Creating new offchain community failed'
-            );
-          } finally {
-            OffchainFormState.saving = false;
-          }
-        },
-      }),
-    ]);
+              symbol,
+            } = this;
+
+            this.saving = true;
+            const additionalArgs: {
+              eth_chain_id?: number;
+              node_url?: string;
+              bech32_prefix?: string;
+            } = {};
+
+            // defaults to be overridden when chain is no longer "offchain" type
+            switch (this.base) {
+              case ChainBase.CosmosSDK: {
+                additionalArgs.node_url = 'https://rpc-osmosis.keplr.app';
+                additionalArgs.bech32_prefix = 'osmo';
+                break;
+              }
+              case ChainBase.NEAR: {
+                additionalArgs.node_url = 'https://rpc.mainnet.near.org';
+                break;
+              }
+              case ChainBase.Solana: {
+                additionalArgs.node_url = 'https://api.mainnet-beta.solana.com';
+                break;
+              }
+              case ChainBase.Substrate: {
+                additionalArgs.node_url = 'wss://mainnet.edgewa.re';
+                break;
+              }
+              case ChainBase.Ethereum:
+              default: {
+                additionalArgs.eth_chain_id = 1;
+                additionalArgs.node_url =
+                  'wss://eth-mainnet.alchemyapi.io/v2/cNC4XfxR7biwO2bfIO5aKcs9EMPxTQfr';
+                break;
+              }
+            }
+            try {
+              const res = await $.post(`${app.serverUrl()}/createChain`, {
+                address: '',
+                id,
+                name,
+                description,
+                icon_url,
+                symbol,
+                website,
+                discord,
+                element,
+                telegram,
+                github,
+                jwt: app.user.jwt,
+                type: ChainType.Offchain,
+                base: this.base,
+                network: baseToNetwork(this.base),
+                ...additionalArgs,
+              });
+              await initAppState(false);
+              m.route.set(`/${res.result.chain?.id}`);
+            } catch (err) {
+              notifyError(
+                err.responseJSON?.error ||
+                  'Creating new offchain community failed'
+              );
+            } finally {
+              this.saving = false;
+            }
+          }}
+        />
+      </div>
+    );
   }
 }
