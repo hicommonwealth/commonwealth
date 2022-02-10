543ad61be28b681d1094d5eea72bdfd64041bcf1
Jake Naviasky <jake@commonwealth.im>
2022-02-10 14:55:40 -0500

    Add explicit typings.

--- client/scripts/views/pages/create_community/offchain_community_form_test.tsx
+++ client/scripts/views/pages/create_community/offchain_community_form_test.tsx
@@ -15,23 +15,43 @@ import { baseToNetwork } from 'views/components/login_with_wallet_dropdown';
 import {
   initChainForm,
   defaultChainRows,
+  ChainFormState,
 } from './chain_input_rows_test';
 import { CWButton } from '../../components/component_kit/cw_button';
 
+// TODO: ChainFormState contains "uploadInProgress" which is technically
+// not part of the form (what we pass to /createChain), but of the general view's state,
+// and should be located elsewhere.
+interface CreateOffchainForm extends ChainFormState {
+  id: string;
+  name: string;
+  symbol: string;
+  base: ChainBase;
+}
+
+interface CreateOffchainState {
+  saving: boolean;
+  loaded: boolean;
+  loading: boolean;
+  status: string;
+  error: string;
+  form: CreateOffchainForm;
+}
+
 export class OffchainFormTest implements m.ClassComponent {
-  private form = {
-    id: '',
-    name: '',
-    symbol: 'XYZ',
-    base: ChainBase.Ethereum,
-    ...initChainForm(),
-  };
-  private state = {
+  private state: CreateOffchainState = {
     saving: false,
     loaded: false,
     loading: false,
     status: '',
     error: '',
+    form: {
+      id: '',
+      name: '',
+      symbol: 'XYZ',
+      base: ChainBase.Ethereum,
+      ...initChainForm(),
+    }
   };
   view() {
     return (
@@ -39,37 +59,37 @@ export class OffchainFormTest implements m.ClassComponent {
         <InputRow
           title="Name"
           placeholder="Enter the name of your community"
-          defaultValue={this.form.name}
+          defaultValue={this.state.form.name}
           onChangeHandler={(v) => {
-            this.form.name = v;
-            this.form.id = slugify(v);
+            this.state.form.name = v;
+            this.state.form.id = slugify(v);
           }}
         />
         <InputRow
           title="ID"
           placeholder="ID will show up here based on your name"
-          defaultValue={this.form.id}
-          value={this.form.id}
+          defaultValue={this.state.form.id}
+          value={this.state.form.id}
           onChangeHandler={(v) => {
-            this.form.id = v;
+            this.state.form.id = v;
           }}
         />
         <InputRow
           title="Symbol"
-          defaultValue={this.form.symbol}
+          defaultValue={this.state.form.symbol}
           onChangeHandler={(v) => {
-            this.form.symbol = v;
+            this.state.form.symbol = v;
           }}
         />
         <SelectRow
           title="Base Chain"
           options={['cosmos', 'ethereum', 'near']}
-          value={this.form.base}
+          value={this.state.form.base}
           onchange={(value) => {
-            this.form.base = value;
+            this.state.form.base = value;
           }}
         />
-        {...defaultChainRows(this.form)}
+        {...defaultChainRows(this.state.form)}
         <CWButton
           class="mt-3"
           label="Save changes"
@@ -84,7 +104,7 @@ export class OffchainFormTest implements m.ClassComponent {
             } = {};
 
             // defaults to be overridden when chain is no longer "offchain" type
-            switch (this.form.base) {
+            switch (this.state.form.base) {
               case ChainBase.CosmosSDK: {
                 additionalArgs.node_url = 'https://rpc-osmosis.keplr.app';
                 additionalArgs.bech32_prefix = 'osmo';
@@ -112,11 +132,11 @@ export class OffchainFormTest implements m.ClassComponent {
             }
             try {
               const res = await $.post(`${app.serverUrl()}/createChain`, {
-                address: '',
-                ...this.form,
                 jwt: app.user.jwt,
+                address: '',
                 type: ChainType.Offchain,
-                network: baseToNetwork(this.form.base),
+                network: baseToNetwork(this.state.form.base),
+                ...this.state.form,
                 ...additionalArgs,
               });
               await initAppState(false);
