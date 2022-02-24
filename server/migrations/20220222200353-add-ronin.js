9d85286318b8e100756630a854dac2f4a09e6a3b
Jake Naviasky <jake@commonwealth.im>
2022-02-24 18:04:05 -0500

    Use our endpoints for axie.

--- server/migrations/20220222200353-add-ronin.js
+++ server/migrations/20220222200353-add-ronin.js
@@ -23,19 +23,29 @@ module.exports = {
         base: 'ethereum',
         active: true,
         description: 'TODO',
-      }], { transaction: t, logging: console.log });
+      }], { transaction: t });
 
       await queryInterface.bulkInsert('ChainNodes', [{
         chain: 'axie-infinity',
-        url: 'wss://eth-mainnet.alchemyapi.io/v2/cNC4XfxR7biwO2bfIO5aKcs9EMPxTQfr', // TODO: this is eth mainnet
+        url: 'wss://ronin-rpc.commonwealth.im/wss',
+        alt_wallet_url: 'https://ronin-rpc.commonwealth.im',
         eth_chain_id: 2020, // ??
       }], { transaction: t });
     });
   },
   down: (queryInterface, Sequelize) => {
     return queryInterface.sequelize.transaction(async (t) => {
-      await queryInterface.bulkDelete('ChainNodes', { chain: 'axie-infinity' }, { transaction: t });
-      await queryInterface.bulkDelete('Chains', { id: ['axie-infinity'] }, { transaction: t });
+      const chain = 'axie-infinity';
+      await queryInterface.bulkDelete('OffchainReactions', { chain }, { transaction: t })
+      await queryInterface.bulkDelete('OffchainThreads', { chain }, { transaction: t });
+      await queryInterface.bulkDelete('OffchainComments', { chain }, { transaction: t });
+
+      const toDelete = await queryInterface.sequelize.query(`SELECT id FROM "Addresses" WHERE chain = 'axie-infinity'`, { transaction: t });
+      const ids = toDelete ? toDelete[0].map(({ id }) => id) : [];
+      await queryInterface.bulkDelete('OffchainProfiles', { address_id: ids }, { transaction: t });
+      await queryInterface.bulkDelete('Addresses', { chain }, { transaction: t });
+      await queryInterface.bulkDelete('ChainNodes', { chain }, { transaction: t });
+      await queryInterface.bulkDelete('Chains', { id: chain }, { transaction: t });
     });
   }
 };
