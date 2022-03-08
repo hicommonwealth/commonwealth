7d4a803f9adf6b22d48977971549d6d7983b1921
Gabriel Duquette <gabrielduquette@gmail.com>
2022-03-08 16:52:38 -0600

    convert subscription button

--- client/scripts/views/components/sidebar/index.ts
+++ client/scripts/views/components/sidebar/index.ts
@@ -15,7 +15,7 @@ import { selectNode, initChain, navigateToSubpage } from 'app';
 import app from 'state';
 import { link } from 'helpers';
 import { ChainInfo, NodeInfo } from 'models';
-import SubscriptionButton from 'views/components/subscription_button';
+import { SubscriptionButton } from 'views/components/subscription_button';
 import ChainStatusIndicator from 'views/components/chain_status_indicator';
 import { ChainIcon } from 'views/components/chain_icon';
 import CommunitySelector from 'views/components/sidebar/community_selector';
