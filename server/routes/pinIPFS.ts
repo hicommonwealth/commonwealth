63df3e8f5cb9535224eaf5883fb23c8d2a310989
sha14r <shamant@commonwealth.im>
2022-03-09 01:04:40 +0530

    Update pinIPFS.ts

--- server/routes/pinIPFS.ts
+++ server/routes/pinIPFS.ts
@@ -1,6 +1,11 @@
 import { Request, Response, NextFunction } from 'express';
 import { DB, sequelize } from '../database';
 import ipfs from '../util/ipfs';
+export const Errors = {
+  NotLoggedIn: 'Not logged in',
+  InvalidAddress: 'Invalid address',
+  NoBlobPresent: 'No JSON blob was input',
+};
 
 const pinIPFS = async (
   models: DB,
@@ -8,8 +13,15 @@ const pinIPFS = async (
   res: Response,
   next: NextFunction
 ) => {
+  if (!req.user) return next(new Error(Errors.NotLoggedIn));
+  if (!req.body.address_id) return next(new Error(Errors.InvalidAddress));
+  if (!req.body.blob) return next(new Error(Errors.NoBlobPresent));
   try {
-    const ipfsHash = await ipfs(req.user.id, req.body.result.id, req.body);
+    const ipfsHash = await ipfs(
+      req.user.id,
+      req.body.address_id,
+      req.body.blob
+    );
     return res.json({ status: 'Success', IPFSHash: ipfsHash });
   } catch (e) {
     return res.json({ status: 'Failure', message: e.message });
