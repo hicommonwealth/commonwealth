7a605419a335e1da9ec4e52702cadb7d11b2e9c2
Gabriel Duquette <gabrielduquette@gmail.com>
2022-03-14 13:14:01 -0500

    fully convert filter bar, fix pin container width

--- client/scripts/views/pages/discussions/discussion_filter_bar.tsx
+++ client/scripts/views/pages/discussions/discussion_filter_bar.tsx
@@ -19,8 +19,6 @@ import { OffchainThreadStage } from 'models';
 import EditTopicModal from 'views/modals/edit_topic_modal';
 import { onFeaturedDiscussionPage } from './helpers';
 
-export const ALL_PROPOSALS_KEY = 'COMMONWEALTH_ALL_PROPOSALS';
-
 type DiscussionFilterBarAttrs = {
   disabled: boolean;
   parentState;
@@ -91,187 +89,214 @@ export class DiscussionFilterBar
     const summaryViewEnabled =
       vnode.attrs.parentState.summaryView && !topicSelected;
 
-    return m('DiscussionFilterBar', [
-      topics.length > 0 &&
-        m(PopoverMenu, {
-          trigger: m(Button, {
-            rounded: true,
-            compact: true,
-            label: selectedTopic ? `Topic: ${topic}` : 'All Topics',
-            iconRight: Icons.CHEVRON_DOWN,
-            size: 'sm',
-            disabled,
-          }),
-          inline: true,
-          hasArrow: false,
-          transitionDuration: 0,
-          closeOnContentClick: true,
-          content: m('.discussions-topic-items', [
-            m(MenuItem, {
-              active: m.route.get() === `/${app.activeChainId()}` || !topic,
-              iconLeft:
-                m.route.get() === `/${app.activeChainId()}` || !topic
-                  ? Icons.CHECK
-                  : null,
-              label: 'All Topics',
-              onclick: () => {
-                localStorage.setItem('discussion-summary-toggle', 'false');
-                vnode.attrs.parentState.summaryView = false;
-                navigateToSubpage('/');
-              },
-            }),
-            m(MenuDivider),
-            // featured topics
-            featuredTopics
-              .concat(otherTopics)
-              .map(
-                (
-                  {
-                    id,
-                    name,
-                    description,
-                    telegram,
-                    featuredInSidebar,
-                    featuredInNewPost,
-                    defaultOffchainTemplate,
-                  },
-                  idx
-                ) => {
-                  const active =
-                    m.route.get() ===
-                      `/${app.activeChainId()}/discussions/${encodeURI(
-                        name.toString().trim()
-                      )}` ||
-                    (topic && topic === name);
-                  return m(MenuItem, {
-                    key: name,
-                    active,
-                    // iconLeft: active ? Icons.CHECK : null,
-                    onclick: (e) => {
+    return (
+      <div class="DiscussionFilterBar">
+        {topics.length > 0 && (
+          <PopoverMenu
+            trigger={
+              <Button
+                rounded={true}
+                compac={true}
+                label={selectedTopic ? `Topic: ${topic}` : 'All Topics'}
+                iconRight={Icons.CHEVRON_DOWN}
+                size="sm"
+                disabled={disabled}
+              />
+            }
+            inline={true}
+            hasArrow={false}
+            transitionDuration={0}
+            closeOnContentClick={true}
+            content={
+              <div class="discussions-topic-items">
+                <MenuItem
+                  active={m.route.get() === `/${app.activeChainId()}` || !topic}
+                  iconLeft={
+                    m.route.get() === `/${app.activeChainId()}` || !topic
+                      ? Icons.CHECK
+                      : null
+                  }
+                  label="All Topics"
+                  onclick={() => {
+                    localStorage.setItem('discussion-summary-toggle', 'false');
+                    vnode.attrs.parentState.summaryView = false;
+                    navigateToSubpage('/');
+                  }}
+                />
+                ,
+                <MenuDivider />,
+                {featuredTopics
+                  .concat(otherTopics)
+                  .map(
+                    ({
+                      id,
+                      name,
+                      description,
+                      telegram,
+                      featuredInSidebar,
+                      featuredInNewPost,
+                      defaultOffchainTemplate,
+                    }) => {
+                      const active =
+                        m.route.get() ===
+                          `/${app.activeChainId()}/discussions/${encodeURI(
+                            name.toString().trim()
+                          )}` ||
+                        (topic && topic === name);
+
+                      return (
+                        <MenuItem
+                          key={name}
+                          active={active}
+                          onclick={(e) => {
+                            e.preventDefault();
+                            navigateToSubpage(`/discussions/${name}`);
+                            vnode.attrs.parentState.summaryView = false;
+                            localStorage.setItem(
+                              'discussion-summary-toggle',
+                              'false'
+                            );
+                          }}
+                          label={
+                            <div class="topic-menu-item">
+                              {active && <Icon name={Icons.CHECK} />}
+                              <div class="topic-menu-item-name">{name}</div>
+                              {app.user?.isAdminOfEntity({
+                                chain: app.activeChainId(),
+                              }) && (
+                                <Button
+                                  size="xs"
+                                  label="Edit"
+                                  compact={true}
+                                  rounded={true}
+                                  onclick={(e) => {
+                                    e.preventDefault();
+                                    e.stopPropagation();
+                                    app.modals.create({
+                                      modal: EditTopicModal,
+                                      data: {
+                                        id,
+                                        name,
+                                        description,
+                                        telegram,
+                                        featuredInSidebar,
+                                        featuredInNewPost,
+                                        defaultOffchainTemplate,
+                                      },
+                                    });
+                                  }}
+                                />
+                              )}
+                            </div>
+                          }
+                        />
+                      );
+                    }
+                  )}
+              </div>
+            }
+          />
+        )}
+        {stagesEnabled && (
+          <PopoverMenu
+            trigger={
+              <Button
+                rounded={true}
+                compact={true}
+                label={
+                  selectedStage
+                    ? `Stage: ${offchainThreadStageToLabel(selectedStage)}`
+                    : 'All Stages'
+                }
+                iconRight={Icons.CHEVRON_DOWN}
+                size="sm"
+                disabled={disabled}
+              />
+            }
+            inline={true}
+            hasArrow={false}
+            transitionDuration={0}
+            closeOnContentClick={true}
+            content={
+              <div class="discussions-stage-items">
+                <MenuItem
+                  onclick={(e) => {
+                    e.preventDefault();
+                    vnode.attrs.parentState.summaryView = false;
+                    localStorage.setItem('discussion-summary-toggle', 'false');
+                    navigateToSubpage('/');
+                  }}
+                  active={!stage}
+                  iconLeft={!stage ? Icons.CHECK : null}
+                  label="All Stages"
+                />
+                <MenuDivider />
+                {stages.map((targetStage) => (
+                  <MenuItem
+                    active={stage === targetStage}
+                    iconLeft={stage === targetStage ? Icons.CHECK : null}
+                    onclick={(e) => {
                       e.preventDefault();
-                      navigateToSubpage(`/discussions/${name}`);
                       vnode.attrs.parentState.summaryView = false;
                       localStorage.setItem(
                         'discussion-summary-toggle',
                         'false'
                       );
-                    },
-                    label: m('.topic-menu-item', [
-                      active && m(Icon, { name: Icons.CHECK }),
-                      m('.topic-menu-item-name', name),
-                      app.user?.isAdminOfEntity({
-                        chain: app.activeChainId(),
-                      }) &&
-                        m(Button, {
-                          size: 'xs',
-                          label: 'Edit',
-                          class: 'edit-topic-button',
-                          compact: true,
-                          rounded: true,
-                          onclick: (e) => {
-                            e.preventDefault();
-                            e.stopPropagation();
-                            app.modals.create({
-                              modal: EditTopicModal,
-                              data: {
-                                id,
-                                name,
-                                description,
-                                telegram,
-                                featuredInSidebar,
-                                featuredInNewPost,
-                                defaultOffchainTemplate,
-                              },
-                            });
-                          },
-                        }),
-                    ]),
-                  });
-                }
-              ),
-          ]),
-        }),
-      stagesEnabled &&
-        m(PopoverMenu, {
-          trigger: m(Button, {
-            rounded: true,
-            compact: true,
-            class: 'stage-filter',
-            label: selectedStage
-              ? `Stage: ${offchainThreadStageToLabel(selectedStage)}`
-              : 'All Stages',
-            iconRight: Icons.CHEVRON_DOWN,
-            size: 'sm',
-            disabled,
-          }),
-          inline: true,
-          hasArrow: false,
-          transitionDuration: 0,
-          closeOnContentClick: true,
-          class: 'StagesFilterPopover',
-          content: m('.discussions-stage-items', [
-            m(MenuItem, {
-              onclick: (e) => {
+                      navigateToSubpage(`/?stage=${targetStage}`);
+                    }}
+                    label={[
+                      `${offchainThreadStageToLabel(targetStage)}`,
+                      targetStage === OffchainThreadStage.Voting && (
+                        <div class="discussions-stage-count">
+                          {app.threads.numVotingThreads}
+                        </div>
+                      ),
+                    ]}
+                  />
+                ))}
+                )
+              </div>
+            }
+          />
+        )}
+        {
+          (topics.length > 0 && (
+            <Button
+              rounded={true}
+              compact={true}
+              class={`summary-toggle ${
+                summaryViewEnabled ? 'active' : 'inactive'
+              }`}
+              label="Summary"
+              size="sm"
+              disabled={disabled}
+              onclick={async (e) => {
+                e.preventDefault();
+                localStorage.setItem('discussion-summary-toggle', 'true');
+                vnode.attrs.parentState.summaryView = true;
+                navigateToSubpage('/');
+              }}
+            />
+          ),
+          (
+            <Button
+              rounded={true}
+              compact={true}
+              class={`latest-toggle ${
+                summaryViewEnabled ? 'inactive' : 'active'
+              }`}
+              label="Latest"
+              size="sm"
+              disabled={disabled}
+              onclick={async (e) => {
                 e.preventDefault();
                 vnode.attrs.parentState.summaryView = false;
                 localStorage.setItem('discussion-summary-toggle', 'false');
-                navigateToSubpage('/');
-              },
-              active: !stage,
-              iconLeft: !stage ? Icons.CHECK : null,
-              label: 'All Stages',
-            }),
-            m(MenuDivider),
-            stages.map((targetStage) =>
-              m(MenuItem, {
-                active: stage === targetStage,
-                iconLeft: stage === targetStage ? Icons.CHECK : null,
-                onclick: (e) => {
-                  e.preventDefault();
-                  vnode.attrs.parentState.summaryView = false;
-                  localStorage.setItem('discussion-summary-toggle', 'false');
-                  navigateToSubpage(`/?stage=${targetStage}`);
-                },
-                label: [
-                  `${offchainThreadStageToLabel(targetStage)}`,
-                  targetStage === OffchainThreadStage.Voting &&
-                    m(
-                      '.discussions-stage-count',
-                      `${app.threads.numVotingThreads}`
-                    ),
-                ],
-              })
-            ),
-          ]),
-        }),
-      topics.length > 0 &&
-        m(Button, {
-          rounded: true,
-          compact: true,
-          class: `summary-toggle ${summaryViewEnabled ? 'active' : 'inactive'}`,
-          label: 'Summary',
-          size: 'sm',
-          disabled,
-          onclick: async (e) => {
-            e.preventDefault();
-            localStorage.setItem('discussion-summary-toggle', 'true');
-            vnode.attrs.parentState.summaryView = true;
-            navigateToSubpage('/');
-          },
-        }),
-      m(Button, {
-        rounded: true,
-        compact: true,
-        class: `latest-toggle ${summaryViewEnabled ? 'inactive' : 'active'}`,
-        label: 'Latest',
-        size: 'sm',
-        disabled,
-        onclick: async (e) => {
-          e.preventDefault();
-          vnode.attrs.parentState.summaryView = false;
-          localStorage.setItem('discussion-summary-toggle', 'false');
-        },
-      }),
-    ]);
+              }}
+            />
+          ))
+        }
+      </div>
+    );
   }
 }
