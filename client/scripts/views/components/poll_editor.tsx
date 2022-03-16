18d65742b788d359cdab9730fc051ae809b59938
Gabriel Duquette <gabrielduquette@gmail.com>
2022-03-15 21:35:24 -0500

    remove dead div and classes

--- client/scripts/views/components/poll_editor.tsx
+++ client/scripts/views/components/poll_editor.tsx
@@ -43,176 +43,170 @@ export class PollEditor implements m.ClassComponent<PollEditorAttrs> {
     }
 
     return (
-      <div class="PollEditor">
-        <Dialog
-          basic={false}
-          closeOnEscapeKey={true}
-          closeOnOutsideClick={true}
-          class="PollEditorDialog"
-          content={[
-            <Switch
-              intent="positive"
-              label="Enable polling"
-              onchange={(e) => {
-                this.pollingEnabled = (e.target as any).checked;
-              }}
-            />,
-            <h4 class={this.pollingEnabled ? '' : 'disabled'}>Question</h4>,
-            <Input
-              class="poll-editor-choices-question"
-              name="Question"
+      <Dialog
+        basic={false}
+        closeOnEscapeKey={true}
+        closeOnOutsideClick={true}
+        class="PollEditorDialog"
+        content={[
+          <Switch
+            intent="positive"
+            label="Enable polling"
+            onchange={(e) => {
+              this.pollingEnabled = (e.target as any).checked;
+            }}
+          />,
+          <h4 class={this.pollingEnabled ? '' : 'disabled'}>Question</h4>,
+          <Input
+            class="poll-editor-choices-question"
+            name="Question"
+            fluid={true}
+            autocomplete="off"
+            disabled={!pollingEnabled}
+            placeholder="Do you support this proposal?"
+            onchange={(e) => {
+              this.name = (e.target as any).value;
+            }}
+          />,
+          <h4 class={this.pollingEnabled ? '' : 'disabled'}>Choices</h4>,
+          <div class="poll-editor-choices">
+            <div class="poll-editor-choice-buttons">
+              {this.choices?.map((choice: string, index: number) => (
+                <Input
+                  class="poll-editor-choice"
+                  placeholder={`${index + 1}.`}
+                  fluid={true}
+                  autocomplete="off"
+                  disabled={!pollingEnabled}
+                  onchange={(e) => {
+                    this.choices[index] = (e.target as any).value;
+                  }}
+                />
+              ))}
+            </div>
+            <Button
+              label="Add choice"
               fluid={true}
-              autocomplete="off"
-              disabled={!pollingEnabled}
-              placeholder="Do you support this proposal?"
-              onchange={(e) => {
-                this.name = (e.target as any).value;
+              rounded={true}
+              disabled={!pollingEnabled || this.choices.length >= 6}
+              onclick={() => {
+                this.choices.push('');
               }}
-            />,
-            <h4 class={this.pollingEnabled ? '' : 'disabled'}>Choices</h4>,
-            <div class="poll-editor-choices">
-              <div class="poll-editor-choice-buttons">
-                {this.choices?.map((choice: string, index: number) => (
-                  <Input
-                    class="poll-editor-choice"
-                    placeholder={`${index + 1}.`}
-                    fluid={true}
-                    autocomplete="off"
-                    disabled={!pollingEnabled}
-                    onchange={(e) => {
-                      this.choices[index] = (e.target as any).value;
-                    }}
-                  />
-                ))}
-              </div>
-              <Button
-                class="poll-editor-add-choice"
-                label="Add choice"
-                fluid={true}
-                rounded={true}
-                disabled={!pollingEnabled || this.choices.length >= 6}
-                onclick={() => {
-                  this.choices.push('');
-                }}
-              />
-              <Button
-                class="poll-editor-remove-choice"
-                label="Remove choice"
-                fluid={true}
-                rounded={true}
-                disabled={!pollingEnabled || this.choices.length <= 2}
-                onclick={() => {
-                  this.choices.pop();
-                }}
-              />
-            </div>,
-            <div class="poll-duration-copy">
-              {customDurationEnabled
-                ? customDuration === 'Infinite'
-                  ? 'This poll will never expire.'
-                  : [
-                      'If started now, this poll will stay open until ',
-                      moment()
-                        .add(customDuration.split(' ')[0], 'days')
-                        .local()
-                        .format('lll'),
-                      '.',
-                    ]
+            />
+            <Button
+              label="Remove choice"
+              fluid={true}
+              rounded={true}
+              disabled={!pollingEnabled || this.choices.length <= 2}
+              onclick={() => {
+                this.choices.pop();
+              }}
+            />
+          </div>,
+          <div class="poll-duration-copy">
+            {customDurationEnabled
+              ? customDuration === 'Infinite'
+                ? 'This poll will never expire.'
                 : [
-                    'By default, offchain polls run for at least 5 days, ending on the 1st and 15th of each month. ',
-                    'If started now, this poll would stay open until ',
-                    getNextOffchainPollEndingTime(moment())
+                    'If started now, this poll will stay open until ',
+                    moment()
+                      .add(customDuration.split(' ')[0], 'days')
                       .local()
                       .format('lll'),
-                    '. Override?',
-                  ]}
-            </div>,
-            <div class="poll-editor-duration">
-              <Switch
-                intent="positive"
-                disabled={!pollingEnabled}
-                label="Custom duration"
-                onchange={() => {
-                  this.customDurationEnabled = !this.customDurationEnabled;
-                  this.customDuration = 'Infinite';
-                }}
-              />
-              <SelectList
-                class="custom-duration-items"
-                filterable={false}
-                items={['Infinite'].concat(
-                  _.range(1, 31).map((n) => pluralize(Number(n), 'day'))
-                )}
-                itemRender={(n) => <div class="duration-item">{n}</div>}
-                onSelect={(e) => {
-                  this.customDuration = e as string;
-                }}
-                trigger={
-                  <Button
-                    disabled={!pollingEnabled || !customDurationEnabled}
-                    align="left"
-                    compact={true}
-                    iconRight={Icons.CHEVRON_DOWN}
-                    label={this.customDuration || 'Infinite'}
-                    style="min-width: 300px"
-                  />
-                }
-              />
-            </div>,
-          ]}
-          hasBackdrop={true}
-          isOpen={true}
-          inline={false}
-          onClose={() => {
-            vnode.attrs.onChangeHandler();
-          }}
-          title="Create poll"
-          transitionDuration={200}
-          footer={
-            <div class={Classes.ALIGN_RIGHT}>
-              <Button
-                label="Cancel"
-                rounded={true}
-                onclick={() => {
-                  vnode.attrs.onChangeHandler();
-                }}
-              />
-              ,
-              <Button
-                label="Save changes"
-                intent="primary"
-                rounded={true}
-                onclick={async () => {
-                  if (this.pollingEnabled) {
-                    if (!this.name) {
-                      notifyError('Must set poll name');
-                      return;
-                    }
-                    if (!this.choices[0]?.length || !this.choices[1]?.length) {
-                      notifyError('Must set poll choices');
-                      return;
-                    }
-                    try {
-                      await app.threads.setPolling({
-                        threadId: thread.id,
-                        name: this.name,
-                        choices: this.choices,
-                        customDuration: this.customDuration,
-                      });
-                      notifySuccess('Poll creation succeeded');
-                    } catch (e) {
-                      console.error(e);
-                    }
-                    vnode.attrs.onChangeHandler();
-                  } else {
-                    vnode.attrs.onChangeHandler();
+                    '.',
+                  ]
+              : [
+                  'By default, offchain polls run for at least 5 days, ending on the 1st and 15th of each month. ',
+                  'If started now, this poll would stay open until ',
+                  getNextOffchainPollEndingTime(moment()).local().format('lll'),
+                  '. Override?',
+                ]}
+          </div>,
+          <div class="poll-editor-duration">
+            <Switch
+              intent="positive"
+              disabled={!pollingEnabled}
+              label="Custom duration"
+              onchange={() => {
+                this.customDurationEnabled = !this.customDurationEnabled;
+                this.customDuration = 'Infinite';
+              }}
+            />
+            <SelectList
+              class="custom-duration-items"
+              filterable={false}
+              items={['Infinite'].concat(
+                _.range(1, 31).map((n) => pluralize(Number(n), 'day'))
+              )}
+              itemRender={(n) => <div class="duration-item">{n}</div>}
+              onSelect={(e) => {
+                this.customDuration = e as string;
+              }}
+              trigger={
+                <Button
+                  disabled={!pollingEnabled || !customDurationEnabled}
+                  align="left"
+                  compact={true}
+                  iconRight={Icons.CHEVRON_DOWN}
+                  label={this.customDuration || 'Infinite'}
+                  style="min-width: 300px"
+                />
+              }
+            />
+          </div>,
+        ]}
+        hasBackdrop={true}
+        isOpen={true}
+        inline={false}
+        onClose={() => {
+          vnode.attrs.onChangeHandler();
+        }}
+        title="Create poll"
+        transitionDuration={200}
+        footer={
+          <div class={Classes.ALIGN_RIGHT}>
+            <Button
+              label="Cancel"
+              rounded={true}
+              onclick={() => {
+                vnode.attrs.onChangeHandler();
+              }}
+            />
+            ,
+            <Button
+              label="Save changes"
+              intent="primary"
+              rounded={true}
+              onclick={async () => {
+                if (this.pollingEnabled) {
+                  if (!this.name) {
+                    notifyError('Must set poll name');
+                    return;
                   }
-                }}
-              />
-            </div>
-          }
-        />
-      </div>
+                  if (!this.choices[0]?.length || !this.choices[1]?.length) {
+                    notifyError('Must set poll choices');
+                    return;
+                  }
+                  try {
+                    await app.threads.setPolling({
+                      threadId: thread.id,
+                      name: this.name,
+                      choices: this.choices,
+                      customDuration: this.customDuration,
+                    });
+                    notifySuccess('Poll creation succeeded');
+                  } catch (e) {
+                    console.error(e);
+                  }
+                  vnode.attrs.onChangeHandler();
+                } else {
+                  vnode.attrs.onChangeHandler();
+                }
+              }}
+            />
+          </div>
+        }
+      />
     );
   }
 }
