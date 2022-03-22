8d833c42a3850e1a1ccfeccf7b972976534f49ee
sha14r <shamant@commonwealth.im>
2022-03-22 23:44:12 +0530

    Update ipfs_pins.ts

--- server/models/ipfs_pins.ts
+++ server/models/ipfs_pins.ts
@@ -24,6 +24,8 @@ export default (
       id: { type: dataTypes.INTEGER, autoIncrement: true, primaryKey: true },
       address_id: { type: dataTypes.INTEGER, allowNull: false },
       ipfs_hash: { type: dataTypes.STRING, allowNull: false },
+      created_at: { type: dataTypes.DATE, allowNull: false },
+      updated_at: { type: dataTypes.DATE, allowNull: false },
     },
     {
       tableName: 'IpfsPins',
