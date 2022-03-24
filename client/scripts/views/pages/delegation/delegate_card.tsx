e4ef803833fd126b00fad46bc76ddb582eed5e2e
Alex Young <alexyoung@Alexs-M1-MBP.lan>
2022-03-24 11:19:10 -0700

    progress

--- client/scripts/views/pages/delegation/delegate_card.tsx
+++ client/scripts/views/pages/delegation/delegate_card.tsx
@@ -1,11 +1,27 @@
 /* @jsx m */
 import m from 'mithril';
+import { CWCard } from '../../components/component_kit/cw_card';
+import 'pages/delegation/delegate_card.scss';
 
 type DelegateCardAttrs = { topic?: string };
 
 class DelegateCard implements m.ClassComponent<DelegateCardAttrs> {
   view(vnode) {
-    return <div>hiii</div>;
+    return (
+      <CWCard
+        elevation="elevation-2"
+        interactive={true}
+        className="delegate-card"
+      >
+        <div class="card-body">
+          <div class="top-section">
+            <div class="top-left"></div>
+            <div class="top-right"></div>
+          </div>
+          <div class="bottom-section"></div>
+        </div>
+      </CWCard>
+    );
   }
 }
 
