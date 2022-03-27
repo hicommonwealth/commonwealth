99bda07c603333c7b394ed1c9fb8aaabc31ddf76
Alex Young <alexyoung@Alexs-M1-MBP.lan>
2022-03-26 19:01:25 -0700

    prog

--- client/scripts/views/pages/delegation/index.tsx
+++ client/scripts/views/pages/delegation/index.tsx
@@ -2,14 +2,22 @@
 import m from 'mithril';
 import Sublayout from '../../sublayout';
 import DelegateCard from './delegate_card';
+import app from 'state';
+import { Account, AddressInfo, Profile } from 'client/scripts/models';
 
 type DelegationPageAttrs = { topic?: string };
 
 class DelegationPage implements m.ClassComponent<DelegationPageAttrs> {
+  private delegate: Account<any> | AddressInfo | Profile;
   view(vnode) {
+    if (!this.delegate) {
+      this.delegate = app.user.activeAccount; // TODO: Replace this with an actual fetch of the delegate
+      m.redraw();
+    }
     return (
       <Sublayout class="DelegationPage" title="Delegation">
-        <DelegateCard />
+        <div style="height: 50px"></div>
+        <DelegateCard delegate={this.delegate} />
       </Sublayout>
     );
   }
