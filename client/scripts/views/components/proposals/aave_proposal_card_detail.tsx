ca51ac9ff5a9fdad05e8c7630a4189b8624d2d89
Gabriel Duquette <gabrielduquette@gmail.com>
2022-04-11 15:12:20 -0500

    revert because i'm dumb

--- client/scripts/views/components/proposals/aave_proposal_card_detail.tsx
+++ client/scripts/views/components/proposals/aave_proposal_card_detail.tsx
@@ -61,19 +61,19 @@ export class AaveProposalCardDetail
         <div class="aave-voting">
           <div class="card-subheader">Voting</div>
           <div class="aave-turnout">
-            <p class="detail-highlight.emphasize">
+            <p class="detail-highlight emphasize">
               {roundVote(proposal.turnout * 100)}%
             </p>
             <p>of token holders</p>
           </div>
           <div class="aave-support">
-            <p class="detail-highlight.emphasize">
+            <p class="detail-highlight emphasize">
               {roundVote(proposal.support * 100)}%
             </p>
             <p>in favor</p>
           </div>
           <div class="aave-differential">
-            <p class="detail-highlight.emphasize">
+            <p class="detail-highlight emphasize">
               {roundVote(proposal.voteDifferential * 100)}%
             </p>
             <p>differential</p>
@@ -82,13 +82,13 @@ export class AaveProposalCardDetail
         <div class="aave-requirements">
           <div class="card-subheader">Required to pass</div>
           <div class="aave-turnout-requirement">
-            <p class="detail-highlight.emphasize">
+            <p class="detail-highlight emphasize">
               {proposal.minimumQuorum * 100}%
             </p>
             <p>of token holders</p>
           </div>
           <div class="aave-differential-requirement">
-            <p class="detail-highlight.emphasize">
+            <p class="detail-highlight emphasize">
               {proposal.minimumVoteDifferential * 100}%
             </p>
             <p>differential</p>
