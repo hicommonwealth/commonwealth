403efb06ac607e8c52653ba552440c72c3199974
Jake Naviasky <jake@commonwealth.im>
2022-04-11 15:22:29 -0400

    Stub out migration.

--- server/migrations/20220408182445-add-address-wallet-field.js
+++ server/migrations/20220408182445-add-address-wallet-field.js
@@ -9,11 +9,49 @@ module.exports = {
         allowNull: true,
       }, { transaction });
 
-      // TODO
       // populate column for all but default eth addresses
-      // 1. all is_magic = magic
-      // 2. all AxieInfinity = ronin, all Injective = inj-metamask, all Terra = terrastation
-      // 3. all solana base = phantom, all near base = near, all substrate = polkadot
+      // update by base
+      // TODO: base is on "chain" but not "address", need to figure out how to make that query
+      await queryInterface.bulkUpdate('Addresses',
+        { wallet_id: 'phantom' },
+        { base: 'solana' },
+        { transaction, });
+      await queryInterface.bulkUpdate('Addresses',
+        { wallet_id: 'near' },
+        { base: 'near' },
+        { transaction, });
+      await queryInterface.bulkUpdate('Addresses',
+        { wallet_id: 'polkadot' },
+        { base: 'substrate' },
+        { transaction, });
+      await queryInterface.bulkUpdate('Addresses',
+        { wallet_id: 'keplr' },
+        { base: 'cosmos' },
+        { transaction, });
+
+      // update by network
+      await queryInterface.bulkUpdate('Addresses',
+        { wallet_id: 'ronin' },
+        { network: 'axie-infinity' },
+        { transaction, });
+      await queryInterface.bulkUpdate('Addresses',
+        { wallet_id: 'inj-metamask' },
+        { network: 'injective' },
+        { transaction, });
+      await queryInterface.bulkUpdate('Addresses',
+        { wallet_id: 'inj-metamask' },
+        { network: 'injective-testnet' },
+        { transaction, });
+      await queryInterface.bulkUpdate('Addresses',
+        { wallet_id: 'terrastation' },
+        { network: 'terra' },
+        { transaction, });
+
+      // update for magic specifically
+      await queryInterface.bulkUpdate('Addresses',
+        { wallet_id: 'magic' },
+        { is_magic: true },
+        { transaction, });
 
       // drop is_magic field (use magic wallet instead)
       await queryInterface.removeColumn('Addresses', 'is_magic', { transaction });
@@ -28,8 +66,12 @@ module.exports = {
         defaultValue: false,
       }, { transaction });
 
-      // TODO
       // repopulate is_magic based on wallet_id === 'magic'
+      await queryInterface.bulkUpdate('Addresses',
+      { is_magic: true },
+      { wallet_id: 'magic' },
+      { transaction, });
+
 
       await queryInterface.removeColumn('Addresses', 'wallet_id', { transaction });
     });
