f3b571492c15fe889cb44ac35d8e9d5ba0485341
Dexter Callender <dec2148@columbia.edu>
2022-04-14 12:01:52 -0400

    Update remove function

--- server/migrations/20220414153911-add-avatarUrl-and-slug-to-profile.js
+++ server/migrations/20220414153911-add-avatarUrl-and-slug-to-profile.js
@@ -20,7 +20,7 @@ module.exports = {
         type: Sequelize.STRING,
         allowNull: true,
       }, { transaction: t });
-      await queryInterface.removeColumn('Profiles', 'avatarUrl', {
+      await queryInterface.removeColumn('Profiles', 'slug', {
         type: Sequelize.STRING,
         allowNull: true,
       }, { transaction: t });
