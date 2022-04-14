4ca815774a705b18aa8b5ef62afd4159d2df42ee
Dexter Callender <dec2148@columbia.edu>
2022-04-14 11:49:50 -0400

    Profiles migration to add avatarURL and slug

---
+++ server/migrations/20220414153911-add-avatarUrl-and-slug-to-profile.js
@@ -0,0 +1,29 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.sequelize.transaction(async (t) => {
+      await queryInterface.addColumn('Profiles', 'avatarUrl', {
+        type: Sequelize.STRING,
+        allowNull: true
+      }, { transaction: t });
+      await queryInterface.addColumn('Profiles', 'slug', {
+        type: Sequelize.STRING,
+        allowNull: true
+      }, { transaction: t });
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.sequelize.transaction(async (t) => {
+      await queryInterface.removeColumn('Profiles', 'avatarUrl', {
+        type: Sequelize.STRING,
+        allowNull: true,
+      }, { transaction: t });
+      await queryInterface.removeColumn('Profiles', 'avatarUrl', {
+        type: Sequelize.STRING,
+        allowNull: true,
+      }, { transaction: t });
+    });
+  }
+};
