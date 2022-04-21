9fb671010592f27fedac55a83ac7234883fa42ab
Gabriel Duquette <gabrielduquette@gmail.com>
2022-04-21 18:46:44 -0500

    typeguard this too, why not

--- client/scripts/views/components/component_kit/helpers.ts
+++ client/scripts/views/components/component_kit/helpers.ts
@@ -1,11 +1,9 @@
-import { isNotUndefined } from 'helpers/typeGuards';
+import { isBoolean, isNotUndefined } from 'helpers/typeGuards';
 
 export const getClasses = <T>(styleAttrs: T, componentType?: string): string =>
   `${isNotUndefined(componentType) ? `${componentType} ` : ''}${Object.entries(
     styleAttrs
   )
     .filter(([key, value]) => isNotUndefined(key) && isNotUndefined(value))
-    .map(([key, value]) =>
-      typeof value === 'boolean' ? (value ? key : null) : value
-    )
+    .map(([key, value]) => (isBoolean(value) ? (value ? key : null) : value))
     .join(' ')}`;
