a80e387de33bcb4603024a7bedd7d97245d5038c
Gabriel Duquette <gabrielduquette@gmail.com>
2022-04-21 18:48:37 -0500

    broader guard

--- client/scripts/views/components/component_kit/helpers.ts
+++ client/scripts/views/components/component_kit/helpers.ts
@@ -1,9 +1,9 @@
-import { isBoolean, isNotUndefined } from 'helpers/typeGuards';
+import { isBoolean, isNil, isNotUndefined } from 'helpers/typeGuards';
 
 export const getClasses = <T>(styleAttrs: T, componentType?: string): string =>
   `${isNotUndefined(componentType) ? `${componentType} ` : ''}${Object.entries(
     styleAttrs
   )
-    .filter(([key, value]) => isNotUndefined(key) && isNotUndefined(value))
+    .filter(([key, value]) => !isNil(key) && !isNil(value))
     .map(([key, value]) => (isBoolean(value) ? (value ? key : null) : value))
     .join(' ')}`;
