bd77e5382316acbd646b9743b8c0d9a39f5b6dfe
Gabriel Duquette <gabrielduquette@gmail.com>
2022-04-21 16:47:20 -0500

    unify classes helpers

--- client/scripts/views/components/component_kit/helpers.ts
+++ client/scripts/views/components/component_kit/helpers.ts
@@ -1,32 +1,11 @@
-import { ButtonStyleAttrs } from './cw_button';
-import { EngagementButtonStyleAttrs } from './cw_engagement_button';
-import { IconStyleAttrs } from './cw_icons/cw_icon';
-import { InputStyleAttrs } from './cw_text_input';
+import { isNotUndefined } from 'helpers/typeGuards';
 
-export const getButtonClasses = (
-  componentType: string,
-  styleAttrs: ButtonStyleAttrs | EngagementButtonStyleAttrs
-): string =>
-  `${componentType} ${Object.entries(styleAttrs)
-    .filter(([key, value]) => key && value) // filters out keys that don't have values
-    .map(
-      ([key, value]) =>
-        key === 'disabled' ? (value === true ? 'disabled' : null) : value // returns disabled string instead of boolean
-    )
-    .join(' ')}`;
-
-export const getIconClasses = (styleAttrs: IconStyleAttrs): string =>
-  `${Object.entries(styleAttrs)
+export const getClasses = <T>(styleAttrs: T, componentType?: string): string =>
+  `${isNotUndefined(componentType) ? `${componentType} ` : ''}${Object.entries(
+    styleAttrs
+  )
     .filter(([key, value]) => key && value)
     .map(([key, value]) =>
-      key === 'disabled' ? (value === true ? 'disabled' : null) : value
-    )
-    .join(' ')}`;
-
-export const getTextInputClasses = (styleAttrs: InputStyleAttrs): string =>
-  `${Object.entries(styleAttrs)
-    .filter(([key, value]) => key && value)
-    .map(([key, value]) =>
-      key === 'disabled' ? (value === true ? 'disabled' : null) : value
+      typeof value === 'boolean' ? (value ? key : null) : value
     )
     .join(' ')}`;
