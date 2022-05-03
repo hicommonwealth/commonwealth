46b7c52a8cb7436e9dfd060daa9c93c66b0a4719
Timothee Legros <62490329+timolegros@users.noreply.github.com>
2022-05-03 17:22:10 -0600

    Add authentication to websocket dashboard (#1833)
    Co-authored-by: zakhap <zhs2104@columbia.edu>

--- server/socket/index.ts
+++ server/socket/index.ts
@@ -12,8 +12,9 @@ import { createClient } from 'redis';
 import { createCeNamespace, publishToCERoom } from './chainEventsNs';
 import { RabbitMQController } from '../util/rabbitmq/rabbitMQController';
 import RabbitMQConfig from '../util/rabbitmq/RabbitMQConfig';
-import { JWT_SECRET, REDIS_URL } from '../config';
+import { JWT_SECRET, REDIS_URL, WEBSOCKET_ADMIN_USERNAME, WEBSOCKET_ADMIN_PASSWORD } from '../config';
 import { factory, formatFilename } from '../../shared/logging';
+import bcrypt from "bcrypt";
 
 const log = factory.getLogger(formatFilename(__filename));
 
@@ -69,7 +70,11 @@ export function setupWebSocketServer(httpServer: http.Server) {
 
   // enables the admin analytics dashboard (creates /admin namespace)
   instrument(io, {
-    auth: false,
+    auth: origin.includes("localhost") ? false : {
+      type: "basic",
+      username: WEBSOCKET_ADMIN_USERNAME,
+      password: bcrypt.hashSync(WEBSOCKET_ADMIN_PASSWORD, WEBSOCKET_ADMIN_PASSWORD.length)
+    }
   });
 
   log.info(`Connecting to Redis at: ${REDIS_URL}`);
