ddf827002de1e3f23d4ad195816d780ea522b023
timolegros <timothee@ualberta.ca>
2022-05-05 15:23:48 -0600

    only create adapter if redis connection successful

--- server/socket/index.ts
+++ server/socket/index.ts
@@ -90,6 +90,8 @@ export async function setupWebSocketServer(
 
   try {
     await Promise.all([pubClient.connect(), subClient.connect()]);
+    // provide the redis connection instances to the socket.io adapters
+    await io.adapter(<any>createAdapter(pubClient, subClient));
   } catch (e) {
     // local env may not have redis so don't do anything if they don't
     if (!origin.includes('localhost')) {
@@ -101,8 +103,6 @@ export async function setupWebSocketServer(
       );
     }
   }
-  // provide the redis connection instances to the socket.io adapters
-  await io.adapter(<any>createAdapter(pubClient, subClient));
 
   // create the chain-events namespace
   const ceNamespace = createCeNamespace(io);
