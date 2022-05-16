91078a828f0cd527ed74b0c5c168d6d22ebb94c4
Alex Young <alexyoung@Alexs-M1-MBP.lan>
2022-05-15 17:25:17 -0700

    fixes

--- server/util/mixpanelUtil.ts
+++ server/util/mixpanelUtil.ts
@@ -2,19 +2,20 @@ import { BaseMixpanelPayload } from '../../shared/analytics/types';
 var Mixpanel = require('mixpanel');
 var mixpanelNode;
 if (process.env.NODE_ENV === 'production') {
-  mixpanelNode = Mixpanel.init(process.env.MIXPANEL_DEV_TOKEN); //TODO: Swap with prod token when tested
+  mixpanelNode = Mixpanel.init(process.env.MIXPANEL_PROD_TOKEN); //TODO: Swap with prod token when tested
 } else if (process.env.NODE_ENV === 'development') {
+  // NOTE: Only works if NODE_ENV defined in .env
+  // Make sure that is set to development if you want to use backend Mixpanel locally.
   mixpanelNode = Mixpanel.init(process.env.MIXPANEL_DEV_TOKEN);
 }
-// TODO: Figure out how to change to production
 
 // ----- Server Side Mixpanel Library Utils ------ //
 export function mixpanelTrack<T extends BaseMixpanelPayload>(data: T) {
   const { event, ...payload } = data;
 
-  mixpanelNode.track(event, payload);
+  mixpanelNode?.track(event, payload);
 }
 
 export function mixpanelPeopleSet(unique_id: string) {
-  mixpanelNode.people.set(unique_id);
+  mixpanelNode?.people.set(unique_id);
 }
