75c8e5994099b7606a5d328ad207de334481e18a
gdjohnson <graham@commonwealth.im>
2022-05-17 17:48:25 -0700

    User Dashboard Update (#1871)
    * Set up routes, change toggle
    
    * Iterate
    
    * New attempt
    
    * Fix default export issue
    
    * Break apart request types to allow separate jwt reqs
    
    * Finalize routes
    
    * Finishing touches
    
    * Better routes
    
    * Update route schema, notifyInfo copy
    
    Co-authored-by: Graham Johnson <graham@commonwealth.com>
    Co-authored-by: zakhap <zhs2104@columbia.edu>

---
+++ server/routes/viewChainActivity.ts
@@ -0,0 +1,26 @@
+import { Request, Response, NextFunction } from 'express';
+import { DB } from '../database';
+
+const viewChainActivity = async (
+  models: DB,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const query = `
+    SELECT ce.*, cet.chain, cet.event_network, c.icon_url FROM "ChainEvents" ce
+    INNER JOIN "ChainEventTypes" cet ON ce.chain_event_type_id = cet.id 
+    INNER JOIN "Chains" c ON c.id = cet.chain 
+    ORDER BY ce.created_at DESC 
+    LIMIT 50;
+  `;
+
+  const notifications = await models.sequelize.query(query, {
+    type: 'SELECT',
+    raw: true,
+  });
+
+  return res.json({ status: 'Success', result: notifications });
+};
+
+export default viewChainActivity;
