27e258c96945979dafc6671ea11443920ac01a07
timolegros <timothee@ualberta.ca>
2022-05-23 09:27:50 -0400

    migration revert func

--- server/migrations/20220521055322-add-id-to-notifications-read.js
+++ server/migrations/20220521055322-add-id-to-notifications-read.js
@@ -36,11 +36,6 @@ module.exports = {
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    await queryInterface.removeColumn('NotificationsRead', 'id');
   }
 };
