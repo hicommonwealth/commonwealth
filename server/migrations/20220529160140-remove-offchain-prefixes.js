aa8cbab4ad39be474388b0246dba1741cb2748af
Jake Naviasky <jake@commonwealth.im>
2022-05-29 12:02:24 -0400

    Add initial migration.

---
+++ server/migrations/20220529160140-remove-offchain-prefixes.js
@@ -0,0 +1,23 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    // Remove offchain prefixes from all database tables
+
+    // Zak
+    // TODO
+
+    // Jake
+    // TODO
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    // Re-add offchain prefixes
+
+    // Zak
+    // TODO
+
+    // Jake
+    // TODO
+  }
+};
