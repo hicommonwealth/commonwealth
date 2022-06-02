52b42e7320794665f4ceffe25d14fb3fe79fd467
Graham Johnson <graham@commonwealth.com>
2022-06-02 12:19:25 -0500

    Change axie id

--- client/scripts/views/components/sidebar/index.tsx
+++ client/scripts/views/components/sidebar/index.tsx
@@ -13,7 +13,7 @@ import { ChatSection } from '../chat/chat_section';
 
 export class Sidebar implements m.ClassComponent {
   view() {
-    const hideChat = ['terra', 'axie'].includes(app.activeChainId());
+    const hideChat = ['terra', 'axie-infinity'].includes(app.activeChainId());
     return app.chain ? (
       <div class="Sidebar">
         <DiscussionSection />
