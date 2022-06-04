9538987194f8597e5e2a2417e9641f0912c29ab0
zakhap <zhs2104@columbia.edu>
2022-06-03 17:52:20 -0700

    Add banner model, write migration, add to db

---
+++ server/models/community_banner.ts
@@ -0,0 +1,40 @@
+import * as Sequelize from 'sequelize';
+import { Model, DataTypes } from 'sequelize';
+import { ModelStatic, ModelInstance } from './types';
+
+export type CommunityBannerAttributes = {
+  id?: number;
+  address: string;
+  message: string;
+  chat_channel_id: number;
+  created_at?: Date;
+  updated_at?: Date;
+}
+
+export type CommunityBannerInstance = ModelInstance<CommunityBannerAttributes> & {
+}
+
+export type CommunityBannerModelStatic = ModelStatic<CommunityBannerInstance>;
+
+export default (
+  sequelize: Sequelize.Sequelize,
+  dataTypes: typeof DataTypes,
+): CommunityBannerModelStatic => {
+  const CommunityBanner = <CommunityBannerModelStatic>sequelize.define('CommunityBanner', {
+    id: { type: dataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    banner_text: { type: dataTypes.TEXT, allowNull: false },
+    chain_id: { type: dataTypes.STRING, allowNull: false },
+  }, {
+    tableName: 'CommunityBanners',
+    underscored: true,
+    createdAt: 'created_at',
+    updatedAt: 'updated_at',
+    timestamps: true,
+  });
+
+  CommunityBanner.associate = (models) => {
+    models.CommunityBanner.belongsTo(models.Chain)
+  }
+
+  return CommunityBanner;
+};
