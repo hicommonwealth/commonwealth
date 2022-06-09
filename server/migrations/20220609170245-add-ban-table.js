27749aad8b0fe952f92b5571cb0b1f41c4e09bd9
zakhap <zhs2104@columbia.edu>
2022-06-09 10:14:15 -0700

    Add ban model, migration, and update database.ts

---
+++ server/migrations/20220609170245-add-ban-table.js
@@ -0,0 +1,21 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+     return queryInterface.sequelize.transaction(async (transaction) => {
+      await queryInterface.createTable('Bans', {
+        id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true },
+        address_id: { type: Sequelize.INTEGER, allowNull: false },
+        chain_id: { type: Sequelize.STRING, allowNull: false, references: { model: 'Chains', key: 'id' } },
+        created_at: { type: Sequelize.DATE, allowNull: false },
+        updated_at: { type: Sequelize.DATE, allowNull: false },
+      }, { transaction });
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.sequelize.transaction(async (transaction) => {
+      await queryInterface.dropTable('Bans', { transaction } );
+    });
+  }
+};
