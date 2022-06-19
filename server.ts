5b3b9164d1faf081dfd48116c4c663c474afdf8d
zakhap <zhs2104@columbia.edu>
2022-06-19 14:40:46 -0700

    Clean up

--- server.ts
+++ server.ts
@@ -234,12 +234,13 @@ async function main() {
     // add security middleware
     app.use(function applyXFrame(req, res, next) {
       res.set('X-Frame-Options', 'DENY');
-      next(); 
-    });
-    app.use(function applyCSP(req, res, next) {
       res.set('Content-Security-Policy', "frame-ancestors 'none';");
-      next(); 
-  });
+      next();
+    });
+    // app.use(function applyCSP(req, res, next) {
+    //   res.set('Content-Security-Policy', "frame-ancestors 'none';");
+    //   next();
+  // });
 
     // serve static files
     app.use(favicon(`${__dirname}/favicon.ico`));
