8a1a3ab794057561680990c9aa087e73b13d644e
Gabriel Duquette <gabrielduquette@gmail.com>
2022-06-19 21:55:34 -0400

    finish refactor

---
+++ client/scripts/views/components/proposals/voting_result_components.tsx
@@ -0,0 +1,306 @@
+/* @jsx m */
+/* eslint-disable max-classes-per-file */
+
+import m from 'mithril';
+
+import 'components/proposals/voting_result_components.scss';
+
+import { AnyProposal, IVote } from 'models';
+import { NearSputnikVoteString } from 'controllers/chain/near/sputnik/types';
+import { VoteListing } from './vote_listing';
+
+type BaseVotingResultAttrs = {
+  proposal: AnyProposal;
+  votes: Array<IVote<any>>;
+};
+
+type VotingResultAttrs = {
+  getAbstainVotes?: () => void;
+  getNoVotes: () => void;
+  getYesVotes: () => void;
+  proposal: AnyProposal;
+};
+
+export class VotingResult implements m.ClassComponent<VotingResultAttrs> {
+  view(vnode) {
+    const { getAbstainVotes, getNoVotes, getYesVotes, proposal } = vnode.attrs;
+
+    return (
+      <div class="VotingResult">
+        <div class="results-column yes-votes">
+          <div class="results-header">{`Yes (${getYesVotes.length})`}</div>
+          <div class="results-cell">
+            <VoteListing proposal={proposal} votes={getYesVotes} />
+          </div>
+        </div>
+        <div class="results-column no-votes">
+          <div class="results-header">{`No (${getNoVotes.length})`}</div>
+          <div class="results-cell">
+            <VoteListing proposal={proposal} votes={getNoVotes} />
+          </div>
+        </div>
+        {getAbstainVotes && (
+          <div class="results-column no-votes">
+            <div class="results-header">
+              {`Abstain (${getAbstainVotes.length})`}
+            </div>
+            <div class="results-cell">
+              <VoteListing proposal={proposal} votes={getAbstainVotes} />
+            </div>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+
+type AaveVotingResultAttrs = {
+  noBalanceString: string;
+  noVotesCount: number;
+  yesBalanceString: string;
+  yesVotesCount: number;
+} & BaseVotingResultAttrs;
+
+export class AaveVotingResult
+  implements m.ClassComponent<AaveVotingResultAttrs>
+{
+  view(vnode) {
+    const {
+      noBalanceString,
+      noVotesCount,
+      proposal,
+      votes,
+      yesBalanceString,
+      yesVotesCount,
+    } = vnode.attrs;
+
+    return (
+      <div class="VotingResult">
+        <div class="results-column yes-votes">
+          <div class="results-header">
+            {`Yes (${yesBalanceString} ${yesVotesCount}) voters`}
+          </div>
+          <div class="results-subheader">
+            <span>User</span>
+            <span>Power</span>
+          </div>
+          <div class="results-cell">
+            <VoteListing
+              proposal={proposal}
+              votes={votes.filter((v) => !!v.choice)}
+            />
+          </div>
+        </div>
+        <div class="results-column no-votes">
+          <div class="results-header">
+            {`No (${noBalanceString} ${noVotesCount}) voters`}
+          </div>
+          <div class="results-subheader">
+            <span>User</span>
+            <span>Power</span>
+          </div>
+          <div class="results-cell">
+            <VoteListing
+              proposal={proposal}
+              votes={votes.filter((v) => !v.choice)}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+type CompletedProposalVotingResultAttrs = {
+  abstainPct: string;
+  abstainResults: string;
+  noPct: string;
+  noResults: string;
+  noWithVetoPct: string;
+  noWithVetoResults: string;
+  yesPct: string;
+  yesResults: string;
+};
+
+export class CompletedProposalVotingResult
+  implements m.ClassComponent<CompletedProposalVotingResultAttrs>
+{
+  view(vnode) {
+    const {
+      abstainPct,
+      abstainResults,
+      noPct,
+      noResults,
+      noWithVetoPct,
+      noWithVetoResults,
+      yesPct,
+      yesResults,
+    } = vnode.attrs;
+
+    return (
+      <div class="VotingResult">
+        <div class="results-column">
+          <div class="results-header">{`${yesPct}% voted Yes`}</div>
+          <div class="results-cell">{`(${yesResults})`}</div>
+        </div>
+        <div class="results-column">
+          <div class="results-header">{`${noPct}% voted No`}</div>
+          <div class="results-cell">{`(${noResults})`}</div>
+        </div>
+        <div class="results-column">
+          <div class="results-header">{`${abstainPct}% voted Abstain`}</div>
+          <div class="results-cell">{`(${abstainResults})`}</div>
+        </div>
+        <div class="results-column">
+          <div class="results-header">{`${noWithVetoPct}% voted Veto`}</div>
+          <div class="results-cell">{`(${noWithVetoResults})`}</div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export class YesNoAbstainVetoVotingResult
+  implements m.ClassComponent<BaseVotingResultAttrs>
+{
+  view(vnode) {
+    const { proposal, votes } = vnode.attrs;
+
+    return (
+      <div class="VotingResult">
+        <div class="results-column">
+          <div class="results-header">
+            {`Voted yes (${votes.filter((v) => v.choice === 'Yes').length})`}
+          </div>
+          <div class="results-cell">
+            <VoteListing
+              proposal={proposal}
+              votes={votes.filter((v) => v.choice === 'Yes')}
+            />
+          </div>
+        </div>
+        <div class="results-column">
+          <div class="results-header">
+            {`Voted no (${votes.filter((v) => v.choice === 'No').length})`}
+          </div>
+          <div class="results-cell">
+            <VoteListing
+              proposal={proposal}
+              votes={votes.filter((v) => v.choice === 'No')}
+            />
+          </div>
+        </div>
+        <div class="results-column">
+          <div class="results-header">
+            {`Voted abstain (${
+              votes.filter((v) => v.choice === 'Abstain').length
+            })`}
+          </div>
+          <div class="results-cell">
+            <VoteListing
+              proposal={proposal}
+              votes={votes.filter((v) => v.choice === 'Abstain')}
+            />
+          </div>
+        </div>
+        <div class="results-column">
+          <div class="results-header">
+            {`Voted veto (${
+              votes.filter((v) => v.choice === 'NoWithVeto').length
+            })`}
+          </div>
+          <div class="results-cell">
+            <VoteListing
+              proposal={proposal}
+              votes={votes.filter((v) => v.choice === 'NoWithVeto')}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+type SimpleYesApprovalVotingResultAttrs = {
+  approvedCount: number;
+} & BaseVotingResultAttrs;
+
+export class SimpleYesApprovalVotingResult
+  implements m.ClassComponent<SimpleYesApprovalVotingResultAttrs>
+{
+  view(vnode) {
+    const { approvedCount, proposal, votes } = vnode.attrs;
+
+    return (
+      <div class="VotingResult">
+        <div class="results-column">
+          <div class="results-header">{`Approved ${approvedCount}`}</div>
+          <div class="results-cell">
+            <VoteListing proposal={proposal} votes={votes} />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export class YesNoRejectVotingResult
+  implements m.ClassComponent<BaseVotingResultAttrs>
+{
+  view(vnode) {
+    const { proposal, votes } = vnode.attrs;
+
+    return (
+      <div class="VotingResult">
+        <div class="results-column">
+          <div class="results-header">
+            {`Voted approve (${
+              votes.filter((v) => v.choice === NearSputnikVoteString.Approve)
+                .length
+            })`}
+          </div>
+          <div class="results-cell">
+            <VoteListing
+              proposal={proposal}
+              votes={votes.filter(
+                (v) => v.choice === NearSputnikVoteString.Approve
+              )}
+            />
+          </div>
+        </div>
+        <div class="results-column">
+          <div class="results-header">
+            {`Voted reject (${
+              votes.filter((v) => v.choice === NearSputnikVoteString.Reject)
+                .length
+            })`}
+          </div>
+          <div class="results-cell">
+            <VoteListing
+              proposal={proposal}
+              votes={votes.filter(
+                (v) => v.choice === NearSputnikVoteString.Reject
+              )}
+            />
+          </div>
+        </div>
+        <div class="results-column">
+          <div class="results-header">
+            {`Voted remove (${
+              votes.filter((v) => v.choice === NearSputnikVoteString.Remove)
+                .length
+            })`}
+          </div>
+          <div class="results-cell">
+            <VoteListing
+              proposal={proposal}
+              votes={votes.filter(
+                (v) => v.choice === NearSputnikVoteString.Remove
+              )}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
--- client/scripts/views/components/proposals/voting_results.tsx
+++ client/scripts/views/components/proposals/voting_results.tsx
@@ -4,8 +4,6 @@ import m from 'mithril';
 import BN from 'bn.js';
 import Web3 from 'web3';
 
-import 'components/proposals/voting_results.scss';
-
 import app from 'state';
 import { formatNumberLong, Coin } from 'adapters/currency';
 import { VotingType, AnyProposal } from 'models';
@@ -15,8 +13,14 @@ import { BravoVote } from 'controllers/chain/ethereum/compound/proposal';
 import AaveProposal, {
   AaveProposalVote,
 } from 'controllers/chain/ethereum/aave/proposal';
-import { NearSputnikVoteString } from 'controllers/chain/near/sputnik/types';
-import { VoteListing } from './vote_listing';
+import {
+  AaveVotingResult,
+  CompletedProposalVotingResult,
+  SimpleYesApprovalVotingResult,
+  VotingResult,
+  YesNoAbstainVetoVotingResult,
+  YesNoRejectVotingResult,
+} from './voting_result_components';
 
 export const VotingResults: m.Component<{ proposal: AnyProposal }> = {
   view: (vnode) => {
@@ -24,61 +28,61 @@ export const VotingResults: m.Component<{ proposal: AnyProposal }> = {
     const votes = proposal.getVotes();
 
     // TODO: fix up this function for cosmos votes
-    if (proposal.votingType === VotingType.SimpleYesNoVoting) {
+    if (
+      proposal.votingType === VotingType.SimpleYesNoVoting ||
+      proposal.votingType === VotingType.ConvictionYesNoVoting
+    ) {
       return (
-        <div class="VotingResults">
-          <div class="results-column yes-votes">
-            <div class="results-header">
-              {`Yes (${votes.filter((v) => v.choice === true).length})`}
-            </div>
-            <div class="results-cell">
-              <VoteListing
-                proposal={proposal}
-                votes={votes.filter((v) => v.choice === true)}
-              />
-            </div>
-          </div>
-          <div class="results-column no-votes">
-            <div class="results-header">
-              {`No (${votes.filter((v) => v.choice === false).length})`}
-            </div>
-            <div class="results-cell">
-              <VoteListing
-                proposal={proposal}
-                votes={votes.filter((v) => v.choice === false)}
-              />
-            </div>
-          </div>
-        </div>
+        <VotingResult
+          getYesVotes={votes.filter((v) => v.choice === true)}
+          getNoVotes={votes.filter((v) => v.choice === false)}
+          proposal={proposal}
+        />
       );
     } else if (proposal.votingType === VotingType.MolochYesNo) {
       return (
-        <div class="VotingResults">
-          <div class="results-column yes-votes">
-            <div class="results-header">
-              {`Yes (${
-                votes.filter((v) => v.choice === MolochVote.YES).length
-              })`}
-            </div>
-            <div class="results-cell">
-              <VoteListing
-                proposal={proposal}
-                votes={votes.filter((v) => v.choice === MolochVote.YES)}
-              />
-            </div>
-          </div>
-          <div class="results-column no-votes">
-            <div class="results-header">
-              {`No (${votes.filter((v) => v.choice === MolochVote.NO).length})`}
-            </div>
-            <div class="results-cell">
-              <VoteListing
-                proposal={proposal}
-                votes={votes.filter((v) => v.choice === MolochVote.NO)}
-              />
-            </div>
-          </div>
-        </div>
+        <VotingResult
+          getYesVotes={votes.filter((v) => v.choice === MolochVote.YES)}
+          getNoVotes={votes.filter((v) => v.choice === MolochVote.NO)}
+          proposal={proposal}
+        />
+      );
+    } else if (proposal.votingType === VotingType.CompoundYesNo) {
+      return (
+        <VotingResult
+          getYesVotes={votes.filter((v) => v.choice === BravoVote.YES)}
+          getNoVotes={votes.filter((v) => v.choice === BravoVote.NO)}
+          proposal={proposal}
+        />
+      );
+    } else if (proposal.votingType === VotingType.CompoundYesNoAbstain) {
+      return (
+        <VotingResult
+          getAbstainVotes={votes.filter((v) => v.choice === BravoVote.ABSTAIN)}
+          getYesVotes={votes.filter((v) => v.choice === BravoVote.YES)}
+          getNoVotes={votes.filter((v) => v.choice === BravoVote.NO)}
+          proposal={proposal}
+        />
+      );
+    } else if (
+      proposal.votingType === VotingType.SimpleYesApprovalVoting &&
+      proposal instanceof CosmosProposal
+    ) {
+      // special case for cosmos proposals in deposit stage
+      return (
+        <SimpleYesApprovalVotingResult
+          approvedCount={proposal.depositorsAsVotes.length}
+          proposal={proposal}
+          votes={proposal.depositorsAsVotes}
+        />
+      );
+    } else if (proposal.votingType === VotingType.SimpleYesApprovalVoting) {
+      return (
+        <SimpleYesApprovalVotingResult
+          approvedCount={votes.length}
+          proposal={proposal}
+          votes={votes}
+        />
       );
     } else if (proposal instanceof AaveProposal) {
       const yesVotes: AaveProposalVote[] = votes.filter((v) => !!v.choice);
@@ -104,140 +108,14 @@ export const VotingResults: m.Component<{ proposal: AnyProposal }> = {
       )} ${app.chain.meta.symbol}`;
 
       return (
-        <div class="VotingResults">
-          <div class="results-column yes-votes">
-            <div class="results-header">
-              {`Yes (${yesBalanceString} ${yesVotes.length}) voters`}
-            </div>
-            <div class="results-subheader">
-              <span>User</span>
-              <span>Power</span>
-            </div>
-            <div class="results-cell">
-              <VoteListing
-                proposal={proposal}
-                votes={votes.filter((v) => !!v.choice)}
-              />
-            </div>
-          </div>
-          <div class="results-column no-votes">
-            <div class="results-header">
-              {`No (${noBalanceString} ${noVotes.length}) voters`}
-            </div>
-            <div class="results-subheader">
-              <span>User</span>
-              <span>Power</span>
-            </div>
-            <div class="results-cell">
-              <VoteListing
-                proposal={proposal}
-                votes={votes.filter((v) => !v.choice)}
-              />
-            </div>
-          </div>
-        </div>
-      );
-    } else if (proposal.votingType === VotingType.CompoundYesNo) {
-      return (
-        <div class="VotingResults">
-          <div class="results-column yes-votes">
-            <div class="results-header">
-              {`Yes (${
-                votes.filter((v) => v.choice === BravoVote.YES).length
-              })`}
-            </div>
-            <div class="results-cell">
-              <VoteListing
-                proposal={proposal}
-                votes={votes.filter((v) => v.choice === BravoVote.YES)}
-              />
-            </div>
-          </div>
-          <div class="results-column no-votes">
-            <div class="results-header">
-              {`No (${votes.filter((v) => v.choice === BravoVote.NO).length})`}
-            </div>
-            <div class="results-cell">
-              <VoteListing
-                proposal={proposal}
-                votes={votes.filter((v) => v.choice === BravoVote.NO)}
-              />
-            </div>
-          </div>
-        </div>
-      );
-    } else if (proposal.votingType === VotingType.CompoundYesNoAbstain) {
-      return (
-        <div class="VotingResults">
-          <div class="results-column yes-votes">
-            <div class="results-header">
-              {`Yes (${
-                votes.filter((v) => v.choice === BravoVote.YES).length
-              })`}
-            </div>
-            <div class="results-cell">
-              <VoteListing
-                proposal={proposal}
-                votes={votes.filter((v) => v.choice === BravoVote.YES)}
-              />
-            </div>
-          </div>
-          <div class="results-column no-votes">
-            <div class="results-header">
-              {`No (${votes.filter((v) => v.choice === BravoVote.NO).length})`}
-            </div>
-            <div class="results-cell">
-              <VoteListing
-                proposal={proposal}
-                votes={votes.filter((v) => v.choice === BravoVote.NO)}
-              />
-            </div>
-          </div>
-          <div class="results-column no-votes">
-            <div class="results-header">
-              {`Abstain (${
-                votes.filter((v) => v.choice === BravoVote.ABSTAIN).length
-              })`}
-            </div>
-            <div class="results-cell">
-              <VoteListing
-                proposal={proposal}
-                votes={votes.filter((v) => v.choice === BravoVote.ABSTAIN)}
-              />
-            </div>
-          </div>
-        </div>
-      );
-    } else if (proposal.votingType === VotingType.ConvictionYesNoVoting) {
-      return (
-        <div class="VotingResults">
-          <div class="results-column yes-votes">
-            <div class="results-header">
-              {`Yes (${votes.filter((v) => v.choice === true).length})`}
-            </div>
-            <div class="results-cell">
-              <VoteListing
-                proposal={proposal}
-                votes={votes.filter((v) => v.choice === true)}
-                amount
-                weight
-              />
-            </div>
-          </div>
-          <div class="results-column no-votes">
-            <div class="results-header">
-              {`No (${votes.filter((v) => v.choice === false).length})`}
-            </div>
-            <div class="results-cell">
-              <VoteListing
-                proposal={proposal}
-                votes={votes.filter((v) => v.choice === false)}
-                amount
-                weight
-              />
-            </div>
-          </div>
-        </div>
+        <AaveVotingResult
+          noBalanceString={noBalanceString}
+          noVotesCount={noVotes.length}
+          proposal={proposal}
+          votes={votes}
+          yesBalanceString={yesBalanceString}
+          yesVotesCount={yesVotes.length}
+        />
       );
     } else if (proposal.votingType === VotingType.YesNoAbstainVeto) {
       // return different voting results on completed cosmos proposal, as voters are not available
@@ -263,167 +141,24 @@ export const VotingResults: m.Component<{ proposal: AnyProposal }> = {
         };
 
         return (
-          <div class="VotingResults">
-            <div class="results-column">
-              <div class="results-header">{`${getPct(yes)}% voted Yes`}</div>
-              <div class="results-cell">{`(${formatCurrency(yes)})`}</div>
-            </div>
-            <div class="results-column">
-              <div class="results-header">{`${getPct(no)}% voted No`}</div>
-              <div class="results-cell">{`(${formatCurrency(no)})`}</div>
-            </div>
-            <div class="results-column">
-              <div class="results-header">
-                {`${getPct(abstain)}% voted Abstain`}
-              </div>
-              <div class="results-cell">{`(${formatCurrency(abstain)})`}</div>
-            </div>
-            <div class="results-column">
-              <div class="results-header">
-                {`${getPct(noWithVeto)}% voted Veto`}
-              </div>
-              <div class="results-cell">
-                {`(${formatCurrency(noWithVeto)})`}
-              </div>
-            </div>
-          </div>
+          <CompletedProposalVotingResult
+            abstainPct={getPct(abstain)}
+            abstainResults={formatCurrency(abstain)}
+            noPct={getPct(no)}
+            noResults={formatCurrency(no)}
+            noWithVetoPct={getPct(noWithVeto)}
+            noWithVetoResults={formatCurrency(noWithVeto)}
+            yesPct={getPct(yes)}
+            yesResults={formatCurrency(yes)}
+          />
+        );
+      } else {
+        return (
+          <YesNoAbstainVetoVotingResult proposal={proposal} votes={votes} />
         );
       }
-
-      return (
-        <div class="VotingResults">
-          <div class="results-column">
-            <div class="results-header">
-              {`Voted yes (${votes.filter((v) => v.choice === 'Yes').length})`}
-            </div>
-            <div class="results-cell">
-              <VoteListing
-                proposal={proposal}
-                votes={votes.filter((v) => v.choice === 'Yes')}
-              />
-            </div>
-          </div>
-          <div class="results-column">
-            <div class="results-header">
-              {`Voted no (${votes.filter((v) => v.choice === 'No').length})`}
-            </div>
-            <div class="results-cell">
-              <VoteListing
-                proposal={proposal}
-                votes={votes.filter((v) => v.choice === 'No')}
-              />
-            </div>
-          </div>
-          <div class="results-column">
-            <div class="results-header">
-              {`Voted abstain (${
-                votes.filter((v) => v.choice === 'Abstain').length
-              })`}
-            </div>
-            <div class="results-cell">
-              <VoteListing
-                proposal={proposal}
-                votes={votes.filter((v) => v.choice === 'Abstain')}
-              />
-            </div>
-          </div>
-          <div class="results-column">
-            <div class="results-header">
-              {`Voted veto (${
-                votes.filter((v) => v.choice === 'NoWithVeto').length
-              })`}
-            </div>
-            <div class="results-cell">
-              <VoteListing
-                proposal={proposal}
-                votes={votes.filter((v) => v.choice === 'NoWithVeto')}
-              />
-            </div>
-          </div>
-        </div>
-      );
-    } else if (
-      proposal.votingType === VotingType.SimpleYesApprovalVoting &&
-      proposal instanceof CosmosProposal
-    ) {
-      // special case for cosmos proposals in deposit stage
-      return (
-        <div class="VotingResults">
-          <div class="results-column">
-            <div class="results-header">{`Approved ${proposal.depositorsAsVotes.length}`}</div>
-            <div class="results-cell">
-              <VoteListing
-                proposal={proposal}
-                votes={proposal.depositorsAsVotes}
-              />
-            </div>
-          </div>
-        </div>
-      );
-    } else if (proposal.votingType === VotingType.SimpleYesApprovalVoting) {
-      return (
-        <div class="VotingResults">
-          <div class="results-column">
-            <div class="results-header">{`Approved ${votes.length}`}</div>
-            <div class="results-cell">
-              <VoteListing proposal={proposal} votes={votes} />
-            </div>
-          </div>
-        </div>
-      );
     } else if (proposal.votingType === VotingType.YesNoReject) {
-      return (
-        <div class="VotingResults">
-          <div class="results-column">
-            <div class="results-header">
-              {`Voted approve (${
-                votes.filter((v) => v.choice === NearSputnikVoteString.Approve)
-                  .length
-              })`}
-            </div>
-            <div class="results-cell">
-              <VoteListing
-                proposal={proposal}
-                votes={votes.filter(
-                  (v) => v.choice === NearSputnikVoteString.Approve
-                )}
-              />
-            </div>
-          </div>
-          <div class="results-column">
-            <div class="results-header">
-              {`Voted reject (${
-                votes.filter((v) => v.choice === NearSputnikVoteString.Reject)
-                  .length
-              })`}
-            </div>
-            <div class="results-cell">
-              <VoteListing
-                proposal={proposal}
-                votes={votes.filter(
-                  (v) => v.choice === NearSputnikVoteString.Reject
-                )}
-              />
-            </div>
-          </div>
-          <div class="results-column">
-            <div class="results-header">
-              {`Voted remove (${
-                votes.filter((v) => v.choice === NearSputnikVoteString.Remove)
-                  .length
-              })`}
-            </div>
-            <div class="results-cell">
-              <VoteListing
-                proposal={proposal}
-                votes={votes.filter(
-                  (v) => v.choice === NearSputnikVoteString.Remove
-                )}
-              />
-            </div>
-          </div>
-        </div>
-      );
+      return <YesNoRejectVotingResult proposal={proposal} votes={votes} />;
     } else if (proposal.votingType === VotingType.RankedChoiceVoting) {
       // to be implemented
     } else {
