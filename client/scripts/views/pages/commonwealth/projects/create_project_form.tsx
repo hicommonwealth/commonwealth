6aec2e5905160874dd8a87ca6e8e28272b484f83
Graham Johnson <graham@commonwealth.com>
2022-06-28 15:56:01 -0500

    Explore page style upgrade

--- client/scripts/views/pages/commonwealth/projects/create_project_form.tsx
+++ client/scripts/views/pages/commonwealth/projects/create_project_form.tsx
@@ -13,11 +13,64 @@ import { notifyError } from 'controllers/app/notifications';
 import { CWTextArea } from 'views/components/component_kit/cw_text_area';
 import Sublayout from 'views/sublayout';
 import { ChainBase } from 'types';
+import Web3 from 'web3';
 import CoverImageUpload from './cover_image_upload';
-import { validateProjectForm } from './helpers';
 
 const weekInSeconds = 604800;
 const nowInSeconds = new Date().getTime() / 1000;
+
+// @Zak and @Gabe for PR review: Thoughts on this validation system, and documenting/standardizing?
+// See logic in create_project_form inputValidationFn (on each text input) + final 'submit' button functionality,
+// especially in conjunction with new `onsuccess` attr
+export const validateProjectForm = (property: string, value: string) => {
+  if (!value)
+    return ['error', `Form is missing a ${property.split(/(?=[A-Z])/)} input.`];
+  let errorMessage: string;
+  switch (property) {
+    case 'title':
+      if (value.length < 8 || value.length > 64) {
+        errorMessage = `Title must be valid string between 3 and 64 characters. Current count: ${value.length}`;
+      }
+      break;
+    case 'shortDescription':
+      if (value.length > 224) {
+        errorMessage = `Input limit is 224 characters. Current count: ${value.length}`;
+      }
+      break;
+    case 'token':
+    case 'beneficiary':
+    case 'creator':
+      if (!Web3.utils.isAddress(value)) {
+        errorMessage = `Invalid ${property} address. Must be a valid Ethereum address.`;
+      }
+      break;
+    case 'fundraiseLength':
+      // TODO: Min fundraiseLength check
+      if (Number.isNaN(+value)) {
+        errorMessage = 'Invalid fundraise length. Must be between [X, Y]';
+      }
+      break;
+    case 'threshold':
+      // TODO: Min threshold check
+      if (Number.isNaN(+value)) {
+        errorMessage = 'Invalid threshold amount. Must be between [X, Y]';
+      }
+      break;
+    case 'curatorFee':
+      if (Number.isNaN(+value) || +value > 100 || +value < 0) {
+        errorMessage = `Curator fee must be a valid number (%) between 0 and 100.`;
+      }
+      break;
+    default:
+      break;
+  }
+
+  if (errorMessage) {
+    return ['error', errorMessage];
+  } else {
+    return ['success', `Valid ${property}`];
+  }
+};
 
 type TokenOption = {
   name: string;
@@ -36,9 +89,9 @@ export interface ICreateProjectForm {
   token: string;
   creator: string;
   beneficiary: string;
-  threshold: number;
-  fundraiseLength: number;
-  curatorFee: number;
+  threshold: string;
+  fundraiseLength: string;
+  curatorFee: string;
 }
 
 export class InformationSlide
