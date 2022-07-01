5f7be31b4a7dfb1d6eebacdb7def70ed80ed8da0
Jake Naviasky <jake@commonwealth.im>
2022-07-01 17:45:53 -0400

    Rework migration address fetching logic.

--- server/migrations/20220701205903-update-axie-addresses-to-checksum.js
+++ server/migrations/20220701205903-update-axie-addresses-to-checksum.js
@@ -6,55 +6,47 @@ const {toChecksumAddress} = require('web3-utils');
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     return queryInterface.sequelize.transaction(async (transaction) => {
-
       const addresses = await queryInterface.sequelize.query(
         `SELECT * FROM "Addresses" WHERE chain='axie-infinity';`,
         { transaction }
       );
 
-      let count = 0;
-      const updatedAddresses = addresses[0].map((addy) => {
-        const newAddress = addy;
-        const checksumAddress = toChecksumAddress(addy.address);
-        if (checksumAddress !== addy.address) count++;
-        newAddress.address = checksumAddress;
-        return newAddress;
-      });
+      const addressesToUpdate = {};
+      const addressesToDelete = {};
+      for (const addressObj of addresses[0]) {
+        const address = addressObj.address;
+        const checksumAddress = toChecksumAddress(address);
+        if (!addressesToUpdate[checksumAddress]) {
+          addressesToUpdate[checksumAddress] = addressObj.id;
+        } else {
+          addressesToDelete[addressObj.id] = addressesToUpdate[checksumAddress];
+        }
+      }
 
-      console.log(count);
-
-      for (const addy of updatedAddresses) {
-        const { id, address } = addy;
-        const anotherOne = await queryInterface.sequelize.query(
-          `SELECT * FROM "Addresses" WHERE address='${address}';`,
+      for (const [idToDelete, idToUpdate] of addressesToDelete) {
+        await queryInterface.sequelize.query(
+          `DELETE FROM "OffchainComments" WHERE address_id=${idToDelete};`,
           { transaction }
         );
-        console.log(anotherOne[0]);
-        if (anotherOne[0].id !== undefined) {
-          // delete cascade
-          console.log(`failed to update: ${address}, ${id}`);
-          await queryInterface.sequelize.query(
-            `DELETE FROM "OffchainComments" WHERE address_id=${id};`,
-            { transaction }
-          );
-          await queryInterface.sequelize.query(
-            `DELETE FROM "OffchainThreads" WHERE address_id=${id};`,
-            { transaction }
-          );
-          await queryInterface.sequelize.query(
-            `DELETE FROM "OffchainReactions" WHERE address_id=${id};`,
-            { transaction }
-          );
-          await queryInterface.sequelize.query(
-            `DELETE FROM "Addresses" WHERE id=${id};`,
-            { transaction }
-          );
-        } else {
-          await queryInterface.sequelize.query(
-            `UPDATE "Addresses" SET address='${address}' WHERE id=${id};`,
-            { transaction }
-          );
-        }
+        await queryInterface.sequelize.query(
+          `DELETE FROM "OffchainThreads" WHERE address_id=${idToDelete};`,
+          { transaction }
+        );
+        await queryInterface.sequelize.query(
+          `DELETE FROM "OffchainReactions" WHERE address_id=${idToDelete};`,
+          { transaction }
+        );
+        await queryInterface.sequelize.query(
+          `DELETE FROM "Addresses" WHERE id=${idToDelete};`,
+          { transaction }
+        );
+      }
+
+      for (const [address, id] of Object.entries(addressesToUpdate)) {
+        await queryInterface.sequelize.query(
+          `UPDATE "Addresses" SET address='${address}' WHERE id=${id};`,
+          { transaction }
+        );
       }
     });
   },
