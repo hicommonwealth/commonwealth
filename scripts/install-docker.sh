9e249347e0127a3fe873de404f12155f2ac06ce1
timolegros <timothee@ualberta.ca>
2022-07-01 11:27:10 -0400

    docker latest installation script

---
+++ scripts/install-docker.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# This script should ONLY be run if updating any image version on the server. This is meant to make it easy
+# to match the versions of the tools used on the server and Heroku
+
+
+# load environment variables from a local .env file
+# https://stackoverflow.com/questions/19331497/set-environment-variables-from-file-of-key-value-pairs/30969768#30969768
+# the -e option may need to be -E on Mac OS
+# shellcheck disable=SC2046
+export $(grep -v '^#' .env | xargs -d '\n' -e)
+
+# check that all required ENV var were properly loaded into the environment
+if [[ -z "$VULTR_IP" ]]; then
+    echo "Must provide VULTR_IP in .env" 1>&2
+    exit 1
+    elif [[ -z "$VULTR_USER" ]]; then
+      echo "Must provide VULTR_USER in .env" 1>&2
+      exit 1
+fi
+
+echo "Connecting to $VULTR_USER@$VULTR_IP"
+# connect to the Vultr server on which the images will be loaded
+#ssh "$VULTR_USER"@"$VULTR_IP" /bin/bash << "EOF"
+echo "Connection Successful!"
+lsb_release -a
+
+# clear any existing docker version
+sudo apt-get remove docker docker-engine docker.io containerd runc
+
+# Update/setup repositories
+# Update the apt package index and install packages to allow apt to use a repository over HTTPS
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg lsb-release
+
+# Add Docker’s official GPG key
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+# setup repository
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# if this causes issues run:
+# sudo chmod a+r /etc/apt/keyrings/docker.gpg
+sudo apt-get update
+
+DOCKER_VERSION=$(apt-cache madison docker-ce | head -n 1 | cut -d " " -f 4)
+echo "Installing Docker version: $DOCKER_VERSION"
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo apt-get install docker-ce="$DOCKER_VERSION" docker-ce-cli="$DOCKER_VERSION" containerd.io docker-compose-plugin
+
+# test the installation
+sudo docker run hello-world
+
+EOF
