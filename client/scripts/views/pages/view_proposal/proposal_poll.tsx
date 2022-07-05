098391e5953e062adfe71599f757d6956e382579
Gabriel Duquette <gabrielduquette@gmail.com>
2022-07-04 21:51:16 -0400

    finish jsx

--- client/scripts/views/pages/view_proposal/proposal_poll.tsx
+++ client/scripts/views/pages/view_proposal/proposal_poll.tsx
@@ -7,11 +7,11 @@ import 'pages/view_proposal/proposal_poll.scss';
 import app from 'state';
 import TopicGateCheck from 'controllers/chain/ethereum/gatedTopic';
 import { OffchainThread, OffchainPoll } from 'models';
-import { Button, Icons } from 'construct-ui';
 import moment from 'moment';
 import { alertModalWithText } from '../../modals/alert_modal';
 import { confirmationModalWithText } from '../../modals/confirm_modal';
 import { OffchainVotingModal } from '../../modals/offchain_voting_modal';
+import { CWButton } from '../../components/component_kit/cw_button';
 
 const vote = async (
   poll: OffchainPoll,
@@ -76,92 +76,102 @@ export class ProposalPoll
 
     const totalVoteCount = poll.votes.length;
 
-    const voteSynopsis = m('.vote-synopsis', [
-      optionScopedVotes.map((optionWithVotes) => {
-        const optionVoteCount = optionWithVotes.votes.length;
+    const voteSynopsis = (
+      <div class="vote-synopsis">
+        {optionScopedVotes.map((optionWithVotes) => {
+          const optionVoteCount = optionWithVotes.votes.length;
+          const optionVotePercentage = optionVoteCount / totalVoteCount;
 
-        const optionVotePercentage = optionVoteCount / totalVoteCount;
-
-        return m('.option-with-votes', [
-          m('.option-results-label', [
-            m(
-              'div',
-              { style: 'font-weight: 500; margin-right: 5px;' },
-              `${optionWithVotes.option}`
-            ),
-            m('div', `(${optionVoteCount})`),
-          ]),
-          m('.poll-bar', {
-            style: `width: ${Math.round(optionVotePercentage * 10000) / 100}%`,
-          }),
-        ]);
-      }),
-      m(
-        'a',
-        {
-          href: '#',
-          onclick: (e) => {
+          return (
+            <div class="option-with-votes">
+              <div class="option-results-label">
+                <div style="font-weight: 500; margin-right: 5px;">
+                  {optionWithVotes.option}
+                </div>
+                <div>{optionVoteCount}</div>
+              </div>
+              <div
+                class="poll-bar"
+                style={`width: ${
+                  Math.round(optionVotePercentage * 10000) / 100
+                }%`}
+              />
+            </div>
+          );
+        })}
+        <a
+          href="#"
+          onclick={(e) => {
             e.preventDefault();
             app.modals.create({
               modal: OffchainVotingModal,
               data: { votes: poll.votes },
             });
-          },
-        },
-        'See all votes'
-      ),
-    ]);
+          }}
+        >
+          See all votes
+        </a>
+      </div>
+    );
 
-    return m('.ProposalPoll', [
-      m('.offchain-poll-header', [
-        poll.prompt || (pollingEnded ? 'Poll closed' : 'Poll open'),
-      ]),
-      !poll.options && m('.offchain-poll-invalid', '[Error loading poll]'),
-      m(
-        '.offchain-poll-options',
-        poll.options?.map((option) => {
-          const hasVoted =
-            app.user.activeAccount &&
-            poll.getUserVote(
-              app.user.activeAccount.chain.id,
-              app.user.activeAccount.address
+    return (
+      <div class="ProposalPoll">
+        <div class="offchain-poll-header">
+          {poll.prompt || (pollingEnded ? 'Poll closed' : 'Poll open')}
+        </div>
+        {!poll.options && (
+          <div class="offchain-poll-invalid">Error loading poll</div>
+        )}
+        <div class="offchain-poll-options">
+          {poll.options?.map((option) => {
+            const hasVoted =
+              app.user.activeAccount &&
+              poll.getUserVote(
+                app.user.activeAccount.chain.id,
+                app.user.activeAccount.address
+              );
+
+            const isSelected = hasVoted?.option === option;
+
+            return (
+              <div class="offchain-poll-option">
+                <div class="offchain-poll-option-left">{option}</div>
+                <div class="offchain-poll-option-right">
+                  <CWButton
+                    onclick={() => vote(poll, option, isSelected)}
+                    label={isSelected ? 'Voted' : 'Vote'}
+                    disabled={
+                      pollingEnded || isSelected || tokenThresholdFailed
+                    }
+                    // style={pollingEnded || isSelected ? 'pointer-events: none' : ''}
+                    // iconLeft={isSelected ? Icons.CHECK : null}
+                  />
+                </div>
+              </div>
             );
-          const isSelected = hasVoted?.option === option;
-          return m('.offchain-poll-option', [
-            m('.offchain-poll-option-left', option),
-            m('.offchain-poll-option-right', [
-              m(Button, {
-                onclick: () => vote(poll, option, isSelected),
-                label: isSelected ? 'Voted' : 'Vote',
-                size: 'sm',
-                rounded: true,
-                disabled: pollingEnded || isSelected || tokenThresholdFailed,
-                style: pollingEnded || isSelected ? 'pointer-events: none' : '',
-                iconLeft: isSelected ? Icons.CHECK : null,
-                compact: true,
-              }),
-            ]),
-          ]);
-        })
-      ),
-      m('.offchain-poll-caption', [
-        poll.endsAt
-          ? [
-              !pollingEnded && moment().from(poll.endsAt).replace(' ago', ''),
-              !pollingEnded && ' left',
-              m('br'),
-              !pollingEnded && 'Ends ',
-              pollingEnded && 'Ended ',
-              poll.endsAt?.format('lll'),
-            ]
-          : 'Poll does not expire.',
-      ]),
-      m('.offchain-poll-header', 'Voters'),
-      m('.offchain-poll-voters', [
-        poll.votes.length === 0
-          ? m('.offchain-poll-no-voters', 'Nobody has voted')
-          : voteSynopsis,
-      ]),
-    ]);
+          })}
+        </div>
+        <div class="offchain-poll-caption">
+          {poll.endsAt
+            ? [
+                !pollingEnded && moment().from(poll.endsAt).replace(' ago', ''),
+                !pollingEnded && ' left',
+                m('br'),
+                !pollingEnded && 'Ends ',
+                pollingEnded && 'Ended ',
+                poll.endsAt?.format('lll'),
+              ]
+            : 'Poll does not expire.'}
+        </div>
+        <div class="offchain-poll-header">Voters</div>
+        <div class="offchain-poll-voters">
+          {poll.votes.length === 0 ? (
+            <div class="offchain-poll-no-voters">Nobody has voted</div>
+          ) : (
+            voteSynopsis
+          )}
+        </div>
+      </div>
+    );
   }
 }
