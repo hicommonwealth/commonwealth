d8fe92e39a6eefc691d73e092f05b6c0e2cc9770
Gabriel Duquette <gabrielduquette@gmail.com>
2022-07-28 22:16:56 +0100

    cleaning up in progress

--- packages/commonwealth/client/scripts/views/pages/settings/email_well.tsx
+++ packages/commonwealth/client/scripts/views/pages/settings/email_well.tsx
@@ -2,17 +2,19 @@
 
 import m from 'mithril';
 import $ from 'jquery';
-import app from 'state';
-import { Button, Colors, Input, Icons, Icon } from 'construct-ui';
 
 import 'pages/settings/email_well.scss';
 
+import app from 'state';
 import { SocialAccount } from 'models';
 import { confirmationModalWithText } from 'views/modals/confirm_modal';
 import { WalletId } from 'common-common/src/types';
 import { CWButton } from '../../components/component_kit/cw_button';
 import { CWTextInput } from '../../components/component_kit/cw_text_input';
 import { CWText } from '../../components/component_kit/cw_text';
+import { CWIcon } from '../../components/component_kit/cw_icons/cw_icon';
+import { CWValidationText } from '../../components/component_kit/cw_validation_text';
+import { getClasses } from '../../components/component_kit/helpers';
 
 export class EmailWell implements m.ClassComponent {
   private discordAccount: SocialAccount;
@@ -47,22 +49,21 @@ export class EmailWell implements m.ClassComponent {
       errorMessage,
     } = this;
 
-    return [
-      m('.EmailWell', [
+    return (
+      <div class="EmailWell">
         <CWText type="h5" fontWeight="semiBold">
           Login
-        </CWText>,
+        </CWText>
         <CWTextInput
           placeholder="name@example.com"
-          iconRight="mail"
           defaultValue={app.user.email || null}
           oninput={(e) => {
             this.emailInputUpdated = true;
             this.verificationSent = false;
             this.email = (e.target as any).value;
           }}
-        />,
-        (!app.user.email || emailInputUpdated || !emailVerified) && (
+        />
+        {(!app.user.email || emailInputUpdated || !emailVerified) && (
           <CWButton
             label={
               app.user.email && !emailInputUpdated && !emailVerified
@@ -96,8 +97,6 @@ export class EmailWell implements m.ClassComponent {
 
                 m.redraw();
 
-                console.log('Failed to update email');
-
                 throw new Error(
                   err.responseJSON && err.responseJSON.error
                     ? err.responseJSON.error
@@ -106,129 +105,98 @@ export class EmailWell implements m.ClassComponent {
               }
             }}
           />
-        ),
-        verificationSent
-          ? m(
-              'label',
-              {
-                style: {
-                  color: Colors.GREEN500,
-                  position: 'relative',
-                  top: '2px',
-                },
-              },
-              'Check your email for a confirmation link'
-            )
-          : [
-              m(Icon, {
-                size: 'lg',
-                intent: emailVerified ? 'positive' : 'warning',
-                name: emailVerified ? Icons.CHECK_CIRCLE : Icons.ALERT_CIRCLE,
-              }),
-              m(
-                'label',
-                {
-                  style: {
-                    color: emailVerified ? Colors.GREEN500 : '#f57c01',
-                    position: 'relative',
-                    top: '2px',
-                  },
-                },
-                emailVerified
-                  ? 'Verified'
-                  : app.user.email
-                  ? 'Not verified'
-                  : 'No email'
-              ),
-            ],
-        errorMessage && m('p.error', errorMessage),
-      ]),
-      m('.LinkButtonWrapper', [
-        m('.GithubWell', [
-          m('form', [
-            githubAccount &&
-              m(Input, {
-                value: `github.com/${githubAccount.username || ''}`,
-                contentLeft: m(Icon, { name: Icons.GITHUB }),
-                disabled: true,
-              }),
-            m(Button, {
-              label: githubAccount ? 'Unlink Github' : 'Link Github',
-              intent: githubAccount ? 'negative' : 'primary',
-              rounded: true,
-              onclick: () => {
-                if (githubAccount) {
-                  $.ajax({
-                    url: `${app.serverUrl()}/githubAccount`,
-                    data: { jwt: app.user.jwt },
-                    type: 'DELETE',
-                    success: (result) => {
-                      this.githubAccount = null;
-                      m.redraw();
-                    },
-                    error: (err) => {
-                      console.dir(err);
-                      m.redraw();
-                    },
-                  });
-                } else {
-                  localStorage.setItem(
-                    'githubPostAuthRedirect',
-                    JSON.stringify({
-                      timestamp: (+new Date()).toString(),
-                      path: m.route.get(),
-                    })
-                  );
-                  document.location = `${app.serverUrl()}/auth/github` as any;
+        )}
+        {verificationSent ? (
+          <CWText>Check your email for a confirmation link</CWText>
+        ) : (
+          <div class="verification-row">
+            <CWIcon
+              iconName={emailVerified ? 'check' : 'cautionCircle'}
+              className="verification-icon"
+            />
+            <CWText
+              className={getClasses<{ emailVerified?: boolean }>(
+                { emailVerified },
+                'verification-text'
+              )}
+            >
+              {emailVerified
+                ? 'Verified'
+                : app.user.email
+                ? 'Not verified'
+                : 'No email'}
+            </CWText>
+          </div>
+        )}
+        {errorMessage && (
+          <CWValidationText message={errorMessage} status="failure" />
+        )}
+        {/* <CWText type="h5" fontWeight="semiBold">
+          Link other sites
+        </CWText> */}
+        <CWButton
+          label={githubAccount ? 'Unlink Github' : 'Link Github'}
+          buttonType={githubAccount ? 'primary-red' : 'primary-blue'}
+          onclick={() => {
+            if (githubAccount) {
+              $.ajax({
+                url: `${app.serverUrl()}/githubAccount`,
+                data: { jwt: app.user.jwt },
+                type: 'DELETE',
+                success: () => {
+                  this.githubAccount = null;
                   m.redraw();
-                }
-              },
-            }),
-          ]),
-        ]),
-        m('.DiscordWell', [
-          m('form', [
-            discordAccount &&
-              m(Input, {
-                value: `${discordAccount.username || ''}`,
-                contentLeft: m(Icon, { name: Icons.DISC }), // TODO: add a discord icon
-                disabled: true,
-              }),
-            m(Button, {
-              label: discordAccount ? 'Unlink Discord' : 'Link Discord',
-              intent: discordAccount ? 'negative' : 'primary',
-              rounded: true,
-              onclick: () => {
-                if (discordAccount) {
-                  $.ajax({
-                    url: `${app.serverUrl()}/discordAccount`,
-                    data: { jwt: app.user.jwt },
-                    type: 'DELETE',
-                    success: (result) => {
-                      this.discordAccount = null;
-                      m.redraw();
-                    },
-                    error: (err) => {
-                      console.dir(err);
-                      m.redraw();
-                    },
-                  });
-                } else {
-                  localStorage.setItem(
-                    'discordPostAuthRedirect',
-                    JSON.stringify({
-                      timestamp: (+new Date()).toString(),
-                      path: m.route.get(),
-                    })
-                  );
-                  document.location = `${app.serverUrl()}/auth/discord` as any;
+                },
+                error: (err) => {
+                  console.dir(err);
                   m.redraw();
-                }
-              },
-            }),
-          ]),
-        ]),
-      ]),
-    ];
+                },
+              });
+            } else {
+              localStorage.setItem(
+                'githubPostAuthRedirect',
+                JSON.stringify({
+                  timestamp: (+new Date()).toString(),
+                  path: m.route.get(),
+                })
+              );
+              document.location = `${app.serverUrl()}/auth/github` as any;
+              m.redraw();
+            }
+          }}
+        />
+        <CWButton
+          label={discordAccount ? 'Unlink Discord' : 'Link Discord'}
+          buttonType={discordAccount ? 'primary-red' : 'primary-blue'}
+          onclick={() => {
+            if (discordAccount) {
+              $.ajax({
+                url: `${app.serverUrl()}/discordAccount`,
+                data: { jwt: app.user.jwt },
+                type: 'DELETE',
+                success: () => {
+                  this.discordAccount = null;
+                  m.redraw();
+                },
+                error: (err) => {
+                  console.dir(err);
+                  m.redraw();
+                },
+              });
+            } else {
+              localStorage.setItem(
+                'discordPostAuthRedirect',
+                JSON.stringify({
+                  timestamp: (+new Date()).toString(),
+                  path: m.route.get(),
+                })
+              );
+              document.location = `${app.serverUrl()}/auth/discord` as any;
+              m.redraw();
+            }
+          }}
+        />
+      </div>
+    );
   }
 }
