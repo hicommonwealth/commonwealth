7decce9ca97a70926457d28117fe9c0d328b0d12
timolegros <timothee@ualberta.ca>
2022-08-01 16:03:48 -0400

    remove thread_id from chain-entities migration

--- packages/commonwealth/server/migrations/20220801191612-change-thread-link-entity.js
+++ packages/commonwealth/server/migrations/20220801191612-change-thread-link-entity.js
@@ -2,15 +2,51 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
+    return queryInterface.sequelize.transaction(async (t) => {
+      await queryInterface.addColumn('Threads', 'chain_entity_id', {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+        unique: true
+      }, { transaction: t });
 
+      await queryInterface.sequelize.query(
+        `UPDATE "Threads" AS T
+        SET chain_entity_id = C.id
+        FROM (SELECT id, thread_id
+                FROM "ChainEntities"
+                WHERE thread_id IS NOT NULL) AS C(id, thread_id)
+         WHERE T.id = C.thread_id;`,
+        { transaction: t }
+      );
+
+      await queryInterface.removeColumn('ChainEntities',
+        'thread_id',
+        { transaction: t }
+      );
+    })
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    return queryInterface.sequelize.transaction(async (t) => {
+      await queryInterface.addColumn('ChainEntities', 'thread_id', {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+      }, { transaction: t });
+
+      await queryInterface.sequelize.query(
+        `UPDATE "ChainEntities" AS C
+        SET thread_id = T.id
+        FROM (SELECT id, chain_entity_id
+                FROM "Threads"
+                WHERE chain_entity_id IS NOT NULL) AS T(id, chain_entity_id)
+        WHERE C.id = T.chain_entity_id;`,
+        { transaction: t }
+      );
+
+      await queryInterface.removeColumn('Threads',
+        'chain_entity_id',
+        { transaction: t }
+      );
+    })
   }
 };
