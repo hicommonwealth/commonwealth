0710f23969987c1ed5e002bb46e2ca5e2a2a20a8
Gabriel Duquette <gabrielduquette@gmail.com>
2022-08-02 19:09:28 +0100

    composer section

--- packages/commonwealth/client/scripts/views/pages/settings/composer_section.tsx
+++ packages/commonwealth/client/scripts/views/pages/settings/composer_section.tsx
@@ -5,33 +5,42 @@ import m from 'mithril';
 import 'pages/settings/composer_section.scss';
 
 import app from 'state';
-import { RadioSelectorFormField } from 'views/components/forms';
 import { notifySuccess } from 'controllers/app/notifications';
 import SettingsController from 'controllers/app/settings';
+import { CWText } from '../../components/component_kit/cw_text';
+import { CWRadioGroup } from '../../components/component_kit/cw_radio_group';
 
 export class ComposerSection implements m.ClassComponent {
+  private selection: string;
+
+  oninit() {
+    this.selection = app.user.disableRichText ? 'markdown' : 'richtext';
+  }
+
   view() {
-    return m('SettingsSection', [
-      m('h4', 'Composer'),
-      m(RadioSelectorFormField, {
-        callback: async (value) => {
-          await SettingsController.disableRichText(value !== 'richtext');
-          notifySuccess('Setting saved');
-        },
-        choices: [
-          {
-            label: 'Rich Text',
-            value: 'richtext',
-            checked: !app.user.disableRichText,
-          },
-          {
-            label: 'Markdown',
-            value: 'markdown',
-            checked: app.user.disableRichText === true,
-          },
-        ],
-        name: 'composer',
-      }),
-    ]);
+    const settingsRadioGroupOptions = [
+      { label: 'Rich Text', value: 'richtext' },
+      { label: 'Markdown', value: 'markdown' },
+    ];
+
+    return (
+      <div class="ComposerSection">
+        <CWText type="h5" fontWeight="semiBold">
+          Composer
+        </CWText>
+        <CWRadioGroup
+          options={settingsRadioGroupOptions}
+          name="composerSectionRadioGroup"
+          toggledOption={this.selection}
+          onchange={async (e) => {
+            this.selection = e.target.value;
+            await SettingsController.disableRichText(
+              e.target.value !== 'richtext'
+            );
+            notifySuccess('Setting saved');
+          }}
+        />
+      </div>
+    );
   }
 }
