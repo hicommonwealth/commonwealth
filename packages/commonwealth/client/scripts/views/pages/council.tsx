7f2692c9292c26edf94444bc8455376b434d51d3
Gabriel Duquette <gabrielduquette@gmail.com>
2022-08-05 18:27:39 +0100

    fix import

--- packages/commonwealth/client/scripts/views/pages/council.tsx
+++ packages/commonwealth/client/scripts/views/pages/council.tsx
@@ -14,7 +14,7 @@ import { PhragmenElectionVote } from 'controllers/chain/substrate/phragmen_elect
 import Sublayout from 'views/sublayout';
 import User from 'views/components/widgets/user';
 import { CountdownUntilBlock } from 'views/components/countdown';
-import CouncilVotingModal from 'views/modals/council_voting_modal';
+import { CouncilVotingModal } from 'views/modals/council_voting_modal';
 import { PageLoading } from 'views/pages/loading';
 import ErrorPage from 'views/pages/error';
 import { loadSubstrateModules } from 'views/components/load_substrate_modules';
