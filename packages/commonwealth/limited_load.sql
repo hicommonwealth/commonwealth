571a8da971d4f7a1d54cc0d98f080e004295866f
Jake Naviasky <jake@commonwealth.im>
2022-08-05 15:40:43 -0400

    Update git ignore and remove limited dumps.

--- packages/commonwealth/limited_load.sql
+++
@@ -1,5 +0,0 @@
-\COPY "ChainEvents" FROM 'ChainEvents.dat' WITH BINARY;
-\COPY "Notifications" FROM 'Notifications.dat' WITH BINARY;
-\COPY "Subscriptions" FROM 'Subscriptions.dat' WITH BINARY;
-\COPY "NotificationsRead" FROM 'NotificationsRead.dat' WITH BINARY;
-
