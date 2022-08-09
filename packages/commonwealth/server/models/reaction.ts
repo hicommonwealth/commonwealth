c8a3ee51667a95a3dc3b35eac40691b4a343dd09
Daniel Trinh <danielhtrinh1@gmail.com>
2022-08-09 14:56:16 -0700

    Update reaction.ts

--- packages/commonwealth/server/models/reaction.ts
+++ packages/commonwealth/server/models/reaction.ts
@@ -16,86 +16,14 @@ export type ReactionAttributes = {
   updated_at?: Date;
   Chain?: ChainAttributes;
   Address?: AddressAttributes;
-};
+}
 
-<<<<<<< HEAD:server/models/offchain_reaction.ts
-export type OffchainReactionInstance =
-  ModelInstance<OffchainReactionAttributes>;
-=======
 export type ReactionInstance = ModelInstance<ReactionAttributes>;
->>>>>>> master:packages/commonwealth/server/models/reaction.ts
 
 export type ReactionModelStatic = ModelStatic<ReactionInstance>;
 
 export default (
   sequelize: Sequelize.Sequelize,
-<<<<<<< HEAD:server/models/offchain_reaction.ts
-  dataTypes: typeof DataTypes
-): OffchainReactionModelStatic => {
-  const OffchainReaction = <OffchainReactionModelStatic>sequelize.define(
-    'OffchainReaction',
-    {
-      id: { type: dataTypes.INTEGER, autoIncrement: true, primaryKey: true },
-      chain: { type: dataTypes.STRING, allowNull: false },
-      thread_id: {
-        type: dataTypes.INTEGER,
-        allowNull: true,
-      },
-      proposal_id: {
-        type: dataTypes.STRING,
-        allowNull: true,
-      },
-      comment_id: {
-        type: dataTypes.INTEGER,
-        allowNull: true,
-      },
-      address_id: { type: dataTypes.INTEGER, allowNull: false },
-      reaction: { type: dataTypes.STRING, allowNull: false },
-    },
-    {
-      tableName: 'OffchainReactions',
-      underscored: true,
-      createdAt: 'created_at',
-      updatedAt: 'updated_at',
-      indexes: [
-        { fields: ['id'] },
-        { fields: ['chain', 'thread_id', 'proposal_id', 'comment_id'] },
-        { fields: ['address_id'] },
-        {
-          fields: [
-            'chain',
-            'address_id',
-            'thread_id',
-            'proposal_id',
-            'comment_id',
-            'reaction',
-          ],
-          unique: true,
-        },
-        { fields: ['chain', 'thread_id'] },
-        { fields: ['chain', 'comment_id'] },
-      ],
-    }
-  );
-
-  OffchainReaction.associate = (models) => {
-    models.OffchainReaction.belongsTo(models.Chain, {
-      foreignKey: 'chain',
-      targetKey: 'id',
-    });
-    models.OffchainReaction.belongsTo(models.Address, {
-      foreignKey: 'address_id',
-      targetKey: 'id',
-    });
-    models.OffchainReaction.belongsTo(models.OffchainComment, {
-      foreignKey: 'comment_id',
-      targetKey: 'id',
-    });
-    models.OffchainReaction.belongsTo(models.OffchainThread, {
-      foreignKey: 'thread_id',
-      targetKey: 'id',
-    });
-=======
   dataTypes: typeof DataTypes,
 ): ReactionModelStatic => {
   const Reaction = <ReactionModelStatic>sequelize.define('Reaction', {
@@ -126,7 +54,6 @@ export default (
     models.Reaction.belongsTo(models.Address, { foreignKey: 'address_id', targetKey: 'id' });
     models.Reaction.belongsTo(models.Comment, { foreignKey: 'comment_id', targetKey: 'id' });
     models.Reaction.belongsTo(models.Thread, { foreignKey: 'thread_id', targetKey: 'id' });
->>>>>>> master:packages/commonwealth/server/models/reaction.ts
   };
 
   return Reaction;
