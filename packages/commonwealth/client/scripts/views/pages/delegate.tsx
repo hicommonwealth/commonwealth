1db714bcf1cd4b3b089d00b541669d6643da0e91
Gabriel Duquette <gabrielduquette@gmail.com>
2022-08-10 15:52:57 +0100

    finish conversion

--- packages/commonwealth/client/scripts/views/pages/delegate.tsx
+++ packages/commonwealth/client/scripts/views/pages/delegate.tsx
@@ -1,15 +1,6 @@
 /* @jsx m */
 
 import m from 'mithril';
-import {
-  Grid,
-  Col,
-  Form,
-  FormGroup,
-  FormLabel,
-  Input,
-  Button,
-} from 'construct-ui';
 
 import 'pages/delegate.scss';
 
@@ -21,90 +12,14 @@ import Compound from 'controllers/chain/ethereum/compound/adapter';
 import Aave from 'controllers/chain/ethereum/aave/adapter';
 import { notifyError, notifySuccess } from 'controllers/app/notifications';
 import { PageNotFound } from './404';
-
-const DelegateStats: m.Component<{ currentDelegate: string }> = {
-  view: (vnode) => {
-    if (!app.chain) return;
-
-    return m(
-      Grid,
-      {
-        align: 'middle',
-        class: 'stats-container',
-        gutter: 5,
-        justify: 'space-between',
-      },
-      [
-        m(Col, { span: { xs: 6, md: 3 } }, [
-          m('.stats-tile', [
-            m('.stats-heading', 'Current Delegate'),
-            vnode.attrs.currentDelegate
-              ? m('.stats-tile-figure-minor', vnode.attrs.currentDelegate)
-              : '--',
-          ]),
-          m('.stats-tile', [
-            m('.stats-heading', 'Your address:'),
-            app.user.activeAccount.address
-              ? m('.stats-tile-figure-minor', app.user.activeAccount.address)
-              : '--',
-          ]),
-        ]),
-      ]
-    );
-  },
-};
+import { CWButton } from '../components/component_kit/cw_button';
+import { CWTextInput } from '../components/component_kit/cw_text_input';
+import { CWText } from '../components/component_kit/cw_text';
+import { GovExplainer } from '../components/gov_explainer';
 
 type DelegateFormType = {
   address: string;
   amount: number;
-};
-
-type DelegateFormState = {
-  currentDelegate: string;
-  form: DelegateFormType;
-  loading: boolean;
-};
-
-const getDelegate = async (
-  vnode: m.Vnode<Record<string, never>, DelegateFormState>
-) => {
-  if (app.chain.network === ChainNetwork.Compound) {
-    this.currentDelegate = await (app.chain as Compound).chain.getDelegate();
-  } else if (app.chain.network === ChainNetwork.Aave) {
-    // TODO: switch on delegation type
-    this.currentDelegate = await (app.chain as Aave).chain.getDelegate(
-      app.user.activeAccount.address,
-      'voting'
-    );
-  }
-  m.redraw();
-};
-
-// TODO: remove popup modal for delegation as we auto-delegate all tokens now
-const setDelegate = async (
-  vnode: m.Vnode<Record<string, never>, DelegateFormState>
-) => {
-  if (app.chain.apiInitialized) {
-    let delegationPromise: Promise<void>;
-    if (app.chain.network === ChainNetwork.Compound) {
-      delegationPromise = (app.chain as Compound).chain.setDelegate(
-        this.form.address
-      );
-    } else if (app.chain.network === ChainNetwork.Aave) {
-      delegationPromise = (app.chain as Aave).chain.setDelegate(
-        this.form.address
-      );
-    }
-    if (delegationPromise) {
-      try {
-        await delegationPromise;
-        notifySuccess(`Sent transaction to delegate to ${this.form.address}`);
-        getDelegate(vnode);
-      } catch (err) {
-        notifyError(`${err.message}`);
-      }
-    }
-  }
 };
 
 class DelegateForm implements m.ClassComponent {
@@ -120,7 +35,47 @@ class DelegateForm implements m.ClassComponent {
 
     this.loading = false;
 
-    getDelegate(vnode);
+    this.getDelegate(vnode);
+  }
+
+  async getDelegate(vnode) {
+    if (app.chain.network === ChainNetwork.Compound) {
+      vnode.state.currentDelegate = await (
+        app.chain as Compound
+      ).chain.getDelegate();
+    } else if (app.chain.network === ChainNetwork.Aave) {
+      // TODO: switch on delegation type
+      vnode.state.currentDelegate = await (app.chain as Aave).chain.getDelegate(
+        app.user.activeAccount.address,
+        'voting'
+      );
+    }
+
+    m.redraw();
+  }
+
+  async setDelegate(vnode) {
+    const { address } = vnode.attrs;
+
+    if (app.chain.apiInitialized) {
+      let delegationPromise: Promise<void>;
+
+      if (app.chain.network === ChainNetwork.Compound) {
+        delegationPromise = (app.chain as Compound).chain.setDelegate(address);
+      } else if (app.chain.network === ChainNetwork.Aave) {
+        delegationPromise = (app.chain as Aave).chain.setDelegate(address);
+      }
+
+      if (delegationPromise) {
+        try {
+          await delegationPromise;
+          notifySuccess(`Sent transaction to delegate to ${address}`);
+          this.getDelegate(vnode);
+        } catch (err) {
+          notifyError(`${err.message}`);
+        }
+      }
+    }
   }
 
   view(vnode) {
@@ -128,58 +83,59 @@ class DelegateForm implements m.ClassComponent {
 
     const hasValue = app.chain.network === ChainNetwork.Compound;
 
-    return [
-      m(DelegateStats, {
-        currentDelegate: this.currentDelegate,
-      }),
-      m(Form, { class: 'DelegateForm' }, [
-        m(Grid, [
-          m(Col, [
-            m('h2', 'Set up your delegation:'),
-            m(FormGroup, [
-              m(FormLabel, 'Your delegate:'),
-              m(Input, {
-                name: 'address',
-                placeholder: 'Paste address you want to delegate to',
-                oninput: (e) => {
-                  const result = (e.target as any).value;
-                  this.form.address = result;
-                  m.redraw();
-                },
-              }),
-              hasValue && m(FormLabel, 'Amount to delegate:'),
-              hasValue &&
-                m(Input, {
-                  name: 'amount',
-                  placeholder: '10000',
-                  defaultValue: '',
-                  oninput: (e) => {
-                    const result = (e.target as any).value;
-                    this.form.amount = result;
-                    m.redraw();
-                  },
-                }),
-            ]),
-            m(FormGroup, [
-              m(Button, {
-                disabled: form.address === '' || loading,
-                intent: 'primary',
-                rounded: true,
-                label: 'Delegate!',
-                onclick: async (e) => {
-                  e.preventDefault();
-                  this.loading = true;
-                  await setDelegate(vnode);
-                  this.loading = false;
-                  m.redraw();
-                },
-                type: 'submit',
-              }),
-            ]),
-          ]),
-        ]),
-      ]),
-    ];
+    return (
+      <div class="DelegateForm">
+        <GovExplainer
+          statHeaders={[
+            {
+              statName: 'Current Delegate:',
+              statDescription: this.currentDelegate || '--',
+            },
+          ]}
+          stats={[
+            {
+              statHeading: 'Your address:',
+              stat: app.user.activeAccount.address || '--',
+            },
+          ]}
+        />
+        <CWText type="h4">Set up your delegation</CWText>,
+        <CWTextInput
+          // name: 'address',
+          label="Your delegate"
+          placeholder="Paste address you want to delegate to"
+          oninput={(e) => {
+            const result = (e.target as any).value;
+            this.form.address = result;
+            m.redraw();
+          }}
+        />
+        {hasValue && (
+          <CWTextInput
+            // name: 'amount',
+            label="Amount to delegate"
+            placeholder="10000"
+            defaultValue=""
+            oninput={(e) => {
+              const result = (e.target as any).value;
+              this.form.amount = result;
+              m.redraw();
+            }}
+          />
+        )}
+        <CWButton
+          disabled={form.address === '' || loading}
+          label="Delegate!"
+          onclick={async (e) => {
+            e.preventDefault();
+            this.loading = true;
+            await this.setDelegate(vnode);
+            this.loading = false;
+            m.redraw();
+          }}
+        />
+      </div>
+    );
   }
 }
 
