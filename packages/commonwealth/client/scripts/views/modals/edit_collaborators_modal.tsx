bb18fe3c8bf314b18ce9e543ac687312e13002da
Gabriel Duquette <gabrielduquette@gmail.com>
2022-08-16 00:15:22 +0100

    cancel button type

--- packages/commonwealth/client/scripts/views/modals/edit_collaborators_modal.tsx
+++ packages/commonwealth/client/scripts/views/modals/edit_collaborators_modal.tsx
@@ -180,6 +180,7 @@ export class EditCollaboratorsModal
           <div class="buttons-row">
             <CWButton
               label="Cancel"
+              buttonType="secondary-blue"
               onclick={(e) => {
                 $(e.target).trigger('modalexit');
               }}
