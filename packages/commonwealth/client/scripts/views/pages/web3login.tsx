dd12e8ae6adf82e401fe714da5079f750b8c3a39
Jake Naviasky <jake@commonwealth.im>
2022-09-08 15:27:54 -0400

    Stub out updated web3login page for cmnbot.

--- packages/commonwealth/client/scripts/views/pages/web3login.tsx
+++ packages/commonwealth/client/scripts/views/pages/web3login.tsx
@@ -1,6 +1,7 @@
 /* @jsx m */
 
 import m from 'mithril';
+import $ from 'jquery';
 import app from 'state';
 
 import 'pages/web3login.scss';
@@ -8,31 +9,51 @@ import 'pages/web3login.scss';
 import { link } from 'helpers';
 import Sublayout from 'views/sublayout';
 import LoginWithWalletDropdown from '../components/login_with_wallet_dropdown';
+import { PageNotFound } from './404';
 
 class Web3LoginPage implements m.ClassComponent {
   view() {
-    const loggingInWithAddress = m.route.param('loggingInWithAddress');
-    const joiningCommunity = m.route.param('joiningCommunity');
-    const joiningChain = m.route.param('joiningChain');
+    const token = m.route.param('token');
+    const profile_id = m.route.param('profile_id');
+    if (app.isCustomDomain() || !token || !profile_id) {
+      // hide page if invalid arguments or via custom domain
+      return (
+        <PageNotFound />
+      );
+    }
 
-    // oops! = address linking interrupted
-    const loginCopy = loggingInWithAddress
-      ? 'Login interrupted'
-      : joiningCommunity || joiningChain
-      ? 'Oops! An error occurred'
-      : app.isLoggedIn()
-      ? 'Oops! An error occurred'
-      : 'Login interrupted';
+    // hit auth callback and redirect
+    const onSuccess = async () => {
+      if (!app.isLoggedIn()) {
+        // TODO: fail
+      }
+      const { status, result } = await $.post(
+        `${app.serverUrl()}/auth/callback`,
+        {
+          jwt: app.user.jwt,
+          token,
+          profile_id,
+        }
+      );
+      if (status === 'Success') {
+        const responseToken = result;
+        // REDIRECT TO CMNBOT
+        window.location.href = `http://commonbot.domain?token=${responseToken}`;
+      } else {
+        // TODO: display error
+      }
+    };
 
     return (
       <Sublayout>
         <div class="Web3LoginPage">
           <div class="web3-login-container">
-            <h3>{loginCopy}</h3>
+            <h3>Log into Commonwealth</h3>
             {m(LoginWithWalletDropdown, {
               label: 'Try again',
-              joiningChain,
-              loggingInWithAddress,
+              joiningChain: null,
+              loggingInWithAddress: true,
+              onSuccess,
             })}
             {m.route.param('prev')
               ? link('a.web3login-go-home', m.route.param('prev'), 'Go back')
