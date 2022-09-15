4033248e105f9dd874a87e5a848db7081dbaf284
Gabriel Duquette <gabrielduquette@gmail.com>
2022-09-16 00:21:34 +0100

    styles

--- packages/commonwealth/client/scripts/views/components/component_kit/cw_breadcrumbs.tsx
+++ packages/commonwealth/client/scripts/views/components/component_kit/cw_breadcrumbs.tsx
@@ -5,14 +5,47 @@ import m from 'mithril';
 import 'components/component_kit/cw_breadcrumbs.scss';
 
 import { ComponentType } from './types';
-import { getClasses } from './helpers';
+import { CWText } from './cw_text';
 
-type BreadcrumbsAttrs = {};
+type BreadcrumbsType = {
+  label: string;
+  path: string;
+};
+
+type BreadcrumbsAttrs = {
+  breadcrumbs: Array<BreadcrumbsType>;
+};
 
 export class CWBreadcrumbs implements m.ClassComponent<BreadcrumbsAttrs> {
   view(vnode) {
-    // const {} = vnode.attrs;
+    const { breadcrumbs } = vnode.attrs;
 
-    return <div class={ComponentType.Breadcrumbs} />;
+    return (
+      <div class={ComponentType.Breadcrumbs}>
+        {breadcrumbs.map((b, k) => (
+          <>
+            <CWText
+              type="caption"
+              fontWeight="medium"
+              className={
+                k === breadcrumbs.length - 1 ? 'current-text' : 'parent-text'
+              }
+              onclick={() => m.route.set(b.path)}
+            >
+              {b.label}
+            </CWText>
+            {k !== breadcrumbs.length - 1 && (
+              <CWText
+                type="caption"
+                fontWeight="medium"
+                className="separator-text"
+              >
+                /
+              </CWText>
+            )}
+          </>
+        ))}
+      </div>
+    );
   }
 }
