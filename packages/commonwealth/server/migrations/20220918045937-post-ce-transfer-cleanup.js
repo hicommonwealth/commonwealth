a8b1316d2e128dd13abc8afa3382d51fbea125b0
Timothee Legros <timothee@ualberta.ca>
2022-09-17 23:32:26 -0600

    clean-up migrations further

--- packages/commonwealth/server/migrations/20220918045937-post-ce-transfer-cleanup.js
+++ packages/commonwealth/server/migrations/20220918045937-post-ce-transfer-cleanup.js
@@ -3,6 +3,53 @@
 module.exports = {
     up: async (queryInterface, Sequelize) => {
         await queryInterface.sequelize.transaction(async (t) => {
+            // copy the required columns to a new table
+            await queryInterface.sequelize.query(`
+                CREATE table "ChainEntityMeta" as (SELECT id, title, chain, author, thread_id
+                                                   from "ChainEntities");
+            `, {transaction: t});
+
+            // copy the id column to ce_id and add NOT NULL and UNIQUE constraints
+            await queryInterface.sequelize.query(`
+                ALTER TABLE "ChainEntityMeta"
+                    ADD COLUMN ce_id INTEGER UNIQUE;
+            `, {transaction: t});
+            await queryInterface.sequelize.query(`
+                UPDATE "ChainEntityMeta"
+                SET ce_id = id;
+            `, {transaction: t});
+            await queryInterface.sequelize.query(`
+                ALTER TABLE "ChainEntityMeta"
+                    ALTER COLUMN ce_id SET NOT NULL;
+            `, {transaction: t});
+
+            // set id column as an auto-increment primary key
+            await queryInterface.sequelize.query(`
+                ALTER TABLE "ChainEntityMeta"
+                    ADD PRIMARY KEY (id);
+            `, {transaction: t});
+            await queryInterface.sequelize.query(`
+                CREATE SEQUENCE ChainEntityMeta_id_seq OWNED BY "ChainEntityMeta".id;
+            `, {transaction: t});
+            await queryInterface.sequelize.query(`
+                ALTER TABLE "ChainEntityMeta"
+                    ALTER id SET DEFAULT nextval('ChainEntityMeta_id_seq'::regclass);
+            `, {transaction: t});
+
+            // set the appropriate foreign keys
+            await queryInterface.sequelize.query(`
+                ALTER TABLE "ChainEntityMeta"
+                    ADD CONSTRAINT "ChainEntityMeta_thread_id_fkey"
+                        FOREIGN KEY (thread_id) REFERENCES "Threads";
+            `, {transaction: t});
+            await queryInterface.sequelize.query(`
+                ALTER TABLE "ChainEntityMeta"
+                    ADD CONSTRAINT "ChainEntityMeta_chain_fkey"
+                        FOREIGN KEY (chain) REFERENCES "Chains";
+            `, {transaction: t});
+
+            await queryInterface.dropTable('ChainEntity', {transaction: t});
+
             // drop chain-events table
             await queryInterface.removeConstraint(
                 'Notifications',
