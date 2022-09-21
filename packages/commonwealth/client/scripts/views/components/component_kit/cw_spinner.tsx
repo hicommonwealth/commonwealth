9892c08a5773b02f86fea67e2b217939df2058d3
Gabriel Duquette <gabrielduquette@gmail.com>
2022-09-21 20:20:26 +0100

    size

--- packages/commonwealth/client/scripts/views/components/component_kit/cw_spinner.tsx
+++ packages/commonwealth/client/scripts/views/components/component_kit/cw_spinner.tsx
@@ -6,12 +6,15 @@ import 'components/component_kit/cw_spinner.scss';
 
 import { ComponentType } from './types';
 import { CWIcon } from './cw_icons/cw_icon';
+import { IconSize } from './cw_icons/types';
 
-export class CWSpinner implements m.ClassComponent {
-  view() {
+export class CWSpinner implements m.ClassComponent<{ size?: IconSize }> {
+  view(vnode) {
+    const { size = 'xl' } = vnode.attrs;
+
     return (
       <div className={ComponentType.Spinner}>
-        <CWIcon iconName="cow" iconSize="xl" />
+        <CWIcon iconName="cow" iconSize={size} />
       </div>
     );
   }
