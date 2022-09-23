0da821253e6b78d08c9fdffcd433df9e94205cd6
zakhap <zhs2104@columbia.edu>
2022-09-23 11:03:16 -0400

    Fix header logo

--- packages/commonwealth/client/scripts/views/pages/landing/landing_page_header.ts
+++ packages/commonwealth/client/scripts/views/pages/landing/landing_page_header.ts
@@ -2,6 +2,7 @@ import m from 'mithril';
 import 'pages/landing/landing_page_header.scss';
 import app from 'state';
 import { LoginModal } from 'views/modals/login_modal';
+import { CWIcon } from '../../components/component_kit/cw_icons/cw_icon';
 
 interface IAttrs {
   navs: { text: string; redirectTo: string }[];
@@ -72,9 +73,9 @@ const HeaderLandingPage: m.Component<IAttrs, IState> = {
           class: `landing-header ${INITIAL_HEADER_STYLE}     mt-8`,
         },
         [
-          m('img', {
-            class: 'w-40 md:w-48 lg:w-60',
-            src: '/static/img/logo.svg',
+          m(CWIcon, {
+            iconName: 'commonLogo',
+            iconSize: 'xxl',
             alt: 'Commonwealth',
             style: m.route.get() === '/' ? '' : 'cursor:pointer',
             onclick: () => redirectClick('/'),
