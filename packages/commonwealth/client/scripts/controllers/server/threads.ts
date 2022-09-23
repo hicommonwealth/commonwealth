c66809503aa9d724ce021a990d9386452a8c4548
zakhap <zhs2104@columbia.edu>
2022-09-23 17:10:10 -0400

    Fix new thread bug

--- packages/commonwealth/client/scripts/controllers/server/threads.ts
+++ packages/commonwealth/client/scripts/controllers/server/threads.ts
@@ -16,6 +16,7 @@ import {
   ChainEntity,
   NotificationSubscription,
   Poll,
+  Topic,
 } from 'models';
 import { NotificationCategories } from 'common-common/src/types';
 
@@ -242,8 +243,7 @@ class ThreadsController {
     stage: string,
     chainId: string,
     title: string,
-    topicName: string,
-    topicId: number,
+    topic: Topic,
     body?: string,
     url?: string,
     attachments?: string[],
@@ -253,7 +253,7 @@ class ThreadsController {
       // TODO: Change to POST /thread
       const response = await $.post(`${app.serverUrl()}/createThread`, {
         author_chain: app.user.activeAccount.chain.id,
-        author: JSON.stringify(app.user.activeAccount.profile),
+        author: JSON.stringify(app.user.activeAccount?.profile),
         chain: chainId,
         address,
         title: encodeURIComponent(title),
@@ -261,8 +261,8 @@ class ThreadsController {
         kind,
         stage,
         'attachments[]': attachments,
-        topic_name: topicName,
-        topic_id: topicId,
+        topic_name: topic.name,
+        topic_id: topic.id,
         url,
         readOnly,
         jwt: app.user.jwt,
@@ -293,7 +293,9 @@ class ThreadsController {
         NotificationSubscription.fromJSON(subscriptionJSON)
       );
       return result;
-    } catch (err) {
+    } 
+    catch (err) {
+      console.log(err.message)
       console.log('Failed to create thread');
       throw new Error(
         err.responseJSON && err.responseJSON.error
