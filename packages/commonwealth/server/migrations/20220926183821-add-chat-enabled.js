89aeab1e3387ac612107c85cd78ce9af8f9a8fff
Alex Young <alexyoung@alexs-m1-mbp.mynetworksettings.com>
2022-09-26 15:06:34 -0400

    fix

---
+++ packages/commonwealth/server/migrations/20220926183821-add-chat-enabled.js
@@ -0,0 +1,27 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.addColumn('Chains', 'chat_enabled', {
+      type: Sequelize.BOOLEAN,
+      allowNull: false,
+      defaultValue: true,
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.removeColumn('Chains', 'chat_enabled');
+  },
+};
--- packages/commonwealth/server/models/chain.ts
+++ packages/commonwealth/server/models/chain.ts
@@ -1,18 +1,15 @@
 import * as Sequelize from 'sequelize'; // must use "* as" to avoid scope errors
+import { ChainBase, ChainNetwork, ChainType } from 'common-common/src/types';
 import { RegisteredTypes } from '@polkadot/types/types';
 import { DataTypes } from 'sequelize';
 import { AddressAttributes, AddressInstance } from './address';
 import { ChainNodeInstance, ChainNodeAttributes } from './chain_node';
 import { StarredCommunityAttributes } from './starred_community';
-import {
-  TopicAttributes,
-  TopicInstance,
-} from './topic';
+import { TopicAttributes, TopicInstance } from './topic';
 import { ThreadAttributes } from './thread';
 import { CommentAttributes } from './comment';
 import { UserAttributes } from './user';
 import { ModelStatic, ModelInstance } from './types';
-import { ChainBase, ChainNetwork, ChainType } from 'common-common/src/types';
 
 export type ChainAttributes = {
   name: string;
@@ -23,6 +20,7 @@ export type ChainAttributes = {
   icon_url: string;
   active: boolean;
   type: ChainType;
+  chat_enabled: boolean;
   id?: string;
   description?: string;
   discord?: string;
@@ -55,12 +53,8 @@ export type ChainAttributes = {
     | StarredCommunityAttributes[]
     | StarredCommunityAttributes['id'][];
   topics?: TopicAttributes[] | TopicAttributes['id'][];
-  Threads?:
-    | ThreadAttributes[]
-    | ThreadAttributes['id'][];
-  Comments?:
-    | CommentAttributes[]
-    | CommentAttributes['id'][];
+  Threads?: ThreadAttributes[] | ThreadAttributes['id'][];
+  Comments?: CommentAttributes[] | CommentAttributes['id'][];
   Users?: UserAttributes[] | UserAttributes['id'][];
   ChainObjectVersion?; // TODO
 };
@@ -121,6 +115,11 @@ export default (
         defaultValue: true,
       },
       type: { type: dataTypes.STRING, allowNull: false },
+      chat_enabled: {
+        type: dataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: true,
+      },
       decimals: { type: dataTypes.INTEGER, allowNull: true },
       substrate_spec: { type: dataTypes.JSONB, allowNull: true },
       has_chain_events_listener: {
