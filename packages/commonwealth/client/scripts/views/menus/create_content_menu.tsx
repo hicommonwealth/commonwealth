1f97f06b8f8beab4dae8f5cc4c04a9799a88951c
Graham Johnson <graham@commonwealth.im>
2022-09-30 13:56:27 -0400

    Rewrite new proposal menu to popover attrs structure

--- packages/commonwealth/client/scripts/views/menus/create_content_menu.tsx
+++ packages/commonwealth/client/scripts/views/menus/create_content_menu.tsx
@@ -5,137 +5,123 @@ import 'components/create_content_popover.scss';
 import app from 'state';
 import { navigateToSubpage } from 'app';
 import { ProposalType, ChainBase, ChainNetwork } from 'common-common/src/types';
-import { CWMenuItem } from '../components/component_kit/cw_menu_item';
+import { SubstrateAccount } from 'client/scripts/controllers/chain/substrate/account';
 
-export class CreateContentMenu implements m.ClassComponent {
-  view(vnode) {
-    const activeAccount = app.user.activeAccount;
+const getCreateContentMenu = () => {
+  const activeAccount = app.user.activeAccount;
 
-    const showSnapshotOptions =
-      app.user.activeAccount && app.chain?.meta.snapshot.length > 0;
+  const showSnapshotOptions =
+    app.user.activeAccount && app.chain?.meta.snapshot.length > 0;
 
-    const topics = app.topics
-      .getByCommunity(app.activeChainId())
-      .reduce(
-        (acc, current) => (current.featuredInNewPost ? [...acc, current] : acc),
-        []
-      )
-      .sort((a, b) => a.name.localeCompare(b.name));
-    return (
-      <>
-        <CWMenuItem
-          onclick={() => {
-            navigateToSubpage('/new/discussion');
-          }}
-          label="New thread"
-        />
-        {topics.map((t) => (
-          <CWMenuItem
-            onclick={() => {
-              // TODO Graham 7-19-22: Let's find a non-localStorage solution
-              localStorage.setItem(
-                `${app.activeChainId()}-active-topic`,
-                t.name
-              );
-              if (t.defaultOffchainTemplate) {
-                localStorage.setItem(
-                  `${app.activeChainId()}-active-topic-default-template`,
-                  t.defaultOffchainTemplate
-                );
-              } else {
-                localStorage.removeItem(
-                  `${app.activeChainId()}-active-topic-default-template`
-                );
-              }
-              navigateToSubpage('/new/discussion');
-            }}
-            label={`New ${t.name} Thread`}
-          />
-        ))}
-        {app.chain?.network === ChainNetwork.Aave ||
-          app.chain?.network === ChainNetwork.dYdX ||
-          app.chain?.network === ChainNetwork.Compound ||
-          app.chain?.base === ChainBase.CosmosSDK ||
-          app.chain?.base === ChainBase.Substrate}
-        {app.chain?.base === ChainBase.CosmosSDK &&
-          app.chain?.network !== ChainNetwork.Terra && (
-            <CWMenuItem
-              onclick={() => navigateToSubpage('/new/proposal')}
-              label="New On-Chain Proposal"
-            />
-          )}
-        {app.chain?.base === ChainBase.Ethereum &&
-          app.chain?.network === ChainNetwork.Aave && (
-            <CWMenuItem
-              onclick={() => navigateToSubpage('/new/proposal')}
-              label="New On-Chain Proposal"
-            />
-          )}
-        {app.chain?.network === ChainNetwork.Compound && (
-          <CWMenuItem
-            onclick={() => navigateToSubpage('/new/proposal')}
-            label="New On-Chain Proposal"
-          />
-        )}
-        {app.chain?.base === ChainBase.Substrate &&
-          app.chain?.network !== ChainNetwork.Plasm && (
-            <>
-              <CWMenuItem
-                onclick={() =>
-                  navigateToSubpage('/new/proposal/:type', {
-                    type: ProposalType.SubstrateTreasuryProposal,
-                  })
-                }
-                label="New treasury proposal"
-              />
-              <CWMenuItem
-                onclick={() =>
-                  navigateToSubpage('/new/proposal/:type', {
-                    type: ProposalType.SubstrateDemocracyProposal,
-                  })
-                }
-                label="New democracy proposal"
-              />
-              <CWMenuItem
-                class={
-                  activeAccount && (activeAccount as any).isCouncillor
-                    ? ''
-                    : 'disabled'
-                }
-                onclick={() =>
-                  navigateToSubpage('/new/proposal/:type', {
-                    type: ProposalType.SubstrateCollectiveProposal,
-                  })
-                }
-                label="New council motion"
-              />
-              <CWMenuItem
-                onclick={() =>
-                  navigateToSubpage('/new/proposal/:type', {
-                    type: ProposalType.SubstrateBountyProposal,
-                  })
-                }
-                label="New bounty proposal"
-              />
-              <CWMenuItem
-                onclick={() =>
-                  navigateToSubpage('/new/proposal/:type', {
-                    type: ProposalType.SubstrateTreasuryTip,
-                  })
-                }
-                label="New tip"
-              />
-            </>
-          )}
-        {app.chain.network === ChainNetwork.Sputnik && (
-          <CWMenuItem
-            onclick={() => navigateToSubpage('/new/proposal')}
-            label="New Sputnik proposal"
-          />
-        )}
-        {showSnapshotOptions && (
-          <CWMenuItem
-            onclick={() => {
+  const topics = app.topics
+    .getByCommunity(app.activeChainId())
+    .reduce(
+      (acc, current) => (current.featuredInNewPost ? [...acc, current] : acc),
+      []
+    )
+    .sort((a, b) => a.name.localeCompare(b.name));
+
+  const showSputnikProposalItem = app.chain.network === ChainNetwork.Sputnik;
+
+  const showOnChainProposalItem =
+    (app.chain?.base === ChainBase.CosmosSDK &&
+      app.chain?.network !== ChainNetwork.Terra) ||
+    (app.chain?.base === ChainBase.Ethereum &&
+      app.chain?.network === ChainNetwork.Aave) ||
+    app.chain?.network === ChainNetwork.Compound;
+
+  const showSubstrateProposalItems =
+    app.chain?.base === ChainBase.Substrate &&
+    app.chain?.network !== ChainNetwork.Plasm;
+
+  return [
+    {
+      onclick: () => {
+        navigateToSubpage('/new/discussion');
+      },
+      label: 'New thread',
+    },
+    ...topics.map((t) => ({
+      onclick: () => {
+        // TODO Graham 7-19-22: Let's find a non-localStorage solution
+        localStorage.setItem(`${app.activeChainId()}-active-topic`, t.name);
+        if (t.defaultOffchainTemplate) {
+          localStorage.setItem(
+            `${app.activeChainId()}-active-topic-default-template`,
+            t.defaultOffchainTemplate
+          );
+        } else {
+          localStorage.removeItem(
+            `${app.activeChainId()}-active-topic-default-template`
+          );
+        }
+        navigateToSubpage('/new/discussion');
+      },
+      label: `New ${t.name} Thread`,
+    })),
+    ...(showOnChainProposalItem
+      ? [
+          {
+            onclick: () => navigateToSubpage('/new/proposal'),
+            label: 'New On-Chain Proposal',
+          },
+        ]
+      : []),
+    ...(showSputnikProposalItem
+      ? [
+          {
+            onclick: () => navigateToSubpage('/new/proposal'),
+            label: 'New Sputnik proposal',
+          },
+        ]
+      : []),
+    ...(showSubstrateProposalItems
+      ? [
+          {
+            onclick: () =>
+              navigateToSubpage('/new/proposal/:type', {
+                type: ProposalType.SubstrateTreasuryProposal,
+              }),
+            label: 'New treasury proposal',
+          },
+          {
+            onclick: () =>
+              navigateToSubpage('/new/proposal/:type', {
+                type: ProposalType.SubstrateDemocracyProposal,
+              }),
+            label: 'New democracy proposal',
+          },
+          {
+            class:
+              activeAccount && (activeAccount as SubstrateAccount).isCouncillor
+                ? ''
+                : 'disabled',
+            onclick: () =>
+              navigateToSubpage('/new/proposal/:type', {
+                type: ProposalType.SubstrateCollectiveProposal,
+              }),
+            label: 'New council motion',
+          },
+          {
+            onclick: () =>
+              navigateToSubpage('/new/proposal/:type', {
+                type: ProposalType.SubstrateBountyProposal,
+              }),
+            label: 'New bounty proposal',
+          },
+          {
+            onclick: () =>
+              navigateToSubpage('/new/proposal/:type', {
+                type: ProposalType.SubstrateTreasuryTip,
+              }),
+            label: 'New tip',
+          },
+        ]
+      : []),
+    ...(showSnapshotOptions
+      ? [
+          {
+            onclick: () => {
               const snapshotSpaces = app.chain.meta.snapshot;
               if (snapshotSpaces.length > 1) {
                 navigateToSubpage('/multiple-snapshots', {
@@ -144,11 +130,15 @@ export class CreateContentMenu implements m.ClassComponent {
               } else {
                 navigateToSubpage(`/new/snapshot/${snapshotSpaces}`);
               }
-            }}
-            label="New Snapshot Proposal"
-          />
-        )}
-      </>
-    );
+            },
+            label: 'New Snapshot Proposal',
+          },
+        ]
+      : []),
+  ];
+};
+export class CreateContentMenu implements m.ClassComponent {
+  view() {
+    return <>{getCreateContentMenu()}</>;
   }
 }
---
+++ packages/commonwealth/client/scripts/views/menus/menu_component.tsx
@@ -0,0 +1,5 @@
+// import { CWMenuItem } from '../components/component_kit/cw_menu_item';
+
+// export interface MenuComponent {
+//   constructor(): CWMenuItem[];
+// }
