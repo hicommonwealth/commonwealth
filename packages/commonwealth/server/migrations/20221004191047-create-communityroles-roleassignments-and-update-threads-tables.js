f8ea72503099046d1731fb747ff423d412cced78
Daniel Trinh <danielhtrinh1@gmail.com>
2022-10-04 16:47:27 -0700

    Migration with Blocker when inserting Role Assignments

--- packages/commonwealth/server/migrations/20221004191047-create-communityroles-roleassignments-and-update-threads-tables.js
+++ packages/commonwealth/server/migrations/20221004191047-create-communityroles-roleassignments-and-update-threads-tables.js
@@ -63,7 +63,17 @@ module.exports = {
         },
         { transaction: t }
       );
+    });
 
+    await queryInterface.sequelize.transaction(async (t) => {
+      // Delete Roles that are associated with chains that no longer exist
+      await queryInterface.sequelize.query(
+        `DELETE FROM "Roles" WHERE "chain_id" NOT IN (SELECT "id" FROM "Chains")`,
+        { transaction: t }
+      );
+    });
+
+    await queryInterface.sequelize.transaction(async (t) => {
       // Migrate CommunityRoles for Current Chains
       const query = `
       SELECT c.id as cid
@@ -113,24 +123,58 @@ module.exports = {
         })
       );
     });
+
+    await queryInterface.sequelize.transaction(async (t) => {
+      // Migrate RoleAssignments for Current Roles
+      const role_query = `
+      SELECT r.id as rid, cr.name as crname, r.address_id, r.permission, r.chain_id, cr.id as crid
+      FROM "Roles" r
+      LEFT JOIN "CommunityRoles" cr
+      ON cr.chain_id = r.chain_id AND CAST(r.permission AS TEXT) = CAST(cr.name AS TEXT);`;
+      const rolesWithCommunityRoles = await queryInterface.sequelize.query(
+        role_query,
+        {
+          transaction: t,
+        }
+      );
+      await Promise.all(
+        rolesWithCommunityRoles[0].map(async (r) => {
+          // create CommunityRoles for each chain
+          console.log(`Creating RoleAssignment for role ${r.rid} with address ${r.address_id}`);
+          await queryInterface.bulkInsert(
+            'RoleAssignments',
+            [
+              {
+                community_role_id: r.crid,
+                address_id: r.address_id,
+                created_at: new Date(),
+                updated_at: new Date(),
+              },
+            ],
+            { transaction: t }
+          );
+        })
+      );
+    });
+
     // Add indexes
-    // await queryInterface.sequelize.transaction(async (t) => {
-    //   await queryInterface.addIndex(
-    //     'CommunityRoles',
-    //     { fields: ['chain_id'] },
-    //     { transaction: t }
-    //   );
-    //   await queryInterface.addIndex(
-    //     'RoleAssignments',
-    //     { fields: ['community_role_id'] },
-    //     { transaction: t }
-    //   );
-    //   await queryInterface.addIndex(
-    //     'RoleAssignments',
-    //     { fields: ['address_id'] },
-    //     { transaction: t }
-    //   );
-    // });
+    await queryInterface.sequelize.transaction(async (t) => {
+      await queryInterface.addIndex(
+        'CommunityRoles',
+        { fields: ['chain_id'] },
+        { transaction: t }
+      );
+      await queryInterface.addIndex(
+        'RoleAssignments',
+        { fields: ['community_role_id'] },
+        { transaction: t }
+      );
+      await queryInterface.addIndex(
+        'RoleAssignments',
+        { fields: ['address_id'] },
+        { transaction: t }
+      );
+    });
 
     return new Promise((resolve, reject) => {
       resolve();
