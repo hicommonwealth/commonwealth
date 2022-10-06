0d451f57b4fe0f34aac1c3589e7086205e76b6cb
Daniel Trinh <danielhtrinh1@gmail.com>
2022-10-06 12:07:04 -0700

    Updating role

--- packages/commonwealth/server/util/roles.ts
+++ packages/commonwealth/server/util/roles.ts
@@ -1,35 +1,44 @@
 import { Transaction } from 'sequelize/types';
-import { RoleName } from 'server/models/community_role';
-import { sequelize, DB } from '../database';
+import { DB } from '../database';
+import { Permission } from '../models/role';
 import { RoleAssignmentInstance } from '../models/role_assignment';
+
+export type RoleInstanceWithPermission = RoleAssignmentInstance & {
+  permission: Permission;
+  chain_id: string;
+};
 
 export async function createDefaultCommunityRoles(
   models: DB,
   chain_id: string
 ): Promise<void> {
   // Create default roles
-  const community_roles = [
-    { name: RoleName.Admin, permissions: BigInt(0) },
-    { name: RoleName.Moderator, permissions: BigInt(0) },
-    { name: RoleName.Member, permissions: BigInt(0) },
-  ];
-  community_roles.forEach(async (crole) => {
-    const role = await models.CommunityRole.create({
+  try {
+    await models.CommunityRole.create({
       chain_id,
-      name: crole.name,
-      permissions: crole.permissions,
+      name: 'member',
+      permissions: BigInt(0),
     });
-    if (!role) {
-      throw new Error('Failed to create new role');
-    }
-  });
+    await models.CommunityRole.create({
+      chain_id,
+      name: 'moderator',
+      permissions: BigInt(0),
+    });
+    await models.CommunityRole.create({
+      chain_id,
+      name: 'admin',
+      permissions: BigInt(0),
+    });
+  } catch (error) {
+    throw new Error(`Couldn't create default community roles ${error}`);
+  }
 }
 
 export async function createRole(
   models: DB,
   address_id: number,
   chain_id: string,
-  role_name: RoleName,
+  role_name: Permission,
   transaction?: Transaction
 ): Promise<RoleAssignmentInstance> {
   // Get the community role that has given chain_id and name
@@ -51,4 +60,8 @@ export async function createRole(
     throw new Error('Failed to create new role');
   }
   return roleAssignment;
+}
+
+export async function findAllRoles(): Promise<RoleInstanceWithPermission> {
+  return;
 }
