286cf733242405675f3bced79dcbb6fba4b5842a
Timothee Legros <timothee@ualberta.ca>
2022-10-11 00:57:44 -0600

    removed custom headers

--- packages/commonwealth/client/scripts/helpers/getUrl.ts
+++ packages/commonwealth/client/scripts/helpers/getUrl.ts
@@ -30,11 +30,11 @@ export async function getFetch(url: string, queryParams?: { [key: string]: any }
     const response = await fetch(queryUrl || url, {
       method: 'GET',
       // mode: 'no-cors',
-      credentials: 'same-origin',
+      // credentials: 'same-origin',
       headers: {
         'Content-Type': 'application/json'
       },
-      referrerPolicy: 'strict-origin-when-cross-origin',
+      // referrerPolicy: 'strict-origin-when-cross-origin',
     });
     if (response.ok) {
       const {result} = await response.json();
