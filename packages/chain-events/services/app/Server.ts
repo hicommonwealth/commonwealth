a9b328f453660b90266e472d6bb6fe70c060a7f8
Timothee Legros <timothee@ualberta.ca>
2022-10-10 23:48:23 -0600

    custom cors

--- packages/chain-events/services/app/Server.ts
+++ packages/chain-events/services/app/Server.ts
@@ -21,7 +21,6 @@ const allowCrossDomain = function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
-  console.log("Added headers:", res.headers);
   next();
 }
 
--- packages/commonwealth/client/scripts/helpers/getUrl.ts
+++ packages/commonwealth/client/scripts/helpers/getUrl.ts
@@ -27,18 +27,19 @@ export async function getFetch(url: string, queryParams?: { [key: string]: any }
   try {
     const response = await fetch(queryUrl || url, {
       method: 'GET',
-      mode: 'no-cors',
+      // mode: 'no-cors',
       credentials: 'same-origin',
       headers: {
         'Content-Type': 'application/json'
       },
       referrerPolicy: 'strict-origin-when-cross-origin',
-    })
+    });
     if (response.ok) {
       const {result} = await response.json();
       return result;
+    } else {
+      console.error(`Fetch to ${queryUrl} failed, `, response);
     }
-    else console.error(response)
   } catch (e) {
     console.error(e);
   }
