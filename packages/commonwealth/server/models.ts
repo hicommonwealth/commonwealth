8150ddff2e5d662c50f30a283d6c83b97ddc2dca
Timothee Legros <timothee@ualberta.ca>
2022-10-17 03:54:08 -0600

    master merge fixes

--- packages/commonwealth/server/models.ts
+++ packages/commonwealth/server/models.ts
@@ -13,9 +13,8 @@ import {
   ChainCategoryTypeModelStatic
 } from './models/chain_category_type';
 import {
-  ChainEntityModelStatic
-} from './models/chain_entity';
-import { ChainEventModelStatic } from './models/chain_event';
+  ChainEntityMetaModelStatic
+} from './models/chain_entity_meta';
 import {
   ChainEventTypeModelStatic
 } from './models/chain_event_type';
@@ -103,8 +102,7 @@ export type Models = {
   Chain: ChainModelStatic;
   ChainCategory: ChainCategoryModelStatic;
   ChainCategoryType: ChainCategoryTypeModelStatic;
-  ChainEntity: ChainEntityModelStatic;
-  ChainEvent: ChainEventModelStatic;
+  ChainEntityMeta: ChainEntityMetaModelStatic;
   ChainEventType: ChainEventTypeModelStatic;
   ChainNode: ChainNodeModelStatic;
   ChatChannel: ChatChannelModelStatic;
