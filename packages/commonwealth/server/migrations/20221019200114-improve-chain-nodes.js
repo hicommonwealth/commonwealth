6a0620f4b34506cc44f23bb8d25dc2e2e61ab37a
zakhap <zhs2104@columbia.edu>
2022-10-19 16:43:55 -0400

    Migration works, now just gotta grind through the labels per node

--- packages/commonwealth/server/migrations/20221019200114-improve-chain-nodes.js
+++ packages/commonwealth/server/migrations/20221019200114-improve-chain-nodes.js
@@ -2,45 +2,55 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-     return queryInterface.sequelize.transaction(async (t) => {
+    return queryInterface.sequelize.transaction(async (transaction) => {
 
       // Add Columns
       await queryInterface.addColumn('ChainNodes', 'ss58', {
         type: Sequelize.INTEGER,
         allowNull: true
-      }, { transaction: t });
+      }, { transaction });
 
       await queryInterface.addColumn('ChainNodes', 'bech32', {
         type: Sequelize.STRING,
         allowNull: true
-      }, { transaction: t });
+      }, { transaction });
 
       await queryInterface.addColumn('ChainNodes', 'created_at', {
         type: Sequelize.DATE,
         allowNull: false,
         defaultValue: new Date(),
-      }, { transaction: t });
+      }, { transaction });
 
       await queryInterface.addColumn('ChainNodes', 'updated_at', {
         type: Sequelize.DATE,
         allowNull: false,
         defaultValue: new Date(),
-      }, { transaction: t });
+      }, { transaction });
 
 
       // Add entry names
+      const chainNodes = [{
+        name: 'Ethereum',
+        url: 'wss://eth-mainnet.g.alchemy.com/v2/pZsX6R3wGdnwhUJHlVmKg4QqsiS32Qm4'
+      }];
+
+      await Promise.all(chainNodes.map(async (cn) => {
+        const query = `UPDATE "ChainNodes" SET name='${cn.name}' WHERE url='${cn.url}';`;
+        queryInterface.sequelize.query(query, { transaction });
+      }))
+
     });
   },
 
   down: async (queryInterface, Sequelize) => {
-     return queryInterface.sequelize.transaction(async (transaction) => {
-      await queryInterface.sequelize.query( `UPDATE "ChainNodes" SET name=NULL;`, { transaction });
-      await queryInterface.sequelize.query( `UPDATE "ChainNodes" SET description=NULL;`, { transaction });
+    return queryInterface.sequelize.transaction(async (transaction) => {
+      await queryInterface.sequelize.query(`UPDATE "ChainNodes" SET name=NULL;`, { transaction });
+      await queryInterface.sequelize.query(`UPDATE "ChainNodes" SET description=NULL;`, { transaction });
       await queryInterface.removeColumn('ChainNodes', 'ss58', { transaction });
       await queryInterface.removeColumn('ChainNodes', 'bech32', { transaction });
       await queryInterface.removeColumn('ChainNodes', 'created_at', { transaction });
       await queryInterface.removeColumn('ChainNodes', 'updated_at', { transaction });
-     });
+    });
 
   }
 };
