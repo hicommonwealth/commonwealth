3d3f566012a84d181127dc9ef8d90e950491fe6f
Jake Naviasky <jake@commonwealth.im>
2022-10-19 17:36:27 -0400

    Stub out tests.

---
+++ packages/token-balance-cache/test/providers/cosmos.spec.ts
@@ -0,0 +1,8 @@
+import { assert } from 'chai';
+import { default as CosmosBalanceProvider } from '../../src/providers/cosmos'
+
+describe('CosmosBalanceProvider unit tests', () => {
+  it('should TODO', () => {
+    
+  })
+});
---
+++ packages/token-balance-cache/test/tbc.spec.ts
@@ -0,0 +1,8 @@
+import { assert } from 'chai';
+import { default as TBC } from '../src/tbc'
+
+describe('TBC unit tests', () => {
+  it('should TODO', () => {
+    
+  })
+});
