0bac378d28ed0ac8bf8b77e4f16f367547da5d5b
Daniel Trinh <danielhtrinh1@gmail.com>
2022-10-20 11:41:48 -0700

    Update contract.ts

--- packages/commonwealth/server/models/contract.ts
+++ packages/commonwealth/server/models/contract.ts
@@ -14,6 +14,8 @@ export type ContractAttributes = {
   token_name?: string;
   symbol?: string;
   type: string;
+  is_factory?: boolean;
+  nickname?: string;
 
   // associations
   ChainNode?: ChainNodeAttributes;
@@ -45,6 +47,8 @@ export default (
       abi_id: { type: dataTypes.INTEGER, allowNull: true },
       created_at: { type: dataTypes.DATE, allowNull: false },
       updated_at: { type: dataTypes.DATE, allowNull: false },
+      is_factory: { type: dataTypes.BOOLEAN, allowNull: true },
+      nickname: { type: dataTypes.STRING, allowNull: true },
     },
     {
       tableName: 'Contracts',
