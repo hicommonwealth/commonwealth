9b862b10baafd952f7af782d43b32678f5a13157
zakhap <zhs2104@columbia.edu>
2022-10-20 16:13:24 -0400

    All sidebar highlight

--- packages/commonwealth/client/scripts/views/components/sidebar/discussion_section.tsx
+++ packages/commonwealth/client/scripts/views/components/sidebar/discussion_section.tsx
@@ -45,7 +45,7 @@ export class DiscussionSection
       }
 
       return (
-        p === `/${app.activeChainId()}/` || p === `/${app.activeChainId()}`
+        p === `/${app.activeChainId()}/discussions` || p === `/${app.activeChainId()}discussions/`
       );
     };
 
