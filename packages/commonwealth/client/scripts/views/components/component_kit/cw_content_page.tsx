2beabfae4a1fa8668ca167036f1f6b959e5341ad
Gabriel Duquette <gabrielduquette@gmail.com>
2022-10-21 13:27:48 +0100

    very basic skeleton complete

--- packages/commonwealth/client/scripts/views/components/component_kit/cw_content_page.tsx
+++ packages/commonwealth/client/scripts/views/components/component_kit/cw_content_page.tsx
@@ -2,53 +2,41 @@ import m from 'mithril';
 
 import 'components/component_kit/cw_content_page.scss';
 
-import { ComponentType } from './types';
+import { Comment } from 'models';
+import { ComponentType, MenuItem } from './types';
 import { CWTabBar, CWTab } from './cw_tabs';
+import { CWText } from './cw_text';
+import { CWPopoverMenu } from './cw_popover/cw_popover_menu';
+import { CWIconButton } from './cw_icon_button';
 import { getClasses } from './helpers';
-
-// type ContentPageAttrs = {
-//     title: string;
-//     author: string;
-//     createdAt: moment.Moment;
-//     updatedAt: moment.Moment;
-//     actions: Array<PopoverMenuItem>
-
-//     subHeader?: m.Vnode; // for misc chain needs
-//     body: FormattedText; // all transforms done in controller or earlier
-//     subBody?: m.Vnode; // for misc chain needs
-//     sidebar?: Array<SidebarItem>; // for misc chain needs
-//     comments?: Array<Comments>; // optional because snapshots don't need them,
-//                                 // all transforms done in controller or earlier
-
-//     onThreadVote: () => void;
-//     onThreadLock: () => void;
-//     onThreadDelete: () => void;
-//     onCommentCreate: () => void;
-//     onCommentUpdate: () => void;
-//     onCommentDelete: () => void;
-//   }
 
 type SidebarItem = {
   label: string;
   item: m.Vnode;
 };
 
+type SidebarComponents = [
+  item?: SidebarItem,
+  item?: SidebarItem,
+  item?: SidebarItem
+];
+
 type ContentPageAttrs = {
+  actions?: Array<MenuItem>;
+  author: string;
   body: m.Vnode;
+  comments?: Array<Comment<any>>;
+  createdAt: moment.Moment;
   showSidebar?: boolean;
-  // tuple, not array
-  sidebarComponents?: [
-    item?: SidebarItem,
-    item?: SidebarItem,
-    item?: SidebarItem,
-    item?: SidebarItem
-  ];
+  sidebarComponents?: SidebarComponents;
+  subBody?: m.Vnode;
+  subHeader?: m.Vnode;
+  title: string;
+  updatedAt?: moment.Moment;
 };
 
 export class CWContentPage implements m.ClassComponent<ContentPageAttrs> {
   private tabSelected: number;
-
-  //
 
   oninit(vnode: m.VnodeDOM<ContentPageAttrs, this>) {
     if (vnode.attrs.sidebarComponents.length > 0) {
@@ -57,7 +45,19 @@ export class CWContentPage implements m.ClassComponent<ContentPageAttrs> {
   }
 
   view(vnode: m.VnodeDOM<ContentPageAttrs, this>) {
-    const { body, showSidebar, sidebarComponents } = vnode.attrs;
+    const {
+      actions,
+      author,
+      body,
+      comments,
+      createdAt,
+      showSidebar,
+      sidebarComponents,
+      subBody,
+      subHeader,
+      title,
+      updatedAt,
+    } = vnode.attrs;
 
     return (
       <div class={ComponentType.ContentPage}>
@@ -68,25 +68,45 @@ export class CWContentPage implements m.ClassComponent<ContentPageAttrs> {
           )}
         >
           <CWTabBar>
+            <CWTab
+              label="Thread"
+              onclick={() => {
+                this.tabSelected = 0;
+              }}
+              isSelected={this.tabSelected === 0}
+            />
             {sidebarComponents.map((item, i) => (
               <CWTab
                 label={item.label}
                 onclick={() => {
-                  this.tabSelected = i;
+                  this.tabSelected = i + 1;
                 }}
-                isSelected={this.tabSelected === i}
+                isSelected={this.tabSelected === i + 1}
               />
             ))}
           </CWTabBar>
-          {this.tabSelected === 'viewProposal' && body}
-          {showSidebar &&
-            this.tabSelected === 'viewSidebar' &&
-            sidebarComponents}
+          {this.tabSelected === 0 && body}
+          {this.tabSelected === 1 && sidebarComponents[0]}
+          {this.tabSelected === 2 && sidebarComponents[1]}
+          {this.tabSelected === 3 && sidebarComponents[2]}
         </div>
         <div
           class={getClasses<{ showSidebar?: boolean }>({ showSidebar }, 'body')}
         >
+          <div>
+            <CWText>{title}</CWText>
+            <CWText>{author}</CWText>
+            <CWText>{createdAt}</CWText>
+            <CWText>{updatedAt}</CWText>
+            <CWPopoverMenu
+              trigger={<CWIconButton iconName="dotsVertical" />}
+              menuItems={actions}
+            />
+          </div>
+          {subHeader}
           {body}
+          {comments}
+          {subBody}
           {showSidebar && sidebarComponents}
         </div>
       </div>
