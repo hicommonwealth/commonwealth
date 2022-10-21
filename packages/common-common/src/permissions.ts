716ed0ec885432484010058bda4e08ed2c4ecd35
Daniel Trinh <danielhtrinh1@gmail.com>
2022-10-21 15:47:46 -0700

    Create permissions.ts

---
+++ packages/common-common/src/permissions.ts
@@ -0,0 +1,24 @@
+export enum Action {
+    INVITE_MEMBERS = 1 << 0,
+    BAN_MEMBERS = 1 << 1,
+    MANAGE_COMMUNITY = 1 << 2,
+    ADMINISTRATOR = 1 << 3,
+    MANAGE_ROLES = 1 << 4,
+    MANAGE_WEBHOOKS = 1 << 5,
+    MANAGE_TOPICS = 1 << 6,
+    MANAGE_CHAT_CHANNELS = 1 << 7,
+    VIEW_COMMUNITY_INSIGHTS = 1 << 8,
+    MANAGE_INVITES = 1 << 9,
+    VIEW_TOPIC = 1 << 10,
+    VIEW_CHAT_CHANNELS = 1 << 11,
+    CREATE_THREAD = 1 << 12,
+    MANAGE_THREADS = 1 << 13,
+    CREATE_CHAT = 1 << 14,
+    CREATE_REACTION = 1 << 15,
+    CREATE_COMMENT = 1 << 16,
+    CREATE_POLL = 1 << 17,
+    VOTE_ON_POLLS = 1 << 18,
+    MANAGE_POLLS = 1 << 19,
+}
+
+type Permission = bigint;
