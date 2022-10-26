4d44d838c0738407c4a919c332302e91ad1b59c1
Graham Johnson <graham@commonwealth.im>
2022-10-26 13:52:43 -0400

    Update proper replacement fn

--- packages/commonwealth/client/scripts/stores/ChainIdStore.ts
+++ packages/commonwealth/client/scripts/stores/ChainIdStore.ts
@@ -2,32 +2,54 @@ import { IHasChainId } from './interfaces';
 import Store from './Store';
 
 class ChainIdStore<T extends IHasChainId> extends Store<T> {
-  private _storeChainId: { [chainId: string]: T } = {};
+  private _storeChainId: { [chainId: string]: Array<T> } = {};
+  private _sortingFunction: (a: T, b: T) => number;
 
-  public add(n: T) {
+  constructor(sortingFunction: (a: T, b: T) => number) {
+    super();
+    this._sortingFunction = sortingFunction;
+  }
+
+  public add(n: T): ChainIdStore<T> {
     super.add(n);
-    this._storeChainId[n.chainId] = n;
+
+    // Create chain-scoped store if it does not exist
+    if (!this._storeChainId[n.chainId]) {
+      this._storeChainId[n.chainId] = [] as Array<T>;
+    }
+
+    // Add to chain-scoped store
+    this._storeChainId[n.chainId].push(n);
+
+    // Sort store
+    if (this._sortingFunction) {
+      this._storeChainId[n.chainId].sort(this._sortingFunction);
+    }
+
     return this;
   }
 
-  public remove(n: T) {
-    super.remove(n);
-    delete this._storeChainId[n.chainId.toString()];
+  public remove(n: T): ChainIdStore<T> {
+    const idx = this._storeChainId[n.chainId].indexOf(n);
+    if (idx === -1) throw new Error('Object not in store.');
+    this._storeChainId[n.chainId].splice(idx, 1);
     return this;
   }
 
-  public update(n: T) {
-    super.update(n, (a) => a.chainId === n.chainId);
-    this._storeChainId[n.chainId.toString()] = n;
+  public update(n: T, eqFn: (a: T) => boolean): ChainIdStore<T> {
+    const idx = this._storeChainId[n.chainId].findIndex(eqFn);
+    if (idx === -1) throw new Error('Object not in store.');
+    this._storeChainId[n.chainId][idx] = n;
     return this;
   }
 
-  public clear() {
+  public clear(): ChainIdStore<T> {
     super.clear();
     this._storeChainId = {};
+    return this;
   }
 
-  public getByChain(chainId: string): T {
+  public getByChainId(chainId: string): Array<T> {
     return this._storeChainId[chainId];
   }
 }
