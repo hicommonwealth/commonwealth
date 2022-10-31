ddd8c7b4c54f2e71ac292927e6fcb04f97e7a6c0
Graham Johnson <graham@commonwealth.com>
2022-10-31 12:07:53 -0400

    First pass add typing to CWDropdown, make menu types flexible

--- packages/commonwealth/client/scripts/views/components/component_kit/cw_dropdown.tsx
+++ packages/commonwealth/client/scripts/views/components/component_kit/cw_dropdown.tsx
@@ -1,48 +1,59 @@
 /* @jsx m */
 
-import m from 'mithril';
+import m, { VnodeDOM } from 'mithril';
 
 import 'components/component_kit/cw_dropdown.scss';
 
 import { CWTextInput } from './cw_text_input';
 import { CWPopoverMenuItem } from './cw_popover/cw_popover_menu';
-import { MenuItem } from './types';
+import { DefaultMenuItem } from './types';
 
 export type DropdownInputAttrs = {
-  inputOptions: Array<MenuItem>;
-  onSelect?: (optionLabel: string, index?: number) => void;
   initialValue?: string;
+  defaultMenuItems: Array<DefaultMenuItem>;
+  onSelect?: (optionLabel: string, index?: number) => void;
+  searchable?: boolean;
 };
 
 export class CWDropdown implements m.ClassComponent<DropdownInputAttrs> {
   private showDropdown: boolean;
   private selectedValue: string;
+  private activeMenuItems: Array<DefaultMenuItem>;
 
-  oninit(vnode) {
+  oninit(vnode: VnodeDOM<DropdownInputAttrs, this>) {
     this.showDropdown = false;
     this.selectedValue =
-      vnode.attrs.initialValue ?? vnode.attrs.inputOptions[0].label;
+      vnode.attrs.initialValue ?? vnode.attrs.defaultMenuItems[0].label;
   }
 
-  view(vnode) {
-    const { inputOptions, onSelect } = vnode.attrs;
+  view(vnode: VnodeDOM<DropdownInputAttrs, this>) {
+    const { defaultMenuItems, onSelect, searchable } = vnode.attrs;
+    if (!this.selectedValue) this.activeMenuItems = defaultMenuItems;
 
     return (
       <div class="dropdown-wrapper">
         <CWTextInput
           iconRight="chevronDown"
           placeholder={this.selectedValue}
-          displayOnly
+          displayOnly={!searchable}
           iconRightonclick={() => {
             // Only here because it makes TextInput display correctly
           }}
           onclick={() => {
             this.showDropdown = !this.showDropdown;
           }}
+          oninput={(e) => {
+            if (e.target.value?.length > 0) {
+              const inputText = e.target.value;
+              this.activeMenuItems = defaultMenuItems.filter((item: DefaultMenuItem) => item.label.includes(inputText))
+              this.selectedValue = this.activeMenuItems[0].label;
+
+            }
+          }}
         />
         {this.showDropdown && (
           <div class="dropdown-options-display">
-            {inputOptions.map((item, idx) => {
+            {this.activeMenuItems.map((item, idx) => {
               return (
                 <CWPopoverMenuItem
                   {...item}
--- packages/commonwealth/client/scripts/views/components/component_kit/types.ts
+++ packages/commonwealth/client/scripts/views/components/component_kit/types.ts
@@ -49,23 +49,31 @@ export type StyleAttrs = {
   className?: string;
 };
 
+export type DividerMenuItem = { type: 'divider' };
+
+export type HeaderMenuItem = { type: 'header'; label: string };
+
+export type DefaultMenuItem = {
+  disabled?: boolean;
+  iconLeft?: IconName;
+  iconRight?: IconName;
+  isSecondary?: boolean;
+  label: string;
+  onclick: () => void;
+  type?: 'default';
+};
+
+export type NotificationMenuItem = {
+  hasUnreads: boolean;
+  iconLeft?: IconName;
+  iconRight?: IconName;
+  label: string;
+  onclick: () => void;
+  type: 'notification';
+};
+
 export type MenuItem =
-  | { type: 'divider' }
-  | { type: 'header'; label: string }
-  | {
-      disabled?: boolean;
-      iconLeft?: IconName;
-      iconRight?: IconName;
-      isSecondary?: boolean;
-      label: string;
-      onclick: () => void;
-      type?: 'default';
-    }
-  | {
-      hasUnreads: boolean;
-      iconLeft?: IconName;
-      iconRight?: IconName;
-      label: string;
-      onclick: () => void;
-      type: 'notification';
-    };
+  | DividerMenuItem
+  | HeaderMenuItem
+  | DefaultMenuItem
+  | NotificationMenuItem;
