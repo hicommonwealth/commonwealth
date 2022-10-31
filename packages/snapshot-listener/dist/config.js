059bec848ccde02bf4938ad81be389c8f715716c
Richard Melkonian <r.v.melkonian@gmail.com>
2022-10-31 14:51:25 +0000

    updated .gitignore

--- packages/snapshot-listener/dist/config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.rabbitMQ = void 0;
-exports.rabbitMQ = {
-    url: 'amqp://localhost',
-    exchangeName: 'logExchange',
-};
