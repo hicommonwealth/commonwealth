aeddaef7fdf2fd24031f34c6c31bad23178a0ac2
Daniel Trinh <danielhtrinh1@gmail.com>
2022-11-01 15:01:15 -0700

    Add tests

--- packages/commonwealth/test/unit/permissions/computePermissions.spec.ts
+++ packages/commonwealth/test/unit/permissions/computePermissions.spec.ts
@@ -10,6 +10,7 @@ describe('computePermissions() unit tests', () => {
   const overwrite_admin = { allow: base_permission, deny: base_permission };
   const overwrite_moderator = { allow: base_permission, deny: base_permission };
   const overwrite_member = { allow: base_permission, deny: base_permission };
+  const chain_permission = { allow: base_permission, deny: base_permission };
 
   it('should correctly computePermissions for allowing createThread action with two roles overwrites', () => {
     // eslint-disable-next-line no-bitwise
@@ -18,9 +19,15 @@ describe('computePermissions() unit tests', () => {
     // eslint-disable-next-line no-bitwise
     overwrite_moderator.deny = BigInt(1) << BigInt(Action.CREATE_THREAD);
 
-    const permission = computePermissions(base_permission, [overwrite_moderator, overwrite_admin]);
+    const permission = computePermissions(base_permission, [
+      overwrite_moderator,
+      overwrite_admin,
+    ]);
     // eslint-disable-next-line no-bitwise
-    assert.deepEqual(permission, (base_permission | (BigInt(1) << BigInt(Action.CREATE_THREAD))));
+    assert.deepEqual(
+      permission,
+      base_permission | (BigInt(1) << BigInt(Action.CREATE_THREAD))
+    );
   });
 
   it('should correctly computePermissions for denying createThread action with two roles overwrites', () => {
@@ -30,9 +37,23 @@ describe('computePermissions() unit tests', () => {
     // eslint-disable-next-line no-bitwise
     overwrite_moderator.allow = BigInt(1) << BigInt(Action.CREATE_THREAD);
 
-    const permission = computePermissions(base_permission, [overwrite_moderator, overwrite_admin]);
+    const permission = computePermissions(base_permission, [
+      overwrite_moderator,
+      overwrite_admin,
+    ]);
     // eslint-disable-next-line no-bitwise
-    assert.deepEqual(permission, (base_permission));
+    assert.deepEqual(permission, base_permission);
   });
 
+  it('should correctly computePermissions for denying createThread action with community permissions overwrite', () => {
+    // eslint-disable-next-line no-bitwise
+    overwrite_admin.deny = BigInt(1) << BigInt(Action.CREATE_THREAD);
+
+    const permission = computePermissions(base_permission, [
+      chain_permission,
+      overwrite_admin,
+    ]);
+    // eslint-disable-next-line no-bitwise
+    assert.deepEqual(permission, base_permission);
+  });
 });
