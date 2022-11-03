26fb919f42ff18894c9574a27a270f416afab4e6
Richard Melkonian <r.v.melkonian@gmail.com>
2022-11-03 21:39:57 +0000

    improved error handling in producer.ts

--- packages/snapshot-listener/src/producer.spec.ts
+++ packages/snapshot-listener/src/producer.spec.ts
@@ -7,4 +7,22 @@ describe('Producer ', () => {
 		const produceMessage = createMQProducer(rabbitMQ.url, "test_queu");
 		expect(produceMessage).to.be.a('function');
 	});
+
+	it('should throw an error if the message is not a string', () => {
+		const produceMessage = createMQProducer(rabbitMQ.url, "test_queue");
+		// @ts-ignore
+		expect(() => produceMessage(123)).to.throw();
+	});
+
+	it('should throw an error if the message is empty', () => {
+		const produceMessage = createMQProducer(rabbitMQ.url, "test_queue");
+		expect(() => produceMessage('')).to.throw();
+	});
+
+	it('should throw an error if the message is undefined', () => {
+		const produceMessage = createMQProducer(rabbitMQ.url, "test_queue");
+		//@ts-ignore
+		expect(() => produceMessage(undefined)).to.throw();
+	});
+
 })
--- packages/snapshot-listener/src/producer.ts
+++ packages/snapshot-listener/src/producer.ts
@@ -1,27 +1,46 @@
 import amqp, { Connection } from 'amqplib/callback_api'
+import { Logger } from 'tslog';
+
+const log: Logger = new Logger({ name: 'producer-logger' });
 
 const createMQProducer = (amqpUrl: string, queueName: string) => {
-  console.log('Connecting to RabbitMQ...')
   let ch: any
   amqp.connect(amqpUrl, (errorConnect: Error, connection: Connection) => {
     if (errorConnect) {
-      console.log('Error connecting to RabbitMQ: ', errorConnect)
+      log.error(`Error connecting to RabbitMQ: ${errorConnect}`);
       return
     }
 
     connection.createChannel((errorChannel, channel) => {
       if (errorChannel) {
-        console.log('Error creating channel: ', errorChannel)
+        log.error(`Error creating channel: ${errorChannel}`);
         return
       }
 
       ch = channel
-      console.log('Connected to RabbitMQ')
     })
   })
+
   return (msg: string) => {
-    console.log('Produce message to RabbitMQ...')
-    ch.sendToQueue(queueName, Buffer.from(msg))
+    try{ 
+      switch (msg) {
+        case '':
+          log.error('Message cannot be empty');
+          throw new Error('Message cannot be empty');
+        case undefined:
+          log.error('Message cannot be undefined');
+          throw new Error('Message cannot be undefined');
+        case null:
+          log.error('Message cannot be null');
+          throw new Error('Message cannot be null');
+        default:
+          log.info(`Sending message to queue ${queueName}`);
+          ch.sendToQueue(queueName, Buffer.from(msg))
+      }
+    } catch (error) {
+      console.log('Error producing message: ', error)
+      throw error
+    }
   }
 }
 
