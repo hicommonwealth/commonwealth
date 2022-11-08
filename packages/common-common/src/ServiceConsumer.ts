e1b09b9abe7531cd3793a4a546d2e776e3df2ae8
Timothee Legros <timothee@ualberta.ca>
2022-11-08 11:08:19 -0500

    ServiceConsumer fixes

--- packages/common-common/src/ServiceConsumer.ts
+++ packages/common-common/src/ServiceConsumer.ts
@@ -2,8 +2,9 @@
 import crypto from "crypto";
 import { addPrefix, factory, formatFilename } from "./logging";
 import { RabbitMQController, RascalSubscriptions } from "./rabbitmq";
+import Rollbar from "rollbar";
+import {Logger} from "typescript-logging";
 
-let log;
 
 export type RabbitMQSubscription = {
   messageProcessor: (data: any) => Promise<any>;
@@ -17,11 +18,14 @@ export class ServiceConsumer {
   public readonly rabbitMQController: RabbitMQController;
   public readonly subscriptions: RabbitMQSubscription[];
   private _initialized = false;
+  protected rollbar: Rollbar;
+  private log: Logger
 
   constructor(
     _serviceName: string,
     _rabbitmqController: RabbitMQController,
-    _subscriptions: RabbitMQSubscription[]
+    _subscriptions: RabbitMQSubscription[],
+    rollbar?: Rollbar
   ) {
     this.serviceName = _serviceName;
     // TODO: make this deterministic somehow
@@ -29,52 +33,63 @@ export class ServiceConsumer {
     this.subscriptions = _subscriptions;
 
     // setup logger
-    log = factory.getLogger(
+    this.log = factory.getLogger(
       addPrefix(formatFilename(__filename), [this.serviceName, this.serviceId])
     );
 
     this.rabbitMQController = _rabbitmqController;
+    this.rollbar = rollbar;
   }
 
   public async init(): Promise<void> {
-    log.info(`Initializing service-consumer: ${this.serviceName}-${this.serviceId}`);
+    this.log.info(`Initializing service-consumer: ${this.serviceName}-${this.serviceId}`);
 
     if (!this.rabbitMQController.initialized) {
       try {
         await this.rabbitMQController.init();
       } catch (e) {
-        log.error("Failed to initialize the RabbitMQ Controller", e);
+        this.log.error("Failed to initialize the RabbitMQ Controller", e);
+        this.rollbar?.error("Failed to initialize the RabbitMQ Controller", e)
       }
     }
 
-    try {
       // start all the subscriptions for this consumer
       for (const sub of this.subscriptions) {
-        await this.rabbitMQController.startSubscription(
-          sub.messageProcessor,
-          sub.subscriptionName,
-          sub.msgProcessorContext
-        );
+        try {
+          await this.rabbitMQController.startSubscription(
+            sub.messageProcessor,
+            sub.subscriptionName,
+            sub.msgProcessorContext
+          );
+        } catch (e) {
+          this.log.error(
+            `Failed to start the '${sub.subscriptionName}' subscription with the '${sub.messageProcessor.name}' `
+              + `processor function using context: ${JSON.stringify(sub.msgProcessorContext)}`,
+            e
+          );
+          this.rollbar?.critical(
+            `Failed to start the '${sub.subscriptionName}' subscription with the '${sub.messageProcessor.name}' `
+            + `processor function using context: ${JSON.stringify(sub.msgProcessorContext)}`,
+            e
+          )
+        }
       }
-    } catch (e) {
-      log.error("A critical error occurred.", e);
-      // TODO: rollbar error reporting
-    }
+
 
     this._initialized = true;
   }
 
   public async shutdown(): Promise<void> {
-    log.info(`Service Consumer ${this.serviceName}:${this.serviceId} shutting down...`);
+    this.log.info(`Service Consumer ${this.serviceName}:${this.serviceId} shutting down...`);
     if (this.rabbitMQController.initialized) {
-      log.info("Attempting to shutdown RabbitMQ Broker...")
+      this.log.info("Attempting to shutdown RabbitMQ Broker...")
       await this.rabbitMQController.shutdown();
     }
 
     // any other future clean-up + logging
 
     this._initialized = false;
-    log.info(`Service Consumer ${this.serviceName}:${this.serviceId} shut down successful`);
+    this.log.info(`Service Consumer ${this.serviceName}:${this.serviceId} shut down successful`);
   }
 
   public get initialized(): boolean {
