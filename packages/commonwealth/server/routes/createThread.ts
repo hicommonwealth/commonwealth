e335bd90cb813b191d06de42a6c9eea87e0fb1e3
Jake Naviasky <jake@commonwealth.im>
2022-11-11 09:01:39 -0500

    Fix imports.

--- packages/commonwealth/server/routes/createThread.ts
+++ packages/commonwealth/server/routes/createThread.ts
@@ -31,7 +31,6 @@ import {
 import checkRule from '../util/rules/checkRule';
 import RuleCache from '../util/rules/ruleCache';
 import BanCache from '../util/banCheckCache';
-import { findAllRoles } from '../util/roles';
 
 const log = factory.getLogger(formatFilename(__filename));
 
