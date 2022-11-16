1d65c225b643b9d53343bfa3a6e90f12c4ee810a
jalfredr <jesse.e.richards@gmail.com>
2022-11-16 12:37:16 -0500

    corrected "snapshots" references to "snapshot" in Chains table

--- packages/commonwealth/server/migrations/20221115210648-migrate-snapshot-data-to-new-table.js
+++ packages/commonwealth/server/migrations/20221115210648-migrate-snapshot-data-to-new-table.js
@@ -11,20 +11,20 @@ module.exports = {
     return queryInterface.sequelize.transaction(async (t) => {
       await queryInterface.sequelize.query(
         `INSERT INTO "SnapshotSpaces" (snapshot_space) 
-      SELECT DISTINCT UNNEST(snapshots) FROM "Chains";`,
+      SELECT DISTINCT UNNEST(snapshot) FROM "Chains";`,
         { transaction: t }
       );
 
       await queryInterface.sequelize.query(
         `INSERT INTO CommunitySnapshotSpaces (chain_id, snapshot_space_id)
           SELECT c.id, s.id 
-          FROM (SELECT DISTINCT id, UNNEST(snapshots) as snaps FROM "Chains") c
+          FROM (SELECT DISTINCT id, UNNEST(snapshot) as snaps FROM "Chains") c
             INNER JOIN "SnapshotSpaces" s
               ON c.snaps = s.snapshot_space;`,
         { transaction: t }
       );
 
-      await queryInterface.sequelize.removeColumn('Chains', 'snapshots', {
+      await queryInterface.sequelize.removeColumn('Chains', 'snapshot', {
         transaction: t,
       });
     });
@@ -33,7 +33,7 @@ module.exports = {
   down: async (queryInterface, Sequelize) => {
     await queryInterface.sequelize.addcolumn(
       'Chains', 
-      'snapshots', 
+      'snapshot', 
       {
         type: dataTypes.ARRAY(dataTypes.STRING),
         allowNull: true,
@@ -43,7 +43,7 @@ module.exports = {
 
       await queryInterface.sequelize.query(
         `UPDATE "Chains" 
-          SET snapshots = sn.snaps
+          SET snapshot = sn.snaps
         FROM (SELECT css.chain_id, array_agg(ss.snapshot_space) as snaps
               FROM CommunitySnapshotSpaces css
               INNER JOIN SnapshotSpaces ss ON css.snapshot_space_id = ss.id
