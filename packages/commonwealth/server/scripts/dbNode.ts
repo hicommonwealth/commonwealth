4ab90ce6e8458d4a56931f8028cacdcc15f7218d
Timothee Legros <timothee@ualberta.ca>
2022-11-17 11:55:19 -0700

    revert dbNode query

--- packages/commonwealth/server/scripts/dbNode.ts
+++ packages/commonwealth/server/scripts/dbNode.ts
@@ -142,7 +142,10 @@ async function mainProcess(
       ON cc.chain_id = c.id
     LEFT JOIN "Contracts" con
       ON con.id = cc.contract_id
-  WHERE c."has_chain_events_listener" = true;
+  WHERE c."has_chain_events_listener" = true
+    AND (con.type IN ('marlin-testnet', 'aave', 'compound') OR
+         (c.base = 'substrate' AND c.type ='chain') OR
+         (c.base = 'cosmos' AND (c.type='token' OR c.type='chain')));
   `;
   const allChains = (await pool.query(query)).rows;
 
