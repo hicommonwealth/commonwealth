25c0533b692cf4568ca684e3e1931b13c0d26ca3
Timothee Legros <timothee@ualberta.ca>
2022-11-23 21:21:15 -0700

    updated build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,6 @@
-if [ "$CE_ENV" ]; then
+if [ "$CE_BUILD" = true ]; then
   yarn --cwd packages/chain-events build-services
-elif [ "$CW_ENV" ]; then
+elif [ "$CW_BUILD" = true ]; then
   yarn --cwd packages/commonwealth heroku-postbuild
 else
   yarn --cwd packages/chain-events build-services && yarn --cwd packages/commonwealth heroku-postbuild
