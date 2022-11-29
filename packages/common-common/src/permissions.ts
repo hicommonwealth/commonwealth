7e71a7fde36fdce6265bf8b26bfb4bf15091f916
Daniel Trinh <danielhtrinh1@gmail.com>
2022-11-28 16:56:59 -0800

    Added more tests for implicit permissions

--- packages/common-common/src/permissions.ts
+++ packages/common-common/src/permissions.ts
@@ -48,19 +48,20 @@ export const BASE_PERMISSIONS: Permissions =
   addPermission(BigInt(0), Action.VIEW_THREADS);
 
 const IMPLICIT_PERMISSIONS_BY_ACTION = new Map<Action, Action[]>([
-  [Action.DELETE_THREAD, [Action.CREATE_REACTION, Action.VIEW_REACTIONS, Action.CREATE_COMMENT, Action.VIEW_COMMENTS, Action.EDIT_COMMENT, Action.DELETE_COMMENT, Action.CREATE_POLL, Action.VIEW_POLLS, Action.VOTE_ON_POLLS, Action.VIEW_THREADS, Action.CREATE_THREAD, Action.EDIT_THREAD]],
-  [Action.EDIT_THREAD, [Action.CREATE_REACTION, Action.VIEW_REACTIONS, Action.CREATE_COMMENT, Action.VIEW_COMMENTS, Action.EDIT_COMMENT, Action.DELETE_COMMENT, Action.CREATE_POLL, Action.VIEW_POLLS, Action.VOTE_ON_POLLS, Action.VIEW_THREADS, Action.CREATE_THREAD]],
-  [Action.CREATE_THREAD, [Action.CREATE_REACTION, Action.VIEW_REACTIONS, Action.CREATE_COMMENT, Action.VIEW_COMMENTS, Action.EDIT_COMMENT, Action.DELETE_COMMENT, Action.CREATE_POLL, Action.VIEW_POLLS, Action.VOTE_ON_POLLS, Action.VIEW_THREADS]],
-  [Action.VIEW_THREADS, [Action.VIEW_REACTIONS, Action.VIEW_COMMENTS, Action.VIEW_POLLS]],
-  [Action.VOTE_ON_POLLS, [Action.CREATE_REACTION, Action.VIEW_REACTIONS, Action.CREATE_COMMENT, Action.VIEW_COMMENTS, Action.EDIT_COMMENT, Action.DELETE_COMMENT, Action.CREATE_POLL, Action.VIEW_POLLS]],
-  [Action.CREATE_POLL, [Action.CREATE_REACTION, Action.VIEW_REACTIONS, Action.CREATE_COMMENT, Action.VIEW_COMMENTS, Action.EDIT_COMMENT, Action.DELETE_COMMENT, Action.VIEW_POLLS, Action.VIEW_THREADS]],
-  [Action.VIEW_POLLS, [Action.VIEW_REACTIONS, Action.VIEW_COMMENTS, Action.VIEW_THREADS]],
-  [Action.DELETE_COMMENT, [Action.CREATE_REACTION, Action.VIEW_REACTIONS, Action.CREATE_COMMENT, Action.VIEW_COMMENTS, Action.EDIT_COMMENT, Action.VIEW_POLLS, Action.VIEW_THREADS]],
-  [Action.EDIT_COMMENT, [Action.CREATE_REACTION, Action.VIEW_REACTIONS,  Action.VIEW_COMMENTS, Action.CREATE_COMMENT, Action.DELETE_COMMENT, Action.VIEW_POLLS, Action.VIEW_THREADS]],
-  [Action.CREATE_COMMENT, [Action.CREATE_REACTION, Action.VIEW_REACTIONS, Action.VIEW_COMMENTS, Action.EDIT_COMMENT, Action.DELETE_COMMENT, Action.VIEW_POLLS, Action.VIEW_THREADS]],
-  [Action.VIEW_COMMENTS, [Action.VIEW_REACTIONS, Action.VIEW_POLLS, Action.VIEW_THREADS]],
-  [Action.CREATE_REACTION, [Action.VIEW_REACTIONS, Action.VIEW_COMMENTS, Action.VIEW_POLLS, Action.VIEW_THREADS]],
+  [Action.CREATE_CHAT, [Action.VIEW_CHAT_CHANNELS]],
   [Action.VIEW_REACTIONS, [Action.VIEW_COMMENTS, Action.VIEW_POLLS, Action.VIEW_THREADS]],
+  [Action.CREATE_REACTION, [Action.VIEW_REACTIONS, Action.VIEW_COMMENTS, Action.VIEW_POLLS, Action.VIEW_THREADS]],
+  [Action.VIEW_COMMENTS, [Action.VIEW_REACTIONS, Action.VIEW_POLLS, Action.VIEW_THREADS]],
+  [Action.CREATE_COMMENT, [Action.CREATE_REACTION, Action.VIEW_REACTIONS, Action.VIEW_COMMENTS, Action.EDIT_COMMENT, Action.DELETE_COMMENT, Action.VIEW_POLLS, Action.VIEW_THREADS]],
+  [Action.EDIT_COMMENT, [Action.CREATE_REACTION, Action.VIEW_REACTIONS, Action.VIEW_COMMENTS, Action.CREATE_COMMENT, Action.DELETE_COMMENT, Action.VIEW_POLLS, Action.VIEW_THREADS]],
+  [Action.DELETE_COMMENT, [Action.CREATE_REACTION, Action.VIEW_REACTIONS, Action.CREATE_COMMENT, Action.VIEW_COMMENTS, Action.EDIT_COMMENT, Action.VIEW_POLLS, Action.VIEW_THREADS]],
+  [Action.VIEW_POLLS, [Action.VIEW_REACTIONS, Action.VIEW_COMMENTS, Action.VIEW_THREADS]],
+  [Action.CREATE_POLL, [Action.CREATE_REACTION, Action.VIEW_REACTIONS, Action.CREATE_COMMENT, Action.VIEW_COMMENTS, Action.EDIT_COMMENT, Action.DELETE_COMMENT, Action.VIEW_POLLS, Action.VIEW_THREADS]],
+  [Action.VOTE_ON_POLLS, [Action.CREATE_REACTION, Action.VIEW_REACTIONS, Action.CREATE_COMMENT, Action.VIEW_COMMENTS, Action.EDIT_COMMENT, Action.DELETE_COMMENT, Action.CREATE_POLL, Action.VIEW_POLLS]],
+  [Action.VIEW_THREADS, [Action.VIEW_REACTIONS, Action.VIEW_COMMENTS, Action.VIEW_POLLS]],
+  [Action.CREATE_THREAD, [Action.CREATE_REACTION, Action.VIEW_REACTIONS, Action.CREATE_COMMENT, Action.VIEW_COMMENTS, Action.EDIT_COMMENT, Action.DELETE_COMMENT, Action.CREATE_POLL, Action.VIEW_POLLS, Action.VOTE_ON_POLLS, Action.VIEW_THREADS, Action.EDIT_THREAD, Action.DELETE_THREAD]],
+  [Action.EDIT_THREAD, [Action.CREATE_REACTION, Action.VIEW_REACTIONS, Action.CREATE_COMMENT, Action.VIEW_COMMENTS, Action.EDIT_COMMENT, Action.DELETE_COMMENT, Action.CREATE_POLL, Action.VIEW_POLLS, Action.VOTE_ON_POLLS, Action.VIEW_THREADS, Action.CREATE_THREAD, Action.DELETE_THREAD]],
+  [Action.DELETE_THREAD, [Action.CREATE_REACTION, Action.VIEW_REACTIONS, Action.CREATE_COMMENT, Action.VIEW_COMMENTS, Action.EDIT_COMMENT, Action.DELETE_COMMENT, Action.CREATE_POLL, Action.VIEW_POLLS, Action.VOTE_ON_POLLS, Action.VIEW_THREADS, Action.CREATE_THREAD, Action.EDIT_THREAD]],
 ]);
 
 export function isPermitted(permission: Permissions, action: number): boolean {
@@ -70,7 +71,9 @@ export function isPermitted(permission: Permissions, action: number): boolean {
   return hasAction;
 }
 
-export function computeImplicitPermissions(permission: Permissions): Permissions {
+export function computeImplicitPermissions(
+  permission: Permissions
+): Permissions {
   let result = BigInt(permission);
   // Find the highest hierachy action that is permitted
   for (const [action, implicitActions] of IMPLICIT_PERMISSIONS_BY_ACTION) {
@@ -79,7 +82,6 @@ export function computeImplicitPermissions(permission: Permissions): Permissions
       for (const implicitAction of implicitActions) {
         result = addPermission(result, implicitAction);
       }
-      return result;
     }
   }
   return result;
@@ -94,7 +96,7 @@ export function computePermissions(
     permission &= ~BigInt(assignment.deny);
     permission |= BigInt(assignment.allow);
   }
-  // Finally, compute implicit permissions
+  // Finally, compute implicit permissions which will overwrite denials that overlap with implicit permissions
   permission = computeImplicitPermissions(permission);
   return permission;
 }
--- packages/common-common/test/unit/computeImplicitPermissions.spec.ts
+++ packages/common-common/test/unit/computeImplicitPermissions.spec.ts
@@ -5,9 +5,10 @@ import {
   Action,
   addPermission,
   isPermitted,
+  computePermissions,
 } from 'common-common/src/permissions';
 
-describe('computePermissions() unit tests', () => {
+describe('computeImplicitPermissions() unit tests', () => {
   let base_permission;
   let overwrite_admin;
   let overwrite_moderator;
@@ -21,12 +22,73 @@ describe('computePermissions() unit tests', () => {
     chain_permission = { allow: base_permission, deny: base_permission };
   });
 
-  it('should correctly computePermissions for allowing createThread action with two roles overwrites', () => {
-    const actionPermission = addPermission(base_permission, Action.VIEW_THREADS);
+  it('should correctly computeImplicitPermissions for an action', () => {
+    const actionPermission = addPermission(
+      base_permission,
+      Action.VIEW_THREADS
+    );
 
     const permission = computeImplicitPermissions(actionPermission);
+    assert.isTrue(isPermitted(permission, Action.VIEW_COMMENTS));
+    assert.isTrue(isPermitted(permission, Action.VIEW_THREADS));
+    assert.isTrue(isPermitted(permission, Action.VIEW_REACTIONS));
+    assert.isTrue(isPermitted(permission, Action.VIEW_POLLS));
+    assert.isFalse(isPermitted(permission, Action.CREATE_THREAD));
+  });
+
+  it('should correctly computePermissions for an action and its implicit permissions', () => {
     // eslint-disable-next-line no-bitwise
+    overwrite_admin.allow = BigInt(1) << BigInt(Action.CREATE_THREAD);
+
+    // eslint-disable-next-line no-bitwise
+    overwrite_moderator.deny = BigInt(1) << BigInt(Action.CREATE_THREAD);
+
+    const permission = computePermissions(base_permission, [
+      overwrite_moderator,
+      overwrite_admin,
+    ]);
+    assert.isTrue(isPermitted(permission, Action.CREATE_THREAD));
+    assert.isTrue(isPermitted(permission, Action.CREATE_COMMENT));
+    assert.isTrue(isPermitted(permission, Action.CREATE_POLL));
+    assert.isTrue(isPermitted(permission, Action.CREATE_REACTION));
+    assert.isTrue(isPermitted(permission, Action.VIEW_COMMENTS));
+    assert.isTrue(isPermitted(permission, Action.VIEW_THREADS));
+    assert.isTrue(isPermitted(permission, Action.VIEW_REACTIONS));
+    assert.isTrue(isPermitted(permission, Action.VIEW_POLLS));
+    assert.isTrue(isPermitted(permission, Action.DELETE_THREAD));
+    assert.isFalse(isPermitted(permission, Action.CREATE_CHAT));
+  });
+
+
+  it('should correctly computePermissions for an action and its implicit permissions with one denial that is overwritten', () => {
+    // If moderator allows viewing comments, but admin denies threads. Then view threads should be allowed because viewing comments implies viewing threads.
+
+    // eslint-disable-next-line no-bitwise
+    overwrite_admin.deny = BigInt(1) << BigInt(Action.VIEW_THREADS);
+
+    // eslint-disable-next-line no-bitwise
+    overwrite_moderator.allow = BigInt(1) << BigInt(Action.VIEW_COMMENTS);
+
+    const permission = computePermissions(base_permission, [
+      overwrite_moderator,
+      overwrite_admin,
+    ]);
+    assert.isTrue(isPermitted(permission, Action.VIEW_THREADS));
     assert.isTrue(isPermitted(permission, Action.VIEW_COMMENTS));
   });
 
+  it('should correctly computePermissions for an action and its implicit permissions with one denial', () => {
+    // eslint-disable-next-line no-bitwise
+    overwrite_admin.deny = BigInt(1) << BigInt(Action.VIEW_COMMENTS);
+
+    // eslint-disable-next-line no-bitwise
+    overwrite_moderator.allow = BigInt(1) << BigInt(Action.VIEW_COMMENTS);
+
+    const permission = computePermissions(base_permission, [
+      overwrite_moderator,
+      overwrite_admin,
+    ]);
+    assert.isFalse(isPermitted(permission, Action.VIEW_THREADS));
+    assert.isFalse(isPermitted(permission, Action.VIEW_COMMENTS));
+  });
 });
