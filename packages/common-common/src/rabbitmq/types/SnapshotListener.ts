308a7560295761c8965b0b9a9e6dead7ca3b6f5f
Richard Melkonian <r.v.melkonian@gmail.com>
2022-11-29 14:12:16 +0000

    fix type error

--- packages/common-common/src/rabbitmq/types/SnapshotListener.ts
+++ packages/common-common/src/rabbitmq/types/SnapshotListener.ts
@@ -15,7 +15,18 @@ export const RmqSnapshotEvent: RmqMsgNamespace<SnapshotEvent> = {
       && data.space === 'string'
       && typeof data.expire === 'number'
     );
+  },
+
+/**
+   * This function combines the isValidMsgFormat and getInvalidFormatError functions. Essentially this function will
+   * check the given data format and throw the RmqMsgFormatError if the format is invalid.
+   * @param data The raw message from RabbitMQ
+   */
+  checkMsgFormat(data: any): void {
+    const valid = this.isValidMsgFormat(data);
+    if (!valid) throw this.getInvalidFormatError(data);
   }
+
 }
 
 // merged with class above
