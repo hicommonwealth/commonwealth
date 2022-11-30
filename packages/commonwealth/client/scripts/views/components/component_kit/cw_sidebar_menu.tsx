7ab013b054fc7e6c119f2ea366eff99cd58673ee
Graham Johnson <graham@commonwealth.im>
2022-11-29 23:28:02 -0500

    Hook up new SidebarMenu system (#2263)
    * Hook up new SidebarMenu system
    
    * Make sidebar heads sticky to top of section :sunglasses:
    
    * Fix Vnodes and close sidebar menu on click
    
    * Float sidebarmenu on top of default menu, add sidebarmenu close to navigateToSubpage
    
    * Increase animation speed to 200ms
    
    * Keep sidebar closed after clicking
    
    Co-authored-by: zakhap <zhs2104@columbia.edu>
    Co-authored-by: Dillon Chen <5334557+dillchen@users.noreply.github.com>

---
+++ packages/commonwealth/client/scripts/views/components/component_kit/cw_sidebar_menu.tsx
@@ -0,0 +1,183 @@
+/* @jsx m */
+
+import m from 'mithril';
+
+import 'components/component_kit/cw_sidebar_menu.scss';
+
+import app from 'state';
+import { navigateToSubpage } from 'app';
+import { ListItem, Icon, Icons } from 'construct-ui';
+import { AddressInfo, ChainInfo, RoleInfo } from 'models';
+import { getClasses } from './helpers';
+import { CWText } from './cw_text';
+import { CWIcon } from './cw_icons/cw_icon';
+import { ComponentType, MenuItem } from './types';
+import { CommunityLabel } from '../community_label';
+import User from '../widgets/user';
+
+// TODO: Switch to new component kit system, migrate to more native setup
+const renderCommunity = (item: ChainInfo) => {
+  const roles: RoleInfo[] = [];
+  roles.push(...app.roles.getAllRolesInCommunity({ chain: item.id }));
+
+  return (
+    <ListItem
+      class={app.communities.isStarred(item.id) ? 'starred' : ''}
+      label={<CommunityLabel community={item} />}
+      selected={app.activeChainId() === item.id}
+      onclick={(e) => {
+        e.preventDefault();
+        e.stopPropagation();
+        app.sidebarMenu = 'default';
+        m.route.set(item.id ? `/${item.id}` : '/');
+      }}
+      contentRight={
+        app.isLoggedIn() &&
+        roles.length > 0 && (
+          <div
+            class="community-star-toggle"
+            onclick={async (e) => {
+              e.preventDefault();
+              e.stopPropagation();
+              await app.communities.setStarred(item.id);
+              m.redraw();
+            }}
+          >
+            {roles.map((role) => {
+              // TODO: sometimes address_chain is null here -- why??
+              return m(User, {
+                avatarSize: 18,
+                avatarOnly: true,
+                user: new AddressInfo(
+                  role.address_id,
+                  role.address,
+                  role.address_chain || role.chain_id,
+                  null
+                ),
+              });
+            })}
+            <div class="star-icon">
+              <Icon name={Icons.STAR} key={item.id} />
+            </div>
+          </div>
+        )
+      }
+    />
+  );
+};
+
+class CWSidebarMenuItem implements m.ClassComponent<MenuItem> {
+  view(vnode: m.Vnode<MenuItem>) {
+    if (vnode.attrs.type === 'default') {
+      const { disabled, iconLeft, iconRight, isSecondary, label, onclick } =
+        vnode.attrs;
+
+      return (
+        <div
+          class={getClasses<{ disabled?: boolean; isSecondary?: boolean }>(
+            { disabled, isSecondary },
+            'SidebarMenuItem default'
+          )}
+          onclick={(e) => {
+            if (onclick) onclick(e);
+          }}
+        >
+          <div class="sidebar-menu-item-left">
+            {iconLeft && <CWIcon iconName={iconLeft} />}
+            <CWText type="b2">{label}</CWText>
+          </div>
+          {iconRight && <CWIcon iconName={iconRight} iconSize="small" />}
+        </div>
+      );
+    } else if (vnode.attrs.type === 'header') {
+      return (
+        <div class="SidebarMenuItem header">
+          <CWText type="caption">{vnode.attrs.label}</CWText>
+        </div>
+      );
+    } else if (vnode.attrs.type === 'community') {
+      return (
+        <div class="SidebarMenuItem community">
+          {renderCommunity(vnode.attrs.community)}
+        </div>
+      );
+    }
+  }
+}
+
+type SidebarMenuAttrs = {
+  className?: string;
+  menuHeader?: { label: string; onclick: (e) => void };
+  menuItems: Array<MenuItem>;
+};
+
+export class CWSidebarMenu implements m.ClassComponent<SidebarMenuAttrs> {
+  view(vnode: m.Vnode<SidebarMenuAttrs>) {
+    const { className, menuHeader, menuItems } = vnode.attrs;
+
+    return (
+      <div
+        class={getClasses<{ className: string }>(
+          { className },
+          ComponentType.SidebarMenu
+        )}
+      >
+        <div class="sidebar-top">
+          {menuHeader && (
+            <div class="sidebar-menu-header" onclick={menuHeader.onclick}>
+              <CWIcon iconName="chevronLeft" />
+              <CWText type="h5" fontWeight="medium">
+                {menuHeader.label}
+              </CWText>
+            </div>
+          )}
+          {menuItems.map((item) => (
+            <CWSidebarMenuItem type={item.type || 'default'} {...item} />
+          ))}
+        </div>
+        <div class="sidebar-bottom">
+          {[
+            {
+              type: 'header',
+              label: 'Other',
+            },
+            {
+              type: 'default',
+              label: 'Explore communities',
+              iconLeft: 'compass',
+              onclick: () => {
+                app.sidebarMenu = 'default';
+                m.route.set('/communities');
+              },
+            },
+            {
+              type: 'default',
+              label: 'Notification settings',
+              iconLeft: 'person',
+              onclick: () => {
+                app.sidebarMenu = 'default';
+                m.route.set('/notification-settings');
+              },
+            },
+            {
+              type: 'default',
+              label: 'Account settings',
+              iconLeft: 'bell',
+              onclick: () => {
+                  if (app.activeChainId()) { navigateToSubpage('/settings'); }
+                  else {
+                    app.sidebarMenu = 'default';
+                    m.route.set('/settings');
+                  }
+                },
+            } as MenuItem,
+          ].map((item: MenuItem) => {
+            return (
+              <CWSidebarMenuItem type={item.type || 'default'} {...item} />
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
+}
--- packages/commonwealth/client/scripts/views/menus/create_content_menu.tsx
+++ packages/commonwealth/client/scripts/views/menus/create_content_menu.tsx
@@ -5,13 +5,16 @@ import m from 'mithril';
 import app from 'state';
 import { navigateToSubpage } from 'app';
 import { ProposalType, ChainBase, ChainNetwork } from 'common-common/src/types';
+import { mixpanelBrowserTrack } from 'helpers/mixpanel_browser_util';
 import { SubstrateAccount } from 'controllers/chain/substrate/account';
+import { MixpanelCommunityCreationEvent } from 'analytics/types';
 import { CWMobileMenu } from '../components/component_kit/cw_mobile_menu';
 import { CWIconButton } from '../components/component_kit/cw_icon_button';
 import { CWPopoverMenu } from '../components/component_kit/cw_popover/cw_popover_menu';
 import { MenuItem } from '../components/component_kit/types';
+import { CWSidebarMenu } from '../components/component_kit/cw_sidebar_menu';
 
-const getCreateContentMenuItems = (): Array<MenuItem> => {
+const getCreateContentMenuItems = (): MenuItem[] => {
   const activeAccount = app.user.activeAccount;
 
   const showSnapshotOptions =
@@ -25,7 +28,7 @@ const getCreateContentMenuItems = (): Array<MenuItem> => {
     )
     .sort((a, b) => a.name.localeCompare(b.name));
 
-  const showSputnikProposalItem = app.chain.network === ChainNetwork.Sputnik;
+  const showSputnikProposalItem = app.chain?.network === ChainNetwork.Sputnik;
 
   const showOnChainProposalItem =
     (app.chain?.base === ChainBase.CosmosSDK &&
@@ -39,7 +42,7 @@ const getCreateContentMenuItems = (): Array<MenuItem> => {
     app.chain?.base === ChainBase.Substrate &&
     app.chain?.network !== ChainNetwork.Plasm;
 
-  const getTopicThreads = (): Array<MenuItem> =>
+  const getTopicTemplateItems = (): MenuItem[] =>
     topics.map((t) => ({
       label: `New ${t.name} Thread`,
       iconLeft: 'write',
@@ -60,7 +63,7 @@ const getCreateContentMenuItems = (): Array<MenuItem> => {
       },
     }));
 
-  const getOnChainProposalItem = (): Array<MenuItem> =>
+  const getOnChainProposalItem = (): MenuItem[] =>
     showOnChainProposalItem
       ? [
           {
@@ -71,7 +74,7 @@ const getCreateContentMenuItems = (): Array<MenuItem> => {
         ]
       : [];
 
-  const getSputnikProposalItem = (): Array<MenuItem> =>
+  const getSputnikProposalItem = (): MenuItem[] =>
     showSputnikProposalItem
       ? [
           {
@@ -82,7 +85,7 @@ const getCreateContentMenuItems = (): Array<MenuItem> => {
         ]
       : [];
 
-  const getSubstrateProposalItems = (): Array<MenuItem> =>
+  const getSubstrateProposalItems = (): MenuItem[] =>
     showSubstrateProposalItems
       ? [
           {
@@ -112,7 +115,7 @@ const getCreateContentMenuItems = (): Array<MenuItem> => {
                   iconLeft: 'councilProposal',
                 },
               ]
-            : []) as Array<MenuItem>),
+            : []) as MenuItem[]),
           {
             label: 'New bounty proposal',
             onclick: () =>
@@ -132,7 +135,7 @@ const getCreateContentMenuItems = (): Array<MenuItem> => {
         ]
       : [];
 
-  const getSnapshotProposalItem = (): Array<MenuItem> =>
+  const getSnapshotProposalItem = (): MenuItem[] =>
     showSnapshotOptions
       ? [
           {
@@ -152,26 +155,87 @@ const getCreateContentMenuItems = (): Array<MenuItem> => {
         ]
       : [];
 
-  return [
+  const getUniversalCreateItems = (): MenuItem[] => [
+    // {
+    //   label: 'New Crowdfund',
+    //   iconLeft: 'wallet',
+    //   onclick: () => {
+
+    //   }
+    // },
     {
-      label: 'New Thread',
-      onclick: () => {
-        navigateToSubpage('/new/discussion');
+      label: 'New Community',
+      iconLeft: 'people',
+      onclick: (e) => {
+        e.preventDefault();
+        mixpanelBrowserTrack({
+          event: MixpanelCommunityCreationEvent.CREATE_BUTTON_PRESSED,
+          chainBase: null,
+          isCustomDomain: app.isCustomDomain(),
+          communityType: null,
+        });
+        app.sidebarMenu = 'default';
+        m.route.set('/createCommunity');
       },
-      iconLeft: 'write',
     },
-    ...getTopicThreads(),
-    ...getOnChainProposalItem(),
-    ...getSputnikProposalItem(),
-    ...getSubstrateProposalItems(),
-    ...getSnapshotProposalItem(),
+  ];
+
+  return [
+    ...(app.activeChainId()
+      ? [
+          {
+            type: 'header',
+            label: 'Create Within Community',
+          } as MenuItem,
+          {
+            label: 'New Thread',
+            onclick: () => {
+              navigateToSubpage('/new/discussion');
+            },
+            iconLeft: 'write',
+          } as MenuItem,
+          ...getTopicTemplateItems(),
+          ...getOnChainProposalItem(),
+          ...getSputnikProposalItem(),
+          ...getSubstrateProposalItems(),
+          ...getSnapshotProposalItem(),
+        ]
+      : []),
+    {
+      type: 'header',
+      label: 'Universal Create',
+    },
+    ...getUniversalCreateItems(),
   ];
 };
+
+export class CreateContentSidebar implements m.ClassComponent {
+  view(vnode: m.Vnode) {
+    return (
+      <CWSidebarMenu
+        className="CreateContentSidebar"
+        menuHeader={{
+          label: 'Create',
+          onclick: async () => {
+            const sidebar = document.getElementsByClassName('CreateContentSidebar');
+            sidebar[0].classList.add('onremove')
+            setTimeout(() => {
+              app.sidebarMenu = 'default';
+              m.redraw();
+            }, 200);
+          },
+        }}
+        menuItems={getCreateContentMenuItems()}
+      />
+    );
+  }
+}
 
 export class CreateContentMenu implements m.ClassComponent {
   view() {
     return (
       <CWMobileMenu
+        className="CreateContentMenu"
         menuHeader={{
           label: 'Create',
           onclick: () => {
