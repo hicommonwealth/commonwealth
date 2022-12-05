f6739218516fffcaad6efc47a6483190f0f93187
Timothee Legros <timothee@ualberta.ca>
2022-12-04 19:53:43 -0700

    db dump fixes

--- packages/commonwealth/limited_load.sql
+++ packages/commonwealth/limited_load.sql
@@ -1,4 +1,3 @@
-\COPY "ChainEvents" FROM 'ChainEvents.dat' WITH BINARY;
 \COPY "Notifications" FROM 'Notifications.dat' WITH BINARY;
 \COPY "Subscriptions" FROM 'Subscriptions.dat' WITH BINARY;
 \COPY "NotificationsRead" FROM 'NotificationsRead.dat' WITH BINARY;
