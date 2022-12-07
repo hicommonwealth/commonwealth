6d5dc575c34d575d8972514474049eb01720036a
Richard Melkonian <r.v.melkonian@gmail.com>
2022-12-07 10:50:46 -0500

    fix typo

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,7 @@
 if [ "$CE_BUILD" = true ]; then
   yarn --cwd packages/chain-events build-services
 elif [ "$SL_BUILD" = true ]; then
-  yarn --cwd packages/snapshpt-listner build && yarn --cwd start 
+  yarn --cwd packages/snapsot-listener build && yarn --cwd start 
 elif [ "$CW_BUILD" = true ]; then
   yarn --cwd packages/commonwealth build
 else
