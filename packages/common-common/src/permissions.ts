db7a6ea72aef6323c5e76c6281afc8907b35b067
Daniel Trinh <danielhtrinh1@gmail.com>
2022-12-07 12:53:31 -0400

    Adding Base Permissions Defaults

--- packages/common-common/src/permissions.ts
+++ packages/common-common/src/permissions.ts
@@ -377,7 +377,22 @@ export function removePermission(
 
 // Default Permissions
 export const BASE_PERMISSIONS: Permissions =
+  addPermission(BigInt(0), Action.VIEW_REACTIONS) |
+  addPermission(BigInt(0), Action.CREATE_REACTION) |
+  addPermission(BigInt(0), Action.DELETE_REACTION) |
+  addPermission(BigInt(0), Action.VIEW_COMMENTS) |
+  addPermission(BigInt(0), Action.CREATE_COMMENT) |
+  addPermission(BigInt(0), Action.EDIT_COMMENT) |
+  addPermission(BigInt(0), Action.DELETE_COMMENT) |
+  addPermission(BigInt(0), Action.VIEW_POLLS) |
+  addPermission(BigInt(0), Action.CREATE_POLL) |
+  addPermission(BigInt(0), Action.VOTE_ON_POLLS) |
+  addPermission(BigInt(0), Action.VIEW_TOPICS) |
   addPermission(BigInt(0), Action.CREATE_THREAD) |
+  addPermission(BigInt(0), Action.EDIT_THREAD) |
+  addPermission(BigInt(0), Action.DELETE_THREAD) |
+  addPermission(BigInt(0), Action.LINK_THREAD_TO_THREAD) |
+  addPermission(BigInt(0), Action.LINK_PROPOSAL_TO_THREAD) |
   addPermission(BigInt(0), Action.VIEW_CHAT_CHANNELS) |
   addPermission(BigInt(0), Action.VIEW_THREADS);
 
