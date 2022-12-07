8376be8b347f7b6a5c5ba4ccfd353037bda89bbe
Richard Melkonian <r.v.melkonian@gmail.com>
2022-12-07 09:57:17 -0500

    update build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,9 @@
 if [ "$CE_BUILD" = true ]; then
   yarn --cwd packages/chain-events build-services
+elif [ "$SL_BUILD" = true ]; then
+  yarn --cwd packages/snapshpt-listner build && yarn --cwd start 
 elif [ "$CW_BUILD" = true ]; then
   yarn --cwd packages/commonwealth build
 else
-  yarn --cwd packages/chain-events build-services && yarn --cwd packages/commonwealth build
+  yarn --cwd packages/chain-events build-services && yarn --cwd packages/commonwealth build 
 fi;
