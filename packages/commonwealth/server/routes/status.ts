9638e3727d39e5b82dc00c5fee25760570c53163
Timothee Legros <timothee@ualberta.ca>
2022-12-09 07:14:41 -0700

    type fix

--- packages/commonwealth/server/routes/status.ts
+++ packages/commonwealth/server/routes/status.ts
@@ -29,7 +29,7 @@ import { JWT_SECRET } from '../config';
 import { DB } from '../models';
 import { sequelize } from '../database';
 import { ServerError } from 'common-common/src/errors';
-import { findAllRoles } from '../util/roles';
+import {findAllRoles, RoleInstanceWithPermission} from '../util/roles';
 
 const log = factory.getLogger(formatFilename(__filename));
 
