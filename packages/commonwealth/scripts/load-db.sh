d471329b1db18b71336672bc0e3521da258b1cbf
Timothee Legros <timothee@ualberta.ca>
2022-12-09 08:45:58 -0700

    load-db

--- packages/commonwealth/scripts/load-db.sh
+++ packages/commonwealth/scripts/load-db.sh
@@ -1,9 +1,19 @@
+#!/bin/bash
+
+ENV_FILE_PATH="$(cd "$(dirname ".env")"; pwd)/$(basename ".env")"
+echo "Grepping: $ENV_FILE_PATH"
+
 # shellcheck disable=SC2046
 if [[ $OSTYPE == 'darwin'* ]]; then
-  export $(grep -v '^#' ../.env | xargs)
+  export $(grep -v '^#' "$ENV_FILE_PATH" | xargs)
   else
-    export $(grep -v '^#' ../.env | xargs -d '\n' -e)
+    # shellcheck disable=SC2086
+    export $(grep -v '^#' $ENV_FILE_PATH | xargs -d '\n' -e)
 fi
 
 psql -d commonwealth -U commonwealth -W -f latest.dump
-psql -d commonwealth -U commonwealth -c "UPDATE \"ChainNodes\" SET url = 'wss://eth-mainnet.g.alchemy.com/v2/pZsX6R3wGdnwhUJHlVmKg4QqsiS32Qm4', alt_wallet_url = 'https://eth-mainnet.g.alchemy.com/v2/pZsX6R3wGdnwhUJHlVmKg4QqsiS32Qm4' WHERE url = 'wss://eth-mainnet.alchemyapi.io/v2/BCNLWCaGqaXwCDHlZymPy3HpjXSxK7j_' OR url = 'wss://eth-mainnet.alchemyapi.io/v2/cNC4XfxR7biwO2bfIO5aKcs9EMPxTQfr' OR url = 'wss://eth-mainnet.g.alchemy.com/v2/CAE5iDO8-QLOFwVRZU8PUtYJvqokWfws'"
+if [ "$ETH_ALCHEMY_URL" ] && [ "$ETH_ALCHEMY_ALT_WALLET_URL" ]; then
+  psql -d commonwealth -U commonwealth -c "UPDATE \"ChainNodes\" SET url = '$ETH_ALCHEMY_URL', alt_wallet_url = '$ETH_ALCHEMY_ALT_WALLET_URL' WHERE url = 'wss://eth-mainnet.alchemyapi.io/v2/BCNLWCaGqaXwCDHlZymPy3HpjXSxK7j_' OR url = 'wss://eth-mainnet.alchemyapi.io/v2/cNC4XfxR7biwO2bfIO5aKcs9EMPxTQfr' OR url = 'wss://eth-mainnet.g.alchemy.com/v2/CAE5iDO8-QLOFwVRZU8PUtYJvqokWfws'"
+else
+  echo "You don't have ETH_ALCHEMY_URL and ETH_ALCHEMY_ALT_WALLET_URL env var set so the Alchemy urls were not updated"
+fi
