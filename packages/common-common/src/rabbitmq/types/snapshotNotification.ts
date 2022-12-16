8fcaf2e3619fe1bc83992ac4c079bb124217248c
Richard Melkonian <r.v.melkonian@gmail.com>
2022-12-16 14:38:57 +0000

    add missing types, test snapshot-listener

---
+++ packages/common-common/src/rabbitmq/types/snapshotNotification.ts
@@ -0,0 +1,59 @@
+import { RmqMsgFormatError, RmqMsgNamespace } from "common-common/src/rabbitmq";
+
+export interface SnapshotNotification {
+  id?: string;
+  title?: string;
+  body?: string;
+  choices?: string[];
+  space?: string;
+  event?: string;
+  start?: string;
+  expire?: string;
+}
+
+export const RmqSnapshotNotification: RmqMsgNamespace<SnapshotNotification> = {
+  getInvalidFormatError(notif: any): RmqMsgFormatError {
+    return new RmqMsgFormatError(
+      `The following Snapshot Notification is improperly formatted: ${JSON.stringify(
+        notif
+      )}`
+    );
+  },
+
+  isValidMsgFormat(data: any): data is SnapshotNotification {
+    return !!(
+      data.id &&
+      typeof data.id === "string" &&
+      data.title &&
+      typeof data.title === "string" &&
+      data.body &&
+      typeof data.id === "string" &&
+      data.choices &&
+      Array.isArray(data.choices) &&
+      data.space &&
+      typeof data.id === "string" &&
+      data.event &&
+      typeof data.id === "string" &&
+      data.start &&
+      typeof data.id === "string" &&
+      data.expire &&
+      typeof data.id === "string"
+    );
+  },
+
+  checkMsgFormat(data: any): void {
+    const valid = this.isValidMsgFormat(data);
+    if (!valid) {
+      console.log(
+        `The following Snapshot Notification is improperly formatted: ${JSON.stringify(
+          data
+        )}`
+      );
+      throw this.getInvalidFormatError(data);
+    }
+  },
+};
+
+export namespace RmqSnapshotNotification {
+  export type RmqMsgType = SnapshotNotification;
+}
---
+++ packages/snapshot-listener/src/types/index.ts
@@ -0,0 +1,37 @@
+export abstract class EventHandler<DBEventType = SnapshotEvent> {
+  public abstract handle(
+    event: SnapshotEvent,
+    dbEvent?: DBEventType
+  ): Promise<DBEventType>;
+}
+
+export class ServerError extends Error {
+  status: number;
+  error: any;
+  constructor(message: string, error?: any) {
+    super(message);
+    this.status = 500;
+    this.name = "ServerError";
+    this.error = error;
+  }
+}
+
+export class AppError extends Error {
+  status: number;
+  constructor(message: string) {
+    super(message);
+    this.status = 400;
+    this.name = "AppError";
+  }
+}
+
+export interface SnapshotEvent {
+  id?: string;
+  title?: string;
+  body?: string;
+  choices?: string[];
+  space: string;
+  event: string;
+  start?: string;
+  expire: string;
+}
