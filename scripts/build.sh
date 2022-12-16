0b26f0e383ded6981a60ffec49769de78d2df7e7
Richard Melkonian <r.v.melkonian@gmail.com>
2022-12-16 19:36:35 +0000

    fix bug in build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,9 +1,9 @@
 if [ "$CE_BUILD" = true ]; then
   yarn --cwd packages/chain-events build-services
-elif [ "$SL_BUILD" = true ]; then
-  yarn --cwd packages/snapshot-listener build  
 elif [ "$CW_BUILD" = true ]; then
   yarn --cwd packages/commonwealth build
 elif [ "$SL_BUILD" = true ]; then
   yarn --cwd packages/snapshot-listener build
 else
+  yarn --cwd packages/chain-events build-services && yarn --cwd packages/commonwealth build
+fi
