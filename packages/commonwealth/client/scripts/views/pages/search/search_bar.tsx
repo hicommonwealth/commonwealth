0aea6e9e5346a856ebb5dd62f1b6ffd073514fba
Gabriel Duquette <gabrielduquette@gmail.com>
2022-12-19 18:57:36 +0000

    move stuff out of helpers and delete

--- packages/commonwealth/client/scripts/views/pages/search/search_bar.tsx
+++ packages/commonwealth/client/scripts/views/pages/search/search_bar.tsx
@@ -2,14 +2,19 @@
 
 import m from 'mithril';
 import ClassComponent from 'class_component';
+import moment from 'moment';
 
 import 'pages/search/search_bar.scss';
 
 import app from 'state';
-import { SearchQuery } from 'models';
-import { getSearchPreview } from './helpers';
+import { notifyError } from 'controllers/app/notifications';
+import { AddressInfo, SearchQuery } from 'models';
+import { SearchScope } from 'models/SearchQuery';
 import { CWIconButton } from '../../components/component_kit/cw_icon_button';
 import { CWText } from '../../components/component_kit/cw_text';
+import { CommunityLabel } from '../../components/community_label';
+import { renderQuillTextBody } from '../../components/quill/helpers';
+import User from '../../components/widgets/user';
 import { getClasses } from '../../components/component_kit/helpers';
 
 type SearchChipAttrs = {
@@ -40,6 +45,131 @@ export class SearchChip extends ClassComponent<SearchChipAttrs> {
   }
 }
 
+const getResultsPreview = (searchQuery: SearchQuery) => {
+  const types = searchQuery.getSearchScope();
+
+  const results = Object.fromEntries(
+    Object.entries(app.search.getByQuery(searchQuery).results).map(([k, v]) => [
+      k,
+      v.slice(0, 2),
+    ])
+  );
+
+  const organizedResults = [];
+
+  types.forEach((type: SearchScope) => {
+    const result = results[type];
+
+    result.forEach((item) => {
+      const resultRow =
+        item.searchType === SearchScope.Threads ? (
+          <div
+            onclick={() =>
+              m.route.set(`/${item.chain}/discussion/${item.proposalid}`)
+            }
+          >
+            {decodeURIComponent(item.title)}
+            <CWText>{moment(item.created_at).fromNow()}</CWText>
+            {m(User, {
+              user: new AddressInfo(
+                item.address_id,
+                item.address,
+                item.address_chain,
+                null
+              ),
+            })}
+            {renderQuillTextBody(item.body, {
+              hideFormatting: true,
+              collapse: true,
+              searchTerm: searchQuery.searchTerm,
+            })}
+          </div>
+        ) : item.searchType === SearchScope.Members ? (
+          m(User, {
+            user: app.profiles.getProfile(item.chain, item.address),
+            linkify: true,
+          })
+        ) : item.searchType === SearchScope.Communities ? (
+          <div
+            onclick={() => {
+              m.route.set(
+                item.address
+                  ? `/${item.address}`
+                  : item.id
+                  ? `/${item.id}`
+                  : '/'
+              );
+            }}
+          >
+            <CommunityLabel community={item} />
+          </div>
+        ) : item.searchType === SearchScope.Replies ? (
+          <div
+            onclick={() => {
+              m.route.set(
+                `/${item.chain}/proposal/${item.proposalid.split('_')[0]}/${
+                  item.proposalid.split('_')[1]
+                }`
+              );
+            }}
+          >
+            <CWText>{`Comment on ${decodeURIComponent(item.title)}`}</CWText>
+            <CWText>{moment(item.created_at).fromNow()}</CWText>
+            {m(User, {
+              user: new AddressInfo(
+                item.address_id,
+                item.address,
+                item.address_chain,
+                null
+              ),
+            })}
+            {renderQuillTextBody(item.text, {
+              hideFormatting: true,
+              collapse: true,
+              searchTerm: searchQuery.searchTerm,
+            })}
+          </div>
+        ) : null;
+
+      organizedResults.push(resultRow);
+    });
+  });
+
+  return organizedResults;
+};
+
+const getSearchPreview = async (searchQuery: SearchQuery, state) => {
+  try {
+    await app.search.search(searchQuery);
+  } catch (err) {
+    state.results = {};
+    notifyError(err.responseJSON?.error || err.responseText || err.toString());
+  }
+
+  state.results = getResultsPreview(searchQuery);
+
+  app.search.addToHistory(searchQuery);
+
+  m.redraw();
+};
+
+// export const executeSearch = (query: SearchQuery) => {
+//   if (!query.searchTerm || !query.searchTerm.toString().trim()) {
+//     notifyError('Enter a valid search term');
+//     return;
+//   }
+
+//   if (query.searchTerm.length < 4) {
+//     notifyError('Query must be at least 4 characters');
+//   }
+
+//   query.isSearchPreview = false;
+
+//   app.search.addToHistory(query);
+
+//   m.route.set(`/search?${query.toUrlParams()}`);
+// };
+
 export class SearchBar extends ClassComponent {
   private results: Array<any>;
   private searchQuery: SearchQuery;
@@ -52,6 +182,7 @@ export class SearchBar extends ClassComponent {
   view() {
     const historyList = app.search.getHistory().map((previousQuery) => (
       <div
+        class="history-row"
         onclick={() => {
           getSearchPreview(previousQuery, this);
         }}
