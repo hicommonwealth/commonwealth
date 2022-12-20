360b4b107dff7c2deae49e09384f197496b04d2f
Daniel Trinh <danielhtrinh1@gmail.com>
2022-12-19 16:12:31 -0800

    Add Recursive Denial Implicit Permissions and Tests

--- packages/common-common/test/unit/getImplicitActions.spec.ts
+++ packages/common-common/test/unit/getImplicitActions.spec.ts
@@ -6,9 +6,8 @@ import {
 } from 'common-common/src/permissions';
 
 describe('getImplicitActions() unit tests', () => {
-
   it('should correctly isImplicitlyPermitted for Allowing createThread action', () => {
-    const actions = getImplicitActionsSet(Action.CREATE_THREAD);
+    const actions = getImplicitActionsSet(Action.CREATE_THREAD, true);
     assert.isTrue(actions.includes(Action.CREATE_THREAD));
     assert.isTrue(actions.includes(Action.CREATE_COMMENT));
     assert.isTrue(actions.includes(Action.EDIT_COMMENT));
@@ -31,7 +30,7 @@ describe('getImplicitActions() unit tests', () => {
   });
 
   it('should correctly isImplicitlyPermitted for Allowing createComment action', () => {
-    const actions = getImplicitActionsSet(Action.CREATE_COMMENT);
+    const actions = getImplicitActionsSet(Action.CREATE_COMMENT, true);
     assert.isTrue(actions.includes(Action.CREATE_COMMENT));
     assert.isTrue(actions.includes(Action.VIEW_COMMENTS));
     assert.isTrue(actions.includes(Action.VIEW_POLLS));
@@ -40,14 +39,14 @@ describe('getImplicitActions() unit tests', () => {
   });
 
   it('should correctly isImplicitlyPermitted for Allowing ViewPoll action', () => {
-    const actions = getImplicitActionsSet(Action.VIEW_POLLS);
+    const actions = getImplicitActionsSet(Action.VIEW_POLLS, true);
     assert.isTrue(actions.includes(Action.VIEW_REACTIONS));
     assert.isTrue(actions.includes(Action.VIEW_COMMENTS));
     assert.isFalse(actions.includes(Action.VIEW_THREADS));
   });
 
   it('should correctly isImplicitlyPermitted for Allowing Create Topic action', () => {
-    const actions = getImplicitActionsSet(Action.CREATE_TOPIC);
+    const actions = getImplicitActionsSet(Action.CREATE_TOPIC, true);
     assert.isTrue(actions.includes(Action.CREATE_TOPIC));
     assert.isTrue(actions.includes(Action.EDIT_TOPIC));
     assert.isTrue(actions.includes(Action.VIEW_TOPICS));
@@ -71,7 +70,41 @@ describe('getImplicitActions() unit tests', () => {
   });
 
   it('should correctly isImplicitlyPermitted for Allowing View Chat Channels action', () => {
-    const actions = getImplicitActionsSet(Action.VIEW_CHAT_CHANNELS);
+    const actions = getImplicitActionsSet(Action.VIEW_CHAT_CHANNELS, true);
     assert.isTrue(actions.includes(Action.VIEW_CHAT_CHANNELS));
+  });
+
+  it('should correctly isImplicitlyPermitted for Denying ViewReactions action', () => {
+    const actions = getImplicitActionsSet(Action.VIEW_REACTIONS, false);
+    assert.isTrue(actions.includes(Action.VIEW_REACTIONS));
+    assert.isTrue(actions.includes(Action.CREATE_REACTION));
+    assert.isTrue(actions.includes(Action.DELETE_REACTION));
+    assert.isTrue(actions.includes(Action.VIEW_COMMENTS));
+    assert.isTrue(actions.includes(Action.VIEW_POLLS));
+    assert.isTrue(actions.includes(Action.VIEW_THREADS));
+    assert.isTrue(actions.includes(Action.VIEW_TOPICS));
+  });
+
+  it('should correctly isImplicitlyPermitted for Denying Create Threads action', () => {
+    const actions = getImplicitActionsSet(Action.CREATE_THREAD, false);
+    assert.isTrue(actions.includes(Action.CREATE_THREAD));
+    assert.isTrue(actions.includes(Action.EDIT_THREAD));
+    assert.isTrue(actions.includes(Action.DELETE_THREAD));
+    assert.isTrue(actions.includes(Action.LINK_PROPOSAL_TO_THREAD));
+    assert.isTrue(actions.includes(Action.LINK_THREAD_TO_THREAD));
+    assert.isTrue(actions.includes(Action.VIEW_TOPICS));
+    assert.isTrue(actions.includes(Action.CREATE_TOPIC));
+    assert.isTrue(actions.includes(Action.EDIT_TOPIC));
+    assert.isFalse(actions.includes(Action.CREATE_COMMENT));
+    assert.isFalse(actions.includes(Action.EDIT_COMMENT));
+    assert.isFalse(actions.includes(Action.DELETE_COMMENT));
+    assert.isFalse(actions.includes(Action.VIEW_COMMENTS));
+    assert.isFalse(actions.includes(Action.CREATE_POLL));
+    assert.isFalse(actions.includes(Action.VOTE_ON_POLLS));
+    assert.isFalse(actions.includes(Action.VIEW_POLLS));
+    assert.isFalse(actions.includes(Action.VIEW_THREADS));
+    assert.isFalse(actions.includes(Action.CREATE_REACTION));
+    assert.isFalse(actions.includes(Action.VIEW_REACTIONS));
+    assert.isFalse(actions.includes(Action.DELETE_REACTION));
   });
 });
