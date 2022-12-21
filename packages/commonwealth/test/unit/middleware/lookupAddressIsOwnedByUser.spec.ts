696e6207cff7cb957af838e450e16b93f7c4e7bd
Daniel Trinh <danielhtrinh1@gmail.com>
2022-12-21 09:42:44 -0800

    Change Mock Names

--- packages/commonwealth/test/unit/middleware/lookupAddressIsOwnedByUser.spec.ts
+++ packages/commonwealth/test/unit/middleware/lookupAddressIsOwnedByUser.spec.ts
@@ -3,7 +3,7 @@ import { Sequelize, DataTypes } from 'sequelize';
 
 import MockExpressRequest from 'mock-express-request';
 import lookupAddressIsOwnedByUser from '../../../server/middleware/lookupAddressIsOwnedByUser';
-import { db } from '../../../server/models/mocks/mock';
+import { db } from '../../../server/models/mocks/mockDatabase';
 
 describe('lookupAddressIsOwnedByUser() unit tests', () => {
   it('should return highest role', async () => {
