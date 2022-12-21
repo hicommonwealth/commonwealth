f4ac54c90108a1446a6f6250eeec537bc84260a4
Timothee Legros <timothee@ualberta.ca>
2022-12-21 15:22:51 -0500

    chain-events db scripts + add option to load-db from custom dump

---
+++ packages/chain-events/scripts/load-db.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+# this path is relative to the location from which the script is called i.e. /commonwealth root rather than the path
+# from which the script exists i.e. commonwealth/scripts/
+. ../../scripts/load-env-var.sh --source-only
+
+load-env-var '.env';
+
+DUMP_NAME=latest.dump
+if [ "$1" ]; then
+  DUMP_NAME=$1
+fi
+
+PGPASSWORD=$DB_PASSWORD psql -d commonwealth_chain_events -U commonwealth -f "$DUMP_NAME";
