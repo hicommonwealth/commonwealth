710f09cc24f37e9066ad98954c2d3b95ec9c540a
Timothee Legros <timothee@ualberta.ca>
2022-12-21 15:36:35 -0500

    comment fix

--- packages/chain-events/scripts/load-db.sh
+++ packages/chain-events/scripts/load-db.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 
-# this path is relative to the location from which the script is called i.e. /commonwealth root rather than the path
-# from which the script exists i.e. commonwealth/scripts/
+# this path is relative to the location from which the script is called i.e. /chain-events root rather than the path
+# from which the script exists i.e. chain-events/scripts/
 . ../../scripts/load-env-var.sh --source-only
 
 load-env-var '.env';
