34c9474c46448264c6a611dca3cd0695bc56ff46
Gabriel Duquette <gabrielduquette@gmail.com>
2022-12-22 22:26:18 +0000

    conversion

--- packages/commonwealth/client/scripts/views/modals/select_address_modal.tsx
+++ packages/commonwealth/client/scripts/views/modals/select_address_modal.tsx
@@ -1,9 +1,10 @@
-/* eslint-disable @typescript-eslint/ban-types */
-import 'modals/select_address_modal.scss';
+/* @jsx m */
 
 import m from 'mithril';
+import ClassComponent from 'class_component';
 import $ from 'jquery';
-import { Tag, Button } from 'construct-ui';
+
+import 'modals/select_address_modal.scss';
 
 import app from 'state';
 import { ChainBase, ChainNetwork, WalletId } from 'common-common/src/types';
@@ -15,30 +16,30 @@ import { setActiveAccount } from 'controllers/app/login';
 import { confirmationModalWithText } from 'views/modals/confirm_modal';
 import { formatAddressShort } from '../../../../shared/utils';
 import { CWIcon } from '../components/component_kit/cw_icons/cw_icon';
-import  ClassComponent from 'class_component';
-
-type SelectAddressModalProps = {
-
-}
+import { CWButton } from '../components/component_kit/cw_button';
+import { getClasses } from '../components/component_kit/helpers';
+import { ModalExitButton } from '../components/component_kit/cw_modal';
+import { CWText } from '../components/component_kit/cw_text';
 
 export class SelectAddressModal extends ClassComponent {
-  private selectedIndex: number;
   private loading: boolean;
-  private activeAccountsByRole: Array<[Account, RoleInfo]> 
+  private selectedIndex: number;
 
-  oninit() {
-    this.activeAccountsByRole = app.roles.getActiveAccountsByRole();
-  }
+  view() {
+    const activeAccountsByRole: Array<[Account, RoleInfo]> =
+      app.roles.getActiveAccountsByRole();
 
-  view (vnode: m.Vnode<SelectAddressModalProps>) {
     const activeEntityInfo = app.chain?.meta;
+
     const createRole = (e) => {
       this.loading = true;
 
-      const [account, role] = this.activeAccountsByRole[this.selectedIndex];
+      const [account] = activeAccountsByRole[this.selectedIndex];
+
       const addressInfo = app.user.addresses.find(
         (a) => a.address === account.address && a.chain.id === account.chain.id
       );
+
       app.roles
         .createRole({
           address: addressInfo,
@@ -66,11 +67,13 @@ export class SelectAddressModal extends ClassComponent {
           m.redraw();
           notifyError(err.responseJSON.error);
         });
-    },
+    };
 
-    const deleteRole = async (index, e) => {
+    const deleteRole = async (index) => {
       this.loading = true;
-      const [account, role] = this.activeAccountsByRole[index];
+
+      const [account] = activeAccountsByRole[index];
+
       const addressInfo = app.user.addresses.find(
         (a) => a.address === account.address && a.chain.id === account.chain.id
       );
@@ -79,6 +82,7 @@ export class SelectAddressModal extends ClassComponent {
       const confirmed = await confirmationModalWithText(
         'Remove this address from the community?'
       )();
+
       if (!confirmed) {
         this.loading = false;
         m.redraw();
@@ -104,7 +108,7 @@ export class SelectAddressModal extends ClassComponent {
           m.redraw();
           notifyError(err.responseJSON.error);
         });
-    }
+    };
 
     const chainbase = app.chain ? app.chain?.meta?.base : ChainBase.Ethereum;
 
@@ -112,24 +116,116 @@ export class SelectAddressModal extends ClassComponent {
     const hasTermsOfService = !!activeCommunityMeta?.terms;
 
     return (
-      <div class='SelectAddressModal'>
-        <div class='.compact-modal-title'>
-          <h3> Manage addresses </h3>
+      <div class="SelectAddressModal">
+        <div class="compact-modal-title">
+          <h3>Manage addresses</h3>
+          <ModalExitButton />
         </div>
-        <div class='compact-modal-body'>
-          {this.activeAccountsByRole === 0 ? (
-            <div class='.select-address-placeholder'>
-              <p>Connect{ chainbase && app.chain.network === ChainNetwork.Terra
-                    ? 'Terra'
-                    : chainbase
-                    ? chainbase[0].toUpperCase() + chainbase.slice(1)
-                    : 'Web3'
-}</p>
+        <div class="compact-modal-body">
+          {activeAccountsByRole.length === 0 ? (
+            <div class="select-address-placeholder">
+              Connect{' '}
+              {chainbase && app.chain.network === ChainNetwork.Terra
+                ? 'Terra'
+                : chainbase
+                ? chainbase[0].toUpperCase() + chainbase.slice(1)
+                : 'Web3'}{' '}
+              address to join this community:
             </div>
+          ) : (
+            <div class="select-address-options">
+              {activeAccountsByRole.map(
+                ([account, role]) =>
+                  role && (
+                    <div class="select-address-option existing">
+                      <div class="select-address-option-left">
+                        {m(UserBlock, { user: account })}
+                        {app.user.addresses.find(
+                          (a) =>
+                            a.address === account.address &&
+                            a.chain.id === account.chain.id
+                        )?.walletId === WalletId.Magic && (
+                          <div class="magic-label">
+                            Magically linked to {app.user.email}
+                          </div>
+                        )}
+                      </div>
+                      <div class="role-remove">
+                        <span class="already-connected">
+                          {formatAsTitleCase(role.permission)} of{' '}
+                          {activeEntityInfo?.name}
+                        </span>
+                        <CWIcon
+                          iconName="close"
+                          iconSize="small"
+                          onclick={deleteRole.bind(this)}
+                        />
+                      </div>
+                    </div>
+                  )
+              )}
+              {activeAccountsByRole.map(
+                ([account, role], index) =>
+                  !role && (
+                    <div
+                      class={getClasses<{ isSelected: boolean }>(
+                        { isSelected: this.selectedIndex === index },
+                        'select-address-option'
+                      )}
+                      onclick={async (e) => {
+                        e.preventDefault();
+                        this.selectedIndex = index;
+                      }}
+                    >
+                      <div class="select-address-option-left">
+                        {m(UserBlock, {
+                          user: account,
+                          showRole: true,
+                          selected: this.selectedIndex === index,
+                        })}
+                        {app.user.addresses.find(
+                          (a) =>
+                            a.address === account.address &&
+                            a.chain.id === account.chain.id
+                        )?.walletId === WalletId.Magic && (
+                          <div class="magic-label">
+                            Magically linked to {app.user.email}
+                          </div>
+                        )}
+                      </div>
+                      {role && (
+                        <div class="role-permission">
+                          <CWText className="role-tag">
+                            {formatAsTitleCase(role.permission)}
+                          </CWText>
+                          {role.is_user_default && (
+                            <CWText className="role-tag">Last used</CWText>
+                          )}
+                        </div>
+                      )}
+                    </div>
+                  )
+              )}
+            </div>
+          )}
+          {hasTermsOfService && (
+            <div class="terms-of-service">
+              By linking an address, you agree to ${activeCommunityMeta.name}'s{' '}
+              <a href={activeCommunityMeta.terms} target="_blank">
+                terms of service
+              </a>
+              .
+            </div>
+          )}
+          {activeAccountsByRole.length !== 0 && (
+            <CWButton
+              label="Join community with address"
+              disabled={typeof this.selectedIndex !== 'number' || this.loading}
+              onclick={createRole.bind(this)}
+            />
           )}
         </div>
       </div>
     );
   }
 }
-
