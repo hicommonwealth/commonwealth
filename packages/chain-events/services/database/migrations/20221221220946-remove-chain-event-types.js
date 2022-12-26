b503daa688f436f52d7976ca2ed568a9546f36de
Timothee Legros <timothee@ualberta.ca>
2022-12-26 18:55:57 -0500

    migration fixes

--- packages/chain-events/services/database/migrations/20221221220946-remove-chain-event-types.js
+++ packages/chain-events/services/database/migrations/20221221220946-remove-chain-event-types.js
@@ -2,18 +2,27 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('ChainEventTypes');
+    await queryInterface.sequelize.transaction(async (t) => {
+      await queryInterface.removeColumn('ChainEvents', 'chain_event_type_id', {transaction: t});
+      await queryInterface.dropTable('ChainEventTypes', {transaction: t});
+    });
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('ChainEventTypes', {
-      // id = chain-event_name (event_name is value of string enum)
-      id: { type: Sequelize.STRING, primaryKey: true },
-      chain: { type: Sequelize.STRING, allowNull: false },
-      // should never be null, but added here for migration purposes
-      event_network: { type: Sequelize.STRING, allowNull: true },
-      event_name: { type: Sequelize.STRING, allowNull: false },
-      queued: { type: Sequelize.SMALLINT, allowNull: false, defaultValue: 0 },
+    await queryInterface.sequelize.transaction(async (t) => {
+      await queryInterface.createTable('ChainEventTypes', {
+        // id = chain-event_name (event_name is value of string enum)
+        id: { type: Sequelize.STRING, primaryKey: true },
+        chain: { type: Sequelize.STRING, allowNull: false },
+        // should never be null, but added here for migration purposes
+        event_network: { type: Sequelize.STRING, allowNull: true },
+        event_name: { type: Sequelize.STRING, allowNull: false },
+        queued: { type: Sequelize.SMALLINT, allowNull: false, defaultValue: 0 },
+      }, {transaction: t});
+
+      await queryInterface.addColumn('ChainEvents', 'chain_event_type_id', {
+        type: Sequelize.STRING, allowNull: true, references: {model: 'ChainEventTypes', key: 'id'}
+      }, {transaction: t});
     });
   }
 };
