51a3bce0fdc697aa18515cb5c9382761e1d29142
Richard Melkonian <r.v.melkonian@gmail.com>
2023-01-02 19:14:25 +0100

    fix missing method errors

--- packages/commonwealth/server/routes/comments/getComments.ts
+++ packages/commonwealth/server/routes/comments/getComments.ts
@@ -1,43 +1,66 @@
-import { NextFunction } from 'express';
+import Sequelize from 'sequelize';
+import { GetCommentsReq, GetCommentsResp } from 'common-common/src/api/extApiTypes';
+import { query, validationResult } from 'express-validator';
 import { Action } from 'common-common/src/permissions';
-import { ServerError } from 'common-common/src/errors';
-import { TypedRequestQuery, TypedResponse, success } from '../../types';
+import { TypedRequestQuery, TypedResponse, success, failure } from '../../types';
 import { DB } from '../../models';
-import { CommentAttributes } from '../../models/comment';
 import { checkReadPermitted } from '../../util/roles';
+import { formatPagination } from '../../util/queries';
 
-type GetCommentsReq = {
-  community_id: string;
-  thread_id?: number;
-  addresses?: string[];
+const { Op } = Sequelize;
 
-  // TODO: goes in pagination helper
-  limit?: number;
-  page?: string;
-  sort?: string;
-  count_only?: boolean;
-};
+export const getCommentsValidation = [
+  query('community_id').isString().trim(),
+  query('addresses').optional().toArray(),
+  query('count_only').optional().isBoolean().toBoolean()
+];
 
-type GetThreadsResp = CommentAttributes[];
-
-const getComments = async (
+export const getComments = async (
   models: DB,
   req: TypedRequestQuery<GetCommentsReq>,
-  res: TypedResponse<GetThreadsResp>,
-  next: NextFunction
+  res: TypedResponse<GetCommentsResp>,
 ) => {
-  try {
+  const errors = validationResult(req).array();
+  if (errors.length !== 0) {
+    return failure(res.status(400), errors);
+  }
+
+  const { community_id, addresses, count_only } = req.query;
+
+  try{
     await checkReadPermitted(
       models,
       req.query.community_id,
-      Action.VIEW_COMMENTS,
-      req.user?.id,
-    );
+      Action.VIEW_REACTIONS,
+      req.user?.id
+  );
+  } catch (err) {
+    return failure(res.status(403), err);
+  }
 
-    return success(res, []);
-    } catch (err) {
-      return next(new ServerError(err));
-    }
+  const where = { chain: community_id };
+
+  const include = [];
+  if (addresses) include.push({
+    model: models.Address,
+    where: { address: { [Op.in]: addresses } },
+    required: true
+  });
+
+  let comments, count;
+  if (!count_only) {
+    ({ rows: comments, count } = await models.Comment.findAndCountAll({
+      where,
+      include,
+      ...formatPagination(req.query)
+    }));
+  } else {
+    count = await models.Comment.count({
+      where,
+      include,
+      ...formatPagination(req.query)
+    });
+  }
+
+  return success(res, { comments, count });
 };
-
-export default getComments;
