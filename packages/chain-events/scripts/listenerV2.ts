7b0c278cb05a338302ed663132064e1aa0fd3266
Timothee Legros <timothee@ualberta.ca>
2023-01-05 17:12:32 -0500

    chain-events eslint fixes

--- packages/chain-events/scripts/listenerV2.ts
+++ packages/chain-events/scripts/listenerV2.ts
@@ -3,8 +3,8 @@ import * as yargs from 'yargs';
 import { createListener, LoggingHandler, SupportedNetwork } from '../src';
 
 import { networkUrls, contracts, networkSpecs } from './listenerUtils';
-
-require('dotenv').config();
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 const { argv } = yargs.options({
   network: {
