b9dc97e0201149626267b972d72d5177af6c3eb4
kassad <kurtis@commonwealth.im>
2023-01-06 13:57:10 -0800

    Reverted to babel-loader

--- packages/commonwealth/webpack/webpack.base.config.js
+++ packages/commonwealth/webpack/webpack.base.config.js
@@ -177,7 +177,7 @@ module.exports = {
         ],
         exclude: /\/node_modules\//,
         use: {
-          loader: 'swc-loader',
+          loader: 'babel-loader',
         },
       },
       {
