471dec04326aceb9ed0eda0a3496112cebca8f29
kassad <kurtis@commonwealth.im>
2023-01-06 09:48:25 -0800

    updated webpack loaders

--- packages/commonwealth/webpack/webpack.base.config.js
+++ packages/commonwealth/webpack/webpack.base.config.js
@@ -172,7 +172,7 @@ module.exports = {
         ],
         exclude: /\/node_modules\//,
         use: {
-          loader: 'babel-loader',
+          loader: 'swc-loader',
         },
       },
       {
