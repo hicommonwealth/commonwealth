90fceb2197d30a307020150f0fdc8b5496050c39
Daniel Trinh <danielhtrinh1@gmail.com>
2023-01-06 11:07:21 -0800

    Update chain.ts

--- packages/commonwealth/server/models/chain.ts
+++ packages/commonwealth/server/models/chain.ts
@@ -127,10 +127,6 @@ export default (
       },
       default_summary_view: { type: dataTypes.BOOLEAN, allowNull: true },
       contracts_viewable: { type: dataTypes.STRING, allowNull: true },
-      snapshot: {
-        type: dataTypes.ARRAY(dataTypes.STRING),
-        allowNull: true,
-      },
       hide_projects: { type: dataTypes.BOOLEAN, allowNull: true },
       terms: { type: dataTypes.STRING, allowNull: true },
       bech32_prefix: { type: dataTypes.STRING, allowNull: true },
