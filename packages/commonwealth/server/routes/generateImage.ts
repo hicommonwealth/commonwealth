55fce240d35a1b740b6d3f7ebb5f61446f1cd66d
Alex Young <alexyoung@alexs-m1-mbp.mynetworksettings.com>
2023-01-06 16:53:31 -0500

    swap to dalle

--- packages/commonwealth/server/routes/generateImage.ts
+++ packages/commonwealth/server/routes/generateImage.ts
@@ -1,11 +1,18 @@
 import AWS from 'aws-sdk';
 import { v4 as uuidv4 } from 'uuid';
-import * as fs from 'fs';
-import fetch, { Blob } from 'node-fetch';
+import { Configuration, OpenAIApi } from 'openai';
+import request from 'request';
+import fetch from 'node-fetch';
 
 import { success, TypedRequestBody, TypedResponse } from '../types';
 import { DB } from '../models';
 import { AppError } from '../../../common-common/src/errors';
+
+const configuration = new Configuration({
+  organization: 'org-D0ty00TJDApqHYlrn1gge2Ql',
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
 
 type generateImageReq = {
   description: string;
@@ -25,32 +32,18 @@ const generateImage = async (
     throw new AppError('No description provided');
   }
 
-  const encodedParams = new URLSearchParams();
-  encodedParams.append('upscale', '1');
-  encodedParams.append('model', 'stablediffusion_1_5');
-  encodedParams.append('steps', '30');
-  encodedParams.append('sampler', 'dpm');
-  encodedParams.append('guidance', '8');
-  encodedParams.append('width', '768');
-  encodedParams.append('prompt', description);
-
-  const options = {
-    method: 'POST',
-    headers: {
-      'content-type': 'application/x-www-form-urlencoded',
-      'X-RapidAPI-Key': '016a70f43fmshd073e94a09775a6p103ff1jsnd2b0f07682d5', // TODO: Replace with real .env key later
-      'X-RapidAPI-Host': 'dezgo.p.rapidapi.com',
-    },
-    body: encodedParams,
-  };
-
-  let imageResp;
   let image;
   try {
     console.log('Starting Image Generation', Date.now());
 
-    imageResp = await fetch('https://dezgo.p.rapidapi.com/text2image', options);
-    image = await imageResp.blob();
+    const response = await openai.createImage({
+      prompt: description,
+      size: '512x512',
+      response_format: 'url',
+    });
+
+    image = response.data.data[0].url;
+    console.log(image);
 
     console.log('End Image Generation', Date.now());
   } catch (e) {
@@ -59,34 +52,29 @@ const generateImage = async (
   }
 
   const s3 = new AWS.S3();
+  const resp = await fetch(image);
+  const buffer = await resp.buffer();
   const params = {
     Bucket: 'commonwealth-uploads',
     Key: `${uuidv4()}.png`,
-    Expires: 3600,
+    Body: buffer,
     ContentType: 'image/png',
   };
 
   let imageUrl = '';
   try {
     console.log('Starting Image Upload', Date.now());
-    imageUrl = await s3.getSignedUrl('putObject', params);
 
-    await fetch(imageUrl, {
-      method: 'PUT',
-      body: image,
-      headers: {
-        'Content-Type': 'image/png',
-      },
-    });
+    const upload = await s3.upload(params).promise();
+    imageUrl = upload.Location;
+
     console.log('End Image Upload', Date.now());
   } catch (e) {
     console.log(e);
-    throw new AppError('Problem!');
+    throw new AppError('Problem uploading image!');
   }
 
-  const trimmedURL = imageUrl.slice(0, imageUrl.indexOf('?'));
-
-  return success(res, { imageUrl: trimmedURL });
+  return success(res, { imageUrl });
 };
 
 export default generateImage;
