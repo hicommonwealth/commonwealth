656f95e373fb4c50c8799a728eda3d58cd6f6b2d
Timothee Legros <timothee@ualberta.ca>
2023-01-12 18:40:22 -0700

    script conditional checking

--- scripts/get_max_old_space_size.sh
+++ scripts/get_max_old_space_size.sh
@@ -5,10 +5,13 @@
 # the node default of 4GB
 
 # retrieves a Heroku dyno's total ram
+# https://help.heroku.com/TWBM7DL0/how-do-i-measure-current-memory-use-and-max-available-memory-on-a-dyno-in-a-private-space
 MEMORY_LIMIT=$(echo -n "$(</sys/fs/cgroup/memory/memory.limit_in_bytes)")
 
-# MEMORY_LIMIT is only set on Heroku Dyno's
-if [ "$MEMORY_LIMIT" ]; then
+# if ram can be retrieved and it is greater than 400MB then return 70% of it
+# the -gt check ensures that the max_old_space_size will never be set to a tiny value if the method of retrieving
+# the total ram breaks
+if [ "$MEMORY_LIMIT" ] && [ "$MEMORY_LIMIT" -gt 400000000 ]; then
   # set to 70% of total ram on the dyno
   echo $($MEMORY_LIMIT*70/100/1000000);
 else
