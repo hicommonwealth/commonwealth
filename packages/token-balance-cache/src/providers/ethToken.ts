7cab903b0cbfcd42b2f590f1ac1652243ddfe74a
Daniel Trinh <danielhtrinh1@gmail.com>
2023-01-19 15:39:53 -0800

    Prettier

--- packages/token-balance-cache/src/providers/ethToken.ts
+++ packages/token-balance-cache/src/providers/ethToken.ts
@@ -1,7 +1,11 @@
 import Web3 from 'web3';
 import { BigNumber, providers } from 'ethers';
 import type { ERC20, ERC721, ERC1155 } from 'common-common/src/eth/types';
-import { ERC20__factory, ERC721__factory, ERC1155__factory } from 'common-common/src/eth/types';
+import {
+  ERC20__factory,
+  ERC721__factory,
+  ERC1155__factory,
+} from 'common-common/src/eth/types';
 
 import type { IChainNode } from '../types';
 import { BalanceProvider } from '../types';
@@ -44,7 +48,7 @@ export default class EthTokenBalanceProvider extends BalanceProvider<EthBPOpts> 
       const web3 = new Web3(provider);
       return await this.fetchBalance(web3, provider, 'eth', address);
     }
-    
+
     if (!Web3.utils.isAddress(tokenAddress)) {
       throw new Error('Invalid token address');
     }
@@ -53,7 +57,7 @@ export default class EthTokenBalanceProvider extends BalanceProvider<EthBPOpts> 
     }
 
     if (contractType === 'erc1155' && !tokenId) {
-      throw new Error('Token Id Required For ERC-1155')
+      throw new Error('Token Id Required For ERC-1155');
     }
     const provider = new Web3.providers.WebsocketProvider(url);
     switch (contractType) {
@@ -64,7 +68,12 @@ export default class EthTokenBalanceProvider extends BalanceProvider<EthBPOpts> 
           new providers.Web3Provider(provider as any)
         );
         await erc20Api.deployed();
-        return await this.fetchBalance(erc20Api, provider, contractType, address);
+        return await this.fetchBalance(
+          erc20Api,
+          provider,
+          contractType,
+          address
+        );
 
       case 'erc721':
         // eslint-disable-next-line no-case-declarations
@@ -73,30 +82,51 @@ export default class EthTokenBalanceProvider extends BalanceProvider<EthBPOpts> 
           new providers.Web3Provider(provider as any)
         );
         await erc721Api.deployed();
-        return await this.fetchBalance(erc721Api, provider, contractType, address);
-      
+        return await this.fetchBalance(
+          erc721Api,
+          provider,
+          contractType,
+          address
+        );
+
       case 'erc1155':
         // eslint-disable-next-line no-case-declarations
         const erc1155Api: ERC1155 = ERC1155__factory.connect(
           tokenAddress,
           new providers.Web3Provider(provider as any)
-        )
+        );
         await erc1155Api.deployed();
-        return await this.fetchBalance(erc1155Api, provider, contractType, address, tokenId);
-      
+        return await this.fetchBalance(
+          erc1155Api,
+          provider,
+          contractType,
+          address,
+          tokenId
+        );
+
       default:
         throw new Error('Invalid contract type');
     }
-    
   }
 
-  private async fetchBalance(api: any, provider: any, tokenType: string, address: string,  tokenId?: string): Promise<string> {
+  private async fetchBalance(
+    api: any,
+    provider: any,
+    tokenType: string,
+    address: string,
+    tokenId?: string
+  ): Promise<string> {
     if (tokenType === 'erc1155') {
-      const balanceBigNum: BigNumber = await (api as ERC1155).balanceOf(address, tokenId);
+      const balanceBigNum: BigNumber = await (api as ERC1155).balanceOf(
+        address,
+        tokenId
+      );
       provider.disconnect(1000, 'finished');
       return balanceBigNum.toString();
     } else if (tokenType === 'erc20' || tokenType === 'erc721') {
-      const balanceBigNum: BigNumber = await (api as ERC20 | ERC721).balanceOf(address);
+      const balanceBigNum: BigNumber = await (api as ERC20 | ERC721).balanceOf(
+        address
+      );
       provider.disconnect(1000, 'finished');
       return balanceBigNum.toString();
     } else if (tokenType === 'eth') {
