9029e1d287cc1666f04db4f6c81f2239d402744a
affordances <gabrielduquette@gmail.com>
2023-01-20 16:07:19 +0000

    Convert and clean up notifications page and menu (#2311)
    * basics
    
    * neatening
    
    * vnode type
    
    * use new class component
    
    * hyperscript out of helpers
    
    * hyperscript out
    
    * tweaks
    
    * WIP
    
    * preserve update from master
    
    * better menu default styles
    
    * WIP
    
    * imports
    
    * pass reference instead of invoke function
    
    * boilerplate changes
    
    * preexisting problems
    
    Co-authored-by: Marcin Maslanka <marcin@commonwealth.im>
    Co-authored-by: zakhap <zhs2104@columbia.edu>

--- packages/commonwealth/client/scripts/app.new.tsx
+++ packages/commonwealth/client/scripts/app.new.tsx
@@ -10,13 +10,17 @@ const CommunitiesPage = lazy(() => import('./views/pages/communities'));
 const SearchPage = lazy(() => import('./views/pages/search'));
 const UserDashboard = lazy(() => import('./views/pages/user_dashboard'));
 const Web3LoginPage = lazy(() => import('./views/pages/web3login'));
-const NotificationsPage = lazy(() => import('./views/pages/notifications_page'));
-const NotificationSettingsPage = lazy(() => import('./views/pages/notification_settings'));
+const NotificationsPage = lazy(() => import('./views/pages/notifications'));
+const NotificationSettingsPage = lazy(
+  () => import('./views/pages/notification_settings')
+);
 // @REACT @TODO cleanup these routes by removing explicit state variable
 // const FinishNearLogin = lazy(() => import('./views/pages/finish_near_login'));
 // const FinishAxieLogin = lazy(() => import('./views/pages/finish_axie_login'));
 const SettingsPage = lazy(() => import('./views/pages/settings'));
-const DiscussionsRedirect = lazy(() => import('./views/pages/discussions_redirect'));
+const DiscussionsRedirect = lazy(
+  () => import('./views/pages/discussions_redirect')
+);
 const OverviewPage = lazy(() => import('./views/pages/overview'));
 const SputnikDAOsPage = lazy(() => import('./views/pages/sputnikdaos'));
 const ChatPage = lazy(() => import('./views/pages/chat'));
@@ -33,13 +37,23 @@ const BountiesPage = lazy(() => import('./views/pages/bounties'));
 const TipsPage = lazy(() => import('./views/pages/tips'));
 const ValidatorsPage = lazy(() => import('./views/pages/validators'));
 const AdminPage = lazy(() => import('./views/pages/admin'));
-const ManageCommunityPage = lazy(() => import('./views/pages/manage_community'));
+const ManageCommunityPage = lazy(
+  () => import('./views/pages/manage_community')
+);
 const SpecSettingsPage = lazy(() => import('./views/pages/spec_settings'));
 const StatsPage = lazy(() => import('./views/pages/stats'));
-const SnapshotProposalsPage = lazy(() => import('./views/pages/snapshot_proposals'));
-const MultipleSnapshotsPage = lazy(() => import('./views/pages/view_multiple_snapshot_spaces'));
-const NewSnapshotProposalPage = lazy(() => import('./views/pages/new_snapshot_proposal'));
-const ViewSnapshotProposalPage = lazy(() => import('./views/pages/view_snapshot_proposal'));
+const SnapshotProposalsPage = lazy(
+  () => import('./views/pages/snapshot_proposals')
+);
+const MultipleSnapshotsPage = lazy(
+  () => import('./views/pages/view_multiple_snapshot_spaces')
+);
+const NewSnapshotProposalPage = lazy(
+  () => import('./views/pages/new_snapshot_proposal')
+);
+const ViewSnapshotProposalPage = lazy(
+  () => import('./views/pages/view_snapshot_proposal')
+);
 
 const DiscussionsPage = lazy(() => import('views/pages/discussions'));
 const WhyCommonwealthPage = lazy(() => import('views/pages/why_commonwealth'));
@@ -67,8 +81,12 @@ const useInitApp = () => {
 
 const LayoutWrapper = ({ Component, params }) => {
   const routerParams = useParams();
-  return <Layout params={Object.assign(params, routerParams)}><Component {...routerParams} /></Layout>;
-}
+  return (
+    <Layout params={Object.assign(params, routerParams)}>
+      <Component {...routerParams} />
+    </Layout>
+  );
+};
 
 const withLayout = (Component, params) => {
   return <LayoutWrapper Component={Component} params={params} />;
@@ -81,33 +99,66 @@ const App = () => {
   return (
     <Suspense fallback={<PageLoading />}>
       <Routes>
-        <Route path="/" element={
-          withLayout(LandingPage, { scoped: false, hideSidebar: false })
-        } />
-        <Route path="/communities" element={
-          withLayout(CommunitiesPage, { scoped: false, hideSidebar: false })
-        } />
-        <Route path="/search" element={
-          withLayout(SearchPage, { scoped: false, deferChain: true })
-        } />
-        <Route path="/whyCommonwealth" element={
-          withLayout(WhyCommonwealthPage, { scoped: false, hideSidebar: true })
-        } />
-        <Route path="/dashboard" element={
-          withLayout(UserDashboard, { scoped: false, deferChain: true })
-        } />
-        <Route path="/dashboard/:type" element={
-          withLayout(UserDashboard, { scoped: false, deferChain: true })
-        } />
-        <Route path="/web3login" element={
-          withLayout(Web3LoginPage, { scoped: false, deferChain: true })
-        } />
-        <Route path="/:scope/notifications" element={
-          withLayout(NotificationsPage, { scoped: true, deferChain: true })
-        } />
-        <Route path="/notification-settings" element={
-          withLayout(NotificationSettingsPage, { scoped: true, deferChain: true })
-        } />
+        <Route
+          path="/"
+          element={withLayout(LandingPage, {
+            scoped: false,
+            hideSidebar: false,
+          })}
+        />
+        <Route
+          path="/communities"
+          element={withLayout(CommunitiesPage, {
+            scoped: false,
+            hideSidebar: false,
+          })}
+        />
+        <Route
+          path="/search"
+          element={withLayout(SearchPage, { scoped: false, deferChain: true })}
+        />
+        <Route
+          path="/whyCommonwealth"
+          element={withLayout(WhyCommonwealthPage, {
+            scoped: false,
+            hideSidebar: true,
+          })}
+        />
+        <Route
+          path="/dashboard"
+          element={withLayout(UserDashboard, {
+            scoped: false,
+            deferChain: true,
+          })}
+        />
+        <Route
+          path="/dashboard/:type"
+          element={withLayout(UserDashboard, {
+            scoped: false,
+            deferChain: true,
+          })}
+        />
+        <Route
+          path="/web3login"
+          element={withLayout(Web3LoginPage, {
+            scoped: false,
+            deferChain: true,
+          })}
+        />
+        <Route
+          path="/:scope/notifications"
+          element={withLayout(NotificationsPage, {
+            scoped: true,
+            deferChain: true,
+          })}
+        />
+        <Route
+          path="/notification-settings"
+          element={withLayout(NotificationSettingsPage, {
+            scoped: true,
+            deferChain: true,
+          })}
+        />
         {/*
         <Route path="/:scope/finishNearLogin" element={
           withLayout(FinishNearLogin, { scoped: true })
@@ -116,102 +167,146 @@ const App = () => {
           withLayout(FinishAxieLogin, { scoped: false })
         } />
         */}
-        <Route path="/:scope/settings" element={
-          withLayout(SettingsPage, { scoped: true })
-        } />
-        <Route path="/:scope" element={
-          withLayout(DiscussionsRedirect, { scoped: true })
-        } />
-        <Route path="/:scope/discussions" element={
-          withLayout(DiscussionsPage, { scoped: true, deferChain: true })
-        } />
-        <Route path="/:scope/overview" element={
-          withLayout(OverviewPage, { scoped: true, deferChain: true })
-        } />
-        <Route path="/:scope/discussions/:topic" element={
-          withLayout(DiscussionsPage, { scoped: true, deferChain: true })
-        } />
-        <Route path="/:scope/search" element={
-          withLayout(SearchPage, { scoped: true, deferChain: true })
-        } />
-        <Route path="/:scope/members" element={
-          withLayout(MembersPage, { scoped: true, deferChain: true })
-        } />
-        <Route path="/:scope/sputnik-daos" element={
-          withLayout(SputnikDAOsPage, { scoped: true, deferChain: true })
-        } />
-        <Route path="/:scope/chat/:channel" element={
-          withLayout(ChatPage, { scoped: true, deferChain: true })
-        } />
-        <Route path="/:scope/new/discussion" element={
-          withLayout(NewThreadPage, { scoped: true, deferChain: true })
-        } />
-        <Route path="/:scope/account/:address" element={
-          withLayout(ProfilePage, { scoped: true, deferChain: true })
-        } />
-        <Route path="/:scope/referenda" element={
-          withLayout(ReferendaPage, { scoped: true })
-        } />
-        <Route path="/:scope/proposals" element={
-          withLayout(ProposalsPage, { scoped: true })
-        } />
-        <Route path="/:scope/council" element={
-          withLayout(CouncilPage, { scoped: true })
-        } />
-        <Route path="/:scope/delegate" element={
-          withLayout(DelegatePage, { scoped: true })
-        } />
-        <Route path="/:scope/proposal/:type/:identifier" element={
-          withLayout(ViewProposalPage, { scoped: true })
-        } />
-        <Route path="/:scope/proposal/:identifier" element={
-          withLayout(ViewProposalPage, { scoped: true })
-        } />
-        <Route path="/:scope/discussion/:identifier" element={
-          withLayout(ViewThreadPage, { scoped: true })
-        } />
-        <Route path="/:scope/new/proposal/:type" element={
-          withLayout(NewProposalPage, { scoped: true })
-        } />
-        <Route path="/:scope/new/proposal" element={
-          withLayout(NewProposalPage, { scoped: true })
-        } />
-        <Route path="/:scope/treasury" element={
-          withLayout(TreasuryPage, { scoped: true })
-        } />
-        <Route path="/:scope/bounties" element={
-          withLayout(BountiesPage, { scoped: true })
-        } />
-        <Route path="/:scope/tips" element={
-          withLayout(TipsPage, { scoped: true })
-        } />
-        <Route path="/:scope/validators" element={
-          withLayout(ValidatorsPage, { scoped: true })
-        } />
-        <Route path="/:scope/admin" element={
-          withLayout(AdminPage, { scoped: true })
-        } />
-        <Route path="/:scope/manage" element={
-          withLayout(ManageCommunityPage, { scoped: true })
-        } />
-        <Route path="/:scope/spec_settings" element={
-          withLayout(SpecSettingsPage, { scoped: true })
-        } />
-        <Route path="/:scope/analytics" element={
-          withLayout(StatsPage, { scoped: true })
-        } />
-        <Route path="/:scope/snapshot/:snapshotId" element={
-          withLayout(SnapshotProposalsPage, { scoped: true })
-        } />
-        <Route path="/:scope/multiple-snapshots" element={
-          withLayout(MultipleSnapshotsPage, { scoped: true })
-        } />
-        <Route path="/:scope/snapshot/:snapshotId/:identifier" element={
-          withLayout(ViewSnapshotProposalPage, { scoped: true })
-        } />
-        <Route path="/:scope/new/snapshot/:snapshotId" element={
-          withLayout(NewSnapshotProposalPage, { scoped: true })
-        } />
+        <Route
+          path="/:scope/settings"
+          element={withLayout(SettingsPage, { scoped: true })}
+        />
+        <Route
+          path="/:scope"
+          element={withLayout(DiscussionsRedirect, { scoped: true })}
+        />
+        <Route
+          path="/:scope/discussions"
+          element={withLayout(DiscussionsPage, {
+            scoped: true,
+            deferChain: true,
+          })}
+        />
+        <Route
+          path="/:scope/overview"
+          element={withLayout(OverviewPage, { scoped: true, deferChain: true })}
+        />
+        <Route
+          path="/:scope/discussions/:topic"
+          element={withLayout(DiscussionsPage, {
+            scoped: true,
+            deferChain: true,
+          })}
+        />
+        <Route
+          path="/:scope/search"
+          element={withLayout(SearchPage, { scoped: true, deferChain: true })}
+        />
+        <Route
+          path="/:scope/members"
+          element={withLayout(MembersPage, { scoped: true, deferChain: true })}
+        />
+        <Route
+          path="/:scope/sputnik-daos"
+          element={withLayout(SputnikDAOsPage, {
+            scoped: true,
+            deferChain: true,
+          })}
+        />
+        <Route
+          path="/:scope/chat/:channel"
+          element={withLayout(ChatPage, { scoped: true, deferChain: true })}
+        />
+        <Route
+          path="/:scope/new/discussion"
+          element={withLayout(NewThreadPage, {
+            scoped: true,
+            deferChain: true,
+          })}
+        />
+        <Route
+          path="/:scope/account/:address"
+          element={withLayout(ProfilePage, { scoped: true, deferChain: true })}
+        />
+        <Route
+          path="/:scope/referenda"
+          element={withLayout(ReferendaPage, { scoped: true })}
+        />
+        <Route
+          path="/:scope/proposals"
+          element={withLayout(ProposalsPage, { scoped: true })}
+        />
+        <Route
+          path="/:scope/council"
+          element={withLayout(CouncilPage, { scoped: true })}
+        />
+        <Route
+          path="/:scope/delegate"
+          element={withLayout(DelegatePage, { scoped: true })}
+        />
+        <Route
+          path="/:scope/proposal/:type/:identifier"
+          element={withLayout(ViewProposalPage, { scoped: true })}
+        />
+        <Route
+          path="/:scope/proposal/:identifier"
+          element={withLayout(ViewProposalPage, { scoped: true })}
+        />
+        <Route
+          path="/:scope/discussion/:identifier"
+          element={withLayout(ViewThreadPage, { scoped: true })}
+        />
+        <Route
+          path="/:scope/new/proposal/:type"
+          element={withLayout(NewProposalPage, { scoped: true })}
+        />
+        <Route
+          path="/:scope/new/proposal"
+          element={withLayout(NewProposalPage, { scoped: true })}
+        />
+        <Route
+          path="/:scope/treasury"
+          element={withLayout(TreasuryPage, { scoped: true })}
+        />
+        <Route
+          path="/:scope/bounties"
+          element={withLayout(BountiesPage, { scoped: true })}
+        />
+        <Route
+          path="/:scope/tips"
+          element={withLayout(TipsPage, { scoped: true })}
+        />
+        <Route
+          path="/:scope/validators"
+          element={withLayout(ValidatorsPage, { scoped: true })}
+        />
+        <Route
+          path="/:scope/admin"
+          element={withLayout(AdminPage, { scoped: true })}
+        />
+        <Route
+          path="/:scope/manage"
+          element={withLayout(ManageCommunityPage, { scoped: true })}
+        />
+        <Route
+          path="/:scope/spec_settings"
+          element={withLayout(SpecSettingsPage, { scoped: true })}
+        />
+        <Route
+          path="/:scope/analytics"
+          element={withLayout(StatsPage, { scoped: true })}
+        />
+        <Route
+          path="/:scope/snapshot/:snapshotId"
+          element={withLayout(SnapshotProposalsPage, { scoped: true })}
+        />
+        <Route
+          path="/:scope/multiple-snapshots"
+          element={withLayout(MultipleSnapshotsPage, { scoped: true })}
+        />
+        <Route
+          path="/:scope/snapshot/:snapshotId/:identifier"
+          element={withLayout(ViewSnapshotProposalPage, { scoped: true })}
+        />
+        <Route
+          path="/:scope/new/snapshot/:snapshotId"
+          element={withLayout(NewSnapshotProposalPage, { scoped: true })}
+        />
         <Route path="*" element={withLayout(PageNotFound, { scoped: false })} />
       </Routes>
     </Suspense>
---
+++ packages/commonwealth/client/scripts/views/pages/notifications/index.tsx
@@ -0,0 +1,258 @@
+/* @jsx jsx */
+import React from 'react';
+
+import {
+  ClassComponent,
+  ResultNode,
+  render,
+  setRoute,
+  getRoute,
+  getRouteParam,
+  redraw,
+  Component,
+  jsx,
+} from 'mithrilInterop';
+// import Infinite from 'mithril-infinite';
+
+import 'pages/notifications/index.scss';
+
+import app from 'state';
+import Sublayout from 'views/sublayout';
+import PageError from 'views/pages/error';
+import { PageLoading } from 'views/pages/loading';
+import { NotificationRow } from './notification_row';
+import { CWButton } from '../../components/component_kit/cw_button';
+import { CWPopover } from '../../components/component_kit/cw_popover/cw_popover';
+import { CWText } from '../../components/component_kit/cw_text';
+
+const MAX_NOTIFS = 40;
+
+let minDiscussionNotification = 0;
+let minChainEventsNotification = 0;
+let init = false;
+let pageKey = 0;
+
+const increment = (type: 'chain-event' | 'discussion') => {
+  if (type === 'chain-event') {
+    if (
+      app.user.notifications.chainEventNotifications.length >=
+      minChainEventsNotification + MAX_NOTIFS
+    ) {
+      minChainEventsNotification += MAX_NOTIFS;
+    }
+  } else if (type === 'discussion') {
+    if (
+      app.user.notifications.discussionNotifications.length >=
+      minDiscussionNotification + MAX_NOTIFS
+    ) {
+      minDiscussionNotification += MAX_NOTIFS;
+    }
+  }
+};
+
+const nextPage = () => {
+  const numChainEventNotif =
+    app.user.notifications.chainEventNotifications.length;
+  const numDiscussionNotif =
+    app.user.notifications.discussionNotifications.length;
+
+  if (numChainEventNotif < minChainEventsNotification + MAX_NOTIFS) {
+    app.user.notifications.getChainEventNotifications().then(() => {
+      increment('chain-event');
+      redraw();
+    });
+  } else {
+    increment('chain-event');
+    redraw();
+  }
+
+  if (numDiscussionNotif < minDiscussionNotification + MAX_NOTIFS) {
+    app.user.notifications.getDiscussionNotifications().then(() => {
+      increment('discussion');
+      redraw();
+    });
+  } else {
+    increment('discussion');
+    redraw();
+  }
+};
+
+const previousPage = () => {
+  let flag = false;
+  if (minChainEventsNotification >= MAX_NOTIFS) {
+    minChainEventsNotification -= MAX_NOTIFS;
+    flag = true;
+  } else if (minChainEventsNotification !== 0) {
+    minChainEventsNotification = 0;
+    flag = true;
+  }
+
+  if (minDiscussionNotification >= MAX_NOTIFS) {
+    minDiscussionNotification -= MAX_NOTIFS;
+    flag = true;
+  } else if (minDiscussionNotification !== 0) {
+    minDiscussionNotification = 0;
+    flag = true;
+  }
+  if (flag) redraw();
+};
+
+class NotificationsPage extends ClassComponent {
+  view() {
+    if (!app.isLoggedIn()) {
+      return <PageError message="This page requires you to be logged in." />;
+    }
+
+    const activeEntity = app.chain;
+
+    if (!activeEntity) {
+      return <PageLoading />;
+    }
+
+    const discussionNotifications =
+      app.user.notifications.discussionNotifications;
+    const chainEventNotifications =
+      app.user.notifications.chainEventNotifications;
+
+    return (
+      <Sublayout>
+        <div className="NotificationsPage">
+          <div className="notifications-buttons-row">
+            <CWButton
+              label="Previous Page"
+              onClick={(e) => {
+                e.preventDefault();
+                pageKey -= 1;
+                // console.log(
+                //   'Before=\t',
+                //   `ChainEvents: ${minChainEventsNotification}-${
+                //     minChainEventsNotification + MAX_NOTIFS
+                //   }, Discussion: ${minDiscussionNotification}-${
+                //     minDiscussionNotification + MAX_NOTIFS
+                //   }`
+                // );
+                previousPage();
+                // console.log(
+                //   'After=\t',
+                //   `ChainEvents: ${minChainEventsNotification}-${
+                //     minChainEventsNotification + MAX_NOTIFS
+                //   }, Discussion: ${minDiscussionNotification}-${
+                //     minDiscussionNotification + MAX_NOTIFS
+                //   }`
+                // );
+              }}
+            />
+            <CWButton
+              label="Next Page"
+              onClick={(e) => {
+                e.preventDefault();
+                pageKey += 1;
+
+                if (!init) {
+                  init = true;
+                  minDiscussionNotification =
+                    app.user.notifications.discussionNotifications.length;
+                  minChainEventsNotification =
+                    app.user.notifications.chainEventNotifications.length;
+                }
+                // console.log(
+                //   'Before=\t',
+                //   `ChainEvents: ${minChainEventsNotification}-${
+                //     minChainEventsNotification + MAX_NOTIFS
+                //   }, Discussion: ${minDiscussionNotification}-${
+                //     minDiscussionNotification + MAX_NOTIFS
+                //   }`
+                // );
+                nextPage();
+                // console.log(
+                //   'After=\t',
+                //   `ChainEvents: ${minChainEventsNotification}-${
+                //     minChainEventsNotification + MAX_NOTIFS
+                //   }, Discussion: ${minDiscussionNotification}-${
+                //     minDiscussionNotification + MAX_NOTIFS
+                //   }`
+                // );
+              }}
+            />
+            <CWButton
+              label="Mark all as read"
+              onClick={(e) => {
+                e.preventDefault();
+                app.user.notifications
+                  .markAsRead(
+                    discussionNotifications.concat(chainEventNotifications)
+                  )
+                  .then(() => redraw());
+              }}
+            />
+            <CWPopover
+              content={
+                <div>
+                  <div style={{ marginBottom: '10px' }}>
+                    Clear all chain notifications?
+                  </div>
+                  <CWButton
+                    label="Confirm"
+                    onClick={async (e) => {
+                      e.preventDefault();
+                      if (
+                        app.user.notifications.chainEventNotifications
+                          .length === 0
+                      )
+                        return;
+                      app.user.notifications
+                        .delete(app.user.notifications.chainEventNotifications)
+                        .then(() => redraw());
+                    }}
+                  />
+                </div>
+              }
+              trigger={<CWButton label="Clear chain events" />}
+            />
+          </div>
+          <div className="NotificationsList">
+            {(() => {
+              const discussionNotif = discussionNotifications.slice(
+                minDiscussionNotification,
+                minDiscussionNotification + MAX_NOTIFS
+              );
+
+              const chainEventNotif = chainEventNotifications.slice(
+                minChainEventsNotification,
+                minChainEventsNotification + MAX_NOTIFS
+              );
+
+              // TODO: sort this?
+              const allNotifications = discussionNotif.concat(chainEventNotif);
+
+              const totalLength = allNotifications.length;
+
+              if (totalLength > 0) {
+                // return m(Infinite, {
+                //   maxPages: 1, // prevents rollover/repeat
+                //   key: totalLength,
+                //   pageData: () => {
+                //     return allNotifications;
+                //   },
+                //   pageKey: () => {
+                //     return pageKey;
+                //   },
+                //   item: (data) => (
+                //     <NotificationRow notifications={[data]} onListPage />
+                //   ),
+                // });
+              } else
+                return (
+                  <div className="no-notifications">
+                    <CWText>No Notifications</CWText>
+                  </div>
+                );
+            })()}
+          </div>
+        </div>
+      </Sublayout>
+    );
+  }
+}
+
+export default NotificationsPage;
