b2af1fe2ae011e25decd77a2a1a598c8d86d9285
Richard Melkonian <r.v.melkonian@gmail.com>
2023-01-23 23:39:02 +0000

    add error cases for edgecases

--- packages/commonwealth/server/routes/getPermissions.ts
+++ packages/commonwealth/server/routes/getPermissions.ts
@@ -6,9 +6,11 @@ import { isAddressPermitted } from '../util/roles';
 
 export const Errors = {
   NeedChainId: 'Must provide a chain id to fetch',
+  NeedAuthor: 'Must provide an author to fetch',
   InvalidBody: 'Invalid Request Body',
   InvalidAction: 'Invalid Action sent in the request body',
-  InvalidActionNumber: 'The Action number provided does not match the Action enum',
+  InvalidActionNumber:
+    'The Action number provided does not match the Action enum',
 };
 
 export const getHasPermissions = async (
@@ -17,32 +19,40 @@ export const getHasPermissions = async (
   res: Response,
   next: NextFunction
 ) => {
-  if(!req.body){
-    throw new AppError(Errors.InvalidBody);
+  try {
+    if (!req.body) {
+      throw new AppError(Errors.InvalidBody);
+    }
+
+    const { action, chain, author } = req.body;
+    if (!action) {
+      throw new AppError(Errors.InvalidAction);
+    }
+
+    if (!chain) {
+      throw new AppError(Errors.NeedChainId);
+    }
+
+    if (!author) {
+      throw new AppError(Errors.NeedAuthor);
+    }
+
+    if (!Object.values(Action).includes(action)) {
+      throw new AppError(Errors.InvalidActionNumber);
+    }
+
+    const hasPermission = await isAddressPermitted(
+      models,
+      author.id,
+      chain.id,
+      action
+    );
+    if (!hasPermission) {
+      return res.status(405).json({ status: 'Success', result: false });
+    }
+
+    return res.json({ status: 'Success', result: true });
+  } catch (err) {
+    return next(new AppError(err));
   }
-
-  const { action } = req.body;
-  if (!action) {
-    throw new AppError(Errors.InvalidAction);
-  }
-
-  const { chain } = req.chain;
-  const { author } = req.address;
-
-  //check if the action matched a value in the Action enum
-  if (!Object.values(Action).includes(action)) {
-    throw new AppError(Errors.InvalidActionNumber);
-  }
-
-  const permission_error = await isAddressPermitted(
-    models,
-    author.id,
-    chain.id,
-    action
-  );
-
-
-
-
-
 };
