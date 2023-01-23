012c1e23c9be085b2e0e2ea084dccd04719dcf7e
zakhap <zhs2104@columbia.edu>
2023-01-23 17:51:32 -0500

    Transactionalize migration

--- packages/commonwealth/server/migrations/20230120162227-remove-waitlist-registrations.js
+++ packages/commonwealth/server/migrations/20230120162227-remove-waitlist-registrations.js
@@ -2,16 +2,20 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('WaitlistRegistrations');
+    return queryInterface.sequelize.transaction(async (transaction) => {
+      queryInterface.dropTable('WaitlistRegistrations', { transaction });
+    });
   },
 
   down: async (queryInterface, Sequelize) => {
-    return queryInterface.createTable('WaitlistRegistrations', {
-      user_id: { type: Sequelize.INTEGER, allowNull: false },
-      chain_id: { type: Sequelize.STRING, allowNull: false },
-      address: { type: Sequelize.STRING, allowNull: true },
-      created_at: { type: Sequelize.DATE, allowNull: false },
-      updated_at: { type: Sequelize.DATE, allowNull: false },
-    });
+    return queryInterface.sequelize.transaction(async (transaction) => {
+      queryInterface.createTable('WaitlistRegistrations', {
+        user_id: { type: Sequelize.INTEGER, allowNull: false },
+        chain_id: { type: Sequelize.STRING, allowNull: false },
+        address: { type: Sequelize.STRING, allowNull: true },
+        created_at: { type: Sequelize.DATE, allowNull: false },
+        updated_at: { type: Sequelize.DATE, allowNull: false },
+      }, { transaction });
+  });
   },
 };
