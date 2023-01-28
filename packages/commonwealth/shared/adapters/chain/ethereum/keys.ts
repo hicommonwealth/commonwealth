7e4800bb9707845a361c2ce63e830e20a27d14c2
Raymond Z <raykyri@gmail.com>
2023-01-27 23:40:28 -0500

    right return type

--- packages/commonwealth/shared/adapters/chain/ethereum/keys.ts
+++ packages/commonwealth/shared/adapters/chain/ethereum/keys.ts
@@ -1,15 +1,12 @@
-import { utils } from 'ethers';
-import type {
-  TypedDataDomain,
-  TypedDataField,
-} from '@ethersproject/abstract-signer';
+import { SessionPayload } from '@canvas-js/interfaces';
+import { TypedMessage, MessageTypes } from '@metamask/eth-sig-util';
 
 export const TEST_BLOCK_INFO_STRING =
   '{"number":1,"hash":"0x0f927bde6fb00940895178da0d32948714ea6e76f6374f03ffbbd7e0787e15bf","timestamp":1665083987891}';
 export const TEST_BLOCK_INFO_BLOCKHASH =
   '0x0f927bde6fb00940895178da0d32948714ea6e76f6374f03ffbbd7e0787e15bf';
 
-export const constructTypedCanvasMessage = (message) => {
+export const constructTypedCanvasMessage = (message: SessionPayload): TypedMessage<MessageTypes> => {
   // canvas implements ethers.js eip712 types, but
   // commonwealth uses web3.js which expects the
   // user to provide a valid EIP712Domain
