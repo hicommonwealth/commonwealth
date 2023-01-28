1faa0bf195de9ea28ae627898fc1b0fdbb3f03bf
Raymond Z <raykyri@gmail.com>
2023-01-27 23:10:22 -0500

    revert keys.ts, node can't import es modules

--- packages/commonwealth/shared/adapters/chain/ethereum/keys.ts
+++ packages/commonwealth/shared/adapters/chain/ethereum/keys.ts
@@ -1,31 +1,38 @@
-import type { SessionPayload } from '@canvas-js/interfaces';
+import { utils } from 'ethers';
 import type {
   TypedDataDomain,
   TypedDataField,
 } from '@ethersproject/abstract-signer';
-import { getSessionSignatureData } from '@canvas-js/chain-ethereum';
+import type { Block, SessionPayload } from '@canvas-js/interfaces';
 
 export const TEST_BLOCK_INFO_STRING =
   '{"number":1,"hash":"0x0f927bde6fb00940895178da0d32948714ea6e76f6374f03ffbbd7e0787e15bf","timestamp":1665083987891}';
 export const TEST_BLOCK_INFO_BLOCKHASH =
   '0x0f927bde6fb00940895178da0d32948714ea6e76f6374f03ffbbd7e0787e15bf';
 
-export const constructTypedCanvasMessage = (message: SessionPayload) => {
-  const [, ethersTypes, value]: [
-    unknown,
-    Record<string, TypedDataField[]>,
-    SessionPayload
-  ] = getSessionSignatureData(message);
-
-  // Commonwealth uses web3.js for `metamask_web_wallet` and
-  // `walletconnect_web_wallet`, so we reset and inject EIP712Domain
-  // into types:
-  const domain = { name: 'Commonwealth' };
-  const types = {
-    EIP712Domain: [{ name: 'name', type: 'string' }],
-    Message: ethersTypes.Message,
+export const constructTypedCanvasMessage = (message) => {
+  // construct the signature data from scratch, since canvas' implementation doesn't
+  // include an EIP712Domain
+  const domain: TypedDataDomain = {
+    name: 'Commonwealth',
+    salt: utils.hexlify(0),
   };
 
-  // Return types compatible with eth-sig-util.
-  return { types, primaryType: 'Message', domain, message };
+  const types: Record<string, TypedDataField[]> = {
+    EIP712Domain: [{ name: 'name', type: 'string' }],
+    Message: [
+      { name: 'app', type: 'string' },
+      { name: 'block', type: 'string' },
+      { name: 'chain', type: 'string' },
+      { name: 'chainId', type: 'string' },
+      { name: 'from', type: 'string' },
+      { name: 'sessionAddress', type: 'string' },
+      { name: 'sessionDuration', type: 'uint256' },
+      { name: 'sessionIssued', type: 'uint256' },
+    ],
+  };
+
+  // canvas uses ethers' signTypedData types while commonwealth uses eth-sig-util's
+  // so we have to coerce the types here
+  return { types, primaryType: 'Message', domain, message } as any;
 };
