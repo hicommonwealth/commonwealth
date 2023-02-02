2d12c153619d81b205f949c778550f95f399e5f8
Timothee Legros <timothee@ualberta.ca>
2023-02-02 14:44:57 -0700

    format

--- packages/commonwealth/server/migrations/20230202213211-update-edgeware-chainNode.js
+++ packages/commonwealth/server/migrations/20230202213211-update-edgeware-chainNode.js
@@ -2,14 +2,22 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    return queryInterface.bulkUpdate('ChainNodes', {
-      url: 'wss://edgeware.jelliedowl.net/'
-    }, {id: 45});
+    return queryInterface.bulkUpdate(
+      'ChainNodes',
+      {
+        url: 'wss://edgeware.jelliedowl.net/',
+      },
+      { id: 45 }
+    );
   },
 
   down: async (queryInterface, Sequelize) => {
-    return queryInterface.bulkUpdate('ChainNodes', {
-      url: 'wss://edgeware-rpc.dwellir.com'
-    }, {id: 45})
-  }
+    return queryInterface.bulkUpdate(
+      'ChainNodes',
+      {
+        url: 'wss://edgeware-rpc.dwellir.com',
+      },
+      { id: 45 }
+    );
+  },
 };
