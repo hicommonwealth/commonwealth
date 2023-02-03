34707e2ec477e90f63f4cfe40085314ac5ba2551
kassad <kurtis@commonwealth.im>
2023-02-03 06:40:23 -0800

    Hardcoded in production url to swagger docs

--- packages/commonwealth/server/routing/addSwagger.ts
+++ packages/commonwealth/server/routing/addSwagger.ts
@@ -1,11 +1,9 @@
 import type { Express } from 'express';
 import swaggerUi from 'swagger-ui-express';
 import externalSwagger from '../swagger/swagger_external.json';
-import { SERVER_URL } from '../config';
 
 export function addSwagger(endpoint: string, app: Express) {
-  const urlParts = SERVER_URL.split('://');
-  externalSwagger.host = urlParts[1];
-  externalSwagger.schemes = [urlParts[0]];
+  externalSwagger.host = 'commonwealth.im';
+  externalSwagger.schemes = ['http', 'https'];
   app.use(endpoint, swaggerUi.serve, swaggerUi.setup(externalSwagger));
 }
