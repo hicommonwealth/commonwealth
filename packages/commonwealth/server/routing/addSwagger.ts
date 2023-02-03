e17fd586dfc843315eb0c296692baaafa851bdba
kassad <kurtis@commonwealth.im>
2023-02-03 06:48:56 -0800

    Redirected certain URLs to be able to test swagger doc on staging environment

--- packages/commonwealth/server/routing/addSwagger.ts
+++ packages/commonwealth/server/routing/addSwagger.ts
@@ -1,9 +1,20 @@
 import type { Express } from 'express';
 import swaggerUi from 'swagger-ui-express';
+import { SERVER_URL } from '../config';
 import externalSwagger from '../swagger/swagger_external.json';
 
 export function addSwagger(endpoint: string, app: Express) {
-  externalSwagger.host = 'commonwealth.im';
-  externalSwagger.schemes = ['http', 'https'];
+
+  // redirect from commonwealthapp.herokuapp.com to commonwealth.im
+  const urlParts = SERVER_URL.split('://');
+  externalSwagger.host = urlParts[1];
+
+  if (externalSwagger.host.includes('commonwealthapp.herokuapp.com')) {
+    externalSwagger.host = 'comomonwealth.im';
+  } else if (externalSwagger.host.includes('commonwealth-staging2.herokuapp.com')) {
+    externalSwagger.host = 'affinity.fun';
+  }
+
+  externalSwagger.schemes = [urlParts[0]];
   app.use(endpoint, swaggerUi.serve, swaggerUi.setup(externalSwagger));
 }
