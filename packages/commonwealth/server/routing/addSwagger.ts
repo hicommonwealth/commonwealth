58b1c2a33930dce8fa4a17f6dc6af202b41033f8
kassad <kurtis@commonwealth.im>
2023-02-03 06:53:00 -0800

    Ran prettier

--- packages/commonwealth/server/routing/addSwagger.ts
+++ packages/commonwealth/server/routing/addSwagger.ts
@@ -4,14 +4,15 @@ import { SERVER_URL } from '../config';
 import externalSwagger from '../swagger/swagger_external.json';
 
 export function addSwagger(endpoint: string, app: Express) {
-
   // redirect from commonwealthapp.herokuapp.com to commonwealth.im
   const urlParts = SERVER_URL.split('://');
   externalSwagger.host = urlParts[1];
 
   if (externalSwagger.host.includes('commonwealthapp.herokuapp.com')) {
     externalSwagger.host = 'comomonwealth.im';
-  } else if (externalSwagger.host.includes('commonwealth-staging2.herokuapp.com')) {
+  } else if (
+    externalSwagger.host.includes('commonwealth-staging2.herokuapp.com')
+  ) {
     externalSwagger.host = 'affinity.fun';
   }
 
