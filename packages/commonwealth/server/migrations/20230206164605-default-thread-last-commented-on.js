6eebd71284683e3d774eeca535fb0fe6ef8032ed
kassad <kurtis@commonwealth.im>
2023-02-06 11:08:45 -0800

    Fixed migration script for default-thread-last-commented-on

--- packages/commonwealth/server/migrations/20230206164605-default-thread-last-commented-on.js
+++ packages/commonwealth/server/migrations/20230206164605-default-thread-last-commented-on.js
@@ -2,8 +2,14 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    return queryInterface.sequelize.query(`
+    await queryInterface.sequelize.query(`
 UPDATE "Threads" SET last_commented_on=created_at WHERE last_commented_on is NULL`);
+
+    await queryInterface.changeColumn('Threads', 'last_commented_on', {
+      type: Sequelize.DATE,
+      allowNull: false,
+      defaultValue: Sequelize.NOW,
+    });
   },
 
   down: async (queryInterface, Sequelize) => {
