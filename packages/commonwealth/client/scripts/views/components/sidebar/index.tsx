677dfea1efd2f2d6db48e3d12a933a551c8730fc
Gabriel Duquette <gabrielduquette@gmail.com>
2023-02-07 23:44:07 +0000

    help menu and admin section

--- packages/commonwealth/client/scripts/views/components/sidebar/index.tsx
+++ packages/commonwealth/client/scripts/views/components/sidebar/index.tsx
@@ -56,6 +56,19 @@ export class Sidebar extends ClassComponent {
         Action.VIEW_CHAT_CHANNELS
       );
 
+    const isAdmin =
+      app.user.isSiteAdmin ||
+      app.roles.isAdminOfEntity({
+        chain: app.activeChainId(),
+      });
+
+    const isMod = app.roles.isRoleOfCommunity({
+      role: 'moderator',
+      chain: app.activeChainId(),
+    });
+
+    const showAdmin = app.user && (isAdmin || isMod);
+
     return (
       <div className="Sidebar">
         {app.sidebarMenu === 'default' && (
@@ -63,7 +76,7 @@ export class Sidebar extends ClassComponent {
             <SidebarQuickSwitcher />
             {app.chain && (
               <div className="community-menu">
-                <AdminSection />
+                {showAdmin && <AdminSection />}
                 <DiscussionSection />
                 <GovernanceSection />
                 {/* app.socket && !hideChat && <ChatSection /> */}
