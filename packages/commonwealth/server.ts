4f76f0a28f1d551aceca718e8d6045f09dd2ba26
Jake Naviasky <jake@commonwealth.im>
2023-02-07 15:42:57 -0500

    Revert "Broke redirect loop between kong and cloudflare. Fixed CORS issue (#2654)" (#2693)
    This reverts commit 0a0b851dd01ebde3be4e49ba3bed69a4ccdee977.

--- packages/commonwealth/server.ts
+++ packages/commonwealth/server.ts
@@ -123,30 +123,14 @@ async function main() {
   });
 
   const setupMiddleware = () => {
-    // configure redirect to proper appname from herokuapp
+    // redirect from commonwealthapp.herokuapp.com to commonwealth.im
     app.all(/.*/, (req, res, next) => {
-      // go straight to herokuapp if traffic coming from disabled redirect url
-      if (req.ip.includes(process.env.DISABLE_REDIRECT_URL)) {
-        return next();
-      }
-
-      // do not redirect if heroku vars not set
       const host = req.header('host');
-      const { HEROKU_APP_NAME, SERVER_URL } = process.env;
-      if (
-        !HEROKU_APP_NAME ||
-        !SERVER_URL ||
-        SERVER_URL.includes(`${HEROKU_APP_NAME}.herokuapp.com`)
-      ) {
-        return next();
+      if (host.match(/commonwealthapp.herokuapp.com/i)) {
+        res.redirect(301, `https://commonwealth.im${req.url}`);
+      } else {
+        next();
       }
-
-      // redirect from appname.herokuapp.com to SERVER_URL (e.g. commonwealth.im) if present
-      if (host.match(`/${process.env.HEROKU_APP_NAME}.herokuapp.com/i`)) {
-        return res.redirect(301, `${process.env.SERVER_URL}${req.url}`);
-      }
-
-      return next();
     });
 
     // redirect to https:// unless we are using a test domain
@@ -156,6 +140,22 @@ async function main() {
 
     // dynamic compression settings used
     app.use(compression());
+
+    // static compression settings unused
+    // app.get('*.js', (req, res, next) => {
+    //   req.url = req.url + '.gz';
+    //   res.set('Content-Encoding', 'gzip');
+    //   res.set('Content-Type', 'application/javascript; charset=UTF-8');
+    //   next();
+    // });
+
+    // // static compression settings unused
+    // app.get('bundle.**.css', (req, res, next) => {
+    //   req.url = req.url + '.gz';
+    //   res.set('Content-Encoding', 'gzip');
+    //   res.set('Content-Type', 'text/css');
+    //   next();
+    // });
 
     // serve the compiled app
     if (!NO_CLIENT_SERVER) {
