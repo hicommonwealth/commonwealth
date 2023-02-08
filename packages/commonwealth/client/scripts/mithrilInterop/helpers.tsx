7e3bf79f9487e4b5983904ca4d74a13a7f4101a4
Daniel Trinh <danielhtrinh1@gmail.com>
2023-02-08 14:12:57 -0800

    Update helpers.tsx

--- packages/commonwealth/client/scripts/mithrilInterop/helpers.tsx
+++ packages/commonwealth/client/scripts/mithrilInterop/helpers.tsx
@@ -8,6 +8,12 @@ export function NavigationWrapper(Component) {
   };
 }
 
+// Fn is the function you want to debounce, delay is the amount of time in milliseconds
+// to wait before calling the function, and dependencies is an array of dependencies that
+// the hook should monitor for changes (if needed). The hook returns a debounced version of fn that is
+// called with a delay of milliseconds whenever the user scrolls. The hook uses the
+// useEffect hook to manage the debouncing.
+
 export const useDebounceOnFunction = (fn, delay, dependencies) => {
   let timeoutId;
 
