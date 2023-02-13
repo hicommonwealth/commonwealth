43f158945fcc2006bb5bfe2aa2836f462a6b789c
Gabriel Duquette <gabrielduquette@gmail.com>
2023-02-13 04:09:12 +0000

    simplify, use tabs

--- packages/commonwealth/client/scripts/views/pages/snapshot_proposals/index.tsx
+++ packages/commonwealth/client/scripts/views/pages/snapshot_proposals/index.tsx
@@ -1,75 +1,17 @@
 import React from 'react';
-
 import moment from 'moment';
-import type { SnapshotProposal } from 'helpers/snapshot_utils';
 
 import 'pages/snapshot_proposals.scss';
 
 import app from 'state';
 import Sublayout from 'views/sublayout';
+import type { SnapshotProposal } from 'helpers/snapshot_utils';
 import { CardsCollection } from '../../components/cards_collection';
-import { CWButton } from '../../components/component_kit/cw_button';
 import { CWText } from '../../components/component_kit/cw_text';
 import { SnapshotProposalCard } from './snapshot_proposal_card';
-import { PageLoading } from '../loading';
+import { CWTab, CWTabBar } from '../../components/component_kit/cw_tabs';
 import { MixpanelSnapshotEvents } from 'analytics/types';
 import { mixpanelBrowserTrack } from '../../../helpers/mixpanel_browser_util';
-
-export const ALL_PROPOSALS_KEY = 'COMMONWEALTH_ALL_PROPOSALS';
-
-enum SnapshotProposalFilter {
-  Core = 'Core',
-  Community = 'Community',
-  Active = 'Active',
-  Ended = 'Ended',
-}
-
-type SnapshotProposalStagesBarProps = {
-  onChangeFilter: (value: SnapshotProposalFilter) => void;
-};
-
-const SnapshotProposalStagesBar = (props: SnapshotProposalStagesBarProps) => {
-  const { onChangeFilter } = props;
-
-  return (
-    <div className="SnapshotProposalStagesBar">
-      {Object.values(SnapshotProposalFilter).map(
-        (option: SnapshotProposalFilter, i) => (
-          <CWButton
-            key={i}
-            disabled={
-              option === SnapshotProposalFilter.Core ||
-              option === SnapshotProposalFilter.Community
-            }
-            onClick={(e) => {
-              e.preventDefault();
-              onChangeFilter(option);
-            }}
-            label={option}
-          />
-        )
-      )}
-    </div>
-  );
-};
-
-const checkProposalByFilter = (
-  proposal: SnapshotProposal,
-  option: SnapshotProposalFilter
-) => {
-  switch (option) {
-    case SnapshotProposalFilter.Core:
-    case SnapshotProposalFilter.Community:
-      return true;
-    case SnapshotProposalFilter.Active:
-      return moment(+proposal.end * 1000) >= moment();
-    case SnapshotProposalFilter.Ended:
-      return moment(+proposal.end * 1000) < moment();
-    default:
-      break;
-  }
-  return true;
-};
 
 type SnapshotProposalsPageProps = {
   topic?: string;
@@ -79,49 +21,90 @@ type SnapshotProposalsPageProps = {
 const SnapshotProposalsPage = (props: SnapshotProposalsPageProps) => {
   const { snapshotId } = props;
 
-  const [proposals, setProposals] = React.useState<Array<SnapshotProposal>>([]);
-  const [selectedFilter, setSelectedFilter] =
-    React.useState<SnapshotProposalFilter>(SnapshotProposalFilter.Active);
+  const [currentTab, setCurrentTab] = React.useState<number>(1);
+  const [activeProposals, setActiveProposals] = React.useState<
+    Array<SnapshotProposal>
+  >([]);
+  const [endedProposals, setEndedProposals] = React.useState<
+    Array<SnapshotProposal>
+  >([]);
 
   React.useEffect(() => {
     const fetch = async () => {
       await app.snapshot.init(snapshotId);
 
       if (app.snapshot.initialized) {
-        setProposals(app.snapshot.proposals);
+        setActiveProposals(
+          app.snapshot.proposals.filter(
+            (proposal: SnapshotProposal) =>
+              moment(+proposal.end * 1000) >= moment()
+          )
+        );
+        setEndedProposals(
+          app.snapshot.proposals.filter(
+            (proposal: SnapshotProposal) =>
+              moment(+proposal.end * 1000) < moment()
+          )
+        );
       }
     };
 
     fetch();
-  }, [snapshotId]);
-
-  const onChangeFilter = (value: SnapshotProposalFilter) => {
-    setSelectedFilter(value);
-  };
+  }, []);
 
   return (
     <Sublayout>
       <div className="SnapshotProposalsPage">
-        <SnapshotProposalStagesBar onChangeFilter={onChangeFilter} />
-        {proposals.length > 0 ? (
-          <CardsCollection
-            content={proposals
-              .filter((proposal: SnapshotProposal) =>
-                checkProposalByFilter(proposal, selectedFilter)
-              )
-              .map((proposal, i) => (
+        <CWTabBar>
+          <CWTab
+            label="Active"
+            isSelected={currentTab === 1}
+            onClick={() => {
+              setCurrentTab(1);
+            }}
+          />
+          <CWTab
+            label="Ended"
+            isSelected={currentTab === 2}
+            onClick={() => {
+              setCurrentTab(2);
+            }}
+          />
+        </CWTabBar>
+        {currentTab === 1 ? (
+          activeProposals.length > 0 ? (
+            <CardsCollection
+              content={activeProposals.map((proposal, i) => (
                 <SnapshotProposalCard
                   key={i}
                   snapshotId={snapshotId}
                   proposal={proposal}
                 />
               ))}
-          />
-        ) : (
-          <CWText className="no-proposals-text">
-            No {selectedFilter.toLowerCase()} proposals found.
-          </CWText>
-        )}
+            />
+          ) : (
+            <CWText className="no-proposals-text">
+              No active proposals found.
+            </CWText>
+          )
+        ) : null}
+        {currentTab === 2 ? (
+          endedProposals.length > 0 ? (
+            <CardsCollection
+              content={endedProposals.map((proposal, i) => (
+                <SnapshotProposalCard
+                  key={i}
+                  snapshotId={snapshotId}
+                  proposal={proposal}
+                />
+              ))}
+            />
+          ) : (
+            <CWText className="no-proposals-text">
+              No active proposals found.
+            </CWText>
+          )
+        ) : null}
       </div>
     </Sublayout>
   );
