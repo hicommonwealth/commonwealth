603e82d2337dda4d59f9d9a0701a8e4523bcb911
Alex Young <alexyoung@alexs-m1-mbp.mynetworksettings.com>
2023-02-14 15:06:15 -0500

    prog

--- packages/commonwealth/client/scripts/controllers/server/notifications.ts
+++ packages/commonwealth/client/scripts/controllers/server/notifications.ts
@@ -384,7 +384,6 @@ class NotificationsController {
       this._subscriptions = [];
 
       const subs = result;
-      console.log({ subs });
       subs.forEach((sub) => this._subscriptions.push(modelFromServer(sub)));
     });
   }
