3e3d2342ea004891d2407dc68c06e7344c75a748
Gabriel Duquette <gabrielduquette@gmail.com>
2023-02-14 17:19:18 +0000

    use new hook

--- packages/commonwealth/client/scripts/views/pages/snapshot_proposals/snapshot_proposal_card.tsx
+++ packages/commonwealth/client/scripts/views/pages/snapshot_proposals/snapshot_proposal_card.tsx
@@ -9,7 +9,7 @@ import app from 'state';
 import { CWCard } from '../../components/component_kit/cw_card';
 import { CWText } from '../../components/component_kit/cw_text';
 import { ProposalTag } from '../../components/proposal_card/proposal_tag';
-import withRouter from 'navigation/helpers';
+import { useCommonNavigate } from 'navigation/helpers';
 
 type SnapshotProposalCardProps = {
   snapshotId: string;
@@ -18,48 +18,46 @@ type SnapshotProposalCardProps = {
 
 export const SnapshotProposalCard = (props: SnapshotProposalCardProps) => {
   const { proposal, snapshotId } = props;
+  const navigate = useCommonNavigate();
 
   const proposalLink = `/snapshot/${snapshotId}/${proposal.id}`;
 
   const time = moment(+proposal.end * 1000);
   const now = moment();
 
-    // TODO: display proposal.scores and proposal.scores_total on card
-    return (
-      <CWCard
-        elevation="elevation-2"
-        interactive={true}
-        className="ProposalCard"
-        onClick={(e) => {
-          e.stopPropagation();
-          e.preventDefault();
-          if (app.chain) {
-            localStorage[`${app.activeChainId()}-proposals-scrollY`] =
-              window.scrollY;
-            this.setRoute(proposalLink);
-          } else {
-            this.setRoute(proposalLink);
-          }
-        }}
-      >
-        <div className="proposal-card-metadata">
-          <ProposalTag
-            label={`${proposal.ipfs.slice(0, 6)}...${proposal.ipfs.slice(
-              proposal.ipfs.length - 6
-            )}`}
-          />
-          <CWText title={proposal.title} fontWeight="semiBold" noWrap>
-            {proposal.title}
-          </CWText>
-        </div>
-        <CWText>
-          {now > time
-            ? `Ended ${formatLastUpdated(time)}`
-            : `Ending in ${formatTimestamp(moment(+proposal.end * 1000))}`}
+  // TODO: display proposal.scores and proposal.scores_total on card
+  return (
+    <CWCard
+      elevation="elevation-2"
+      interactive={true}
+      className="ProposalCard"
+      onClick={(e) => {
+        e.stopPropagation();
+        e.preventDefault();
+        if (app.chain) {
+          localStorage[`${app.activeChainId()}-proposals-scrollY`] =
+            window.scrollY;
+          navigate(proposalLink);
+        } else {
+          navigate(proposalLink);
+        }
+      }}
+    >
+      <div className="proposal-card-metadata">
+        <ProposalTag
+          label={`${proposal.ipfs.slice(0, 6)}...${proposal.ipfs.slice(
+            proposal.ipfs.length - 6
+          )}`}
+        />
+        <CWText title={proposal.title} fontWeight="semiBold" noWrap>
+          {proposal.title}
         </CWText>
-      </CWCard>
-    );
-  }
-}
-
-export const SnapshotProposalCard = withRouter(SnapshotProposalCardComponent);
+      </div>
+      <CWText>
+        {now > time
+          ? `Ended ${formatLastUpdated(time)}`
+          : `Ending in ${formatTimestamp(moment(+proposal.end * 1000))}`}
+      </CWText>
+    </CWCard>
+  );
+};
