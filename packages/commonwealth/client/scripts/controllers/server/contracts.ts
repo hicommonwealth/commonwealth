3e510351f5ecf8e9dec22cd6b1530029f0ebbd4c
Daniel Trinh <danielhtrinh1@gmail.com>
2023-02-14 12:46:34 -0800

    Update contracts.ts

--- packages/commonwealth/client/scripts/controllers/server/contracts.ts
+++ packages/commonwealth/client/scripts/controllers/server/contracts.ts
@@ -5,6 +5,25 @@ import { Contract } from 'models';
 import app from 'state';
 import type { BalanceType, ContractType } from 'common-common/src/types';
 
+type AddCommunityContractTemplateAttributes = {
+  slug: string;
+  nickname: string;
+  display_name: string;
+  display_options: string;
+  contract_id: number;
+  community_id: string;
+  template_id: number;
+};
+
+type EditCommunityContractTemplateAttributes = {
+  cct_id: string;
+  slug: string;
+  nickname: string;
+  display_name: string;
+  display_options: string;
+  contract_id: number;
+};
+
 class ContractsController {
   private _store: ContractsStore = new ContractsStore();
   private _initialized = false;
@@ -86,6 +105,7 @@ class ContractsController {
       symbol,
       is_factory,
       nickname,
+      ccts,
     } = contractAttributes;
     this._store.add(
       new Contract({
@@ -101,8 +121,67 @@ class ContractsController {
         abi,
         isFactory: is_factory,
         nickname,
+        ccts,
       })
     );
+  }
+
+  public async updateTemplate({
+    contract_id,
+    cct_id,
+    cctmd,
+    isNewCCT,
+    template_id,
+    isDeletion = false,
+  }: {
+    contract_id: number;
+    cct_id: number;
+    cctmd: {
+      id: number;
+      slug: string;
+      nickname: string;
+      display_name: string;
+      display_options: string;
+    };
+    isNewCCT: boolean;
+    template_id?: number;
+    isDeletion?: boolean;
+  }) {
+    const currentContractInStore = this._store.getById(contract_id);
+    // TODO: Verify that this is a shallow copy situation
+    this._store.remove(this._store.getById(contract_id));
+
+    // Update the cctmd in the ccts array
+    if (!isNewCCT) {
+      let ccts = currentContractInStore.ccts.map((cct) => {
+        if (cct.id === cct_id) {
+          return {
+            ...cct,
+            cctmd: { ...cctmd },
+          };
+        } else {
+          return cct;
+        }
+      });
+
+      // reduce the ccts array if we're deleting
+      if (isDeletion) {
+        // remove the cct from the ccts array
+        ccts = ccts.filter((cct) => cct.id !== cct_id);
+      }
+
+      this._store.add(new Contract({ ...currentContractInStore, ccts }));
+      console.log('updated cctmd in ccts array', this._store);
+    } else {
+      const ccts = currentContractInStore.ccts || [];
+      ccts.push({
+        id: cct_id,
+        communityContractId: contract_id,
+        templateId: template_id,
+        cctmd: { ...cctmd },
+      });
+      this._store.add(new Contract({ ...currentContractInStore, ccts }));
+    }
   }
 
   public async add({
@@ -167,29 +246,255 @@ class ContractsController {
     this._store.add(result);
     return result;
   }
+
   public addToStore(contract: Contract) {
     return this._store.add(contract);
   }
 
-  public initialize(contracts = [], reset = true) {
+  // TODO there is similar function above => "add"
+  // I created new one which has less params
+  // Should we remove one of those functions? Which one we should keep?
+  public async addContractAndAbi({
+    chain_node_id,
+    address,
+    abi,
+  }: {
+    chain_node_id: number;
+    address: string;
+    abi?: string;
+  }) {
+    try {
+      const response = await $.post(`${app.serverUrl()}/contract`, {
+        community: app.activeChainId(),
+        chain_node_id,
+        jwt: app.user.jwt,
+        address,
+        abi,
+      });
+
+      const responseContract = response.result.contract;
+      const { id, type, is_factory } = responseContract;
+
+      let abiParsed;
+
+      try {
+        if (abi) {
+          abiParsed = JSON.parse(abi);
+        } else {
+          abiParsed = abi;
+        }
+      } catch (err) {
+        abiParsed = abi;
+      }
+
+      const result = new Contract({
+        id,
+        address,
+        chainNodeId: chain_node_id,
+        type,
+        abi: abiParsed,
+        isFactory: is_factory,
+      });
+
+      if (this._store.getById(result.id)) {
+        this._store.remove(this._store.getById(result.id));
+      }
+
+      this._store.add(result);
+    } catch (err) {
+      console.log('err', err);
+      throw new Error('Failed to add contract');
+    }
+  }
+
+  public async addTemplate({
+    name,
+    template,
+    contract_id,
+  }: {
+    name: string;
+    template: string;
+    contract_id: string;
+  }) {
+    try {
+      console.log('chain_id', app.activeChainId());
+      await $.post(`${app.serverUrl()}/contract/template`, {
+        jwt: app.user.jwt,
+        chain_id: app.activeChainId(),
+        name,
+        template,
+        contract_id,
+      });
+    } catch (err) {
+      console.log(err);
+      throw new Error('Failed to create template');
+    }
+  }
+
+  public async getTemplatesForContract(contractId: number) {
+    try {
+      const res = await $.get(`${app.serverUrl()}/contract/template`, {
+        jwt: app.user.jwt,
+        contract_id: contractId,
+      });
+      return res.result.templates;
+    } catch (e) {
+      console.log(e);
+      throw new Error('Failed to get templates');
+    }
+  }
+
+  public async addCommunityContractTemplate(
+    communityContractTemplateAndMetadata: AddCommunityContractTemplateAttributes
+  ) {
+    try {
+      const newContract = await $.post(
+        `${app.serverUrl()}/contract/community_template_and_metadata`,
+        {
+          ...communityContractTemplateAndMetadata,
+          chain_id: app.activeChainId(),
+          jwt: app.user.jwt,
+        }
+      );
+
+      this.updateTemplate({
+        contract_id: communityContractTemplateAndMetadata.contract_id,
+        cct_id: newContract.result.cct.id,
+        cctmd: newContract.result.metadata,
+        isNewCCT: true,
+        template_id: communityContractTemplateAndMetadata.template_id,
+      });
+    } catch (err) {
+      console.log(err);
+      throw new Error('Failed to add community contract template');
+    }
+  }
+
+  public async editCommunityContractTemplate(
+    communityContractTemplateMetadata: EditCommunityContractTemplateAttributes
+  ) {
+    try {
+      const updateContract = await $.ajax({
+        url: `${app.serverUrl()}/contract/community_template`,
+        data: {
+          ...communityContractTemplateMetadata,
+          jwt: app.user.jwt,
+        },
+        type: 'PUT',
+      });
+
+      this.updateTemplate({
+        contract_id: communityContractTemplateMetadata.contract_id,
+        cct_id: updateContract.result.cct.id,
+        cctmd: updateContract.result.metadata,
+        isNewCCT: false,
+      });
+    } catch (err) {
+      console.log(err);
+      throw new Error('Failed to save community contract template');
+    }
+  }
+
+  public async deleteCommunityContractTemplate(contract: {
+    contract_id: number;
+    template_id: number;
+    cctmd_id: number;
+  }) {
+    try {
+      const res = await $.ajax({
+        url: `${app.serverUrl()}/contract/community_template`,
+        data: {
+          ...contract,
+          jwt: app.user.jwt,
+        },
+        type: 'DELETE',
+      });
+
+      if (res.result.deletedContract) {
+        this._store.remove(this._store.getById(contract.contract_id));
+      } else {
+        this.updateTemplate({
+          contract_id: contract.contract_id,
+          cct_id: res.result.cct.id,
+          cctmd: res.result.metadata,
+          isNewCCT: false,
+          isDeletion: true,
+        });
+      }
+    } catch (err) {
+      console.log(err);
+      throw new Error('Failed to delete contract template');
+    }
+  }
+
+  public async deleteCommunityContract(contract: {
+    contract_id: number;
+    template_id?: number;
+  }) {
+    try {
+      const contractToDelete = this._store.getById(contract.contract_id);
+
+      await $.ajax({
+        url: `${app.serverUrl()}/contract/community_template?community_contract=true`,
+        data: {
+          ...contract,
+          jwt: app.user.jwt,
+        },
+        type: 'DELETE',
+      });
+
+      this._store.remove(contractToDelete);
+    } catch (err) {
+      console.log(err);
+      throw new Error('Failed to delete contract');
+    }
+  }
+
+  public initialize(contractsWithTemplates = [], reset = true) {
     if (reset) {
       this._store.clear();
     }
-    contracts.forEach((contract) => {
+    contractsWithTemplates.forEach((contractWithTemplate) => {
       try {
         let abiJson: Array<Record<string, unknown>>;
-        if (contract.ContractAbi) {
+        let ccts: Array<{
+          id: number;
+          communityContractId: number;
+          templateId: number;
+          cctmd: {
+            id: number;
+            slug: string;
+            nickname: string;
+            display_name: string;
+            display_options: string;
+          };
+        }>;
+        if (contractWithTemplate.contract.ContractAbi) {
           // Necessary because the contract abi was stored as a string in some contracts
-          if (typeof contract.ContractAbi.abi === 'string') {
-            abiJson = JSON.parse(contract.ContractAbi.abi);
+          if (
+            typeof contractWithTemplate.contract.ContractAbi.abi === 'string'
+          ) {
+            abiJson = JSON.parse(contractWithTemplate.contract.ContractAbi.abi);
           } else {
-            abiJson = contract.ContractAbi.abi;
+            abiJson = contractWithTemplate.contract.ContractAbi.abi;
           }
         }
+        if (contractWithTemplate.ccts) {
+          ccts = contractWithTemplate.ccts.map((cct) => {
+            return {
+              id: cct.id,
+              communityContractId: cct.community_contract_id,
+              templateId: cct.template_id,
+              cctmd: cct.CommunityContractTemplateMetadatum,
+            };
+          });
+        }
+
         this._store.add(
           Contract.fromJSON({
-            ...contract,
+            ...contractWithTemplate.contract,
             abi: abiJson,
+            ccts: ccts,
           })
         );
       } catch (e) {
