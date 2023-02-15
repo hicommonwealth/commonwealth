cc3398e6b14d92c653536d116fa2b484b1ffcc08
Gabriel Duquette <gabrielduquette@gmail.com>
2023-02-15 15:37:09 +0000

    style changes

--- packages/commonwealth/client/scripts/views/pages/discussions/DiscussionsPage.tsx
+++ packages/commonwealth/client/scripts/views/pages/discussions/DiscussionsPage.tsx
@@ -1,11 +1,12 @@
 import React, { useCallback, useEffect, useState } from 'react';
 import { Virtuoso } from 'react-virtuoso';
 import app from '../../../state';
-import { Footer } from '../../footer';
 import Sublayout from '../../sublayout';
 import { PageLoading } from '../loading';
 import { RecentThreadsHeader } from './recent_threads_header';
 import { ThreadPreview } from './thread_preview';
+
+import 'pages/discussions/index.scss';
 
 function DiscussionsPage({ topicName, stageName }) {
   const [threads, setThreads] = useState([]);
@@ -14,18 +15,23 @@ function DiscussionsPage({ topicName, stageName }) {
   // setup initial threads
   useEffect(() => {
     // set pinned threads
-    setThreads(app.threads.listingStore.getThreads({
-      topicName,
-      stageName,
-      pinned: true,
-    }));
-    app.threads.loadNextPage({ topicName, stageName }).then(() => {
-      // add unpinned threads
-      setThreads((oldThreads) => [...oldThreads, ...app.threads.listingStore.getThreads({
+    setThreads(
+      app.threads.listingStore.getThreads({
         topicName,
         stageName,
-        pinned: false,
-      })]);
+        pinned: true,
+      })
+    );
+    app.threads.loadNextPage({ topicName, stageName }).then(() => {
+      // add unpinned threads
+      setThreads((oldThreads) => [
+        ...oldThreads,
+        ...app.threads.listingStore.getThreads({
+          topicName,
+          stageName,
+          pinned: false,
+        }),
+      ]);
 
       setInitializing(false);
     });
@@ -42,33 +48,27 @@ function DiscussionsPage({ topicName, stageName }) {
   }, [threads]);
 
   if (initializing) {
-    return <PageLoading/>;
+    return <PageLoading />;
   }
-  return <Sublayout hideFooter={true}>
-    <Virtuoso
-      style={{ height: '100%', width: '100%' }}
-      data={threads}
-      itemContent={(i, thread) => {
-        return <ThreadPreview thread={thread} key={`${i}`}/>;
-      }}
-      endReached={loadMore}
-      overscan={200}
-      components={{
-        Header: () => {
-          return <div className="DiscussionsPage">
-            <RecentThreadsHeader
-            topic={topicName}
-            stage={stageName}
-            totalThreadCount={threads.length}
-          />
-          </div>;
-        },
-        Footer: () => {
-          return <Footer/>;
-        }
-      }}
-    />
-  </Sublayout>;
+  return (
+    <Sublayout>
+      <div className="DiscussionsPage">
+        <RecentThreadsHeader
+          topic={topicName}
+          stage={stageName}
+          totalThreadCount={threads.length}
+        />
+        <Virtuoso
+          style={{ height: '600px', width: '100%' }}
+          data={threads}
+          itemContent={(i, thread) => {
+            return <ThreadPreview thread={thread} key={`${i}`} />;
+          }}
+          endReached={loadMore}
+        />
+      </div>
+    </Sublayout>
+  );
 }
 
 export default DiscussionsPage;
