a6b5a12048c3b775d6259ecec274932a80615790
Timothee Legros <timothee@ualberta.ca>
2023-02-20 07:39:20 -0700

    added a jet engine to the CET removal migration

--- packages/commonwealth/server/migrations/20221226221226-delete-existing-ce-subs-notifs.js
+++ packages/commonwealth/server/migrations/20221226221226-delete-existing-ce-subs-notifs.js
@@ -3,6 +3,11 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.sequelize.transaction(async (t) => {
+      // disables foreign key checks i.e. triggers for the duration of the session (connection)
+      await queryInterface.sequelize.query(`
+        SET session_replication_role = 'replica';
+      `, {transaction: t});
+
       // delete chain-event notification reads that are associated with chain-event notifications
       await queryInterface.sequelize.query(
         `
@@ -43,6 +48,12 @@ module.exports = {
           type: queryInterface.sequelize.QueryTypes.DELETE,
         }
       );
+
+      // revert back to origin to re-enable foreign key checks for the session i.e. so following migrations have
+      // foreign key checks
+      await queryInterface.sequelize.query(`
+        SET session_replication_role = 'origin';
+      `, {transaction: t});
     });
   },
 
