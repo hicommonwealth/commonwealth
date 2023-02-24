fdbd413b0357143bac04fd3e7388a37bf21a4fb8
Marcin Maslanka <marcin@commonwealth.im>
2023-02-24 16:24:23 -0500

    Added select list component

--- packages/commonwealth/client/scripts/views/components/topic_selector.tsx
+++ packages/commonwealth/client/scripts/views/components/topic_selector.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import Select from 'react-select';
 
 import type { Topic } from 'models';
 
+import { SelectList } from 'views/components/component_kit/cw_select_list';
 import 'components/topic_selector.scss';
 
 interface TopicSelectorProps {
@@ -23,7 +23,7 @@ export const TopicSelector = ({
 }: TopicSelectorProps) => {
   const options = topics
     .filter((topic) => topic.featuredInSidebar)
-    .sort((a, b) => a.order -b.order)
+    .sort((a, b) => a.order - b.order)
     .map(topicToOption);
 
   const handleOnChange = ({ value }) => {
@@ -32,7 +32,7 @@ export const TopicSelector = ({
   };
 
   return (
-    <Select
+    <SelectList
       defaultValue={defaultTopic ? topicToOption(defaultTopic) : null}
       placeholder="Select the topic"
       isSearchable={false}
