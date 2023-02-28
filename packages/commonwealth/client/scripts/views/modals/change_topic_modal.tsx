7b39074ac9180c139f84b9ece1cf71faecc2b79e
Marcin Maslanka <marcin@commonwealth.im>
2023-02-28 14:41:18 +0100

    change topic modal

--- packages/commonwealth/client/scripts/views/modals/change_topic_modal.tsx
+++ packages/commonwealth/client/scripts/views/modals/change_topic_modal.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import 'modals/change_topic_modal.scss';
 import type { Thread, Topic } from 'models';
@@ -14,57 +14,54 @@ type ChangeTopicModalProps = {
   thread: Thread;
 };
 
-export const ChangeTopicModal = (props: ChangeTopicModalProps) => {
-  const { onChangeHandler, onModalClose, thread } = props;
+export const ChangeTopicModal = ({
+  onChangeHandler,
+  onModalClose,
+  thread,
+}: ChangeTopicModalProps) => {
+  const [activeTopic, setActiveTopic] = useState<Topic>(thread.topic);
+  const topics = app.topics.getByCommunity(app.activeChainId());
 
-  const [activeTopic, setActiveTopic] = React.useState<Topic>(thread.topic);
+  const handleSaveChanges = async () => {
+    try {
+      const topic: Topic = await app.topics.update(
+        thread.id,
+        activeTopic.name,
+        activeTopic.id
+      );
+
+      onChangeHandler(topic);
+      onModalClose();
+    } catch (err) {
+      console.log('Failed to update topic');
+
+      throw new Error(
+        err.responseJSON && err.responseJSON.error
+          ? err.responseJSON.error
+          : 'Failed to update topic'
+      );
+    }
+  };
 
   return (
     <div className="ChangeTopicModal">
       <div className="compact-modal-title">
         <h3>Change topic</h3>
-        <CWIconButton iconName="close" onClick={() => onModalClose()} />
+        <CWIconButton iconName="close" onClick={onModalClose} />
       </div>
       <div className="compact-modal-body">
         <TopicSelector
           defaultTopic={activeTopic}
-          topics={app.topics.getByCommunity(app.activeChainId())}
-          updateFormData={(topic: Topic) => {
-            onChangeHandler(topic);
-            setActiveTopic(topic);
-          }}
+          topics={topics}
+          onChange={setActiveTopic}
         />
         <div className="buttons-row">
           <CWButton
             buttonType="secondary-blue"
             label="Cancel"
-            onClick={() => {
-              onModalClose();
-            }}
+            onClick={onModalClose}
           />
-          <CWButton
-            label="Save changes"
-            onClick={async () => {
-              try {
-                const topic: Topic = await app.topics.update(
-                  thread.id,
-                  activeTopic.name,
-                  activeTopic.id
-                );
-
-                onChangeHandler(topic);
-                onModalClose();
-              } catch (err) {
-                console.log('Failed to update topic');
-
-                throw new Error(
-                  err.responseJSON && err.responseJSON.error
-                    ? err.responseJSON.error
-                    : 'Failed to update topic'
-                );
-              }
-            }}
-          />
+          <CWButton label="Save changes" onClick={handleSaveChanges} />
         </div>
       </div>
     </div>
--- packages/commonwealth/client/scripts/views/pages/discussions/thread_preview_menu.tsx
+++ packages/commonwealth/client/scripts/views/pages/discussions/thread_preview_menu.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import type { Dispatch, SetStateAction } from 'react';
+import React, { useState } from 'react';
 
 import { redraw } from 'mithrilInterop';
-import type { Thread, ThreadStage, Topic } from 'models';
+import type { Thread, ThreadStage } from 'models';
 import app from 'state';
 import { confirmationModalWithText } from '../../modals/confirm_modal';
 import { UpdateProposalStatusModal } from '../../modals/update_proposal_status_modal';
-import { ChangeTopicModal } from '../../modals/change_topic_modal';
 import { PopoverMenu } from '../../components/component_kit/cw_popover/cw_popover_menu';
 import { CWIconButton } from '../../components/component_kit/cw_icon_button';
 import { Modal } from '../../components/component_kit/cw_modal';
@@ -13,16 +13,17 @@ import { useCommonNavigate } from 'navigation/helpers';
 
 type ThreadPreviewMenuProps = {
   thread: Thread;
+  setIsChangeTopicModalOpen: Dispatch<SetStateAction<boolean>>;
 };
 
-export const ThreadPreviewMenu = (props: ThreadPreviewMenuProps) => {
-  const { thread } = props;
+export const ThreadPreviewMenu = ({
+  thread,
+  setIsChangeTopicModalOpen,
+}: ThreadPreviewMenuProps) => {
   const navigate = useCommonNavigate();
 
-  const [isChangeTopicModalOpen, setIsChangeTopicModalOpen] =
-    React.useState<boolean>(false);
   const [isUpdateProposalStatusModalOpen, setIsUpdateProposalStatusModalOpen] =
-    React.useState<boolean>(false);
+    useState(false);
 
   const hasAdminPermissions =
     app.user.activeAccount &&
@@ -87,10 +88,7 @@ export const ThreadPreviewMenu = (props: ThreadPreviewMenuProps) => {
             ...(hasAdminPermissions
               ? [
                   {
-                    onClick: (e) => {
-                      e.preventDefault();
-                      setIsChangeTopicModalOpen(true);
-                    },
+                    onClick: () => setIsChangeTopicModalOpen(true),
                     label: 'Change topic',
                     iconLeft: 'filter' as const,
                   },
@@ -139,20 +137,6 @@ export const ThreadPreviewMenu = (props: ThreadPreviewMenuProps) => {
           )}
         />
       </div>
-      <Modal
-        content={
-          <ChangeTopicModal
-            onChangeHandler={(topic: Topic) => {
-              thread.topic = topic;
-              redraw();
-            }}
-            thread={thread}
-            onModalClose={() => setIsChangeTopicModalOpen(false)}
-          />
-        }
-        onClose={() => setIsChangeTopicModalOpen(false)}
-        open={isChangeTopicModalOpen}
-      />
       <Modal
         content={
           <UpdateProposalStatusModal
