76935132c1a1dbf1a5370b750dd20ec4d0d0ceed
Kurtis Assad <kurtis@commonwealth.im>
2023-03-02 13:58:46 -0800

    Made ChainEntitiesController more efficient (#2630)
    * prettier updates
    
    * eslint updates save
    
    * save
    
    * save
    
    * save
    
    * update package.json
    
    * remove old procfiles + fix tsconfig
    
    * server tsconfig + global tsconfig
    
    * tsconfig inheritance from root tsconfig
    
    * update gitignore to ignore yarn-error.log
    
    * TBC tsconfig
    
    * check-types script in tbc package.json
    
    * remove test/tsconfig.test.json
    
    * save
    
    * update eslintrc
    
    * eslint fixes
    
    * stylelint fix
    
    * save
    
    * save
    
    * common-common eslint
    
    * common-common eslint fixes
    
    * common-common prettier config + call fix
    
    * common-common tsconfig updates
    
    * common-common prettier formatting
    
    * prettier + tsconfig fixes
    
    * fixed commonwealth types
    
    * CW eslint fixes part 1
    
    * eslint config update
    
    * CW eslint fixes part 2
    
    * chain-events eslint fixes part 1
    
    * chain-events linter fixes
    
    * chain-events formatting
    
    * chain-events type fixes
    
    * chain-events eslint config + fixes
    
    * snapshot-listener prettier format
    
    * TBC prettier format
    
    * TBC eslint config + fixes
    
    * eslint config fixes
    
    * chain-events eslint fixes
    
    * eslint fixes
    
    * updated gh action to v16
    
    * code quality gh action checks
    
    * fix cw tests
    
    * add install dependencies step to Code Quality Checks GH action
    
    * commonwealth package prettier format
    
    * fix CW type after prettier formatting
    
    * fix CW type after prettier formatting
    
    * run prettier
    
    * update actions to v3
    
    * GH CI fix
    
    * upgrade to node v18
    
    * update procfile with node options max_old_space_size
    
    * update CI to node v18
    
    * Added eslint rule to enforce "import type" style imports when possible.
    
    * Resolved merge conflicts
    
    * Made web controllers dynamically import
    
    * Reverted node change
    
    * Added prettier fixes
    
    * downgraded CI to use node 14
    
    * fixed eslint errors
    
    * Fixed remaining eslint warnings
    
    * Converted hardhat compiled types to use import type
    
    * Removed unused imports and added rule
    
    * Performed more code splitting
    
    * Fixed webpack issue
    
    * resolved merge conflicts
    
    * Fixed prettier warnings
    
    * Fixed eslint error
    
    * Code split terra imports
    
    * ran through prettier
    
    * ran through prettier and converted imports to typed imports
    
    * Moved css-minimizer-plugin to prod webpack config
    
    * Switched to lazy importing cosmos
    
    * Lazily loaded quill
    
    * Lazily loaded @snapshot-labs
    
    * Added deduplication plugin to webpack and split cosmos bundle
    
    * Lazy loaded most of ethereum
    
    * Lazy loaded solana
    
    * Replaced imports with type imports where possible
    
    * Fixed ethereum imports
    
    * Inline fonts
    
    * Removed web manifest
    
    * Fixed bugs
    
    * Ran prettier
    
    * fixed abi bug
    
    * Commented out cache groups that are eagerly loaded. Allows webpack to tree-shake imports
    
    * split webpack vendor into chunks to improve caching
    
    * deduplicated dependencies
    
    * Removed unused dependencies
    
    * Made the ChainEntitiesController more efficient
    
    * Fixed errors
    
    * Reverted yarn lock changes
    
    * Minor bug fix
    
    * Linter fixes.
    
    ---------
    
    Co-authored-by: Timothee Legros <timothee@ualberta.ca>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Jake Naviasky <jake@commonwealth.im>
    Co-authored-by: Muon Shot <120686579+CowMuon@users.noreply.github.com>

--- packages/commonwealth/client/scripts/stores/index.ts
+++ packages/commonwealth/client/scripts/stores/index.ts
@@ -13,7 +13,6 @@ export { default as ThreadUniqueAddressesCountStore } from './ThreadUniqueAddres
 export { default as Store } from './Store';
 export { default as TopicStore } from './TopicStore';
 export { default as RecentListingStore } from './RecentListingStore';
-export { default as ChainEntityStore } from './ChainEntityStore';
 export { default as PersistentStore } from './PersistentStore';
 
 export { UpdateType } from './interfaces';
