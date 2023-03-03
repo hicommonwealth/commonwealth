0351acc5eb051be09d1e1622e52838f2c56ca58d
Timothee Legros <timothee@ualberta.ca>
2023-03-03 01:52:25 -0700

    8 min local

--- packages/commonwealth/server/migrations/20221226221226-delete-existing-ce-subs-notifs.js
+++ packages/commonwealth/server/migrations/20221226221226-delete-existing-ce-subs-notifs.js
@@ -3,12 +3,27 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.sequelize.transaction(async (t) => {
+      // disable constraints for the duration of this transaction (speeds up migration)
+      await queryInterface.sequelize.query(`
+          CREATE INDEX idx_notifications_read_notification_id ON "NotificationsRead" (notification_id);
+      `, { transaction: t, raw: true });
+
+      await queryInterface.sequelize.query(`
+          CREATE INDEX idx_notifications_category_id ON "Notifications" (category_id);
+      `, { transaction: t, raw: true });
+
+      await queryInterface.sequelize.query(`
+          CREATE INDEX idx_subscriptions_category_id ON "Subscriptions" (category_id);
+      `, { transaction: t, raw: true });
+
+      console.log("Indexes created")
+
       // delete chain-event notification reads that are associated with chain-event notifications
       await queryInterface.sequelize.query(
         `
-        DELETE 
-        FROM "NotificationsRead"
-        WHERE notification_id IN (SELECT id FROM "Notifications" WHERE category_id = 'chain-event');
+          DELETE FROM "NotificationsRead" nr
+              USING "Notifications" n
+          WHERE nr.notification_id = n.id AND n.category_id = 'chain-event';
       `,
         {
           transaction: t,
@@ -16,6 +31,14 @@ module.exports = {
           type: queryInterface.sequelize.QueryTypes.DELETE,
         }
       );
+
+      console.log("NR done")
+
+      // await queryInterface.sequelize.query(`
+      //     DROP INDEX idx_notifications_category_id;
+      // `, { transaction: t, raw: true });
+      //
+      // console.log("Index dropped")
 
       // delete chain-event notifications
       await queryInterface.sequelize.query(
@@ -31,6 +54,8 @@ module.exports = {
         }
       );
 
+      console.log("N done")
+
       await queryInterface.sequelize.query(
         `
           DELETE
@@ -43,6 +68,16 @@ module.exports = {
           type: queryInterface.sequelize.QueryTypes.DELETE,
         }
       );
+
+      await queryInterface.sequelize.query(`
+          DROP INDEX idx_notifications_read_notification_id;
+      `, { transaction: t, raw: true });
+
+      await queryInterface.sequelize.query(`
+          DROP INDEX idx_subscriptions_category_id;
+      `, { transaction: t, raw: true });
+
+      console.log("Indexes dropped")
     });
   },
 
