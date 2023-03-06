f7fe65896748a755d85973e412473f8ac5e2bcfc
Marcin Maslanka <maslankam92@gmail.com>
2023-03-06 19:31:12 +0100

    Replace Query list (#3021)
    * Scaffold for query list
    
    * Make QueryList Generic
    
    * Use Querylist in Snapshot Proposal Selector
    
    * Use Querylist in chain entities selector
    
    ---------
    
    Co-authored-by: Marcin Maslanka <marcin@commonwealth.im>

---
+++ packages/commonwealth/client/scripts/views/components/chain_entities_selector/index.ts
@@ -0,0 +1,4 @@
+import { ChainEntitiesSelector } from './chain_entities_selector';
+import { ChainEntitiesSelectorItem } from './chain_entities_selector_item';
+
+export { ChainEntitiesSelector, ChainEntitiesSelectorItem };
