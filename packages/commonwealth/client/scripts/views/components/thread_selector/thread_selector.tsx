0f3146755196691adb257232eb10d348b003cc8b
Marcin Maslanka <marcin@commonwealth.im>
2023-03-06 13:46:26 +0100

    Make QueryList Generic

--- packages/commonwealth/client/scripts/views/components/thread_selector/thread_selector.tsx
+++ packages/commonwealth/client/scripts/views/components/thread_selector/thread_selector.tsx
@@ -1,16 +1,13 @@
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
 
 import 'components/thread_selector.scss';
 import { notifyError, notifySuccess } from 'controllers/app/notifications';
 import type { Thread } from 'models';
 import type { SearchParams } from 'models/SearchQuery';
 import app from 'state';
-import { CWSpinner } from './component_kit/cw_spinner';
-import { CWTextInput } from './component_kit/cw_text_input';
-import { Virtuoso } from 'react-virtuoso';
-import { formatAddressShort } from 'utils';
-import { CWText } from 'views/components/component_kit/cw_text';
-import { CWCheck } from 'views/components/component_kit/cw_icons/cw_icons';
+import { CWTextInput } from '../component_kit/cw_text_input';
+import { QueryList } from 'views/components/component_kit/cw_query_list';
+import { ThreadSelectorItem } from 'views/components/thread_selector/thread_selector_item';
 
 // The thread-to-thread relationship is comprised of linked and linking threads,
 // i.e. child and parent nodes.
@@ -20,40 +17,6 @@ type ThreadSelectorProps = {
   linkingThread: Thread;
 };
 
-interface ThreadPreviewProps {
-  linkedThreads: Array<Thread>;
-  thread: Thread;
-  onClick: (thread: Thread) => void;
-}
-
-const ThreadPreview = ({
-  linkedThreads,
-  thread,
-  onClick,
-}: ThreadPreviewProps) => {
-  const selected = linkedThreads.some((lt) => +lt.id === +thread.id);
-  const author = app.profiles.getProfile(thread.authorChain, thread.author);
-
-  return (
-    <div className="thread-preview-row" onClick={() => onClick(thread)}>
-      <div className="selected">{selected && <CWCheck />}</div>
-      <div>
-        <CWText fontWeight="medium" noWrap>
-          {thread.title}
-        </CWText>
-        <CWText type="caption">
-          {author.name
-            ? `${author.name} • ${formatAddressShort(thread.author)}`
-            : thread.author}
-        </CWText>
-      </div>
-    </div>
-  );
-};
-
-// todo simplify handler functions
-// todo make query list generic
-// todo make liking thread work (rerender properly on click and save)
 export const ThreadSelector = ({
   linkedThreads: linkedThreadsProp = [],
   linkingThread,
@@ -81,39 +44,38 @@ export const ThreadSelector = ({
   const options =
     showOnlyLinkedThreads && !queryLength ? linkedThreads : searchResults;
 
-  const EmptyComp = () => (
-    <div className="empty-component">{getEmptyContentMessage()}</div>
+  const handleClick = useCallback(
+    (thread: Thread) => {
+      const selectedThreadIdx = linkedThreads.findIndex(
+        (linkedThread) => linkedThread.id === thread.id
+      );
+
+      if (selectedThreadIdx !== -1) {
+        app.threads
+          .removeLinkedThread(linkingThread.id, thread.id)
+          .then(() => {
+            const newLinkedThreads = linkedThreads.splice(selectedThreadIdx, 1);
+            setLinkedThreads(newLinkedThreads);
+            notifySuccess('Thread unlinked.');
+          })
+          .catch(() => {
+            notifyError('Thread failed to unlink.');
+          });
+      } else {
+        app.threads
+          .addLinkedThread(linkingThread.id, thread.id)
+          .then(() => {
+            const newLinkedThreads = [...linkedThreads, thread];
+            setLinkedThreads(newLinkedThreads);
+            notifySuccess('Thread linked.');
+          })
+          .catch(() => {
+            notifyError('Thread failed to link.');
+          });
+      }
+    },
+    [linkedThreads, linkingThread?.id]
   );
-
-  const onClick = (thread: Thread) => {
-    const selectedThreadIdx = linkedThreads.findIndex(
-      (linkedThread) => linkedThread.id === thread.id
-    );
-
-    if (selectedThreadIdx !== -1) {
-      app.threads
-        .removeLinkedThread(linkingThread.id, thread.id)
-        .then(() => {
-          const newLinkedThreads = linkedThreads.splice(selectedThreadIdx, 1);
-          setLinkedThreads(newLinkedThreads);
-          notifySuccess('Thread unlinked.');
-        })
-        .catch(() => {
-          notifyError('Thread failed to unlink.');
-        });
-    } else {
-      app.threads
-        .addLinkedThread(linkingThread.id, thread.id)
-        .then(() => {
-          const newLinkedThreads = [...linkedThreads, thread];
-          setLinkedThreads(newLinkedThreads);
-          notifySuccess('Thread linked.');
-        })
-        .catch(() => {
-          notifyError('Thread failed to link.');
-        });
-    }
-  };
 
   const handleInputChange = (e) => {
     const target = e.target as HTMLInputElement;
@@ -155,6 +117,25 @@ export const ThreadSelector = ({
     setShowOnlyLinkedThreads(true);
   };
 
+  const renderItem = useCallback(
+    (i: number, thread: Thread) => {
+      const isSelected = linkedThreads.some((lt) => +lt.id === +thread.id);
+
+      return (
+        <ThreadSelectorItem
+          thread={thread}
+          onClick={handleClick}
+          isSelected={isSelected}
+        />
+      );
+    },
+    [handleClick, linkedThreads]
+  );
+
+  const EmptyComponent = () => (
+    <div className="empty-component">{getEmptyContentMessage()}</div>
+  );
+
   return (
     <div className="ThreadSelector">
       <CWTextInput
@@ -165,23 +146,12 @@ export const ThreadSelector = ({
         onInput={handleInputChange}
       />
 
-      <div className="results">
-        {loading ? (
-          <CWSpinner />
-        ) : (
-          <Virtuoso
-            data={options}
-            components={{ EmptyPlaceholder: EmptyComp }}
-            itemContent={(i, data) => (
-              <ThreadPreview
-                linkedThreads={linkedThreads}
-                thread={data}
-                onClick={onClick}
-              />
-            )}
-          />
-        )}
-      </div>
+      <QueryList
+        loading={loading}
+        options={options}
+        components={{ EmptyPlaceholder: EmptyComponent }}
+        renderItem={renderItem}
+      />
     </div>
   );
 };
