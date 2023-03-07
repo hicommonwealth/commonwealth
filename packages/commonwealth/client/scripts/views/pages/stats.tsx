aab266ee7c08c588c27340c211c05c6e65919d89
Gabriel Duquette <gabrielduquette@gmail.com>
2023-03-07 13:51:44 +0000

    break out helper, type stuff

--- packages/commonwealth/client/scripts/views/pages/stats.tsx
+++ packages/commonwealth/client/scripts/views/pages/stats.tsx
@@ -9,10 +9,89 @@ import { PageLoading } from 'views/pages/loading';
 import Sublayout from 'views/sublayout';
 import { CWText } from '../components/component_kit/cw_text';
 
+type Batchable = {
+  date: string;
+  new_items: string;
+};
+
+type Batchables = {
+  activeAccounts: Array<Batchable>;
+  comments: Array<Batchable>;
+  roles: Array<Batchable>;
+  threads: Array<Batchable>;
+};
+
+type TotalDataType = {
+  totalComments: number;
+  totalRoles: number;
+  totalThreads: number;
+};
+
+type Batched = {
+  day: number;
+  month: number;
+  twoWeek: number;
+  week: number;
+};
+
+type BatchedDataType = {
+  batchedActiveAccounts: Batched;
+  batchedComments: Batched;
+  batchedRoles: Batched;
+  batchedThreads: Batched;
+};
+
+const getBatched = ({
+  activeAccounts,
+  comments,
+  roles,
+  threads,
+}: Batchables) => {
+  const batchedComments = {};
+  const batchedRoles = {};
+  const batchedThreads = {};
+  const batchedActiveAccounts = {};
+  const c = comments.map((a) => Number(a.new_items));
+  const r = roles.map((a) => Number(a.new_items));
+  const t = threads.map((a) => Number(a.new_items));
+  const aa = activeAccounts.map((a) => Number(a.new_items));
+
+  // Comments
+  batchedComments['day'] = c.slice(0, 1).reduce((a, b) => a + b, 0);
+  batchedComments['week'] = c.slice(0, 7).reduce((a, b) => a + b, 0);
+  batchedComments['twoWeek'] = c.slice(0, 14).reduce((a, b) => a + b, 0);
+  batchedComments['month'] = c.slice(0, 28).reduce((a, b) => a + b, 0);
+
+  // Roles
+  batchedRoles['day'] = r.slice(0, 1).reduce((a, b) => a + b, 0);
+  batchedRoles['week'] = r.slice(0, 7).reduce((a, b) => a + b, 0);
+  batchedRoles['twoWeek'] = r.slice(0, 14).reduce((a, b) => a + b, 0);
+  batchedRoles['month'] = r.slice(0, 28).reduce((a, b) => a + b, 0);
+
+  // Threads
+  batchedThreads['day'] = t.slice(0, 1).reduce((a, b) => a + b, 0);
+  batchedThreads['week'] = t.slice(0, 7).reduce((a, b) => a + b, 0);
+  batchedThreads['twoWeek'] = t.slice(0, 14).reduce((a, b) => a + b, 0);
+  batchedThreads['month'] = t.slice(0, 28).reduce((a, b) => a + b, 0);
+
+  // Active Accounts
+  batchedActiveAccounts['day'] = aa.slice(0, 1).reduce((a, b) => a + b, 0);
+  batchedActiveAccounts['week'] = aa.slice(0, 7).reduce((a, b) => a + b, 0);
+  batchedActiveAccounts['twoWeek'] = aa.slice(0, 14).reduce((a, b) => a + b, 0);
+  batchedActiveAccounts['month'] = aa.slice(0, 28).reduce((a, b) => a + b, 0);
+
+  return {
+    batchedRoles,
+    batchedComments,
+    batchedThreads,
+    batchedActiveAccounts,
+  } as BatchedDataType;
+};
+
 const StatsPage = () => {
-  const [batchedData, setBatchedData] = useState<any>();
-  const [totalData, setTotalData] = useState<any>();
-  const [error, setError] = useState<string>();
+  const [batchedData, setBatchedData] = useState<BatchedDataType>();
+  const [totalData, setTotalData] = useState<TotalDataType>();
+  const [error, setError] = useState('');
 
   useEffect(() => {
     const fetch = async () => {
@@ -40,53 +119,14 @@ const StatsPage = () => {
           totalThreads: +totalThreads[0].new_items,
         });
 
-        const batchedComments = {};
-        const batchedRoles = {};
-        const batchedThreads = {};
-        const batchedActiveAccounts = {};
-        const c = comments.map((a) => Number(a.new_items));
-        const r = roles.map((a) => Number(a.new_items));
-        const t = threads.map((a) => Number(a.new_items));
-        const aa = activeAccounts.map((a) => Number(a.new_items));
-
-        // Comments
-        batchedComments['day'] = c.slice(0, 1).reduce((a, b) => a + b, 0);
-        batchedComments['week'] = c.slice(0, 7).reduce((a, b) => a + b, 0);
-        batchedComments['2week'] = c.slice(0, 14).reduce((a, b) => a + b, 0);
-        batchedComments['month'] = c.slice(0, 28).reduce((a, b) => a + b, 0);
-
-        // Roles
-        batchedRoles['day'] = r.slice(0, 1).reduce((a, b) => a + b, 0);
-        batchedRoles['week'] = r.slice(0, 7).reduce((a, b) => a + b, 0);
-        batchedRoles['2week'] = r.slice(0, 14).reduce((a, b) => a + b, 0);
-        batchedRoles['month'] = r.slice(0, 28).reduce((a, b) => a + b, 0);
-
-        // Threads
-        batchedThreads['day'] = t.slice(0, 1).reduce((a, b) => a + b, 0);
-        batchedThreads['week'] = t.slice(0, 7).reduce((a, b) => a + b, 0);
-        batchedThreads['2week'] = t.slice(0, 14).reduce((a, b) => a + b, 0);
-        batchedThreads['month'] = t.slice(0, 28).reduce((a, b) => a + b, 0);
-
-        // Active Accounts
-        batchedActiveAccounts['day'] = aa
-          .slice(0, 1)
-          .reduce((a, b) => a + b, 0);
-        batchedActiveAccounts['week'] = aa
-          .slice(0, 7)
-          .reduce((a, b) => a + b, 0);
-        batchedActiveAccounts['2week'] = aa
-          .slice(0, 14)
-          .reduce((a, b) => a + b, 0);
-        batchedActiveAccounts['month'] = aa
-          .slice(0, 28)
-          .reduce((a, b) => a + b, 0);
-
-        setBatchedData({
-          batchedRoles,
-          batchedComments,
-          batchedThreads,
-          batchedActiveAccounts,
-        });
+        setBatchedData(
+          getBatched({
+            comments,
+            roles,
+            threads,
+            activeAccounts,
+          })
+        );
       } catch (err) {
         if (err.responseJSON?.error) {
           setError(err.responseJSON.error);
@@ -142,10 +182,10 @@ const StatsPage = () => {
         </div>
         <div className="stat-row">
           <CWText>2 weeks</CWText>
-          <CWText>{batchedRoles['2week']}</CWText>
-          <CWText>{batchedComments['2week']}</CWText>
-          <CWText>{batchedThreads['2week']}</CWText>
-          <CWText>{batchedActiveAccounts['2week']}</CWText>
+          <CWText>{batchedRoles['twoWeek']}</CWText>
+          <CWText>{batchedComments['twoWeek']}</CWText>
+          <CWText>{batchedThreads['twoWeek']}</CWText>
+          <CWText>{batchedActiveAccounts['twoWeek']}</CWText>
         </div>
         <div className="stat-row">
           <CWText>1 month</CWText>
