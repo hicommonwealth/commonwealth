c7e3898381fd6ff64b36689587f017082f7f94a6
Timothee Legros <timothee@ualberta.ca>
2023-03-16 15:30:08 -0600

    txn

--- packages/commonwealth/server/migrations/20230316212013-revert-cet-migrations.js
+++ packages/commonwealth/server/migrations/20230316212013-revert-cet-migrations.js
@@ -26,7 +26,7 @@ module.exports = {
         type: Sequelize.STRING,
         allowNull: true,
         references: { model: 'ChainEntityMeta', key: 'id' },
-      });
+      }, { transaction: t });
     });
   },
 
