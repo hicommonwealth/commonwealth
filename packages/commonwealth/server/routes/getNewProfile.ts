e81c24faff5ae82789b31108e47b65a6ff72819e
kassad <kurtis@commonwealth.im>
2023-03-21 07:54:53 -0700

    Fixed test

--- packages/commonwealth/server/routes/getNewProfile.ts
+++ packages/commonwealth/server/routes/getNewProfile.ts
@@ -67,9 +67,6 @@ const getNewProfile = async (
 
   const comments = await models.Comment.findAll({
     where: {
-      root_id: {
-        [Op.like]: 'discussion%',
-      },
       address_id: {
         [Op.in]: addressIds,
       },
@@ -82,7 +79,7 @@ const getNewProfile = async (
 
   const commentThreadIds = [
     ...new Set<number>(
-      comments.map((c) => parseInt(c.root_id.replace('discussion_', ''), 10))
+      comments.map((c) => parseInt(c.thread_id, 10))
     ),
   ];
   const commentThreads = await models.Thread.findAll({
