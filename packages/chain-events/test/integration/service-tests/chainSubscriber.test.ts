a8050f3bcaafc3c2a680a92ef94643fad8e11292
Timothee Legros <timothee@ualberta.ca>
2023-03-25 17:04:07 -0600

    bravo tests

--- packages/chain-events/test/integration/service-tests/chainSubscriber.test.ts
+++ packages/chain-events/test/integration/service-tests/chainSubscriber.test.ts
@@ -24,6 +24,10 @@ describe('Integration tests for Compound Bravo', () => {
     ChainNode: { id: 1, url: 'http://localhost:8545' }
   }
   const sdk = new ChainTesting('http://127.0.0.1:3000');
+  let proposalId: string;
+
+  // This function delays the execution of the test for the specified number of milliseconds
+  const delay = (ms: number) => new Promise(res => setTimeout(res, ms));
 
   describe('Tests the Bravo event listener using the chain subscriber', () => {
     before(async () => {
@@ -35,16 +39,57 @@ describe('Integration tests for Compound Bravo', () => {
     });
 
     it('Should capture proposal created events', async () => {
-      await sdk.createProposal(1);
+      proposalId = await sdk.createProposal(1);
+
+      await delay(3000);
 
       // verify the event was created and appended to the correct queue
-      expect(rmq.queuedMessages[RascalSubscriptions.ChainEvents].length).to.equal(0);
+      console.log(rmq.queuedMessages[RascalSubscriptions.ChainEvents]);
+      expect(rmq.queuedMessages[RascalSubscriptions.ChainEvents].length).to.equal(1);
     });
 
-    it('Should capture votes on the created proposal');
-    it('Should capture delegation events');
+    it('Should capture votes on the created proposal', async () => {
+      await sdk.castVote(proposalId, 1, false);
+
+      await delay(3000);
+
+      // verify the event was created and appended to the correct queue
+      console.log(rmq.queuedMessages[RascalSubscriptions.ChainEvents]);
+      expect(rmq.queuedMessages[RascalSubscriptions.ChainEvents].length).to.equal(2);
+    });
+
+    it('Should capture proposal executed events', async () => {
+      await sdk.executeProposal(proposalId);
+
+      await delay(3000);
+
+      // verify the event was created and appended to the correct queue
+      console.log(rmq.queuedMessages[RascalSubscriptions.ChainEvents]);
+      expect(rmq.queuedMessages[RascalSubscriptions.ChainEvents].length).to.equal(3);
+    });
+
+    it('Should capture proposal cancelled events', async () => {
+      const proposalIdToCancel = await sdk.createProposal(1);
+      await sdk.cancelProposal(proposalIdToCancel);
+
+      await delay(3000);
+
+      // verify the event was created and appended to the correct queue
+      console.log(rmq.queuedMessages[RascalSubscriptions.ChainEvents]);
+      expect(rmq.queuedMessages[RascalSubscriptions.ChainEvents].length).to.equal(5);
+    });
+
+    it('Should capture proposal queued events', async () => {
+      const proposalIdToQueue = await sdk.createProposal(1);
+      await sdk.queueProposal(proposalIdToQueue);
+
+      await delay(3000);
+      // verify the event was created and appended to the correct queue
+      console.log(rmq.queuedMessages[RascalSubscriptions.ChainEvents]);
+      expect(rmq.queuedMessages[RascalSubscriptions.ChainEvents].length).to.equal(7);
+    });
   })
 
-  describe('Tests for processing Bravo events with the consumer', async () => {});
-  describe('Tests for retrieving Bravo events with the app', async () => {})
+  describe.skip('Tests for processing Bravo events with the consumer', async () => {});
+  describe.skip('Tests for retrieving Bravo events with the app', async () => {})
 })
