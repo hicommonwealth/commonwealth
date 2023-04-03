e57d70be6f2d8bfcb9bd5d8f72dcef3a49b5fd7d
Timothee Legros <timothee@ualberta.ca>
2023-04-02 18:10:51 -0600

    add thread_id column to notifications

---
+++ packages/commonwealth/server/migrations/20230402232942-add-thread-id-to-notif.js
@@ -0,0 +1,25 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.sequelize.transaction(async (t) => {
+      await queryInterface.addColumn('Notifications', 'thread_id', {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+        references: { model: 'Threads', key: 'id' },
+      }, { transaction: t });
+
+      await queryInterface.sequelize.query(`
+        UPDATE "Notifications"
+        SET thread_id = (notification_data::jsonb->>'thread_id')::INTEGER
+        WHERE notification_data::jsonb->>'thread_id' IS NOT NULL;
+      `, { raw: true, transaction: t });
+
+      await queryInterface.addIndex('Notifications', ['thread_id'], { transaction: t });
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.removeColumn('Notifications', 'thread_id');
+  }
+};
