7ee34f53089509c8b4c91f0c91d080cf47766691
Timothee Legros <timothee@ualberta.ca>
2023-04-02 18:25:39 -0600

    update viewUserActivity query

--- packages/commonwealth/server/migrations/20230402232942-add-thread-id-to-notif.js
+++ packages/commonwealth/server/migrations/20230402232942-add-thread-id-to-notif.js
@@ -3,23 +3,33 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.sequelize.transaction(async (t) => {
-      await queryInterface.addColumn('Notifications', 'thread_id', {
-        type: Sequelize.INTEGER,
-        allowNull: true,
-        references: { model: 'Threads', key: 'id' },
-      }, { transaction: t });
+      await queryInterface.addColumn(
+        'Notifications',
+        'thread_id',
+        {
+          type: Sequelize.INTEGER,
+          allowNull: true,
+          references: { model: 'Threads', key: 'id' },
+        },
+        { transaction: t }
+      );
 
-      await queryInterface.sequelize.query(`
+      await queryInterface.sequelize.query(
+        `
         UPDATE "Notifications"
         SET thread_id = (notification_data::jsonb->>'thread_id')::INTEGER
         WHERE notification_data::jsonb->>'thread_id' IS NOT NULL;
-      `, { raw: true, transaction: t });
+      `,
+        { raw: true, transaction: t }
+      );
 
-      await queryInterface.addIndex('Notifications', ['thread_id'], { transaction: t });
+      await queryInterface.addIndex('Notifications', ['thread_id'], {
+        transaction: t,
+      });
     });
   },
 
   down: async (queryInterface, Sequelize) => {
     await queryInterface.removeColumn('Notifications', 'thread_id');
-  }
+  },
 };
--- packages/commonwealth/server/routes/viewUserActivity.ts
+++ packages/commonwealth/server/routes/viewUserActivity.ts
@@ -18,34 +18,34 @@ export default async (
   const { id } = req.user;
 
   const query = `
-    SELECT nt.thread_id, nts.created_at as last_activity, nts.notification_data, nts.category_id,
-      MAX(thr.view_count) as view_count,
-      COUNT(DISTINCT oc.id) AS comment_count,
-      COUNT(DISTINCT tr.id) + COUNT(DISTINCT cr.id) AS reaction_count
-    FROM
-      (SELECT nnn.mx_not_id, nnn.thread_id,
-          ROW_NUMBER() OVER (ORDER BY mx_not_id DESC) as thread_rank
-        FROM
-        (SELECT DISTINCT nn.thread_id, nn.mx_not_id
-          FROM (SELECT (n.notification_data::jsonb->>'thread_id') AS thread_id,
-                  MAX(n.id) OVER (PARTITION BY (n.notification_data::jsonb->>'thread_id')) AS mx_not_id
-                FROM "Notifications" n
-                WHERE n.category_id IN('new-thread-creation','new-comment-creation')
-                  AND n.chain_id IN(SELECT a."chain" FROM "Addresses" a WHERE a.user_id = ?)
-                ORDER BY id DESC
-                FETCH FIRST 500 ROWS ONLY
-                ) nn
-          ) nnn
-      ) nt
-    INNER JOIN "Notifications" nts ON nt.mx_not_id = nts.id
-    LEFT JOIN "Comments" oc ON nt.thread_id = CAST(oc.thread_id AS VARCHAR)
-      --TODO: eval execution path with alternate aggregations
-    LEFT JOIN "Reactions" tr ON nt.thread_id = CAST(tr.thread_id AS VARCHAR)
-    LEFT JOIN "Reactions" cr ON oc.id = cr.comment_id
-    LEFT JOIN "Threads" thr ON thr.id = CAST(nt.thread_id AS int)
-    WHERE nt.thread_rank <= 50
-    GROUP BY nt.thread_id, nts.created_at, nts.notification_data, nts.category_id
-    ORDER BY nts.created_at DESC;
+      SELECT nt.thread_id, nts.created_at as last_activity, nts.notification_data, nts.category_id,
+             MAX(thr.view_count) as view_count,
+             COUNT(DISTINCT oc.id) AS comment_count,
+             COUNT(DISTINCT tr.id) + COUNT(DISTINCT cr.id) AS reaction_count
+      FROM
+          (SELECT nnn.mx_not_id, nnn.thread_id,
+                  ROW_NUMBER() OVER (ORDER BY mx_not_id DESC) as thread_rank
+           FROM
+               (SELECT DISTINCT nn.thread_id, nn.mx_not_id
+                FROM (SELECT n.thread_id AS thread_id,
+                             MAX(n.id) OVER (PARTITION BY n.thread_id) AS mx_not_id
+                      FROM "Notifications" n
+                      WHERE n.category_id IN('new-thread-creation','new-comment-creation')
+                        AND n.chain_id IN(SELECT a."chain" FROM "Addresses" a WHERE a.user_id = ?)
+                      ORDER BY id DESC
+                          FETCH FIRST 500 ROWS ONLY
+                     ) nn
+               ) nnn
+          ) nt
+              INNER JOIN "Notifications" nts ON nt.mx_not_id = nts.id
+              LEFT JOIN "Comments" oc ON nt.thread_id = oc.thread_id
+              --TODO: eval execution path with alternate aggregations
+              LEFT JOIN "Reactions" tr ON nt.thread_id = tr.thread_id
+              LEFT JOIN "Reactions" cr ON oc.id = cr.comment_id
+              LEFT JOIN "Threads" thr ON thr.id = nt.thread_id
+      WHERE nt.thread_rank <= 50
+      GROUP BY nt.thread_id, nts.created_at, nts.notification_data, nts.category_id
+      ORDER BY nts.created_at DESC;
   `;
 
   const notifications: any = await models.sequelize.query(query, {
