b19028c5eaddf4194033af317666c76a829c41d2
Malik Zulqurnain <malikzulqurnain2121@gmail.com>
2023-04-10 22:31:17 +0500

    Make poll duration selector overflow poll modal

--- packages/commonwealth/client/scripts/views/pages/view_thread/poll_cards.tsx
+++ packages/commonwealth/client/scripts/views/pages/view_thread/poll_cards.tsx
@@ -49,6 +49,7 @@ export const ThreadPollEditorCard = ({
         }
       />
       <Modal
+        className="PollEditorCardModal"
         content={
           <PollEditorModal
             thread={thread}
