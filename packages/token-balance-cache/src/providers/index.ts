91b3c5b8e98278a9aa7dad3b5d840c6ff8639741
ianrowan <i.rowan299@gmail.com>
2023-04-11 16:06:52 -0500

    get changes from other branch(evm tbc consolidation)

--- packages/token-balance-cache/src/providers/index.ts
+++ packages/token-balance-cache/src/providers/index.ts
@@ -1,7 +1,4 @@
 import { default as CosmosBalanceProvider } from './cosmos';
-import Erc1155BalanceProvider from './erc1155';
-import Erc20BalanceProvider from './erc20';
-import Erc721BalanceProvider from './erc721';
 import { default as EthTokenBalanceProvider } from './ethToken';
 import { default as RoninBalanceProvider } from './ronin';
 import { default as SplTokenBalanceProvider } from './splToken';
@@ -10,9 +7,6 @@ import { default as TerraBalanceProvider } from './terra';
 const providers = [
   new CosmosBalanceProvider(),
   new EthTokenBalanceProvider(),
-  new Erc20BalanceProvider(),
-  new Erc721BalanceProvider(),
-  new Erc1155BalanceProvider(),
   new RoninBalanceProvider(),
   new SplTokenBalanceProvider(),
   new TerraBalanceProvider(),
