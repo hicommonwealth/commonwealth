b9b6a0b43a5f49944375ba8700699763eefaf2c9
Ryan Bennett <126922418+rbennettcw@users.noreply.github.com>
2023-04-17 12:22:35 -0700

    Implement unscoped search (#3431)
    * fix unscoped search for threads and comments
    
    * prevent usage of all_chains as ID for creating or updating a chain
    
    * format files
    
    * tweaks
    
    * fix search scope and click handling
    
    * member search WIP
    
    * simplify and optimize queries
    
    * fix scroll to comment
    
    * revert sidebar placeholder
    
    * fix memberes page
    
    * apply feedback
    
    * add click event to member container
    
    * remove unused onClick prop
    
    * typecheck fix
    
    * typecheck fix again
    
    ---------
    
    Co-authored-by: Muon Shot <120686579+CowMuon@users.noreply.github.com>

--- packages/commonwealth/server/routes/searchDiscussions.ts
+++ packages/commonwealth/server/routes/searchDiscussions.ts
@@ -1,14 +1,118 @@
 /* eslint-disable quotes */
-import { AppError, ServerError } from 'common-common/src/errors';
+import { AppError } from 'common-common/src/errors';
 import type { NextFunction, Request, Response } from 'express';
 import { Op, QueryTypes } from 'sequelize';
+
 import type { DB } from '../models';
+import { ChainInstance } from '../models/chain';
+import { ALL_CHAINS } from '../middleware/databaseValidationService';
+
+const MIN_SEARCH_QUERY_LENGTH = 4;
 
 const Errors = {
   UnexpectedError: 'Unexpected error',
   QueryMissing: 'Must enter query to begin searching',
   QueryTooShort: 'Query must be at least 4 characters',
-  NoChain: 'Title search must be chain-scoped',
+  NoChains: 'No chains resolved to execute search',
+};
+
+type SearchDiscussionsQuery = {
+  search?: string;
+  thread_title_only?: 'true' | 'false';
+  chain?: string;
+  results_size?: string;
+  sort?: string;
+};
+
+type SearchDiscussionsBindOptions = {
+  chain?: string;
+  searchTerm?: string;
+  limit?: number;
+};
+
+const search = async (
+  models: DB,
+  chain: ChainInstance | null,
+  options: SearchDiscussionsQuery
+) => {
+  if (options.thread_title_only === 'true') {
+    const encodedSearchTerm = encodeURIComponent(options.search);
+    const params: any = {
+      title: {
+        [Op.or]: [
+          { [Op.iLike]: `%${encodedSearchTerm}%` },
+          { [Op.iLike]: `%${options.search}%` },
+        ],
+      },
+    };
+    if (chain) {
+      params.chain = chain.id;
+    }
+
+    const threads = await models.Thread.findAll({
+      where: params,
+      limit: parseInt(options.results_size, 10) || 20,
+      attributes: {
+        exclude: ['body', 'plaintext', 'version_history'],
+      },
+      include: [
+        {
+          model: models.Address,
+          as: 'Address',
+        },
+      ],
+    });
+
+    return threads;
+  }
+
+  const bind: SearchDiscussionsBindOptions = {
+    searchTerm: options.search,
+    limit: 50, // must be same as SEARCH_PAGE_SIZE on frontend
+  };
+  if (chain) {
+    bind.chain = chain.id;
+  }
+
+  const sortOption =
+    options.sort === 'Newest'
+      ? '"Threads".created_at DESC'
+      : options.sort === 'Oldest'
+      ? '"Threads".created_at ASC'
+      : 'rank DESC';
+  const sort = `ORDER BY ${sortOption}`;
+
+  const chainWhere = bind.chain ? '"Threads".chain = $chain AND' : '';
+
+  const threads = await models.sequelize.query(
+    `
+    SELECT
+        "Threads".title,
+        "Threads".body,
+        CAST("Threads".id as VARCHAR) as proposalId,
+        'thread' as type,
+        "Addresses".id as address_id,
+        "Addresses".address,
+        "Addresses".chain as address_chain,
+        "Threads".created_at,
+        "Threads".chain,
+        ts_rank_cd("Threads"._search, query) as rank
+      FROM "Threads"
+      JOIN "Addresses" ON "Threads".address_id = "Addresses".id,
+      websearch_to_tsquery('english', $searchTerm) as query
+      WHERE
+        ${chainWhere}
+        "Threads".deleted_at IS NULL AND
+        query @@ "Threads"._search
+      ${sort} LIMIT $limit
+  `,
+    {
+      bind,
+      type: QueryTypes.SELECT,
+    }
+  );
+
+  return threads;
 };
 
 const searchDiscussions = async (
@@ -17,110 +121,31 @@ const searchDiscussions = async (
   res: Response,
   next: NextFunction
 ) => {
-  let bind = {};
-
-  const chain = req.chain;
-
-  if (!req.query.search) {
-    return next(new AppError(Errors.QueryMissing));
-  }
-  if (req.query.search.length < 4) {
-    return next(new AppError(Errors.QueryTooShort));
-  }
-
-  if (req.query.thread_title_only === 'true') {
-    if (!req.query.chain) {
-      return next(new AppError(Errors.NoChain));
-    }
-    const encodedSearchTerm = encodeURIComponent(req.query.search);
-    const params = {
-      chain: chain.id,
-      title: {
-        [Op.or]: [
-          { [Op.iLike]: `%${encodedSearchTerm}%` },
-          { [Op.iLike]: `%${req.query.search}%` },
-        ],
-      },
-    };
-
-    try {
-      const threads = await models.Thread.findAll({
-        where: params,
-        limit: req.query.results_size || 20,
-        attributes: {
-          exclude: ['body', 'plaintext', 'version_history'],
-        },
-        include: [
-          {
-            model: models.Address,
-            as: 'Address',
-          },
-        ],
-      });
-      return res.json({
-        status: 'Success',
-        result: threads,
-      });
-    } catch (e) {
-      console.log(e);
-      return next(new ServerError(Errors.UnexpectedError));
-    }
-  }
-
-  // Community-scoped search
-  let communityOptions = '';
-  if (req.query.chain) {
-    // set up query parameters
-    communityOptions = `AND "Threads".chain = $chain `;
-    bind = { chain: chain.id };
-  }
-
-  const sort =
-    req.query.sort === 'Newest'
-      ? 'ORDER BY "Threads".created_at DESC'
-      : req.query.sort === 'Oldest'
-      ? 'ORDER BY "Threads".created_at ASC'
-      : 'ORDER BY rank DESC';
-
-  bind['searchTerm'] = req.query.search;
-  bind['limit'] = 50; // must be same as SEARCH_PAGE_SIZE on frontend
-
-  // query for both threads and comments, and then execute a union and keep only the most recent :limit
-  let threadsAndComments;
   try {
-    threadsAndComments = await models.sequelize.query(
-      `
-  SELECT
-      "Threads".title,
-      "Threads".body,
-      CAST("Threads".id as VARCHAR) as proposalId,
-      'thread' as type,
-      "Addresses".id as address_id,
-      "Addresses".address,
-      "Addresses".chain as address_chain,
-      "Threads".created_at,
-      "Threads".chain,
-      ts_rank_cd("Threads"._search, query) as rank
-    FROM "Threads"
-    JOIN "Addresses" ON "Threads".address_id = "Addresses".id, 
-    websearch_to_tsquery('english', $searchTerm) as query
-    WHERE query @@ "Threads"._search ${communityOptions} AND "Threads".deleted_at IS NULL
-    ${sort} LIMIT $limit
-`,
-      {
-        bind,
-        type: QueryTypes.SELECT,
-      }
-    );
-  } catch (e) {
-    console.log(e);
-    return next(new ServerError(Errors.UnexpectedError));
-  }
+    const options = req.query as SearchDiscussionsQuery;
+    if (!options.search) {
+      throw new AppError(Errors.QueryMissing);
+    }
+    if (options.search.length < MIN_SEARCH_QUERY_LENGTH) {
+      throw new AppError(Errors.QueryTooShort);
+    }
+    if (!options.chain) {
+      throw new AppError(Errors.NoChains);
+    }
+    if (!req.chain && options.chain !== ALL_CHAINS) {
+      // if no chain resolved, ensure that client explicitly requested all chains
+      throw new AppError(Errors.NoChains);
+    }
 
-  return res.json({
-    status: 'Success',
-    result: threadsAndComments,
-  });
+    const threads = await search(models, req.chain, req.query);
+    return res.json({
+      status: 'Success',
+      result: threads,
+    });
+  } catch (err) {
+    console.error(err);
+    return next(err);
+  }
 };
 
 export default searchDiscussions;
