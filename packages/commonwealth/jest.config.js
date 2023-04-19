17de9a4e13cd380f3995c46fa55d83ffe2e6889e
Ifu Aniemeka <ianiemeka@gmail.com>
2023-04-19 14:57:54 -0500

    WIP: setting up Jest config

---
+++ packages/commonwealth/jest.config.js
@@ -0,0 +1,20 @@
+// module.exports = {
+//   rootDir: "./",
+//   moduleDirectories: [
+//     "node_modules",
+//     "<rootDir>/client/scripts",
+//     "<rootDir>/shared"
+//   ],
+//   preset: 'ts-jest',
+//   moduleNameMapper: {
+//     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+//       '<rootDir>/test/__mocks__/fileMock.js',
+//     '\\.(scss|css|less)$': '<rootDir>/test/__mocks__/styleMock.js',
+//   },
+//   transform: {
+//     // "^.+\\.tsx?$":[ "ts-jest", {tsconfig: './tsconfig.frontend.json', useESM: true} ],
+//     // "\\.tsx?$": "babel-jest"
+//   },
+//   transformIgnorePatterns: ['node_modules/(?!@rxjs/)'],
+//   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"]
+// };
