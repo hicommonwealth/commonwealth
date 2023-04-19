1128c746e69d1f94969cabc7d997507a7d6d4445
Ifu Aniemeka <ianiemeka@gmail.com>
2023-04-19 15:37:04 -0500

    jest mad. jest smash

--- packages/commonwealth/jest.config.js
+++ packages/commonwealth/jest.config.js
@@ -1,20 +1,21 @@
-// module.exports = {
-//   rootDir: "./",
-//   moduleDirectories: [
-//     "node_modules",
-//     "<rootDir>/client/scripts",
-//     "<rootDir>/shared"
-//   ],
-//   preset: 'ts-jest',
-//   moduleNameMapper: {
-//     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-//       '<rootDir>/test/__mocks__/fileMock.js',
-//     '\\.(scss|css|less)$': '<rootDir>/test/__mocks__/styleMock.js',
-//   },
-//   transform: {
-//     // "^.+\\.tsx?$":[ "ts-jest", {tsconfig: './tsconfig.frontend.json', useESM: true} ],
-//     // "\\.tsx?$": "babel-jest"
-//   },
-//   transformIgnorePatterns: ['node_modules/(?!@rxjs/)'],
-//   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"]
-// };
+module.exports = {
+  // rootDir: "../../.",
+  moduleDirectories: [
+    "node_modules",
+    "<rootDir>/client/scripts",
+    "<rootDir>/shared",
+    "../chain-events"
+  ],
+  preset: 'ts-jest',
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/test/__mocks__/fileMock.js',
+    '\\.(scss|css|less)$': '<rootDir>/test/__mocks__/styleMock.js',
+  },
+  transform: {
+    "\\.[jt]sx?$": "babel-jest",
+    "^.+\\.[ts]x?$":[ "ts-jest", {tsconfig: './tsconfig.frontend.json', useESM: true} ],
+  },
+  // transformIgnorePatterns: ['node_modules/(?!@rxjs/)'],
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"]
+};
