71e72b39cdcf06f482b9c4a3bab3dc82e4e0ed4b
Marcin Maslanka <maslankam92@gmail.com>
2023-04-25 20:30:00 +0200

    Handle chunk error (#3586)
    Co-authored-by: Marcin Maslanka <marcin@commonwealth.im>
    Co-authored-by: Jake Naviasky <jake@commonwealth.im>

--- packages/commonwealth/client/scripts/views/pages/error.tsx
+++ packages/commonwealth/client/scripts/views/pages/error.tsx
@@ -2,20 +2,45 @@ import React from 'react';
 
 import Sublayout from 'views/sublayout';
 import { CWEmptyState } from '../components/component_kit/cw_empty_state';
+import { openConfirmation } from 'views/modals/confirmation_modal';
 
 type ErrorPageProps = { title?: any; message?: string };
 
-const ErrorPage = (props: ErrorPageProps) => {
-  const { message } = props;
+const ErrorPage = ({ message }: ErrorPageProps) => {
+  const chunkLoadingErrRe = /^Uncaught SyntaxError: Unexpected token/;
+
+  const isChunkLoadingError = () => {
+    if (typeof message === 'string' && chunkLoadingErrRe.test(message)) {
+      openConfirmation({
+        title: 'Info',
+        description: (
+          <>
+            A new version of the application has been released. Please refresh
+            the page.
+          </>
+        ),
+        buttons: [
+          {
+            label: 'Refresh',
+            buttonType: 'mini-black',
+            onClick: () => window.location.reload(),
+          },
+        ],
+      });
+    }
+
+    return null;
+  };
 
   return (
-    <Sublayout
-    // title={title}
-    >
-      <CWEmptyState
-        iconName="cautionTriangle"
-        content={message || 'An error occurred while loading this page.'}
-      />
+    <Sublayout>
+      <>
+        {isChunkLoadingError()}
+        <CWEmptyState
+          iconName="cautionTriangle"
+          content={message || 'An error occurred while loading this page.'}
+        />
+      </>
     </Sublayout>
   );
 };
