d021266aa43f86043ee18577adc63fd053c51167
dillchen <dillon@commonwealth.im>
2023-04-25 08:42:16 -0400

    add safe-area insert cordova plugin

--- packages/commonwealth/client/scripts/index.tsx
+++ packages/commonwealth/client/scripts/index.tsx
@@ -9,7 +9,36 @@ import 'react-toastify/dist/ReactToastify.css';
 
 import App from './app';
 
-const container = document.getElementById('root');
-const root = createRoot(container);
+function cordovaReady() {
+  (window as any).plugins.safearea.get(successCallback, errorCallback);
+}
 
-root.render(<App />);
+function successCallback(result) {
+  console.log('successCallback called with result:', result);
+  // Set CSS variables with the safe area insets
+  document.documentElement.style.setProperty('--safe-area-inset-top', result.top + 'px');
+  document.documentElement.style.setProperty('--safe-area-inset-right', result.right + 'px');
+  document.documentElement.style.setProperty('--safe-area-inset-bottom', result.bottom + 'px');
+  document.documentElement.style.setProperty('--safe-area-inset-left', result.left + 'px');
+}
+
+function errorCallback(error) {
+  console.log('Error:', error);
+}
+
+function initApp() {
+  const container = document.getElementById('root');
+  const root = createRoot(container);
+  root.render(<App />);
+}
+
+// Check if the app is running in a Cordova environment
+if ((window as any).cordova) {
+  document.addEventListener("deviceready", () => {
+    cordovaReady();
+    initApp();
+  }, false);
+} else {
+  // Non-Cordova environment (regular web browser)
+  initApp();
+}
