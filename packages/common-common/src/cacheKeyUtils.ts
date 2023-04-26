3186ea11bc329bd8049939ce1f14087c28c52075
Nakul Manchanda <nakul@common.xyz>
2023-04-26 18:29:41 -0400

    add helper redis script, instruction on readme
    Signed-off-by: Nakul Manchanda <nakul@common.xyz>

--- packages/common-common/src/cacheKeyUtils.ts
+++ packages/common-common/src/cacheKeyUtils.ts
@@ -7,7 +7,7 @@ export const defaultKeyGenerator = (req: Request) => {
 export const defaultUserKeyGenerator = (req: Request) => {
     const user = req.user;
     if(user && user.id) {
-        return `${user.id}_${req.originalUrl}`;
+        return `user:${user.id}_${req.originalUrl}`;
     }
     return req.originalUrl;
 }
