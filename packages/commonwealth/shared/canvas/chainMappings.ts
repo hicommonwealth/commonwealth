7ea9dd257f62b4a32ea29962ca586697bab83883
Bob Webb <bob.wbb@gmail.com>
2023-04-26 18:02:05 +0100

    implement function for creating CAIP-2 identifier

--- packages/commonwealth/shared/canvas/chainMappings.ts
+++ packages/commonwealth/shared/canvas/chainMappings.ts
@@ -1,25 +1,25 @@
 import { ChainBase } from 'common-common/src/types';
 
-export function chainBaseToCanvasChainId(
+export function chainBaseToCanvasChain(
   chainBase: ChainBase,
   idOrPrefix: string | number
 ): string {
-  // The Canvas chain id is a stringified ETH chain ID, or Cosmos bech32 prefix, or equivalent.
+  // The canvas "chain" value is a CAIP-2 compliment chain id
   if (chainBase === ChainBase.CosmosSDK) {
-    return idOrPrefix.toString();
+    return `cosmos:${idOrPrefix.toString()}`;
   } else if (chainBase === ChainBase.Ethereum) {
-    return idOrPrefix.toString();
+    return `eip155:${idOrPrefix.toString()}`;
   } else if (chainBase === ChainBase.NEAR) {
     // Temporarily locked to mainnet
     // See also: client/scripts/views/pages/finish_near_login.tsx
-    return 'mainnet';
+    return `near:mainnet`
   } else if (chainBase === ChainBase.Solana) {
     // Temporarily locked to mainnet
     // See also: client/scripts/controllers/app/webWallets/phantom_web_wallet.ts
-    return 'mainnet';
+    return `solana:mainnet`
   } else if (chainBase === ChainBase.Substrate) {
     // Temporarily locked to generic Substrate chain id
     // See also: client/scripts/controllers/app/webWallets/polkadot_web_wallet.ts
-    return '42';
+    return `polkadot:42`
   }
 }
