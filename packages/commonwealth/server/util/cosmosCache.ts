7403ea3a618aaeb3ef16a7a35b7202f1919bebb6
mhagel <mark.hagelberg@common.xyz>
2023-04-26 16:28:38 -0600

    cosmos cache
    LCD cache key
    
    cosmos cache rules
    
    cosmosCache test
    
    no cache test
    
    proposal cache tests
    
    expiry test
    
    dry lcd tests
    
    completed proposals 60 minute cache

---
+++ packages/commonwealth/server/util/cosmosCache.ts
@@ -0,0 +1,111 @@
+import type { Request, RequestHandler } from 'express';
+import { cacheDecorator } from 'common-common/src/cacheDecorator';
+
+const oneBlock = 6; // typical Cosmos block time is ~6 seconds
+
+export const cosmosCacheRPC = (duration: number): RequestHandler => {
+  // RPC specific codes from cosmJS requests
+  const activeCodes = ['0801', '0802']; // ['DepositPerion', 'VotingPeriod']
+  const completedCodes = [
+    '0803', // 'Passed'
+    '0804', // 'Rejected'
+    '0805', // 'Failed'
+    '0803220a0a080000000000000087', // 'Passed'
+    '0803220a0a080000000000000100', // 'Passed'
+    '0803220a0a080000000000000189', // 'Passed'
+  ];
+
+  return async function cacheIfCacheworthyRPC(req, res, next) {
+    const body = parseReqBody(req);
+    const params = JSON.stringify(body?.params);
+    const method = body?.method;
+
+    if (/^(tx)/.test(method)) {
+      // TX Response: do not cache
+      return next();
+    } else if (/^(block|status)/.test(method)) {
+      // BLOCK CHECK: short cache
+      duration = oneBlock;
+    } else if (/^Query\/(Votes|TallyResult|Deposits)/.test(params)) {
+      // LIVE DATA: short cache
+      duration = oneBlock;
+    } else if (/^Query\/(Params|Pool)/.test(params)) {
+      // chain PARAMS: cache long-term (1 day)
+      duration = 60 * 60 * 24;
+    } else if (activeCodes.some((c) => c === body?.params?.data)) {
+      // ACTIVE PROPOSALS: short cache
+      duration = oneBlock;
+    } else if (completedCodes.some((c) => c === body?.params?.data)) {
+      // COMPLETED PROPOSALS: cache 60 minutes
+      duration = 60 * 60;
+    }
+
+    return cacheDecorator.cacheMiddleware(duration, cosmosRPCKeyGenerator)(
+      req,
+      res,
+      next
+    );
+  };
+};
+
+export const cosmosCacheLCD = (duration: number): RequestHandler => {
+  // Matches ProposalStatus from common-common/src/cosmos-ts/src/codegen/cosmos/gov/v1/gov.ts
+  const activeProposalCodes = [1, 2]; // ['DepositPeriod', 'VotingPeriod']
+  const completedProposalCodes = [3, 4, 5]; // ['Passed', 'Rejected', 'Failed']
+
+  return async function cacheIfCacheworthyLCD(req, res, next) {
+    const url = req.url;
+    const proposalStatus = req?.query?.proposal_status;
+
+    if (proposalStatus) {
+      if (activeProposalCodes.some((c) => c === +proposalStatus)) {
+        // ACTIVE PROPOSALS: short cache
+        duration = oneBlock;
+      } else if (completedProposalCodes.some((c) => c === +proposalStatus)) {
+        // COMPLETED PROPOSALS: cache 60 minutes
+        duration = 60 * 60;
+      }
+      // specific proposal request:
+    } else if (url.includes('/proposals/')) {
+      // LIVE VOTES/TALLY/DEPOSITS: short cache
+      duration = oneBlock;
+    } else if (url.includes('/params/')) {
+      // PARAMS: cache long term (1 day)
+      duration = 60 * 60 * 24;
+    }
+
+    return cacheDecorator.cacheMiddleware(duration, cosmosLCDKeyGenerator)(
+      req,
+      res,
+      next
+    );
+  };
+};
+
+const cosmosRPCKeyGenerator = (req: Request) => {
+  const body = parseReqBody(req);
+  let identifier = JSON.stringify(body?.params);
+  if (identifier === '{}') {
+    identifier = JSON.stringify(body);
+  }
+
+  return `${req.originalUrl}_${identifier}`;
+};
+
+const cosmosLCDKeyGenerator = (req: Request) => {
+  return req.originalUrl;
+};
+
+const parseReqBody = (req: Request) => {
+  let body = req.body;
+
+  if (typeof body === 'string') {
+    try {
+      body = JSON.parse(req.body);
+    } catch (err) {
+      console.log('Error parsing body: ', err);
+    }
+  }
+
+  return body;
+};
---
+++ packages/commonwealth/test/integration/api/cosmosCache.spec.ts
@@ -0,0 +1,171 @@
+require('dotenv').config();
+import chai from 'chai';
+import 'chai/register-should';
+import chaiHttp from 'chai-http';
+chai.use(chaiHttp);
+const expect = chai.expect;
+
+import { RedisCache } from 'common-common/src/redisCache';
+import { RedisNamespaces } from 'common-common/src/types';
+import { cacheDecorator } from 'common-common/src/cacheDecorator';
+import app, { resetDatabase } from '../../../server-test';
+import { connectToRedis } from '../../util/redisUtils';
+import { sleep } from '../../util/delayUtils';
+
+function verifyNoCacheResponse(res) {
+  expect(res.body).to.not.be.null;
+  expect(res).to.have.status(200);
+  expect(res).to.not.have.header('X-Cache', 'HIT');
+}
+
+async function verifyCacheResponse(key, res, resEarlier) {
+  expect(res).to.have.status(200);
+  expect(res).to.have.header('X-Cache', 'HIT');
+  const valFromRedis = await cacheDecorator.checkCache(key);
+  expect(valFromRedis).to.not.be.null;
+  expect(JSON.parse(valFromRedis)).to.be.deep.equal(res.body);
+  expect(JSON.parse(valFromRedis)).to.be.deep.equal(resEarlier.body);
+}
+
+describe('Cosmos Cache', () => {
+  const redisCache: RedisCache = new RedisCache();
+  const route_namespace: RedisNamespaces = RedisNamespaces.Route_Response;
+
+  before(async () => {
+    await resetDatabase();
+    await connectToRedis(redisCache);
+    cacheDecorator.setCache(redisCache);
+  });
+
+  after(async () => {
+    await redisCache.deleteNamespaceKeys(route_namespace);
+    await redisCache.closeClient();
+  });
+
+  describe('Cosmos Cache', () => {
+    describe('cosmosAPI', () => {
+      async function makeRPCRequest(
+        body,
+        path = '/cosmosAPI/juno',
+        headers = {
+          'content-type': 'text/plain;charset=UTF-8',
+          'accept-language': 'en-US,en;q=0.9',
+        }
+      ) {
+        return chai.request(app).post(path).set(headers).send(body);
+      }
+
+      async function rpcTestBody(body, key) {
+        const res1 = await makeRPCRequest(body);
+        verifyNoCacheResponse(res1);
+        const res2 = await makeRPCRequest(body);
+        await verifyCacheResponse(key, res2, res1);
+      }
+
+      const rpcProposalsCacheExpectedTest = async (proposalStatus: string) => {
+        const pathParam = `{"path":"/cosmos.gov.v1beta1.Query/Proposals","data":"${proposalStatus}","prove":false}`;
+        const body = `{"jsonrpc":"2.0","id":382288611593,"method":"abci_query","params":${pathParam}}`;
+        const key = `/cosmosAPI/juno_{"path":"/cosmos.gov.v1beta1.Query/Proposals","data":"${proposalStatus}","prove":false}`;
+        await rpcTestBody(body, key);
+      };
+
+      it('should cache passed proposals', async () => {
+        await rpcProposalsCacheExpectedTest('0803');
+      });
+      it('should cache rejected proposals', async () => {
+        await rpcProposalsCacheExpectedTest('0804');
+      });
+      it('should cache rejected proposals', async () => {
+        await rpcProposalsCacheExpectedTest('0805');
+      });
+      it('should cache deposit period proposals', async () => {
+        await rpcProposalsCacheExpectedTest('0801');
+      });
+      it('should cache voting period proposals for short period', async () => {
+        const statusCode = '0802';
+        const body = `{"jsonrpc":"2.0","id":382288611593,"method":"abci_query","params":{"path":"/cosmos.gov.v1beta1.Query/Proposals","data":"${statusCode}","prove":false}}`;
+        await rpcProposalsCacheExpectedTest(statusCode);
+
+        console.log('waiting 7 seconds');
+        // wait 7 seconds then expect cached request to be expired
+        await sleep(7000);
+        const res3 = await makeRPCRequest(body);
+
+        expect(res3).to.have.status(200);
+        expect(res3).to.not.have.header('X-Cache', 'HIT');
+      });
+      it('should cache params requests', async () => {
+        const body =
+          '{"jsonrpc":"2.0","id":533311223528,"method":"abci_query","params":{"path":"/cosmos.staking.v1beta1.Query/Params","data":"","prove":false}}';
+        const key =
+          '/cosmosAPI/juno_{"path":"/cosmos.staking.v1beta1.Query/Params","data":"","prove":false}';
+        await rpcTestBody(body, key);
+      });
+      it('should cache status requests', async () => {
+        const body =
+          '{"jsonrpc":"2.0","id":599352122315,"method":"status","params":{}}';
+        const key =
+          '/cosmosAPI/juno_{"jsonrpc":"2.0","id":599352122315,"method":"status","params":{}}';
+        await rpcTestBody(body, key);
+      });
+    });
+    describe('cosmosLCD', () => {
+      const lcdProposalsCacheExpectedTest = async (proposalStatus: string) => {
+        const query = `/cosmosLCD/csdk/cosmos/gov/v1/proposals?proposal_status=${proposalStatus}&voter=&depositor=`;
+        const res1 = await chai.request(app).get(query);
+        await verifyNoCacheResponse(res1);
+
+        const res2 = await chai.request(app).get(query);
+        await verifyCacheResponse(query, res2, res1);
+      };
+
+      const lcdParamsCacheExpectedTest = async (param: string) => {
+        const query = `/cosmosLCD/csdk/cosmos/gov/v1/params/${param}`;
+        const res1 = await chai.request(app).get(query);
+        await verifyNoCacheResponse(res1);
+
+        const res2 = await await chai.request(app).get(query);
+        await verifyCacheResponse(query, res2, res1);
+      };
+
+      it('should cache deposit period proposals for a short period', async () => {
+        const statusCode = '1';
+        const query = `/cosmosLCD/csdk/cosmos/gov/v1/proposals?proposal_status=${statusCode}&voter=&depositor=`;
+        await lcdProposalsCacheExpectedTest(statusCode);
+
+        console.log('waiting 7 seconds');
+        // wait 7 seconds then expect cached request to be expired
+        await sleep(7000);
+
+        const res3 = await chai
+          .request(app)
+          .get(query)
+          .set('accept', 'application/json, text/plain, */*');
+
+        expect(res3).to.have.status(200);
+        expect(res3).to.not.have.header('X-Cache', 'HIT');
+      });
+      it('should cache voting period proposals', async () => {
+        await lcdProposalsCacheExpectedTest('2');
+      });
+      it('should cache passed proposals', async () => {
+        await lcdProposalsCacheExpectedTest('3');
+      });
+      it('should cache rejected proposals', async () => {
+        await lcdProposalsCacheExpectedTest('4');
+      });
+      it('should cache failed proposals', async () => {
+        await lcdProposalsCacheExpectedTest('5');
+      });
+      it('should cache deposit params requests', async () => {
+        await lcdParamsCacheExpectedTest('deposit');
+      });
+      it('should cache tallying params requests', async () => {
+        await lcdParamsCacheExpectedTest('tallying');
+      });
+      it('should cache voting params requests', async () => {
+        await lcdParamsCacheExpectedTest('voting');
+      });
+    });
+  });
+});
