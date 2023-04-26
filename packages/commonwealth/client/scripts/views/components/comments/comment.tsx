65a9022920ccc2c1e0167a11ca0685bda57bc2b9
kassad <kurtis@commonwealth.im>
2023-04-26 12:39:18 -0700

    Added confirmation before deleting comment

--- packages/commonwealth/client/scripts/views/components/comments/comment.tsx
+++ packages/commonwealth/client/scripts/views/components/comments/comment.tsx
@@ -5,8 +5,9 @@ import type { Account, Comment as CommentType } from 'models';
 import moment from 'moment';
 
 import app from 'state';
-import { ContentType } from 'types';
+import { ContentType, ThreadActionType } from 'types';
 import { ChainType } from '../../../../../../common-common/src/types';
+import deleteComment from '../../../../../server/routes/deleteComment';
 import { CWIconButton } from '../component_kit/cw_icon_button';
 import { CWIcon } from '../component_kit/cw_icons/cw_icon';
 import { PopoverMenu } from '../component_kit/cw_popover/cw_popover_menu';
@@ -100,9 +101,30 @@ export const Comment = (props: CommentProps) => {
     !isLocked &&
     (comment.author === app.user.activeAccount?.address || isAdminOrMod);
 
-  const deleteComment = async () => {
-    await app.comments.delete(comment);
-    updatedCommentsCallback();
+  const handleDeleteComment = () => {
+    openConfirmation({
+      title: 'Delete Comment',
+      description: <>Delete this comment?</>,
+      buttons: [
+        {
+          label: 'Delete',
+          buttonType: 'mini-red',
+          onClick: async () => {
+            try {
+              app.comments.delete(comment).then(() => {
+                updatedCommentsCallback();
+              });
+            } catch (err) {
+              console.log(err);
+            }
+          },
+        },
+        {
+          label: 'Cancel',
+          buttonType: 'mini-black',
+        },
+      ],
+    });
   };
 
   // if (!this.verificationChecked) {
@@ -260,7 +282,7 @@ export const Comment = (props: CommentProps) => {
                         {
                           label: 'Delete',
                           iconLeft: 'trash',
-                          onClick: deleteComment,
+                          onClick: handleDeleteComment,
                         },
                       ]}
                     />
