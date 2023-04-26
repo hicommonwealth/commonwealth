ce384ac042f5ada06850e3db0f44253197be8dc4
Raymond Z <raykyri@gmail.com>
2023-04-25 20:06:18 -0400

    return vals, unpack

--- packages/commonwealth/server/routes/canvas.ts
+++ packages/commonwealth/server/routes/canvas.ts
@@ -11,14 +11,27 @@ export const getCanvasData = async (
 ) => {
   const before = req.params.before ?? null;
 
-  const [result, metadata] = await models.sequelize.query(`
-(SELECT canvas_action, canvas_session, canvas_hash, updated_at FROM "Threads" WHERE canvas_action IS NOT NULL AND updated_at < COALESCE(?, NOW()) ORDER BY updated_at DESC LIMIT 50)
+  const [rows, metadata] = await models.sequelize.query(`
+(SELECT canvas_action as action, canvas_session as session, canvas_hash as hash, updated_at FROM "Threads" WHERE canvas_action IS NOT NULL AND updated_at < COALESCE(?, NOW()) ORDER BY updated_at DESC LIMIT 50)
 UNION
-(SELECT canvas_action, canvas_session, canvas_hash, updated_at FROM "Comments" WHERE canvas_action IS NOT NULL AND updated_at < COALESCE(?, NOW()) ORDER BY updated_at DESC LIMIT 50)
+(SELECT canvas_action as action, canvas_session as session, canvas_hash as hash, updated_at FROM "Comments" WHERE canvas_action IS NOT NULL AND updated_at < COALESCE(?, NOW()) ORDER BY updated_at DESC LIMIT 50)
 UNION
-(SELECT canvas_action, canvas_session, canvas_hash, updated_at FROM "Reactions" WHERE canvas_action IS NOT NULL AND updated_at < COALESCE(?, NOW()) ORDER BY updated_at DESC LIMIT 50)
+(SELECT canvas_action as action, canvas_session as session, canvas_hash as hash, updated_at FROM "Reactions" WHERE canvas_action IS NOT NULL AND updated_at < COALESCE(?, NOW()) ORDER BY updated_at DESC LIMIT 50)
 ORDER BY updated_at DESC LIMIT 50;
 `, { replacements: [before, before, before] });
+
+  const result = rows.map((row) => {
+    try {
+      return {
+        action: JSON.parse(row.action),
+        session: JSON.parse(row.session),
+        hash: row.hash,
+        updated_at: row.updated_at,
+      }
+    } catch (e) {
+      return null
+    }
+  }).filter((res) => res);
 
   return res.json({
     status: 'Success',
