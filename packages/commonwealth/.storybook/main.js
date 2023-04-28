915fb5e786d6fa4632aa2011ad8cf93a63c9fd46
Daniel Martins <martinsda@gmail.com>
2023-04-28 09:13:48 -0300

    babel + webpack attributes

--- packages/commonwealth/.storybook/main.js
+++ packages/commonwealth/.storybook/main.js
@@ -20,88 +20,88 @@ module.exports = {
   docs: {
     autodocs: 'tag',
   },
-  // webpackFinal: async (config, { configType }) => {
-  //   // console.log(
-  //   //   'path:',
-  //   //   path.resolve(__dirname, '..', 'client', 'scripts', 'views')
-  //   //   // path.resolve('..', 'client', 'scripts', 'views')
-  //   // );
-  //   // C:\dev\common\commonwealth\packages\client\scripts\views
+  webpackFinal: async (config, { configType }) => {
+    // console.log(
+    //   'path:',
+    //   path.resolve(__dirname, '..', 'client', 'scripts', 'views')
+    //   // path.resolve('..', 'client', 'scripts', 'views')
+    // );
+    // C:\dev\common\commonwealth\packages\client\scripts\views
 
-  //   const configObj = {
-  //     ...config,
-  //     mode: 'development',
-  //     context: __dirname,
-  //     module: {
-  //       ...config.module,
-  //       rules: [...config.module.rules, ...custom.module.rules],
-  //     },
-  //     resolve: {
-  //       ...config.resolve,
-  //       // ...custom.resolve,
-  //       extensions: [...custom.resolve.extensions],
-  //       // modules: [
-  //       //   '../client/scripts',
-  //       //   '../client/styles',
-  //       //   '../shared',
-  //       //   'node_modules', // local node modules
-  //       //   '../node_modules', // global node modules
-  //       // ],
-  //       modules: [...custom.resolve.modules],
-  //       alias: { ...custom.resolve.alias },
-  //       // alias: {
-  //       //   components: path.resolve(__dirname, '..', 'client', 'styles'),
-  //       // },
-  //     },
-  //   };
+    const configObj = {
+      ...config,
+      mode: 'development',
+      context: __dirname,
+      module: {
+        ...config.module,
+        rules: [...config.module.rules, ...custom.module.rules],
+      },
+      resolve: {
+        ...config.resolve,
+        // ...custom.resolve,
+        extensions: [...custom.resolve.extensions],
+        // modules: [
+        //   '../client/scripts',
+        //   '../client/styles',
+        //   '../shared',
+        //   'node_modules', // local node modules
+        //   '../node_modules', // global node modules
+        // ],
+        modules: [...custom.resolve.modules],
+        alias: { ...custom.resolve.alias },
+        // alias: {
+        //   components: path.resolve(__dirname, '..', 'client', 'styles'),
+        // },
+      },
+    };
 
-  //   // console.log('configObj:', configObj.resolve);
+    // console.log('configObj:', configObj.resolve);
 
-  //   // Make whatever fine-grained changes you need
-  //   return configObj;
+    // Make whatever fine-grained changes you need
+    return configObj;
 
-  //   // C:\dev\common\commonwealth\packages\commonwealth\client\scripts\views
-  //   // __dirname = C:\dev\common\commonwealth\packages\commonwealth\.storybook
-  //   // config.resolve.alias = {
-  //   //   'components/component_kit/': path.resolve(
-  //   //     __dirname,
-  //   //     '..',
-  //   //     'client',
-  //   //     'scripts',
-  //   //     'views'
-  //   //   ),
-  //   // };
+    // C:\dev\common\commonwealth\packages\commonwealth\client\scripts\views
+    // __dirname = C:\dev\common\commonwealth\packages\commonwealth\.storybook
+    // config.resolve.alias = {
+    //   'components/component_kit/': path.resolve(
+    //     __dirname,
+    //     '..',
+    //     'client',
+    //     'scripts',
+    //     'views'
+    //   ),
+    // };
 
-  //   // Return the altered config
-  //   return config;
-  // },
-  // babel: async (options) => ({
-  //   ...options,
-  //   presets: [
-  //     [
-  //       '@babel/preset-env',
-  //       {
-  //         useBuiltIns: 'usage',
-  //         corejs: '3.22',
-  //       },
-  //     ],
-  //     '@babel/preset-typescript',
-  //     '@babel/preset-react',
-  //   ],
-  //   plugins: [
-  //     ...options.plugins,
-  //     [
-  //       '@babel/plugin-transform-react-jsx',
-  //       {
-  //         pragma: 'jsx',
-  //         pragmaFrag: 'React.Fragment',
-  //       },
-  //     ],
-  //     '@babel/plugin-syntax-dynamic-import',
-  //     '@babel/plugin-proposal-class-properties',
-  //     '@babel/plugin-proposal-async-generator-functions',
-  //     'babel-plugin-transform-scss',
-  //   ],
-  // }),
+    // Return the altered config
+    return config;
+  },
+  babel: async (options) => ({
+    ...options,
+    presets: [
+      [
+        '@babel/preset-env',
+        {
+          useBuiltIns: 'usage',
+          corejs: '3.22',
+        },
+      ],
+      '@babel/preset-typescript',
+      '@babel/preset-react',
+    ],
+    plugins: [
+      ...options.plugins,
+      [
+        '@babel/plugin-transform-react-jsx',
+        {
+          pragma: 'jsx',
+          pragmaFrag: 'React.Fragment',
+        },
+      ],
+      '@babel/plugin-syntax-dynamic-import',
+      '@babel/plugin-proposal-class-properties',
+      '@babel/plugin-proposal-async-generator-functions',
+      'babel-plugin-transform-scss',
+    ],
+  }),
 };
 // export default config;
