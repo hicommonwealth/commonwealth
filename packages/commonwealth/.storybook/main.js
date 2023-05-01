faecfca490ccda346e7c83860783a75de034c45d
Daniel Martins <martinsda@gmail.com>
2023-05-01 00:07:17 -0400

    Edit storybook folder structure

--- packages/commonwealth/.storybook/main.js
+++ packages/commonwealth/.storybook/main.js
@@ -4,7 +4,7 @@
 import custom from '../webpack/webpack.dev.config.js';
 
 module.exports = {
-  stories: ['../stories/**/*.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: ['./stories/**/*.mdx', './stories/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
