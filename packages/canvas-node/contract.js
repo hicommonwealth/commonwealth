0b61ddb9728e8570b485cd4cf077d80ae87cf5d4
Raymond Z <raykyri@gmail.com>
2023-05-01 16:12:26 -0400

    update syncModule, add /Commonwealth source

--- packages/canvas-node/contract.js
+++ packages/canvas-node/contract.js
@@ -75,4 +75,8 @@ export const actions = {
 	},
 }
 
+const sources = {
+  '/Commonwealth': { ...actions }
+}
+
 export const routes = {}
--- packages/canvas-node/syncModule.js
+++ packages/canvas-node/syncModule.js
@@ -5,14 +5,28 @@ const onPeerRecv = (action, session) => {
 
 // Declare a handler that fetches new actions from the CW API.
 const api = {
-  endpoint: "http://localhost:8080/api/oplog",
-  params: () => {},
   frequency: 5000,
-  onPoll: (result) => {
-    // Executes on poll.
+  endpoint: (cursor) => {
+    return "http://localhost:8080/api/oplog" +
+      (cursor === undefined ? '' : `?updated_at=${cursor.updated_at}`);
+  },
+  onPoll: (response, apply) => {
+    const { status, result } = response;
 
-    // returning true
-    return true
+    // Recent actions may not come totally ordered, so we fetch the last X
+    // results, try to apply them all, and optionally look for more.
+    const applied = result.filter((res) => apply(res.hash, res.action, res.session));
+
+    // If all results have been seen before (successfullyApplied = false) then
+    // we're done. Otherwise, advance to the earliest value of updated_at.
+    if (applied.length === 0) {
+      return false
+    }
+    const earliestApplied = applied[applied.length - 1];
+    return { updated_at: earliestApplied.updated_at }
+
+    // TODO: Instead of using setInterval, we should advance the cursor
+    // until running out of results, using setTimeout after each query.
   }
 }
 
