db8e4d34fcf38776506f4880848c6bcada8eebad
Raymond Z <raykyri@gmail.com>
2023-05-02 01:49:59 -0400

    fix types

--- packages/commonwealth/server/routes/canvas.ts
+++ packages/commonwealth/server/routes/canvas.ts
@@ -4,12 +4,19 @@ import type { TypedRequestBody, TypedResponse } from '../types';
 import { success } from '../types';
 import * as Sequelize from 'sequelize';
 
+type CanvasDataReq = {
+  query: {
+    before: string
+  }
+};
+type CanvasDataResp = {};
+
 export const getCanvasData = async (
   models: DB,
   req: TypedRequestBody<CanvasDataReq>,
   res: TypedResponse<CanvasDataResp>
 ) => {
-  const before = req.params.before ?? null;
+  const before = req.query.before ?? null;
 
   const [rows, metadata] = await models.sequelize.query(`
 (SELECT canvas_action as action, canvas_session as session, canvas_hash as hash, updated_at FROM "Threads" WHERE canvas_action IS NOT NULL AND updated_at < COALESCE(?, NOW()) ORDER BY updated_at DESC LIMIT 50)
@@ -20,7 +27,13 @@ UNION
 ORDER BY updated_at DESC LIMIT 50;
 `, { replacements: [before, before, before] });
 
-  const result = rows.map((row) => {
+  type QueryResult = {
+    action: string,
+    session: string,
+    hash: string,
+    updated_at: string,
+  }
+  const result = rows.map((row: QueryResult) => {
     try {
       return {
         action: JSON.parse(row.action),
