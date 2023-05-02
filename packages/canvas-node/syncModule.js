d5b4a3b45d4903a3bdb5dc3e24155a427e05c5af
Raymond Z <raykyri@gmail.com>
2023-05-02 15:49:54 -0400

    temporarily process actions in-order in syncModule, export async onPoll from syncModule

--- packages/canvas-node/syncModule.js
+++ packages/canvas-node/syncModule.js
@@ -11,19 +11,26 @@ const api = {
       (cursor === undefined ? '' : `?updated_at=${cursor.updated_at}`)
     );
   },
-  onPoll: (response, apply) => {
+  onPoll: async (response, apply) => {
     const { status, result } = response;
 
     // Recent actions may not come totally ordered, so we fetch the last X
     // results, try to apply them all, and optionally look for more.
-    const applied = result.filter((res) =>
-      apply(res.hash, res.action, res.session)
-    );
+    const applied = [];
+    for (const res of result) {
+      if (await apply(res.hash, res.action, res.session)) {
+        applied.push(res);
+      }
+    }
+    // TODO: Better to avoid blocking here, and let the action processing queue handle async instead.
+    // const applied = result.filter((res) =>
+    //   apply(res.hash, res.action, res.session)
+    // );
 
     // If all results have been seen before (successfullyApplied = false) then
     // we're done. Otherwise, advance to the earliest value of updated_at.
     if (applied.length === 0) {
-      return;
+      return null;
     }
     const earliestApplied = applied[applied.length - 1];
     return { updated_at: earliestApplied.updated_at };
