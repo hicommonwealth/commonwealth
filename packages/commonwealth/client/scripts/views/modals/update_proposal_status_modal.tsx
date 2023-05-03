b7cdda5c84a7a041e32259ffabd48aaa35185352
Ian Rowan <36428666+ianrowan@users.noreply.github.com>
2023-05-03 16:28:11 -0500

    Proposal <-> Thread Linking  (#3542)
    * method stubs + failing unit tests
    
    * PR comments + migration
    
    * Proposal Linking Backend implementation & tests (#3575)
    
    * add endpoints and initial logic BE
    
    * add full migration, working links and initial integration tests
    
    * fix tests + uncovered bugs
    
    * add typed request/response
    
    * transactionalize db migration
    
    * update speed of migration
    
    * add ability to get list of sources, snapshot migration
    
    * update bulk threads to populate from links
    
    * add links to response
    
    * filter for ce chains only
    
    * Add title
    
    * Add stubs for link redirect pages. (#3639)
    
    * Add stubs for link redirect pages.
    
    * Remove TODO for update step.
    
    * Linking threads, snapshots, proposals - front end (#3628)
    
    * Make thread.links available
    
    * Update linking threads
    
    * Update linking snapshots
    
    * Update linking proposals
    
    * Cleanup
    
    * Add logic for snapshot and proposal pills + add util for filtering links
    
    * make function for adding and deleting generic
    
    * remove not used anymore functions and routes
    
    ---------
    
    Co-authored-by: Marcin Maslanka <marcin@commonwealth.im>
    
    * update store after addlinks and deletelinks
    
    * add titles migration steps
    
    * remove CE query and use title on link
    
    * 3646 - Fix update thread status modal
    
    * Deprecate fetchThreadForSnapshot
    
    * Linking - Cleanup (#3655)
    
    * Deprecate snapshot_proposal on thread
    
    * Deprecate linked_threads on thread
    
    ---------
    
    Co-authored-by: Marcin Maslanka <marcin@commonwealth.im>
    
    * Fixed bug with updating stage of new thread
    
    * remove redundant tests
    
    * Add title to addlinks
    
    * Cosmos linking additions (#3681)
    
    * Add cosmos select component
    
    * add proposal text
    
    * use correct class to show proposals and link
    
    * fetch completed proposals if not loaded
    
    * useGetActiveCosmosProposals, load both into modal
    
    * useGetActiveCosmosProposals
    
    * don't show chain events for cosmos
    
    * space between words in status
    
    * better cosmos check
    
    * fix bobber
    
    * fix bobber and add title
    
    * rm console log
    
    ---------
    
    Co-authored-by: mhagel <mark.hagelberg@common.xyz>
    
    * Loading completed text
    
    * Fix typecheck error.
    
    ---------
    
    Co-authored-by: Jake Naviasky <jake@commonwealth.im>
    Co-authored-by: Marcin Maslanka <maslankam92@gmail.com>
    Co-authored-by: Marcin Maslanka <marcin@commonwealth.im>
    Co-authored-by: mhagel <mark.hagelberg@common.xyz>

--- packages/commonwealth/client/scripts/views/modals/update_proposal_status_modal.tsx
+++ packages/commonwealth/client/scripts/views/modals/update_proposal_status_modal.tsx
@@ -13,13 +13,32 @@ import { ChainEntitiesSelector } from '../components/chain_entities_selector';
 import { CWButton } from '../components/component_kit/cw_button';
 import { SnapshotProposalSelector } from '../components/snapshot_proposal_selector';
 import { CWIconButton } from '../components/component_kit/cw_icon_button';
+import { Link, LinkSource } from 'models/Thread';
+import { filterLinks, getAddedAndDeleted } from 'helpers/threads';
+import { ProposalSelector } from '../components/cosmos_proposal_selector';
+import { CosmosProposal } from '/controllers/chain/cosmos/gov/v1beta1/proposal-v1beta1';
+import { ChainBase } from 'common-common/src/types';
+
+const getInitialSnapshots = (thread: Thread) =>
+  filterLinks(thread.links, LinkSource.Snapshot).map((l) => ({
+    id: l.identifier,
+    title: l.title,
+  }));
+
+const getInitialProposals = (thread: Thread) =>
+  filterLinks(thread.links, LinkSource.Proposal).map((l) => ({
+    typeId: l.identifier,
+    title: l.title,
+  }));
+
+const getInitialCosmosProposals = (thread: Thread) =>
+  filterLinks(thread.links, LinkSource.Proposal).map((l) => ({
+    identifier: l.identifier,
+    title: l.title,
+  }));
 
 type UpdateProposalStatusModalProps = {
-  onChangeHandler: (
-    stage: ThreadStage,
-    chainEntities?: ChainEntity[],
-    snapshotProposal?: SnapshotProposal[]
-  ) => void;
+  onChangeHandler?: (stage: ThreadStage, links?: Link[]) => void;
   onModalClose: () => void;
   thread: Thread;
 };
@@ -31,16 +50,14 @@ export const UpdateProposalStatusModal = ({
 }: UpdateProposalStatusModalProps) => {
   const [tempStage, setTempStage] = useState<ThreadStage>(thread.stage);
   const [tempSnapshotProposals, setTempSnapshotProposals] = useState<
-    Array<SnapshotProposal>
-  >(
-    [
-      thread.snapshotProposal &&
-        ({ id: thread.snapshotProposal } as SnapshotProposal),
-    ].filter(Boolean)
-  );
-  const [tempChainEntities, setTempChainEntities] = useState<
-    Array<ChainEntity>
-  >(thread.chainEntities || []);
+    Array<Pick<SnapshotProposal, 'id' | 'title'>>
+  >(getInitialSnapshots(thread));
+  const [tempProposals, setTempProposals] = useState<
+    Array<Pick<ChainEntity, 'typeId' | 'title'>>
+  >(getInitialProposals(thread));
+  const [tempCosmosProposals, setTempCosmosProposals] = useState<
+    Array<Pick<CosmosProposal, 'identifier' | 'title'>>
+  >(getInitialCosmosProposals(thread));
 
   if (!app.chain?.meta) {
     return;
@@ -58,6 +75,9 @@ export const UpdateProposalStatusModal = ({
       ]
     : parseCustomStages(customStages);
   const showSnapshot = !!app.chain.meta.snapshot?.length;
+  const isCosmos = app.chain.base === ChainBase.CosmosSDK;
+  const showChainEvents =
+    !isCosmos && app.chainEntities.store.get(thread.chain).length > 0;
 
   const handleSaveChanges = async () => {
     // set stage
@@ -75,28 +95,114 @@ export const UpdateProposalStatusModal = ({
       );
     }
 
-    // set linked chain entities
+    let links: Link[] = thread.links;
+
     try {
-      await app.threads.setLinkedChainEntities({
-        threadId: thread.id,
-        entities: tempChainEntities,
-      });
-      if (tempSnapshotProposals.length > 0 && tempSnapshotProposals[0]?.id) {
-        await app.threads.setLinkedSnapshotProposal({
+      const { toAdd, toDelete } = getAddedAndDeleted(
+        tempSnapshotProposals,
+        getInitialSnapshots(thread)
+      );
+
+      if (toAdd.length > 0) {
+        const updatedThread = await app.threads.addLinks({
           threadId: thread.id,
-          snapshotProposal: tempSnapshotProposals[0]?.id,
+          links: toAdd.map((sn) => ({
+            source: LinkSource.Snapshot,
+            identifier: String(sn.id),
+            title: sn.title,
+          })),
         });
+
+        links = updatedThread.links;
+      }
+
+      if (toDelete.length > 0) {
+        const updatedThread = await app.threads.deleteLinks({
+          threadId: thread.id,
+          links: toDelete.map((sn) => ({
+            source: LinkSource.Snapshot,
+            identifier: String(sn.id),
+          })),
+        });
+
+        links = updatedThread.links;
       }
     } catch (err) {
-      console.log('Failed to update linked proposals');
-      throw new Error(
-        err.responseJSON && err.responseJSON.error
-          ? err.responseJSON.error
-          : 'Failed to update linked proposals'
-      );
+      console.log(err);
+      throw new Error('Failed to update proposal links');
     }
 
-    onChangeHandler(tempStage, tempChainEntities, tempSnapshotProposals);
+    try {
+      const { toAdd, toDelete } = getAddedAndDeleted(
+        tempProposals,
+        getInitialProposals(thread),
+        'typeId'
+      );
+
+      if (toAdd.length > 0) {
+        const updatedThread = await app.threads.addLinks({
+          threadId: thread.id,
+          links: toAdd.map(({ typeId }) => ({
+            source: LinkSource.Proposal,
+            identifier: String(typeId),
+          })),
+        });
+
+        links = updatedThread.links;
+      }
+
+      if (toDelete.length > 0) {
+        const updatedThread = await app.threads.deleteLinks({
+          threadId: thread.id,
+          links: toDelete.map(({ typeId }) => ({
+            source: LinkSource.Proposal,
+            identifier: String(typeId),
+          })),
+        });
+
+        links = updatedThread.links;
+      }
+    } catch (err) {
+      console.log(err);
+      throw new Error('Failed to update linked proposals');
+    }
+
+    try {
+      const { toAdd, toDelete } = getAddedAndDeleted(
+        tempCosmosProposals,
+        getInitialCosmosProposals(thread),
+        'identifier'
+      );
+      if (toAdd.length > 0) {
+        const updatedThread = await app.threads.addLinks({
+          threadId: thread.id,
+          links: toAdd.map(({ identifier, title }) => ({
+            source: LinkSource.Proposal,
+            identifier: identifier,
+            title: title,
+          })),
+        });
+
+        links = updatedThread.links;
+      }
+
+      if (toDelete.length > 0) {
+        const updatedThread = await app.threads.deleteLinks({
+          threadId: thread.id,
+          links: toDelete.map(({ identifier }) => ({
+            source: LinkSource.Proposal,
+            identifier: String(identifier),
+          })),
+        });
+
+        links = updatedThread.links;
+      }
+    } catch (err) {
+      console.log(err);
+      throw new Error('Failed to update linked proposals');
+    }
+
+    onChangeHandler?.(tempStage, links);
     onModalClose();
   };
 
@@ -112,18 +218,38 @@ export const UpdateProposalStatusModal = ({
   const handleSelectProposal = (sn: SnapshotProposal) => {
     const isSelected = tempSnapshotProposals.find(({ id }) => sn.id === id);
 
-    setTempSnapshotProposals(isSelected ? [] : [sn]);
+    setTempSnapshotProposals(
+      isSelected ? [] : [{ id: sn.id, title: sn.title }]
+    );
     setVotingStage();
   };
 
-  const handleSelectChainEntity = (ce: ChainEntity) => {
-    const isSelected = tempChainEntities.find(({ id }) => ce.id === id);
+  const handleSelectChainEntity = (ce: { typeId: string }) => {
+    const isSelected = tempProposals.find(
+      ({ typeId }) => ce.typeId === String(typeId)
+    );
 
     const updatedChainEntities = isSelected
-      ? tempChainEntities.filter(({ id }) => ce.id !== id)
-      : [...tempChainEntities, ce];
+      ? tempProposals.filter(({ typeId }) => ce.typeId !== String(typeId))
+      : [...tempProposals, ce];
 
-    setTempChainEntities(updatedChainEntities);
+    setTempProposals(updatedChainEntities);
+    setVotingStage();
+  };
+
+  const handleSelectCosmosProposal = (proposal: {
+    identifier: string;
+    title: string;
+  }) => {
+    const isSelected = tempCosmosProposals.find(
+      ({ identifier }) => proposal.identifier === String(identifier)
+    );
+    const updatedProposals = isSelected
+      ? tempCosmosProposals.filter(
+          ({ identifier }) => proposal.identifier !== String(identifier)
+        )
+      : [...tempCosmosProposals, proposal];
+    setTempCosmosProposals(updatedProposals);
     setVotingStage();
   };
 
@@ -155,10 +281,16 @@ export const UpdateProposalStatusModal = ({
             snapshotProposalsToSet={tempSnapshotProposals}
           />
         )}
-        {app.chainEntities && (
+        {showChainEvents && (
           <ChainEntitiesSelector
             onSelect={handleSelectChainEntity}
-            chainEntitiesToSet={tempChainEntities}
+            proposalsToSet={tempProposals}
+          />
+        )}
+        {isCosmos && (
+          <ProposalSelector
+            onSelect={handleSelectCosmosProposal}
+            proposalsToSet={tempCosmosProposals}
           />
         )}
         <div className="buttons-row">
