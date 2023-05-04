2d5ddd165e325ec60987a8481a0bcbbc0b3dd054
Timothee Legros <timothee@ualberta.ca>
2023-05-03 18:35:00 -0600

    revert to chain host

--- packages/chain-events/chain-testing/src/routes/chain.ts
+++ packages/chain-events/chain-testing/src/routes/chain.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
 import Web3 from 'web3';
-import getProvider from '../utils/getProvider';
+import getProvider, {providerUrl} from '../utils/getProvider';
 import axios from 'axios';
 import { chainAdvanceTime, chainGetEth } from '../types';
 
@@ -51,7 +51,7 @@ export const advanceEvmTime = async (
 ) => {
   const advance_secs = Web3.utils.numberToHex(time).toString();
   await axios.post(
-    'http://127.0.0.1:8545',
+    providerUrl,
     // '{"jsonrpc": "2.0", "id": 1, "method": "evm_increaseTime", "params": ["0x15180"] }',
     {
       jsonrpc: '2.0',
@@ -66,7 +66,7 @@ export const advanceEvmTime = async (
     }
   );
   await axios.post(
-    'http://127.0.0.1:8545',
+    providerUrl,
     {
       jsonrpc: '2.0',
       id: 1,
