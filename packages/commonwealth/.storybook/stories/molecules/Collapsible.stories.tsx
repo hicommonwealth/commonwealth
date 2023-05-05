5aa889c72e511ca328b07da478ac4caa4983884b
Daniel Martins <martinsda@gmail.com>
2023-05-05 17:32:15 -0400

    Add collapsible stories

---
+++ packages/commonwealth/.storybook/stories/molecules/Collapsible.stories.tsx
@@ -0,0 +1,24 @@
+import React, { FC, useState } from 'react';
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { CWCollapsible } from '../../../client/scripts/views/components/component_kit/cw_collapsible';
+import { CWText } from '../../../client/scripts/views/components/component_kit/cw_text';
+
+const collapsible = {
+  title: 'Molecules/Collapsible',
+  component: CWCollapsible,
+} satisfies Meta<typeof CWCollapsible>;
+
+export default collapsible;
+// type Story = StoryObj<typeof collapsible>;
+
+// export const Collapsible: Story = {
+export const Collapsible = {
+  name: 'Collapsible',
+  render: () => (
+    <CWCollapsible
+      headerContent={<CWText>Header content</CWText>}
+      collapsibleContent={<CWText>Body content</CWText>}
+    />
+  )
+}
