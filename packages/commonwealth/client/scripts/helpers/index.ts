90b4f93e2407a0f06dae299c872d9d16947931de
Ian Rowan <36428666+ianrowan@users.noreply.github.com>
2023-05-08 16:14:16 -0500

    default stages for non-parseable stage values (#3756)

--- packages/commonwealth/client/scripts/helpers/index.ts
+++ packages/commonwealth/client/scripts/helpers/index.ts
@@ -32,10 +32,18 @@ export function parseCustomStages(str) {
   // Parse customStages into a `string[]` and then cast to ThreadStage[]
   // If parsing fails, return an empty array.
   let arr;
+  const default_stages = [
+    ThreadStage.Discussion,
+    ThreadStage.ProposalInReview,
+    ThreadStage.Voting,
+    ThreadStage.Passed,
+    ThreadStage.Failed,
+  ];
   try {
-    arr = Array.from(JSON.parse(str));
+    const stages = JSON.parse(str);
+    arr = Array.isArray(stages) ? Array.from(stages) : default_stages;
   } catch (e) {
-    return [];
+    return default_stages;
   }
   return arr
     .map((s) => s?.toString())
