5a4d287872ebf0964d8d030dc76fcf821c364eaf
kassad <kurtis@commonwealth.im>
2023-05-09 12:59:20 -0700

    Removed BlockInfo from models/index.ts

--- packages/commonwealth/client/scripts/models/index.ts
+++ packages/commonwealth/client/scripts/models/index.ts
@@ -1,4 +1,3 @@
-export { default as BlockInfo } from './BlockInfo';
 export { default as ChainInfo } from './ChainInfo';
 export { default as DiscussionDraft } from './DiscussionDraft';
 export { default as IChainAdapter } from './IChainAdapter';
