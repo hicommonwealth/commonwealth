baa0bea84d3f2e71ca8c1c7c3e3ea759afc44323
Daniel Martins <martinsda@gmail.com>
2023-05-09 10:45:08 -0400

    Adds Storybook and stories for components (#3758)
    * first commit
    
    * Add chromatic to package.
    
    ---------
    
    Co-authored-by: Jake Naviasky <jake@commonwealth.im>

---
+++ packages/commonwealth/.storybook/stories/organisms/CoverImageUploader.stories.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { CWCoverImageUploader } from '../../../client/scripts/views/components/component_kit/cw_cover_image_uploader';
+import { notifySuccess } from '../../../client/scripts/controllers/app/notifications';
+
+const coverImageUploader = {
+  title: 'Organisms/Image Upload',
+  component: CWCoverImageUploader,
+} satisfies Meta<typeof CWCoverImageUploader>;
+
+export default coverImageUploader;
+// type Story = StoryObj<typeof coverImageUploader>;
+
+// export const ImageUpload: Story = {
+export const ImageUpload  = {
+  name: 'Image Upload ',
+  render: () => (
+    <CWCoverImageUploader
+      uploadCompleteCallback={(url: string) => {
+        notifySuccess(`Image uploaded to ${url.slice(0, 18)}...`);
+      }}
+      enableGenerativeAI
+    />
+  )
+}
