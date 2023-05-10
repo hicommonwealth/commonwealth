bf654c7d5a0e34044d9d378179af441d8366a202
Alex Young <alexyoung23j@gmail.com>
2023-05-10 15:40:00 -0400

    Bet1 Fast Follows Pt 2 (#3536)
    * start
    
    * bet1 fast follows pt 1
    
    * prog
    
    * show enabled by on contract card
    
    * fast follows
    
    * cleanup
    
    * proig
    
    * bet1 fast follows part 2
    
    * bet1 fast follows 2 cleanup
    
    * fixes for fast follow 2
    
    ---------
    
    Co-authored-by: Alex Young <alexyoung@MacBook-Pro-2.local>
    Co-authored-by: Alex Young <alexyoung@macbook-pro-2.mynetworksettings.com>
    Co-authored-by: Jake Naviasky <jake@commonwealth.im>

--- packages/commonwealth/client/scripts/views/pages/view_snapshot_proposal/index.tsx
+++ packages/commonwealth/client/scripts/views/pages/view_snapshot_proposal/index.tsx
@@ -50,6 +50,7 @@ export const ViewProposalPage = ({
   const validatedAgainstStrategies: boolean = !power
     ? true
     : power.totalScore > 0;
+
   const totalScore: number = power?.totalScore || 0;
   const votes: SnapshotProposalVote[] = voteResults?.votes || [];
   const totals: VoteResultsData = voteResults?.results || {
