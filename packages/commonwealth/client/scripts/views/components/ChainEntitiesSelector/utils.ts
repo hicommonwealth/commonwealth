4f24a864ed1ec62b1725aae4d577ed9bf7e247c5
Marcin Maslanka <marcin@commonwealth.im>
2023-05-11 17:47:14 +0200

    merge conflicts

--- packages/commonwealth/client/scripts/views/components/ChainEntitiesSelector/utils.ts
+++ packages/commonwealth/client/scripts/views/components/ChainEntitiesSelector/utils.ts
@@ -1,18 +1,6 @@
 import ChainEntity from 'models/ChainEntity';
 import { chainEntityTypeToProposalName } from 'identifiers';
 
-export const sortChainEntities = (a: ChainEntity, b: ChainEntity) => {
-  if (!a.threadId && b.threadId) {
-    return -1;
-  }
-
-  if (a.threadId && !b.threadId) {
-    return 1;
-  }
-
-  return 0;
-};
-
 export const filterChainEntities = (ce: ChainEntity, searchTerm: string) => {
   if (ce.typeId.startsWith('0x')) {
     return false;
