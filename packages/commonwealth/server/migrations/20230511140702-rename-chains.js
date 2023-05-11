8dfbe60d18a6f47aeca671efc2a17c5dbe90e2ce
ianrowan <i.rowan299@gmail.com>
2023-05-11 15:00:17 -0500

    convert migration to transaction

--- packages/commonwealth/server/migrations/20230511140702-rename-chains.js
+++ packages/commonwealth/server/migrations/20230511140702-rename-chains.js
@@ -3,27 +3,77 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     return queryInterface.sequelize.transaction(async (transaction) => {
-      await queryInterface.renameTable('Chains', 'Communities');
+      await queryInterface.renameTable('Chains', 'Communities', {
+        transaction,
+      });
 
-      const tables = await queryInterface.showAllTables();
+      const tables = await queryInterface.showAllTables({ transaction });
       for (const table of tables) {
-        const columns = await queryInterface.describeTable(table);
-        const changeColumnName = Object.keys(columns).includes("chain") ? "chain" : 
-        Object.keys(columns).includes("chain_id") ? "chain_id" : null;
-        if(changeColumnName){
-          console.log(table)
-          await queryInterface.renameColumn(table, changeColumnName, "community_id",)
+        const [results] = await queryInterface.sequelize.query(
+          `
+          SELECT column_name
+          FROM information_schema.columns
+          WHERE table_name = '${table}'
+        `,
+          { transaction }
+        );
+        const changeColumnName = results.find(
+          (column) =>
+            column.column_name === 'chain' || column.column_name === 'chain_id'
+        );
+        if (changeColumnName) {
+          console.log(table);
+          await queryInterface.renameColumn(
+            table,
+            changeColumnName.column_name,
+            'community_id',
+            { transaction }
+          );
         }
       }
-    })
+    });
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-  }
+    // This is the only way I know how to maintain/map the varying column names for reversion
+    const prevTableColumns = {
+      ChatChannels: 'chain_id',
+      Bans: 'chain_id',
+      Comments: 'chain',
+      ChainEntityMeta: 'chain',
+      CommunityContracts: 'chain_id',
+      CommunitySnapshotSpaces: 'chain_id',
+      CommunityBanners: 'chain_id',
+      CommunityRoles: 'chain_id',
+      DiscordBotConfig: 'chain_id',
+      DiscussionDrafts: 'chain',
+      Polls: 'chain_id',
+      Reactions: 'chain',
+      Topics: 'chain_id',
+      Votes: 'chain_id',
+      Notifications: 'chain_id',
+      Threads: 'chain',
+      OldChatMessages: 'chain',
+      Roles: 'chain_id',
+      StarredCommunities: 'chain',
+      Tokens: 'chain_id',
+      Subscriptions: 'chain_id',
+      Webhooks: 'chain_id',
+      Addresses: 'chain',
+      Rules: 'chain_id',
+    };
+    return queryInterface.sequelize.transaction(async (transaction) => {
+      await queryInterface.renameTable('Communities', 'Chains', {
+        transaction,
+      });
+      for (const table of Object.keys(prevTableColumns)) {
+        await queryInterface.renameColumn(
+          table,
+          'community_id',
+          prevTableColumns[table],
+          { transaction }
+        );
+      }
+    });
+  },
 };
