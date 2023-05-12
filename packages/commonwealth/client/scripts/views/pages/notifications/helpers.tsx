8bef33f43fdb62fcb9530bd9a1c0ce8f28bc6c3d
Jake Naviasky <jake@commonwealth.im>
2023-05-11 21:50:57 -0400

    Clean up getThreadUrl usage to fix invalid notification links. (#3835)

--- packages/commonwealth/client/scripts/views/pages/notifications/helpers.tsx
+++ packages/commonwealth/client/scripts/views/pages/notifications/helpers.tsx
@@ -133,11 +133,7 @@ const getNotificationFields = (category, data: IPostNotificationData) => {
     chain: chain_id,
   };
 
-  const args = comment_id
-    ? [root_type, pseudoProposal, { id: comment_id }]
-    : [root_type, pseudoProposal];
-
-  const path = (getThreadUrl as any)(...args);
+  const path = getThreadUrl(pseudoProposal, comment_id);
 
   const pageJump = comment_id
     ? () => jumpHighlightNotification(comment_id)
@@ -259,14 +255,10 @@ export const getBatchNotificationFields = (
     chain: chain_id,
   };
 
-  const args = comment_id
-    ? [pseudoProposal, { id: comment_id }]
-    : [pseudoProposal, undefined];
-
   const path =
     category === NotificationCategories.NewThread
-      ? (getCommunityUrl as any)(chain_id)
-      : (getThreadUrl as any)(...args);
+      ? getCommunityUrl(chain_id)
+      : getThreadUrl(pseudoProposal, comment_id);
 
   const pageJump = comment_id
     ? () => jumpHighlightNotification(comment_id)
