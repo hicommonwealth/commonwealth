c91de780b3aafe25776c1488f9163dcd6240303b
mhagel <mark.hagelberg@common.xyz>
2023-05-12 15:08:55 -0600

    cleanup

--- packages/commonwealth/client/scripts/views/Layout.context.tsx
+++ packages/commonwealth/client/scripts/views/Layout.context.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useState } from 'react';
+import { createContext } from 'react';
 
 type LayoutContextProps = {
   onRerender: () => void;
@@ -11,20 +11,3 @@ const initialValues = {
 };
 
 export const LayoutContext = createContext<LayoutContextProps>(initialValues);
-
-// export const LayoutContextProvider = ({ children }) => {
-//   const [renderKey, setRenderKey] = useState('');
-
-//   const onRerender = () => {
-//     console.log('onRerender');
-//     setRenderKey(Date.now().toString());
-//   };
-
-//   console.log('renderKey', renderKey);
-
-//   return (
-//     <LayoutContext.Provider value={{ onRerender, renderKey }}>
-//       {children}
-//     </LayoutContext.Provider>
-//   );
-// };
--- packages/commonwealth/client/scripts/views/pages/overview/index.tsx
+++ packages/commonwealth/client/scripts/views/pages/overview/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import type Thread from '../../../models/Thread';
 import type Topic from '../../../models/Topic';
