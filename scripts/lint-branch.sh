b0f4a350b10da49e2852d25238cede41fc669c3c
Timothee Legros <timothee@ualberta.ca>
2023-05-15 20:00:01 +0200

    fetch master for branch linting

--- scripts/lint-branch.sh
+++ scripts/lint-branch.sh
@@ -1,3 +1,9 @@
+# Check if the master branch exists
+if ! git show-ref --quiet refs/remotes/origin/master; then
+    # Fetch the master branch from the origin
+    git fetch origin master
+fi
+
 LINES=$(git diff master... --name-only --diff-filter=d | grep \\.ts)
 
 if [ -z "$LINES" ]
