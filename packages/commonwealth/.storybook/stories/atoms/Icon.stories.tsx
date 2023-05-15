76fd18e601a0ccf3c2ee2c21d6f92938b65eaa69
Daniel Martins <martinsda@gmail.com>
2023-05-15 12:18:23 -0300

    Add controls to Storybook (#3816)
    * first commit
    
    * Add chromatic to package.
    
    * Add controls to Button
    
    * Edit Button story imports
    
    * Add controls to Icon
    
    * Add controls to Text
    
    * Add controls to Toggle Overview
    
    * Edit Icon stories
    
    * Add controls to Breadcrumbs
    
    * Add controls to Checkbox
    
    * Add controls to RadioButton
    
    * Add controls to Tag
    
    * Add controls to Tabs
    
    * Add controls to Card
    
    * Add controls to Dropdown
    
    * Add controls to TextInput
    
    * Add controls to Tooltip
    
    * Add controls to VoteButton
    
    * Add controls to FilterMenu
    
    * Add controls to Collapsible
    
    * Add controls to ProgressBar
    
    * Add controls to Textarea
    
    * Add controls to TextInput with validation
    
    * Add controls to PopoverMenu
    
    ---------
    
    Co-authored-by: Jake Naviasky <jake@commonwealth.im>

---
+++ packages/commonwealth/.storybook/stories/atoms/Icon.stories.tsx
@@ -0,0 +1,85 @@
+import React, { FC, useState } from 'react';
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { CWIcon } from '../../../client/scripts/views/components/component_kit/cw_icons/cw_icon';
+import { CWIconButton } from '../../../client/scripts/views/components/component_kit/cw_icon_button';
+import { iconLookup } from '../../../client/scripts/views/components/component_kit/cw_icons/cw_icon_lookup';
+import { IconComponentProps } from '../../../client/scripts/views/components/component_kit/cw_icons/types';
+
+const iconOptions = [ ...Object.keys(iconLookup) ];
+
+const icons = {
+  title: 'Atoms/Icon',
+  component: CWIcon,
+} satisfies Meta<typeof CWIcon>;
+
+export default icons;
+type Story = StoryObj<typeof icons>;
+
+const IconButton: FC<IconComponentProps> = (props) => {
+  const { selected = false } = props;
+  const [isSelected, setIsSelected] = useState<boolean>(selected);
+
+  return (
+    <CWIconButton
+      {...props}
+      selected={isSelected}
+      onClick={() => setIsSelected(isSelected => !isSelected)}
+    />
+  );
+};
+
+const commonArgTypes = {
+  argTypes: {
+    iconName: {
+      control: { type: "select" },
+      options: iconOptions,
+    },
+    iconSize: {
+      control: { type: "select" },
+      options: ["xxs", "xs", "small", "medium", "large", "xl", "xxl"],
+    },
+  },
+};
+
+export const IconStory: Story = {
+  name: "Overview",
+  args: {
+    iconName: "arrowLeft",
+    iconSize: "medium",
+    disabled: false,
+  },
+  argTypes: {
+    ...commonArgTypes,
+    disabled: {
+      options: [ true, false ],
+    },
+  },
+  render: ({...args}) => <CWIcon {...args} />
+}
+
+export const IconButtonStory: Story = {
+  name: "Icon Button",
+  args: {
+    iconName: "arrowLeft",
+    iconSize: "medium",
+    iconButtonTheme: undefined,
+    disabled: false,
+    selected: false,
+  },
+  argTypes: {
+    ...commonArgTypes,
+    iconButtonTheme: {
+      control: { type: "select" },
+      options: ["black", "neutral", "primary", "hasBackground"],
+    },
+    disabled: {
+      options: [ true, false ],
+    },
+    selected: {
+      options: [ true, false ],
+    },
+    // TODO handle selected
+  },
+  render: ({...args}) => <IconButton {...args} />
+}
