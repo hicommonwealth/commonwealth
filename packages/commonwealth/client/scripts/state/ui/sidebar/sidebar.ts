d814d91b837cbcf2b615013f2a30c0e8e70f6dcc
Marcin Maslanka <marcin@commonwealth.im>
2023-05-16 13:29:04 +0200

    Add devtools to store

--- packages/commonwealth/client/scripts/state/ui/sidebar/sidebar.ts
+++ packages/commonwealth/client/scripts/state/ui/sidebar/sidebar.ts
@@ -1,4 +1,5 @@
 import { createStore } from 'zustand/vanilla';
+import { devtools } from 'zustand/middleware';
 import { createBoundedUseStore } from 'state/ui/utils';
 import { SidebarMenuName } from 'views/components/sidebar';
 import { MobileMenuName } from 'views/AppMobileMenus';
@@ -17,18 +18,20 @@ interface SidebarStore {
   }) => void;
 }
 
-export const sidebarStore = createStore<SidebarStore>()((set) => ({
-  mobileMenuName: null,
-  setMobileMenuName: (name) => set(() => ({ mobileMenuName: name })),
-  menuName: 'default',
-  menuVisible: false,
-  setMenu: ({ name, isVisible }) =>
-    set((state) => ({
-      menuName: name,
-      menuVisible:
-        typeof isVisible === 'boolean' ? isVisible : state.menuVisible,
-    })),
-}));
+export const sidebarStore = createStore<SidebarStore>()(
+  devtools((set) => ({
+    mobileMenuName: null,
+    setMobileMenuName: (name) => set(() => ({ mobileMenuName: name })),
+    menuName: 'default',
+    menuVisible: false,
+    setMenu: ({ name, isVisible }) =>
+      set((state) => ({
+        menuName: name,
+        menuVisible:
+          typeof isVisible === 'boolean' ? isVisible : state.menuVisible,
+      })),
+  }))
+);
 
 const useSidebarStore = createBoundedUseStore(sidebarStore);
 
