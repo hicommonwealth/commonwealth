e86ff080e4217ee195bd732833f2c4b07c71c610
dillchen <dillon@commonwealth.im>
2023-05-16 19:52:27 -0400

    add copy pasta template modal

---
+++ packages/commonwealth/client/scripts/views/modals/template_action_modal.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Modal } from '../components/component_kit/cw_modal';
+import { ViewTemplateForm } from '../pages/view_template'; // Import the extracted form component
+
+type TemplateFormModalProps = {
+  isOpen: boolean;
+  threadContent: string; // Pass the thread content to the form
+  onSave: () => void;
+  onClose: () => void;
+};
+
+export const TemplateActionModal = ({
+  isOpen,
+  threadContent,
+  onSave,
+  onClose,
+}: TemplateFormModalProps) => {
+  return (
+    <Modal
+      content={
+        <ViewTemplateForm
+          threadContent={threadContent}
+          onSave={onSave}
+          onModalClose={onClose}
+        />
+      }
+      onClose={onClose}
+      open={isOpen}
+    />
+  );
+};
--- packages/commonwealth/client/scripts/views/pages/view_thread/template_action_card.tsx
+++ packages/commonwealth/client/scripts/views/pages/view_thread/template_action_card.tsx
@@ -1,28 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { CWButton } from '../../components/component_kit/cw_button';
 import { CWContentPageCard } from '../../components/component_kit/cw_content_page';
+import { TemplateActionModal } from '../../modals/template_action_modal'; // Import the new modal component
 
 import 'pages/view_thread/template_action_card.scss';
 
 type TemplateActionCardProps = {
-  onButtonClick: () => void;
+  threadContent: string; // Pass the thread content to the modal
 };
 
-export const TemplateActionCard: React.FC<TemplateActionCardProps> = ({
-  onButtonClick,
-}) => {
+export const TemplateActionCard = ({
+  threadContent,
+}: TemplateActionCardProps) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   return (
-    <CWContentPageCard
-      header="Turn into Proposal"
-      content={
-        <div className="TemplateActionCard">
-          <CWButton
-            buttonType="mini-black"
-            label="Turn into proposal"
-            onClick={onButtonClick}
-          />
-        </div>
-      }
-    />
+    <div>
+      <CWContentPageCard
+        header="Turn into Proposal"
+        content={
+          <div className="TemplateActionCard">
+            <CWButton
+              buttonType="primary-black"
+              label="Turn into proposal"
+              onClick={() => setIsModalOpen(true)}
+            />
+          </div>
+        }
+      />
+      <TemplateActionModal
+        isOpen={isModalOpen}
+        threadContent={threadContent}
+        onSave={() => setIsModalOpen(false)}
+        onClose={() => setIsModalOpen(false)}
+      />
+    </div>
   );
 };
