d20c193d463199eccb1e2d0bbdfa57b3e22f0add
Marcin Maslanka <marcin@commonwealth.im>
2023-05-16 15:27:20 +0200

    Comment

--- packages/commonwealth/client/scripts/state/ui/utils.ts
+++ packages/commonwealth/client/scripts/state/ui/utils.ts
@@ -2,6 +2,7 @@ import { StoreApi, useStore } from 'zustand';
 
 type ExtractState<S> = S extends { getState: () => infer X } ? X : never;
 
+// This util creates react-hook store out of vanilla store
 export const createBoundedUseStore = ((store) => (selector, equals) =>
   useStore(store, selector as never, equals)) as <S extends StoreApi<unknown>>(
   store: S
