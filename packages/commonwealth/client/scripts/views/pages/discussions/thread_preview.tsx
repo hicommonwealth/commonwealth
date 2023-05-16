db7ca459b19dc60e6eee9d70059d236ef75ade82
ianrowan <i.rowan299@gmail.com>
2023-05-16 17:24:13 -0500

    update snapshot tag logic

--- packages/commonwealth/client/scripts/views/pages/discussions/thread_preview.tsx
+++ packages/commonwealth/client/scripts/views/pages/discussions/thread_preview.tsx
@@ -51,10 +51,8 @@ type ThreadPreviewProps = {
 
 export const ThreadPreview = ({ thread }: ThreadPreviewProps) => {
   const [isChangeTopicModalOpen, setIsChangeTopicModalOpen] = useState(false);
-  const [
-    isUpdateProposalStatusModalOpen,
-    setIsUpdateProposalStatusModalOpen,
-  ] = useState(false);
+  const [isUpdateProposalStatusModalOpen, setIsUpdateProposalStatusModalOpen] =
+    useState(false);
 
   const [windowIsSmall, setWindowIsSmall] = useState(
     isWindowSmallInclusive(window.innerWidth)
@@ -183,7 +181,10 @@ export const ThreadPreview = ({ thread }: ThreadPreviewProps) => {
             {linkedSnapshots.length > 0 && (
               <CWTag
                 type="active"
-                label={`Snap ${linkedSnapshots[0].identifier
+                label={`Snap ${(linkedSnapshots[0].identifier.split('/')
+                  ? linkedSnapshots[0].identifier.split('/')[1]
+                  : linkedSnapshots[0].identifier
+                )
                   .toString()
                   .slice(0, 4)}…`}
               />
