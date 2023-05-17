e6e8995520909256aaf4875e9639874c0e278c10
Malik Zulqurnain <malikzulqurnain2121@gmail.com>
2023-05-18 00:41:42 +0500

    Added phosphor icons to lookup

--- packages/commonwealth/client/scripts/views/components/component_kit/cw_icons/cw_icon_lookup.ts
+++ packages/commonwealth/client/scripts/views/components/component_kit/cw_icons/cw_icon_lookup.ts
@@ -1,5 +1,14 @@
+import {
+  CaretDown,
+  CaretUp,
+  ChatDots,
+  ClockCounterClockwise,
+  Heart,
+  Sparkle,
+} from '@phosphor-icons/react';
 import * as CustomIcons from './cw_custom_icons';
 import * as Icons from './cw_icons';
+import { withPhosphorIcon } from './cw_phosphor_icons';
 
 export const iconLookup = {
   arrowLeft: Icons.CWArrowLeft,
@@ -9,12 +18,15 @@ export const iconLookup = {
   bell: Icons.CWBell,
   cautionCircle: Icons.CWCautionCircle,
   cautionTriangle: Icons.CWCautionTriangle,
+  chatDots: withPhosphorIcon(ChatDots),
   check: Icons.CWCheck,
   checkCircle: Icons.CWCheckCircle,
   chevronDown: Icons.CWChevronDown,
   chevronLeft: Icons.CWChevronLeft,
   chevronRight: Icons.CWChevronRight,
   chevronUp: Icons.CWChevronUp,
+  carotUp: withPhosphorIcon(CaretUp),
+  carotDown: withPhosphorIcon(CaretDown),
   clock: Icons.CWClock,
   close: Icons.CWClose,
   cloud: Icons.CWCloud,
@@ -22,6 +34,7 @@ export const iconLookup = {
   commonLogo: Icons.CWCommonLogo,
   compass: Icons.CWCompass,
   copy: Icons.CWCopy,
+  clockCounterClockwise: withPhosphorIcon(ClockCounterClockwise),
   cow: Icons.CWCow,
   curator: Icons.CWCurator,
   delegate: Icons.CWDelegate,
@@ -42,6 +55,7 @@ export const iconLookup = {
   github: Icons.CWGithub,
   hamburger: Icons.CWHamburger,
   hash: Icons.CWHash,
+  heart: withPhosphorIcon(Heart),
   heartEmpty: Icons.CWHeartEmpty,
   heartFilled: Icons.CWHeartFilled,
   help: Icons.CWHelp,
@@ -66,6 +80,7 @@ export const iconLookup = {
   share2: Icons.CWShare2,
   sidebarCollapse: Icons.CWSidebarCollapse,
   sidebarExpand: Icons.CWSidebarExpand,
+  sparkle: withPhosphorIcon(Sparkle),
   star: Icons.CWStar,
   sun: Icons.CWSun,
   telegram: Icons.CWTelegram,
