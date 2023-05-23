4b54e1ed52b5139ef10774aa938e03b01c9be613
dillchen <dillon@commonwealth.im>
2023-05-22 21:21:52 -0400

    extra message

--- packages/commonwealth/client/scripts/views/modals/template_action_modal.tsx
+++ packages/commonwealth/client/scripts/views/modals/template_action_modal.tsx
@@ -72,7 +72,7 @@ export const TemplateActionModal = ({
       (_cct) => String(_cct.templateId) === identifier
     );
 
-    const newIdentifier = `${identifier}/${contract.address}${cct.cctmd.slug}`;
+    const newIdentifier = `${identifier}/${contract.address}${cct?.cctmd.slug}`;
 
     return { contract, cct, newIdentifier };
   };
