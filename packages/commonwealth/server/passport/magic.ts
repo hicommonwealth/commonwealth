d77996b263e9b54afcae6b2bab28519d14a58c44
Jake Naviasky <jake@commonwealth.im>
2023-05-23 17:59:29 -0400

    Always use server-side Cosmos address.

--- packages/commonwealth/server/passport/magic.ts
+++ packages/commonwealth/server/passport/magic.ts
@@ -1,8 +1,8 @@
-import type { MagicUserMetadata } from '@magic-sdk/admin';
+import { MagicUserMetadata, MagicWallet, WalletType } from '@magic-sdk/admin';
 import { Magic } from '@magic-sdk/admin';
 
 import { AppError, ServerError } from 'common-common/src/errors';
-import { NotificationCategories, WalletId } from 'common-common/src/types';
+import { ChainBase, NotificationCategories, WalletId } from 'common-common/src/types';
 import passport from 'passport';
 import { Strategy as MagicStrategy } from 'passport-magic';
 import { MAGIC_API_KEY, MAGIC_SUPPORTED_BASES } from '../config';
@@ -13,6 +13,7 @@ import type { ProfileAttributes } from '../models/profile';
 
 import '../types';
 import { createRole } from '../util/roles';
+import { ChainAttributes } from '../models/chain';
 
 export function initMagicAuth(models: DB) {
   // allow magic login if configured with key
@@ -22,17 +23,20 @@ export function initMagicAuth(models: DB) {
     passport.use(
       new MagicStrategy({ passReqToCallback: true }, async (req, user, cb) => {
         // determine login location
-        let chain, error, registrationChainAddress;
+        let chain, error;
         if (req.body.chain || req.body.community) {
           [chain, error] = await validateChain(models, req.body);
           if (error) return cb(error);
         }
 
-        if (req.body.address) {
-          registrationChainAddress = req.body.address;
-        }
+        // grab cosmos address for registration
+        const cosmosMetadata = await magic.users.getMetadataByIssuerAndWallet(
+          user.issuer,
+          WalletType.COSMOS
+        );
+        const registrationChainAddress = (cosmosMetadata?.wallets[0] as any)?.public_address;
 
-        const registrationChain = chain;
+        const registrationChain: ChainAttributes = chain;
 
         // fetch user data from magic backend
         let userMetadata: MagicUserMetadata;
@@ -90,10 +94,13 @@ export function initMagicAuth(models: DB) {
             );
 
             // create a default Eth address
+            const ethereumChain = registrationChain.base === ChainBase.Ethereum
+              ? registrationChain.id
+              : 'ethereum';
             const newDefaultAddress = await models.Address.create(
               {
                 address: ethAddress,
-                chain: 'ethereum',
+                chain: ethereumChain,
                 verification_token: 'MAGIC',
                 verification_token_expires: null,
                 verified: new Date(), // trust addresses from magic
@@ -108,7 +115,7 @@ export function initMagicAuth(models: DB) {
             await createRole(
               models,
               newDefaultAddress.id,
-              'ethereum',
+              ethereumChain,
               'member',
               false,
               t
@@ -116,10 +123,13 @@ export function initMagicAuth(models: DB) {
 
             if (registrationChainAddress) {
               // create an address on their selected chain
+              const cosmosChain = registrationChain.base === ChainBase.CosmosSDK
+                ? registrationChain.id
+                : 'osmosis';
               const newRegistrationChainAddress = await models.Address.create(
                 {
                   address: registrationChainAddress,
-                  chain: registrationChain.id,
+                  chain: cosmosChain,
                   verification_token: 'MAGIC',
                   verification_token_expires: null,
                   verified: new Date(), // trust addresses from magic
@@ -133,7 +143,7 @@ export function initMagicAuth(models: DB) {
               await createRole(
                 models,
                 newRegistrationChainAddress.id,
-                registrationChain.id,
+                cosmosChain,
                 'member',
                 false,
                 t
@@ -236,11 +246,12 @@ export function initMagicAuth(models: DB) {
               const [newRegistrationChainAddress, created] =
                 await models.Address.findOrCreate({
                   where: {
-                    address: registrationChainAddress,
+                    address: registrationChain.base === ChainBase.CosmosSDK
+                      ? registrationChainAddress
+                      : userMetadata.publicAddress,
                     chain: registrationChain.id,
                     user_id: existingUser.id,
-                    profile_id: (existingUser.Profiles[0] as ProfileAttributes)
-                      .id,
+                    profile_id: (existingUser.Profiles[0] as ProfileAttributes).id,
                     wallet_id: WalletId.Magic,
                   },
                   defaults: {
@@ -294,8 +305,7 @@ export function initMagicAuth(models: DB) {
                   verified: new Date(), // trust addresses from magic
                   last_active: new Date(),
                   user_id: existingUser.id,
-                  profile_id: (existingUser.Profiles[0] as ProfileAttributes)
-                    .id,
+                  profile_id: (existingUser.Profiles[0] as ProfileAttributes).id,
                   wallet_id: WalletId.Magic,
                 },
                 { transaction: t }
