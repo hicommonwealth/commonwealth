47acf344cd5e6f8adf48bbfa283ff131008c7886
Ryan Bennett <ryan@common.xyz>
2023-05-24 11:03:36 -0700

    lint

--- packages/commonwealth/client/scripts/views/pages/members.tsx
+++ packages/commonwealth/client/scripts/views/pages/members.tsx
@@ -8,7 +8,6 @@ import MinimumProfile from '../../models/MinimumProfile';
 import { User } from 'views/components/user/user';
 import Sublayout from 'views/Sublayout';
 import { CWText } from '../components/component_kit/cw_text';
-import { AccessLevel } from 'permissions';
 import { useDebounce } from 'usehooks-ts';
 import { MembersSearchBar } from '../components/members_search_bar';
 
@@ -93,11 +92,13 @@ const MembersPage = () => {
     if (debouncedSearchTerm.length >= 3) {
       handleLoadNextPage(debouncedSearchTerm, true);
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [debouncedSearchTerm]);
 
   // on init, load first page
   useEffect(() => {
     handleLoadNextPage('', true);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
