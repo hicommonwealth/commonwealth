a7344af2c16181ee0d712222c3df86d7c1e28707
Timothee Legros <timothee@ualberta.ca>
2023-05-25 17:57:03 +0200

    migrationHandler tests passing

--- packages/chain-events/test/unit/events/migrationHandler.spec.ts
+++ packages/chain-events/test/unit/events/migrationHandler.spec.ts
@@ -4,13 +4,12 @@ import 'chai/register-should';
 import MigrationHandler from 'chain-events/services/ChainEventsConsumer/ChainEventHandlers/migration';
 import StorageHandler from 'chain-events/services/ChainEventsConsumer/ChainEventHandlers/storage';
 import models from 'chain-events/services/database/database';
-
 import type { CWEvent } from 'chain-events/src';
 import { SupportedNetwork } from 'chain-events/src';
-import { SubstrateTypes } from 'chain-events/src/types';
 import { getRabbitMQConfig } from 'common-common/src/rabbitmq';
 import { MockRabbitMQController } from 'common-common/src/rabbitmq/mockRabbitMQController';
 import type { BrokerConfig } from 'rascal';
+import { EventKind, IEventData } from '../../../src/chains/aave/types';
 
 chai.use(chaiHttp);
 const { assert } = chai;
@@ -20,39 +19,47 @@ const rmqController = new MockRabbitMQController(
 );
 
 const setupDbEvent = async (event: CWEvent) => {
-  const storageHandler = new StorageHandler(models, rmqController, 'edgeware');
+  const storageHandler = new StorageHandler(models, rmqController, 'aave');
   return storageHandler.handle(event);
 };
 
-describe('Edgeware Migration Event Handler Tests', () => {
+describe.only('Aave Migration Event Handler Tests', () => {
+  before(async () => {
+    await models.sequelize.sync({ force: true });
+    await rmqController.init();
+  });
+
   it('should create new event', async () => {
     // setup
-    const event: CWEvent<SubstrateTypes.IEventData> = {
+    const event: CWEvent<IEventData> = {
       blockNumber: 10,
-      network: SupportedNetwork.Substrate,
+      network: SupportedNetwork.Aave,
       data: {
-        kind: SubstrateTypes.EventKind.DemocracyStarted,
-        referendumIndex: 0,
+        kind: EventKind.ProposalCreated,
+        id: 1,
+        proposer: '0x327BeaE3B570d9bdD8C6b9236199991Ab2c5fefe',
+        executor: '0x06C2E02aDB73238d7eE7DbD69fEA12B14091cB8a',
+        targets: ['0x6972E49bFbA4dbc4981101724CC87bd18c152cBA'],
+        values: ['testValue'],
+        signatures: ['testSignature'],
+        calldatas: ['testCalldatas'],
+        startBlock: 10,
         endBlock: 100,
-        proposalHash: 'hash',
-        voteThreshold: 'Supermajorityapproval',
+        strategy: 'testStrategy',
+        ipfsHash: 'testIpfsHash',
       },
     };
 
-    const eventHandler = new MigrationHandler(
-      models,
-      rmqController,
-      'edgeware'
-    );
+    const eventHandler = new MigrationHandler(models, rmqController, 'aave');
 
     // process event
     const dbEvent = await eventHandler.handle(event);
 
     // expect results
     assert.deepEqual(dbEvent.event_data, event.data);
-    const chainEvents = await models['ChainEvent'].findAll({
+    const chainEvents = await models.ChainEvent.findAll({
       where: {
-        chain: 'edgeware',
+        chain: 'aave',
         block_number: 10,
       },
     });
@@ -60,68 +67,74 @@ describe('Edgeware Migration Event Handler Tests', () => {
     assert.deepEqual(chainEvents[0].toJSON(), dbEvent.toJSON());
   });
 
-  it('should upgrade existing event', async () => {
-    const legacyEvent: any = {
-      blockNumber: 10,
-      network: SupportedNetwork.Substrate,
+  it('should update an existing event', async () => {
+    const legacyEvent: CWEvent<IEventData> = {
+      blockNumber: 11,
+      network: SupportedNetwork.Aave,
       data: {
-        kind: SubstrateTypes.EventKind.PreimageNoted,
-        proposalHash: 'hash',
-        noter: 'Alice',
+        kind: EventKind.ProposalCreated,
+        id: 2,
+        proposer: '0x327BeaE3B570d9bdD8C6b9236199991Ab2c5fefe',
+        executor: '0x06C2E02aDB73238d7eE7DbD69fEA12B14091cB8a',
+        targets: ['0x6972E49bFbA4dbc4981101724CC87bd18c152cBA'],
+        values: ['testValue'],
+        signatures: ['testSignature'],
+        calldatas: ['testCalldatas'],
+        startBlock: 10,
+        endBlock: 100,
+        strategy: 'testStrategy',
+        ipfsHash: 'testIpfsHash',
       },
     };
-    const currentEvent: CWEvent<SubstrateTypes.IEventData> = {
-      blockNumber: 10,
-      network: SupportedNetwork.Substrate,
+    const currentEvent: CWEvent<IEventData> = {
+      blockNumber: 11,
+      network: SupportedNetwork.Aave,
       data: {
-        kind: SubstrateTypes.EventKind.PreimageNoted,
-        proposalHash: 'hash',
-        noter: 'Alice',
-        preimage: {
-          method: 'method',
-          section: 'section',
-          args: ['arg1', 'arg2'],
-        },
+        kind: EventKind.ProposalCreated,
+        id: 2,
+        proposer: 'newProposer',
+        executor: 'newExecutor',
+        targets: ['newTarget'],
+        values: ['testValue'],
+        signatures: ['testSignature'],
+        calldatas: ['testCalldatas'],
+        startBlock: 10,
+        endBlock: 100,
+        strategy: 'testStrategy',
+        ipfsHash: 'testIpfsHash',
       },
     };
 
     const oldDbEvent = await setupDbEvent(legacyEvent);
-    const eventHandler = new MigrationHandler(
-      models,
-      rmqController,
-      'edgeware'
-    );
+    const eventHandler = new MigrationHandler(models, rmqController, 'aave');
 
     // process event
     const dbEvent = await eventHandler.handle(currentEvent);
     assert.deepEqual(dbEvent.event_data, currentEvent.data);
-    const chainEvents = await models['ChainEvent'].findAll({
+    const chainEvents = await models.ChainEvent.findAll({
       where: {
-        chain: 'edgeware',
-        block_number: 10,
+        chain: 'aave',
+        block_number: 11,
       },
     });
     assert.lengthOf(chainEvents, 1);
     assert.deepEqual(chainEvents[0].toJSON(), dbEvent.toJSON());
   });
 
-  it('should ignore irrelevant events', async () => {
-    const event: CWEvent<SubstrateTypes.IEventData> = {
-      blockNumber: 11,
-      includeAddresses: ['5FHneW46xGXgs5mUiveU4sbTyGBzmstUspZC92UhjJM694ty'],
-      network: SupportedNetwork.Substrate,
+  it('should ignore events that are not related to an entity', async () => {
+    const event: CWEvent<IEventData> = {
+      blockNumber: 125,
+      network: SupportedNetwork.Aave,
       data: {
-        kind: SubstrateTypes.EventKind.Slash,
-        validator: '5FHneW46xGXgs5mUiveU4sbTyGBzmstUspZC92UhjJM694ty',
+        kind: EventKind.Transfer,
+        tokenAddress: 'testAddress',
+        from: 'testFrom',
+        to: 'testTo',
         amount: '10000',
       },
     };
 
-    const eventHandler = new MigrationHandler(
-      models,
-      rmqController,
-      'edgeware'
-    );
+    const eventHandler = new MigrationHandler(models, rmqController, 'aave');
 
     // process event
     const dbEvent = await eventHandler.handle(event);
@@ -131,22 +144,18 @@ describe('Edgeware Migration Event Handler Tests', () => {
   it('should ignore unknown events', async () => {
     const event = {
       blockNumber: 13,
-      network: SupportedNetwork.Substrate,
+      network: SupportedNetwork.Aave,
       data: {
         kind: 'democracy-exploded',
         whoops: true,
       },
     };
 
-    const eventHandler = new MigrationHandler(
-      models,
-      rmqController,
-      'edgeware'
-    );
+    const eventHandler = new MigrationHandler(models, rmqController, 'aave');
 
     // process event
     const dbEvent = await eventHandler.handle(
-      event as unknown as CWEvent<SubstrateTypes.IEventData>
+      event as unknown as CWEvent<IEventData>
     );
     assert.equal(dbEvent, null);
   });
