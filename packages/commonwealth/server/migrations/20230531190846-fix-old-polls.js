b8f16a82db67f9c2f3f0a6dcc9b505131852f88b
Alex Young <alexyoung@macbook-pro-2.mynetworksettings.com>
2023-05-31 16:09:00 -0400

    fix poll tag

---
+++ packages/commonwealth/server/migrations/20230531190846-fix-old-polls.js
@@ -0,0 +1,20 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.sequelize.query(`
+      UPDATE "Threads"
+      SET "has_poll" = CASE WHEN (
+        SELECT COUNT(*)
+        FROM "Polls"
+        WHERE "Threads"."id" = "Polls"."thread_id"
+      ) > 0 THEN TRUE ELSE FALSE END
+    `);
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.sequelize.query(
+      'UPDATE "Threads" SET "has_poll" = NULL'
+    );
+  },
+};
