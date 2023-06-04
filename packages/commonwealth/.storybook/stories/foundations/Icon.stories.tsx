2bae7620dff9ba3fe0096bc1c79bb2cae0e6028b
Daniel Martins <martinsda@gmail.com>
2023-06-04 20:41:37 -0300

    Add icon search

--- packages/commonwealth/.storybook/stories/foundations/Icon.stories.tsx
+++ packages/commonwealth/.storybook/stories/foundations/Icon.stories.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import type { Meta } from '@storybook/react';
 import * as Icons from "@phosphor-icons/react";
 
@@ -15,22 +15,45 @@ interface PhosphorIconProps {
 const weightOptions = ["thin", "light", "regular", "bold", "fill", "duotone" ];
 
 const PhosphorIcon: FC<PhosphorIconProps> = ({ size, weight }) => {
+  const [searchTerm, setSearchTerm] = useState<string>("");
+
+  const handleChange = (e: any) => setSearchTerm(e.target.value);
+
+  const handleSubmit = (e: any) => e.preventDefault();
+
   return (
-    <div className="gallery">
-      {Object.entries(phosphorIconLookup).map(([key, icon], index) => {
-        const IconComponent = icon;
-        return (
-          <div key={index} className="item">
-            <div className="icon">
-              <IconComponent size={size} weight={weight} />
+    <>
+      <form onSubmit={handleSubmit}>
+        <input
+          className="input"
+          type="text"
+          value={searchTerm}
+          onChange={handleChange}
+          autoComplete="on"
+          placeholder="Search icons..."
+        />
+      </form>
+      <div className="gallery">
+        {Object.entries(phosphorIconLookup)
+          .filter((key: any) => {
+            let lowercaseKey: string = String(key).toLowerCase();
+            return lowercaseKey.includes(searchTerm.toLowerCase());
+          })
+          .map(([key, icon], index) => {
+          const IconComponent = icon;
+          return (
+            <div key={index} className="item">
+              <div className="icon">
+                <IconComponent size={size} weight={weight} />
+              </div>
+              <CWText className="name" type="caption">
+                {key.charAt(0).toUpperCase() + key.slice(1)}
+              </CWText>
             </div>
-            <CWText className="name" type="caption">
-              {key.charAt(0).toUpperCase() + key.slice(1)}
-            </CWText>
-          </div>
-        )
-      })}
-    </div>
+          )
+        })}
+      </div>
+    </>
   )
 }
 
