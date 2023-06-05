4adcd7fd19c4486cc923fdac2edbcb488231dcd8
dillchen <dillon@commonwealth.im>
2023-06-05 18:40:40 -0400

    update and create card column for display pages

--- packages/commonwealth/client/scripts/views/pages/discussions/recent_threads_header.tsx
+++ packages/commonwealth/client/scripts/views/pages/discussions/recent_threads_header.tsx
@@ -34,29 +34,6 @@ export const RecentThreadsHeader = ({
 }: RecentThreadsHeaderProps) => {
   const navigate = useCommonNavigate();
   const [topicSelectedToEdit, setTopicSelectedToEdit] = useState<Topic>(null);
-  const forceRerender = useForceRerender();
-  const { rightSidebarVisible, setRightMenu } = useSidebarStore();
-
-  const [windowIsExtraSmall, setWindowIsExtraSmall] = useState(
-    isWindowExtraSmall(window.innerWidth)
-  );
-
-  useEffect(() => {
-    const onResize = () => {
-      setWindowIsExtraSmall(isWindowExtraSmall(window.innerWidth));
-    };
-
-    window.addEventListener('resize', onResize);
-    app.loginStateEmitter.on('redraw', forceRerender);
-    app.user.isFetched.on('redraw', forceRerender);
-
-    return () => {
-      window.removeEventListener('resize', onResize);
-      app.loginStateEmitter.off('redraw', forceRerender);
-      app.user.isFetched.off('redraw', forceRerender);
-    };
-  }, [forceRerender]);
-
   const { stagesEnabled, customStages } = app.chain?.meta || {};
 
   const topics = app.topics.getByCommunity(app.activeChainId());
@@ -106,50 +83,6 @@ export const RecentThreadsHeader = ({
 
   return (
     <div className="RecentThreadsHeader">
-      {isUndefined(topic) && (
-        <>
-          <div className="header-row">
-            <CWText type="h3" fontWeight="semiBold" className="header-text">
-              All Discussions
-            </CWText>
-            <div className="count-and-button">
-              <CWText
-                type="caption"
-                fontWeight="medium"
-                className="thread-count-text"
-              >
-                {totalThreadCount} Threads
-              </CWText>
-              {windowIsExtraSmall ? (
-                <CWIconButton
-                  iconName="plusCircle"
-                  iconButtonTheme="black"
-                  onClick={() => {
-                    setRightMenu({ isVisible: !rightSidebarVisible });
-                  }}
-                  disabled={!app.user.activeAccount}
-                />
-              ) : (
-                <CWButton
-                  buttonType="mini-black"
-                  label="Add Action"
-                  iconLeft="plus"
-                  onClick={() => {
-                    setRightMenu({ isVisible: !rightSidebarVisible });
-                  }}
-                  disabled={!app.user.activeAccount}
-                />
-              )}
-            </div>
-          </div>
-          <CWText className="subheader-text">
-            This section is for the community to discuss how to manage the
-            community treasury and spending on contributor grants, community
-            initiatives, liquidity mining and other programs.
-          </CWText>
-          <NewThreadForm />
-        </>
-      )}
       {app.chain?.meta && (
         <div className="header-row">
           <div className="buttons-row">
@@ -172,18 +105,6 @@ export const RecentThreadsHeader = ({
               />
             )}
           </div>
-          {!isUndefined(topic) && <NewThreadForm />}
-          {!isUndefined(topic) && (
-            <CWButton
-              buttonType="mini-black"
-              label="Add Action"
-              iconLeft="plus"
-              onClick={() => {
-                setRightMenu({ isVisible: !rightSidebarVisible });
-              }}
-              disabled={!app.user.activeAccount}
-            />
-          )}
         </div>
       )}
 
