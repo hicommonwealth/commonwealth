22aab55e74143fba0741ca9adb9356b733daefc7
kassad <kurtis@commonwealth.im>
2023-06-05 07:47:12 -0700

    Added failure case to findOneRole

--- packages/commonwealth/server/util/roles.ts
+++ packages/commonwealth/server/util/roles.ts
@@ -198,7 +198,7 @@ export async function findOneRole(
       permissions
     );
   let communityRole: CommunityRoleAttributes;
-  if (communityRoles) {
+  if (communityRoles && communityRoles.length > 0) {
     // find the highest role
     communityRole = await getHighestRoleFromCommunityRoles(communityRoles);
   } else {
