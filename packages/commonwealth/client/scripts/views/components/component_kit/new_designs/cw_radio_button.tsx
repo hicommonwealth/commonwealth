4238467f4a149d10defc42b958b599400cac6930
Daniel Martins <martinsda@gmail.com>
2023-06-06 13:01:57 -0300

    Add new RadioButton design

---
+++ packages/commonwealth/client/scripts/views/components/component_kit/new_designs/cw_radio_button.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+
+import 'components/component_kit/new_designs/cw_radio_button.scss';
+import { CWText } from '../cw_text';
+import { getClasses } from '../helpers';
+
+import { ComponentType } from '../types';
+
+export type RadioButtonType = {
+  label?: string;
+  value: string;
+  disabled?: boolean;
+};
+
+type RadioButtonStyleProps = {
+  disabled?: boolean;
+  checked?: boolean;
+};
+
+type RadioButtonProps = {
+  groupName?: string;
+  onChange?: (e?: any) => void;
+} & Omit<RadioButtonType, 'disabled'> &
+  RadioButtonStyleProps;
+
+export const CWRadioButton = (props: RadioButtonProps) => {
+  const {
+    disabled = false,
+    groupName,
+    label,
+    onChange,
+    checked,
+    value,
+  } = props;
+
+  const params = {
+    disabled,
+    name: groupName,
+    onChange,
+    checked,
+    type: 'radio',
+    value,
+  };
+
+  return (
+    <label
+      className={getClasses<RadioButtonStyleProps>(
+        {
+          checked,
+          disabled,
+        },
+        ComponentType.RadioButton
+      )}
+    >
+      <input className="radio-input" {...params} />
+      <div className="radio-control" />
+      <CWText>{label || value}</CWText>
+    </label>
+  );
+};
