a13697e25aa47e74db18cc9eece1b067ec884bde
ianrowan <i.rowan299@gmail.com>
2023-06-07 15:42:07 -0500

    move metamask and rm un-needed package

--- packages/commonwealth/test/unit/wallets/metamask.spec.ts
+++ packages/commonwealth/test/unit/wallets/metamask.spec.ts
@@ -1,6 +1,6 @@
-import MetamaskWebWalletController from "client/scripts/controllers/app/webWallets/metamask_web_wallet";
+import MetamaskWebWalletController from "../../../client/scripts/controllers/app/webWallets/metamask_web_wallet";
 import { ethers } from "ethers";
-import { getMetaMaskMock } from "test/e2eStateful/mockMetaMaskUtil";
+import { getMetaMaskMock } from "../../util/mockMetaMaskUtil";
 
 const window = {ethereum: getMetaMaskMock('https://mainnet.infura.io/v3/17253b2fd784479abff55a32c9b3282c', ethers.Wallet.createRandom().privateKey)};
 
