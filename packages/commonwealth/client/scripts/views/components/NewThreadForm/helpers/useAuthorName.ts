530bd0c580ed78b63a589d7aa521858089659401
Malik Zulqurnain <malikzulqurnain2121@gmail.com>
2023-06-09 01:53:25 +0500

    Fix thread creation for cosmos chains

--- packages/commonwealth/client/scripts/views/components/NewThreadForm/helpers/useAuthorName.ts
+++ packages/commonwealth/client/scripts/views/components/NewThreadForm/helpers/useAuthorName.ts
@@ -4,12 +4,18 @@ import NewProfilesController from '../../../../controllers/server/newProfiles';
 
 const useAuthorName = () => {
   const [authorName, setAuthorName] = useState(
-    app.user.activeAccount?.profile?.name
+    app.chain.base === 'cosmos'
+      ? 'Anonymous'
+      : app.user.activeAccount?.profile?.name
   );
 
   useEffect(() => {
     NewProfilesController.Instance.isFetched.on('redraw', () => {
-      setAuthorName(app.user.activeAccount?.profile?.name);
+      setAuthorName(
+        app.chain.base === 'cosmos'
+          ? 'Anonymous'
+          : app.user.activeAccount?.profile?.name
+      );
     });
   }, []);
 
