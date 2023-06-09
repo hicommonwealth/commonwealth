b648f24db2fa9399d299c80d52aa8a3264a03929
Jake Naviasky <jnaviask@gmail.com>
2023-06-09 14:11:24 -0400

    Revert "Add new privacy terms document (#4180)"
    This reverts commit 4393e76bfc02f0e45c4d070c4516c47beaa48857.

--- packages/commonwealth/client/scripts/navigation/GeneralRoutes.tsx
+++ packages/commonwealth/client/scripts/navigation/GeneralRoutes.tsx
@@ -3,13 +3,11 @@ import React, { lazy } from 'react';
 import { withLayout } from 'views/Layout';
 
 const TermsPage = lazy(() => import('views/pages/terms'));
-const OldTermsPage = lazy(() => import('views/pages/old_terms'));
 const PrivacyPage = lazy(() => import('views/pages/privacy'));
 const ComponentsPage = lazy(() => import('views/pages/components'));
 
 const GeneralRoutes = () => [
   <Route path="/terms" element={withLayout(TermsPage, {})} />,
-  <Route path="/tos-1-26-2023" element={withLayout(OldTermsPage, {})} />,
   <Route path="/privacy" element={withLayout(PrivacyPage, {})} />,
   <Route
     path="/components"
