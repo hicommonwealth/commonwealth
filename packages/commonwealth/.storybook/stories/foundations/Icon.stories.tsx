9aff02e88cf8eb707ec0b8e14ca525eb529837cb
Daniel Martins <martinsda@gmail.com>
2023-06-12 02:40:22 +0100

    Add icon gallery

--- packages/commonwealth/.storybook/stories/foundations/Icon.stories.tsx
+++ packages/commonwealth/.storybook/stories/foundations/Icon.stories.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import type { Meta } from '@storybook/react';
 import * as Icons from "@phosphor-icons/react";
 
@@ -12,12 +12,113 @@ interface PhosphorIconProps {
   weight: Icons.IconWeight | undefined;
 };
 
-const weightOptions = ["thin", "light", "regular", "bold", "fill", "duotone" ];
+interface GalleryProps {
+  size: number;
+  weight: Icons.IconWeight | undefined;
+  searchTerm: string;
+};
+
+const weightOptions = ["thin", "regular", "fill" ];
+
+const Gallery: FC<GalleryProps> = ({ searchTerm, size, weight }) => {
+  const itemsPerRow: number = 6;
+
+  // const [iconsFound, setIconsFound] = useState<number>(0);
+  // const [gallery, setGallery] = useState<any[]>([]);
+
+  // useEffect(() => {
+  //   let iconGrid: any[] = [];
+  //   let filteredIcons = Object.entries(phosphorIconLookup)
+  //     // .slice(30, 70)
+  //     .filter((key: any) => {
+  //       let lowercaseKey: string = String(key).toLowerCase();
+  //       return lowercaseKey.includes(searchTerm.toLowerCase());
+  //     })
+  //     .map(([key, icon], index) => {
+  //       const IconComponent = icon;
+  //       return (
+  //         <div key={index} className="item">
+  //           <div className="icon">
+  //             <IconComponent size={size} weight={weight} />
+  //           </div>
+  //           <CWText className="name" type="caption">
+  //             {key.charAt(0).toUpperCase() + key.slice(1)}
+  //           </CWText>
+  //         </div>
+  //       );
+  //     });
+  //   console.log('filtered icons:', filteredIcons);
+      
+  //   for (let i: number = 0; i < filteredIcons.length; i++) {
+  //     let rowIcons: any[] = [];
+  //     for (let j: number = i % itemsPerRow; j < itemsPerRow; j++) {
+  //       rowIcons.push(filteredIcons[j + (i % itemsPerRow)]);
+  //     }
+  //     iconGrid.push(rowIcons);
+  //   }
+      
+  //   setIconsFound(filteredIcons.length);
+  //   setGallery(iconGrid);
+  // }, [searchTerm]);
+
+  const showGallery = () => {
+    let gallery: any[] = [];
+    let filteredIcons = Object.entries(phosphorIconLookup)
+      .filter((key: any) => {
+        let lowercaseKey: string = String(key).toLowerCase();
+        return lowercaseKey.includes(searchTerm.toLowerCase());
+      })
+      .map(([key, icon], index) => {
+        const IconComponent = icon;
+        return (
+          <div key={index} className="item">
+            <IconComponent size={size} weight={weight} />
+            <CWText className="name" type="caption">
+              {key.charAt(0).toUpperCase() + key.slice(1)}
+            </CWText>
+          </div>
+        );
+      });
+    
+    for (let i: number = 0; i < filteredIcons.length; i += itemsPerRow) {
+      let rowIcons: any[] = [];
+      for (let j: number = 0; j < itemsPerRow; j++) {
+        rowIcons.push(filteredIcons[j + i]);
+      }
+      gallery.push(rowIcons);
+    }
+    
+    return (
+      <>
+        <CWText className="results" type="caption">
+          {`${filteredIcons.length} results found`}
+        </CWText>
+        {gallery.map((row: any, i: number) => (
+          <div key={i} className="row">
+            {row.map((item: any, j: number) => (
+              <div key={j}>{item}</div>
+            ))}
+          </div>
+        ))}
+      </>
+    );
+  }
+
+  return (
+    <>
+      {showGallery()}
+    </>
+  );
+};
 
 const PhosphorIcon: FC<PhosphorIconProps> = ({ size, weight }) => {
   const [searchTerm, setSearchTerm] = useState<string>("");
 
-  const handleChange = (e: any) => setSearchTerm(e.target.value);
+  const handleChange = (e: any) => {
+    console.log('current term:', searchTerm);
+    console.log('typed:', e.target.value);
+    setSearchTerm(e.target.value);
+  }
 
   const handleSubmit = (e: any) => e.preventDefault();
 
@@ -33,26 +134,7 @@ const PhosphorIcon: FC<PhosphorIconProps> = ({ size, weight }) => {
           placeholder="Search icons..."
         />
       </form>
-      <div className="gallery">
-        {Object.entries(phosphorIconLookup)
-          .filter((key: any) => {
-            let lowercaseKey: string = String(key).toLowerCase();
-            return lowercaseKey.includes(searchTerm.toLowerCase());
-          })
-          .map(([key, icon], index) => {
-          const IconComponent = icon;
-          return (
-            <div key={index} className="item">
-              <div className="icon">
-                <IconComponent size={size} weight={weight} />
-              </div>
-              <CWText className="name" type="caption">
-                {key.charAt(0).toUpperCase() + key.slice(1)}
-              </CWText>
-            </div>
-          )
-        })}
-      </div>
+      <Gallery size={size} weight={weight} searchTerm={searchTerm} />
     </>
   );
 }
@@ -67,7 +149,7 @@ export default icon;
 export const PhosphorIconStory = {
   name: "Icon",
   args: {
-    size: 24,
+    size: 32,
     weight: "regular",
   },
   argTypes: {
