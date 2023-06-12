6735edb53e19306b8085dcc74f83f5b3e353873a
Alex Young <alexyoung@macbook-pro-2.mynetworksettings.com>
2023-06-12 14:23:03 -0400

    fix

--- packages/commonwealth/server/routes/deleteChain.ts
+++ packages/commonwealth/server/routes/deleteChain.ts
@@ -1,6 +1,7 @@
 import { AppError } from 'common-common/src/errors';
 import type { NextFunction } from 'express';
 import { Op } from 'sequelize';
+import { sequelize } from '../database';
 import type { DB } from '../models';
 import type { TypedRequestBody, TypedResponse } from '../types';
 import { success } from '../types';
@@ -56,156 +57,180 @@ const deleteChain = async (
     return next(new AppError(Errors.NoChain));
   }
 
-  // eslint-disable-next-line no-new
-  await new Promise(async () => {
-    await models.sequelize.transaction(async (t) => {
-      const admins = await findAllRoles(models, {}, chain.id, ['admin']);
-      if (admins) {
-        // delete admin role assignments
-        await models.RoleAssignment.destroy({
-          where: {
-            community_role_id:
-              admins[0]._roleAssignmentAttributes.community_role_id,
-          },
-          transaction: t,
+  try {
+    // eslint-disable-next-line no-new
+    await new Promise<void>(async (resolve, reject) => {
+      try {
+        await models.sequelize.transaction(async (t) => {
+          const admins = await findAllRoles(models, {}, chain.id, ['admin']);
+          if (admins && admins[0]) {
+            // delete admin role assignments
+            await models.RoleAssignment.destroy({
+              where: {
+                community_role_id:
+                  admins[0]._roleAssignmentAttributes.community_role_id,
+              },
+              transaction: t,
+            });
+          }
+          // TODO: need a parallel API call to chain-events to destroy chain-entities there too
+          await models.ChainEntityMeta.destroy({
+            where: { chain: chain.id },
+            transaction: t,
+          });
+
+          await models.User.update(
+            {
+              selected_chain_id: null,
+            },
+            {
+              where: {
+                selected_chain_id: chain.id,
+              },
+              transaction: t,
+            }
+          );
+
+          await models.Reaction.destroy({
+            where: { chain: chain.id },
+            transaction: t,
+          });
+
+          // Add the created by field to comments for redundancy
+          await sequelize.query(
+            `UPDATE "Comments" SET created_by = (SELECT address FROM "Addresses" WHERE "Comments".address_id = "Addresses".id) WHERE chain = '${chain.id}'`,
+            { transaction: t }
+          );
+
+          await models.Comment.destroy({
+            where: { chain: chain.id },
+            transaction: t,
+          });
+
+          await models.Topic.destroy({
+            where: { chain_id: chain.id },
+            transaction: t,
+          });
+
+          await models.Role.destroy({
+            where: { chain_id: chain.id },
+            transaction: t,
+          });
+
+          await models.Subscription.destroy({
+            where: { chain_id: chain.id },
+            transaction: t,
+          });
+
+          await models.CommunityContract.destroy({
+            where: {
+              chain_id: chain.id,
+            },
+            transaction: t,
+          });
+
+          await models.Webhook.destroy({
+            where: { chain_id: chain.id },
+            transaction: t,
+          });
+
+          const threads = await models.Thread.findAll({
+            where: { chain: chain.id },
+            attributes: ['id'],
+          });
+
+          await models.Collaboration.destroy({
+            where: {
+              thread_id: { [Op.in]: threads.map((thread) => thread.id) },
+            },
+            transaction: t,
+          });
+
+          await models.Vote.destroy({
+            where: { chain_id: chain.id },
+            transaction: t,
+          });
+
+          await models.Poll.destroy({
+            where: { chain_id: chain.id },
+            transaction: t,
+          });
+
+          // Add the created by field to threads for redundancy
+          await sequelize.query(
+            `UPDATE "Threads" SET created_by = (SELECT address FROM "Addresses" WHERE "Threads".address_id = "Addresses".id) WHERE chain = '${chain.id}'`,
+            { transaction: t }
+          );
+
+          await models.Thread.destroy({
+            where: { chain: chain.id },
+            transaction: t,
+          });
+
+          await models.StarredCommunity.destroy({
+            where: { chain: chain.id },
+            transaction: t,
+          });
+
+          const addresses = await models.Address.findAll({
+            where: { chain: chain.id },
+          });
+
+          await models.CommunityBanner.destroy({
+            where: { chain_id: chain.id },
+            transaction: t,
+          });
+
+          // notifications + notifications_read (cascade)
+          await models.Notification.destroy({
+            where: { chain_id: chain.id },
+            transaction: t,
+          });
+
+          await models.RoleAssignment.destroy({
+            where: { address_id: { [Op.in]: addresses.map((a) => a.id) } },
+            transaction: t,
+          });
+
+          await models.Address.destroy({
+            where: { chain: chain.id },
+            transaction: t,
+          });
+
+          const communityRoles = await models.CommunityRole.findAll({
+            where: { chain_id: chain.id },
+            transaction: t,
+          });
+
+          await models.RoleAssignment.destroy({
+            where: {
+              community_role_id: { [Op.in]: communityRoles.map((r) => r.id) },
+            },
+            transaction: t,
+          });
+
+          await Promise.all(
+            communityRoles.map((r) => r.destroy({ transaction: t }))
+          );
+
+          await models.Chain.destroy({
+            where: { id: chain.id },
+            transaction: t,
+          });
+
+          resolve();
         });
+      } catch (e) {
+        console.log(e);
+        reject(e);
       }
-      // TODO: need a parallel API call to chain-events to destroy chain-entities there too
-      await models.ChainEntityMeta.destroy({
-        where: { chain: chain.id },
-        transaction: t,
-      });
-
-      await models.User.update(
-        {
-          selected_chain_id: null,
-        },
-        {
-          where: {
-            selected_chain_id: chain.id,
-          },
-          transaction: t,
-        }
-      );
-
-      await models.Reaction.destroy({
-        where: { chain: chain.id },
-        transaction: t,
-      });
-
-      await models.Comment.destroy({
-        where: { chain: chain.id },
-        transaction: t,
-      });
-
-      await models.Topic.destroy({
-        where: { chain_id: chain.id },
-        transaction: t,
-      });
-
-      await models.Role.destroy({
-        where: { chain_id: chain.id },
-        transaction: t,
-      });
-
-      await models.Subscription.destroy({
-        where: { chain_id: chain.id },
-        transaction: t,
-      });
-
-      await models.CommunityContract.destroy({
-        where: {
-          chain_id: chain.id,
-        },
-        transaction: t,
-      });
-
-      await models.Webhook.destroy({
-        where: { chain_id: chain.id },
-        transaction: t,
-      });
-
-      const threads = await models.Thread.findAll({
-        where: { chain: chain.id },
-        attributes: ['id'],
-      });
-
-      await models.Collaboration.destroy({
-        where: { thread_id: { [Op.in]: threads.map((thread) => thread.id) } },
-        transaction: t,
-      });
-
-      await models.Vote.destroy({
-        where: { chain_id: chain.id },
-        transaction: t,
-      });
-
-      await models.Poll.destroy({
-        where: { chain_id: chain.id },
-        transaction: t,
-      });
-
-      await models.Thread.destroy({
-        where: { chain: chain.id },
-        transaction: t,
-      });
-
-      await models.StarredCommunity.destroy({
-        where: { chain: chain.id },
-        transaction: t,
-      });
-
-      const addresses = await models.Address.findAll({
-        where: { chain: chain.id },
-      });
-
-      await models.CommunityBanner.destroy({
-        where: { chain_id: chain.id },
-        transaction: t,
-      });
-
-      // notifications + notifications_read (cascade)
-      await models.Notification.destroy({
-        where: { chain_id: chain.id },
-        transaction: t,
-      });
-
-      await models.RoleAssignment.destroy({
-        where: { address_id: { [Op.in]: addresses.map((a) => a.id) } },
-        transaction: t,
-      });
-
-      await models.Address.destroy({
-        where: { chain: chain.id },
-        transaction: t,
-      });
-
-      const communityRoles = await models.CommunityRole.findAll({
-        where: { chain_id: chain.id },
-        transaction: t,
-      });
-
-      await models.RoleAssignment.destroy({
-        where: {
-          community_role_id: { [Op.in]: communityRoles.map((r) => r.id) },
-        },
-        transaction: t,
-      });
-
-      await Promise.all(
-        communityRoles.map((r) => r.destroy({ transaction: t }))
-      );
-
-      await models.Chain.destroy({
-        where: { id: chain.id },
-        transaction: t,
-      });
     });
-  }).catch((err) => {
-    console.log(err);
-  });
 
-  return success(res, { result: 'success' });
+    return success(res, { result: 'success' });
+  } catch (e) {
+    console.log(e);
+    return next(new AppError(Errors.CannotDeleteChain));
+  }
 };
 
 export default deleteChain;
