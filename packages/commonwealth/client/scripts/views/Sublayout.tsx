2a911662199353b36577a87e068d2e0e458e2d43
Malik Zulqurnain <51641047+mzparacha@users.noreply.github.com>
2023-06-13 23:12:48 +0500

    Replaced instance of window.onResize with useBrowserWindow hook (#4202)
    * Replaced instance of window.onResize with useBrowserWindow hook
    
    * Fixed lint issues
    
    * Fix thread poll section location in small screens
    
    ---------
    
    Co-authored-by: Jake Naviasky <jake@commonwealth.im>

--- packages/commonwealth/client/scripts/views/Sublayout.tsx
+++ packages/commonwealth/client/scripts/views/Sublayout.tsx
@@ -1,16 +1,14 @@
-import React, { useState, useEffect } from 'react';
-
-import 'Sublayout.scss';
-
+import useBrowserWindow from 'hooks/useBrowserWindow';
+import useForceRerender from 'hooks/useForceRerender';
+import React, { useEffect } from 'react';
 import app from 'state';
+import useSidebarStore from 'state/ui/sidebar';
+import 'Sublayout.scss';
 import { Sidebar } from 'views/components/sidebar';
 import { AppMobileMenus } from './AppMobileMenus';
-import { isWindowSmallInclusive } from './components/component_kit/helpers';
 import { Footer } from './Footer';
 import { SublayoutBanners } from './SublayoutBanners';
 import { SublayoutHeader } from './SublayoutHeader';
-import useForceRerender from 'hooks/useForceRerender';
-import useSidebarStore from 'state/ui/sidebar';
 
 type SublayoutProps = {
   hideFooter?: boolean;
@@ -28,9 +26,7 @@ const Sublayout = ({
 }: SublayoutProps) => {
   const forceRerender = useForceRerender();
   const { menuVisible, mobileMenuName } = useSidebarStore();
-  const [isWindowSmall, setIsWindowSmall] = useState(
-    isWindowSmallInclusive(window.innerWidth)
-  );
+  const { isWindowSmallInclusive } = useBrowserWindow({});
 
   useEffect(() => {
     app.sidebarRedraw.on('redraw', forceRerender);
@@ -47,29 +43,19 @@ const Sublayout = ({
     ) {
       document.getElementsByTagName('html')[0].classList.add('invert');
     }
-
-    const onResize = () => {
-      setIsWindowSmall(isWindowSmallInclusive(window.innerWidth));
-    };
-
-    window.addEventListener('resize', onResize);
-
-    return () => {
-      window.removeEventListener('resize', onResize);
-    };
   }, []);
 
   const chain = app.chain ? app.chain.meta : null;
   const terms = app.chain ? chain.terms : null;
   const banner = app.chain ? chain.communityBanner : null;
-  const showSidebar = menuVisible || !isWindowSmall;
+  const showSidebar = menuVisible || !isWindowSmallInclusive;
 
   return (
     <div className="Sublayout">
       <div className="header-and-body-container">
         <SublayoutHeader
           hideSearch={hideSearch}
-          onMobile={isWindowSmall}
+          onMobile={isWindowSmallInclusive}
           isLoadingProfileData={isLoadingProfileData}
         />
         <div className="sidebar-and-body-container">
