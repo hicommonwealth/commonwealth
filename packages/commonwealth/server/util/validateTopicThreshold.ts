5be15d74b40122c48dbccd61f10159ed70941240
Timothee Legros <timothee@ualberta.ca>
2023-06-19 19:33:13 +0200

    lint fix

--- packages/commonwealth/server/util/validateTopicThreshold.ts
+++ packages/commonwealth/server/util/validateTopicThreshold.ts
@@ -14,59 +14,54 @@ const validateTopicThreshold = async (
   userAddress: string
 ): Promise<boolean> => {
   if (!topicId || !userAddress) return true;
+  const topic = await models.Topic.findOne({
+    where: { id: topicId },
+    include: [
+      {
+        model: models.Chain,
+        required: true,
+        as: 'chain',
+        include: [
+          {
+            model: models.ChainNode,
+            required: true,
+          },
+        ],
+      },
+    ],
+  });
+  if (!topic?.chain?.ChainNode?.id) {
+    // if we have no node, always approve
+    return true;
+  }
+  // skip query if no threshold
+  const threshold = new BN(topic.token_threshold || '0');
+  if (threshold.isZero()) {
+    return true;
+  }
+
+  // TODO: @JAKE in the future, we will have more than one contract,
+  // need to handle this through the TBC Rule, passing in associated Contract.id
+  const communityContracts = await models.CommunityContract.findOne({
+    where: { chain_id: topic.chain.id },
+    include: [{ model: models.Contract, required: true }],
+  });
+
   try {
-    const topic = await models.Topic.findOne({
-      where: { id: topicId },
-      include: [
-        {
-          model: models.Chain,
-          required: true,
-          as: 'chain',
-          include: [
-            {
-              model: models.ChainNode,
-              required: true,
-            },
-          ],
-        },
-      ],
-    });
-    if (!topic?.chain?.ChainNode?.id) {
-      // if we have no node, always approve
+    const balance = await tbc.fetchUserBalance(
+      topic.chain.network,
+      topic.chain.ChainNode.id,
+      userAddress,
+      communityContracts?.Contract?.address
+    );
+
+    return new BN(balance).gte(threshold);
+  } catch (e) {
+    if (e.message === FetchTokenBalanceErrors.NoBalanceProvider) {
       return true;
+    } else {
+      throw e;
     }
-    // skip query if no threshold
-    const threshold = new BN(topic.token_threshold || '0');
-    if (threshold.isZero()) {
-      return true;
-    }
-
-    // TODO: @JAKE in the future, we will have more than one contract,
-    // need to handle this through the TBC Rule, passing in associated Contract.id
-    const communityContracts = await models.CommunityContract.findOne({
-      where: { chain_id: topic.chain.id },
-      include: [{ model: models.Contract, required: true }],
-    });
-
-    try {
-      const balance = await tbc.fetchUserBalance(
-        topic.chain.network,
-        topic.chain.ChainNode.id,
-        userAddress,
-        communityContracts?.Contract?.address
-      );
-
-      return new BN(balance).gte(threshold);
-    } catch (e) {
-      if (e.message === FetchTokenBalanceErrors.NoBalanceProvider) {
-        return true;
-      } else {
-        throw e;
-      }
-    }
-  } catch (err) {
-    // routes will throw thus logging the error and reporting it on Rollbar
-    throw err;
   }
 };
 
