c1812518fca3478f102f5f5609b361ca6b773c02
Timothee Legros <timothee@ualberta.ca>
2023-06-20 15:27:00 +0200

    remove legacy listener script + subscribeEvents func in subscribeFunc.ts

--- packages/chain-events/src/chains/aave/subscribeFunc.ts
+++ packages/chain-events/src/chains/aave/subscribeFunc.ts
@@ -1,12 +1,6 @@
 import sleep from 'sleep-promise';
 
 import { createProvider } from '../../eth';
-import type {
-  IDisconnectedRange,
-  CWEvent,
-  SubscribeFunc,
-  ISubscribeOptions,
-} from '../../interfaces';
 import { SupportedNetwork } from '../../interfaces';
 import { addPrefix, factory } from '../../logging';
 import {
@@ -15,8 +9,7 @@ import {
   GovernancePowerDelegationERC20__factory as GovernancePowerDelegationERC20Factory,
 } from '../../contractTypes';
 
-import { Processor } from './processor';
-import type { IEventData, RawEvent, Api } from './types';
+import type { Api } from './types';
 
 /**
  * Attempts to open an API connection, retrying if it cannot be opened.
@@ -111,102 +104,3 @@ export async function createApi(
     }]: Failed to start Aave listener for ${governanceAddress} at ${ethNetworkUrl}`
   );
 }
-
-/**
- * This is the main function for edgeware event handling. It constructs a connection
- * to the chain, connects all event-related modules, and initializes event handling.
- * @param options
- * @returns An active block subscriber.
- */
-// export const subscribeEvents: SubscribeFunc<
-//   Api,
-//   RawEvent,
-//   ISubscribeOptions<Api>
-// > = async (options) => {
-//   const { chain, api, handlers, skipCatchup, discoverReconnectRange, verbose } =
-//     options;
-//   const log = factory.getLogger(
-//     addPrefix(__filename, [SupportedNetwork.Aave, chain])
-//   );
-//   // helper function that sends an event through event handlers
-//   const handleEventFn = async (event: CWEvent<IEventData>): Promise<void> => {
-//     let prevResult = null;
-//     for (const handler of handlers) {
-//       try {
-//         // pass result of last handler into next one (chaining db events)
-//         prevResult = await handler.handle(event, prevResult);
-//       } catch (err) {
-//         log.error(`Event handle failure: ${err.message}`);
-//         break;
-//       }
-//     }
-//   };
-//
-//   // helper function that sends a block through the event processor and
-//   // into the event handlers
-//   const processor = new Processor(api);
-//   const processEventFn = async (event: RawEvent): Promise<void> => {
-//     // retrieve events from block
-//     const cwEvents: CWEvent<IEventData>[] = await processor.process(event);
-//
-//     // process events in sequence
-//     for (const cwEvent of cwEvents) {
-//       await handleEventFn(cwEvent);
-//     }
-//   };
-//
-//   const subscriber = new Subscriber(api, chain, verbose);
-//
-//   // helper function that runs after we've been offline/the server's been down,
-//   // and attempts to fetch skipped events
-//   const pollMissedEventsFn = async (): Promise<void> => {
-//     if (!discoverReconnectRange) {
-//       log.warn(
-//         'No function to discover offline time found, skipping event catchup.'
-//       );
-//       return;
-//     }
-//     log.info(`Fetching missed events since last startup of ${chain}...`);
-//     let offlineRange: IDisconnectedRange;
-//     try {
-//       offlineRange = await discoverReconnectRange();
-//       if (!offlineRange) {
-//         log.warn('No offline range found, skipping event catchup.');
-//         return;
-//       }
-//     } catch (e) {
-//       log.error(
-//         `Could not discover offline range: ${e.message}. Skipping event catchup.`
-//       );
-//       return;
-//     }
-//
-//     const fetcher = new StorageFetcher(api);
-//     try {
-//       const cwEvents = await fetcher.fetch(offlineRange);
-//
-//       // process events in sequence
-//       for (const cwEvent of cwEvents) {
-//         await handleEventFn(cwEvent);
-//       }
-//     } catch (e) {
-//       log.error(`Unable to fetch events from storage: ${e.message}`);
-//     }
-//   };
-//
-//   if (!skipCatchup) {
-//     await pollMissedEventsFn();
-//   } else {
-//     log.info('Skipping event catchup on startup!');
-//   }
-//
-//   try {
-//     log.info(`Subscribing to contracts ${chain}...`);
-//     // TODO: fix or remove the local listener script which utilizes this function
-//     // await subscriber.subscribe(processEventFn, );
-//   } catch (e) {
-//     log.error(`Subscription error: ${e.message}`);
-//   }
-//
-//   return subscriber;
-// };
