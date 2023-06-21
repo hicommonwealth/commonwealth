906f90a4ebe9773458706353a20d5cb513517040
nakulmanchanda <nakul@common.xyz>
2023-06-21 11:47:59 -0400

    add on delete cascade notifread to allow sub del
    Signed-off-by: nakulmanchanda <nakul@common.xyz>

---
+++ packages/commonwealth/server/migrations/20230621152651-subscription-ondelete-cascade.js
@@ -0,0 +1,32 @@
+'use strict';
+
+// TODO: update all null count columns by zero as default
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.sequelize.transaction(async (t) => {
+      let label = 'drop existing constraint';
+      console.log(label);
+      console.time(label);
+      await queryInterface.sequelize.query(
+        `
+        ALTER TABLE "NotificationsRead" DROP CONSTRAINT "NotificationsRead_subscription_id_fkey";
+        `,
+        { raw: true, transaction: t, logging: console.log }
+      );
+      console.timeEnd(label);
+
+      label = 'Add delete cascade to subscription foreign key constraint';
+      console.log(label);
+      console.time(label);
+      await queryInterface.sequelize.query(
+        `
+        ALTER TABLE "NotificationsRead" ADD CONSTRAINT "NotificationsRead_subscription_id_fkey" FOREIGN KEY (subscription_id) REFERENCES "Subscriptions"(id) ON DELETE CASCADE;
+        `,
+        { raw: true, transaction: t, logging: console.log }
+      );
+      console.timeEnd(label);
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {},
+};
