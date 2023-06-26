13eccdd61fedfec8a642eba7abe1b045fdadab5f
Timothee Legros <timothee@ualberta.ca>
2023-06-26 15:26:51 +0200

    v2 of setup-vultr-tunnel.sh

--- .profile.d/setup-vultr-tunnel.sh
+++ .profile.d/setup-vultr-tunnel.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
+# TODO: This will make the build fail (for MVP may not be ideal)
+set -e
 
 echo $0: Setting up the heroku-tunnel ssh private key
+
+# Check if required variables are set
+if [[ -z "${HEROKU_TUNNEL_PRIVATE_KEY}" || -z "${HEROKU_TUNNEL_IP}" || -z "${HEROKU_TUNNEL_PORT}" || -z "${HEROKU_TUNNEL_USER}" ]]; then
+    echo "Required environment variable is not set, exiting."
+    # TODO: This will make the build fail (for MVP may not be ideal)
+    exit 1
+fi
 
 # Create the .ssh directory
 mkdir -p ~/.ssh
@@ -10,11 +19,18 @@ chmod 700 ~/.ssh
 echo "${HEROKU_TUNNEL_PRIVATE_KEY}" > ~/.ssh/heroku-tunnel
 chmod 600 ~/.ssh/heroku-tunnel
 
-# Fetch the RSA public key of the remote server
-ssh-keyscan -t rsa "${HEROKU_TUNNEL_IP}" > ~/.ssh/known_hosts
+# Fetch the RSA public key of the remote server and append to known_hosts
+ssh-keyscan -t rsa "${HEROKU_TUNNEL_IP}" >> ~/.ssh/known_hosts
+
+# Check if ssh-keyscan was successful
+if [ $? -ne 0 ]; then
+    echo "ssh-keyscan failed, exiting."
+    # TODO: This will make the build fail (for MVP may not be ideal)
+    exit 1
+fi
 
 # Start the SSH tunnel if not already running
-SSH_CMD="autossh -f -N -L ${HEROKU_TUNNEL_PORT}:localhost:${HEROKU_TUNNEL_PORT} ${HEROKU_TUNNEL_USER}@${HEROKU_TUNNEL_IP}"
+SSH_CMD="autossh -f -N -L ${HEROKU_TUNNEL_PORT}:localhost:${HEROKU_TUNNEL_PORT} ${HEROKU_TUNNEL_USER}@${HEROKU_TUNNEL_IP} -i ~/.ssh/heroku-tunnel"
 
 PID=`pgrep -f "${SSH_CMD}"`
 if [ $PID ] ; then
@@ -22,4 +38,9 @@ if [ $PID ] ; then
 else
     echo $0 launching tunnel
     $SSH_CMD
+    if [ $? -ne 0 ]; then
+        echo "Failed to start SSH tunnel, exiting."
+        # TODO: This will make the build fail (for MVP may not be ideal)
+        exit 1
+    fi
 fi
