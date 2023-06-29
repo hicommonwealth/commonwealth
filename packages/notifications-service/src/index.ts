b57b74b08d3fc291cbb19cb29d5c3bd2aa1054d8
Ryan Bennett <ryan@common.xyz>
2023-06-29 07:05:59 -0700

    use direct runner

--- packages/notifications-service/src/index.ts
+++ packages/notifications-service/src/index.ts
@@ -1,7 +1,7 @@
 // REFERENCE: https://github.com/apache/beam-starter-typescript/tree/main
 
 import * as beam from 'apache-beam';
-import * as runner from 'apache-beam/runners/runner';
+import { PortableRunner } from 'apache-beam/runners/portable_runner/runner';
 
 export function createPipeline(inputText) {
   // A pipeline is simply a callable that takes a root object.
@@ -12,23 +12,19 @@ export function createPipeline(inputText) {
   };
 }
 
-export async function runPipeline(options) {
-  // Here we create a runner based on the provided options, and use it
-  // to run the above pipeline.
-  await runner
-    .createRunner(options)
-    .run(createPipeline(options.inputText || 'Default Input Text'));
-}
-
 function printAndReturn(element) {
   console.log(element);
   return element;
 }
 
-function main() {
-  runPipeline({}).catch((err) => {
-    console.error(err);
-    process.exit(1);
+async function main() {
+  const runner = new PortableRunner({
+    jobEndpoint: 'localhost:8081',
+    environmentType: 'LOOPBACK',
   });
+  return runner.run(createPipeline('Blah blah input!'));
 }
-main();
+main().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
