6d0e82e72315f79ddd1e7ceb021d7ed6543fc3fc
Timothee Legros <timothee@ualberta.ca>
2023-07-10 17:38:28 +0200

    docs

--- packages/commonwealth/server/middleware/methodNotAllowed.ts
+++ packages/commonwealth/server/middleware/methodNotAllowed.ts
@@ -12,6 +12,16 @@ const routesMethods: { [key: string]: string[] } = {};
 
 type ValidateThenHandle = [ValidationChain[], ...RequestHandler[]];
 
+/**
+ * Use this function to register a route on a given Express Router. This function updates an object that stores the
+ * valid methods available for each given path. For example the object may contain the '/comment' path which is valid
+ * with the 'post', 'delete', and 'get' methods. Once a route is registered in this object, the
+ * methodNotAllowedMiddleware can function correctly.
+ * @param router The router on which to add the request/route handlers.
+ * @param method The HTTP method of the route e.g. 'post', 'get', 'patch', etc.
+ * @param path The path of the route e.g. '/comments'.
+ * @param handlers Any request body validators or route handlers.
+ */
 export const registerRoute = (
   router: Router,
   method: HttpMethod,
@@ -24,6 +34,11 @@ export const registerRoute = (
   routesMethods[realPath].push(method.toUpperCase());
 };
 
+/*
+ * This middleware function checks whether the requested path and method are valid (have a matching handler).
+ * If they are not, the middleware will set the `Allow` header indicating what methods are allowed with the requested
+ * path and then return with a status code of 405 (Method Not Allowed).
+ */
 export const methodNotAllowedMiddleware = () => {
   return (req: Request, res: Response, next: NextFunction) => {
     const methods = routesMethods[req.path];
