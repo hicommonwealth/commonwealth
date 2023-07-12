8272dd59bc54d406b9eeaba0cdd79893d9063b3b
Timothee Legros <timothee@ualberta.ca>
2023-07-12 17:20:01 +0200

    drop NR fkey constraints

--- packages/commonwealth/server/migrations/20230630150848-discard-useless-notif-subs.js
+++ packages/commonwealth/server/migrations/20230630150848-discard-useless-notif-subs.js
@@ -3,15 +3,59 @@
 const { QueryTypes } = require('sequelize');
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.addIndex('Subscriptions', ['subscriber_id'], {
-      name: 'subscriptions_subscriber_id',
+    await queryInterface.sequelize.transaction(async (t) => {
+      await queryInterface.addIndex('Subscriptions', ['subscriber_id'], {
+        name: 'subscriptions_subscriber_id',
+        transaction: t,
+      });
+
+      await queryInterface.removeConstraint(
+        'NotificationsRead',
+        'NotificationsRead_subscription_id_fkey',
+        {
+          transaction: t,
+        }
+      );
+
+      await queryInterface.removeConstraint(
+        'NotificationsRead',
+        'NotificationsRead_notification_id_fkey',
+        {
+          transaction: t,
+        }
+      );
     });
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.removeIndex(
-      'Subscriptions',
-      'subscriptions_subscriber_id'
-    );
+    await queryInterface.sequelize.transaction(async (t) => {
+      await queryInterface.removeIndex(
+        'Subscriptions',
+        'subscriptions_subscriber_id',
+        { transaction: t }
+      );
+
+      await queryInterface.addConstraint('NotificationsRead', {
+        fields: ['subscription_id'],
+        type: 'foreign key',
+        name: 'NotificationsRead_subscription_id_fkey',
+        references: {
+          table: 'Subscriptions',
+          field: 'id',
+        },
+        transaction: t,
+      });
+
+      await queryInterface.addConstraint('NotificationsRead', {
+        fields: ['notification_id'],
+        type: 'foreign key',
+        name: 'NotificationsRead_notification_id_fkey',
+        references: {
+          table: 'Notifications',
+          field: 'id',
+        },
+        transaction: t,
+      });
+    });
   },
 };
