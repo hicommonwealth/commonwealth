54f07e6d26cab1b55f957be2cf01f844f7e0abb0
Daniel Martins <martinsda@gmail.com>
2023-07-12 12:49:08 -0300

    add mui-base autocomplete

--- packages/commonwealth/client/scripts/views/components/component_kit/new_designs/cw_search_bar.tsx
+++ packages/commonwealth/client/scripts/views/components/component_kit/new_designs/cw_search_bar.tsx
@@ -1,5 +1,7 @@
 import React, { FC, useState } from 'react';
 import { MagnifyingGlass, X } from '@phosphor-icons/react';
+import useAutocomplete from '@mui/base/useAutocomplete';
+import Popper from '@mui/base/Popper';
 
 import { ComponentType } from './../types';
 
@@ -52,6 +54,20 @@ type TagProps = {
   onClick: () => void;
 };
 
+const communities = [
+  'Altitude',
+  'Terra Classic',
+  'Osmosis',
+  'Qwoyn Network',
+  '1inch',
+  'Stargate Finance',
+  'Timeless',
+  'Terra Agora',
+  'Injective',
+  'Juno',
+  'Common',
+];
+
 const Tag: FC<TagProps> = ({ communityName, disabled, onClick }) => {
   const handleClick = () => {
     console.log('clicked!');
@@ -65,7 +81,7 @@ const Tag: FC<TagProps> = ({ communityName, disabled, onClick }) => {
           size="small"
           // community={app.chain.meta}
           community={null}
-          onClick={() => {}}
+          onClick={() => console.log('clicked!')}
         />
         <CWText type="b2" fontWeight="regular">
           {communityName}
@@ -85,16 +101,13 @@ const Tag: FC<TagProps> = ({ communityName, disabled, onClick }) => {
 };
 
 export const CWSearchBar = (props: SearchBarProps) => {
-  const [searchTerm, setSearchTerm] = useState<string>('');
+  const [value, setValue] = useState<string>(null);
   const [isTyping, setIsTyping] = useState<boolean>(false);
-  const [tag, setTag] = useState<boolean>(true);
+  // const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
 
   const { disabled, placeholder } = props;
 
-  const handleInputChange = (e) => {
-    const value = e.target.value;
-    setSearchTerm(value);
-  };
+  const handleInputChange = (e) => setValue(e.target.value);
 
   const handleOnInput = (e) => {
     if (e.currentTarget.value?.length === 0) {
@@ -104,27 +117,78 @@ export const CWSearchBar = (props: SearchBarProps) => {
     }
   };
 
+  const {
+    getRootProps,
+    getInputProps,
+    getListboxProps,
+    getOptionProps,
+    popupOpen,
+    anchorEl,
+    setAnchorEl,
+    groupedOptions,
+  } = useAutocomplete({
+    options: communities,
+    value,
+    onChange: (event: any, newValue) => setValue(newValue),
+    getOptionLabel: (option) => option,
+  });
+
+  const open = Boolean(anchorEl);
+  const id = open ? 'simple-popper' : undefined;
+
   return (
-    <div
-      className={getClasses<InputStyleProps & InputInternalStyleProps>(
-        {
-          isTyping,
-          disabled,
-        },
-        ComponentType.SearchBar
-      )}
-    >
-      <MagnifyingGlass className="magnifyingGlass" weight="regular" size={24} />
-      {tag && <Tag communityName="1inch" onClick={() => setTag(false)} />}
-      <div className="inputElement">
-        <input
-          placeholder={placeholder}
-          value={searchTerm}
-          onChange={handleInputChange}
-          onInput={handleOnInput}
-          disabled={disabled}
+    <>
+      <div
+        className={getClasses<InputStyleProps & InputInternalStyleProps>(
+          {
+            isTyping,
+            disabled,
+          },
+          ComponentType.SearchBar
+        )}
+      >
+        <MagnifyingGlass
+          className="magnifyingGlass"
+          weight="regular"
+          size={24}
         />
+        {value && (
+          <Tag
+            communityName={value}
+            // onClick={() => setCommunityName(null)}
+            onClick={() => setValue(null)}
+          />
+        )}
+        <div className="inputElement" {...getRootProps()}>
+          <input
+            placeholder={placeholder}
+            value={value}
+            onChange={handleInputChange}
+            onInput={handleOnInput}
+            disabled={disabled}
+            {...getInputProps()}
+          />
+        </div>
       </div>
-    </div>
+      <Popper
+        // className="popper"
+        id={id}
+        open={true}
+        anchorEl={anchorEl}
+        placement="bottom"
+      >
+        <div className="listBox">
+          {groupedOptions.length > 0 && (
+            <ul {...getListboxProps()}>
+              {(groupedOptions as typeof communities).map((option, index) => (
+                <li className="option" {...getOptionProps({ option, index })}>
+                  {option}
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
+      </Popper>
+    </>
   );
 };
