d5ee158825b64c21c44029826ffe7b9444d49474
dillchen <dillon@commonwealth.im>
2023-07-16 19:28:10 +0200

    window size

--- packages/commonwealth/client/scripts/views/pages/user_dashboard/index.tsx
+++ packages/commonwealth/client/scripts/views/pages/user_dashboard/index.tsx
@@ -1,6 +1,7 @@
 import { notifyInfo } from 'controllers/app/notifications';
 import { useBrowserAnalyticsTrack } from 'hooks/useBrowserAnalyticsTrack';
 import { useCommonNavigate } from 'navigation/helpers';
+import useBrowserWindow from '../../../hooks/useBrowserWindow';
 import 'pages/user_dashboard/index.scss';
 import React, { useEffect } from 'react';
 import app, { LoginState } from 'state';
@@ -27,6 +28,8 @@ const UserDashboard = (props: UserDashboardProps) => {
   const [activePage, setActivePage] = React.useState<DashboardViews>(
     DashboardViews.Global
   );
+
+  const { isWindowLarge } = useBrowserWindow({});
 
   useBrowserAnalyticsTrack({
     payload: {
@@ -171,9 +174,11 @@ const UserDashboard = (props: UserDashboardProps) => {
           )}
         </>
       </div>
-      <div>
-        <DashboardCommunitiesPreview />
-      </div>
+      {isWindowLarge && (
+        <div>
+          <DashboardCommunitiesPreview />
+        </div>
+      )}
     </div>
   );
 };
