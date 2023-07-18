ce0249b6c96613fd174ecf8994372bc9dffe0f84
kassad <kurtis@commonwealth.im>
2023-07-18 14:12:38 -0400

    Fixed Fantom Opera chainNode

--- packages/commonwealth/server/migrations/20230718143432-update-eth-chainNode.js
+++ packages/commonwealth/server/migrations/20230718143432-update-eth-chainNode.js
@@ -3,9 +3,14 @@
 const oldChainNodeEth = 'https://matic-mainnet.chainstacklabs.com';
 const newChainNodeEth =
   'https://polygon-mainnet.g.alchemy.com/v2/5yLkuoKshDbUJdebSAQgmQUPtqLe3LO8';
+
 const oldChainNodeBnb =
   'wss://holy-spring-wave.bsc.quiknode.pro/205d64ae5a12236f4d40137a3ef53ec663091881/';
 const newChainNodeBnb = 'https://bsc-dataseed3.binance.org';
+
+const oldFantomNode =
+  'wss://misty-rough-haze.fantom.quiknode.pro/057c57b1c826b348daa2694034b52a96cbb1b8ca/';
+const newFantomNode = 'https://rpc.ftm.tools/';
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
@@ -29,6 +34,16 @@ module.exports = {
         },
         {
           url: oldChainNodeBnb,
+        },
+        { transaction }
+      );
+      await queryInterface.bulkUpdate(
+        'ChainNodes',
+        {
+          url: newFantomNode,
+        },
+        {
+          url: oldFantomNode,
         },
         { transaction }
       );
@@ -56,6 +71,16 @@ module.exports = {
         },
         {
           url: newChainNodeBnb,
+        },
+        { transaction }
+      );
+      await queryInterface.bulkUpdate(
+        'ChainNodes',
+        {
+          url: oldFantomNode,
+        },
+        {
+          url: newFantomNode,
         },
         { transaction }
       );
