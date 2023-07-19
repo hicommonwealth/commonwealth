ea0e901560caa6ba95063c645307178716bece2b
Alex Young <alexyoung23j@gmail.com>
2023-07-19 15:42:22 -0400

    Client Side Image Compression (#4517)
    * image compression on client
    
    * set options for compression
    
    * Downgrade quill.
    
    ---------
    
    Co-authored-by: Jake Naviasky <jake@commonwealth.im>

--- packages/commonwealth/client/scripts/views/components/react_quill_editor/utils.tsx
+++ packages/commonwealth/client/scripts/views/components/react_quill_editor/utils.tsx
@@ -4,6 +4,7 @@ import { Icon, IconProps } from '@phosphor-icons/react';
 import ReactDOMServer from 'react-dom/server';
 import React from 'react';
 import { replaceBucketWithCDN } from '../../../helpers/awsHelpers';
+import { compressImage } from 'utils/ImageCompression';
 
 export const VALID_IMAGE_TYPES = ['jpeg', 'gif', 'png'];
 
@@ -92,8 +93,10 @@ export const uploadFileToS3 = async (
 
     const signedUploadUrl = sigResponse.data.result;
 
+    const compressedFile = await compressImage(file);
+
     // upload the file via the signed URL
-    await axios.put(signedUploadUrl, file, {
+    await axios.put(signedUploadUrl, compressedFile, {
       params: {
         'Content-Type': file.type,
       },
