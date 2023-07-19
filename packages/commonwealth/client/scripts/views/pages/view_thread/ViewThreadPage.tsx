73db3c81356e591611ba1b1a241bc7dde01c8603
Ian Rowan <36428666+ianrowan@users.noreply.github.com>
2023-07-19 16:06:47 -0500

    POC of new topic gating flow (#4485)
    * POC of new gating flow
    
    * add balance to account model
    
    * async token balance check
    
    * update balance type
    
    * bug fixes
    
    * make token balance fetch reuseable
    
    * solana support + fixes
    
    * typecheck fix
    
    * PR comments, replace name with Id
    
    * null check userBalance
    
    * force balance for comment if not aquired
    
    * rm console.log
    
    * reactive loading of threshold data
    
    ---------
    
    Co-authored-by: Jake Naviasky <jake@commonwealth.im>

--- packages/commonwealth/client/scripts/views/pages/view_thread/ViewThreadPage.tsx
+++ packages/commonwealth/client/scripts/views/pages/view_thread/ViewThreadPage.tsx
@@ -1,6 +1,5 @@
 import { ProposalType } from 'common-common/src/types';
 import { notifyError } from 'controllers/app/notifications';
-import TopicGateCheck from 'controllers/chain/ethereum/gatedTopic';
 import { modelFromServer as modelReactionCountFromServer } from 'controllers/server/reactionCounts';
 import { extractDomain, isDefaultStage } from 'helpers';
 import { filterLinks } from 'helpers/threads';
@@ -512,7 +511,7 @@ const ViewThreadPage = ({ identifier }: ViewThreadPageProps) => {
 
   const canComment =
     !!hasJoinedCommunity ||
-    (!isAdminOrMod && TopicGateCheck.isGatedTopic(thread?.topic?.name));
+    (!isAdminOrMod && app.chain.isGatedTopic(thread?.topic?.id));
 
   const handleLinkedThreadChange = (links: Thread['links']) => {
     const updatedThread = new Thread({
