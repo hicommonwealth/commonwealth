0fca7428d17cb860a676eaf9c28825ad7d4416ed
Malik Zulqurnain <51641047+mzparacha@users.noreply.github.com>
2023-07-21 00:32:31 +0500

    Migrated reactions state to react query (#4426)
    * Migrated thread add/delete reaction queries to react query
    
    * Migrated comment add/delete reaction queries to react query
    
    * Migrated fetch comment reactions query to react query
    
    * Merged reaction controller state with comments controller
    
    * Migrated (half) reactionCounts query to react query files
    
    * Fix type check
    
    * Code linted and updated letter case for some func
    
    * removed extra comment
    
    * Remove .then from react query queries

---
+++ packages/commonwealth/client/scripts/state/api/comments/index.ts
@@ -0,0 +1,9 @@
+import useCreateCommentReactionMutation from './createReaction';
+import useDeleteCommentReactionMutation from './deleteReaction';
+import useFetchCommentReactionsQuery from './fetchReaction';
+
+export {
+    useCreateCommentReactionMutation,
+    useDeleteCommentReactionMutation,
+    useFetchCommentReactionsQuery
+};
