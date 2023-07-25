f5d1389b694be9b172c6f5331f6067c678217e9c
Timothee Legros <timothee@ualberta.ca>
2023-07-25 16:24:20 +0200

    notificationDataToSubscription finish

--- packages/commonwealth/shared/subscriptionMapping.ts
+++ packages/commonwealth/shared/subscriptionMapping.ts
@@ -3,12 +3,46 @@
 
 import { NotificationDataAndCategory } from 'types';
 import { NotificationCategories } from 'common-common/src/types';
+import { factory, formatFilename } from 'common-common/src/logging';
 
-export function notifDataToSubscription(
+const log = factory.getLogger(formatFilename(__filename));
+
+export function notificationDataToSubscription(
   notification: NotificationDataAndCategory
 ) {
-  if (notification.category === NotificationCategories.NewComment) {
-    // data is now inferred to be IForumNotificationData
-    console.log(notification.data.view_count); // This should work now
+  const uniqueData = { category_id: notification.category };
+  if (notification.category === NotificationCategories.ChainEvent) {
+    uniqueData['chain_id'] = notification.data.chain;
+  } else if (
+    notification.category === NotificationCategories.SnapshotProposal
+  ) {
+    uniqueData['snapshot_id'] = notification.data.space;
+  } else if (notification.category === NotificationCategories.NewThread) {
+    uniqueData['chain_id'] = notification.data.chain_id;
+  } else if (notification.category === NotificationCategories.NewComment) {
+    if (
+      notification.data.parent_comment_id &&
+      notification.data.parent_comment_text
+    ) {
+      uniqueData['comment_id'] = notification.data.parent_comment_id;
+    } else {
+      uniqueData['thread_id'] = notification.data.thread_id;
+    }
+  } else if (notification.category === NotificationCategories.NewReaction) {
+    if (notification.data.comment_id) {
+      uniqueData['comment_id'] = notification.data.comment_id;
+    } else {
+      uniqueData['thread_id'] = notification.data.thread_id;
+    }
+  } else if (notification.category === NotificationCategories.NewMention) {
+    uniqueData['subscriber_id'] = notification.data.mentioned_user_id;
+  } else if (
+    notification.category === NotificationCategories.NewCollaboration
+  ) {
+    uniqueData['subscriber_id'] = notification.data.collaborator_user_id;
+  } else {
+    log.info(`${notification.category} does not support subscriptions`);
+    return;
   }
+  return uniqueData;
 }
