3ec57d3204c06aa76657ce84718b320b2cb8f415
Timothee Legros <timothee@ualberta.ca>
2023-07-28 02:33:25 +0200

    create subscription function

--- packages/commonwealth/shared/subscriptionMapping.ts
+++ packages/commonwealth/shared/subscriptionMapping.ts
@@ -1,9 +1,8 @@
-// Create 2 functions one for mapping NotificationData to a subscription and
-// one for mapping client provided data to local/fetched subscriptions
-
 import { NotificationDataAndCategory } from 'types';
 import { NotificationCategories } from 'common-common/src/types';
 import { factory, formatFilename } from 'common-common/src/logging';
+import { SubscriptionAttributes } from '../server/models/subscription';
+import models from '../server/database';
 
 const log = factory.getLogger(formatFilename(__filename));
 
@@ -45,4 +44,49 @@ export function mapNotificationsDataToSubscriptions(
     return;
   }
   return uniqueData;
+}
+
+export async function createSubscription(subData: SubscriptionAttributes) {
+  if (
+    subData.category_id === NotificationCategories.ChainEvent &&
+    !subData.chain_id
+  ) {
+    throw new Error(
+      `chain_id cannot be undefined for a ${NotificationCategories.ChainEvent} subscription`
+    );
+  } else if (
+    subData.category_id === NotificationCategories.SnapshotProposal &&
+    !subData.snapshot_id
+  ) {
+    throw new Error(
+      `snapshot_id cannot be undefined for a ${NotificationCategories.SnapshotProposal} subscription`
+    );
+  } else if (
+    subData.category_id === NotificationCategories.NewThread &&
+    !subData.chain_id
+  ) {
+    throw new Error(
+      `chain_id cannot be undefined for a ${NotificationCategories.NewThread} subscription`
+    );
+  } else if (subData.category_id === NotificationCategories.NewComment) {
+    if (!subData.thread_id && !subData.comment_id) {
+      throw new Error(
+        `A thread-level (root) ${subData.category_id} subscription must define a thread_id` +
+          ` and a sub-level subscription must define comment_id`
+      );
+    } else if (subData.thread_id && subData.comment_id) {
+      throw new Error(
+        `A ${subData.category_id} subscription cannot define` +
+          `both a thread_id and a comment_id`
+      );
+    }
+  } else if (
+    subData.category_id === NotificationCategories.ThreadEdit ||
+    subData.category_id === NotificationCategories.CommentEdit
+  ) {
+    throw new Error(`${subData.category_id} subscriptions are not supported`);
+  }
+  // no need to check NewMention + NewCollaboration because subscriber_id is always required anyway
+
+  return models.Subscription.create(subData);
 }
