bce5b06ee748a315dbb31f9a449ec3f5cb74a8a2
Malik Zulqurnain <malikzulqurnain2121@gmail.com>
2023-08-04 07:54:38 -0700

    Moved thread sorters to helpers

--- packages/commonwealth/client/scripts/views/pages/discussions/DiscussionsPage.tsx
+++ packages/commonwealth/client/scripts/views/pages/discussions/DiscussionsPage.tsx
@@ -1,6 +1,4 @@
 import { getProposalUrlPath } from 'identifiers';
-import Thread from 'models/Thread';
-import moment from 'moment';
 import { getScopePrefix, useCommonNavigate } from 'navigation/helpers';
 import 'pages/discussions/index.scss';
 import React, { useState } from 'react';
@@ -20,41 +18,10 @@ import app from '../../../state';
 import { useFetchTopicsQuery } from '../../../state/api/topics';
 import { HeaderWithFilters } from './HeaderWithFilters';
 import { ThreadCard } from './ThreadCard';
+import { sortByFeaturedFilter, sortPinned } from './helpers';
 
 type DiscussionsPageProps = {
   topicName?: string;
-};
-
-/**
- * This function is responsible for sorting threads in state. Maybe the user pins a
- * thread, this thread is still in a lower position in the state object/arrary. This
- * function will sort those correctly.
- */
-const sortPinned = (t: Thread[]) => {
-  return [...t].sort((a, b) => (a.pinned === b.pinned ? 1 : a.pinned ? -1 : 0));
-};
-
-/**
- * This function is responsible for sorting threads in state that were earlier
- * sorted by another featured flag
- */
-const sortByFeaturedFilter = (t: Thread[], featuredFilter) => {
-  if (featuredFilter === ThreadFeaturedFilterTypes.Oldest) {
-    return [...t].sort((a, b) => moment(a.createdAt).diff(moment(b.createdAt)));
-  }
-
-  if (featuredFilter === ThreadFeaturedFilterTypes.MostComments) {
-    return [...t].sort((a, b) => b.numberOfComments - a.numberOfComments);
-  }
-
-  if (featuredFilter === ThreadFeaturedFilterTypes.MostLikes) {
-    return [...t].sort(
-      (a, b) => b.associatedReactions.length - a.associatedReactions.length
-    );
-  }
-
-  // Default: Assuming featuredFilter === 'newest'
-  return [...t].sort((a, b) => moment(b.createdAt).diff(moment(a.createdAt)));
 };
 
 const DiscussionsPage = ({ topicName }: DiscussionsPageProps) => {
