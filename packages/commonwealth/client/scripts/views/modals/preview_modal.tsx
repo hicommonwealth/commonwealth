6455037a0853c78e67475037c50a63b58bdb937b
Daniel Martins <martinsda@gmail.com>
2023-08-08 09:29:40 -0300

    add new design system to preview_modal

--- packages/commonwealth/client/scripts/views/modals/preview_modal.tsx
+++ packages/commonwealth/client/scripts/views/modals/preview_modal.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { X } from '@phosphor-icons/react';
+import type { DeltaStatic } from 'quill';
+
+import { QuillRenderer } from '../components/react_quill_editor/quill_renderer';
+import { CWText } from '../components/component_kit/cw_text';
 
 import 'modals/preview_modal.scss';
-
-import { CWIconButton } from '../components/component_kit/cw_icon_button';
-import type { DeltaStatic } from 'quill';
-import { QuillRenderer } from '../components/react_quill_editor/quill_renderer';
 
 type PreviewModalProps = {
   doc: DeltaStatic | string;
@@ -20,8 +21,8 @@ export const PreviewModal = ({
   return (
     <div className="PreviewModal">
       <div className="compact-modal-title">
-        <h3>{title ? `Preview: ${title}` : 'Preview'}</h3>
-        <CWIconButton iconName="close" onClick={() => onModalClose()} />
+        <CWText type="h4">{title ? `Preview: ${title}` : 'Preview'}</CWText>
+        <X className="close-icon" onClick={() => onModalClose()} />
       </div>
       <div className="compact-modal-body">
         <QuillRenderer
