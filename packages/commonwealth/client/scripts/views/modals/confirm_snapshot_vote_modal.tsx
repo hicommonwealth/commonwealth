238e48688c82d5f189d8859fe8a124f079b255b2
Daniel Martins <martinsda@gmail.com>
2023-08-10 11:01:29 -0300

    add new design system to confirm_snapshot_vote_modal

--- packages/commonwealth/client/scripts/views/modals/confirm_snapshot_vote_modal.tsx
+++ packages/commonwealth/client/scripts/views/modals/confirm_snapshot_vote_modal.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
+import { X } from '@phosphor-icons/react';
 
 import { notifyError } from 'controllers/app/notifications';
 import type { SnapshotProposal, SnapshotSpace } from 'helpers/snapshot_utils';
 import { castVote } from 'helpers/snapshot_utils';
 import { formatNumberShort } from 'adapters/currency';
+import app from 'state';
+import { CWButton } from '../components/component_kit/new_designs/cw_button';
+import { CWText } from '../components/component_kit/cw_text';
 
 import 'modals/confirm_snapshot_vote_modal.scss';
-
-import app from 'state';
-import { CWButton } from '../components/component_kit/cw_button';
-import { CWText } from '../components/component_kit/cw_text';
-import { CWIconButton } from '../components/component_kit/cw_icon_button';
 
 type ConfirmSnapshotVoteModalProps = {
   id: string;
@@ -44,8 +43,10 @@ export const ConfirmSnapshotVoteModal = (
   return (
     <div className="ConfirmSnapshotVoteModal">
       <div className="compact-modal-title">
-        <h3>Confirm vote</h3>
-        <CWIconButton iconName="close" onClick={() => onModalClose()} />
+        <CWText className="title-text" type="h4">
+          Confirm vote
+        </CWText>
+        <X className="close-icon" onClick={() => onModalClose()} size={24} />
       </div>
       <div className="compact-modal-body">
         <CWText type="h4" fontWeight="semiBold">
@@ -69,7 +70,8 @@ export const ConfirmSnapshotVoteModal = (
         <div className="button-group">
           <CWButton
             label="Cancel"
-            buttonType="secondary-blue"
+            buttonType="secondary"
+            buttonHeight="sm"
             disabled={isSaving}
             onClick={async (e) => {
               e.preventDefault();
@@ -78,12 +80,12 @@ export const ConfirmSnapshotVoteModal = (
           />
           <CWButton
             label="Vote"
+            buttonType="primary"
+            buttonHeight="sm"
             disabled={isSaving}
             onClick={async (e) => {
               e.preventDefault();
-
               setIsSaving(true);
-
               const votePayload = {
                 space: space.id,
                 proposal: id,
@@ -91,7 +93,6 @@ export const ConfirmSnapshotVoteModal = (
                 choice: parseInt(selectedChoice) + 1,
                 metadata: JSON.stringify({}),
               };
-
               try {
                 castVote(author.address, votePayload).then(async () => {
                   await app.snapshot.refreshProposals();
@@ -103,7 +104,6 @@ export const ConfirmSnapshotVoteModal = (
                 const errorMessage = err.message;
                 notifyError(errorMessage);
               }
-
               setIsSaving(false);
             }}
           />
