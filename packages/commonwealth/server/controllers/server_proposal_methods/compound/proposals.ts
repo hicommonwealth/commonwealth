db266ced7f53e2900b16265338474a799bda82be
Timothee Legros <timothee@ualberta.ca>
2023-08-15 23:55:41 +0200

    compound init

---
+++ packages/commonwealth/server/controllers/server_proposal_methods/compound/proposals.ts
@@ -0,0 +1,113 @@
+import { BigNumber, providers } from 'ethers';
+import {
+  GovernorCompatibilityBravo__factory,
+  IGovernorCompatibilityBravo,
+} from 'common-common/src/eth/types';
+import { TypedEvent } from 'common-common/src/eth/types/commons';
+import { ICompoundProposalResponse } from 'adapters/chain/compound/types';
+
+type UnwrapPromise<T> = T extends Promise<infer U> ? U : T;
+
+type CompoundBravoProposalType = UnwrapPromise<
+  ReturnType<IGovernorCompatibilityBravo['proposals']>
+>;
+
+type ProposalCreatedEventArgsArray = [
+  BigNumber,
+  string,
+  string[],
+  BigNumber[],
+  string[],
+  string[],
+  BigNumber,
+  BigNumber,
+  string
+];
+
+type ProposalCreatedEventArgsObject = {
+  proposalId: BigNumber;
+  proposer: string;
+  targets: string[];
+  values: BigNumber[];
+  signatures: string[];
+  calldatas: string[];
+  startBlock: BigNumber;
+  endBlock: BigNumber;
+  description: string;
+};
+
+type ProposalDataType = {
+  rawProposal: CompoundBravoProposalType;
+  proposalState: number;
+  proposalCreatedEvent: TypedEvent<
+    ProposalCreatedEventArgsArray & ProposalCreatedEventArgsObject
+  >;
+};
+
+export function formatCompoundBravoProposal(
+  proposalData: ProposalDataType
+): ICompoundProposalResponse {
+  return {
+    identifier: proposalData.rawProposal.id.toString(),
+  };
+}
+
+export async function getCompoundBravoProposals(
+  compoundGovAddress: string,
+  provider: providers.Web3Provider
+): Promise<ProposalDataType[]> {
+  const contract = GovernorCompatibilityBravo__factory.connect(
+    compoundGovAddress,
+    provider
+  );
+  await contract.deployed();
+
+  const initialProposalId = await contract.initialProposalId();
+  const proposalCount = await contract.proposalCount();
+
+  const proposalPromises: Promise<CompoundBravoProposalType>[] = [];
+  const proposalStatePromises: Promise<number>[] = [];
+  for (let i = initialProposalId; i <= proposalCount; i++) {
+    const proposal = contract.proposals(i);
+    proposalPromises.push(proposal);
+    proposalStatePromises.push(contract.state(i));
+  }
+
+  const proposalCreatedEventPromise = contract.queryFilter<
+    ProposalCreatedEventArgsArray,
+    ProposalCreatedEventArgsObject
+  >(
+    contract.filters.ProposalCreated(
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null,
+      null
+    ),
+    0,
+    'latest'
+  );
+
+  const [proposals, proposalStates, proposalCreatedEvents] = await Promise.all([
+    Promise.all(proposalPromises),
+    Promise.all(proposalStatePromises),
+    proposalCreatedEventPromise,
+  ]);
+
+  const allProposalData: ProposalDataType[] = [];
+  for (let i = 0; i < proposals.length; i++) {
+    allProposalData.push({
+      rawProposal: proposals[i],
+      proposalState: proposalStates[i],
+      proposalCreatedEvent: proposalCreatedEvents.find((p) =>
+        p.args.proposalId.eq(i)
+      ),
+    });
+  }
+
+  return allProposalData;
+}
