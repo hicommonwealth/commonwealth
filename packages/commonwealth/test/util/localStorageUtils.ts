5cfea48e058eb00e8087ff4e2516ac9b3181fdf9
dillchen <dillon@commonwealth.im>
2023-08-23 04:34:12 +0800

    mobile behavior fixed

--- packages/commonwealth/test/util/localStorageUtils.ts
+++ packages/commonwealth/test/util/localStorageUtils.ts
@@ -1,6 +1,8 @@
 import { JSDOM } from 'jsdom';
 
-const dom = new JSDOM('<!doctype html><html><body></body></html>');
+const dom = new JSDOM('<!doctype html><html><body></body></html>', {
+  url: 'http://localhost/',
+});
 (global as any).window = dom.window;
 (global as any).document = dom.window.document;
 (global as any).localStorage = dom.window.localStorage;
