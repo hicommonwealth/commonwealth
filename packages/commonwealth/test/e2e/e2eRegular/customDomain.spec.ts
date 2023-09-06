570b623058c17e94ba782efab47a1d24af4dda71
kassad <kurtis@commonwealth.im>
2023-09-06 12:01:12 -0700

    Added cleanup to newDiscussion test

--- packages/commonwealth/test/e2e/e2eRegular/customDomain.spec.ts
+++ packages/commonwealth/test/e2e/e2eRegular/customDomain.spec.ts
@@ -4,7 +4,7 @@ import { addAlchemyKey } from '../utils/e2eUtils';
 import { discussionTests } from './discussionsTest';
 
 // ssl proxy takes a while, so increase test timeout
-test.setTimeout(220000);
+test.setTimeout(200000);
 
 test.use({ ignoreHTTPSErrors: true });
 
@@ -16,7 +16,7 @@ test.beforeEach(async ({ page }) => {
   // proxy can be slow to start up, so we wait for it.
   await expect(async () => {
     await page.goto(`https://${testChains[0]['custom_domain']}/`);
-  }).toPass();
+  }).toPass({ intervals: [20_000] });
 });
 
 test.describe('Commonwealth custom domain Homepage', discussionTests(test));
