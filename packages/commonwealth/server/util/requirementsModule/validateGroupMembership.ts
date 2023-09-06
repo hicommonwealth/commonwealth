a5c2a29e1d313f181166005d4a2cbe5eff5890f3
ianrowan <i.rowan299@gmail.com>
2023-09-06 10:28:40 -0500

    add docs

--- packages/commonwealth/server/util/requirementsModule/validateGroupMembership.ts
+++ packages/commonwealth/server/util/requirementsModule/validateGroupMembership.ts
@@ -9,6 +9,12 @@ export type validateGroupMembershipResponse = {
   }[];
 };
 
+/**
+ * Validates if a given user address passes a set of requirements and grants access to group
+ * @param userAddress address of user
+ * @param requirements An array of requirement types to be validated against
+ * @returns validateGroupMembershipResponse validity and messages on requirements that failed
+ */
 export default function validateGroupMembership(
   userAddress: string,
   requirements: Requirement[]
--- packages/commonwealth/server/util/requirementsModule/validateRequirements.ts
+++ packages/commonwealth/server/util/requirementsModule/validateRequirements.ts
@@ -1,5 +1,10 @@
 import { Requirement } from './requirmentsTypes';
 
+/**
+ * validates a set of requirements against the schema
+ * @param requirements an array of requirements types
+ * @returns
+ */
 export default function validateRequirements(
   requirements: Requirement[]
 ): requirements is Requirement[] {
