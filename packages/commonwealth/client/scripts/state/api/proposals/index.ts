880836e03c566a8f68fad4132e2300a68b6136c8
Timothee Legros <timothee@ualberta.ca>
2023-09-06 23:15:53 +0200

    move cosmos files

--- packages/commonwealth/client/scripts/state/api/proposals/index.ts
+++ packages/commonwealth/client/scripts/state/api/proposals/index.ts
@@ -1,13 +1,12 @@
 import { useActiveCosmosProposalsQuery } from './cosmos/fetchActiveCosmosProposals';
 import { useCompletedCosmosProposalsQuery } from './cosmos/fetchCompletedCosmosProposals';
-import { useCosmosProposalMetadataQuery } from './fetchCosmosProposalMetadata';
-import { useCosmosProposal } from './fetchCosmosProposal';
-import { useCosmosTally } from './fetchCosmosTally';
-import { useCosmosVotes } from './fetchCosmosVotes';
-import { useCosmosDeposits } from './fetchCosmosDeposits';
+import { useCosmosProposalMetadataQuery } from './cosmos/fetchCosmosProposalMetadata';
+import { useCosmosProposal } from './cosmos/fetchCosmosProposal';
+import { useCosmosTally } from './cosmos/fetchCosmosTally';
+import { useCosmosVotes } from './cosmos/fetchCosmosVotes';
+import { useCosmosDeposits } from './cosmos/fetchCosmosDeposits';
 import useAaveProposalsQuery from 'state/api/proposals/aave/fetchAaveProposals';
 import useAaveProposalVotesQuery from 'state/api/proposals/aave/fetchAaveProposalVotes';
-
 
 export {
   useActiveCosmosProposalsQuery,
