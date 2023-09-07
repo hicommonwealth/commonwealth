ef2ef899a8f87986978ebbe481b42dbb2fe9fdea
ianrowan <i.rowan299@gmail.com>
2023-09-07 13:39:47 -0500

    test unit test

--- packages/commonwealth/test/unit/requirementsModule/requirements.spec.ts
+++ packages/commonwealth/test/unit/requirementsModule/requirements.spec.ts
@@ -3,16 +3,40 @@ import { Requirement } from 'server/util/requirementsModule/requirementsTypes';
 import validateGroupMembership, {
   validateGroupMembershipResponse,
 } from 'server/util/requirementsModule/validateGroupMembership';
+import { TokenBalanceCache } from '../../../../token-balance-cache/src';
 
 describe('validateGroupMembership', () => {
-  it('should return a valid response', () => {
-    const userAddress: string = 'mockUserAddress';
-    const requirements: Requirement[] = [];
-
-    const result: validateGroupMembershipResponse = validateGroupMembership(
-      userAddress,
-      requirements
-    );
+  it('should pass basic erc20 check', async () => {
+    const userAddress: string = '0x123456';
+    const requirements: Requirement[] = [
+      {
+        rule: 'threshold',
+        data: {
+          threshold: '1000',
+          source: {
+            source_type: 'erc20',
+            chain_id: '1',
+            contract_address: '0x12345',
+          },
+        },
+      },
+    ];
+    const tbc = {
+      fetchUserBalanceWithChain: async function (
+        network,
+        userAddress: string,
+        chainId: string,
+        contractAddress?: string
+      ): Promise<string> {
+        return '1000';
+      },
+    };
+    const result: validateGroupMembershipResponse =
+      await validateGroupMembership(
+        userAddress,
+        requirements,
+        tbc as TokenBalanceCache
+      );
 
     assert.equal(result.isValid, true);
   });
