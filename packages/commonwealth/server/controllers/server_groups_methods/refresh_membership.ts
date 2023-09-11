ba8252170f50c03cdb4507c9a38298d778433735
Ryan Bennett <126922418+rbennettcw@users.noreply.github.com>
2023-09-11 09:19:05 -0700

    Added gating stub API routes + controller + unit tests (#5016)
    * added gating API routes + controller + unit tests
    
    * fix imports + add requirements validation
    
    * move requirements validation to controller
    
    * add todos
    
    * make refresh-memberships topic ID optional

---
+++ packages/commonwealth/server/controllers/server_groups_methods/refresh_membership.ts
@@ -0,0 +1,41 @@
+import { ChainInstance } from 'server/models/chain';
+import { ServerChainsController } from '../server_chains_controller';
+import { AddressInstance } from '../../models/address';
+import { UserInstance } from '../../models/user';
+
+export type RefreshMembershipOptions = {
+  user: UserInstance;
+  chain: ChainInstance;
+  address: AddressInstance;
+  topicId: number;
+};
+export type RefreshMembershipResult = {
+  topicId?: number;
+  allowed: boolean;
+  rejectReason?: string;
+};
+
+export async function __refreshMembership(
+  this: ServerChainsController,
+  options: RefreshMembershipOptions
+): Promise<RefreshMembershipResult> {
+  /*
+    TODO: Check membership status of address for all groups for all topics within the chain,
+    or optionally for the single specified topic
+      - if membership missing or stale => recompute, save and return membership
+      - else if membership fresh => return membership
+
+      Membership model:
+      {
+        group_id: number
+        address_id: number
+        allowed: boolean
+        reject_reason?: string
+        last_checked: Date
+      }
+  */
+  return {
+    topicId: 1,
+    allowed: true,
+  };
+}
