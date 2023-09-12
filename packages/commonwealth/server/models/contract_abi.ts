ff4dc0480f7596d529cfb0bc55c1faacff0105cf
Timothee Legros <timothee@ualberta.ca>
2023-09-13 01:43:59 +0200

    add abi_hash to contract_abi model

--- packages/commonwealth/server/models/contract_abi.ts
+++ packages/commonwealth/server/models/contract_abi.ts
@@ -1,11 +1,25 @@
 import type * as Sequelize from 'sequelize'; // must use "* as" to avoid scope errors
 import type { DataTypes } from 'sequelize';
 import type { ModelInstance, ModelStatic } from './types';
+import crypto from 'crypto';
+
+function hashAbi(abi: any) {
+  let stringifiedAbi: any;
+  if (typeof abi === 'string') {
+    stringifiedAbi = JSON.parse(abi);
+  } else {
+    stringifiedAbi = abi;
+  }
+  const hash = crypto.createHash('sha256');
+  hash.update(stringifiedAbi);
+  return hash.digest('hex');
+}
 
 export type ContractAbiAttributes = {
   id: number;
   nickname?: string;
   abi: string;
+  abi_hash?: string;
   verified?: boolean;
   created_at?: Date;
   updated_at?: Date;
@@ -24,7 +38,8 @@ export default (
     {
       id: { type: dataTypes.INTEGER, primaryKey: true, autoIncrement: true },
       nickname: { type: dataTypes.STRING, allowNull: true },
-      abi: { type: dataTypes.JSONB, allowNull: false, unique: true },
+      abi: { type: dataTypes.JSONB, allowNull: false },
+      abi_hash: { type: dataTypes.TEXT, allowNull: false, unique: true },
       verified: {
         type: dataTypes.BOOLEAN,
         allowNull: false,
@@ -39,6 +54,13 @@ export default (
       underscored: true,
       createdAt: 'created_at',
       updatedAt: 'updated_at',
+      hooks: {
+        beforeValidate(instance: ContractAbiInstance) {
+          if (!instance.abi_hash) {
+            instance.abi_hash = hashAbi(instance.abi);
+          }
+        },
+      },
     }
   );
 
