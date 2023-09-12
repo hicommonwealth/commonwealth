889e6d28554abb4d901afcdc95e3e7686a8e4d3b
Timothee Legros <timothee@ualberta.ca>
2023-09-12 22:12:02 +0200

    Revert "added event_definition to EvmEventSources table"
    This reverts commit 670054081c488cf983f13219b2b8489a2696f068.

--- packages/commonwealth/server/migrations/20230908215828-evm-ce-2.js
+++ packages/commonwealth/server/migrations/20230908215828-evm-ce-2.js
@@ -13,7 +13,6 @@ module.exports = {
           chain_node_id INTEGER NOT NULL REFERENCES "ChainNodes"(id),
           contract_address VARCHAR(255) NOT NULL,
           event_signature VARCHAR(255) NOT NULL,
-          event_definition TEXT NOT NULL,
           kind VARCHAR(255) NOT NULL,
           CONSTRAINT unique_event_source UNIQUE(chain_node_id, contract_address, event_signature)
         );
@@ -76,7 +75,6 @@ module.exports = {
             contract_address: '0xEC568fffba86c094cf06b22134B23074DFE2252c',
             event_signature:
               '0x789cf55be980739dad1d0699b93b58e806b51c9d96619bfa8fe0a28abaa7b30c',
-            event_definition: 'ProposalCanceled(uint256)',
             kind: 'proposal-canceled',
           },
           {
@@ -84,8 +82,6 @@ module.exports = {
             contract_address: '0xEC568fffba86c094cf06b22134B23074DFE2252c',
             event_signature:
               '0xd272d67d2c8c66de43c1d2515abb064978a5020c173e15903b6a2ab3bf7440ec',
-            event_definition:
-              'ProposalCreated(uint256,address,address,address[],uint256[],string[],bytes[],bool[],uint256,uint256,address,bytes32)',
             kind: 'proposal-created',
           },
           {
@@ -93,7 +89,6 @@ module.exports = {
             contract_address: '0xEC568fffba86c094cf06b22134B23074DFE2252c',
             event_signature:
               '0x9c85b616f29fca57a17eafe71cf9ff82ffef41766e2cf01ea7f8f7878dd3ec24',
-            event_definition: 'ProposalExecuted(uint256,address)',
             kind: 'proposal-executed',
           },
           {
@@ -101,7 +96,6 @@ module.exports = {
             contract_address: '0xEC568fffba86c094cf06b22134B23074DFE2252c',
             event_signature:
               '0x11a0b38e70585e4b09b794bd1d9f9b1a51a802eb8ee2101eeee178d0349e73fe',
-            event_definition: 'ProposalQueued(uint256,uint256,address)',
             kind: 'proposal-queued',
           },
 
@@ -111,7 +105,6 @@ module.exports = {
             contract_address: '0x7E9B1672616FF6D6629Ef2879419aaE79A9018D2',
             event_signature:
               '0x789cf55be980739dad1d0699b93b58e806b51c9d96619bfa8fe0a28abaa7b30c',
-            event_definition: 'ProposalCanceled(uint256)',
             kind: 'proposal-canceled',
           },
           {
@@ -119,8 +112,6 @@ module.exports = {
             contract_address: '0x7E9B1672616FF6D6629Ef2879419aaE79A9018D2',
             event_signature:
               '0xd272d67d2c8c66de43c1d2515abb064978a5020c173e15903b6a2ab3bf7440ec',
-            event_definition:
-              'ProposalCreated(uint256,address,address,address[],uint256[],string[],bytes[],bool[],uint256,uint256,address,bytes32)',
             kind: 'proposal-created',
           },
           {
@@ -128,7 +119,6 @@ module.exports = {
             contract_address: '0x7E9B1672616FF6D6629Ef2879419aaE79A9018D2',
             event_signature:
               '0x9c85b616f29fca57a17eafe71cf9ff82ffef41766e2cf01ea7f8f7878dd3ec24',
-            event_definition: 'ProposalExecuted(uint256,address)',
             kind: 'proposal-executed',
           },
           {
@@ -136,7 +126,6 @@ module.exports = {
             contract_address: '0x7E9B1672616FF6D6629Ef2879419aaE79A9018D2',
             event_signature:
               '0x11a0b38e70585e4b09b794bd1d9f9b1a51a802eb8ee2101eeee178d0349e73fe',
-            event_definition: 'ProposalQueued(uint256,uint256,address)',
             kind: 'proposal-queued',
           },
 
@@ -146,7 +135,6 @@ module.exports = {
             contract_address: '0x0BEF27FEB58e857046d630B2c03dFb7bae567494',
             event_signature:
               '0x789cf55be980739dad1d0699b93b58e806b51c9d96619bfa8fe0a28abaa7b30c',
-            event_definition: 'ProposalCanceled(uint256)',
             kind: 'proposal-canceled',
           },
           {
@@ -154,8 +142,6 @@ module.exports = {
             contract_address: '0x0BEF27FEB58e857046d630B2c03dFb7bae567494',
             event_signature:
               '0x7d84a6263ae0d98d3329bd7b46bb4e8d6f98cd35a7adb45c274c8b7fd5ebd5e0',
-            event_definition:
-              'ProposalCreated(uint256,address,address[],uint256[],string[],bytes[],uint256,uint256,string)',
             kind: 'proposal-created',
           },
           {
@@ -163,7 +149,6 @@ module.exports = {
             contract_address: '0x0BEF27FEB58e857046d630B2c03dFb7bae567494',
             event_signature:
               '0x712ae1383f79ac853f8d882153778e0260ef8f03b504e2866e0593e04d2b291f',
-            event_definition: 'ProposalExecuted(uint256)',
             kind: 'proposal-executed',
           },
           {
@@ -171,7 +156,6 @@ module.exports = {
             contract_address: '0x0BEF27FEB58e857046d630B2c03dFb7bae567494',
             event_signature:
               '0x9a2e42fd6722813d69113e7d0079d3d940171428df7373df9c7f7617cfda2892',
-            event_definition: 'ProposalQueued(uint256,uint256)',
             kind: 'proposal-queued',
           },
         ];
@@ -188,7 +172,6 @@ module.exports = {
             contract_address: '0xac4610582926DcF22bf327AbB6F6aC82BD49FE0f',
             event_signature:
               '0x789cf55be980739dad1d0699b93b58e806b51c9d96619bfa8fe0a28abaa7b30c',
-            event_definition: 'ProposalCanceled(uint256)',
             kind: 'proposal-canceled',
           },
           {
@@ -196,8 +179,6 @@ module.exports = {
             contract_address: '0xac4610582926DcF22bf327AbB6F6aC82BD49FE0f',
             event_signature:
               '0x7d84a6263ae0d98d3329bd7b46bb4e8d6f98cd35a7adb45c274c8b7fd5ebd5e0',
-            event_definition:
-              'ProposalCreated(uint256,address,address[],uint256[],string[],bytes[],uint256,uint256,string)',
             kind: 'proposal-created',
           },
           {
@@ -205,7 +186,6 @@ module.exports = {
             contract_address: '0xac4610582926DcF22bf327AbB6F6aC82BD49FE0f',
             event_signature:
               '0x712ae1383f79ac853f8d882153778e0260ef8f03b504e2866e0593e04d2b291f',
-            event_definition: 'ProposalExecuted(uint256)',
             kind: 'proposal-executed',
           },
           {
@@ -213,7 +193,6 @@ module.exports = {
             contract_address: '0xac4610582926DcF22bf327AbB6F6aC82BD49FE0f',
             event_signature:
               '0x9a2e42fd6722813d69113e7d0079d3d940171428df7373df9c7f7617cfda2892',
-            event_definition: 'ProposalQueued(uint256,uint256)',
             kind: 'proposal-queued',
           },
         ];
@@ -230,7 +209,6 @@ module.exports = {
             contract_address: '0x8f8BB984e652Cb8D0aa7C9D6712Ec2020EB1BAb4',
             event_signature:
               '0x789cf55be980739dad1d0699b93b58e806b51c9d96619bfa8fe0a28abaa7b30c',
-            event_definition: 'ProposalCanceled(uint256)',
             kind: 'proposal-canceled',
           },
           {
@@ -238,8 +216,6 @@ module.exports = {
             contract_address: '0x8f8BB984e652Cb8D0aa7C9D6712Ec2020EB1BAb4',
             event_signature:
               '0x7d84a6263ae0d98d3329bd7b46bb4e8d6f98cd35a7adb45c274c8b7fd5ebd5e0',
-            event_definition:
-              'ProposalCreated(uint256,address,address[],uint256[],string[],bytes[],uint256,uint256,string)',
             kind: 'proposal-created',
           },
           {
@@ -247,7 +223,6 @@ module.exports = {
             contract_address: '0x8f8BB984e652Cb8D0aa7C9D6712Ec2020EB1BAb4',
             event_signature:
               '0x712ae1383f79ac853f8d882153778e0260ef8f03b504e2866e0593e04d2b291f',
-            event_definition: 'ProposalExecuted(uint256)',
             kind: 'proposal-executed',
           },
           {
@@ -255,7 +230,6 @@ module.exports = {
             contract_address: '0x8f8BB984e652Cb8D0aa7C9D6712Ec2020EB1BAb4',
             event_signature:
               '0x9a2e42fd6722813d69113e7d0079d3d940171428df7373df9c7f7617cfda2892',
-            event_definition: 'ProposalQueued(uint256,uint256)',
             kind: 'proposal-queued',
           },
 
@@ -265,7 +239,6 @@ module.exports = {
             contract_address: '0xde457ed1A713C290C4f8dE1dE0D0308Fc7722937',
             event_signature:
               '0x789cf55be980739dad1d0699b93b58e806b51c9d96619bfa8fe0a28abaa7b30c',
-            event_definition: 'ProposalCanceled(uint256)',
             kind: 'proposal-canceled',
           },
           {
@@ -273,8 +246,6 @@ module.exports = {
             contract_address: '0xde457ed1A713C290C4f8dE1dE0D0308Fc7722937',
             event_signature:
               '0x7d84a6263ae0d98d3329bd7b46bb4e8d6f98cd35a7adb45c274c8b7fd5ebd5e0',
-            event_definition:
-              'ProposalCreated(uint256,address,address[],uint256[],string[],bytes[],uint256,uint256,string)',
             kind: 'proposal-created',
           },
           {
@@ -282,7 +253,6 @@ module.exports = {
             contract_address: '0xde457ed1A713C290C4f8dE1dE0D0308Fc7722937',
             event_signature:
               '0x712ae1383f79ac853f8d882153778e0260ef8f03b504e2866e0593e04d2b291f',
-            event_definition: 'ProposalExecuted(uint256)',
             kind: 'proposal-executed',
           },
           {
@@ -290,7 +260,6 @@ module.exports = {
             contract_address: '0xde457ed1A713C290C4f8dE1dE0D0308Fc7722937',
             event_signature:
               '0x9a2e42fd6722813d69113e7d0079d3d940171428df7373df9c7f7617cfda2892',
-            event_definition: 'ProposalQueued(uint256,uint256)',
             kind: 'proposal-queued',
           },
         ];
