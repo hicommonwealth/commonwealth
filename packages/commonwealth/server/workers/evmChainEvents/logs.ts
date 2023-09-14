c0dac594c47b12a875d3c57783affe6cd0fe5730
Timothee Legros <timothee@ualberta.ca>
2023-09-15 01:09:20 +0200

    separate out and update getEventSources - integrate ABI changes

--- packages/commonwealth/server/workers/evmChainEvents/logs.ts
+++ packages/commonwealth/server/workers/evmChainEvents/logs.ts
@@ -1,17 +1,27 @@
-import { EvmEventSourceInstance } from '../../models/evmEventSource';
 import { ethers } from 'ethers';
 import { Log } from '@ethersproject/providers';
+import {
+  AbiSignatures,
+  ContractSources,
+  EvmSource,
+  RawEvmEvent,
+} from './types';
+import { decimalToHex } from 'chain-events/src';
+import { StatsDController } from 'common-common/src/statsd';
+import { factory, formatFilename } from 'common-common/src/logging';
+import { rollbar } from './startEvmPolling';
+
+const logger = factory.getLogger(formatFilename(__filename));
 
 function getProvider(rpc: string) {
   return new ethers.providers.JsonRpcProvider(rpc);
 }
 
 async function getLogs(
-  rpc: string,
-  eventSources: EvmEventSourceInstance[],
+  evmSource: EvmSource,
   startingBlockNum?: number
 ): Promise<{ logs: Log[]; lastBlockNum: number }> {
-  const provider = getProvider(rpc);
+  const provider = getProvider(evmSource.rpc);
   const currentBlockNum = await provider.getBlockNumber();
 
   // limit the number of blocks to fetch to 500 to avoid rate limiting on some EVM nodes like Celo
@@ -19,42 +29,67 @@ async function getLogs(
   // rate limits e.g. Alchemy hasa limit of 10k logs while Celo public nodes have a limit of 500.
   if (!startingBlockNum || currentBlockNum - startingBlockNum > 500) {
     // TODO: log and report warning with the exact block range that is skipped
-    startingBlockNum = currentBlockNum - 10;
+    startingBlockNum = currentBlockNum - 9999;
   }
 
+  console.log(`Fetching logs from ${startingBlockNum} to ${currentBlockNum}`);
   const logs: Log[] = await provider.send('eth_getLogs', [
     {
-      fromBlock: startingBlockNum,
-      toBlock: currentBlockNum,
-      address: eventSources.map((eventSource) => eventSource.contract_address),
+      fromBlock: decimalToHex(startingBlockNum),
+      toBlock: decimalToHex(currentBlockNum),
+      address: Object.keys(evmSource.contracts),
     },
   ]);
 
   return { logs, lastBlockNum: currentBlockNum };
 }
 
-async function parseLogs(eventSources: EvmEventSourceInstance[], logs: Log[]) {
-  const events = [];
-  const eventSourceMap = new Set(
-    eventSources.map((eventSource) => eventSource.contract_address)
-  );
+async function parseLogs(
+  sources: ContractSources,
+  logs: Log[]
+): Promise<RawEvmEvent[]> {
+  const events: RawEvmEvent[] = [];
+  const interfaces = {};
   for (const log of logs) {
-    if (eventSourceMap.has(log.topics[0])) {
-      events.push(log);
+    const address = ethers.utils.getAddress(log.address);
+    const data: AbiSignatures = sources[address];
+    const signature = data.sources.find(
+      (s) => s.event_signature === log.topics[0]
+    );
+    if (signature) {
+      if (!interfaces[address]) {
+        interfaces[address] = new ethers.utils.Interface(data.abi);
+      }
+      let parsedLog: ethers.utils.LogDescription;
+      try {
+        parsedLog = interfaces[address].parseLog(log);
+      } catch (e) {
+        const msg = `Failed to parse log from contract ${address} with signature ${log.topics[0]}`;
+        logger.error(msg, e);
+        rollbar.error(msg, e);
+        continue;
+      }
+      StatsDController.get().increment('ce.evm.event', {
+        contractAddress: address,
+        kind: signature.kind,
+      });
+      events.push({
+        contractAddress: address,
+        kind: signature.kind,
+        blockNumber: parseInt(log.blockNumber.toString(), 16),
+        args: parsedLog.args,
+      });
     }
   }
+
+  return events;
 }
 
 export async function getEvents(
-  rpc: string,
-  eventSources: EvmEventSourceInstance[],
+  evmSource: EvmSource,
   startingBlockNum?: number
-): Promise<{ events: any; lastBlockNum: number }> {
-  const { logs, lastBlockNum } = await getLogs(
-    rpc,
-    eventSources,
-    startingBlockNum
-  );
-  const events = await parseLogs(eventSources, logs);
+): Promise<{ events: RawEvmEvent[]; lastBlockNum: number }> {
+  const { logs, lastBlockNum } = await getLogs(evmSource, startingBlockNum);
+  const events = await parseLogs(evmSource.contracts, logs);
   return { events, lastBlockNum };
 }
--- packages/commonwealth/server/workers/evmChainEvents/startEvmPolling.ts
+++ packages/commonwealth/server/workers/evmChainEvents/startEvmPolling.ts
@@ -1,109 +1,168 @@
 import { getEvents } from './logs';
-import { EvmEventSourceInstance } from '../../models/evmEventSource';
 import models from '../../database';
+import { EvmSource, EvmSources, RawEvmEvent } from './types';
+import { QueryTypes } from 'sequelize';
+import { getEventSources } from './getEventSources';
+import emitNotifications from '../../util/emitNotifications';
+import {
+  IChainEventNotificationData,
+  NotificationDataAndCategory,
+} from 'types';
+import { NotificationCategories } from 'common-common/src/types';
+import { ChainAttributes } from '../../models/chain';
+import { ContractAttributes } from '../../models/contract';
+import { SupportedNetwork } from 'chain-events/src';
+import { factory, formatFilename } from 'common-common/src/logging';
+import { StatsDController } from 'common-common/src/statsd';
+import Rollbar from 'rollbar';
+import { ROLLBAR_ENV, ROLLBAR_SERVER_TOKEN } from '../../config';
 
-export type EventRpcSources = Record<string, EvmEventSourceInstance[]>;
+const log = factory.getLogger(formatFilename(__filename));
 
-async function getEventSources(): Promise<EventRpcSources> {
-  // this function needs to first fetch all chain-event subscription chains
-  // then we fetch all contract addresses and chain node ids (through either
-  // CommunityContracts or ChainNodes) before fetching all event sources
-  // that match the contract address and chain node id.
-  // Thus this function will only return event sources on which there are
-  // active chain-event subscriptions.
-  const eventSources = await models.EvmEventSource.findAll();
+// TODO: can we make this a util singleton for easy across the server?
+export const rollbar = new Rollbar({
+  accessToken: ROLLBAR_SERVER_TOKEN,
+  environment: ROLLBAR_ENV,
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+});
 
-  return eventSources.reduce((acc, eventSource) => {
-    if (!eventSource.ChainNode) {
-      // TODO: log and report error
-      return acc;
-    }
-
-    const url = eventSource.ChainNode.private_url || eventSource.ChainNode.url;
-    if (!acc[url]) {
-      acc[url] = [eventSource];
-    }
-    acc[url].push(eventSource);
-    return acc;
-  }, {});
-}
-
-async function emitChainEventNotifs(events: any): Promise<void> {
-  // this function needs to map event sources to chain-based subscriptions
-  // this can be done as follows:
-  // EvmEventSources(contract_address, chain_node_d) maps to
-  // Contracts(address, chain_node_id) which maps to
-  // CommunityContracts(contract_id, chain_id) which maps to
-  // Subscriptions(category = 'chain-event', chain_id)
-  // Since we need Chain.network in the final event data we
-  // will need to join with the Chains table as well
-  console.log();
-}
-
-export async function processChain(
-  rpc: string,
-  eventSources: EvmEventSourceInstance[]
+async function emitChainEventNotifs(
+  chainNodeId: number,
+  events: RawEvmEvent[]
 ): Promise<void> {
-  if (!eventSources.length) {
+  if (!events.length) {
     return;
   }
 
+  const queryFilter: [string, number][] = events.map((event) => [
+    event.contractAddress,
+    chainNodeId,
+  ]);
+
+  const chainData: {
+    chain_id: ChainAttributes['id'];
+    chain_network: ChainAttributes['network'];
+    contract_address: ContractAttributes['address'];
+    chain_node_id: ContractAttributes['chain_node_id'];
+  }[] = await models.sequelize.query(
+    `
+    SELECT CH.id as chain_id, CH.network as chain_network, C.address as contract_address, C.chain_node_id
+    FROM "Contracts" C
+             JOIN "CommunityContracts" CC on C.id = CC.contract_id
+             JOIN "Chains" CH ON CC.chain_id = CH.id
+    WHERE (C.address, C.chain_node_id) IN (?);
+  `,
+    { type: QueryTypes.SELECT, raw: true, replacements: [queryFilter] }
+  );
+
+  for (const event of events) {
+    const chain = chainData.find(
+      (c) =>
+        c.contract_address === event.contractAddress &&
+        c.chain_node_id === chainNodeId
+    );
+
+    const notification: NotificationDataAndCategory = {
+      categoryId: NotificationCategories.ChainEvent,
+      data: {
+        chain: chain.chain_id,
+        network: chain.chain_network as unknown as SupportedNetwork,
+        event_data: {
+          block_number: event.blockNumber,
+          kind: event.kind,
+          // TODO: @Timothee for now all event sources have proposal id as the first argument in the future
+          //  we will store raw arguments and use the custom labeling system when pulling/viewing event notifications
+          id: event.args[0].toNumber(),
+        },
+      } as IChainEventNotificationData, // TODO: @Timothee type coercion removed once Cosmos Notif Generator merged
+    };
+    emitNotifications(models, notification).catch((e) => {
+      const msg = `Error occurred while emitting a chain-event notification for event: ${JSON.stringify(
+        event,
+        null,
+        2
+      )}`;
+      log.error(msg, e);
+      rollbar.error(msg, e);
+    });
+  }
+}
+
+async function processChainNode(
+  chainNodeId: number,
+  evmSource: EvmSource
+): Promise<void> {
   try {
+    log.info(
+      'Processing:\n' +
+        `\tchainNodeId: ${chainNodeId}\n` +
+        `\tcontracts: ${JSON.stringify(Object.keys(evmSource.contracts))}`
+    );
+    StatsDController.get().increment('ce.evm.chain_node_id', {
+      chainNodeId: String(chainNodeId),
+    });
     const startBlock = await models.LastProcessedEvmBlock.findOne({
       where: {
-        chain_node_id: eventSources[0].chain_node_id,
+        chain_node_id: chainNodeId,
       },
     });
 
     const { events, lastBlockNum } = await getEvents(
-      rpc,
-      eventSources,
+      evmSource,
       startBlock?.block_number
     );
 
-    await emitChainEventNotifs(events);
+    await emitChainEventNotifs(chainNodeId, events);
 
-    // save new block number
-    // no need to transactionalize emitNotifications and save new block number
-    // since duplicate notifications are not possible
-    startBlock.block_number = lastBlockNum;
-    startBlock.save();
-  } catch (e) {}
+    if (!startBlock) {
+      await models.LastProcessedEvmBlock.create({
+        chain_node_id: chainNodeId,
+        block_number: lastBlockNum,
+      });
+    } else {
+      startBlock.block_number = lastBlockNum;
+      startBlock.save();
+    }
+
+    log.info(
+      `Processed ${events.length} events for chainNodeId ${chainNodeId}`
+    );
+  } catch (e) {
+    const msg = `Error occurred while processing chainNodeId ${chainNodeId}`;
+    log.error(msg, e);
+    rollbar.critical(msg, e);
+  }
 }
 
 /**
  * Schedules processFn execution for each chainNode RPC in eventRpcSources. processFn execution is scheduled
  * evenly across the interval time so that blocks are not fetched all at once for all chainNodes. For example,
  * if there are 2 chainNodes and the interval is 4000ms, then processFn will be called for chainNode1 at T=0ms
- * and at T=2000ms and for chainNode2. This would repeat every 4000ms so the next call for chainNode1 would be
- * at T=4000ms and for chainNode2 at T=6000ms.
- * @param eventRpcSources An object where the keys are chainNode RPCs and the values are EvmEventSourceInstance arrays
+ * and at T=2000ms and for chainNode2.
  * @param interval Time in milliseconds between each fetch of a ChainNode's blocks
  * @param processFn WARNING: must never throw an error. Errors thrown by processFn will not be caught.
  */
-export function scheduleBlockFetching(
-  eventRpcSources: EventRpcSources,
+export async function scheduleBlockFetching(
   interval: number,
-  processFn: (
-    rpc: string,
-    eventSources: EvmEventSourceInstance[]
-  ) => Promise<void>
+  processFn: (chainNodeId: number, sources: EvmSource) => Promise<void>
 ) {
-  if (!Object.keys(eventRpcSources).length) {
+  const evmSources = await getEventSources();
+
+  const numEvmSources = Object.keys(evmSources).length;
+  if (!numEvmSources) {
     return;
   }
 
-  const rpcs = Object.keys(eventRpcSources);
-  const betweenInterval = interval / rpcs.length;
+  const chainNodeIds = Object.keys(evmSources);
+  const betweenInterval = interval / numEvmSources;
 
-  rpcs.forEach((rpc, index) => {
-    const initialDelay = index * betweenInterval;
+  chainNodeIds.forEach((chainNodeId, index) => {
+    const delay = index * betweenInterval;
 
     setTimeout(() => {
-      processFn(rpc, eventRpcSources[rpc]);
-
-      setInterval(processFn, interval, rpc, eventRpcSources[rpc]);
-    }, initialDelay);
+      processFn(+chainNodeId, evmSources[chainNodeId]);
+    }, delay);
   });
 }
 
@@ -111,18 +170,30 @@ export function scheduleBlockFetching(
  * Starts an infinite loop that periodically fetches and parses blocks from
  * relevant EVM blockchains. Events parsed from these blocks are emitted
  * as chain-event notifications. The interval between each fetch is specified in milliseconds and should be
- * no less than 500k ms (500 seconds) so that we support any EVM chain that has an average block time of
+ * no more than 500k ms (500 seconds) so that we support any EVM chain that has an average block time of
  * 1 second or more (block fetching is limited to 500 blocks per interval). The recommended interval
- * is 60_000 ms (60 seconds).
+ * is 120_000 ms (120 seconds) to avoid issues with public EVM nodes rate limiting requests.
  */
 export async function startEvmPolling(interval: number) {
-  if (interval < 500_000) {
+  log.info(`Starting EVM poller`);
+  if (interval > 500_000) {
     throw new Error(
       `Interval for EVM polling must be at least 500_000 ms (500 seconds)`
     );
   }
 
-  const eventSources = await getEventSources();
+  log.info(
+    `All chains will be polled for events every ${interval / 1000} seconds`
+  );
+  scheduleBlockFetching(interval, processChainNode);
+  setInterval(scheduleBlockFetching, interval, interval, processChainNode);
+}
 
-  scheduleBlockFetching(eventSources, interval, processChain);
+if (require.main === module) {
+  startEvmPolling(120_000).catch((e) => {
+    console.error(e);
+    log.error('Evm poller shutting down due to a critical error:', e);
+    rollbar.critical('Evm poller shutting down due to a critical error:', e);
+    process.exit(1);
+  });
 }
