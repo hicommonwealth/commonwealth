6c3020fee0df76ecbd746c20597b98c187813203
Raymond Z <raykyri@gmail.com>
2023-09-18 19:23:28 -0400

    Revert "Reverts cosmosProxy changes. To be reverted later once tests are fixed"
    This reverts commit bb1e8fdf2a2cff4787f5d794ebf1f45d27fc34a1.

--- packages/commonwealth/server/util/cosmosProxy.ts
+++ packages/commonwealth/server/util/cosmosProxy.ts
@@ -12,6 +12,7 @@ import {
 } from './cosmosCache';
 import { lookupKeyDurationInReq } from 'common-common/src/cacheKeyUtils';
 import { cacheDecorator } from 'common-common/src/cacheDecorator';
+import { bech32 } from 'bech32';
 
 const log = factory.getLogger(formatFilename(__filename));
 const defaultCacheDuration = 60 * 10; // 10 minutes
@@ -57,7 +58,7 @@ function setupCosmosProxy(app: Express, models: DB) {
     }
   );
 
-  // for gov v1 queries.
+  // for gov v1 queries
   app.use(
     '/cosmosLCD/:chain',
     bodyParser.text(),
@@ -97,6 +98,130 @@ function setupCosmosProxy(app: Express, models: DB) {
             2
           )}`
         );
+        return res.send(response.data);
+      } catch (err) {
+        res.status(500).json({ message: err.message });
+      }
+    }
+  );
+
+  // two cosmos-api proxies for the magic link iframe
+  // - node_info for fetching chain status (used by magic iframe, and magic login flow)
+  // - auth/accounts/:address for fetching address status (use by magic iframe)
+  app.use(
+    '/magicCosmosAPI/:chain/node_info',
+    bodyParser.text(),
+    cacheDecorator.cacheMiddleware(
+      defaultCacheDuration,
+      lookupKeyDurationInReq
+    ),
+    async function cosmosProxy(req, res) {
+      log.trace(`Got request: ${JSON.stringify(req.body, null, 2)}`);
+      try {
+        log.trace(`Querying cosmos endpoint for chain: ${req.params.chain}`);
+        const chain = await models.Chain.findOne({
+          where: { id: req.params.chain },
+          include: models.ChainNode,
+        });
+        if (!chain) {
+          throw new AppError('Invalid chain');
+        }
+        let targetUrl = chain.ChainNode?.alt_wallet_url;
+        if (!targetUrl) {
+          const fallback = await models.Chain.findOne({
+            where: { id: 'osmosis' },
+            include: models.ChainNode,
+          });
+          targetUrl = fallback.ChainNode.alt_wallet_url;
+        }
+        log.trace(`Found cosmos endpoint: ${targetUrl}`);
+
+        const response = await axios.get(targetUrl + '/node_info', {
+          headers: {
+            origin: 'https://commonwealth.im',
+          },
+        });
+        log.trace(
+          `Got response from endpoint: ${JSON.stringify(
+            response.data,
+            null,
+            2
+          )}`
+        );
+
+        // magicCosmosAPI is CORS-approved for the magic iframe
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        return res.send(response.data);
+      } catch (err) {
+        res.status(500).json({ message: err.message });
+      }
+    }
+  );
+  app.get(
+    '/magicCosmosAPI/:chain/auth/accounts/:address',
+    bodyParser.text(),
+    cacheDecorator.cacheMiddleware(
+      defaultCacheDuration,
+      lookupKeyDurationInReq
+    ),
+    async function cosmosProxy(req, res) {
+      log.trace(`Got request: ${JSON.stringify(req.body, null, 2)}`);
+      try {
+        log.trace(`Querying cosmos endpoint for chain: ${req.params.chain}`);
+        const chain = await models.Chain.findOne({
+          where: { id: req.params.chain },
+          include: models.ChainNode,
+        });
+        if (!chain) {
+          throw new AppError('Invalid chain');
+        }
+        let targetUrl = chain.ChainNode?.alt_wallet_url;
+        if (!targetUrl) {
+          const fallback = await models.Chain.findOne({
+            where: { id: 'osmosis' },
+            include: models.ChainNode,
+          });
+          targetUrl = fallback.ChainNode.alt_wallet_url;
+        }
+        log.trace(`Found cosmos endpoint: ${targetUrl}`);
+        // special case: rewrite cosmos- prefix to chain specific prefix
+        // either the requested chain or osmo, if we're using the fallback
+        const { words } = bech32.decode(req.params.address);
+        const rewrittenAddress = bech32.encode(
+          chain.ChainNode?.alt_wallet_url ? chain.bech32_prefix : 'osmo',
+          words
+        );
+        const rewrite = req.originalUrl
+          .replace(req.baseUrl, targetUrl)
+          .replace(req.params.address, rewrittenAddress)
+          .replace(`/magicCosmosAPI/${req.params.chain}`, '');
+
+        const response = await axios.get(rewrite, {
+          headers: {
+            origin: 'https://commonwealth.im',
+          },
+        });
+        log.trace(
+          `Got response from endpoint: ${JSON.stringify(
+            response.data,
+            null,
+            2
+          )}`
+        );
+        // magic expects a cosmos-sdk/Account, because their signer is still on launchpad:
+        // https://github.com/cosmos/cosmjs/issues/702
+        if (response?.data?.result?.type === 'cosmos-sdk/BaseAccount') {
+          response.data.result.type = 'cosmos-sdk/Account';
+          response.data.result.value = {
+            address: rewrittenAddress,
+            public_key: { type: 'tendermint/PubKeySecp256k1', value: '' },
+            account_number: '0',
+            sequence: '0',
+          };
+        }
+
+        // magicCosmosAPI is CORS-approved for the magic iframe
+        res.setHeader('Access-Control-Allow-Origin', '*');
         return res.send(response.data);
       } catch (err) {
         res.status(500).json({ message: err.message });
