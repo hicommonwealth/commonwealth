bdd9beb304e015ac59d30f2dceffcb16daaca640
Timothee Legros <timothee@ualberta.ca>
2023-09-20 22:02:06 +0200

    emitChainEventNotifs test stubs

---
+++ packages/commonwealth/test/integration/evmChainEvents/emitChainEventNotifs.spec.ts
@@ -0,0 +1,6 @@
+describe('emitChainEventNotifs', () => {
+  it('should not emit any notifications if there are no events', () => {});
+  it('should not throw if emitting a notification fails', () => {});
+  it('should fail to create notifications for unsupported events without throwing', () => {});
+  it('should emit a notification for each given proposal event', () => {});
+});
