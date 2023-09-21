13ac06a00dd8dd0c37b1231c7a0275e5e2ca11d6
Malik Zulqurnain <malikzulqurnain2121@gmail.com>
2023-09-21 09:27:11 -0700

    Added form validation to select list

--- packages/commonwealth/client/scripts/views/components/component_kit/new_designs/CWSelectList/CWSelectList.tsx
+++ packages/commonwealth/client/scripts/views/components/component_kit/new_designs/CWSelectList/CWSelectList.tsx
@@ -1,52 +1,109 @@
 import './CWSelectList.scss';
-import React from 'react';
+import React, { useEffect } from 'react';
 import type { GroupBase, Props } from 'react-select';
 import Select, { components } from 'react-select';
 import { CWIcon } from '../../cw_icons/cw_icon';
 import { getClasses } from '../../helpers';
 import { ComponentType } from '../../types';
+import { MessageRow } from '../CWTextInput/MessageRow';
+import { useFormContext } from 'react-hook-form';
+
+type CustomCWSelectListProps = {
+  label?: string;
+  hookToForm?: boolean;
+};
 
 export const CWSelectList = <
   Option,
   IsMulti extends boolean = false,
   Group extends GroupBase<Option> = GroupBase<Option>
 >(
-  props: Props<Option, IsMulti, Group>
+  props: Props<Option, IsMulti, Group> & CustomCWSelectListProps
 ) => {
+  const formContext = useFormContext();
+  const formFieldContext = props.hookToForm
+    ? formContext.register(props.name)
+    : ({} as any);
+  const formFieldErrorMessage =
+    props.hookToForm &&
+    (formContext?.formState?.errors?.[props.name]?.message as string);
+
+  useEffect(() => {
+    props.hookToForm &&
+      formContext &&
+      props.name &&
+      props.defaultValue &&
+      formContext.setValue(props.name, props.defaultValue);
+  }, [props.hookToForm, props.name, props.defaultValue, formContext]);
+
+  useEffect(() => {
+    props.hookToForm &&
+      formContext &&
+      props.name &&
+      props.value &&
+      formContext.setValue(props.name, props.value);
+  }, [props.hookToForm, props.name, props.value, formContext]);
+
   return (
-    <Select
-      {...props}
-      styles={{
-        control: (baseStyles) => ({
-          ...baseStyles,
-          // removes unnecessary styles
-          border: 0,
-          boxShadow: 'none',
-          minHeight: 'unset',
-        }),
-        menu: (baseStyles) => ({
-          ...baseStyles,
-          maxHeight: '300px',
-        }),
-      }}
-      components={{
-        DropdownIndicator: () => (
-          <CWIcon className="caret-icon" iconName={'caretDown'} />
-        ),
-        MultiValueRemove: (removeProps) => (
-          <components.MultiValueRemove {...removeProps}>
-            <CWIcon iconName="close" className="close-btn" />
-          </components.MultiValueRemove>
-        ),
-      }}
-      className={getClasses<{
-        className?: string;
-      }>(
-        {
-          className: props.className,
-        },
-        ComponentType.SelectList
+    <div>
+      {props.label && <MessageRow label={props.label} />}
+      <Select
+        {...props}
+        {...formFieldContext}
+        isDisabled={props?.isDisabled || formFieldContext?.disabled}
+        required={props?.required || formFieldContext?.required}
+        onBlur={(e) => {
+          props?.onBlur?.(e);
+          formFieldContext?.onBlur?.(e);
+        }}
+        onChange={(newValue: any, actionMeta) => {
+          props?.onChange?.(newValue, actionMeta);
+          if (props.hookToForm) {
+            formContext.setValue(props.name, newValue);
+            newValue?.length && formContext.setError(props.name, null);
+          }
+        }}
+        styles={{
+          control: (baseStyles) => ({
+            ...baseStyles,
+            // removes unnecessary styles
+            border: 0,
+            boxShadow: 'none',
+            minHeight: 'unset',
+          }),
+          menu: (baseStyles) => ({
+            ...baseStyles,
+            maxHeight: '300px',
+          }),
+        }}
+        components={{
+          DropdownIndicator: () => (
+            <CWIcon className="caret-icon" iconName={'caretDown'} />
+          ),
+          MultiValueRemove: (removeProps) => (
+            <components.MultiValueRemove {...removeProps}>
+              <CWIcon iconName="close" className="close-btn" />
+            </components.MultiValueRemove>
+          ),
+        }}
+        className={getClasses<{
+          className?: string;
+          failure?: boolean;
+        }>(
+          {
+            className: props.className,
+            failure: !!formFieldErrorMessage,
+          },
+          ComponentType.SelectList
+        )}
+      />
+      {formFieldErrorMessage && (
+        <MessageRow
+          hasFeedback={!!formFieldErrorMessage}
+          statusMessage={formFieldErrorMessage}
+          validationStatus={formFieldErrorMessage ? 'failure' : undefined}
+        />
       )}
-    />
+    </div>
   );
 };
