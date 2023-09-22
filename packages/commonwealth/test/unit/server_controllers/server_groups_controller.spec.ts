2a79fc16577d14a2438b499e6a0325d9a80b3ba6
Ryan Bennett <ryan@common.xyz>
2023-09-22 14:43:03 -0700

    fix unit test WIP

--- packages/commonwealth/test/unit/server_controllers/server_groups_controller.spec.ts
+++ packages/commonwealth/test/unit/server_controllers/server_groups_controller.spec.ts
@@ -2,10 +2,52 @@ import { expect } from 'chai';
 import { ServerGroupsController } from 'server/controllers/server_groups_controller';
 import { AddressInstance } from 'server/models/address';
 import { ChainInstance } from 'server/models/chain';
+import { GroupAttributes } from 'server/models/group';
+import { TopicAttributes } from 'server/models/topic';
 import { UserInstance } from 'server/models/user';
 
 const createMockedGroupsController = () => {
-  const db: any = {};
+  const db: any = {
+    Topic: {
+      findAll: async (): Promise<TopicAttributes[]> => {
+        return [
+          {
+            id: 1,
+            chain_id: 'ethereum',
+            token_threshold: '1000',
+            name: 'hello',
+            featured_in_sidebar: false,
+            featured_in_new_post: false,
+            group_ids: [7],
+          },
+        ];
+      },
+    },
+    Group: {
+      findAll: async (): Promise<(GroupAttributes & { toJSON: any })[]> => {
+        const obj: GroupAttributes = {
+          id: 7,
+          chain_id: 'ethereum',
+          metadata: {
+            name: 'hello',
+            description: '123',
+          },
+          requirements: [],
+        };
+        return [
+          {
+            ...obj,
+            toJSON: () => obj,
+          },
+        ];
+      },
+    },
+    Membership: {
+      findAll: async () => {
+        return [{}];
+      },
+    },
+  };
   const tokenBalanceCache: any = {};
   const banCache: any = {};
   const controller = new ServerGroupsController(
@@ -16,13 +58,25 @@ const createMockedGroupsController = () => {
   return controller;
 };
 
+const createMockParams = () => {
+  const user = {
+    getAddresses: async () => {
+      return [];
+    },
+  } as UserInstance;
+  const chain = {} as ChainInstance;
+  const address = {} as AddressInstance;
+  return { user, chain, address };
+};
+
 describe('ServerGroupsController', () => {
   describe('#refreshMembership', async () => {
     const controller = createMockedGroupsController();
+    const { user, chain, address } = createMockParams();
     const result = await controller.refreshMembership({
-      user: {} as UserInstance,
-      chain: {} as ChainInstance,
-      address: {} as AddressInstance,
+      user,
+      chain,
+      address,
       topicId: 1,
     });
     expect(result).to.have.property('topicId');
@@ -32,29 +86,35 @@ describe('ServerGroupsController', () => {
 
   describe('#getGroups', async () => {
     const controller = createMockedGroupsController();
+    const { chain } = createMockParams();
     const result = await controller.getGroups({
-      withMembers: true,
+      chain,
+      includeMembers: true,
     });
     expect(result).to.have.length(1);
     expect(result[0]).to.have.property('id');
     expect(result[0]).to.have.property('chain_id');
     expect(result[0]).to.have.property('metadata');
     expect(result[0]).to.have.property('requirements');
-    expect(result[0]).to.have.property('members');
-    expect(result[0].members).to.have.length(1);
-    expect(result[0].members[0]).to.have.property('group_id');
-    expect(result[0].members[0]).to.have.property('address_id');
-    expect(result[0].members[0]).to.have.property('allowed');
-    expect(result[0].members[0]).to.have.property('last_checked');
+    expect(result[0]).to.have.property('memberships');
+    expect(result[0].memberships).to.have.length(1);
+    expect(result[0].memberships[0]).to.have.property('group_id');
+    expect(result[0].memberships[0]).to.have.property('address_id');
+    expect(result[0].memberships[0]).to.have.property('allowed');
+    expect(result[0].memberships[0]).to.have.property('last_checked');
   });
 
   describe('#createGroup', async () => {
     const controller = createMockedGroupsController();
+    const { user, chain, address } = createMockParams();
     const result = await controller.createGroup({
-      user: {} as UserInstance,
-      chain: {} as ChainInstance,
-      address: {} as AddressInstance,
-      metadata: {},
+      user,
+      chain,
+      address,
+      metadata: {
+        name: 'blah',
+        description: 'blah',
+      },
       requirements: [],
       topics: [],
     });
@@ -67,11 +127,16 @@ describe('ServerGroupsController', () => {
 
   describe('#updateGroup', async () => {
     const controller = createMockedGroupsController();
+    const { user, chain, address } = createMockParams();
     const result = await controller.updateGroup({
-      user: {} as UserInstance,
-      chain: {} as ChainInstance,
-      address: {} as AddressInstance,
-      metadata: {},
+      user,
+      chain,
+      address,
+      groupId: 1,
+      metadata: {
+        name: 'blah',
+        description: 'blah',
+      },
       requirements: [],
     });
     expect(result).to.have.length(1);
@@ -83,10 +148,11 @@ describe('ServerGroupsController', () => {
 
   describe('#deleteGroup', async () => {
     const controller = createMockedGroupsController();
+    const { user, chain, address } = createMockParams();
     const result = await controller.deleteGroup({
-      user: {} as UserInstance,
-      chain: {} as ChainInstance,
-      address: {} as AddressInstance,
+      user,
+      chain,
+      address,
       groupId: 1,
     });
     expect(result).to.be.undefined;
