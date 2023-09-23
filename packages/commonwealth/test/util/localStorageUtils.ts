f89d0894d7a68ba35c5ce269827cf1e1ec4cf397
dillchen <dillon@commonwealth.im>
2023-09-23 11:15:58 -0400

    add comment to localStorageUtils

--- packages/commonwealth/test/util/localStorageUtils.ts
+++ packages/commonwealth/test/util/localStorageUtils.ts
@@ -1,5 +1,11 @@
 import { JSDOM } from 'jsdom';
 
+/* 
+This allows tests to use window, document, or localStorage as if they were running 
+in a real browser environment, even though they're actually running in a Node.js environment. 
+This is particularly useful for testing code that relies on these browser-specific APIs.
+such as window, document, and localStorage
+*/
 const dom = new JSDOM('<!doctype html><html><body></body></html>', {
   url: 'http://localhost/',
 });
