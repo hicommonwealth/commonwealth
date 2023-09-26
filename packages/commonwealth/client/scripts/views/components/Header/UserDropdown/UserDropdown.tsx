65996ad6cdb111dce206574f7c793a6ec688e88a
Jake Naviasky <jake@commonwealth.im>
2023-09-26 15:34:54 -0400

    Fix enforce flag and suppress comment console error.

--- packages/commonwealth/client/scripts/views/components/Header/UserDropdown/UserDropdown.tsx
+++ packages/commonwealth/client/scripts/views/components/Header/UserDropdown/UserDropdown.tsx
@@ -90,7 +90,7 @@ const UserDropdown = () => {
           />
         ),
         onClick: async () => {
-          if (app.config.enforceSessionKeys !== 'true' || signed) {
+          if (!app.config.enforceSessionKeys || signed) {
             return await setActiveAccount(account);
           }
 
