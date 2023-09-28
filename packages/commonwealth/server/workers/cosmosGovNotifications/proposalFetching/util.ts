02c361a8a05a4168a9bde5d9f732cbd624be218c
Timothee Legros <62490329+timolegros@users.noreply.github.com>
2023-09-28 17:50:32 +0200

    Cosmos Proposal Submitted Notifications (#4928)
    * cosmos notif generator init
    
    * working v1beta1 latest proposal fetching with enhanced pagination
    
    * working v1beta1 proposal submitted notifications backend
    
    * gov v1 notifications
    
    * make block number conditional
    
    * test setup + file restructuring
    
    * filterProposals fix
    
    * cosmos client mocking init + emit notif init
    
    * bug fixes + tests
    
    * update chain event notification data type + fix emitProposalNotification function
    
    * fix emitNotifications for cosmos notif
    
    * fix script
    
    * cosmos notif test updates
    
    * server-test use bulkCreate
    
    * test devnet CI
    
    * update chain name
    
    * add rollbar error reporting to cosmos notif generator
    
    * more rollbar reporting
    
    * util error logs for CI
    
    * new client failure error
    
    * v1beta1 fetching logs for debugging CI
    
    * client loading error update + CI env var fix
    
    * working Cosmos devnet tests
    
    * finished cosmos notif generator devnet tests
    
    * CI fixes
    
    * added cosmos notif to emitTestNotification
    
    * revert no client CI for devnet tests
    
    * lint fix
    
    * update dir name + docs
    
    * Package-Scripts.md docs for new script
    
    * remove .only
    
    * raise CI timeout
    
    * restore clocks to fix subsequent devnet tests
    
    * move funcs + files for clarity
    
    * random prop ids for cosmos test notifications
    
    * use subs to determine chains to listen to
    
    * remove .only

---
+++ packages/commonwealth/server/workers/cosmosGovNotifications/proposalFetching/util.ts
@@ -0,0 +1,95 @@
+import { ChainInstance } from '../../../models/chain';
+import { ProposalSDKType } from 'common-common/src/cosmos-ts/src/codegen/cosmos/gov/v1/gov';
+import { Proposal } from 'cosmjs-types/cosmos/gov/v1beta1/gov';
+import { AllCosmosProposals } from './types';
+import Rollbar from 'rollbar';
+import { COSMOS_GOV_V1_CHAIN_IDS } from '../../../config';
+import { factory, formatFilename } from 'common-common/src/logging';
+
+const log = factory.getLogger(formatFilename(__filename));
+
+export function uint8ArrayToNumberBE(bytes) {
+  if (!bytes) return 0;
+
+  let value = 0;
+  for (let i = 0; i < bytes.length; i++) {
+    value = (value << 8) + bytes[i];
+  }
+  return value;
+}
+
+export function numberToUint8ArrayBE(num, byteLength = 8) {
+  if (num < 0) {
+    throw new Error('This function only handles non-negative numbers.');
+  }
+
+  const bytes = new Uint8Array(byteLength);
+
+  for (let i = 0; i < byteLength; i++) {
+    bytes[byteLength - 1 - i] = num & 0xff;
+    num >>= 8;
+  }
+
+  return bytes;
+}
+
+export function filterV1GovChains(chains: ChainInstance[]) {
+  const v1Chains = [];
+  const v1Beta1Chains = [];
+
+  chains.forEach((c) => {
+    if (COSMOS_GOV_V1_CHAIN_IDS.includes(c.id)) {
+      v1Chains.push(c);
+    } else {
+      v1Beta1Chains.push(c);
+    }
+  });
+
+  return { v1Chains, v1Beta1Chains };
+}
+
+export function mapChainsToProposals(
+  v1Chains: ChainInstance[],
+  v1Beta1Chains: ChainInstance[],
+  v1Proposals: ProposalSDKType[][],
+  v1Beta1Proposals: Proposal[][]
+): AllCosmosProposals {
+  return {
+    v1: v1Proposals.reduce(
+      (acc, proposals, i) => ({ ...acc, [v1Chains[i].id]: proposals }),
+      {}
+    ),
+    v1Beta1: v1Beta1Proposals.reduce(
+      (acc, proposals, i) => ({ ...acc, [v1Beta1Chains[i].id]: proposals }),
+      {}
+    ),
+  };
+}
+
+export function processProposalSettledPromises(
+  v1ProposalResults: PromiseSettledResult<ProposalSDKType[]>[],
+  v1Beta1ProposalResults: PromiseSettledResult<Proposal[]>[],
+  rollbar?: Rollbar
+) {
+  const v1Proposals: ProposalSDKType[][] = [];
+  for (const result of v1ProposalResults) {
+    if (result.status === 'rejected') {
+      log.error('Fetching v1 proposal failed.', result.reason);
+      rollbar?.error('Fetching v1 proposal failed.', result.reason);
+    } else {
+      v1Proposals.push(result.value);
+    }
+  }
+
+  const v1Beta1Proposals: Proposal[][] = [];
+  for (const result of v1Beta1ProposalResults) {
+    if (result.status === 'rejected') {
+      log.error('Fetching v1beta1 proposal failed.', result.reason);
+      rollbar?.error('Fetching v1beta1 proposal failed.', result.reason);
+    } else {
+      v1Beta1Proposals.push(result.value);
+    }
+  }
+
+  return { v1Proposals, v1Beta1Proposals };
+}
