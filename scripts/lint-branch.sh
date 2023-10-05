7b287b327db4a36f91244fd275b75e0295844c99
kassad <kurtis@commonwealth.im>
2023-10-05 07:52:25 -0700

    Get the CI to fail on eslint warnings

--- scripts/lint-branch.sh
+++ scripts/lint-branch.sh
@@ -17,5 +17,5 @@ then
     echo "There is nothing to lint"
 else
     echo $LINES
-    NODE_OPTIONS="--max-old-space-size=8192" eslint $LINES
+    NODE_OPTIONS="--max-old-space-size=8192" eslint $LINES --max-warnings=0
 fi
