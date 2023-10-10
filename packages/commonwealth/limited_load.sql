a7a1b12c5bf7f133cdb1d5e6677ad7ee31e56f0b
Graham Johnson <graham@commonwealth.im>
2023-10-10 08:47:40 -0400

    Reset limited db scripts

--- packages/commonwealth/limited_load.sql
+++ packages/commonwealth/limited_load.sql
@@ -1,3 +1,4 @@
 \COPY "Notifications" FROM 'Notifications.dat' WITH BINARY;
 \COPY "Subscriptions" FROM 'Subscriptions.dat' WITH BINARY;
 \COPY "NotificationsRead" FROM 'NotificationsRead.dat' WITH BINARY;
+
