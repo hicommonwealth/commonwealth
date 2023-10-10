cea8608785c2a816569add9fafa01e69347bc86f
mhagel <mark.hagelberg@common.xyz>
2023-10-10 15:02:47 -0600

    use declarative sql instead of looping

--- packages/commonwealth/server/migrations/20231005213247-assign_master_profile.js
+++ packages/commonwealth/server/migrations/20231005213247-assign_master_profile.js
@@ -14,84 +14,29 @@ module.exports = {
 
     await queryInterface.sequelize.transaction(async (t) => {
       try {
-        // Update the query that selects the addresses to consolidate
-        // to include the profile_id column.
-        const [hexes] = await queryInterface.sequelize.query(
+        await queryInterface.sequelize.query(
           `
-          SELECT hex, profile_id, id
-          FROM "Addresses"
-          WHERE hex IS NOT NULL
-          GROUP BY hex, profile_id, id;
+          WITH MasterProfiles AS (
+            SELECT
+              A.hex,
+              P.id AS master_profile_id,
+              P.user_id AS master_user_id
+            FROM "Addresses" A
+            LEFT JOIN "Profiles" P ON A.profile_id = P.id
+            WHERE A.hex IS NOT NULL
+            AND P.id IS NOT NULL
+          )
+          UPDATE "Addresses" A
+          SET
+            legacy_user_id = A.user_id,
+            legacy_profile_id = A.profile_id,
+            user_id = MP.master_user_id,
+            profile_id = MP.master_profile_id
+          FROM MasterProfiles MP
+          WHERE A.hex = MP.hex;
           `,
           { transaction: t }
         );
-
-        for (const hex of hexes) {
-          // get each address that shares the hex
-          const [addresses] = await queryInterface.sequelize.query(
-            `
-            SELECT id, user_id, profile_id
-            FROM "Addresses"
-            WHERE hex = '${hex.hex}';
-            `,
-            { transaction: t }
-          );
-
-          let profiles = [];
-
-          // for each address get the Profile
-          for (const address of addresses) {
-            const [profile] = await queryInterface.sequelize.query(
-              `
-              SELECT id, user_id, profile_name, bio, email
-              FROM "Profiles"
-              WHERE id = ${address.profile_id};
-              `,
-              { transaction: t }
-            );
-            profiles.push(profile[0]);
-          }
-
-          // if the profile has more populated fields than the current masterProfile
-          // set the masterProfile to the current profile
-          // The following reduce() function calculates the most populated profile in the profiles array.
-          const masterProfile = profiles.reduce(
-            (accumulator, currentValue) => {
-              if (!currentValue) return accumulator;
-              // Count the number of populated fields in the current profile.
-              const numPopulatedFields = Object.keys(currentValue).filter(
-                (key) => !!currentValue[key]
-              ).length;
-
-              // If the current profile has more populated fields than the accumulator, return the current profile.
-              if (numPopulatedFields > accumulator.numPopulatedFields) {
-                currentValue.numPopulatedFields = numPopulatedFields;
-                return currentValue;
-              }
-
-              // Otherwise, return the accumulator.
-              return accumulator;
-            },
-            { numPopulatedFields: 0 }
-          );
-
-          if (!masterProfile.id || !masterProfile.user_id) {
-            return;
-          }
-
-          await queryInterface.sequelize.query(
-            `
-            UPDATE "Addresses"
-            SET
-              legacy_user_id = "Addresses".user_id,
-              legacy_profile_id = "Addresses".profile_id,
-              user_id = ${masterProfile.user_id},
-              profile_id = ${masterProfile.id}
-            WHERE hex = '${hex.hex}';
-            `,
-            { transaction: t }
-          );
-        }
       } catch (e) {
         console.log('error', e);
         throw new Error(e);
