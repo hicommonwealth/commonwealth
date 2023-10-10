4a2e58768abe0ef7cce050bc3a20d20d3e563d75
lzach83 <lzach83@gmail.com>
2023-10-10 16:00:36 -0400

    remove clg

--- packages/commonwealth/server/util/ipfsProxy.ts
+++ packages/commonwealth/server/util/ipfsProxy.ts
@@ -30,7 +30,6 @@ function setupIpfsProxy(app: Express) {
         }
         return res.send(response.data);
       } catch (err) {
-        console.log('err!', err);
         res.status(500).json({ message: err.message });
       }
     }
