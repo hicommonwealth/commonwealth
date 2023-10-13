15dce147150e0360548200f39160f053f18fa0cb
Marcin <marcin@commonwealth.im>
2023-10-13 18:29:06 +0200

    Fix CWTabsRow

--- packages/commonwealth/client/scripts/views/components/component_kit/new_designs/CWTabs/CWTabsRow.tsx
+++ packages/commonwealth/client/scripts/views/components/component_kit/new_designs/CWTabs/CWTabsRow.tsx
@@ -1,4 +1,4 @@
-import React, { Children, isValidElement } from 'react';
+import React from 'react';
 
 import './CWTabsRow.scss';
 
@@ -6,23 +6,7 @@ interface CWTabsRowProps {
   children: React.ReactNode;
 }
 const CWTabsRow = ({ children }: CWTabsRowProps) => {
-  const childrenArray = Children.toArray(children);
-
-  // The only children of CWTabsRow can be CWTab
-  const allChildrenValid = childrenArray.every((child) => {
-    return (
-      isValidElement(child) &&
-      typeof child.type !== 'string' &&
-      child.type?.name === 'CWTab'
-    );
-  });
-
-  if (!allChildrenValid) {
-    console.error('CWTabsRow component can only contain CWTab components.');
-    return null;
-  }
-
-  return <div className="CWTabsRow">{childrenArray}</div>;
+  return <div className="CWTabsRow">{children}</div>;
 };
 
 export default CWTabsRow;
