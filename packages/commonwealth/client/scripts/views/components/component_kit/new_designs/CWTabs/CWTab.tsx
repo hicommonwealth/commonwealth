20d095a26d7e3a4b7adc1bec32ca2f887ba4f8ce
Marcin <marcin@commonwealth.im>
2023-10-17 13:45:10 +0200

    CR

--- packages/commonwealth/client/scripts/views/components/component_kit/new_designs/CWTabs/CWTab.tsx
+++ packages/commonwealth/client/scripts/views/components/component_kit/new_designs/CWTabs/CWTab.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import clsx from 'clsx';
 
 import { CWText } from 'views/components/component_kit/cw_text';
 import { CWTag } from 'views/components/component_kit/new_designs/CWTag';
 
 import './CWTab.scss';
+import { getClasses } from 'views/components/component_kit/helpers';
+import { ComponentType } from 'views/components/component_kit/types';
 
 interface CWTabProps {
   label: string;
@@ -23,9 +24,10 @@ const CWTab = ({
 }: CWTabProps) => {
   return (
     <button
-      className={clsx('CWTab', { isSelected, isDisabled })}
+      className={getClasses({ isSelected, isDisabled }, ComponentType.Tab)}
       disabled={isDisabled}
       onClick={onClick}
+      type="button"
     >
       <CWText className="label">{label}</CWText>
       {showTag && <CWTag label="New" type="new" iconName="newStar" />}
