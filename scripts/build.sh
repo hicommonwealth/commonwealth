7c1d13ab6b424185a37fb30dc8479f3ae20aa0fc
Timothee Legros <timothee@ualberta.ca>
2023-10-18 00:59:04 +0200

    remove CE from build.sh (2)

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,5 +5,5 @@ elif [ "$SL_BUILD" = true ]; then
 elif [ "$DL_BUILD" = true ]; then
   yarn --cwd packages/discord-bot build
 else
-  yarn --cwd packages/chain-events build-services && yarn --cwd packages/commonwealth build
+  yarn --cwd packages/commonwealth build
 fi
