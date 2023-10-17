eb1ebdf2d60bc92d2840b1430f18086e9f548da2
Timothee Legros <timothee@ualberta.ca>
2023-10-18 00:58:34 +0200

    remove CE from build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,4 @@
-if [ "$CE_BUILD" = true ]; then
-  yarn --cwd packages/chain-events build-services
-elif [ "$CW_BUILD" = true ]; then
+if [ "$CW_BUILD" = true ]; then
   yarn --cwd packages/commonwealth build-all
 elif [ "$SL_BUILD" = true ]; then
   yarn --cwd packages/snapshot-listener build
