80b4de36af56603c65c959043d0f439401e082f0
Malik Zulqurnain <malikzulqurnain2121@gmail.com>
2023-10-23 07:17:01 -0700

    Revert: Added members search component

---
+++ packages/commonwealth/client/scripts/views/components/members_search_bar.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import { CWIconButton } from './component_kit/cw_icon_button';
+import { getClasses } from './component_kit/helpers';
+
+import 'pages/search/search_bar.scss';
+
+type MembersSearchBarProps = {
+  searchTerm: string;
+  setSearchTerm: (v: string) => void;
+  chainName: string;
+};
+
+export const MembersSearchBar = ({
+  searchTerm,
+  setSearchTerm,
+  chainName,
+}: MembersSearchBarProps) => {
+  return (
+    <div className="SearchBar members-search-bar">
+      <div className="search-and-icon-container">
+        <div className="search-icon">
+          <CWIconButton iconName="search" />
+        </div>
+        <input
+          className={getClasses<{ isClearable: boolean }>({
+            isClearable: searchTerm?.length > 0,
+          })}
+          placeholder={`Search members in ${chainName}`}
+          value={searchTerm}
+          autoComplete="off"
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        {searchTerm?.length > 0 && (
+          <div className="clear-icon">
+            <CWIconButton iconName="close" onClick={() => setSearchTerm('')} />
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
