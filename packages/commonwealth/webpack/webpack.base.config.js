fb73129937350358df4e465ace42dcf49d449ac6
Malik Zulqurnain <malikzulqurnain2121@gmail.com>
2023-10-24 09:54:31 -0700

    Updated gating flag

--- packages/commonwealth/webpack/webpack.base.config.js
+++ packages/commonwealth/webpack/webpack.base.config.js
@@ -69,7 +69,7 @@ module.exports = {
       'process.env.ETH_RPC': JSON.stringify(process.env.ETH_RPC),
     }),
     new webpack.DefinePlugin({
-      'process.env.GATING_ENABLED': process.env.GATING_ENABLED,
+      'process.env.GATING_API_ENABLED': process.env.GATING_API_ENABLED,
     }),
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, '../client/index.html'),
