8dfc97607279386c2d5f6e4144613be77c30dd21
kassad <kurtis@commonwealth.im>
2023-11-02 05:23:50 -0700

    Fixed prettier script

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -5,16 +5,15 @@ RED='\033[31m'
 YELLOW='\033[33m'
 NC='\033[0m' 
 
-FILES=$(git diff --staged --name-only --diff-filter=d)
+FILES=$(git diff --name-only master)
 
 if [ -n "$FILES" ]
 then
     echo -e -n $YELLOW
     echo -e "📐 prettier staged before commit..."
     echo "------------------------------------"
-    echo -e -n $GRAY
-    echo "$FILES" | tr ' ' '\n' | xargs -I {} echo "- {}"
     echo -e $NC
-    echo "$FILES" | tr ' ' '\n' | xargs -I {} npx prettier --write --ignore-unknown {}
-    git add .
+    echo "$FILES" | xargs npx prettier --write --ignore-unknown
+    echo "$FILES" | xargs git add
+    exit 0
 fi
