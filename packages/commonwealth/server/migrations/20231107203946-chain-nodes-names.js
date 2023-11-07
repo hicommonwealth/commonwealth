a519f3660a480134d02d6185e503d37698e4092f
Ryan Bennett <ryan@common.xyz>
2023-11-07 13:18:37 -0800

    update by eth_chain_id

--- packages/commonwealth/server/migrations/20231107203946-chain-nodes-names.js
+++ packages/commonwealth/server/migrations/20231107203946-chain-nodes-names.js
@@ -5,12 +5,12 @@ module.exports = {
     return queryInterface.sequelize.transaction(async (t) => {
       // xDAI -> Gnosis
       await queryInterface.sequelize.query(`
-        UPDATE "ChainNodes" SET name = 'Gnosis' WHERE name = 'xDAI';
+        UPDATE "ChainNodes" SET name = 'Gnosis' WHERE eth_chain_id = 100;
       `, { transaction: t })
 
       // GoodDAO Governance -> Fuse Mainnet
       await queryInterface.sequelize.query(`
-        UPDATE "ChainNodes" SET name = 'Fuse Mainnet' WHERE name = 'GoodDAO Governance';
+        UPDATE "ChainNodes" SET name = 'Fuse Mainnet' WHERE eth_chain_id = 122;
       `, { transaction: t })
     });
   },
@@ -18,11 +18,11 @@ module.exports = {
   down: async (queryInterface, Sequelize) => {
     return queryInterface.sequelize.transaction(async (t) => {
       await queryInterface.sequelize.query(`
-        UPDATE "ChainNodes" SET name = 'xDAI' WHERE name = 'Gnosis';
+        UPDATE "ChainNodes" SET name = 'xDAI' WHERE eth_chain_id = 100;
       `, { transaction: t })
 
       await queryInterface.sequelize.query(`
-        UPDATE "ChainNodes" SET name = 'GoodDAO Governance' WHERE name = 'Fuse Mainnet';
+        UPDATE "ChainNodes" SET name = 'GoodDAO Governance' WHERE eth_chain_id = 122;
       `, { transaction: t })
     });
   }
