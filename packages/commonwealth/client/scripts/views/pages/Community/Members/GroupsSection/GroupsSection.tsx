320ba6e41fcf0a3a8d53c58375f3a228fdcebadf
Ifu Aniemeka <ianiemeka@gmail.com>
2023-11-09 17:33:36 -0600

    bug fix: prevent groups tab visit from also logging  members tab visit (#5677)

--- packages/commonwealth/client/scripts/views/pages/Community/Members/GroupsSection/GroupsSection.tsx
+++ packages/commonwealth/client/scripts/views/pages/Community/Members/GroupsSection/GroupsSection.tsx
@@ -1,4 +1,3 @@
-import { useBrowserAnalyticsTrack } from 'hooks/useBrowserAnalyticsTrack';
 import Group from 'models/Group';
 import { useCommonNavigate } from 'navigation/helpers';
 import React from 'react';
@@ -6,7 +5,6 @@ import app from 'state';
 import { CWIcon } from 'views/components/component_kit/cw_icons/cw_icon';
 import { CWText } from 'views/components/component_kit/cw_text';
 import { CWButton } from 'views/components/component_kit/new_designs/cw_button';
-import { MixpanelPageViewEvent } from '../../../../../../../shared/analytics/types';
 import { chainTypes, requirementTypes } from '../../common/constants';
 import { convertRequirementAmountFromWeiToTokens } from '../../common/helpers';
 import GroupCard from './GroupCard';
@@ -24,10 +22,6 @@ const GroupsSection = ({
   hasNoGroups,
 }: GroupSectionProps) => {
   const navigate = useCommonNavigate();
-
-  useBrowserAnalyticsTrack({
-    payload: { event: MixpanelPageViewEvent.GROUPS_PAGE_VIEW },
-  });
 
   return (
     <section className="GroupsSection">
