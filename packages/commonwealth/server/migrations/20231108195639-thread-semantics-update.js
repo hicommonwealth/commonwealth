41f3e8aa2c4b055a7b9d0e5843d0b9b386722cfe
Timothee Legros <timothee@ualberta.ca>
2023-11-09 19:01:46 +0200

    migration update

--- packages/commonwealth/server/migrations/20231108195639-thread-semantics-update.js
+++ packages/commonwealth/server/migrations/20231108195639-thread-semantics-update.js
@@ -42,6 +42,30 @@ module.exports = {
       `,
         { transaction },
       );
+      await queryInterface.sequelize.query(
+        `
+        ALTER INDEX IF EXISTS "offchain_threads_chain_updated_at" RENAME TO "threads_community_id_updated_at";
+      `,
+        { transaction },
+      );
+      await queryInterface.sequelize.query(
+        `
+        ALTER INDEX IF EXISTS "offchain_threads_chain_created_at" RENAME TO "threads_community_id_created_at";
+      `,
+        { transaction },
+      );
+      await queryInterface.sequelize.query(
+        `
+        ALTER INDEX IF EXISTS "offchain_threads_author_id" RENAME TO "threads_author_id";
+      `,
+        { transaction },
+      );
+      await queryInterface.sequelize.query(
+        `
+        ALTER INDEX IF EXISTS "offchain_threads_created_at" RENAME TO "threads_created_at";
+      `,
+        { transaction },
+      );
     });
   },
 
@@ -80,6 +104,30 @@ module.exports = {
       await queryInterface.sequelize.query(
         `
         ALTER INDEX IF EXISTS "Threads_pkey" RENAME TO "OffchainThreads_pkey";
+      `,
+        { transaction },
+      );
+      await queryInterface.sequelize.query(
+        `
+        ALTER INDEX IF EXISTS "threads_community_id_updated_at" RENAME TO "offchain_threads_chain_updated_at";
+      `,
+        { transaction },
+      );
+      await queryInterface.sequelize.query(
+        `
+        ALTER INDEX IF EXISTS "threads_community_id_created_at" RENAME TO "offchain_threads_chain_created_at";
+      `,
+        { transaction },
+      );
+      await queryInterface.sequelize.query(
+        `
+        ALTER INDEX IF EXISTS "threads_author_id" RENAME TO "offchain_threads_author_id";
+      `,
+        { transaction },
+      );
+      await queryInterface.sequelize.query(
+        `
+        ALTER INDEX IF EXISTS "threads_created_at" RENAME TO "offchain_threads_created_at";
       `,
         { transaction },
       );
