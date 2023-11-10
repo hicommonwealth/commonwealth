7cb46c0752ea4a3b00973d6bbc1bbe9e541088fe
Jake Naviasky <jake@commonwealth.im>
2023-11-10 16:12:03 -0500

    Add truncate and transaction to new migration.

--- packages/commonwealth/server/migrations/20231110202631-add-unique-membership-constraint.js
+++ packages/commonwealth/server/migrations/20231110202631-add-unique-membership-constraint.js
@@ -2,19 +2,45 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.addColumn('Memberships', 'id', {
-      type: Sequelize.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-    });
-    await queryInterface.addIndex('Memberships', {
-      fields: ['address_id', 'group_id'],
-      unique: true,
+    await queryInterface.sequelize.transaction(async (transaction) => {
+      await queryInterface.sequelize.query(
+        `
+        TRUNCATE TABLE "Memberships";
+      `,
+        {
+          raw: true,
+          type: 'RAW',
+          transaction,
+        },
+      );
+      await queryInterface.addColumn(
+        'Memberships',
+        'id',
+        {
+          type: Sequelize.INTEGER,
+          primaryKey: true,
+          autoIncrement: true,
+        },
+        {
+          transaction,
+        },
+      );
+      await queryInterface.addIndex('Memberships', {
+        fields: ['address_id', 'group_id'],
+        unique: true,
+        transaction,
+      });
     });
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.removeIndex('Memberships', ['address_id', 'group_id']);
-    await queryInterface.removeColumn('Memberships', 'id');
+    await queryInterface.sequelize.transaction(async (transaction) => {
+      await queryInterface.removeIndex(
+        'Memberships',
+        ['address_id', 'group_id'],
+        { transaction },
+      );
+      await queryInterface.removeColumn('Memberships', 'id', { transaction });
+    });
   },
 };
