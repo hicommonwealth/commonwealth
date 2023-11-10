127011b88ebfed37a304bef0c6a52f3d9f609d79
Jake Naviasky <jake@commonwealth.im>
2023-11-10 13:50:56 -0500

    Use bulk queries in refreshCommunityMemberships. (#5693)
    * Grab changes from Ryan PR 5688.
    
    * Use loop for updates but not creates.

--- packages/commonwealth/server/controllers/server_groups_methods/refresh_community_memberships.ts
+++ packages/commonwealth/server/controllers/server_groups_methods/refresh_community_memberships.ts
@@ -1,30 +1,38 @@
 import Bluebird from 'bluebird';
-import { flatten } from 'lodash';
-import { Op } from 'sequelize';
-import { CommunityInstance } from 'server/models/community';
-import { MembershipAttributes } from '../../models/membership';
-import { refreshMembershipsForAddress } from '../../util/requirementsModule/refreshMembershipsForAddress';
+import moment from 'moment';
+import { Op, Sequelize } from 'sequelize';
+import { GroupAttributes } from 'server/models/group';
+import { AddressAttributes } from '../../models/address';
+import { CommunityInstance } from '../../models/community';
+import {
+  MembershipAttributes,
+  MembershipInstance,
+} from '../../models/membership';
+import validateGroupMembership from '../../util/requirementsModule/validateGroupMembership';
 import { ServerGroupsController } from '../server_groups_controller';
+
+const MEMBERSHIP_TTL_SECONDS = 60 * 2;
 
 export type RefreshCommunityMembershipsOptions = {
   community: CommunityInstance;
+  group?: GroupAttributes;
 };
+
 export type RefreshCommunityMembershipsResult = MembershipAttributes[];
 
 export async function __refreshCommunityMemberships(
   this: ServerGroupsController,
-  { community }: RefreshCommunityMembershipsOptions,
+  { community, group }: RefreshCommunityMembershipsOptions,
 ): Promise<void> {
   const startedAt = Date.now();
 
-  // get all groups across the community
-  const groups = await this.models.Group.findAll({
-    where: {
-      community_id: community.id,
-    },
-  });
+  let groupsToUpdate: GroupAttributes[];
+  if (group) {
+    groupsToUpdate = [group];
+  } else {
+    groupsToUpdate = await this.getGroups({ community });
+  }
 
-  // refresh memberships for all addresses in community
   const addresses = await this.models.Address.findAll({
     where: {
       community_id: community.id,
@@ -33,28 +41,111 @@ export async function __refreshCommunityMemberships(
       },
     },
     attributes: ['id', 'address'],
+    include: {
+      model: this.models.Membership,
+      as: 'Memberships',
+      required: false,
+    },
   });
 
-  const result = await Bluebird.map(
-    addresses,
-    (address) => {
-      return refreshMembershipsForAddress(
-        this.models,
-        this.tokenBalanceCache,
-        address,
-        groups,
-      );
-    },
-    { concurrency: 300 },
-  );
+  const toCreate = [];
+  const toUpdate: [MembershipInstance, MembershipAttributes][] = [];
 
-  const allMemberships = flatten(result);
+  const processMembership = async (
+    address: AddressAttributes,
+    currentGroup: GroupAttributes,
+  ) => {
+    const existingMembership = address.Memberships.find(
+      ({ group_id }) => group_id === currentGroup.id,
+    );
+    if (existingMembership) {
+      // membership exists
+      const expiresAt = moment(existingMembership.last_checked).add(
+        MEMBERSHIP_TTL_SECONDS,
+        'seconds',
+      );
+      if (moment().isBefore(expiresAt)) {
+        // membership is fresh, do nothing
+        return;
+      }
+      // membership stale, update
+      const computedMembership = await refreshAndQueueOperation(
+        address,
+        currentGroup,
+      );
+      toUpdate.push([
+        existingMembership as MembershipInstance,
+        computedMembership,
+      ]);
+      return;
+    }
+
+    // membership does not exist, create
+    const computedMembership = await refreshAndQueueOperation(
+      address,
+      currentGroup,
+    );
+    toCreate.push(computedMembership);
+  };
+
+  const refreshAndQueueOperation = async (
+    address: AddressAttributes,
+    currentGroup: GroupAttributes,
+  ) => {
+    const { requirements } = currentGroup;
+    const { isValid, messages } = await validateGroupMembership(
+      address.address,
+      requirements,
+      this.tokenBalanceCache,
+    );
+    const computedMembership = {
+      group_id: currentGroup.id,
+      address_id: address.id,
+      reject_reason: isValid ? null : JSON.stringify(messages),
+      last_checked: Sequelize.literal('CURRENT_TIMESTAMP') as any,
+    };
+    return computedMembership;
+  };
 
   console.log(
-    `refreshed ${allMemberships.length} memberships for ${
-      groups.length
-    } groups and ${addresses.length} addresses in ${community.id} within ${
-      (Date.now() - startedAt) / 1000
-    }s`,
+    `Checking ${addresses.length} addresses in ${groupsToUpdate.length} groups in ${community.id}...`,
+  );
+
+  await Bluebird.map(
+    groupsToUpdate,
+    async (currentGroup) => {
+      return Bluebird.map(
+        addresses,
+        async (address) => {
+          return processMembership(address, currentGroup);
+        },
+        {
+          concurrency: 20,
+        },
+      );
+    },
+    { concurrency: 20 },
+  );
+
+  console.log(`Done checking. Starting ${toCreate.length} creates...`);
+
+  // first create new rows
+  await this.models.Membership.bulkCreate(toCreate);
+
+  console.log(`Done creating. Starting ${toUpdate.length} updates...`);
+  await Bluebird.map(
+    toUpdate,
+    async ([existingMembership, { reject_reason, last_checked }]) => {
+      return existingMembership.update({ reject_reason, last_checked });
+    },
+    {
+      concurrency: 5,
+    },
+  );
+
+  console.log(
+    `Created ${toCreate.length} and updated ${toUpdate.length} memberships in ${
+      community.id
+    } within ${(Date.now() - startedAt) / 1000}s`,
   );
 }
