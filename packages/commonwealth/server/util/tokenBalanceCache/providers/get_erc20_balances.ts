dd398c809edcbcc3f8d33cdb83d4518f9659d5d3
Timothee Legros <timothee@ualberta.ca>
2023-11-27 15:43:16 +0200

    erc20 generalized implementation for eth native

--- packages/commonwealth/server/util/tokenBalanceCache/providers/get_erc20_balances.ts
+++ packages/commonwealth/server/util/tokenBalanceCache/providers/get_erc20_balances.ts
@@ -1,5 +1,11 @@
+import Web3 from 'web3';
 import { ChainNodeInstance } from '../../../models/chain_node';
 import { Balances } from '../types';
+import {
+  evmBalanceFetcherBatching,
+  evmOffChainRpcBatching,
+  mapNodeToBalanceFetcherContract,
+} from '../util';
 
 export type GetErc20BalancesOptions = {
   chainNode: ChainNodeInstance;
@@ -10,7 +16,37 @@ export type GetErc20BalancesOptions = {
 export async function __getErc20Balances(
   options: GetErc20BalancesOptions,
 ): Promise<Balances> {
-  return {};
+  if (options.addresses.length === 0) {
+    return {};
+  }
+
+  const rpcEndpoint = options.chainNode.private_url || options.chainNode.url;
+  if (options.addresses.length === 1) {
+    return await getErc20Balance(
+      rpcEndpoint,
+      options.contractAddress,
+      options.addresses[0],
+    );
+  }
+
+  const balanceFetcherContract = mapNodeToBalanceFetcherContract(
+    options.chainNode.eth_chain_id,
+  );
+  if (balanceFetcherContract) {
+    return await getOnChainBatchErc20Balances(
+      options.chainNode.eth_chain_id,
+      rpcEndpoint,
+      options.contractAddress,
+      options.addresses,
+    );
+  } else {
+    return await getOffChainBatchErc20Balances(
+      options.chainNode.eth_chain_id,
+      rpcEndpoint,
+      options.contractAddress,
+      options.addresses,
+    );
+  }
 }
 
 async function getOnChainBatchErc20Balances(
@@ -19,15 +55,50 @@ async function getOnChainBatchErc20Balances(
   contractAddress: string,
   addresses: string[],
 ): Promise<Balances> {
-  return {};
+  // ignore failedAddresses returned property for now -> revisit if we want to implement retry strategy
+  const { balances } = await evmBalanceFetcherBatching(
+    {
+      evmChainId: ethChainId,
+      url: rpcEndpoint,
+      contractAddress: contractAddress,
+    },
+    {
+      batchSize: 1000,
+    },
+    addresses,
+  );
+  return balances;
 }
 
 async function getOffChainBatchErc20Balances(
+  evmChainId: number,
   rpcEndpoint: string,
   contractAddress: string,
   addresses: string[],
 ): Promise<Balances> {
-  return {};
+  // ignore failedAddresses returned property for now -> revisit if we want to implement retry strategy
+  const { balances } = await evmOffChainRpcBatching(
+    {
+      evmChainId,
+      url: rpcEndpoint,
+      contractAddress,
+    },
+    {
+      method: 'eth_call',
+      getParams: (web3, address, contractAddress) => {
+        const calldata =
+          '0x70a08231' +
+          web3.eth.abi.encodeParameters(['address'], [address]).substring(2);
+        return {
+          to: contractAddress,
+          data: calldata,
+        };
+      },
+      batchSize: 1000,
+    },
+    addresses,
+  );
+  return balances;
 }
 
 async function getErc20Balance(
@@ -35,5 +106,34 @@ async function getErc20Balance(
   contractAddress: string,
   address: string,
 ): Promise<Balances> {
-  return {};
+  const web3 = new Web3();
+  const calldata =
+    '0x70a08231' +
+    web3.eth.abi.encodeParameters(['address'], [address]).substring(2);
+  const requestBody = {
+    method: 'eth_call',
+    params: [
+      {
+        to: contractAddress,
+        data: calldata,
+      },
+      'latest',
+    ],
+    id: 1,
+    jsonrpc: '2.0',
+  };
+
+  const response = await fetch(rpcEndpoint, {
+    method: 'POST',
+    body: JSON.stringify(requestBody),
+    headers: { 'Content-Type': 'application/json' },
+  });
+  const data = await response.json();
+
+  return {
+    [address]: web3.eth.abi.decodeParameter(
+      'uint256',
+      data.result,
+    ) as unknown as string,
+  };
 }
