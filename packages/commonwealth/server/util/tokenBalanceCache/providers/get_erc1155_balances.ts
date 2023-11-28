9aac0747ed6d467e856e23a003536e75ca0535f6
Timothee Legros <62490329+timolegros@users.noreply.github.com>
2023-11-28 17:42:20 +0200

    TBC Stubs + Types (#5751)
    * init stubs + types
    
    * remove unit test .only
    
    * eslint ignore stub
    
    * only export necessary types
    
    * fix method stubs
    
    * move tbc to standalone class

---
+++ packages/commonwealth/server/util/tokenBalanceCache/providers/get_erc1155_balances.ts
@@ -0,0 +1,22 @@
+import { ChainNodeInstance } from '../../../models/chain_node';
+import { Balances } from '../types';
+
+export type GetErc1155BalancesOptions = {
+  chainNode: ChainNodeInstance;
+  addresses: string[];
+  contractAddress: string;
+  tokenId: string;
+};
+
+export async function __getErc1155Balances(
+  options: GetErc1155BalancesOptions,
+) {}
+
+async function getOffChainBatchErc1155Balances(
+  rpcEndpoint: string,
+  contractAddress: string,
+  addresses: string[],
+  tokenId: string,
+): Promise<Balances> {
+  return {};
+}
