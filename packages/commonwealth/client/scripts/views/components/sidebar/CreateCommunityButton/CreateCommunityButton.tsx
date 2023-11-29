e676f949a9b50d9fc6872763e7fedab6fea9a97d
Marcin <marcin@commonwealth.im>
2023-11-29 11:29:23 +0100

    Create feature flag

---
+++ packages/commonwealth/client/scripts/views/components/sidebar/CreateCommunityButton/CreateCommunityButton.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import { useCommonNavigate } from 'navigation/helpers';
+import { CWButton } from 'views/components/component_kit/new_designs/cw_button';
+import './CreateCommunityButton.scss';
+
+const CreateCommunityButton = () => {
+  const navigate = useCommonNavigate();
+
+  const handleCreateCommunity = () => {
+    navigate('/createCommunity/starter', {}, null);
+  };
+
+  return (
+    <div className="CreateCommunityButton">
+      <CWButton
+        label="Create Community"
+        buttonHeight="sm"
+        buttonWidth="full"
+        onClick={handleCreateCommunity}
+      />
+    </div>
+  );
+};
+
+export default CreateCommunityButton;
