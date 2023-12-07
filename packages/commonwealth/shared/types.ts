2c9a36120bf8f8fc47a3df1e9003dbbd4e660511
Roger Torres <41707007+Rotorsoft@users.noreply.github.com>
2023-12-07 17:21:53 -0500

    Refactor reverse references to types in project structure (#5832)
    * refactor types
    
    * fix import
    
    * use relative path to fix discord bot ts check - missing alias
    
    ---------
    
    Co-authored-by: rotorsoft <rotorsoft@outlook.com>

--- packages/commonwealth/shared/types.ts
+++ packages/commonwealth/shared/types.ts
@@ -1,11 +1,22 @@
-import type { SnapshotProposalAttributes } from '../server/models/snapshot_proposal';
-import type { AccessLevel } from './permissions';
 import {
   ChainEventAttributes,
   NotificationCategories,
   NotificationCategory,
 } from 'common-common/src/types';
 import type { SupportedNetwork } from '../shared/chain/types/types';
+import type { AccessLevel } from './permissions';
+
+export type SnapshotProposalAttributes = {
+  id: string;
+  title?: string;
+  body?: string;
+  choices?: string[];
+  space: string;
+  event: string;
+  start?: string;
+  expire: string;
+  is_upstream_deleted?: boolean;
+};
 
 export enum WebsocketMessageNames {
   ChainEventNotification = 'chain-event-notification',
