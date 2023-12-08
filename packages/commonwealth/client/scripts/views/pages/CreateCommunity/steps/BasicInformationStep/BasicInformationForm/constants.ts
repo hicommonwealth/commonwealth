bbad7742d463860411f522a4a6c7f7b4f37f4964
Malik Zulqurnain <malikzulqurnain2121@gmail.com>
2023-12-08 13:01:21 +0500

    Fix app var

--- packages/commonwealth/client/scripts/views/pages/CreateCommunity/steps/BasicInformationStep/BasicInformationForm/constants.ts
+++ packages/commonwealth/client/scripts/views/pages/CreateCommunity/steps/BasicInformationStep/BasicInformationForm/constants.ts
@@ -1,10 +1,10 @@
-import App from 'state';
+import app from 'state';
 
 export const POLOGON_CHAIN_OPTION = {
   label: 'Polygon',
   value: 'polygon',
 };
 
-export const existingCommunityNames = App.config.chains
+export const existingCommunityNames = app.config.chains
   .getAll()
   .map((community) => community.name.toLowerCase().trim());
