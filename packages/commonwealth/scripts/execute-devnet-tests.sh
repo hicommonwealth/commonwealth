02253c15b1f3792fe078e6b262f925fbfad83941
Timothee Legros <timothee@ualberta.ca>
2023-12-08 21:04:49 +0200

    updated script + fixed CI.yml

--- packages/commonwealth/scripts/execute-devnet-tests.sh
+++ packages/commonwealth/scripts/execute-devnet-tests.sh
@@ -1,18 +1,31 @@
 #!/bin/bash
 
-# Check if an argument is provided
-if [ "$#" -eq 1 ]; then
-    # Check if the argument is 'cosmos'
-    if [ "$1" == "cosmos" ]; then
-        ts-mocha './test/devnet/cosmos/**/*.spec.ts'
-    # Check if the argument is 'evm'
-    elif [ "$1" == "evm" ]; then
-        ts-mocha './test/devnet/evm/**/*.spec.ts'
-    else
-        echo "Invalid argument. Please use 'cosmos' or 'evm'."
-        exit 1
-    fi
-else
-    # Execute the default command if no argument is provided
-    ts-mocha './test/devnet/**/*.spec.ts'
-fi
+# Function to execute ts-mocha with the provided path and additional arguments
+execute_ts_mocha() {
+    path=$1
+    shift  # Shift to skip the first argument (path) and get additional arguments
+
+    # Construct the full command
+    full_command="ts-mocha \"$path\" $*"
+
+    # Log the full command
+    echo "Executing: $full_command"
+
+    # Execute the command
+    eval $full_command
+}
+
+# Check if the first argument is 'cosmos', 'evm', or neither
+case $1 in
+    cosmos)
+        execute_ts_mocha './test/devnet/cosmos/**/*.spec.ts' "${@:2}"
+        ;;
+    evm)
+        execute_ts_mocha './test/devnet/evm/**/*.spec.ts' "${@:2}"
+        ;;
+    *)
+        # If the first argument is not 'cosmos' or 'evm', use the default path
+        # and treat all arguments as additional parameters for ts-mocha
+        execute_ts_mocha './test/devnet/**/*.spec.ts' "$@"
+        ;;
+esac
