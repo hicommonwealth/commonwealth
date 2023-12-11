8198967e653d30efac573274aa382baa87e46a43
Malik Zulqurnain <malikzulqurnain2121@gmail.com>
2023-12-11 18:31:40 +0500

    Added community is live screen

--- packages/commonwealth/client/scripts/views/pages/CreateCommunity/steps/SuccessStep/SuccessStep.tsx
+++ packages/commonwealth/client/scripts/views/pages/CreateCommunity/steps/SuccessStep/SuccessStep.tsx
@@ -1,9 +1,26 @@
 import React from 'react';
 
+import { CWText } from 'views/components/component_kit/cw_text';
+import { CWButton } from 'views/components/component_kit/new_designs/cw_button';
 import './SuccessStep.scss';
 
 const SuccessStep = () => {
-  return <div className="SuccessStep">SuccessStep</div>;
+  return (
+    <div className="SuccessStep">
+      <img src="../../static/img/communityIslive.png" alt="" className="img" />
+      <CWText type="h2">Your community is live!</CWText>
+      <CWText type="b1">
+        Your community is now live and discoverable on Common. You can now use
+        the admin panel to change settings and create topics for discussion.
+      </CWText>
+      <CWButton
+        buttonWidth="wide"
+        label="Go to community"
+        containerClassName="cta-btn"
+        className="w-full"
+      />
+    </div>
+  );
 };
 
 export default SuccessStep;
