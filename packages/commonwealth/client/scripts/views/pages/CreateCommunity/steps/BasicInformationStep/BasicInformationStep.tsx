57aeb61df6b413f0b3fe7e76bcb13515fc80472a
Marcin Maslanka <maslankam92@gmail.com>
2023-12-11 16:12:23 +0100

    Create Community - Community Selection Step  (#5846)
    * Community Selection Step with modals and signup flow
    
    * Remove unnecessary components
    
    * Cleanup
    
    * CR
    
    * Add styling
    
    * CR
    
    ---------
    
    Co-authored-by: Marcin <marcin@commonwealth.im>

--- packages/commonwealth/client/scripts/views/pages/CreateCommunity/steps/BasicInformationStep/BasicInformationStep.tsx
+++ packages/commonwealth/client/scripts/views/pages/CreateCommunity/steps/BasicInformationStep/BasicInformationStep.tsx
@@ -1,9 +1,26 @@
 import React from 'react';
 
+import AddressInfo from 'models/AddressInfo';
+import { SelectedCommunity } from 'views/components/component_kit/new_designs/CWCommunitySelector';
 import './BasicInformationStep.scss';
 
-const BasicInformationStep = () => {
-  return <div className="BasicInformationStep">BasicInformationStep</div>;
+interface BasicInformationStepProps {
+  selectedAddress: AddressInfo;
+  selectedCommunity: SelectedCommunity;
+}
+
+const BasicInformationStep = ({
+  selectedAddress,
+  selectedCommunity,
+}: BasicInformationStepProps) => {
+  return (
+    <div className="BasicInformationStep">
+      BasicInformationStep
+      <div>selected address: {selectedAddress?.address}</div>
+      <div>selected community type: {selectedCommunity.type}</div>
+      <div>selected community base: {selectedCommunity.chainBase}</div>
+    </div>
+  );
 };
 
 export default BasicInformationStep;
