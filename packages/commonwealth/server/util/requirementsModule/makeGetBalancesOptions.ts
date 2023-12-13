d274d96bbfe69bd862e2c668e1d0e0718669dfde
Timothee Legros <timothee@ualberta.ca>
2023-12-13 03:24:22 +0200

    iterate through addresses exactly once

--- packages/commonwealth/server/util/requirementsModule/makeGetBalancesOptions.ts
+++ packages/commonwealth/server/util/requirementsModule/makeGetBalancesOptions.ts
@@ -21,95 +21,82 @@ export function makeGetBalancesOptions(
   const allOptions: Exclude<GetBalancesOptions, GetErc1155BalanceOptions>[] =
     [];
 
-  for (const address of addresses) {
-    for (const group of groups) {
-      for (const requirement of group.requirements) {
-        if (requirement.rule === 'threshold') {
-          // for each requirement, upsert the appropriate option
-          switch (requirement.data.source.source_type) {
-            // ContractSource
-            case BalanceSourceType.ERC20:
-            case BalanceSourceType.ERC721: {
-              const castedSource = requirement.data.source as ContractSource;
-              const existingOptions = allOptions.find((opt) => {
-                const castedOpt = opt as GetErcBalanceOptions;
-                return (
-                  castedOpt.balanceSourceType === castedSource.source_type &&
-                  castedOpt.sourceOptions.evmChainId ===
-                    castedSource.evm_chain_id &&
-                  castedOpt.sourceOptions.contractAddress ===
-                    castedSource.contract_address
-                );
+  const addressStrings = addresses.map((a) => a.address);
+
+  for (const group of groups) {
+    for (const requirement of group.requirements) {
+      if (requirement.rule === 'threshold') {
+        // for each requirement, upsert the appropriate option
+        switch (requirement.data.source.source_type) {
+          // ContractSource
+          case BalanceSourceType.ERC20:
+          case BalanceSourceType.ERC721: {
+            const castedSource = requirement.data.source as ContractSource;
+            const existingOptions = allOptions.find((opt) => {
+              const castedOpt = opt as GetErcBalanceOptions;
+              return (
+                castedOpt.balanceSourceType === castedSource.source_type &&
+                castedOpt.sourceOptions.evmChainId ===
+                  castedSource.evm_chain_id &&
+                castedOpt.sourceOptions.contractAddress ===
+                  castedSource.contract_address
+              );
+            });
+            if (!existingOptions) {
+              allOptions.push({
+                balanceSourceType: castedSource.source_type as any,
+                sourceOptions: {
+                  contractAddress: castedSource.contract_address,
+                  evmChainId: castedSource.evm_chain_id,
+                },
+                addresses: addressStrings,
               });
-              if (existingOptions) {
-                if (!existingOptions.addresses.includes(address.address)) {
-                  existingOptions.addresses.push(address.address);
-                }
-              } else {
-                allOptions.push({
-                  balanceSourceType: castedSource.source_type as any,
-                  sourceOptions: {
-                    contractAddress: castedSource.contract_address,
-                    evmChainId: castedSource.evm_chain_id,
-                  },
-                  addresses: [address.address],
-                });
-              }
-              break;
             }
-            // NativeSource
-            case BalanceSourceType.ETHNative: {
-              const castedSource = requirement.data.source as NativeSource;
-              const existingOptions = allOptions.find((opt) => {
-                const castedOpt = opt as GetEthNativeBalanceOptions;
-                return (
-                  castedOpt.balanceSourceType === BalanceSourceType.ETHNative &&
-                  castedOpt.sourceOptions.evmChainId ===
-                    castedSource.evm_chain_id
-                );
+            break;
+          }
+          // NativeSource
+          case BalanceSourceType.ETHNative: {
+            const castedSource = requirement.data.source as NativeSource;
+            const existingOptions = allOptions.find((opt) => {
+              const castedOpt = opt as GetEthNativeBalanceOptions;
+              return (
+                castedOpt.balanceSourceType === BalanceSourceType.ETHNative &&
+                castedOpt.sourceOptions.evmChainId === castedSource.evm_chain_id
+              );
+            });
+            if (!existingOptions) {
+              allOptions.push({
+                balanceSourceType: BalanceSourceType.ETHNative,
+                sourceOptions: {
+                  evmChainId: castedSource.evm_chain_id,
+                },
+                addresses: addressStrings,
               });
-              if (existingOptions) {
-                if (!existingOptions.addresses.includes(address.address)) {
-                  existingOptions.addresses.push(address.address);
-                }
-              } else {
-                allOptions.push({
-                  balanceSourceType: BalanceSourceType.ETHNative,
-                  sourceOptions: {
-                    evmChainId: castedSource.evm_chain_id,
-                  },
-                  addresses: [address.address],
-                });
-              }
-              break;
             }
-            // CosmosSource
-            case BalanceSourceType.CosmosNative: {
-              const castedSource = requirement.data.source as CosmosSource;
-              const existingOptions = allOptions.find((opt) => {
-                const castedOpt = opt as GetCosmosBalancesOptions;
-                return (
-                  castedOpt.balanceSourceType ===
-                    BalanceSourceType.CosmosNative &&
-                  castedOpt.sourceOptions.cosmosChainId ===
-                    castedSource.cosmos_chain_id
-                );
+            break;
+          }
+          // CosmosSource
+          case BalanceSourceType.CosmosNative: {
+            const castedSource = requirement.data.source as CosmosSource;
+            const existingOptions = allOptions.find((opt) => {
+              const castedOpt = opt as GetCosmosBalancesOptions;
+              return (
+                castedOpt.balanceSourceType ===
+                  BalanceSourceType.CosmosNative &&
+                castedOpt.sourceOptions.cosmosChainId ===
+                  castedSource.cosmos_chain_id
+              );
+            });
+            if (!existingOptions) {
+              allOptions.push({
+                balanceSourceType: BalanceSourceType.CosmosNative,
+                sourceOptions: {
+                  cosmosChainId: castedSource.cosmos_chain_id,
+                },
+                addresses: addressStrings,
               });
-              if (existingOptions) {
-                if (!existingOptions.addresses.includes(address.address)) {
-                  existingOptions.addresses.push(address.address);
-                }
-              } else {
-                allOptions.push({
-                  balanceSourceType: BalanceSourceType.CosmosNative,
-                  sourceOptions: {
-                    cosmosChainId: castedSource.cosmos_chain_id,
-                  },
-                  addresses: [address.address],
-                });
-              }
-              break;
             }
+            break;
           }
         }
       }
