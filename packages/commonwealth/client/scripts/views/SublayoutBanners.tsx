5e8fe3ca340c01259ecf02444d1360810265ad6d
Timothee Legros <timothee@ualberta.ca>
2023-12-18 14:06:05 +0200

    removed banner

--- packages/commonwealth/client/scripts/views/SublayoutBanners.tsx
+++ packages/commonwealth/client/scripts/views/SublayoutBanners.tsx
@@ -1,13 +1,9 @@
 import React, { useState } from 'react';
 
-import { ChainBase, ChainNetwork } from 'common-common/src/types';
 import { isNonEmptyString } from 'helpers/typeGuards';
 import app from 'state';
 import ChainInfo from '../models/ChainInfo';
-import {
-  CWMessageBanner,
-  Old_CWBanner,
-} from './components/component_kit/cw_banner';
+import { CWMessageBanner } from './components/component_kit/cw_banner';
 import { TermsBanner } from './components/terms_banner';
 
 type SublayoutBannersProps = {
@@ -37,21 +33,6 @@ export const SublayoutBanners = ({
       {banner && bannerStatus !== 'off' && (
         <CWMessageBanner bannerContent={banner} onClose={handleDismissBanner} />
       )}
-      {app.isLoggedIn() &&
-        ([
-          ChainNetwork.ERC721,
-          ChainNetwork.ERC20,
-          ChainNetwork.AxieInfinity,
-          ChainNetwork.Ethereum,
-        ].includes(app.chain.meta.network) ||
-          [ChainBase.CosmosSDK, ChainBase.Solana].includes(
-            app.chain.meta.base,
-          )) &&
-        !app.user.activeAccount && (
-          <Old_CWBanner
-            bannerContent={`Link an address that holds ${chain.default_symbol} to participate in governance.`}
-          />
-        )}
       {isNonEmptyString(terms) && <TermsBanner terms={terms} />}
     </>
   );
