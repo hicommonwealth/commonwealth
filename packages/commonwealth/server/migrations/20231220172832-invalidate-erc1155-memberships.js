33d5eade3d329e19fcb7c880bbd7825aeb9b2f52
Timothee Legros <timothee@ualberta.ca>
2023-12-20 18:50:10 +0100

    invalidate ERC1155 memberships script

---
+++ packages/commonwealth/server/migrations/20231220172832-invalidate-erc1155-memberships.js
@@ -0,0 +1,33 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.sequelize.transaction(async (transaction) => {
+      const result = await queryInterface.sequelize.query(
+        `
+        SELECT id
+        FROM "Groups" G,
+             jsonb_array_elements(requirements::JSONB) AS elem
+        WHERE elem -> 'data' -> 'source' ->> 'source_type' = 'erc1155';
+      `,
+        { type: 'SELECT', transaction },
+      );
+
+      const yesterday = new Date();
+      yesterday.setDate(yesterday.getDate() - 1);
+      await queryInterface.bulkUpdate(
+        'Memberships',
+        {
+          last_checked: yesterday,
+        },
+        {
+          group_id: result.map((g) => g.id),
+        },
+      );
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    // not possible
+  },
+};
