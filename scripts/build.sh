5dad023c211f5248e74a7c7a509c011cfe57dc56
rotorsoft <rotorsoft@outlook.com>
2024-01-03 12:00:11 -0500

    add scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,6 @@
+# build libs (use tsc -b once project references are enabled)
+yarn workspace @hicommonwealth/core build
+
 if [ "$CW_BUILD" = true ]; then
   yarn --cwd packages/commonwealth build-all
 elif [ "$SL_BUILD" = true ]; then
