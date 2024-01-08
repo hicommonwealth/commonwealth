0ae77834fe019abd3c73a8128696cfff51701f2c
Timothee Legros <timothee@ualberta.ca>
2024-01-08 21:48:41 +0100

    fix migration

--- packages/commonwealth/server/migrations/20240108201454-ban-banners-model-semantics.js
+++ packages/commonwealth/server/migrations/20240108201454-ban-banners-model-semantics.js
@@ -9,14 +9,14 @@ module.exports = {
       });
       await queryInterface.sequelize.query(
         `
-        ALTER INDEX "bans_chain_id" RENAME TO "bans_community_id"
+        ALTER INDEX IF EXISTS "bans_chain_id" RENAME TO "bans_community_id"
       `,
         { transaction },
       );
       await queryInterface.sequelize.query(
         `
         ALTER TABLE "Bans"
-        RENAME CONSTRAINT "Bans_chain_id_fkey" RENAME TO "Bans_community_id_fkey"
+        RENAME CONSTRAINT "Bans_chain_id_fkey" TO "Bans_community_id_fkey"
       `,
         { transaction },
       );
@@ -33,7 +33,7 @@ module.exports = {
       await queryInterface.sequelize.query(
         `
         ALTER TABLE "CommunityBanners"
-        RENAME CONSTRAINT "CommunityBanners_chain_id_fkey" RENAME TO "CommunityBanners_community_id_fkey"
+        RENAME CONSTRAINT "CommunityBanners_chain_id_fkey" TO "CommunityBanners_community_id_fkey"
       `,
         { transaction },
       );
@@ -48,14 +48,14 @@ module.exports = {
       });
       await queryInterface.sequelize.query(
         `
-        ALTER INDEX "bans_community_id" RENAME TO "bans_chain_id"
+        ALTER INDEX IF EXISTS "bans_community_id" RENAME TO "bans_chain_id"
       `,
         { transaction },
       );
       await queryInterface.sequelize.query(
         `
         ALTER TABLE "Bans"
-        RENAME CONSTRAINT "Bans_community_id_fkey" RENAME TO "Bans_chain_id_fkey"
+        RENAME CONSTRAINT "Bans_community_id_fkey" TO "Bans_chain_id_fkey"
       `,
         { transaction },
       );
@@ -72,7 +72,7 @@ module.exports = {
       await queryInterface.sequelize.query(
         `
         ALTER TABLE "CommunityBanners"
-        RENAME CONSTRAINT "CommunityBanners_community_id_fkey" RENAME TO "CommunityBanners_chain_id_fkey"
+        RENAME CONSTRAINT "CommunityBanners_community_id_fkey" TO "CommunityBanners_chain_id_fkey"
       `,
         { transaction },
       );
