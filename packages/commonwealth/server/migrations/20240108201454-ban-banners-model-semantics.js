ce6446fb14c887db5fd63090d35e61c899191834
Timothee Legros <timothee@ualberta.ca>
2024-01-08 21:32:26 +0100

    community banners migration

--- packages/commonwealth/server/migrations/20240108201454-ban-banners-model-semantics.js
+++ packages/commonwealth/server/migrations/20240108201454-ban-banners-model-semantics.js
@@ -3,21 +3,37 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.sequelize.transaction(async (transaction) => {
+      // Bans
       await queryInterface.renameColumn('Bans', 'chain_id', 'community_id', {
         transaction,
       });
-
       await queryInterface.sequelize.query(
         `
         ALTER INDEX "bans_chain_id" RENAME TO "bans_community_id"
       `,
         { transaction },
       );
-
       await queryInterface.sequelize.query(
         `
         ALTER TABLE "Bans"
         RENAME CONSTRAINT "Bans_chain_id_fkey" RENAME TO "Bans_community_id_fkey"
+      `,
+        { transaction },
+      );
+
+      // CommunityBanners
+      await queryInterface.renameColumn(
+        'CommunityBanners',
+        'chain_id',
+        'community_id',
+        {
+          transaction,
+        },
+      );
+      await queryInterface.sequelize.query(
+        `
+        ALTER TABLE "CommunityBanners"
+        RENAME CONSTRAINT "CommunityBanners_chain_id_fkey" RENAME TO "CommunityBanners_community_id_fkey"
       `,
         { transaction },
       );
@@ -26,21 +42,37 @@ module.exports = {
 
   down: async (queryInterface, Sequelize) => {
     await queryInterface.sequelize.transaction(async (transaction) => {
+      // Bans
       await queryInterface.renameColumn('Bans', 'community_id', 'chain_id', {
         transaction,
       });
-
       await queryInterface.sequelize.query(
         `
         ALTER INDEX "bans_community_id" RENAME TO "bans_chain_id"
       `,
         { transaction },
       );
-
       await queryInterface.sequelize.query(
         `
         ALTER TABLE "Bans"
         RENAME CONSTRAINT "Bans_community_id_fkey" RENAME TO "Bans_chain_id_fkey"
+      `,
+        { transaction },
+      );
+
+      // CommunityBanners
+      await queryInterface.renameColumn(
+        'CommunityBanners',
+        'community_id',
+        'chain_id',
+        {
+          transaction,
+        },
+      );
+      await queryInterface.sequelize.query(
+        `
+        ALTER TABLE "CommunityBanners"
+        RENAME CONSTRAINT "CommunityBanners_community_id_fkey" RENAME TO "CommunityBanners_chain_id_fkey"
       `,
         { transaction },
       );
