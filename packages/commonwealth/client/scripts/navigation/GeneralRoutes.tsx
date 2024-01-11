424fafa2067a6cc32e603e8e2b5e5b4c32f75a6a
Marcin <marcin@commonwealth.im>
2024-01-11 12:47:43 +0100

    PoC for Grid and Layout components

--- packages/commonwealth/client/scripts/navigation/GeneralRoutes.tsx
+++ packages/commonwealth/client/scripts/navigation/GeneralRoutes.tsx
@@ -6,20 +6,33 @@ const TermsPage = lazy(() => import('views/pages/terms'));
 const OldTermsPage = lazy(() => import('views/pages/old_terms'));
 const PrivacyPage = lazy(() => import('views/pages/privacy'));
 const ComponentsPage = lazy(() => import('views/pages/components'));
+const Testing = lazy(() => import('views/pages/testing'));
 
 const GeneralRoutes = () => [
-  <Route path="/terms" element={withLayout(TermsPage, { type: 'common' })} />,
   <Route
+    key="/terms"
+    path="/terms"
+    element={withLayout(TermsPage, { type: 'common' })}
+  />,
+  <Route
+    key="/privacy"
     path="/privacy"
     element={withLayout(PrivacyPage, { type: 'common' })}
   />,
   <Route
+    key="/tos"
     path="/tos-1-26-2023"
     element={withLayout(OldTermsPage, { type: 'blank' })}
   />,
   <Route
+    key="/components"
     path="/components"
     element={withLayout(ComponentsPage, { type: 'common' })}
+  />,
+  <Route
+    key="/testing"
+    path="/testing"
+    element={withLayout(Testing, { type: 'common', scoped: true })}
   />,
 ];
 
