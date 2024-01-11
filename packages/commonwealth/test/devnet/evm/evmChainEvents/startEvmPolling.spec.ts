f222692ff97e594c95137332ffd02871fd6f039c
Timothee Legros <timothee@ualberta.ca>
2024-01-11 20:24:26 +0100

    remove.only

--- packages/commonwealth/test/devnet/evm/evmChainEvents/startEvmPolling.spec.ts
+++ packages/commonwealth/test/devnet/evm/evmChainEvents/startEvmPolling.spec.ts
@@ -49,7 +49,7 @@ async function sleep(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-describe.only('EVM Chain Events End to End Tests', () => {
+describe('EVM Chain Events End to End Tests', () => {
   const sandbox = sinon.createSandbox();
   let clock: sinon.SinonFakeTimers;
   let propCreatedResult: { block: number; proposalId: string };
