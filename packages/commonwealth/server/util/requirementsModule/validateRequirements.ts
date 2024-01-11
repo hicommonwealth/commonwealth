8824c115858e9ef317b16376aa254f44967b0152
Mihir Wadekar <mwadekar2000@gmail.com>
2024-01-11 13:23:31 -0800

    Added fix for rpc url

--- packages/commonwealth/server/util/requirementsModule/validateRequirements.ts
+++ packages/commonwealth/server/util/requirementsModule/validateRequirements.ts
@@ -1,6 +1,8 @@
+import { ERC165__factory } from '@hicommonwealth/chains';
 import Ajv from 'ajv';
 import { CosmosSDK } from 'cosmos-sdk';
-import Web3 from 'web3';
+import { ethers } from 'ethers';
+import db from 'server/database';
 import requirementsSchema from './requirementsSchema_v1.json';
 import { Requirement } from './requirementsTypes';
 
@@ -8,6 +10,13 @@ const Errors = {
   InvalidRequirements: 'Invalid requirements',
   InvalidAddress: 'Invalid address',
   ContractNotFound: 'Contract not found',
+  InvalidContractType: 'Invalid contract type',
+};
+
+const InterfaceIds = {
+  ERC1155: '0xd9b67a26',
+  ERC721: '0x80ac58cd',
+  ERC20: '0x36372b07',
 };
 
 function isValidEVMContractAddress(address: string): boolean {
@@ -24,14 +33,60 @@ async function isEVMAddressContract(
   address: string,
   network_url: string,
 ): Promise<boolean> {
-  const web3 = new Web3(network_url);
+  const provider = new ethers.providers.JsonRpcProvider(network_url);
   try {
-    const code = await web3.eth.getCode(address);
+    const code = await provider.getCode(address);
     return code !== '0x';
   } catch (error) {
     console.error('Error checking address:', error);
     return false;
   }
+}
+
+async function getCosmosContractType(
+  contractAddress: string,
+  network_url: string,
+): Promise<string | null> {
+  // Implement the logic to query the Cosmos chain and get the contract type
+  const cosmosSDK = new CosmosSDK(network_url);
+  const contractInfo = await cosmosSDK.getContractInfo(contractAddress);
+
+  if (contractInfo) {
+    return contractInfo.type;
+  }
+
+  return null;
+}
+
+async function getEVMContractType(
+  contractAddress: string,
+  network_url: string,
+): Promise<string | null> {
+  const provider = new ethers.providers.JsonRpcProvider(network_url);
+  const contract = ERC165__factory.connect(contractAddress, provider);
+
+  // Check if the contract implements ERC1155 interface
+  const supportsERC1155 = await contract.supportsInterface(
+    InterfaceIds.ERC1155,
+  );
+  if (supportsERC1155) {
+    return 'erc1155';
+  }
+
+  // Check if the contract implements ERC721 interface
+  const supportsERC721 = await contract.supportsInterface(InterfaceIds.ERC721);
+  if (supportsERC721) {
+    return 'erc721';
+  }
+
+  // Check if the contract implements ERC20 interface
+  const supportsERC20 = await contract.supportsInterface(InterfaceIds.ERC20);
+  if (supportsERC20) {
+    return 'erc20';
+  }
+
+  // If none of the above interfaces are implemented, return null
+  return null;
 }
 
 async function isCosmosAddressContract(
@@ -79,14 +134,35 @@ export default function validateRequirements(
         if (!isValidAddress) {
           return new Error(`${Errors.InvalidAddress}: ${contract_address}`);
         }
+
         const evmId = requirement.data.source.evm_chain_id;
+        const node = await db.ChainNode.findOne({
+          where: {
+            eth_chain_id: evmId,
+          },
+          attributes: ['url'],
+        });
+
         const isAddressContract = await isEVMAddressContract(
           contract_address,
-          `rpcURL`,
+          node.url,
         );
 
         if (!isAddressContract) {
           return new Error(`${Errors.ContractNotFound}: ${contract_address}`);
+        }
+
+        const isContractType = await getEVMContractType(
+          contract_address,
+          `rpcURL`,
+        );
+        if (
+          !isContractType ||
+          requirement.data.source.source_type !== isContractType
+        ) {
+          return new Error(
+            `${Errors.InvalidContractType}: ${contract_address}`,
+          );
         }
       }
 
@@ -99,13 +175,33 @@ export default function validateRequirements(
         }
 
         const cosmosId = requirement.data.source.cosmos_chain_id;
-        const isAddressContract = await isEVMAddressContract(
+        const node = await db.ChainNode.findOne({
+          where: {
+            cosmos_chain_id: cosmosId,
+          },
+          attributes: ['url'],
+        });
+
+        const isAddressContract = await isCosmosAddressContract(
           contract_address,
-          `rpcURL`,
+          node.url,
         );
 
         if (!isAddressContract) {
           return new Error(`${Errors.ContractNotFound}: ${contract_address}`);
+        }
+
+        const isContractType = await getCosmosContractType(
+          contract_address,
+          `rpcURL`,
+        );
+        if (
+          !isContractType ||
+          requirement.data.source.source_type !== isContractType
+        ) {
+          return new Error(
+            `${Errors.InvalidContractType}: ${contract_address}`,
+          );
         }
       }
     }
