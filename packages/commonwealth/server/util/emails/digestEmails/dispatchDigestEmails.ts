ff2edbbda034246784caeeb7fe6c2b2edc629021
Timothee Legros <timothee@ualberta.ca>
2024-01-12 01:02:00 +0100

    email data + objects for digest emails

---
+++ packages/commonwealth/server/util/emails/digestEmails/dispatchDigestEmails.ts
@@ -0,0 +1,6 @@
+export async function dispatchDigestEmails() {
+  // attempt to acquire daily lock
+  // attempt to acquire weekly lock
+  // if lock is acquired batch fetch users
+  // send emails to users in batches
+}
