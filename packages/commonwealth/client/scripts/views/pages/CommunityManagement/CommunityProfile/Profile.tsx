3189d6b2b83104babfa8d118ec7cd005515ac7c2
Malik Zulqurnain <malikzulqurnain2121@gmail.com>
2024-01-18 23:54:46 +0500

    Use CommunityManagementLayout in community profile component

--- packages/commonwealth/client/scripts/views/pages/CommunityManagement/CommunityProfile/Profile.tsx
+++ packages/commonwealth/client/scripts/views/pages/CommunityManagement/CommunityProfile/Profile.tsx
@@ -1,42 +1,21 @@
 import React from 'react';
-import app from 'state';
-import Permissions from 'utils/Permissions';
-import FeatureHint from 'views/components/FeatureHint';
-import { CWText } from 'views/components/component_kit/cw_text';
-import { PageNotFound } from '../../404';
+import CommunityManagementLayout from '../common/CommunityManagementLayout';
 import CommunityProfileForm from './CommunityProfileForm';
-import './Profile.scss';
 
 const CommunityProfile = () => {
-  if (
-    !app?.user?.activeAccount?.address ||
-    !(Permissions.isSiteAdmin() || Permissions.isCommunityAdmin())
-  ) {
-    return <PageNotFound />;
-  }
-
   return (
-    <section className="CommunityProfile">
-      <section className="left-section">
-        <div className="header">
-          <CWText type="h2">Community Profile</CWText>
-          <CWText type="b1">Manage the basic details of your community</CWText>
-        </div>
-
-        <CommunityProfileForm />
-      </section>
-
-      <section className="right-section">
-        <FeatureHint
-          title="URL vs Namespace"
-          hint={`
-          Your community URL is the web2.0 link to your Common space online. 
+    <CommunityManagementLayout
+      title="Community Profile"
+      description="Manage the basic details of your community"
+      featureHint={{
+        title: 'URL vs Namespace',
+        description: `Your community URL is the web2.0 link to your Common space online. 
           The namespace is the web3.0 equivalent where you can point to your community on-chain. 
-          You can purchase additional community namespaces.
-        `}
-        />
-      </section>
-    </section>
+          You can purchase additional community namespaces.`,
+      }}
+    >
+      <CommunityProfileForm />
+    </CommunityManagementLayout>
   );
 };
 
