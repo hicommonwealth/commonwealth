b05549abb4400db5e378d84cd5cead43d8311420
Malik Zulqurnain <51641047+mzparacha@users.noreply.github.com>
2024-01-18 23:46:07 +0500

    [Admin-Onboarding] New Community Integrations Screen (#6364)
    * Refactored CreateCommunityHint -> FeatureHint
    
    * Added layout to integrations screen
    
    * Added LinkArray component
    
    * Added Directory integration to new integrations page
    
    * Added Snapshot integration to new integrations page
    
    * Added Webhooks integration to new integration page
    
    * Added CustomTOS integration to new integrations page
    
    * Added CustomURL integration to new integrations page
    
    * Updated import for WebhookCategory type
    
    * Updated import
    
    * Added Discord integration to new integrations page
    
    * Added common validation file for links
    
    * Fix css
    
    * Fix discord channels render condition
    
    * Fix trim condition
    
    * Fix trim condition
    
    * Use pluralizeWithoutNumberPrefix helper
    
    * Purged unnecessary css
    
    * Adjusted padding on small screen sizes
    
    * Update sidebar on directory settings change
    
    * Redirect to 404 page if not admin
    
    * Fix integrations page redirect from admin onboarding slider
    
    * Added common layout for community management

--- packages/commonwealth/client/scripts/views/components/FeatureHint/FeatureHint.tsx
+++ packages/commonwealth/client/scripts/views/components/FeatureHint/FeatureHint.tsx
@@ -1,18 +1,18 @@
+import clsx from 'clsx';
 import React from 'react';
-
 import { CWIcon } from 'views/components/component_kit/cw_icons/cw_icon';
 import { CWText } from 'views/components/component_kit/cw_text';
+import './FeatureHint.scss';
 
-import './CreateCommunityHint.scss';
-
-interface CreateCommunityHintProps {
+interface FeatureHintProps {
   title: string;
   hint: string;
+  className?: string;
 }
 
-const CreateCommunityHint = ({ title, hint }: CreateCommunityHintProps) => {
+const FeatureHint = ({ title, hint, className }: FeatureHintProps) => {
   return (
-    <div className="CreateCommunityHint">
+    <div className={clsx('FeatureHint', className)}>
       <div className="title-row">
         <CWIcon iconName="lightbulb" />
         <CWText fontWeight="medium">{title}</CWText>
@@ -22,4 +22,4 @@ const CreateCommunityHint = ({ title, hint }: CreateCommunityHintProps) => {
   );
 };
 
-export default CreateCommunityHint;
+export default FeatureHint;
