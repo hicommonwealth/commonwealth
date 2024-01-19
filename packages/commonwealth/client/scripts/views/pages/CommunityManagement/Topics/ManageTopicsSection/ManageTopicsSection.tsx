68dc03e408bee47393959ab48a1314dcc01f4dbc
Ifu Aniemeka <ianiemeka@gmail.com>
2024-01-19 16:16:47 -0600

    nevermind

--- packages/commonwealth/client/scripts/views/pages/CommunityManagement/Topics/ManageTopicsSection/ManageTopicsSection.tsx
+++ packages/commonwealth/client/scripts/views/pages/CommunityManagement/Topics/ManageTopicsSection/ManageTopicsSection.tsx
@@ -66,7 +66,7 @@ export const ManageTopicsSection = () => {
         <div className="featured-topic-list">
           {topics.length ? (
             <DraggableTopicsList
-              topics={getFilteredTopics(rawTopics)}
+              topics={topics}
               setTopics={setTopics}
               onEdit={setTopicSelectedToEdit}
             />
