ec21ea06555406e70ed240a664e01f89591ef054
rotorsoft <rotorsoft@outlook.com>
2024-01-19 15:48:22 -0500

    add comment

--- libs/model/src/middleware/isCommunityAuthor.ts
+++ libs/model/src/middleware/isCommunityAuthor.ts
@@ -1,6 +1,10 @@
 import { models } from '../database'; // TODO: use port/adapter pattern to test in-memory
 import { ActorMiddleware } from '../types';
 
+// TODO: Important - Let's analyze all existing validation routines and their usage patterns
+// - Auth loading/caching operations can be optimized
+// - Instead of loading the community and the admin address in two db request, we can load everything at once if this is a common pattern
+
 /**
  * Middleware to validate user as author of the loaded community
  * @param actor the actor state
