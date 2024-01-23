20218cd91c4658b187dfe6d5460289c14a446592
ianrowan <i.rowan299@gmail.com>
2024-01-23 13:39:26 -0600

    add initialization flow to contract helpers

--- packages/commonwealth/client/scripts/helpers/ContractHelpers/ContractBase.ts
+++ packages/commonwealth/client/scripts/helpers/ContractHelpers/ContractBase.ts
@@ -9,22 +9,43 @@ abstract class ContractBase {
   public contractAddress;
   protected wallet: IWebWallet<any>;
   protected web3: Web3;
+  protected initialized: boolean;
+  private abi;
 
   constructor(contractAddress: string, abi: any) {
     this.contractAddress = contractAddress;
-    this.wallet = WebWalletController.Instance.availableWallets(
-      ChainBase.Ethereum,
-    )[0];
+    this.abi = abi;
+  }
 
-    if (!this.wallet.api) {
-      throw new Error('Web3 Api Not Initialized');
+  async initialize(): Promise<void> {
+    if (!this.initialized) {
+      try {
+        this.wallet = WebWalletController.Instance.availableWallets(
+          ChainBase.Ethereum,
+        )[0];
+
+        if (!this.wallet.api) {
+          await this.wallet.enable();
+        }
+
+        this.web3 = new Web3(this.wallet.api.givenProvider);
+        this.contract = new this.web3.eth.Contract(
+          this.abi as AbiItem[],
+          this.contractAddress,
+        );
+        this.initialized = true;
+      } catch (error) {
+        throw new Error('Failed to initialize contract: ' + error);
+      }
     }
+  }
 
-    this.web3 = new Web3(this.wallet.api.givenProvider);
-    this.contract = new this.web3.eth.Contract(
-      abi as AbiItem[],
-      contractAddress,
-    );
+  isInitialized(): void {
+    if (!this.initialized) {
+      throw new Error(
+        'Contract not initialzed. Call Contract.initialize() first.',
+      );
+    }
   }
 
   protected toBN(number: string | number) {
--- packages/commonwealth/client/scripts/helpers/ContractHelpers/NamespaceFactory.ts
+++ packages/commonwealth/client/scripts/helpers/ContractHelpers/NamespaceFactory.ts
@@ -15,19 +15,21 @@ class NamespaceFactory extends ContractBase {
    */
   constructor(factoryAddress: string) {
     super(factoryAddress, namespaceFactoryAbi);
-    this.contract.methods
-      .reservationHook()
-      .call()
-      .then((addr) => {
-        if (
-          addr.toLowerCase() !== '0x0000000000000000000000000000000000000000'
-        ) {
-          this.reservationHook = new this.web3.eth.Contract(
-            reservationHookAbi as AbiItem[],
-            addr,
-          );
-        }
-      });
+  }
+
+  /**
+   * Initializes wallet and contracts.
+   * This must be called after instantiation before other methods are available.
+   */
+  async initialize(): Promise<void> {
+    await super.initialize();
+    const addr = await this.contract.methods.reservationHook().call();
+    if (addr.toLowerCase() !== '0x0000000000000000000000000000000000000000') {
+      this.reservationHook = new this.web3.eth.Contract(
+        reservationHookAbi as AbiItem[],
+        addr,
+      );
+    }
   }
 
   /**
@@ -36,6 +38,7 @@ class NamespaceFactory extends ContractBase {
    * @returns contract address 0x...
    */
   async getNamespaceAddress(name: string): Promise<string> {
+    this.isInitialized();
     const hexString = this.web3.utils.utf8ToHex(name);
     const activeNamespace = await this.contract.methods
       .getNamespace(hexString)
@@ -50,6 +53,7 @@ class NamespaceFactory extends ContractBase {
    * @returns contract address 0x...
    */
   async checkNamespaceReservation(name: string): Promise<boolean> {
+    this.isInitialized();
     const activeNamespace = await this.getNamespaceAddress(name);
     if (activeNamespace !== '0x0000000000000000000000000000000000000000') {
       return false;
@@ -69,6 +73,7 @@ class NamespaceFactory extends ContractBase {
    * @returns txReceipt or Error if name is taken or tx fails
    */
   async deployNamespace(name: string, feeManager: string): Promise<any> {
+    this.isInitialized();
     // Check if name is available
     const namespaceStatus = await this.checkNamespaceReservation(name);
     if (!namespaceStatus) {
@@ -95,6 +100,7 @@ class NamespaceFactory extends ContractBase {
    * @returns tx receipt or failure message
    */
   async configureCommunityStakes(name: string, stakesId: number): Promise<any> {
+    this.isInitialized();
     let txReceipt;
     try {
       txReceipt = await this.contract.methods
