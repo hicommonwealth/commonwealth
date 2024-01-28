35ac22a67a20dce3c3902add8de46a996c734f93
rotorsoft <rotorsoft@outlook.com>
2024-01-28 11:46:02 -0500

    improve sanity checks, fix test import

--- scripts/sanity.sh
+++ scripts/sanity.sh
@@ -1,4 +1,17 @@
 #! /bin/bash
 
 # Sanity scripts we should run locally before pushing code
-yarn lint-branch-warnings && yarn workspaces run check-types && yarn workspace commonwealth unit-test
+
+# lint changes
+yarn lint-branch-warnings
+
+# check types 
+# build libs to update types (tsc -b --noEmit is not allowed)
+yarn workspaces run clean
+yarn workspace @hicommonwealth/adapters tsc -b --emitDeclarationOnly
+yarn workspace @hicommonwealth/chains tsc -b --emitDeclarationOnly
+yarn workspace @hicommonwealth/model tsc -b --emitDeclarationOnly
+yarn workspaces run check-types
+
+# unit test
+yarn workspace commonwealth unit-test
