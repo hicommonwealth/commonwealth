b3d23ffe6a7577201f31bf32ef890ce75e5e6ae9
Kurtis Assad <kurtis@commonwealth.im>
2024-01-29 06:43:04 -0800

    Added fixes for community stake (#6470)

--- packages/commonwealth/server/controllers/server_communities_methods/get_community_stake.ts
+++ packages/commonwealth/server/controllers/server_communities_methods/get_community_stake.ts
@@ -3,13 +3,27 @@ import { CommunityStakeAttributes } from '@hicommonwealth/model/build/models/com
 import { ServerCommunitiesController } from '../server_communities_controller';
 
 export type GetCommunityStakeOptions = Community.GetCommunityStake;
-export type GetCommunityStakeResult = CommunityStakeAttributes;
+export type GetCommunityStakeResult = CommunityStakeAttributes & {
+  Chain: { namespace: string };
+};
 
 export async function __getCommunityStake(
   this: ServerCommunitiesController,
   { community_id, stake_id }: GetCommunityStakeResult,
 ): Promise<CommunityStakeAttributes> {
+  const where = { community_id };
+  if (stake_id) {
+    where['stake_id'] = stake_id;
+  }
+
   return await this.models.CommunityStake.findOne({
-    where: { community_id, stake_id },
+    where,
+    include: [
+      {
+        model: this.models.Community,
+        required: true,
+        attributes: ['namespace'],
+      },
+    ],
   });
 }
