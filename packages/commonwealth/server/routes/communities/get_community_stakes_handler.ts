939c1fc494bff0e1d11bd003a801bdc3417d6b16
rotorsoft <rotorsoft@outlook.com>
2024-01-30 20:27:38 -0500

    fix imports

--- packages/commonwealth/server/routes/communities/get_community_stakes_handler.ts
+++ packages/commonwealth/server/routes/communities/get_community_stakes_handler.ts
@@ -1,6 +1,5 @@
 import { AppError } from '@hicommonwealth/adapters';
-import { Community, DB } from '@hicommonwealth/model';
-import { CommunityStakeAttributes } from '@hicommonwealth/model/build/models/community_stake';
+import { Community, CommunityStakeAttributes, DB } from '@hicommonwealth/model';
 import { ServerControllers } from '../../routing/router';
 import { TypedRequestParams, TypedResponse, success } from '../../types';
 import { formatErrorPretty } from '../../util/errorFormat';
