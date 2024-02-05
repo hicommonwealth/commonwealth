3515e811463090a7eacd47c6231843f490f781d8
Timothee Legros <timothee@ualberta.ca>
2024-02-05 19:37:56 +0100

    working bundle but failing build with `set -e` (testing fix)

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,5 +7,6 @@ if [ "$SL_BUILD" = true ]; then
 elif [ "$DL_BUILD" = true ]; then
   yarn workspace discord-bot build
 else
+  NODE_OPTIONS=--max_old_space_size=4096 yarn workspace commonwealth bundle
   exit 2
 fi
