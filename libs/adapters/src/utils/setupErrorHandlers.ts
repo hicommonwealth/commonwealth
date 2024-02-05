473724b596076c917ba521520aa3dae61f798819
Roger Torres <41707007+Rotorsoft@users.noreply.github.com>
2024-02-05 11:21:06 -0500

    hotfix sending app errors to rollbar (#6562)
    Co-authored-by: rotorsoft <rotorsoft@outlook.com>

--- libs/adapters/src/utils/setupErrorHandlers.ts
+++ libs/adapters/src/utils/setupErrorHandlers.ts
@@ -1,12 +1,6 @@
 import { AppError, ServerError, logger } from '@hicommonwealth/core';
 import type { Express, Request, Response } from 'express';
 
-class ExpressError extends Error {
-  constructor(public error: Error, public req: Request) {
-    super(error.message);
-  }
-}
-
 // Handle server and application errors.
 // 401 Unauthorized errors are handled by Express' middleware and returned
 // before this handler. Errors that hit the final condition should be either
@@ -26,19 +20,22 @@ export const setupErrorHandlers = (app: Express) => {
   // Handle our ServerErrors (500), AppErrors (400), or unknown errors.
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   app.use((error, req, res: Response, next) => {
-    log.error(error.message, new ExpressError(error, req));
+    error.req = req;
     if (error instanceof ServerError) {
+      log.error(error.message, error);
       res.status(error.status).send({
         status: error.status,
         // Use external facing error message
         error: 'Server error, please try again later.',
       });
     } else if (error instanceof AppError) {
+      log.warn(error.message, error); // just warn, to avoid overloading rollbar with bots and attacks
       res.status(error.status).send({
         status: error.status,
         error: error.message,
       });
     } else {
+      log.error(error.message, error);
       res.status(500);
       res.json({
         status: error.status,
