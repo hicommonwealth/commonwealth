eeee1ffb0a152fffa333131c036b8e7d16d2163f
Malik Zulqurnain <51641047+mzparacha@users.noreply.github.com>
2024-02-08 23:58:36 +0500

    Added valentine growl (#6657)
    * Added valentine growl
    
    * Added invite info

--- packages/commonwealth/client/scripts/views/Sublayout.tsx
+++ packages/commonwealth/client/scripts/views/Sublayout.tsx
@@ -13,6 +13,7 @@ import { SublayoutBanners } from './SublayoutBanners';
 import { SublayoutHeader } from './SublayoutHeader';
 import { AdminOnboardingSlider } from './components/AdminOnboardingSlider';
 import { Breadcrumbs } from './components/Breadcrumbs';
+import { ValentineGrowl } from './components/ValentineGrowl';
 
 type SublayoutProps = {
   hideFooter?: boolean;
@@ -100,6 +101,7 @@ const Sublayout = ({
             )}
           </div>
         </div>
+        <ValentineGrowl />
       </div>
     </div>
   );
