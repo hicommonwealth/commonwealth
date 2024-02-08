5665eb02b8d789179705fccb5ac8ecac922499f0
Graham Johnson <graham@commonwealth.im>
2024-02-08 11:56:44 -0500

    Swap lint scripts from accidental deletion

--- scripts/lint-new-work.sh
+++
@@ -1,9 +0,0 @@
-LINES=$(git status --porcelain | grep "/*.ts" | sed 's/^...//' | tr '\n' ' ' | xargs ls -d 2>/dev/null)
-
-if [ -z "$LINES" ]
-then
-    echo "There is nothing to lint"
-else
-    echo $LINES
-    NODE_OPTIONS="--max-old-space-size=4096" eslint $LINES
-fi
