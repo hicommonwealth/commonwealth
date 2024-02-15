1d4d06e394514b3a3de2f18da42d937009aaab96
rotorsoft <rotorsoft@outlook.com>
2024-02-15 17:03:39 -0500

    add more tests

--- libs/model/__tests__/community/group-lifecycle.spec.ts
+++ libs/model/__tests__/community/group-lifecycle.spec.ts
@@ -13,4 +13,8 @@ describe('group lifecycle', () => {
   it('should create group when none exists', () => {});
 
   it('should fail creation when group with same id found', () => {});
+
+  it('should fail creation when community reached max number of groups allowed', () => {});
+
+  it('should fail creation when sending invalid topics', () => {});
 });
