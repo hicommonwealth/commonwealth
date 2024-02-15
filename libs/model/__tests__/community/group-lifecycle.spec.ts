5c595c6db1c9fe4526df0b4fa25eae91831d0278
rotorsoft <rotorsoft@outlook.com>
2024-02-15 14:39:44 -0500

    add tests

--- libs/model/__tests__/community/group-lifecycle.spec.ts
+++ libs/model/__tests__/community/group-lifecycle.spec.ts
@@ -1,1 +1,16 @@
-describe('group lifecycle', () => {});
+import { dispose } from '@hicommonwealth/core';
+import { tester } from '@hicommonwealth/model';
+
+describe('group lifecycle', () => {
+  before(async () => {
+    await tester.seedDb();
+  });
+
+  after(async () => {
+    await dispose()();
+  });
+
+  it('should create group when none exists', () => {});
+
+  it('should fail creation when group with same id found', () => {});
+});
