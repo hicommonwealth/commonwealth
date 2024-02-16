18bd067ff3464c7bdece31ff82e90bfa09d7be2e
Timothee Legros <62490329+timolegros@users.noreply.github.com>
2024-02-16 22:07:05 +0100

    Sanitize `custom_stages` and `stages_enabled` (#6770)
    * stages_enabled column fixes
    
    * custom_stages fixes
    
    * validate custom stages array in API
    
    * fix client/API custom stages
    
    * type fixes
    
    * fix migration
    
    * disallow single quotes in validation
    
    * remove quote replacement

--- packages/commonwealth/server/controllers/server_communities_methods/update_community.ts
+++ packages/commonwealth/server/controllers/server_communities_methods/update_community.ts
@@ -194,8 +194,9 @@ export async function __updateCommunity(
   if (hide_projects) community.hide_projects = hide_projects;
   if (typeof stages_enabled === 'boolean')
     community.stages_enabled = stages_enabled;
-  if (typeof custom_stages === 'string')
+  if (Array.isArray(custom_stages)) {
     community.custom_stages = custom_stages;
+  }
   if (typeof terms === 'string') community.terms = terms;
   if (has_homepage) community.has_homepage = has_homepage;
   if (default_page) {
