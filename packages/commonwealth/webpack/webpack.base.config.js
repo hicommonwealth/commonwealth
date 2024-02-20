62f87796fcebed415eb2672a40a46f671278fee9
Kurtis Assad <kurtis@commonwealth.im>
2024-02-20 10:44:08 -0800

    Added unleash provider for openconfig (#6707)

--- packages/commonwealth/webpack/webpack.base.config.js
+++ packages/commonwealth/webpack/webpack.base.config.js
@@ -85,6 +85,24 @@ module.exports = {
         process.env.FLAG_COMMUNITY_STAKE,
       ),
     }),
+    new webpack.DefinePlugin({
+      'process.env.IS_PRODUCTION': JSON.stringify(process.env.IS_PRODUCTION),
+    }),
+    new webpack.DefinePlugin({
+      'process.env.FLAG_ROOT_DOMAIN_REBRAND': JSON.stringify(
+        process.env.FLAG_ROOT_DOMAIN_REBRAND,
+      ),
+    }),
+    new webpack.DefinePlugin({
+      'process.env.UNLEASH_FRONTEND_SERVER_URL': JSON.stringify(
+        process.env.UNLEASH_FRONTEND_SERVER_URL,
+      ),
+    }),
+    new webpack.DefinePlugin({
+      'process.env.UNLEASH_FRONTEND_API_TOKEN': JSON.stringify(
+        process.env.UNLEASH_FRONTEND_API_TOKEN,
+      ),
+    }),
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, '../client/index.html'),
       attributes: {
