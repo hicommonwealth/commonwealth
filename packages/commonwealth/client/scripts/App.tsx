a06f2f6eeb28a9583b52d28996411a85df6a1fa2
Mihir Wadekar <mwadekar2000@gmail.com>
2024-02-22 10:04:59 -0800

    Adding a delay for loading prompt

--- packages/commonwealth/client/scripts/App.tsx
+++ packages/commonwealth/client/scripts/App.tsx
@@ -4,7 +4,7 @@ import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import useInitApp from 'hooks/useInitApp';
 import router from 'navigation/Router';
-import React, { StrictMode } from 'react';
+import React, { StrictMode, useEffect, useState } from 'react';
 import { RouterProvider } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import { queryClient } from 'state/api/config';
@@ -30,6 +30,24 @@ const App = () => {
   const { isAddedToHomeScreen, isMarketingPage, isIOS, isAndroid } =
     useAppStatus();
 
+  const [showPrompt, setShowPrompt] = useState(false);
+  const [isSplashUnloaded, setIsSplashUnloaded] = useState(false);
+
+  useEffect(() => {
+    if (!isLoading) {
+      // Delay the unloading of the Splash component
+      setTimeout(() => {
+        setIsSplashUnloaded(true);
+      }, 1000); // Adjust the delay as needed
+    }
+  }, [isLoading]);
+
+  useEffect(() => {
+    if (isSplashUnloaded) {
+      setShowPrompt(true);
+    }
+  }, [isSplashUnloaded]);
+
   return (
     <StrictMode>
       <QueryClientProvider client={queryClient}>
@@ -39,7 +57,7 @@ const App = () => {
           ) : (
             <>
               <RouterProvider router={router(customDomain)} />
-              {isAddedToHomeScreen || isMarketingPage ? null : (
+              {isAddedToHomeScreen || isMarketingPage || !showPrompt ? null : (
                 <AddToHomeScreenPrompt isIOS={isIOS} isAndroid={isAndroid} />
               )}
             </>
