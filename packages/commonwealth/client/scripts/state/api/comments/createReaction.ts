2b03b9beb09d1bedff839d92c08e312a41f5d25e
Bob Webb <bob.wbb@gmail.com>
2024-03-05 12:06:07 +0000

    more updates for API calls

--- packages/commonwealth/client/scripts/state/api/comments/createReaction.ts
+++ packages/commonwealth/client/scripts/state/api/comments/createReaction.ts
@@ -1,3 +1,4 @@
+import ipldDagJson from '@ipld/dag-json';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import axios from 'axios';
 import Reaction from 'models/Reaction';
@@ -20,9 +21,10 @@ const createReaction = async ({
   commentId,
 }: CreateReactionProps) => {
   const {
-    session = null,
-    action = null,
-    hash = null,
+    sessionMessage,
+    sessionMessageSignature,
+    actionMessage,
+    actionMessageSignature,
   } = await app.sessions.signCommentReaction(address, {
     comment_id: commentId,
     like: reactionType === 'like',
@@ -36,9 +38,18 @@ const createReaction = async ({
       address,
       reaction: reactionType,
       jwt: app.user.jwt,
-      canvas_action: action,
-      canvas_session: session,
-      canvas_hash: hash,
+      canvas_action_message: actionMessage
+        ? ipldDagJson.stringify(ipldDagJson.encode(actionMessage))
+        : null,
+      canvas_action_message_signature: actionMessageSignature
+        ? ipldDagJson.stringify(ipldDagJson.encode(actionMessageSignature))
+        : null,
+      canvas_session_message: sessionMessage
+        ? ipldDagJson.stringify(ipldDagJson.encode(sessionMessage))
+        : null,
+      canvas_session_message_signature: sessionMessageSignature
+        ? ipldDagJson.stringify(ipldDagJson.encode(sessionMessageSignature))
+        : null,
       comment_id: commentId,
     },
   );
