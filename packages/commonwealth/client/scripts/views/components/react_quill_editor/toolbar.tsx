329886f95c28df36205c39b9132aa7d532e99fb3
lzach83 <lzach83@gmail.com>
2024-03-05 16:45:59 -0500

    Added TSDoc fixed a small bug, switched to using .map instead of .reduce for readability.

--- packages/commonwealth/client/scripts/views/components/react_quill_editor/toolbar.tsx
+++ packages/commonwealth/client/scripts/views/components/react_quill_editor/toolbar.tsx
@@ -189,34 +189,71 @@ export const useMarkdownToolbarHandlers = ({
     };
   };
 
-  // // Handles selecting and formatting ordered lists
+  /**
+   * Replaces the selections list item prefixes in the given text with the specified prefix.
+   * @param {string} text - The input text containing list items.
+   * @param {string} prefix - The new prefix to be applied to the list items.
+   * @returns {string} The text with list item prefixes replaced or added.
+   */
   const handleListText = (text: string, prefix: string) => {
     let counter = 1;
     let hasIncremented = false;
 
-    return text.split('\n').reduce((acc, line) => {
-      if (line.trim().length === 0) {
-        return acc;
-      }
-
-      if (line.startsWith(prefix)) {
-        return acc + `${line.trim()}\n`;
-      }
-
-      if (prefix === '1.') {
-        const numberedPrefix = `${counter++}.`;
-        if (line.trim() === '' && !hasIncremented) {
-          hasIncremented = true; // Mark the counter as incremented
-          return acc + `${numberedPrefix} ${line}\n`;
+    return text
+      .split('\n')
+      .map((line) => {
+        if (line.trim().length === 0) {
+          return line;
         }
-        hasIncremented = false;
-        return acc + `${numberedPrefix} ${line}\n`;
-      }
-      return acc + `${prefix} ${line}\n`;
-    }, '');
+
+        const startsWithPrefix = Object.values(LIST_ITEM_PREFIX).some((p) =>
+          line.trim().startsWith(p),
+        );
+
+        if (startsWithPrefix) {
+          return Object.keys(LIST_ITEM_PREFIX).reduce((modifiedLine, key) => {
+            if (line.trim().startsWith(LIST_ITEM_PREFIX[key])) {
+              return modifiedLine.replace(
+                new RegExp(`^\\s*(${LIST_ITEM_PREFIX[key]})`),
+                prefix,
+              );
+            }
+            return modifiedLine;
+          }, line);
+        }
+
+        if (/^\s*\d+\./.test(line)) {
+          return line.replace(/^\s*\d+\./, prefix);
+        }
+
+        if (line.startsWith(prefix)) {
+          return line;
+        }
+
+        if (prefix === LIST_ITEM_PREFIX.ordered) {
+          const numberedPrefix = `${counter++}.`;
+          if (line.trim() === '' && !hasIncremented) {
+            hasIncremented = true; // Mark the counter as incremented
+            return `${numberedPrefix} ${line}`;
+          }
+          hasIncremented = false;
+          return `${numberedPrefix} ${line}`;
+        }
+
+        return `${prefix} ${line}`;
+      })
+      .join('\n');
   };
 
+  /**
+   * Creates a handler for applying list markdown to the text in the editor.
+   * @returns {Function} A function that applies list markdown to the text in line or selected text.
+   */
   const createListHandler = () => {
+    /**
+     * Applies list markdown to the selected text in the editor.
+     * @param {string} value - The value representing the type of list (e.g., 'ordered', 'bullet', 'check').
+     */
     return (value: string) => {
       const editor = editorRef?.current?.getEditor();
 
@@ -239,7 +276,7 @@ export const useMarkdownToolbarHandlers = ({
         editor.deleteText(selection.index, selection.length);
         editor.insertText(selection.index, newText);
       } else {
-        const [currentLine, offset] = editor.getLeaf(selection.index);
+        const [currentLine] = editor.getLeaf(selection.index);
         const currentLineIdx = editor.getIndex(currentLine);
 
         //if there is no text in the editor, insert the prefix and a space
@@ -250,27 +287,27 @@ export const useMarkdownToolbarHandlers = ({
         }
 
         //if the current line has a prefix and there is text after the prefix, replace the prefix
-        if (
-          currentLine &&
-          currentLine.text.startsWith(prefix) &&
-          editorLength > prefix.length
-        ) {
-          const currentLineText = currentLine.text.replace(prefix, '');
-          editor.deleteText(currentLineIdx, currentLine.text.length);
-          editor.insertText(currentLineIdx, currentLineText.trim());
-          return;
-        }
+        Object.values(LIST_ITEM_PREFIX).forEach((p) => {
+          if (p === prefix) {
+            const currentLineText = currentLine.text.replace(prefix, '');
+            editor.deleteText(currentLineIdx, currentLine.text.length);
+            editor.insertText(currentLineIdx, currentLineText.trim());
+          }
+          if (currentLine.text.startsWith(p)) {
+            const currentLineText = currentLine.text.replace(p, '');
+            editor.deleteText(currentLineIdx, currentLine.text.length);
+            editor.insertText(currentLineIdx, currentLineText.trim());
+            return;
+          }
+        });
 
         //if the current line has a prefix and there is no text after the prefix, remove the prefix
-        if (
-          (currentLine.text && currentLine.text.trim() === prefix) ||
-          currentLine.text.startsWith(prefix)
-        ) {
+        if (currentLine.text && currentLine.text.trim() === prefix) {
           editor.deleteText(currentLineIdx, editorLength || prefix.length);
           return;
         }
 
-        editor.insertText(currentLineIdx || 0, `${prefix} `);
+        return editor.insertText(currentLineIdx || 0, `${prefix} `);
       }
 
       setContentDelta({
