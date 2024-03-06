57e73ee478de9dc28a073fff9ae25cb02e200f13
Timothee Legros <timothee@ualberta.ca>
2024-03-06 19:08:03 +0200

    sushi image migration

---
+++ packages/commonwealth/server/migrations/20240306163523-sushi-image-fix.js
@@ -0,0 +1,43 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.sequelize.transaction(async (transaction) => {
+      await queryInterface.sequelize.query(
+        `
+        UPDATE "Threads"
+        SET body = REPLACE(body, 'https%3A%2F%2Fglobal.discourse-cdn.com%2Fbusiness7%2Fuploads%2Fsushiswapclassic%2F', 'https%3A%2F%2Fassets.commonwealth.im%2Fsushiswapclassic%2F')
+        WHERE community_id = 'sushi';
+      `,
+        { transaction },
+      );
+
+      await queryInterface.sequelize.query(
+        `
+        UPDATE "Threads"
+        SET body = REPLACE(body, '%2Foptimized%2F', '%2Foriginal%2F')
+        WHERE community_id = 'sushi';
+      `,
+        { transaction },
+      );
+
+      await queryInterface.sequelize.query(
+        `
+        UPDATE "Threads"
+        SET body = REGEXP_REPLACE(body, '_\\d+_\\d+x\\d+(?=\\.\\w+)', '', 'g')
+        WHERE community_id = 'sushi';
+      `,
+        { transaction },
+      );
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  },
+};
