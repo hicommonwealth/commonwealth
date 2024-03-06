4b99b8c11d8d4e2c502116abded8023fca30d823
Timothee Legros <timothee@ualberta.ca>
2024-03-06 19:33:34 +0200

    added comment link updates

--- packages/commonwealth/server/migrations/20240306163523-sushi-image-fix.js
+++ packages/commonwealth/server/migrations/20240306163523-sushi-image-fix.js
@@ -29,6 +29,33 @@ module.exports = {
       `,
         { transaction },
       );
+
+      await queryInterface.sequelize.query(
+        `
+        UPDATE "Comments"
+        SET text = REPLACE(text, 'https%3A%2F%2Fglobal.discourse-cdn.com%2Fbusiness7%2Fuploads%2Fsushiswapclassic%2F', 'https%3A%2F%2Fassets.commonwealth.im%2Fsushiswapclassic%2F')
+        WHERE community_id = 'sushi';
+      `,
+        { transaction },
+      );
+
+      await queryInterface.sequelize.query(
+        `
+        UPDATE "Comments"
+        SET text = REPLACE(text, '%2Foptimized%2F', '%2Foriginal%2F')
+        WHERE community_id = 'sushi';
+      `,
+        { transaction },
+      );
+
+      await queryInterface.sequelize.query(
+        `
+        UPDATE "Threads"
+        SET text = REGEXP_REPLACE(text, '_\\d+_\\d+x\\d+(?=\\.\\w+)', '', 'g')
+        WHERE community_id = 'sushi';
+      `,
+        { transaction },
+      );
     });
   },
 
