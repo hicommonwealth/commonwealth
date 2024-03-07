4d3d678ea84d61c2b42844d0f626c956d279aa2b
Ryan Bennett <ryan@common.xyz>
2024-03-07 15:51:33 -0800

    add compute to migration

--- packages/commonwealth/server/migrations/20240307173953-add-community-thread-adresses-count.js
+++ packages/commonwealth/server/migrations/20240307173953-add-community-thread-adresses-count.js
@@ -24,6 +24,46 @@ module.exports = {
         },
         { transaction },
       );
+
+      // Set community thread_count
+      await queryInterface.sequelize.query(
+        `
+        ;with threadCntByCommunity AS (
+          SELECT
+            count(id) as cnt,
+            community_id
+          FROM "Threads"
+          WHERE deleted_at IS NULL
+          GROUP BY community_id
+        )
+
+        Update "Communities"
+        SET thread_count=cc.cnt
+        FROM threadCntByCommunity cc
+        where cc.community_id="Communities".id
+        `,
+        { transaction },
+      );
+
+      // Set community address_count
+      await queryInterface.sequelize.query(
+        `
+        ;with addressCntByCommunity AS (
+          SELECT
+            count(id) as cnt,
+            community_id
+          FROM "Addresses"
+          WHERE verified IS NOT NULL
+          GROUP BY community_id
+        )
+
+        Update "Communities"
+        SET address_count=cc.cnt
+        FROM addressCntByCommunity cc
+        where cc.community_id="Communities".id
+        `,
+        { transaction },
+      );
     });
   },
 
