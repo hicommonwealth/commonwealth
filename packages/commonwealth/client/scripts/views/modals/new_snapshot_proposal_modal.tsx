c7a430ffc6ee4f6947696fcd51a505dc22b9609b
israellund <israelmlund@gmail.com>
2024-03-06 23:34:17 -0500

    snapshot message now changes when space is changed in dropdown

--- packages/commonwealth/client/scripts/views/modals/new_snapshot_proposal_modal.tsx
+++ packages/commonwealth/client/scripts/views/modals/new_snapshot_proposal_modal.tsx
@@ -1,13 +1,13 @@
-import React, { useState, useEffect, useMemo } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 
 import type Thread from '../../models/Thread';
 import app from '../../state';
-import { NewSnapshotProposalForm } from '../pages/new_snapshot_proposal';
 import { CWDropdown } from '../components/component_kit/cw_dropdown';
 import {
   CWModalBody,
   CWModalHeader,
 } from '../components/component_kit/new_designs/CWModal';
+import { NewSnapshotProposalForm } from '../pages/new_snapshot_proposal';
 
 import '../../../styles/modals/new_snapshot_proposal_modal.scss';
 
@@ -23,8 +23,15 @@ export const NewSnapshotProposalModal = ({
   onModalClose,
 }: NewSnapshotProposalModalProps) => {
   const [selectedSnapshotId, setSelectedSnapshotId] = useState<string | null>(
-    null
+    null,
   );
+  const handleSelect = (item) => {
+    setSelectedSnapshotId(item.value);
+    const init = async () => {
+      await app.snapshot.init(item.value);
+    };
+    init();
+  };
 
   const snapshotOptions = useMemo(
     () =>
@@ -32,9 +39,9 @@ export const NewSnapshotProposalModal = ({
         value: snapshot,
         label: snapshot,
       })) || [],
-    []
+    [],
   );
-
+  console.log('snapshotOptions', snapshotOptions);
   useEffect(() => {
     if (snapshotOptions.length > 0) {
       setSelectedSnapshotId(snapshotOptions[0].value);
@@ -50,7 +57,8 @@ export const NewSnapshotProposalModal = ({
             <CWDropdown
               label="Select Snapshot Space"
               options={snapshotOptions}
-              onSelect={(item) => setSelectedSnapshotId(item.value)}
+              // onSelect={(item) => setSelectedSnapshotId(item.value)}
+              onSelect={handleSelect}
             />
             {selectedSnapshotId && (
               <NewSnapshotProposalForm
