554f6ea432b195e59d6bb4b002f67a63ec95db09
israellund <israelmlund@gmail.com>
2024-03-07 17:13:49 -0500

    cleaning up files from previous commit

--- packages/commonwealth/client/scripts/views/modals/ban_user_modal.tsx
+++ packages/commonwealth/client/scripts/views/modals/ban_user_modal.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 
 import { useBanProfileByAddressMutation } from 'state/api/profiles';
 import { CWText } from 'views/components/component_kit/cw_text';
-import '../../../styles/modals/ban_user_modal.scss';
 import {
   notifyError,
   notifySuccess,
