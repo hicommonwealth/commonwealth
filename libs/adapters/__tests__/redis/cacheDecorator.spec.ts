d063b3eda98d42504eedecc925155a11be0e6d70
Roger Torres <41707007+Rotorsoft@users.noreply.github.com>
2024-03-12 17:18:22 -0400

    Fix redis connection loop (#6994)
    * fix redis
    
    * set connect timeout to 5 secs
    
    * add cacheReady util
    
    * fix tests
    
    * fix chain tests
    
    * more time to ready
    
    * add test NODE_ENV
    
    * add seed
    
    * fix more tests
    
    * fix more tests
    
    * fix more tests
    
    * fix more tests
    
    * 25 minute devnet test timeout
    
    * set up ci nodes in notif devnet test
    
    * semantics
    
    * test cleanup
    
    * refactor redis cache
    
    * fix getkeys
    
    * seedDb with devnets for notif tests
    
    * fix tests
    
    * fix tests
    
    * update cosmosChainId for tbc test
    
    * remove reliance on COSMOS_GOV_V1 env var
    
    * async fix
    
    * async fix
    
    * gov version in integration tests
    
    * ethermint
    
    * update seedDb to include ethermint local devnet
    
    * missed seedDb()s
    
    * debug
    
    * set node env test
    
    * add logs
    
    * add REDIS_URL
    
    * nest cosmos gov v1 query tests
    
    * fix mixed up devnet endpoints
    
    * skip gov notif test for now
    
    * update tests to account for two chains
    
    * set timeouts
    
    * set timeouts
    
    ---------
    
    Co-authored-by: rotorsoft <rotorsoft@outlook.com>
    Co-authored-by: mhagel <mark.hagelberg@common.xyz>

--- libs/adapters/__tests__/redis/cacheDecorator.spec.ts
+++ libs/adapters/__tests__/redis/cacheDecorator.spec.ts
@@ -2,16 +2,11 @@
 /* eslint-disable dot-notation */
 /* eslint-disable no-unused-expressions */
 require('dotenv').config();
-import { CacheNamespaces, delay } from '@hicommonwealth/core';
+import { CacheNamespaces, cache, delay, dispose } from '@hicommonwealth/core';
 import chai from 'chai';
 import chaiHttp from 'chai-http';
-import express from 'express';
-import {
-  CacheDecorator,
-  RedisCache,
-  XCACHE_VALUES,
-  connectToRedis,
-} from '../../src/redis';
+import express, { RequestHandler, json } from 'express';
+import { CacheDecorator, RedisCache, XCACHE_VALUES } from '../../src/redis';
 import {
   CACHE_ENDPOINTS,
   setupCacheTestEndpoints,
@@ -20,7 +15,7 @@ import {
 chai.use(chaiHttp);
 const expect = chai.expect;
 const app = express();
-app.use(express.json());
+app.use(json() as RequestHandler);
 
 const content_type = {
   json: 'application/json; charset=utf-8',
@@ -53,10 +48,8 @@ async function makePostRequest(endpoint, body, headers = {}) {
 }
 
 describe('Cache Decorator', () => {
-  const redisCache: RedisCache = new RedisCache();
+  let cacheDecorator: CacheDecorator;
   const route_namespace: CacheNamespaces = CacheNamespaces.Route_Response;
-  const cacheDecorator = new CacheDecorator(redisCache);
-  setupCacheTestEndpoints(app, cacheDecorator);
 
   async function verifyCacheResponse(key, res, resEarlier) {
     expect(res).to.have.status(200);
@@ -65,18 +58,21 @@ describe('Cache Decorator', () => {
     expect(valFromRedis).to.not.be.null;
     if (key === CACHE_ENDPOINTS.JSON) {
       // to avoid unhandled exceptions when response is not json
-      expect(JSON.parse(valFromRedis)).to.be.deep.equal(res.body);
-      expect(JSON.parse(valFromRedis)).to.be.deep.equal(resEarlier.body);
+      expect(JSON.parse(valFromRedis!)).to.be.deep.equal(res.body);
+      expect(JSON.parse(valFromRedis!)).to.be.deep.equal(resEarlier.body);
     }
   }
 
   before(async () => {
-    await connectToRedis(redisCache);
+    cache(new RedisCache('redis://localhost:6379'));
+    cacheDecorator = new CacheDecorator();
+    setupCacheTestEndpoints(app, cacheDecorator);
+    await cache().ready();
   });
 
   after(async () => {
-    await redisCache.deleteNamespaceKeys(route_namespace);
-    await redisCache.closeClient();
+    await cache().deleteNamespaceKeys(route_namespace);
+    await dispose()();
   });
 
   it(`verify cache route ${CACHE_ENDPOINTS.TEXT} route and expire`, async () => {
