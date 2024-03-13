b392ec814752b9c91a7e4a5b0f7264cd3a3adadc
Malik Zulqurnain <malikzulqurnain2121@gmail.com>
2024-03-13 19:48:19 +0500

    Added community name and symbol search

--- packages/commonwealth/client/scripts/views/pages/MyCommunityStake/Transactions/Transactions.tsx
+++ packages/commonwealth/client/scripts/views/pages/MyCommunityStake/Transactions/Transactions.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import CommunityInfo from '../common/CommunityInfo';
 import { transactionHistoryData } from '../common/sampleData'; // TODO: get data from API
+import { FilterOptions } from '../types';
 import './Transactions.scss';
 import { CWIcon } from '/views/components/component_kit/cw_icons/cw_icon';
 import { CWTable } from '/views/components/component_kit/new_designs/CWTable';
@@ -58,12 +59,34 @@ const columnInfo = [
   },
 ];
 
-const Transactions = () => {
+type TransactionsProps = {
+  filterOptions: FilterOptions;
+};
+
+const Transactions = ({ filterOptions }: TransactionsProps) => {
+  const [filteredTransactionHistoryData, setFilteredTransactionHistoryData] =
+    useState(transactionHistoryData);
+
+  useEffect(() => {
+    let tempFilteredData = [...transactionHistoryData];
+
+    // check if community name and symbol contains 'searchText'
+    if (filterOptions.searchText) {
+      tempFilteredData = tempFilteredData.filter((tx) =>
+        (tx.community.symbol + tx.community.name)
+          .toLowerCase()
+          .includes(filterOptions.searchText.toLowerCase()),
+      );
+    }
+
+    setFilteredTransactionHistoryData(tempFilteredData);
+  }, [filterOptions.searchText]);
+
   return (
     <section className="Transactions">
       <CWTable
         columnInfo={columnInfo}
-        rowData={transactionHistoryData.map((tx) => ({
+        rowData={filteredTransactionHistoryData.map((tx) => ({
           ...tx,
           community: (
             <CommunityInfo
---
+++ packages/commonwealth/client/scripts/views/pages/MyCommunityStake/types.ts
@@ -0,0 +1,3 @@
+export type FilterOptions = {
+  searchText?: string;
+};
