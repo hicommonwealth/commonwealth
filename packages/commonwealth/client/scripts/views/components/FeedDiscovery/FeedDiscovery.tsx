8b35f6f7cf88d24b76a40cc803355c61bf53bc8e
Kevin Burton <burtonator@gmail.com>
2024-03-14 10:29:33 -0600

    fixed eslint issues...

--- packages/commonwealth/client/scripts/views/components/FeedDiscovery/FeedDiscovery.tsx
+++ packages/commonwealth/client/scripts/views/components/FeedDiscovery/FeedDiscovery.tsx
@@ -1,23 +1,26 @@
-import React from "react"
-import {Helmet} from "react-helmet";
+/* eslint-disable max-len */
+import React from 'react';
+import { Helmet } from 'react-helmet';
 
-export default function FeedDiscovery() {
-
+export const FeedDiscovery = () => {
   if (document.location.pathname.endsWith('/discussions')) {
-    const community = document.location.pathname.split('/')[0]
-    const url = new URL(document.location.href)
+    const community = document.location.pathname.split('/')[0];
+    const url = new URL(document.location.href);
 
-    const orderBy = url.searchParams.get('orderBy') ?? 'createdAt:desc'
-    const includePinnedThreads = url.searchParams.get('includePinnedThreads') ?? 'true'
+    const orderBy = url.searchParams.get('orderBy') ?? 'createdAt:desc';
+    const includePinnedThreads =
+      url.searchParams.get('includePinnedThreads') ?? 'true';
     return (
       <Helmet>
-        <link rel="alternate"
-              type="application/atom+xml"
-              title="ATOM" href={`/api/feed?bulk=true&page=1&limit=20&community_id=${community}&includePinnedThreads=${includePinnedThreads}&orderBy=${orderBy}`}/>
+        <link
+          rel="alternate"
+          type="application/atom+xml"
+          title="ATOM"
+          href={`/api/feed?bulk=true&page=1&limit=20&community_id=${community}&includePinnedThreads=${includePinnedThreads}&orderBy=${orderBy}`}
+        />
       </Helmet>
-    )
+    );
   }
 
-  return null
-
-}
+  return null;
+};
