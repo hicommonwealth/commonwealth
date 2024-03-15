27b838a888c630ed8f0209653831c4aea5d9400e
kassad <kurtis@commonwealth.im>
2024-03-15 10:16:39 -0700

    Fixed linter error

--- packages/commonwealth/client/scripts/views/pages/discussions/CommentCard/CommentCard.tsx
+++ packages/commonwealth/client/scripts/views/pages/discussions/CommentCard/CommentCard.tsx
@@ -90,7 +90,7 @@ export const CommentCard = ({
     useState<boolean>(false);
   const [verifiedAction, setVerifiedAction] = useState<Action>();
   const [verifiedSession, setVerifiedSession] = useState<Session>();
-  const [onReaction, setOnReaction] = useState<boolean>(false);
+  const [, setOnReaction] = useState<boolean>(false);
   const [isUpvoteDrawerOpen, setIsUpvoteDrawerOpen] = useState<boolean>(false);
 
   useEffect(() => {
