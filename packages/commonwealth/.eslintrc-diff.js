58cb08b12d6c4086f993be0a7edfb9aaf1c26d9c
Kevin Burton <burtonator@gmail.com>
2024-03-16 16:11:37 -0600

    working on making this production ready...

--- packages/commonwealth/.eslintrc-diff.js
+++ packages/commonwealth/.eslintrc-diff.js
@@ -12,9 +12,10 @@ const path = require('path');
  * git push origin my_branch
  *
  */
-process.env.ESLINT_PLUGIN_DIFF_COMMIT = 'origin/MASTER_CIRCA_2024_03_16'
+process.env.ESLINT_PLUGIN_DIFF_COMMIT = 'origin/MASTER_CIRCA_2024_03_16';
 
-const ENABLE_ESLINT_DIFF_PLUGIN = process.env.ENABLE_ESLINT_DIFF_PLUGIN || 'true'
+const ENABLE_ESLINT_DIFF_PLUGIN =
+  process.env.ENABLE_ESLINT_DIFF_PLUGIN || 'true';
 
 module.exports = {
   settings: {
@@ -23,15 +24,15 @@ module.exports = {
         config: path.resolve(__dirname, 'webpack/webpack.base.config.js'),
       },
     },
-    'react': {
-      'version': 'detect',
-    }
+    react: {
+      version: 'detect',
+    },
   },
   plugins: ['@tanstack/query'],
   // TODO: I won't be able to turn this on as it will break the normal dev workflow
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: `./tsconfig.json`
+    project: `./tsconfig.json`,
   },
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
@@ -44,7 +45,7 @@ module.exports = {
     'no-underscore-dangle': 0,
     'no-param-reassign': 0,
     'no-console': 0,
-    'camelcase': 0,
+    camelcase: 0,
     'no-else-return': 0,
     'no-unused-vars': 0,
     'no-unused-expressions': 0,
@@ -59,7 +60,7 @@ module.exports = {
     'object-curly-newline': 0,
     'nonblock-statement-body-position': 0,
     'no-extraneous-dependencies': 0,
-    'curly': 0,
+    curly: 0,
     'no-nested-ternary': 0,
     'import/first': 0,
     'import/no-extraneous-dependencies': 0,
@@ -107,45 +108,56 @@ module.exports = {
       },
     ],
     '@tanstack/query/exhaustive-deps': 'error',
-    "@tanstack/query/prefer-query-object-syntax": "error",
+    '@tanstack/query/prefer-query-object-syntax': 'error',
     'react/destructuring-assignment': [1, 'always'],
-    'react/function-component-definition': [1, { "namedComponents": "arrow-function" }],
-    'react/no-multi-comp': [1, { "ignoreStateless": false }],
-    'react/jsx-curly-brace-presence': ['error', { props: "never", children: "never" }],
+    'react/function-component-definition': [
+      1,
+      { namedComponents: 'arrow-function' },
+    ],
+    'react/no-multi-comp': [1, { ignoreStateless: false }],
+    'react/jsx-curly-brace-presence': [
+      'error',
+      { props: 'never', children: 'never' },
+    ],
     '@typescript-eslint/no-unused-vars': 1,
     'react/jsx-key': 'error',
-    "no-restricted-imports": ["error", {
-      patterns: [{
-        group: [
-          "@hicommonwealth/core/**",
-          "@hicommonwealth/adapters/**",
-          "@hicommonwealth/model/**"
+    'no-restricted-imports': [
+      'error',
+      {
+        patterns: [
+          {
+            group: [
+              '@hicommonwealth/core/**',
+              '@hicommonwealth/adapters/**',
+              '@hicommonwealth/model/**',
+            ],
+            message:
+              "Avoid importing from 'lib' directories. Import from the main entry point instead.",
+          },
         ],
-        message: "Avoid importing from 'lib' directories. Import from the main entry point instead.",
-      }]
-    }],
+      },
+    ],
 
     // TODO: needs parser services and when these are on we run out of memory
-    "@typescript-eslint/no-floating-promises": "error",
-    "@typescript-eslint/require-await": "error",
-    "@typescript-eslint/no-misused-promises": "error",
-    "@typescript-eslint/await-thenable": "error",
+    '@typescript-eslint/no-floating-promises': 'error',
+    '@typescript-eslint/require-await': 'error',
+    '@typescript-eslint/no-misused-promises': 'error',
+    '@typescript-eslint/await-thenable': 'error',
 
     // ** 'any' issues...
-    "@typescript-eslint/no-explicit-any": "error",
-    "@typescript-eslint/no-unsafe-argument": "error",
-    "@typescript-eslint/no-unsafe-assignment": "error",
-    "@typescript-eslint/no-unsafe-call": "error",
-    "@typescript-eslint/no-unsafe-member-access": "error",
-    "@typescript-eslint/no-unsafe-return": "error",
-
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/no-unsafe-argument': 'error',
+    '@typescript-eslint/no-unsafe-assignment': 'error',
+    '@typescript-eslint/no-unsafe-call': 'error',
+    '@typescript-eslint/no-unsafe-member-access': 'error',
+    '@typescript-eslint/no-unsafe-return': 'error',
   },
   ignorePatterns: ['server/scripts/setupPrerenderService.ts'],
   extends: [
-    "eslint:recommended",
+    'eslint:recommended',
     'plugin:@tanstack/eslint-plugin-query/recommended',
-    "plugin:react-hooks/recommended",
-    "plugin:react/recommended",
+    'plugin:react-hooks/recommended',
+    'plugin:react/recommended',
     ENABLE_ESLINT_DIFF_PLUGIN !== 'false' ? 'plugin:diff/diff' : null,
-  ].filter(current => current !== null),
-}
+  ].filter((current) => current !== null),
+};
