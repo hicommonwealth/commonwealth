255bb3dc6984d6a28d9e7c99fe3558afb38aba30
Timothee Legros <timothee@ualberta.ca>
2024-03-19 20:38:17 +0200

    pg-listen

--- packages/commonwealth/server/workers/messageRelayer/messageRelayer.ts
+++ packages/commonwealth/server/workers/messageRelayer/messageRelayer.ts
@@ -1,7 +1,15 @@
-// set health check
+import {
+  PinoLogger,
+  ServiceKey,
+  startHealthCheckLoop,
+} from '@hicommonwealth/adapters';
+import { logger } from '@hicommonwealth/core';
+import { DATABASE_URI } from '@hicommonwealth/model';
+import createSubscriber from 'pg-listen';
 
-import { ServiceKey, startHealthCheckLoop } from '@hicommonwealth/adapters';
+const log = logger(PinoLogger()).getLogger(__filename);
 
+const OUTBOX_CHANNEL = 'outbox-channel';
 let isServiceHealthy = false;
 
 startHealthCheckLoop({
@@ -13,6 +21,38 @@ startHealthCheckLoop({
     }
   },
 });
+
+const subscriber = createSubscriber({ connectionString: DATABASE_URI });
+
+subscriber.events.on('connected', () => {
+  log.info('Message relayer connected to Postgres');
+});
+
+subscriber.events.on('reconnect', () => {
+  log.warn('Message relayer reconnecting to Postgres');
+});
+subscriber.notifications.on(OUTBOX_CHANNEL, async (payload) => {
+  relay();
+});
+
+subscriber.events.on('error', (error) => {
+  log.fatal('Fatal database connection error:', error);
+  isServiceHealthy = false;
+  // restart dyno after multiple reconnection failures
+  process.exit(1);
+});
+
+process.on('exit', () => {
+  subscriber.close();
+});
+
+async function connectToPostgres() {
+  await subscriber.connect();
+  await subscriber.listenTo(OUTBOX_CHANNEL);
+  isServiceHealthy = true;
+}
+
+async function relay() {}
 
 async function main() {
   // init broker port (rabbitMQ)
