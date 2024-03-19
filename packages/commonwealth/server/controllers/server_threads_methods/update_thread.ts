c60cf2a65a82da48be7366c01dbde00713644ea5
kassad <kurtis@commonwealth.im>
2024-03-19 10:21:05 -0700

    Fixed collaborator issue

--- packages/commonwealth/server/controllers/server_threads_methods/update_thread.ts
+++ packages/commonwealth/server/controllers/server_threads_methods/update_thread.ts
@@ -269,10 +269,45 @@ export async function __updateThread(
   const finalThread = await this.models.Thread.findOne({
     where: { id: thread.id },
     include: [
-      { model: this.models.Address, as: 'Address' },
+      {
+        model: this.models.Address,
+        as: 'Address',
+        include: [
+          {
+            model: this.models.User,
+            as: 'User',
+            required: true,
+            attributes: ['id'],
+            include: [
+              {
+                model: this.models.Profile,
+                as: 'Profiles',
+                required: true,
+                attributes: ['id', 'avatar_url', 'profile_name'],
+              },
+            ],
+          },
+        ],
+      },
       {
         model: this.models.Address,
         as: 'collaborators',
+        include: [
+          {
+            model: this.models.User,
+            as: 'User',
+            required: true,
+            attributes: ['id'],
+            include: [
+              {
+                model: this.models.Profile,
+                as: 'Profiles',
+                required: true,
+                attributes: ['id', 'avatar_url', 'profile_name'],
+              },
+            ],
+          },
+        ],
       },
       { model: this.models.Topic, as: 'topic' },
     ],
