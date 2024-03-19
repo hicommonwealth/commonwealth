5fee2c4a950ec5f879fc32312a4b9e814bcb4d0c
Marcin <maslankam92@gmail.com>
2024-03-19 12:59:13 +0100

    Add grid to directory page

--- packages/commonwealth/client/scripts/views/pages/DirectoryPage/DirectoryPage.tsx
+++ packages/commonwealth/client/scripts/views/pages/DirectoryPage/DirectoryPage.tsx
@@ -8,6 +8,7 @@ import { useDebounce } from 'usehooks-ts';
 import { CWDivider } from 'views/components/component_kit/cw_divider';
 import { CWIconButton } from 'views/components/component_kit/cw_icon_button';
 import { CWText } from 'views/components/component_kit/cw_text';
+import CWPageLayout from 'views/components/component_kit/new_designs/CWPageLayout';
 import { CWTextInput } from 'views/components/component_kit/new_designs/CWTextInput';
 import { CWButton } from 'views/components/component_kit/new_designs/cw_button';
 import DirectoryPageContent from 'views/pages/DirectoryPage/DirectoryPageContent';
@@ -63,70 +64,72 @@ const DirectoryPage = () => {
   }
 
   return (
-    <div className="DirectoryPage">
-      <div className="header-row">
-        <CWText type="h2"> Directory</CWText>
-        <CWButton
-          label="Create community"
-          iconLeft="plus"
-          onClick={handleCreateCommunity}
-        />
-      </div>
-      <CWText className="page-description">
-        Access relevant communities linked to your base chain for a connected
-        experience.
-      </CWText>
-      <CWDivider />
-      <CWText type="h4" className="subtitle">
-        {baseChain?.name} ecosystem
-      </CWText>
-      <div className="search-row">
-        <div className="community-search">
-          <CWTextInput
-            value={communitySearch}
-            onInput={(e: any) => setCommunitySearch(e.target.value)}
-            fullWidth
-            placeholder="Search communities"
-            iconLeft={<MagnifyingGlass size={24} weight="regular" />}
+    <CWPageLayout>
+      <div className="DirectoryPage">
+        <div className="header-row">
+          <CWText type="h2"> Directory</CWText>
+          <CWButton
+            label="Create community"
+            iconLeft="plus"
+            onClick={handleCreateCommunity}
           />
         </div>
-        <div className="toggle-view-icons">
-          <div
-            className={clsx('icon-container', {
-              selected: selectedViewType === ViewType.Rows,
-            })}
-          >
-            <CWIconButton
-              onClick={() => setSelectedViewType(ViewType.Rows)}
-              iconName="rows"
-              weight="light"
+        <CWText className="page-description">
+          Access relevant communities linked to your base chain for a connected
+          experience.
+        </CWText>
+        <CWDivider />
+        <CWText type="h4" className="subtitle">
+          {baseChain?.name} ecosystem
+        </CWText>
+        <div className="search-row">
+          <div className="community-search">
+            <CWTextInput
+              value={communitySearch}
+              onInput={(e: any) => setCommunitySearch(e.target.value)}
+              fullWidth
+              placeholder="Search communities"
+              iconLeft={<MagnifyingGlass size={24} weight="regular" />}
             />
           </div>
-          <div
-            className={clsx('icon-container', {
-              selected: selectedViewType === ViewType.Tiles,
-            })}
-          >
-            <CWIconButton
-              onClick={() => setSelectedViewType(ViewType.Tiles)}
-              iconName="squaresFour"
-              weight="light"
-            />
+          <div className="toggle-view-icons">
+            <div
+              className={clsx('icon-container', {
+                selected: selectedViewType === ViewType.Rows,
+              })}
+            >
+              <CWIconButton
+                onClick={() => setSelectedViewType(ViewType.Rows)}
+                iconName="rows"
+                weight="light"
+              />
+            </div>
+            <div
+              className={clsx('icon-container', {
+                selected: selectedViewType === ViewType.Tiles,
+              })}
+            >
+              <CWIconButton
+                onClick={() => setSelectedViewType(ViewType.Tiles)}
+                iconName="squaresFour"
+                weight="light"
+              />
+            </div>
           </div>
         </div>
-      </div>
 
-      <DirectoryPageContent
-        isLoading={isLoading}
-        noFilteredCommunities={noFilteredCommunities}
-        noCommunitiesInChain={noCommunitiesInChain}
-        chainName={baseChain?.name}
-        communitySearch={communitySearch}
-        filteredRelatedCommunitiesData={filteredRelatedCommunitiesData}
-        tableData={tableData}
-        selectedViewType={selectedViewType}
-      />
-    </div>
+        <DirectoryPageContent
+          isLoading={isLoading}
+          noFilteredCommunities={noFilteredCommunities}
+          noCommunitiesInChain={noCommunitiesInChain}
+          chainName={baseChain?.name}
+          communitySearch={communitySearch}
+          filteredRelatedCommunitiesData={filteredRelatedCommunitiesData}
+          tableData={tableData}
+          selectedViewType={selectedViewType}
+        />
+      </div>
+    </CWPageLayout>
   );
 };
 
