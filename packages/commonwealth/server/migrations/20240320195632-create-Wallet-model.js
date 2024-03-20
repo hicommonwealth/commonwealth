52e57987ee308aabfe7661b06805e424be7a7c5a
ianrowan <i.rowan299@gmail.com>
2024-03-20 15:01:36 -0500

    create New Model

---
+++ packages/commonwealth/server/migrations/20240320195632-create-Wallet-model.js
@@ -0,0 +1,43 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Wallets', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Users',
+          key: 'id',
+        },
+        allowNull: false,
+      },
+      user_address: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      relay_address: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      wallet_address: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Wallet');
+  },
+};
