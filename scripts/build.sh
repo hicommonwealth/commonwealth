37c4183aed25e9df97ac586000f2f20a6c9d29b3
Kevin Burton <burtonator@gmail.com>
2024-03-25 09:58:04 -0600

    build all other modules first.

--- scripts/build.sh
+++ scripts/build.sh
@@ -10,5 +10,9 @@ elif [ "$DL_BUILD" = true ]; then
   yarn workspace discord-bot build
 else
   NODE_OPTIONS=--max_old_space_size=4096 yarn workspace commonwealth bundle
+  yarn workspace @hicommonwealth/core build
+  yarn workspace @hicommonwealth/model build
+  yarn workspace @hicommonwealth/chains build
+  yarn workspace @hicommonwealth/adapters build
   yarn workspace commonwealth build
 fi
