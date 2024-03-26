92e4c7cc412c38f61f20404077a06a6004291633
Kevin Burton <burtonator@gmail.com>
2024-03-26 09:26:53 -0600

    I have to merge master into this branch but I think this should work...

---
+++ packages/commonwealth/test/canary/eslint_canary.ts
@@ -0,0 +1,5 @@
+// An eslint 'canary' that verifies that eslint-diff properly handles errors
+// during the build.  If we don't actually *test* that we fail properly then
+// it's possible we added an eslint rule that doesn't actually do anything
+
+// ***** import from outside the project root
