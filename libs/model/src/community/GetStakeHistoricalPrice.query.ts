436a27b0a06ed4e5657cf691c38e70c44075f55b
kassad <kurtis@commonwealth.im>
2024-03-28 10:29:11 -0700

    Fixed price history logic

--- libs/model/src/community/GetStakeHistoricalPrice.query.ts
+++ libs/model/src/community/GetStakeHistoricalPrice.query.ts
@@ -16,11 +16,11 @@ export const GetStakeHistoricalPrice: Query<
         SELECT s.community_id, (s.stake_price / s.stake_amount::REAL)::TEXT as old_price
         FROM "StakeTransactions" s
         INNER JOIN (
-            SELECT community_id, MAX(timestamp) AS latest_timestamp
+            SELECT community_id, MIN(timestamp) AS latest_timestamp
             FROM "StakeTransactions"
             WHERE stake_id = :stake_id
             AND stake_direction = 'buy'
-            AND timestamp <= :past_date_epoch
+            AND timestamp >= :past_date_epoch
             ${community_id ? 'AND community_id = :community_id' : ''}
             GROUP BY community_id
         ) AS latest ON s.community_id = latest.community_id AND s.timestamp = latest.latest_timestamp
