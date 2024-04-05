4a99af5e637dc5fc7e7b7fa67ac3bcf2c0be91b6
Malik Zulqurnain <malikzulqurnain2121@gmail.com>
2024-04-05 20:45:50 +0500

    Fix import

--- packages/commonwealth/client/scripts/views/modals/AuthModal/MobileWalletConfirmationSubModal/MobileWalletConfirmationSubModal.tsx
+++ packages/commonwealth/client/scripts/views/modals/AuthModal/MobileWalletConfirmationSubModal/MobileWalletConfirmationSubModal.tsx
@@ -1,4 +1,4 @@
-import { CWButton } from 'client/scripts/views/components/component_kit/new_designs/cw_button';
+import { CWButton } from 'client/scripts/views/components/component_kit/new_designs/CWButton';
 import React from 'react';
 import { CWIcon } from '../../../components/component_kit/cw_icons/cw_icon';
 import { CWText } from '../../../components/component_kit/cw_text';
