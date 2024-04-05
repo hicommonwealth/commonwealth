68aa4ffc999d742007c9c0e85e35a69ea582681f
ianrowan <i.rowan299@gmail.com>
2024-04-05 10:45:37 -0500

    update version and refactor

--- packages/commonwealth/client/scripts/views/components/proposals/voting_results.tsx
+++ packages/commonwealth/client/scripts/views/components/proposals/voting_results.tsx
@@ -18,7 +18,7 @@ import {
   useAaveProposalVotesQuery,
   useCompoundProposalVotesQuery,
 } from 'state/api/proposals';
-import Web3 from 'web3-utils';
+import { Web3 } from 'web3';
 import {
   AaveVotingResult,
   CompletedProposalVotingResult,
@@ -127,7 +127,7 @@ export const VotingResults = (props: VotingResultsProps) => {
     );
 
     const yesBalanceString = `${formatNumberLong(
-      +Web3.fromWei(yesBalance.toString()),
+      +Web3.utils.fromWei(yesBalance.toString(), 'ether'),
     )} ${app.chain.meta.default_symbol}`;
 
     const noVotes: AaveProposalVote[] = votes.filter((v) => !v.choice);
@@ -138,7 +138,7 @@ export const VotingResults = (props: VotingResultsProps) => {
     );
 
     const noBalanceString = `${formatNumberLong(
-      +Web3.fromWei(noBalance.toString()),
+      +Web3.utils.fromWei(noBalance.toString(), 'ether'),
     )} ${app.chain.meta.default_symbol}`;
 
     return (
