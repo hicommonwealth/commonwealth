d9f2039fa2bfa36dd41442b010cd791e6fe3325f
Malik Zulqurnain <51641047+mzparacha@users.noreply.github.com>
2024-04-05 13:03:33 +0500

    Aggregate stakes transaction history per community per address (#7353)
    * Aggregate stakes transaction history per community per address
    
    * Updated var names
    
    * Filter stakeholdings with 0 stake value
    
    * Updated logic to get dynamic stake symbol based on tx community chain

--- packages/commonwealth/client/scripts/views/pages/MyCommunityStake/Stakes/Stakes.tsx
+++ packages/commonwealth/client/scripts/views/pages/MyCommunityStake/Stakes/Stakes.tsx
@@ -1,5 +1,8 @@
+import { WEI_PER_ETHER } from 'controllers/chain/ethereum/util';
 import { formatAddressShort } from 'helpers';
+import { getCommunityStakeSymbol } from 'helpers/stakes';
 import React from 'react';
+import app from 'state';
 import { CWTooltip } from 'views/components/component_kit/new_designs/CWTooltip';
 import useTransactionHistory, {
   TransactionHistoryProps,
@@ -53,11 +56,54 @@ const columnInfo = [
 const Stakes = ({ filterOptions, addressFilter }: TransactionHistoryProps) => {
   const data = useTransactionHistory({ filterOptions, addressFilter });
 
+  // aggregate transaction per community per address
+  const stakes = (() => {
+    const accumulatedStakes = {};
+
+    data.map((transaction) => {
+      const key = (
+        transaction.community.id + transaction.address
+      ).toLowerCase();
+      const action = transaction.action === 'mint' ? 1 : -1;
+
+      accumulatedStakes[key] = {
+        ...transaction,
+        ...(accumulatedStakes[key] || {}),
+        stake:
+          (accumulatedStakes[key]?.stake || 0) + transaction.stake * action,
+        voteWeight:
+          (accumulatedStakes[key]?.voteWeight || 0) +
+          transaction.voteWeight * action,
+        avgPrice:
+          (accumulatedStakes[key]?.avgPrice || 0) +
+          parseFloat(
+            (
+              parseFloat(transaction.price) /
+              WEI_PER_ETHER /
+              transaction.stake
+            ).toFixed(5),
+          ) *
+            action,
+      };
+    });
+
+    return Object.values(accumulatedStakes)
+      .map((transaction: any) => ({
+        ...transaction,
+        voteWeight: transaction.voteWeight + 1, // total vote weight is +1 of the stake weight
+        avgPrice: `${transaction.avgPrice.toFixed(5)} ${getCommunityStakeSymbol(
+          app.config.chains.getById(transaction?.community?.id)?.ChainNode
+            ?.name || '',
+        )}`,
+      }))
+      .filter((transaction) => transaction.stake);
+  })();
+
   return (
     <section className="Stakes">
       <CWTable
         columnInfo={columnInfo}
-        rowData={data.map((tx) => ({
+        rowData={stakes.map((tx) => ({
           ...tx,
           community: {
             sortValue: tx.community.name.toLowerCase(),
