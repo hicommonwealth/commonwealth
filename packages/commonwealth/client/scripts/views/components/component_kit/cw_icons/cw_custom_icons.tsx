312bba3280f56d166c9b7a95cf4746555546ffb5
lzach83 <lzach83@gmail.com>
2024-04-08 10:20:51 -0400

    more ts error

--- packages/commonwealth/client/scripts/views/components/component_kit/cw_icons/cw_custom_icons.tsx
+++ packages/commonwealth/client/scripts/views/components/component_kit/cw_icons/cw_custom_icons.tsx
@@ -244,7 +244,7 @@ export const CWBlast = ({
   iconSize,
   className,
   ...otherProps
-}) => {
+}: CustomIconProps) => {
   return (
     <svg
       {...otherProps}
@@ -256,7 +256,7 @@ export const CWBlast = ({
       xmlns="http://www.w3.org/2000/svg"
       xmlnsXlink="http://www.w3.org/1999/xlink"
     >
-      <g clip-path="url(#clip0_465_7874)">
+      <g clipPath="url(#clip0_465_7874)">
         <path
           d="M6 12C9.31371 12 12 9.31371 12 6C12 2.68629 9.31371 0 6 0C2.68629 0 0 2.68629 0 6C0 9.31371 2.68629 12 6 12Z"
           fill="black"
