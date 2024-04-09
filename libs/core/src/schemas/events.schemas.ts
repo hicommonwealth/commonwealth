912665940cbf67cfcb1e0d326fc97465113c51e3
rotorsoft <rotorsoft@outlook.com>
2024-04-09 10:20:52 -0400

    refine schemas

--- libs/core/src/schemas/events.schemas.ts
+++ libs/core/src/schemas/events.schemas.ts
@@ -47,60 +47,51 @@ export const DiscordMessageCreated = z.object({
   ]),
 });
 
-// All events carry this common metadata
+// All events should carry this common metadata
 export const EventMetadata = z.object({
-  createdAt: z.date(),
+  createdAt: z.date().describe('When the event was emitted'),
   // TODO: TBD
-  // aggregateType: z.enum(Aggregates)
-  // aggregateId: z.string()
-  // correlation: z.string()
-  // causation: z.object({})
+  // aggregateType: z.enum(Aggregates).describe("Event emitter aggregate type")
+  // aggregateId: z.string().describe("Event emitter aggregate id")
+  // correlation: z.string().describe("Event correlation key")
+  // causation: z.object({}).describe("Event causation")
 });
-
-// event NewContest (
-//     address contest,
-//     address namespace,
-//     uint256 interval,
-//     bool oneOff
-//   ); // from Namespace factory
-
-// event NewContestStarted(
-//         uint256 indexed contestId,
-//         uint256 startTime,
-//         uint256 endTime
-//     ); // from contest manager address
-
-// event ContentAdded(
-//         uint256 indexed contentId,
-//         address indexed creator,
-//         string url
-//     ); // from contest manager address
 
 // on-chain contest manager events
 export const ContestManagerDeployed = EventMetadata.extend({
   namespace: z.string().describe('Community namespace'),
-  contestAddress: z.string().describe('Contest manager on-chain address'),
+  contest: z.string().describe('Contest manager address'),
+  interval: z.number().int().positive().describe('Recurring constest interval'),
+  oneOff: z.boolean().describe('Flags one-off contests'),
 }).describe('When a contest contract manager gets deployed');
 
 export const ContestStarted = EventMetadata.extend({
-  namespace: z.string().describe('Community namespace'),
-  contestAddress: z.string().describe('Contest manager on-chain address'),
-  contestId: z.number().describe('New on-chain contest id'),
+  contest: z.string().describe('Contest manager address'),
+  contestId: z.number().int().describe('New contest instance id'),
+  startTime: z.date().describe('Contest start time'),
+  endTime: z.date().describe('Contest end time'),
 }).describe('When a contest instance gets started');
 
 export const ContestContentAdded = EventMetadata.extend({
-  contestId: z.number().describe('On-chain contest id'),
-  contentId: z.number().describe('New on-chain content id'),
+  contest: z.string().describe('Contest manager address'),
+  contestId: z.number().describe('Contest instance id'),
+  contentId: z.number().describe('New content id'),
+  creator: z.string().describe('Address of content creator'),
+  url: z.string().describe('Content url'),
 }).describe('When new content is added to a running contest');
 
 export const ContestContentUpvoted = EventMetadata.extend({
-  contestId: z.number().describe('On-chain contest id'),
-  contentId: z.number().describe('New on-chain content id'),
+  contest: z.string().describe('Contest manager address'),
+  contestId: z.number().describe('Contest instance id'),
+  contentId: z.number().describe('New content id'),
   address: z.string().describe('Address upvoting on content'),
   weight: z.number().describe('Stake weight of address upvoting on content'),
 }).describe('When users upvote content on running contest');
 
 export const ContestWinnersRecorded = EventMetadata.extend({
-  contestId: z.number().describe('On-chain contest id'),
-  winners: z.array(z.string()).describe('Ranked contest winning addresses'),
+  contest: z.string().describe('Contest manager address'),
+  contestId: z.number().describe('Contest instance id'),
+  winners: z
+    .array(z.string())
+    .describe('Ranked contest-winning creator addresses'),
 }).describe('When contest winners are recorded and contest ends');
--- libs/core/src/schemas/projections/contest.schemas.ts
+++ libs/core/src/schemas/projections/contest.schemas.ts
@@ -1,5 +1,43 @@
 import { z } from 'zod';
 
-export const ContestManager = z.object({});
-export const Contest = z.object({});
-export const ContestAction = z.object({});
+export const ContestManager = z
+  .object({
+    communityId: z.number().int(),
+    address: z.string().describe('On-Chain contest manager address'),
+    interval: z
+      .number()
+      .int()
+      .positive()
+      .describe('Recurring contest interval, 0 when one-off'),
+    createdAt: z.date(),
+  })
+  .describe('On-Chain Contest Manager');
+
+export const Contest = z
+  .object({
+    id: z.number().int().describe('On-Chain contest id'),
+    contest: z.string().describe('On-Chain contest manager address'),
+    startTime: z.date(),
+    endTime: z.date(),
+  })
+  .describe('On-Chain contest instance');
+
+export const CONSTEST_ACTIONS = ['added', 'upvoted'] as const;
+
+export const ContestAction = z
+  .object({
+    id: z.number().int().describe('On-Chain contest id'),
+    contentId: z.number().int().describe('On-Chain content id'),
+    address: z.string().describe('Actor address'),
+    action: z.enum(CONSTEST_ACTIONS).describe('Type of content action'),
+    contentUrl: z.string().url().describe('Content url'),
+    weight: z
+      .number()
+      .describe('Stake weight of address when action was recorded'),
+    createdAt: z.date().describe('Date-time when action was recorded'),
+    winners: z
+      .array(z.string())
+      .describe('Ranked contest-winning creator addresses')
+      .optional(),
+  })
+  .describe('On-Chain content related actions on contest instance');
