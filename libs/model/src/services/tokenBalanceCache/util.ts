e7a62c39fcaf13f7c83f94db1f58bc0cd78c8909
Raymond Z <raykyri@gmail.com>
2024-04-09 11:58:28 -0400

    wip: deal with some potentially cjs module imports

--- libs/model/src/services/tokenBalanceCache/util.ts
+++ libs/model/src/services/tokenBalanceCache/util.ts
@@ -1,10 +1,10 @@
 import { HttpBatchClient, Tendermint34Client } from '@cosmjs/tendermint-rpc';
 import { logger } from '@hicommonwealth/core';
 import AbiCoder from 'web3-eth-abi';
-import { toBN } from 'web3-utils';
 import { ChainNodeAttributes } from '../../models/chain_node';
 import { Balances, GetTendermintClientOptions } from './types';
 
+const toBN = require('web3-utils');
 const log = logger().getLogger(__filename);
 
 /**
