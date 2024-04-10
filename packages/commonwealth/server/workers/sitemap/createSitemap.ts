29b2fb0ab8a913e96ccb18e92622e65fcabc389b
Kevin Burton <burtonator@gmail.com>
2024-04-10 16:05:41 -0600

    writing out the sitemap file...

--- packages/commonwealth/server/workers/sitemap/createSitemap.ts
+++ packages/commonwealth/server/workers/sitemap/createSitemap.ts
@@ -1,25 +1,5 @@
+import { createBuffer } from './createBuffer';
 import { Link } from './createPaginator';
-
-/**
- * Creates a buffer object that allows appending data and converting the buffer to a string.
- *
- * @returns {Object} The buffer object.
- * @property {function} append - Appends the provided data to the buffer.
- * @property {function} toString - Converts the buffer to a string.
- */
-function createBuffer() {
-  let buff = '';
-
-  function append(data: string) {
-    buff += data;
-  }
-
-  function toString() {
-    return buff;
-  }
-
-  return { append, toString };
-}
 
 /**
  * Creates a sitemap XML string based on the provided links array.
--- packages/commonwealth/server/workers/sitemap/createSitemapGenerator.ts
+++ packages/commonwealth/server/workers/sitemap/createSitemapGenerator.ts
@@ -1,32 +1,48 @@
 import { AsyncWriter } from './createAsyncWriter';
 import { Paginator } from './createPaginator';
 import { createSitemap } from './createSitemap';
+import { createSitemapIndex } from './createSitemapIndex';
 
 export interface SitemapFile {
   readonly location: string;
 }
 
+export interface SitemapManifest {
+  readonly index: SitemapFile;
+  readonly children: ReadonlyArray<SitemapFile>;
+}
+
 export interface SitemapGenerator {
-  readonly exec: () => Promise<ReadonlyArray<SitemapFile>>;
+  readonly exec: () => Promise<SitemapManifest>;
 }
 
 export function createSitemapGenerator(
   writer: AsyncWriter,
   paginator: Paginator,
 ): SitemapGenerator {
-  async function exec() {
+  async function exec(): Promise<SitemapManifest> {
     let idx = 0;
 
-    let written = [];
+    let children = [];
 
     while (await paginator.hasNext()) {
       const page = await paginator.next();
       const sitemap = createSitemap(page.links);
       const res = await writer.write(`sitemap-${idx++}.xml`, sitemap);
-      written.push({ location: res.location });
+      children.push({ location: res.location });
     }
 
-    return written;
+    async function writeIndex() {
+      const index = createSitemapIndex(
+        children.map((current) => current.location),
+      );
+      const res = await writer.write(`sitemap-index.xml`, index);
+      return { location: res.location };
+    }
+
+    const index = await writeIndex();
+
+    return { index, children };
   }
 
   return { exec };
