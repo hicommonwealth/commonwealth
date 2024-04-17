86fcbb1a9f0b5d9f0ec181842cf16dd31b4a839a
Raymond Z <raykyri@gmail.com>
2024-04-17 17:07:35 -0400

    fix stub esm modules issue

--- libs/model/src/services/commonProtocol/index.ts
+++ libs/model/src/services/commonProtocol/index.ts
@@ -1,3 +1,13 @@
 export * as communityStakeConfigValidator from './communityStakeConfigValidator';
-export * as contractHelpers from './contractHelpers';
 export * as newNamespaceValidator from './newNamespaceValidator';
+import * as contractHelpersModule from './contractHelpers';
+
+// Export contractHelpers as an object and not an ES Module,
+// so it can be stubbed in tests
+const contractHelpers = { ...contractHelpersModule };
+
+export {
+  communityStakeConfigValidator,
+  contractHelpers,
+  newNamespaceValidator,
+};
