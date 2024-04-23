0946216930ad3683b1593302748e191c5a0d35b4
kassad <kurtis@commonwealth.im>
2024-04-23 08:02:58 -0700

    Fixed PR comment

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,9 +8,9 @@ if [ "$SL_BUILD" = true ]; then
   yarn workspace snapshot-listener build
 elif [ "$DL_BUILD" = true ]; then
   yarn workspace discord-bot build
-elif [ "$NO_WEBPACK" = true ]; then
-  yarn workspace commonwealth build
 else
-  NODE_OPTIONS=--max_old_space_size=4096 yarn workspace commonwealth bundle
+  if [ -z "$NO_WEBPACK" ]; then
+    NODE_OPTIONS=--max_old_space_size=4096 yarn workspace commonwealth bundle
+  fi
   yarn workspace commonwealth build
 fi
