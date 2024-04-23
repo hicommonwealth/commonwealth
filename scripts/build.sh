5f9053c401cadfb413c0a6a30ebac8defa4883e2
kassad <kurtis@commonwealth.im>
2024-04-23 06:33:10 -0700

    Fixed CI so that we only bundle when necessary

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,6 +8,8 @@ if [ "$SL_BUILD" = true ]; then
   yarn workspace snapshot-listener build
 elif [ "$DL_BUILD" = true ]; then
   yarn workspace discord-bot build
+elif [ "$NO_WEBPACK" = true ]; then
+  yarn workspace commonwealth build
 else
   NODE_OPTIONS=--max_old_space_size=4096 yarn workspace commonwealth bundle
   yarn workspace commonwealth build
