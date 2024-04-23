0c19e1806bc776b38d8c522fb8ed2b46a5a76f84
Marcin <maslankam92@gmail.com>
2024-04-23 12:28:36 +0200

    Add admin contets page

--- packages/commonwealth/client/scripts/views/pages/CommunityManagement/Contests/ContestsList/ContestsList.tsx
+++ packages/commonwealth/client/scripts/views/pages/CommunityManagement/Contests/ContestsList/ContestsList.tsx
@@ -1,77 +1,69 @@
 import React from 'react';
 
-import { useCommonNavigate } from 'navigation/helpers';
-import app from 'state';
-import Permissions from 'utils/Permissions';
-import { CWText } from 'views/components/component_kit/cw_text';
-import { CWButton } from 'views/components/component_kit/new_designs/CWButton';
-import CWPageLayout from 'views/components/component_kit/new_designs/CWPageLayout';
-import { PageNotFound } from 'views/pages/404';
+import { Skeleton } from 'views/components/Skeleton';
 
 import EmptyContestsList from '../EmptyContestsList';
-import useCommunityContests from '../useCommunityContests';
 import ContestCard from './ContestCard';
 
 import './ContestsList.scss';
 
-const ContestsList = () => {
-  const navigate = useCommonNavigate();
-
-  const isAdmin = Permissions.isSiteAdmin() || Permissions.isCommunityAdmin();
-
-  const { stakeEnabled, contestsData, isContestAvailable } =
-    useCommunityContests();
-
-  if (!app.isLoggedIn() || !isAdmin) {
-    return <PageNotFound />;
+interface ContestsListProps {
+  contests: {
+    id: number;
+    name: string;
+    imageUrl?: string;
+    finishDate: string;
+    topics: string[];
+    payouts: number[];
+    isActive: boolean;
+  }[];
+  isAdmin: boolean;
+  isLoading: boolean;
+  stakeEnabled: boolean;
+  isContestAvailable: boolean;
+}
+const ContestsList = ({
+  contests,
+  isAdmin,
+  isLoading,
+  stakeEnabled,
+  isContestAvailable,
+}: ContestsListProps) => {
+  if (isLoading) {
+    return (
+      <div>
+        {Array.from({ length: 3 }).map((_, index) => (
+          <Skeleton key={index} className="ContestsListSkeleton" />
+        ))}
+      </div>
+    );
   }
 
   return (
-    <CWPageLayout>
-      <div className="ContestsList">
-        <div className="header-row">
-          <CWText type="h2">Contests</CWText>
-          <CWButton
-            iconLeft="plusPhosphor"
-            label="Create contest"
-            onClick={() => navigate('/manage/contests/launch')}
-          />
-        </div>
-
-        {!stakeEnabled || !isContestAvailable ? (
-          <EmptyContestsList
-            isStakeEnabled={stakeEnabled}
-            isContestAvailable={isContestAvailable}
-          />
-        ) : (
-          <div className="list-container">
-            {contestsData.map(
-              ({
-                id,
-                name,
-                imageUrl,
-                finishDate,
-                topics,
-                payouts,
-                isActive,
-              }) => (
-                <ContestCard
-                  key={id}
-                  isAdmin={isAdmin}
-                  id={id}
-                  name={name}
-                  imageUrl={imageUrl}
-                  topics={topics}
-                  payouts={payouts}
-                  finishDate={finishDate}
-                  isActive={isActive}
-                />
-              ),
-            )}
-          </div>
-        )}
-      </div>
-    </CWPageLayout>
+    <div className="ContestsList">
+      {isAdmin && (!stakeEnabled || !isContestAvailable) ? (
+        <EmptyContestsList
+          isStakeEnabled={stakeEnabled}
+          isContestAvailable={isContestAvailable}
+        />
+      ) : (
+        contests.map(
+          ({ id, name, imageUrl, finishDate, topics, payouts, isActive }) => (
+            <ContestCard
+              key={id}
+              isAdmin={isAdmin}
+              id={id}
+              name={name}
+              imageUrl={imageUrl}
+              topics={topics}
+              payouts={payouts}
+              finishDate={finishDate}
+              isActive={isActive}
+            />
+          ),
+        )
+      )}
+    </div>
   );
 };
 
--- packages/commonwealth/client/scripts/views/pages/Contests/Contests.tsx
+++ packages/commonwealth/client/scripts/views/pages/Contests/Contests.tsx
@@ -2,21 +2,43 @@ import React from 'react';
 import { Navigate } from 'react-router-dom';
 
 import app from 'state';
+import { CWText } from 'views/components/component_kit/cw_text';
 import CWPageLayout from 'views/components/component_kit/new_designs/CWPageLayout';
+import ContestsList from 'views/pages/CommunityManagement/Contests/ContestsList';
 import useCommunityContests from 'views/pages/CommunityManagement/Contests/useCommunityContests';
 
 import './Contests.scss';
 
 const Contests = () => {
-  const { stakeEnabled, isContestAvailable } = useCommunityContests();
+  const {
+    stakeEnabled,
+    contestsData,
+    isContestAvailable,
+    isContestDataLoading,
+  } = useCommunityContests();
 
-  if (!stakeEnabled || !isContestAvailable) {
+  if (!isContestDataLoading && (!stakeEnabled || !isContestAvailable)) {
     return <Navigate replace to={`/${app.activeChainId()}`} />;
   }
 
   return (
     <CWPageLayout>
-      <div className="Contests">Contests</div>
+      <div className="Contests">
+        <CWText type="h2">Active Contests</CWText>
+        <CWText className="description">
+          Check out the contests in this community. Winners are determined by
+          having the most upvoted content in the contest topics.{' '}
+          <a href="https://blog.commonwealth.im">Learn more</a>
+        </CWText>
+
+        <ContestsList
+          contests={contestsData}
+          isAdmin={false}
+          isLoading={isContestDataLoading}
+          stakeEnabled={stakeEnabled}
+          isContestAvailable={isContestAvailable}
+        />
+      </div>
     </CWPageLayout>
   );
 };
