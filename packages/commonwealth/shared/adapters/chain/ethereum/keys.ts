031a179ae9bb4706988319368ed47de27f832ac9
Bob Webb <bob.wbb@gmail.com>
2024-04-25 18:19:36 +0100

    Remove sortedStringify

--- packages/commonwealth/shared/adapters/chain/ethereum/keys.ts
+++ packages/commonwealth/shared/adapters/chain/ethereum/keys.ts
@@ -1,12 +1,3 @@
-import { configure as configureStableStringify } from 'safe-stable-stringify';
-
-const sortedStringify = configureStableStringify({
-  bigint: false,
-  circularValue: Error,
-  strict: true,
-  deterministic: true,
-});
-
 export const TEST_BLOCK_INFO_STRING =
   '{"number":1,"hash":"0x0f927bde6fb00940895178da0d32948714ea6e76f6374f03ffbbd7e0787e15bf","timestamp":1665083987891}';
 
