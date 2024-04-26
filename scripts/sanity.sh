65f1eb431d987a84234b018190c3ad92d512254b
rotorsoft <rotorsoft@outlook.com>
2024-04-26 15:35:37 -0400

    fixes

--- scripts/sanity.sh
+++ scripts/sanity.sh
@@ -9,13 +9,18 @@ yarn lint-branch-warnings
 # check types 
 # incrementally builds server/libs (tsc -b --noEmit is not allowed)
 yarn workspaces run clean
+echo "Building before type checking..."
 yarn workspace commonwealth build
+yarn workspace scripts build
+echo "Type checking..."
 yarn workspaces run check-types
 
 # run unit tests
+echo "Unit tests..."
 yarn workspaces run test
 
 # run api test
+echo "Api test..."
 yarn workspace commonwealth test-api
 
 # we are aiming for a devx without builds
