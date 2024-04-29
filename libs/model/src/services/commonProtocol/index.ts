fd3f1c455d03d654794096a1d954d30be85f26d0
Raymond Z <raykyri@gmail.com>
2024-04-29 13:11:18 -0400

    fix merge

--- libs/model/src/services/commonProtocol/index.ts
+++ libs/model/src/services/commonProtocol/index.ts
@@ -1,6 +1,4 @@
-export * as communityStakeConfigValidator from './communityStakeConfigValidator';
 export * as ContestHelper from './contestHelper';
-export * as contractHelpers from './contractHelpers';
 export * as newNamespaceValidator from './newNamespaceValidator';
 
 import * as communityStakeConfigValidatorModule from './communityStakeConfigValidator';
