f64e916fb3796a96d6a589e87ac3348f6931ca3e
Ryan Bennett <ryan@common.xyz>
2024-04-29 13:21:26 -0700

    topic fixes

--- libs/model/src/contest/CreateContestManagerMetadata.command.ts
+++ libs/model/src/contest/CreateContestManagerMetadata.command.ts
@@ -18,53 +18,53 @@ export const CreateContestManagerMetadata: Command<
   body: async ({ id, payload }) => {
     const { topic_ids, ...rest } = payload;
 
-    const transaction = await models.sequelize.transaction();
-    try {
-      const contestManager = await models.ContestManager.create(
-        {
-          ...rest,
-          community_id: id!,
-          created_at: new Date(),
-          cancelled: false,
+    // verify topics exist
+    const topics = await models.Topic.findAll({
+      where: {
+        id: {
+          [Op.in]: topic_ids,
         },
-        { transaction },
-      );
-      if (mustExist('ContestManager', contestManager)) {
-        // verify topics exist
-        const topics = await models.Topic.findAll({
-          where: {
-            id: {
-              [Op.in]: topic_ids,
-            },
-          },
-        });
-        if (topics.length !== topic_ids.length) {
-          throw new InvalidState(Errors.InvalidTopics);
-        }
-        // create topic associates
-        const contestTopicsToCreate = topics.map((t) => ({
-          contest_address: rest.contest_address,
-          topic_id: t.id!,
-          created_at: new Date(),
-        }));
+      },
+    });
+    if (topics.length !== topic_ids.length) {
+      throw new InvalidState(Errors.InvalidTopics);
+    }
+
+    const contestTopicsToCreate = topics.map((t) => ({
+      contest_address: rest.contest_address,
+      topic_id: t.id!,
+      created_at: new Date(),
+    }));
+
+    const contestManager = await models.sequelize.transaction(
+      async (transaction) => {
         await models.ContestTopic.bulkCreate(contestTopicsToCreate, {
           transaction,
         });
 
-        return {
-          contest_managers: [
-            {
-              ...contestManager.get({ plain: true }),
-              topics: topics.map((t) =>
-                t.get({ plain: true }),
-              ) as Required<TopicAttributes>[],
-            },
-          ],
-        };
-      }
-    } catch (err) {
-      await transaction.rollback();
-      throw err;
+        return models.ContestManager.create(
+          {
+            ...rest,
+            community_id: id!,
+            created_at: new Date(),
+            cancelled: false,
+          },
+          { transaction },
+        );
+      },
+    );
+
+    if (mustExist('ContestManager', contestManager)) {
+      return {
+        contest_managers: [
+          {
+            ...contestManager.get({ plain: true }),
+            topics: topics.map((t) =>
+              t.get({ plain: true }),
+            ) as Required<TopicAttributes>[],
+          },
+        ],
+      };
     }
   },
 });
