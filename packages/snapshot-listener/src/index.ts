2092693ffac010acb15dda6b13dbe29c4a6ee4de
Timothee Legros <timothee@ualberta.ca>
2024-04-30 00:53:56 +0300

    check if event is needed + create outbox record

--- packages/snapshot-listener/src/index.ts
+++ packages/snapshot-listener/src/index.ts
@@ -6,14 +6,9 @@ import {
   getRabbitMQConfig,
   startHealthCheckLoop,
 } from '@hicommonwealth/adapters';
-import {
-  Broker,
-  BrokerTopics,
-  EventContext,
-  broker,
-  stats,
-} from '@hicommonwealth/core';
+import { Broker, broker, schemas, stats } from '@hicommonwealth/core';
 import { logger } from '@hicommonwealth/logging';
+import { models } from '@hicommonwealth/model';
 import type { Request, RequestHandler, Response } from 'express';
 import express, { json } from 'express';
 import v8 from 'v8';
@@ -75,40 +70,44 @@ registerRoute(app, 'post', '/snapshot', async (req: Request, res: Response) => {
 
     const response = await fetchNewSnapshotProposal(parsedId, eventType);
 
-    const event: EventContext<'SnapshotProposalCreated'> = {
-      name: 'SnapshotProposalCreated',
-      payload: {
+    const space = response.data.proposal?.space.id;
+
+    if (!space) {
+      log.error('Space not defined!');
+      return res.status(400).send('Error getting snapshot space');
+    }
+
+    const associatedCommunities = await models.CommunitySnapshotSpaces.count({
+      where: { snapshot_space_id: space },
+    });
+
+    if (associatedCommunities === 0) {
+      log.info(`No associated communities found for space ${space}`);
+      return res.status(200).json({ message: 'No associated community' });
+    }
+
+    await models.Outbox.create({
+      event_name: schemas.EventNames.SnapshotProposalCreated,
+      event_payload: {
         id: parsedId,
         event: req.body.event,
         title: response.data.proposal?.title ?? null,
         body: response.data.proposal?.body ?? null,
         choices: response.data.proposal?.choices ?? null,
-        space: response.data.proposal?.space.id ?? null,
+        space: space ?? null,
         start: response.data.proposal?.start ?? null,
         expire: response.data.proposal?.end ?? null,
         token: req.body.token,
         secret: req.body.secret,
       },
-    };
-
-    const result = await controller?.publish(
-      BrokerTopics.SnapshotListener,
-      event,
-    );
-
-    if (!result) {
-      log.error('Failed to publish snapshot message', undefined, {
-        event,
-      });
-      res.status(500).send('Failed to publish snapshot');
-    }
+    });
 
     stats().increment('snapshot_listener.received_snapshot_event', {
       event: eventType,
-      space: event.payload.space,
+      space: space,
     });
 
-    res.status(200).send({ message: 'Snapshot event received', event });
+    res.status(200).send({ message: 'Snapshot event received' });
   } catch (err) {
     log.error('Error sending snapshot event', err);
     res.status(500).send('error: ' + err);
