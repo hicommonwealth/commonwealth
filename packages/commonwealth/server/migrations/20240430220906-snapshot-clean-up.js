906635169eaaf44b5c9a214fa6b4d76755cb928b
Timothee Legros <timothee@ualberta.ca>
2024-05-01 02:35:57 +0300

    type fix + down migration

--- packages/commonwealth/server/migrations/20240430220906-snapshot-clean-up.js
+++ packages/commonwealth/server/migrations/20240430220906-snapshot-clean-up.js
@@ -30,11 +30,54 @@ module.exports = {
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    await queryInterface.sequelize.transaction(async (transaction) => {
+      await queryInterface.sequelize.query(
+        `
+            create table "SnapshotSpaces"
+            (
+                snapshot_space varchar(255)             not null
+                    primary key,
+                created_at     timestamp with time zone not null,
+                updated_at     timestamp with time zone not null
+            );
+
+            create table "CommunitySnapshotSpaces"
+            (
+                id                serial
+                    primary key,
+                snapshot_space_id varchar(255)             not null,
+                community_id      varchar(255)             not null,
+                created_at        timestamp with time zone not null,
+                updated_at        timestamp with time zone not null
+            );
+
+            create table "SnapshotProposals"
+            (
+                id                  varchar(255)             not null
+                    primary key,
+                title               varchar(255),
+                body                text,
+                choices             varchar(255)[],
+                space               varchar(255)             not null,
+                event               varchar(255),
+                start               varchar(255),
+                expire              varchar(255),
+                is_upstream_deleted boolean default false    not null,
+                "createdAt"         timestamp with time zone not null,
+                "updatedAt"         timestamp with time zone not null
+            );
+
+            INSERT INTO "SnapshotSpaces"(snapshot_space, created_at, updated_at)
+            SELECT DISTINCT UNNEST(C.snapshot_spaces), CURRENT_TIMESTAMP, CURRENT_TIMESTAMP
+            FROM "Communities" C
+            ON CONFLICT (snapshot_space) DO NOTHING;
+
+            INSERT INTO "CommunitySnapshotSpaces" (community_id, snapshot_space_id)
+            SELECT C.id, UNNEST(C.snapshot_spaces)
+            FROM "Communities" C;
+        `,
+        { transaction },
+      );
+    });
   },
 };
