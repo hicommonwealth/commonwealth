05554e9807dcd996c45236acafc2168e5b03d110
Kevin Burton <burtonator@gmail.com>
2024-04-30 10:04:02 -0600

    ... SITEMAP_ENV

--- packages/scripts/src/sitemaps/runner.ts
+++ packages/scripts/src/sitemaps/runner.ts
@@ -8,6 +8,12 @@ import {
 const log = logger(__filename);
 
 async function doExec() {
+  if (process.env.SITEMAP_ENV !== 'production') {
+    throw new Error(
+      'Define SITEMAP_ENV to signify you understand that this should only run in production to avoid breaking sitemaps.',
+    );
+  }
+
   if (process.env.NODE_ENV !== 'production') {
     // we have to enforce production because if we don't we will get localhost
     // URLs and that might be very destructive to our SEO
