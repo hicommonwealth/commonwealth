d8aab22cfffc339581ea6ae3b9a2c4504850fdad
rotorsoft <rotorsoft@outlook.com>
2024-05-01 11:40:41 -0400

    fix sanity

--- scripts/sanity.sh
+++ scripts/sanity.sh
@@ -3,31 +3,30 @@
 # Sanity scripts we should run locally before pushing code
 set -e
 
-# lint changes
-echo -e "\n \033[1;33m > Linting ... \033[0m \n"
+function step() {
+  echo -e "\n \033[1;33m > $1 ... \033[0m \n"
+}
+
+step 'Linting'
 yarn lint-branch-warnings
 
-echo -e "\n \033[1;33m > Initial cleaning... \033[0m \n"
+step 'Initial cleaning'
 yarn workspaces run clean
 
-# check types
-# incrementally builds server/libs (tsc -b --noEmit is not allowed)
-echo -e "\n \033[1;33m > Building ... \033[0m \n"
+step 'Building'
 yarn workspace commonwealth build
 yarn workspace scripts build
 
-echo -e "\n \033[1;33m > Type checking ... \033[0m \n"
+step 'Type checking'
 yarn workspaces run check-types
 
-# run unit tests
-echo -e "\n \033[1;33m > Unit testing ... \033[0m \n"
+step 'Unit testing'
 yarn workspaces run test
 
-# run api test
-echo -e "\n \033[1;33m > Integration testing ... \033[0m \n"
+step 'Integration testing'
 yarn workspace commonwealth test-api
 yarn workspace commonwealth test-integration-util
 
 # we are aiming for a devx without builds
-echo -e "\n \033[1;33m > Final cleaning ... \033[0m \n"
+step 'Final cleaning'
 yarn workspaces run clean
