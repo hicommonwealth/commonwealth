c63fc88ddf0af6b63ff771f5533ead5b47a17f6d
israellund <israelmlund@gmail.com>
2024-05-01 08:57:49 -0400

    pwa prompt cleaned up on iOS

--- packages/commonwealth/client/scripts/hooks/useAnimation.ts
+++ packages/commonwealth/client/scripts/hooks/useAnimation.ts
@@ -1,24 +1,3 @@
-// import useNecessaryEffect from 'hooks/useNecessaryEffect';
-// import { useState } from 'react';
-
-// export const useAnimation = () => {
-//   const [animationStyles, setAnimationStyles] = useState({
-//     opacity: 0,
-//     transform: 'translateY(40px)',
-//     transition: '1s',
-//   });
-
-//   useNecessaryEffect(() => {
-//     setAnimationStyles({
-//       opacity: 1,
-//       transform: 'translateY(0)',
-//       transition: '1s',
-//     });
-//   }, []);
-
-//   return { animationStyles };
-// };
-
 import useNecessaryEffect from 'hooks/useNecessaryEffect';
 import { useState } from 'react';
 
