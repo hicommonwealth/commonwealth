9307b500fc831dd9bce247e7d7d7376117cf3226
Kevin Burton <burtonator@gmail.com>
2024-05-01 10:50:53 -0600

    basic knock page just for dedicted debugging

---
+++ packages/commonwealth/client/scripts/Knock.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export function Knock() {
+  return <div>this is knock</div>;
+}
--- packages/commonwealth/client/scripts/navigation/GeneralRoutes.tsx
+++ packages/commonwealth/client/scripts/navigation/GeneralRoutes.tsx
@@ -1,10 +1,11 @@
 import React, { lazy } from 'react';
 import { Route } from 'react-router-dom';
 import { withLayout } from 'views/Layout';
-
+import { Knock } from '../Knock';
 const TermsPage = lazy(() => import('views/pages/terms'));
 const OldTermsPage = lazy(() => import('views/pages/old_terms'));
 const PrivacyPage = lazy(() => import('views/pages/privacy'));
+
 const ComponentsShowcasePage = lazy(
   () => import('views/pages/ComponentsShowcase'),
 );
@@ -30,6 +31,7 @@ const GeneralRoutes = () => [
     path="/components"
     element={withLayout(ComponentsShowcasePage, { type: 'common' })}
   />,
+  <Route key="/knock" path="/knock" element={<Knock />} />,
 ];
 
 export default GeneralRoutes;
