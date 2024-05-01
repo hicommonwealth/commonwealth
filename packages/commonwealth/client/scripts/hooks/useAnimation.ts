6a7b2d827c2506f55570e925fb6bc70e61579b7e
israellund <israelmlund@gmail.com>
2024-05-01 08:47:42 -0400

    committing to change branches

--- packages/commonwealth/client/scripts/hooks/useAnimation.ts
+++ packages/commonwealth/client/scripts/hooks/useAnimation.ts
@@ -1,18 +1,45 @@
+// import useNecessaryEffect from 'hooks/useNecessaryEffect';
+// import { useState } from 'react';
+
+// export const useAnimation = () => {
+//   const [animationStyles, setAnimationStyles] = useState({
+//     opacity: 0,
+//     transform: 'translateY(40px)',
+//     transition: '1s',
+//   });
+
+//   useNecessaryEffect(() => {
+//     setAnimationStyles({
+//       opacity: 1,
+//       transform: 'translateY(0)',
+//       transition: '1s',
+//     });
+//   }, []);
+
+//   return { animationStyles };
+// };
+
 import useNecessaryEffect from 'hooks/useNecessaryEffect';
 import { useState } from 'react';
 
-export const useAnimation = () => {
+interface useAnimationProps {
+  transitionDuration?: string;
+}
+
+export const useAnimation = ({
+  transitionDuration = '1s',
+}: useAnimationProps = {}) => {
   const [animationStyles, setAnimationStyles] = useState({
     opacity: 0,
-    transform: 'translateY(40px)',
-    transition: '1s',
+    transform: 'translateY(100%)',
+    transition: transitionDuration,
   });
 
   useNecessaryEffect(() => {
     setAnimationStyles({
       opacity: 1,
       transform: 'translateY(0)',
-      transition: '1s',
+      transition: transitionDuration,
     });
   }, []);
 
--- packages/commonwealth/client/scripts/views/components/AddToHomeScreenPrompt/IOSPrompt/IOSPrompt.tsx
+++ packages/commonwealth/client/scripts/views/components/AddToHomeScreenPrompt/IOSPrompt/IOSPrompt.tsx
@@ -1,3 +1,54 @@
+// import React from 'react';
+// import { CWIcon } from '../../component_kit/cw_icons/cw_icon';
+// import { CWText } from '../../component_kit/cw_text';
+// import { CWButton } from '../../component_kit/new_designs/CWButton';
+// import './IOSPrompt.scss';
+
+// interface IOSPromptProps {
+//   hidePromptAction: () => void;
+//   showPrompt: boolean;
+//   setShowPrompt: (showPrompt: boolean) => void;
+// }
+
+// export const IOSPrompt = ({
+//   hidePromptAction,
+//   showPrompt,
+//   setShowPrompt,
+// }: IOSPromptProps) => {
+//   return (
+//     <div className="IOSPrompt">
+//       <div className="prompt-content">
+//         <div className="header">
+//           <div className="icon">
+//             <img src="/static/img/branding/common.svg" alt="Commonwealth" />
+//           </div>
+//           <CWText className="title">Add to Home Screen</CWText>
+//         </div>
+//         <CWText className="description">
+//           For the best mobile experience we recommend installing the Common
+//           web-app.
+//         </CWText>
+//         <div className="instructions">
+//           <CWText className="instruction">
+//             1. Tap the share <CWIcon className="share-icon" iconName="export" />{' '}
+//             icon
+//           </CWText>
+//           <CWText className="instruction">
+//             2. Select <span className="highlight">Add to Home Screen</span>
+//           </CWText>
+//         </div>
+//         <CWButton
+//           buttonType="tertiary"
+//           label="Show less often"
+//           containerClassName="hide-prompt"
+//           onClick={() => hidePromptAction()}
+//         />
+//       </div>
+//     </div>
+//   );
+// };
+
+import { useAnimation } from 'hooks/useAnimation';
 import React from 'react';
 import { CWIcon } from '../../component_kit/cw_icons/cw_icon';
 import { CWText } from '../../component_kit/cw_text';
@@ -10,14 +61,13 @@ interface IOSPromptProps {
   setShowPrompt: (showPrompt: boolean) => void;
 }
 
-export const IOSPrompt = ({
-  hidePromptAction,
-  showPrompt,
-  setShowPrompt,
-}: IOSPromptProps) => {
+export const IOSPrompt = ({ hidePromptAction }: IOSPromptProps) => {
+  const { animationStyles } = useAnimation({
+    transitionDuration: '0.5s',
+  });
   return (
     <div className="IOSPrompt">
-      <div className="prompt-content">
+      <div className="prompt-content" style={animationStyles}>
         <div className="header">
           <div className="icon">
             <img src="/static/img/branding/common.svg" alt="Commonwealth" />
