6ef58528794ba06de190ef8673f50ae97d7ced30
Graham Johnson <graham@commonwealth.im>
2024-05-06 12:24:55 -0400

    Update fkey

--- packages/commonwealth/server/migrations/20240415153219-make-template-table-plural.js
+++ packages/commonwealth/server/migrations/20240415153219-make-template-table-plural.js
@@ -2,10 +2,33 @@
 
 module.exports = {
   up: (queryInterface) => {
-    return queryInterface.renameTable('Template', 'Templates');
+    return queryInterface.sequelize.transaction(async (transaction) => {
+      await queryInterface.renameTable('Template', 'Templates', {
+        transaction,
+      });
+      await queryInterface.sequelize.query(
+        `
+        ALTER TABLE "Templates"
+        RENAME CONSTRAINT "Template_created_for_community_fkey" TO "Templates_created_for_community_fkey";
+      `,
+        { transaction },
+      );
+      ``;
+    });
   },
 
   down: (queryInterface) => {
-    return queryInterface.renameTable('Templates', 'Template');
+    return queryInterface.sequelize.transaction(async (transaction) => {
+      await queryInterface.renameTable('Templates', 'Template', {
+        transaction,
+      });
+      await queryInterface.sequelize.query(
+        `
+        ALTER TABLE "Template"
+        RENAME CONSTRAINT "Templates_created_for_community_fkey" TO "Template_created_for_community_fkey";
+      `,
+        { transaction },
+      );
+    });
   },
 };
