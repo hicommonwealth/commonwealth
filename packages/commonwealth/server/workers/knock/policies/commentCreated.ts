3f988490f34c1a657d1f4b4e7d663b3bd77a9197
Timothee Legros <timothee@ualberta.ca>
2024-05-06 20:49:56 +0300

    add knock nodejs SDK + CommentCreated event handler stub

--- packages/commonwealth/server/workers/knock/policies/commentCreated.ts
+++ packages/commonwealth/server/workers/knock/policies/commentCreated.ts
@@ -1,14 +1,28 @@
 import { EventHandler } from '@hicommonwealth/core';
 import { models } from '@hicommonwealth/model';
+import { Op } from 'sequelize';
 import { ZodUndefined } from 'zod';
 
 export const processCommentCreated: EventHandler<
   'CommentCreated',
   ZodUndefined
 > = async ({ payload }) => {
-  await models.CommentSubscription.findAll({
+  const authorAddress = await models.Address.findOne({
     where: {
-      comment_id: payload.id,
+      id: payload.address_id,
     },
   });
+
+  const commentSubs = (
+    await models.CommentSubscription.findAll({
+      where: {
+        comment_id: payload.id,
+        user_id: { [Op.not]: authorAddress.user_id ?? null },
+      },
+      attributes: ['user_id'],
+    })
+  ).map((c) => c.user_id);
+
+  if (commentSubs.length > 0) {
+  }
 };
