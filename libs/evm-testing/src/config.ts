a056f65e331e50665af8994fa465679c3701e3ee
Timothee Legros <timothee@ualberta.ca>
2024-05-08 00:08:33 +0300

    update default provider url for evm chain testing

--- libs/evm-testing/src/config.ts
+++ libs/evm-testing/src/config.ts
@@ -3,6 +3,6 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 // URL of the local Ganache, Anvil, or Hardhat chain
-export const PROVIDER_URL = process.env.PROVIDER_URL ?? 'http://chain:8545';
+export const PROVIDER_URL = process.env.PROVIDER_URL ?? 'http://127.0.0.1:8545';
 
 export const ETH_ALCHEMY_API_KEY = process.env.ETH_ALCHEMY_API_KEY;
