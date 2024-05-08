053f05b6a19f23aefd353ce47ae89ff5beed06ba
Timothee Legros <timothee@ualberta.ca>
2024-05-08 17:42:40 +0300

    Revert "Switch DB to test env"
    This reverts commit c344872204a29d75958c991426cbc7ce1d75109f.

--- libs/model/src/config.ts
+++ libs/model/src/config.ts
@@ -2,13 +2,10 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 export const TEST_DB_NAME = 'common_test';
-
-export const DATABASE_URI = (() => {
-  if (process.env.DATABASE_URL) return process.env.DATABASE_URL;
-  const env = process.env.DATABASE_ENV || process.env.NODE_ENV;
-  if (env === 'test')
-    return `postgresql://commonwealth:edgeware@localhost/${TEST_DB_NAME}`;
-  else return 'postgresql://commonwealth:edgeware@localhost/commonwealth';
-})();
-
+export const DATABASE_URI =
+  process.env.NODE_ENV === 'test'
+    ? `postgresql://commonwealth:edgeware@localhost/${TEST_DB_NAME}`
+    : !process.env.DATABASE_URL || process.env.NODE_ENV === 'development'
+    ? 'postgresql://commonwealth:edgeware@localhost/commonwealth'
+    : process.env.DATABASE_URL;
 export const TESTING = DATABASE_URI.endsWith(TEST_DB_NAME);
