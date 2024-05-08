2f612fc98aee7078d5b6a32a48bd6d53adb26966
Timothee Legros <timothee@ualberta.ca>
2024-05-08 17:51:50 +0300

    debug logs

--- libs/model/src/config.ts
+++ libs/model/src/config.ts
@@ -8,4 +8,6 @@ export const DATABASE_URI =
     : !process.env.DATABASE_URL || process.env.NODE_ENV === 'development'
     ? 'postgresql://commonwealth:edgeware@localhost/commonwealth'
     : process.env.DATABASE_URL;
+
+console.log('\n\n', TEST_DB_NAME, DATABASE_URI, '\n\n');
 export const TESTING = DATABASE_URI.endsWith(TEST_DB_NAME);
