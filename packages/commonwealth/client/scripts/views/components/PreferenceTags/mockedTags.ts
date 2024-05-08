7538cd85f4ace077ed08dfaee8e6cb29aa631b75
Malik Zulqurnain <malikzulqurnain2121@gmail.com>
2024-05-09 02:01:45 +0500

    Added preference tags component

---
+++ packages/commonwealth/client/scripts/views/components/PreferenceTags/mockedTags.ts
@@ -0,0 +1,18 @@
+// TODO: these should come from API
+export const preferenceTags = [
+  { id: 1, tag: '🐒 NFTs' },
+  { id: 2, tag: '🎮 Gaming' },
+  { id: 3, tag: '💻 Social' },
+  { id: 4, tag: '😂 Memes' },
+  { id: 5, tag: '🤖 AI' },
+  { id: 6, tag: '💸 DeFi' },
+  { id: 7, tag: '💎 ReFi' },
+  { id: 8, tag: '🏦 Lending/Borrowing' },
+  { id: 9, tag: '🚀 Staking' },
+  { id: 10, tag: '🔄 Swaps' },
+  { id: 11, tag: '🛰️ dApp' },
+  { id: 12, tag: '🔬 Technology' },
+  { id: 13, tag: '🔒 Security/Auditing' },
+  { id: 14, tag: '🏛️ Governance' },
+  { id: 15, tag: '🛒 Marketplace' },
+];
---
+++ packages/commonwealth/client/scripts/views/components/PreferenceTags/usePreferenceTags.tsx
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+import { preferenceTags } from './mockedTags';
+import { PreferenceTagsHookProps, SelectedTag, Tag } from './types';
+
+const usePreferenceTags = ({
+  initialSelectedTag = [],
+}: PreferenceTagsHookProps) => {
+  const [selectedTags, setSelectedTags] = useState<SelectedTag[]>(
+    [...preferenceTags].map((item) => ({
+      item,
+      isSelected: !!initialSelectedTag.find((x) => x.tag === item.tag),
+    })),
+  );
+
+  console.log('selectedTags => ', selectedTags);
+
+  const toggleTagFromSelection = (item: Tag, isSelected: boolean) => {
+    const updatedTags = [...selectedTags];
+    const foundTag = updatedTags.find((t) => t.item.tag === item.tag);
+    foundTag.isSelected = isSelected;
+    setSelectedTags([...updatedTags]);
+  };
+
+  return {
+    selectedTags,
+    setSelectedTags,
+    toggleTagFromSelection,
+  };
+};
+
+export default usePreferenceTags;
