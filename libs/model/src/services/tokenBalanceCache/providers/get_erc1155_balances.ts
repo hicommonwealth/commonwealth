5ab94d5ffaf8137ea48ed46ae0eb498e6e0d9482
Raymond Z <raykyri@gmail.com>
2024-05-09 11:40:52 -0400

    fix imports

--- libs/model/src/services/tokenBalanceCache/providers/get_erc1155_balances.ts
+++ libs/model/src/services/tokenBalanceCache/providers/get_erc1155_balances.ts
@@ -1,6 +1,6 @@
 import { logger } from '@hicommonwealth/logging';
 import { fileURLToPath } from 'url';
-import AbiCoder from 'web3-eth-abi';
+import * as AbiCoder from 'web3-eth-abi';
 import { ChainNodeInstance } from '../../../models/chain_node';
 import { Balances } from '../types';
 import { evmRpcRequest } from '../util';
