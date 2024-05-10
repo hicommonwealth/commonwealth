ba875c703bd8d3b5edc6838d351305415e7076af
Timothee Legros <62490329+timolegros@users.noreply.github.com>
2024-05-10 14:06:48 +0300

    Switch from Ganache to Anvil.js (#7699)
    * move evm-testing to its own lib
    
    * fix types
    
    * use anvil.js!!!
    
    * TBC tests working without API + Ganache
    
    * remove erc721 and erc1155 routes
    
    * remove remaining routes + fix up TBC tests
    
    * all evm devnet tests passing without Ganache + CI update
    
    * compoundGov update
    
    * type update to fix builds
    
    * test type fix
    
    * remove unused files
    
    * remove foundry from cosmos job
    
    * CI fix
    
    * fix import
    
    * fix imports
    
    * update default provider url for evm chain testing
    
    * bump time for evm devnet log test
    
    * add logs to debug CI test
    
    * add logs to mining
    
    * hugely increase test time --- to test if anvil is hanging
    
    * remove logs
    
    * remove logs + add back forbid-only
    
    * add Redis to EVM job for TBC tests
    
    * fix yarn.lock conflict

---
+++ libs/evm-testing/src/utils/chainUtil.ts
@@ -0,0 +1,65 @@
+import { Anvil, createAnvil, CreateAnvilOptions } from '@viem/anvil';
+import Web3 from 'web3';
+import { ETH_ALCHEMY_API_KEY, PROVIDER_URL } from '../config';
+import getProvider from './getProvider';
+
+export async function advanceTime(seconds: number, blocks = 1) {
+  const provider = new Web3.providers.HttpProvider(PROVIDER_URL);
+
+  const web3 = getProvider();
+  let block = await web3.eth.getBlock('latest');
+
+  const preTime = block.timestamp.toString();
+  const advance_secs = Web3.utils.numberToHex(seconds).toString();
+  const res = await provider.request({
+    jsonrpc: '2.0',
+    id: 1,
+    method: 'evm_increaseTime',
+    params: [advance_secs],
+  });
+
+  if (res.error) {
+    throw new Error((res.error as { code: number; message: string }).message);
+  }
+
+  await mineBlocks(blocks);
+  block = await web3.eth.getBlock('latest');
+  const postTime = block.timestamp.toString();
+  return {
+    preTime,
+    postTime,
+  };
+}
+
+export async function mineBlocks(blocks: number) {
+  const provider = new Web3.providers.HttpProvider(PROVIDER_URL);
+
+  // mine blocks
+  const res = await provider.request({
+    jsonrpc: '2.0',
+    id: 1,
+    method: 'anvil_mine',
+    params: [blocks],
+  });
+
+  if (res.error) {
+    throw new Error((res.error as { code: number; message: string }).message);
+  }
+
+  return true;
+}
+
+export async function getAnvil(
+  options: CreateAnvilOptions = {},
+): Promise<Anvil> {
+  const anvil = createAnvil({
+    forkUrl: `https://eth-mainnet.g.alchemy.com/v2/${ETH_ALCHEMY_API_KEY}`,
+    silent: false,
+    port: 8545,
+    autoImpersonate: true,
+    startTimeout: 10_000,
+    ...options,
+  });
+  await anvil.start();
+  return anvil;
+}
