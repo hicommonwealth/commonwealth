f358a66f581dc9504e542326a940726442a8dea0
rotorsoft <rotorsoft@outlook.com>
2024-05-13 11:25:45 -0400

    fix type

--- packages/commonwealth/client/scripts/controllers/chain/cosmos/gov/v1beta1/utils-v1beta1.ts
+++ packages/commonwealth/client/scripts/controllers/chain/cosmos/gov/v1beta1/utils-v1beta1.ts
@@ -228,7 +228,7 @@ export const encodeMsgSubmitProposal = (
 
 export const encodeMsgVote = (
   voter: string,
-  proposalId: number | Long.Long | string,
+  proposalId: number | Long | string,
   option: number,
 ): MsgVoteEncodeObject => {
   return {
