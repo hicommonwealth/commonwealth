031e25b7f82fa9f707f16a4c23de99a584a88a6f
Malik Zulqurnain <malikzulqurnain2121@gmail.com>
2024-05-13 21:28:09 +0500

    Updated /updateCommunityCategory api to add/remove tags to communities

--- packages/commonwealth/server/routes/updateCommunityCategory.ts
+++ packages/commonwealth/server/routes/updateCommunityCategory.ts
@@ -4,9 +4,15 @@ import { CommunityCategoryType } from '@hicommonwealth/shared';
 import type { TypedRequestBody, TypedResponse } from '../types';
 import { success } from '../types';
 
+const Errors = {
+  InvalidCommunityId: 'Invalid Community Id',
+  InvalidTagIds: 'Some tag ids are invalid',
+};
+
 type UpdateCommunityCategoryReq = {
   selected_tags: { [tag: string]: boolean };
   community_id: string;
+  tag_ids?: number[];
   auth: string;
   jwt: string;
 };
@@ -26,7 +32,7 @@ const updateCommunityCategory = async (
       id: req.body.community_id,
     },
   });
-  if (!community) throw new AppError('Invalid Community Id');
+  if (!community) throw new AppError(Errors.InvalidCommunityId);
 
   const existingCategories = community.category
     ? (community.category as string[])
@@ -37,6 +43,45 @@ const updateCommunityCategory = async (
       Object.keys(CommunityCategoryType).includes(tag)
     );
   });
+
+  const { tag_ids } = req.body;
+  if (tag_ids) {
+    // verify all tag ids exist
+    if (tag_ids.length > 0) {
+      const tagCount = await models.Tags.count({
+        where: {
+          id: tag_ids,
+        },
+      });
+
+      const allTagsFound = tagCount === tag_ids.length;
+
+      if (!allTagsFound) {
+        throw new AppError(Errors.InvalidTagIds);
+      }
+    }
+
+    // remove all existing tags
+    await models.CommunityTags.destroy({
+      where: {
+        community_id: community.id,
+      },
+    });
+
+    // create new tags
+    if (tag_ids.length > 0) {
+      const [status, newRows] = await models.CommunityTags.bulkCreate(
+        tag_ids.map((id) => ({
+          tag_id: id,
+          community_id: community.id,
+        })),
+      );
+
+      if (!status || !newRows) {
+        throw new AppError('Failed');
+      }
+    }
+  }
 
   if (
     existingCategories.length !== updateCategories.length ||
