764251da94af64d35fb2fefca96ce8a9e959e165
Kevin Burton <burtonator@gmail.com>
2024-05-14 15:13:05 -0600

    we have the ability to activate when we want now.  It's a bit hacky though.

--- packages/commonwealth/client/scripts/views/components/Mava/Mava.tsx
+++ packages/commonwealth/client/scripts/views/components/Mava/Mava.tsx
@@ -1,5 +1,6 @@
-import { memo, useEffect, useRef } from 'react';
+import { memo, useCallback, useEffect, useRef } from 'react';
 import app from 'state';
+import './Mava.scss';
 
 // eslint-disable-next-line no-var
 declare var window: any;
@@ -9,6 +10,13 @@ export default memo(function Mava() {
 
   const userId = app.user.id;
   const email = app.user.email;
+
+  const activateMava = useCallback(() => {
+    const chatElement = document.getElementById('mava');
+    if (chatElement) {
+      chatElement.style.display = 'block';
+    }
+  }, []);
 
   useEffect(() => {
     if (window.Mava) {
@@ -23,12 +31,14 @@ export default memo(function Mava() {
           'User Id': userId,
         });
 
+        activateMava();
+
         console.log('Identified with mava.');
       }
     } else {
       console.warn('No mava');
     }
-  }, [email, userId]);
+  }, [email, userId, activateMava]);
 
   return null;
 });
