a84c6e70e83365f4013245f53126f4c077c3d5ac
ianrowan <i.rowan299@gmail.com>
2024-05-15 10:56:03 -0500

    update schema

--- packages/commonwealth/server/util/requirementsModule/requirementsSchema_v1.ts
+++ packages/commonwealth/server/util/requirementsModule/requirementsSchema_v1.ts
@@ -13,7 +13,6 @@ export default {
         },
         contract_address: {
           type: 'string',
-          pattern: '^[a-zA-Z0-9]{32,44}$',
         },
         token_id: {
           type: 'string',
@@ -21,6 +20,27 @@ export default {
         },
       },
       required: ['source_type', 'evm_chain_id', 'contract_address'],
+      if: {
+        properties: {
+          source_type: {
+            const: 'spl',
+          },
+        },
+      },
+      then: {
+        properties: {
+          contract_address: {
+            pattern: '^[a-zA-Z0-9]{32,44}$',
+          },
+        },
+      },
+      else: {
+        properties: {
+          contract_address: {
+            pattern: '^0x[a-fA-F0-9]{40}$',
+          },
+        },
+      },
     },
     NativeSource: {
       type: 'object',
