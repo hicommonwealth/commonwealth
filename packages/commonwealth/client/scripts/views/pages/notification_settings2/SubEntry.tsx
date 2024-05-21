c073dbf5e08dad5a0c3181971ae02a68e590a7d3
Kevin Burton <burtonator@gmail.com>
2024-05-21 09:36:47 -0600

    cleanup so we use a sub-component.

---
+++ packages/commonwealth/client/scripts/views/pages/notification_settings2/SubEntry.tsx
@@ -0,0 +1,77 @@
+import { ThreadSubscription } from '@hicommonwealth/schemas';
+import { getThreadUrl } from '@hicommonwealth/shared';
+import { getRelativeTimestamp } from 'helpers/dates';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { getCommunityUrl } from 'utils';
+import { CWCommunityAvatar } from 'views/components/component_kit/cw_community_avatar';
+import { CWText } from 'views/components/component_kit/cw_text';
+import { CWThreadAction } from 'views/components/component_kit/new_designs/cw_thread_action';
+import { User } from 'views/components/user/user';
+import { z } from 'zod';
+
+interface SubscriptionEntryProps {
+  readonly thread: z.infer<typeof ThreadSubscription>['Thread'];
+}
+
+export const SubEntry = (props: SubscriptionEntryProps) => {
+  const { thread } = props;
+
+  return (
+    <div className="SubEntry">
+      <div className="SubHeader">
+        <div>
+          <CWCommunityAvatar
+            community={{
+              iconUrl: thread.Community.icon_url,
+              name: thread.Community.name,
+            }}
+            size="small"
+          />
+        </div>
+        <div style={{ marginLeft: '8px' }}>
+          <Link to={getCommunityUrl(thread.Community.name)}>
+            <CWText fontWeight="semiBold">{thread.Community.name}</CWText>
+          </Link>
+        </div>
+
+        <div style={{ marginLeft: '8px', marginRight: '8px' }}>•</div>
+
+        <div>
+          <User
+            userAddress={thread.Address.address}
+            userCommunityId={thread.Community.id}
+          />
+        </div>
+
+        <div style={{ marginLeft: '8px', marginRight: '8px' }}>•</div>
+
+        <div>{getRelativeTimestamp(thread.created_at.getTime())}</div>
+      </div>
+      <div>
+        <CWText type="h4" fontWeight="semiBold">
+          <Link
+            to={getThreadUrl({
+              chain: thread.community_id,
+              id: thread.id,
+              title: thread.title,
+            })}
+          >
+            <CWText type="h4">{decodeURIComponent(thread.title)}</CWText>
+          </Link>
+        </CWText>
+      </div>
+
+      <div className="SubFooter">
+        <CWThreadAction
+          label={`${thread.comment_count}`}
+          action="comment"
+          onClick={(e) => {
+            // e.preventDefault();
+            // onCommentBtnClick();
+          }}
+        />
+      </div>
+    </div>
+  );
+};
--- packages/commonwealth/client/scripts/views/pages/notification_settings2/index.tsx
+++ packages/commonwealth/client/scripts/views/pages/notification_settings2/index.tsx
@@ -1,20 +1,13 @@
 import { GetThreadSubscriptions } from '@hicommonwealth/schemas';
-import { getThreadUrl } from '@hicommonwealth/shared';
-import { getRelativeTimestamp } from 'helpers/dates';
 import { useCommonNavigate } from 'navigation/helpers';
 import 'pages/notification_settings/index.scss';
 import React, { useMemo } from 'react';
 import app from 'state';
-import { getCommunityUrl } from 'utils';
 import { trpc } from 'utils/trpcClient';
 import CWPageLayout from 'views/components/component_kit/new_designs/CWPageLayout';
-import { CWCommunityAvatar } from '../../components/component_kit/cw_community_avatar';
+import { SubEntry } from 'views/pages/notification_settings2/SubEntry';
 import { CWText } from '../../components/component_kit/cw_text';
-import { User } from '../../components/user/user';
 import { PageLoading } from '../loading';
-
-import { Link } from 'react-router-dom';
-import { CWThreadAction } from 'views/components/component_kit/new_designs/cw_thread_action';
 import './index.scss';
 
 function useThreadSubscriptions() {
@@ -32,7 +25,7 @@ function useThreadSubscriptions() {
   }, [threadSubscriptions]);
 }
 
-const NotificationSettingsPage2 = () => {
+const Index = () => {
   const navigate = useCommonNavigate();
   const threadSubscriptions = useThreadSubscriptions();
 
@@ -56,71 +49,11 @@ const NotificationSettingsPage2 = () => {
         {(threadSubscriptions.data || [])
           .filter((current) => current.Thread)
           .map((current) => (
-            <div key={current.id} className="SubEntry">
-              <div className="SubHeader">
-                <div>
-                  <CWCommunityAvatar
-                    community={{
-                      iconUrl: current.Thread.Community.icon_url,
-                      name: current.Thread.Community.name,
-                    }}
-                    size="small"
-                  />
-                </div>
-                <div style={{ marginLeft: '8px' }}>
-                  <Link to={getCommunityUrl(current.Thread.Community.name)}>
-                    <CWText fontWeight="semiBold">
-                      {current.Thread.Community.name}
-                    </CWText>
-                  </Link>
-                </div>
-
-                <div style={{ marginLeft: '8px', marginRight: '8px' }}>•</div>
-
-                <div>
-                  <User
-                    userAddress={current.Thread.Address.address}
-                    userCommunityId={current.Thread.Community.id}
-                  />
-                </div>
-
-                <div style={{ marginLeft: '8px', marginRight: '8px' }}>•</div>
-
-                <div>
-                  {getRelativeTimestamp(current.Thread.created_at.getTime())}
-                </div>
-              </div>
-              <div>
-                <CWText type="h4" fontWeight="semiBold">
-                  <Link
-                    to={getThreadUrl({
-                      chain: current.Thread.community_id,
-                      id: current.Thread.id,
-                      title: current.Thread.title,
-                    })}
-                  >
-                    <CWText type="h4">
-                      {decodeURIComponent(current.Thread.title)}
-                    </CWText>
-                  </Link>
-                </CWText>
-              </div>
-
-              <div className="SubFooter">
-                <CWThreadAction
-                  label={`${current.Thread.comment_count}`}
-                  action="comment"
-                  onClick={(e) => {
-                    // e.preventDefault();
-                    // onCommentBtnClick();
-                  }}
-                />
-              </div>
-            </div>
+            <SubEntry key={current.id} thread={current.Thread} />
           ))}
       </div>
     </CWPageLayout>
   );
 };
 
-export default NotificationSettingsPage2;
+export default Index;
