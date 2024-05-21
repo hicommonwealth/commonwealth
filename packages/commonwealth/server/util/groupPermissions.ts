2e4ba4fb1224d3518a29732c05b7d485ed2d225a
kassad <kurtis@commonwealth.im>
2024-05-21 06:24:00 -0700

    Fixed type error

--- packages/commonwealth/server/util/groupPermissions.ts
+++ packages/commonwealth/server/util/groupPermissions.ts
@@ -1,6 +1,7 @@
 import { AppError } from '@hicommonwealth/core';
 import { DB } from '@hicommonwealth/model';
 import { GroupPermissionType } from '@hicommonwealth/schemas';
+import { QueryTypes } from 'sequelize';
 
 export async function assertGroupPermission(
   type: GroupPermissionType,
@@ -8,18 +9,20 @@ export async function assertGroupPermission(
   communityId: string,
   models: DB,
 ) {
-  const isInGroup = await models.sequelize.query(
+  const isInGroup: { reject_reason: string }[] = await models.sequelize.query(
     `
     SELECT reject_reason FROM "Groups" as g JOIN "GroupPermissions" gp ON g.id = gp.group_id 
     JOIN "Memberships" as m ON m.group_id = g.id
-    WHERE g.community_id = :communityId AND gp.type = :type AND m.address_id = :addressId;
+    WHERE g.community_id = :communityId AND gp.type = :type AND m.address_id = :addressId LIMIT 1;
   `,
     {
+      type: QueryTypes.SELECT,
+      raw: true,
       replacements: { type, communityId, addressId },
     },
   );
 
-  if (isInGroup[0][0].reject_reason) {
+  if (isInGroup?.[0]?.reject_reason) {
     throw new AppError(
       `User does not have sufficient permissions to perform action "${type}"`,
     );
