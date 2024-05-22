8f755ae8d0ccb94735e36dd6345eaae7f77bfc4a
Timothee Legros <timothee@ualberta.ca>
2024-05-22 15:52:04 +0300

    simplify script

--- packages/commonwealth/server/scripts/releasePhaseEnvCheck.ts
+++ packages/commonwealth/server/scripts/releasePhaseEnvCheck.ts
@@ -3,18 +3,14 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const log = logger(__filename);
 
-async function releasePhaseEnvCheck() {
-  try {
-    await import('../config');
-  } catch (e) {
-    log.fatal('Environment variables not properly configured!', e);
-    process.exit(1);
-  }
-}
-
 if (import.meta.url.endsWith(process.argv[1])) {
-  releasePhaseEnvCheck().then(() => {
-    log.info('Environment variables are properly configured');
-    process.exit(0);
-  });
+  import('../config')
+    .then(() => {
+      log.info('Environment variables are properly configured');
+      process.exit(0);
+    })
+    .catch((err) => {
+      log.fatal('Environment variables not properly configured!', err);
+      process.exit(1);
+    });
 }
