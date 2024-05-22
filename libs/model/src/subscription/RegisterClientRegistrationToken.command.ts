d5f449e5c31571a9583e75914d68d141fac4903f
Kevin Burton <burtonator@gmail.com>
2024-05-22 17:05:06 -0600

    ok.. now I just need the knock env information.

--- libs/model/src/subscription/RegisterClientRegistrationToken.command.ts
+++ libs/model/src/subscription/RegisterClientRegistrationToken.command.ts
@@ -1,5 +1,12 @@
 import { type Command } from '@hicommonwealth/core';
 import * as schemas from '@hicommonwealth/schemas';
+import { Knock } from '@knocklabs/node';
+
+// FIXME: get the right secret key
+const knockClient = new Knock('sk_12345');
+
+// FIXME: ge tthe right channel ID
+const CHANNEL_ID = '8209f26c-62a5-461d-95e2-a5716a26e652';
 
 export function RegisterClientRegistrationToken(): Command<
   typeof schemas.RegisterClientRegistrationToken
@@ -10,9 +17,10 @@ export function RegisterClientRegistrationToken(): Command<
     secure: true,
     body: async ({ payload, actor }) => {
       console.log('Registering client token!');
-      // if (config.NOTIFICATIONS.FLAG_KNOCK_INTEGRATION_ENABLED) {
-      //
-
+      // FIXME: verify that I can trust the actor and that it can't be faked
+      await knockClient.users.setChannelData(`${actor.user.id}`, CHANNEL_ID, {
+        tokens: [payload.token],
+      });
       return {};
     },
   };
