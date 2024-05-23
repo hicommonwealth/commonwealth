c7c417f2a9f21fbff66a37d3a6348d4444f1f80e
Timothee Legros <timothee@ualberta.ca>
2024-05-23 15:45:20 +0300

    type fix

--- packages/commonwealth/server/routes/getUploadSignature.ts
+++ packages/commonwealth/server/routes/getUploadSignature.ts
@@ -3,7 +3,7 @@ import { AppError } from '@hicommonwealth/core';
 import { PutObjectCommand, S3 } from '@aws-sdk/client-s3';
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 
-import { DB } from '@hicommonwealth/model';
+import type { DB } from '@hicommonwealth/model';
 import type { NextFunction, Request, Response } from 'express';
 import { v4 as uuidv4 } from 'uuid';
 
