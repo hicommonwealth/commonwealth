75ea260ba3c52cd57c9917f338fea1102504898c
Kevin Burton <burtonator@gmail.com>
2024-05-29 11:16:47 -0600

    fixing this...

--- packages/commonwealth/client/scripts/views/pages/notification_settings/getFirebaseMessaging.ts
+++ packages/commonwealth/client/scripts/views/pages/notification_settings/getFirebaseMessaging.ts
@@ -1,12 +1,9 @@
 import { initializeApp } from 'firebase/app';
 import { getMessaging, getToken } from 'firebase/messaging';
 
-// FIXME: move to env...
-const PUBLIC_VAPID_KEY =
-  'BDMNzw-2Dm1HcE9hFr3T4Li_pCp_w7L4tCcq-OETD71J1DdC0VgIogt6rC8Hh0bHtTacyZHSoQ1ax5KCU4ZjS30';
-
-// FIXME move to env...
-const PUBLIC_FIREBASE_CONFIG = {
+// NOTE that this CAN be public and served to clients. All this data is public
+// and designed by Firebase to be public. It's not a secret.
+const DEFAULT_FIREBASE_CONFIG = {
   apiKey: 'AIzaSyA93Av0xLkOB_nP9hyzhGYg78n9JEfS1bQ',
   authDomain: 'common-staging-384806.firebaseapp.com',
   projectId: 'common-staging-384806',
@@ -15,6 +12,14 @@ const PUBLIC_FIREBASE_CONFIG = {
   appId: '1:158803639844:web:b212938a52d995c6d862b1',
   measurementId: 'G-4PNZZQDNFE',
 };
+
+// FIXME: move to env...
+const PUBLIC_VAPID_KEY =
+  'BDMNzw-2Dm1HcE9hFr3T4Li_pCp_w7L4tCcq-OETD71J1DdC0VgIogt6rC8Hh0bHtTacyZHSoQ1ax5KCU4ZjS30';
+
+const PUBLIC_FIREBASE_CONFIG = process.env.PUBLIC_FIREBASE_CONFIG
+  ? JSON.parse(process.env.PUBLIC_FIREBASE_CONFIG)
+  : DEFAULT_FIREBASE_CONFIG;
 
 // Initialize Firebase
 const app = initializeApp(PUBLIC_FIREBASE_CONFIG);
