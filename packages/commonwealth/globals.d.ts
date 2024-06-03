0026861ba330f0115571676a0913db342baaaad9
kassad <kurtis@commonwealth.im>
2024-06-03 06:41:14 -0700

    Fixed build error

---
+++ packages/commonwealth/globals.d.ts
@@ -0,0 +1,17 @@
+declare module '*.scss';
+
+/*
+declare namespace JSX {
+  interface ElementClass {
+    view: any;
+  }
+
+  interface IntrinsicElements {
+    __props: any;
+  }
+
+  interface ElementAttributesProperty {
+    __props: any;
+  }
+}
+*/
