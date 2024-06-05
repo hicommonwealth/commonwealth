21555baa829477e413f3c53a3bbce61c4080c462
ilijabojanovic <ilijabojanovic@gmail.com>
2024-06-05 21:09:03 +0200

    Initial commit

---
+++ packages/commonwealth/test/k6/browser.js
@@ -0,0 +1,90 @@
+import { check } from 'k6';
+import { browser } from 'k6/experimental/browser';
+
+const BASE_URL = `http://${__ENV.BASE_URL}/` || 'http://localhost:9000/';
+
+export const options = {
+  scenarios: {
+    browser: {
+      executor: 'constant-vus',
+      exec: 'dashboard',
+      vus: 3,
+      duration: '1m',
+      options: {
+        browser: {
+          type: 'chromium',
+        },
+      },
+    },
+    layer_zero: {
+      executor: 'constant-vus',
+      exec: 'layer_zero',
+      vus: 3,
+      duration: '1m',
+      options: {
+        browser: {
+          type: 'chromium',
+        },
+      },
+    },
+  },
+  search: {
+    executor: 'constant-vus',
+    exec: 'search',
+    vus: 3,
+    duration: '1m',
+    options: {
+      browser: {
+        type: 'chromium',
+      },
+    },
+  },
+};
+
+export async function dashboard() {
+  const page = browser.newPage();
+
+  try {
+    await page.goto(BASE_URL);
+    page.waitForLoadState('load');
+
+    check(page, {
+      'Page title': page.title() == 'Common',
+    });
+  } finally {
+    page.close();
+  }
+}
+
+export async function layer_zero() {
+  const page = browser.newPage();
+
+  try {
+    await page.goto(`${BASE_URL}layerzero/discussion`);
+    page.waitForLoadState('load');
+
+    check(page, {
+      'Page title': page.title() == 'Common',
+    });
+  } finally {
+    page.close();
+  }
+}
+
+export async function search() {
+  const page = browser.newPage();
+
+  try {
+    await page.goto(`${BASE_URL}dashboard/global`);
+    page.waitForLoadState('load');
+    let searchBox = await page.getByPlaceholder('Search Common');
+    searchBox.type('Proto');
+    searchBox.press('Enter');
+    page.waitForLoadState('load');
+    check(page, {
+      'Page title': page.title() == 'Common',
+    });
+  } finally {
+    page.close();
+  }
+}
---
+++ packages/commonwealth/test/k6/readme.md
@@ -0,0 +1,9 @@
+# Install docker with docker compose
+
+`docker compose -f monitoring.yaml`
+
+# Start application locally with Redis
+
+# Run browser script
+
+k6 -e BASE_URL=localhost:9000 run browser.js
