f9c8d0514ac7b4e0358dfa31799943979c57828e
Kevin Burton <burtonator@gmail.com>
2024-06-06 09:57:36 -0600

    minor cleanup before the final PR

--- libs/model/src/subscription/GetCommunityAlerts.query.ts
+++ libs/model/src/subscription/GetCommunityAlerts.query.ts
@@ -2,13 +2,6 @@ import { type Query } from '@hicommonwealth/core';
 import * as schemas from '@hicommonwealth/schemas';
 import { models } from '../database';
 
-const associationParams = [
-  {
-    model: models.Community,
-    as: 'Community',
-  },
-];
-
 export function GetCommunityAlerts(): Query<typeof schemas.GetCommunityAlerts> {
   return {
     ...schemas.GetCommunityAlerts,
@@ -20,8 +13,6 @@ export function GetCommunityAlerts(): Query<typeof schemas.GetCommunityAlerts> {
           where: {
             user_id: actor.user.id,
           },
-          //include: [...associationParams],
-          logging: console.log,
         })
       ).map((alert) => alert.get({ plain: true }));
     },
