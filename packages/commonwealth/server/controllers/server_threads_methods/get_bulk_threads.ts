6fef8463d6c38233c7b9d44ddc55e5ee8b901096
kassad <kurtis@commonwealth.im>
2024-06-06 05:33:43 -0700

    Fixed get_bulk_threads for contests

--- packages/commonwealth/server/controllers/server_threads_methods/get_bulk_threads.ts
+++ packages/commonwealth/server/controllers/server_threads_methods/get_bulk_threads.ts
@@ -103,10 +103,6 @@ export async function __getBulkThreads(
     all: '',
   };
 
-  const contestJoin =
-    ' JOIN "ContestActions" CA ON CA.thread_id = id' +
-    ' JOIN "Contests" CON ON CON.contest_id = CA.contest_id';
-
   const responseThreadsQuery = this.models.sequelize.query<ThreadsQuery>(
     `
         WITH top_threads AS (
@@ -116,7 +112,6 @@ export async function __getBulkThreads(
             marked_as_spam_at, archived_at, topic_id, reaction_weights_sum, canvas_action as "canvasAction",
             canvas_session as "canvasSession", canvas_hash as "canvasHash", plaintext, last_edited, address_id
         FROM "Threads" T
-        ${contestAddress ? contestJoin : ''}
         WHERE
             community_id = :communityId AND
             deleted_at IS NULL AND
