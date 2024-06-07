be00d1df396093939479924cbeff6cd31c986adc
Kevin Burton <burtonator@gmail.com>
2024-06-07 10:41:37 -0600

    moved out the notifications button.

--- packages/commonwealth/client/scripts/views/pages/NotificationSettingsOld/index.tsx
+++ packages/commonwealth/client/scripts/views/pages/NotificationSettingsOld/index.tsx
@@ -1,15 +1,12 @@
 import { NotificationCategories } from '@hicommonwealth/shared';
-import { useFlag } from 'hooks/useFlag';
 import useForceRerender from 'hooks/useForceRerender';
 import moment from 'moment';
 import { useCommonNavigate } from 'navigation/helpers';
 import 'pages/notification_settings/index.scss';
-import React, { useCallback, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import app from 'state';
-import { trpc } from 'utils/trpcClient';
 import { PopoverMenu } from 'views/components/component_kit/CWPopoverMenu';
 import CWPageLayout from 'views/components/component_kit/new_designs/CWPageLayout';
-import { getFirebaseMessagingToken } from 'views/pages/notification_settings/getFirebaseMessagingToken';
 import { CWCard } from '../../components/component_kit/cw_card';
 import { CWCheckbox } from '../../components/component_kit/cw_checkbox';
 import { CWCollapsible } from '../../components/component_kit/cw_collapsible';
@@ -40,7 +37,6 @@ const emailIntervalFrequencyMap = {
 const NotificationSettingsPage = () => {
   const navigate = useCommonNavigate();
   const forceRerender = useForceRerender();
-  const enableKnockPushNotifications = useFlag('knockPushNotifications');
 
   const {
     email,
@@ -59,25 +55,6 @@ const NotificationSettingsPage = () => {
     chainEventSubs,
     relevantSubscribedCommunities,
   } = useNotificationSettings();
-
-  const registerClientRegistrationToken =
-    trpc.subscription.registerClientRegistrationToken.useMutation();
-
-  const handlePushNotificationSubscription = useCallback(() => {
-    async function doAsync() {
-      const permission = await Notification.requestPermission();
-      if (permission === 'granted') {
-        console.log('Notification permission granted.');
-        const token = await getFirebaseMessagingToken();
-        await registerClientRegistrationToken.mutateAsync({
-          id: 'none',
-          token,
-        });
-      }
-    }
-
-    doAsync().catch(console.error);
-  }, [registerClientRegistrationToken]);
 
   useEffect(() => {
     app.user.notifications.isLoaded.once('redraw', forceRerender);
@@ -101,19 +78,6 @@ const NotificationSettingsPage = () => {
           Notification settings for all new threads, comments, mentions, likes,
           and chain events in the following communities.
         </CWText>
-
-        {enableKnockPushNotifications && (
-          <div>
-            <CWText type="h5">Push Notifications</CWText>
-
-            <p>
-              <CWButton
-                label="Subscribe to Push Notifications"
-                onClick={handlePushNotificationSubscription}
-              />
-            </p>
-          </div>
-        )}
 
         <div className="email-management-section">
           <div className="text-description">
