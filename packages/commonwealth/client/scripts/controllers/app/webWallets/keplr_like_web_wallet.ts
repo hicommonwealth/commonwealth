c1352cca23c96f9558e59e06bea6c31bcae88da6
Raymond Z <raykyri@gmail.com>
2024-06-10 14:21:14 -0400

    Add osmosis chain id to local chainnode if it's missing; use osmosis as default for keplr_like_web_wallet canvas signatures

--- packages/commonwealth/client/scripts/controllers/app/webWallets/keplr_like_web_wallet.ts
+++ packages/commonwealth/client/scripts/controllers/app/webWallets/keplr_like_web_wallet.ts
@@ -85,12 +85,12 @@ class KeplrLikeWebWalletController implements IWebWallet<AccountData> {
 
   public getSessionSigner() {
     return new CosmosSignerCW({
-      bech32Prefix: app.chain?.meta.bech32Prefix,
+      bech32Prefix: app.chain?.meta.bech32Prefix || 'osmo',
       signer: {
         type: 'amino',
         signAmino: window.wallet.signAmino,
         getAddress: () => this.accounts[0].address,
-        getChainId: () => this.getChainId(),
+        getChainId: () => this.getChainId() || 'osmosis-1',
       },
     });
   }
