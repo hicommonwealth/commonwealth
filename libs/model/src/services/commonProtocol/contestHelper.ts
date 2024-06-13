cdc6f543d99f9a7a2eef42103aa26d8c161b9539
ianrowan <i.rowan299@gmail.com>
2024-06-13 14:57:32 -0500

    implementation

--- libs/model/src/services/commonProtocol/contestHelper.ts
+++ libs/model/src/services/commonProtocol/contestHelper.ts
@@ -344,3 +344,34 @@ export const voteContentBatch = async (
     return Promise.allSettled(promises);
   });
 };
+
+export const rollOverContest = async (
+  rpcNodeUrl: string,
+  contest: string,
+  oneOff: boolean,
+): Promise<boolean> => {
+  const web3 = await createWeb3Provider(rpcNodeUrl);
+  const contestInstance = new web3.eth.Contract(
+    contestABI as AbiItem[],
+    contest,
+  );
+
+  const contractCall = oneOff
+    ? contestInstance.methods.endContest()
+    : contestInstance.methods.newContest();
+
+  let gasResult;
+  try {
+    gasResult = await contractCall.estimateGas({
+      from: web3.eth.defaultAccount,
+    });
+  } catch {
+    return false;
+  }
+
+  await contractCall.send({
+    from: web3.eth.defaultAccount,
+    gas: gasResult.toString(),
+  });
+  return true;
+};
