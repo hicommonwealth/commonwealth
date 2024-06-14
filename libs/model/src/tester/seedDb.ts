9f717ca42f4616645e72e31239192022ead0e462
Malik Zulqurnain <51641047+mzparacha@users.noreply.github.com>
2024-06-14 13:02:18 -0400

    Show welcome modal to new signups only (#8135)
    * Only show welcome modal to new users/signups
    
    * Fix training cards showing after dismiss
    
    * Fix CI
    
    * Fix CI
    
    * Updated `isWelcomeOnboardFlowComplete` to `is_welcome_onboard_flow_complete`

--- libs/model/src/tester/seedDb.ts
+++ libs/model/src/tester/seedDb.ts
@@ -27,11 +27,13 @@ export const seedDb = async () => {
         email: 'drewstone329@gmail.com',
         emailVerified: true,
         isAdmin: true,
+        is_welcome_onboard_flow_complete: true,
       },
       {
         email: 'temp@gmail.com',
         emailVerified: true,
         isAdmin: true,
+        is_welcome_onboard_flow_complete: true,
       },
     ]);
 
