513477f072d3c9aa4e9f4143ef952533b5c7be8a
Timothee Legros <timothee@ualberta.ca>
2024-06-17 21:41:18 +0300

    fix migration for empty DB

--- packages/commonwealth/server/migrations/20240617181557-fix-composable-finance-cn.js
+++ packages/commonwealth/server/migrations/20240617181557-fix-composable-finance-cn.js
@@ -4,7 +4,7 @@
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.sequelize.transaction(async (transaction) => {
-      const res = await queryInterface.sequelize.query(
+      let res = await queryInterface.sequelize.query(
         `
           SELECT id
           FROM "ChainNodes"
@@ -12,6 +12,30 @@ module.exports = {
       `,
         { transaction, type: 'SELECT', raw: true },
       );
+
+      if (!res.length) {
+        await queryInterface.bulkInsert(
+          'ChainNodes',
+          [
+            {
+              url: 'https://rpc-composable-ia.cosmosia.notional.ventures',
+              balance_type: 'cosmos',
+              name: 'Composable Finance',
+              cosmos_chain_id: 'composable',
+            },
+          ],
+          { transaction },
+        );
+
+        res = await queryInterface.sequelize.query(
+          `
+          SELECT id
+          FROM "ChainNodes"
+          WHERE cosmos_chain_id = 'composable';
+      `,
+          { transaction, type: 'SELECT', raw: true },
+        );
+      }
 
       await queryInterface.bulkUpdate(
         'Communities',
