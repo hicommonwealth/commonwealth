ad3a598080e668ebe58375d5e812b0fed971e44e
Timothee Legros <timothee@ualberta.ca>
2024-06-24 22:40:51 +0300

    update migration

--- packages/commonwealth/server/migrations/20240621195630-rotate-alchemy-keys.js
+++ packages/commonwealth/server/migrations/20240621195630-rotate-alchemy-keys.js
@@ -15,13 +15,32 @@ module.exports = {
       );
     }
 
-    await queryInterface.sequelize.query(`
-      UPDATE "ChainNodes"
-      SET url = regexp_replace(url, '\\/([^\\/]*)$', '/${publicKey}'),
-          alt_wallet_url = regexp_replace(url, '\\/([^\\/]*)$', '/${publicKey}'),
-          private_url = regexp_replace(url, '\\/([^\\/]*)$', '/${privateKey}')
-      WHERE url LIKE '%.g.alchemy%' OR private_url LIKE '%.g.alchemy%';
-    `);
+    await queryInterface.sequelize.transaction(async (transaction) => {
+      await queryInterface.sequelize.query(
+        `
+        UPDATE "ChainNodes"
+        SET url = regexp_replace(url, '\\/([^\\/]*)$', '/${publicKey}'),
+            alt_wallet_url = regexp_replace(url, '\\/([^\\/]*)$', '/${publicKey}'),
+            private_url = regexp_replace(url, '\\/([^\\/]*)$', '/${privateKey}')
+        WHERE url LIKE '%.g.alchemy%' OR private_url LIKE '%.g.alchemy%';
+      `,
+        { transaction },
+      );
+
+      // Eth mainnet node is still using a legacy Alchemy API url
+      await queryInterface.bulkUpdate(
+        'ChainNodes',
+        {
+          url: `https://eth-mainnet.g.alchemy.com/v2/${publicKey}`,
+          alt_wallet_url: `https://eth-mainnet.g.alchemy.com/v2/${publicKey}`,
+          private_url: `https://eth-mainnet.g.alchemy.com/v2/${privateKey}`,
+        },
+        {
+          eth_chain_id: 1,
+        },
+        { transaction },
+      );
+    });
   },
 
   async down(queryInterface, Sequelize) {},
