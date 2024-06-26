9d3b73fdc6a840271aadadd5c84a70be388ad715
Raymond Z <raykyri@gmail.com>
2024-06-25 17:04:42 -0700

    revert changes to chain ids

--- packages/commonwealth/client/scripts/controllers/app/webWallets/keplr_ethereum_web_wallet.ts
+++ packages/commonwealth/client/scripts/controllers/app/webWallets/keplr_ethereum_web_wallet.ts
@@ -100,7 +100,7 @@ class EVMKeplrWebWalletController implements IWebWallet<AccountData> {
           const { data: addressData } = fromBech32(this.accounts[0].address);
           return `0x${bytesToHex(addressData)}`;
         },
-        getChainId: () => this._chainId || 'cosmoshub-1',
+        getChainId: () => this._chainId || 'evmos_9001-2',
       },
     });
   }
