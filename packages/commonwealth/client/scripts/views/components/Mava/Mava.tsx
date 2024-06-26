9dbd33af505f3a1f870584b9fc71d3bf6d06d4fc
Kevin Burton <burtonator@gmail.com>
2024-06-26 11:40:49 -0700

    cleanup.

--- packages/commonwealth/client/scripts/views/components/Mava/Mava.tsx
+++ packages/commonwealth/client/scripts/views/components/Mava/Mava.tsx
@@ -1,9 +1,6 @@
 import { memo } from 'react';
 import './Mava.scss';
 
-// eslint-disable-next-line no-var, @typescript-eslint/no-explicit-any
-declare var window: any;
-
 export const Mava = memo(function Mava() {
   return null;
 });
