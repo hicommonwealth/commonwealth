7ee9d5afe75cf2696fd752bdcce1e1afa0edcae5
Marcin <maslankam92@gmail.com>
2024-06-26 18:00:49 +0200

    fee manager balance

--- packages/commonwealth/client/scripts/views/pages/CommunityManagement/Contests/AdminContestsPage/AdminContestsPage.tsx
+++ packages/commonwealth/client/scripts/views/pages/CommunityManagement/Contests/AdminContestsPage/AdminContestsPage.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { useCommonNavigate } from 'navigation/helpers';
 import app from 'state';
 import Permissions from 'utils/Permissions';
+import { useCommunityStake } from 'views/components/CommunityStake';
 import { CWText } from 'views/components/component_kit/cw_text';
 import { CWButton } from 'views/components/component_kit/new_designs/CWButton';
 import CWPageLayout from 'views/components/component_kit/new_designs/CWPageLayout';
@@ -10,6 +11,7 @@ import { PageNotFound } from 'views/pages/404';
 
 import ContestsList from '../ContestsList';
 import useCommunityContests from '../useCommunityContests';
+import FeeManagerBanner from './FeeManagerBanner';
 
 import './AdminContestsPage.scss';
 
@@ -17,6 +19,10 @@ const AdminContestsPage = () => {
   const navigate = useCommonNavigate();
 
   const isAdmin = Permissions.isSiteAdmin() || Permissions.isCommunityAdmin();
+
+  const ethChainId = app?.chain?.meta?.ChainNode?.ethChainId;
+  const { stakeData } = useCommunityStake();
+  const namespace = stakeData?.Community?.namespace;
 
   const {
     stakeEnabled,
@@ -28,6 +34,9 @@ const AdminContestsPage = () => {
   if (!app.isLoggedIn() || !isAdmin) {
     return <PageNotFound />;
   }
+
+  const showBanner =
+    stakeEnabled && isContestAvailable && ethChainId && namespace;
 
   return (
     <CWPageLayout>
@@ -43,6 +52,10 @@ const AdminContestsPage = () => {
             />
           )}
         </div>
+
+        {showBanner && (
+          <FeeManagerBanner ethChainId={ethChainId} namespace={namespace} />
+        )}
 
         <ContestsList
           contests={contestsData}
