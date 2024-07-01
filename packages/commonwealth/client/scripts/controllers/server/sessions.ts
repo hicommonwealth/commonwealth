04e0b373e317a7e7441e39eca6ab6016ab7fa802
Raymond Z <raykyri@users.noreply.github.com>
2024-07-01 16:20:02 -0400

    Fixes for session keys after v0.10 Canvas upgrade (#8210)
    * fixes for keplr/metamask-evm login, fix for signed-out state when ENFORCE_SESSION_KEYS is off
    
    * fixes for SessionRevalidationModal
    
    * copy and design revisions for SessionRevalidationModal
    
    * don't show SessionRevalidationModal if ENFORCE_SESSION_KEYS isn't set
    
    * type fix
    
    * revert changes to chain ids
    
    * only validate comment signature validity if enforceSessionKeys = true
    
    * revert reduced verifyAddress requirements from v0.10 upgrade
    
    * remove error message when client-side verification fails
    
    * remove @ts-expect-error StrictNullChecks where possible
    
    * lint fix
    
    * change variable name for clarity
    
    * upgrade to canvas v0.10.0-beta.3 to catch siwe signatures with incorrect topic
    
    * add verifyAddress tests for ethereum
    
    * lint fixes
    
    * Update packages/commonwealth/server/util/verifySessionSignature.ts
    
    Co-authored-by: Timothee Legros <62490329+timolegros@users.noreply.github.com>
    
    * Update packages/commonwealth/server/util/verifySessionSignature.ts
    
    Co-authored-by: Timothee Legros <62490329+timolegros@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Jake Naviasky <jake@commonwealth.im>
    Co-authored-by: Timothee Legros <62490329+timolegros@users.noreply.github.com>

--- packages/commonwealth/client/scripts/controllers/server/sessions.ts
+++ packages/commonwealth/client/scripts/controllers/server/sessions.ts
@@ -100,7 +100,7 @@ async function sign(
   call: string,
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   args: any,
-): Promise<CanvasSignResult> {
+): Promise<CanvasSignResult | null> {
   const address = getCaip2Address(address_);
   const sessionSigners = getSessionSigners();
   for (const signer of sessionSigners) {
@@ -123,6 +123,9 @@ async function sign(
         address: lookupAddress,
       });
       if (!savedSessionMessage) {
+        if (!app.config.enforceSessionKeys) {
+          return null;
+        }
         throw new SessionKeyError({
           name: 'Authentication Error',
           message: `No session found for address ${address}`,
@@ -136,6 +139,9 @@ async function sign(
       if (session.duration !== null) {
         const sessionExpirationTime = session.timestamp + session.duration;
         if (Date.now() > sessionExpirationTime) {
+          if (!app.config.enforceSessionKeys) {
+            return null;
+          }
           throw new SessionKeyError({
             name: 'Authentication Error',
             message: `Session expired for address ${address}`,
