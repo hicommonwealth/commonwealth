284011390e49080a5531e4e6bc0b4ca4a3a3f03f
israellund <israelmlund@gmail.com>
2024-07-03 11:37:33 -0400

    added export markdown functionality to proposals

--- packages/commonwealth/client/scripts/views/pages/view_proposal/json_display.tsx
+++ packages/commonwealth/client/scripts/views/pages/view_proposal/json_display.tsx
@@ -1,41 +1,100 @@
-import React from 'react';
-import ReactJson from 'react-json-view';
-
+import { CoinObject } from 'client/scripts/controllers/chain/cosmos/types';
 import 'components/proposals/json_display.scss';
-import { CWText } from '../../components/component_kit/cw_text';
+import React from 'react';
 import { CWDivider } from '../../components/component_kit/cw_divider';
+import { CWText } from '../../components/component_kit/cw_text';
+import { CWButton } from '../../components/component_kit/new_designs/CWButton';
+import { CWTooltip } from '../../components/component_kit/new_designs/CWTooltip';
+import { openConfirmation } from '../../modals/confirmation_modal';
+import JSONViewer from './json_viewer';
 
+interface DataType {
+  title?: string;
+  details?: string;
+  recipient?: string;
+  amount?: CoinObject[];
+}
 type JSONDisplayProps = {
-  data: object;
+  data: DataType;
   title?: string;
 };
 
-const JSONViewer = ({ data }: { data: any }) => (
-  <div className="Blob">
-    <ReactJson
-      src={data}
-      theme="grayscale:inverted"
-      style={{
-        background: 'inherit',
-      }}
-      name={false}
-      displayDataTypes={false}
-      enableClipboard={false}
-      displayObjectSize={false}
-      quotesOnKeys={false}
-      collapsed={true}
-    />
-  </div>
-);
-
 export const JSONDisplay = ({ data, title }: JSONDisplayProps) => {
+  const handleExport = () => {
+    const dataTitle = data.title || 'Proposal';
+    let markdownContent = `# ${dataTitle}\n\n`;
+
+    // Iterate over all keys in data object
+    Object.keys(data).forEach((key) => {
+      const value = data[key];
+
+      // Append key-value pair to markdown content
+      markdownContent += `${key}\n\n`;
+      markdownContent += `${value}\n\n`;
+    });
+
+    // Create a Blob from the markdown content
+    const blob = new Blob([markdownContent], { type: 'text/markdown' });
+
+    // Create a temporary link element
+    const link = document.createElement('a');
+    link.href = URL.createObjectURL(blob);
+    link.download = `${dataTitle}.md`;
+
+    // Append the link to the body (required for Firefox)
+    document.body.appendChild(link);
+
+    // Trigger the download
+    link.click();
+
+    // Remove the link from the document
+    document.body.removeChild(link);
+
+    // Release the object URL
+    URL.revokeObjectURL(link.href);
+  };
+
   return (
     <div className="BlobContainer">
       {title && (
         <>
-          <CWText type="b2" fontWeight="medium" className="labelText">
-            {title}
-          </CWText>
+          <div className="title-and-button">
+            <CWText type="b2" fontWeight="medium" className="labelText">
+              {title}
+            </CWText>
+            <CWTooltip
+              placement="top"
+              content="Export as markdown file"
+              renderTrigger={(handleInteraction) => (
+                <div
+                  onMouseEnter={handleInteraction}
+                  onMouseLeave={handleInteraction}
+                  className="cta-button-container"
+                >
+                  <CWButton
+                    label="Export"
+                    onClick={() => {
+                      console.log('data: ', data);
+                      openConfirmation({
+                        title: 'Warning',
+                        description:
+                          'A markdown file will be downloaded automatically',
+                        buttons: [
+                          {
+                            label: 'Export markdown file',
+                            buttonType: 'primary',
+                            buttonHeight: 'sm',
+                            onClick: handleExport,
+                          },
+                        ],
+                      });
+                    }}
+                    buttonHeight="sm"
+                  />
+                </div>
+              )}
+            ></CWTooltip>
+          </div>
           <CWDivider />
         </>
       )}
---
+++ packages/commonwealth/client/scripts/views/pages/view_proposal/json_viewer.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactJson from 'react-json-view';
+
+const JSONViewer = ({ data }: { data: any }) => {
+  return (
+    <div className="Blob">
+      <ReactJson
+        src={data}
+        theme="grayscale:inverted"
+        style={{
+          background: 'inherit',
+        }}
+        name={false}
+        displayDataTypes={false}
+        enableClipboard={false}
+        displayObjectSize={false}
+        quotesOnKeys={false}
+        collapsed={true}
+      />
+    </div>
+  );
+};
+
+export default JSONViewer;
