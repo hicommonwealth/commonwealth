3ccb9549b21efcbc7b535ae377293156c5fe3f44
Malik Zulqurnain <malikzulqurnain2121@gmail.com>
2024-07-03 21:32:05 +0500

    Cleanup extra state in user controller

--- packages/commonwealth/client/scripts/controllers/server/user.ts
+++ packages/commonwealth/client/scripts/controllers/server/user.ts
@@ -1,35 +1,17 @@
-/* eslint-disable @typescript-eslint/no-empty-function */
-/* eslint-disable @typescript-eslint/ban-types */
-/* eslint-disable @typescript-eslint/consistent-type-imports */
-
-import Account from '../../models/Account';
-
 // eslint-disable-next-line
-import { EventEmitter } from 'events';
 import NotificationsController from './notifications';
 
 export class UserController {
-  // private _activeAccount: Account;
-  // public get activeAccount(): Account {
-  //   return this._activeAccount;
-  // }
-
-  public isFetched = new EventEmitter();
-
   private _jwt: string;
   public get jwt(): string {
     return this._jwt;
   }
 
-  private _setJWT(JWT: string): void {
+  public setJWT(JWT: string): void {
     this._jwt = JWT;
   }
 
-  public setJWT(JWT: string): void {
-    this._setJWT(JWT);
-  }
-
-  constructor() {}
+  constructor() { }
 
   // --- TODO: should this be migrated as well? -- since this will be deprecated
   private _notifications: NotificationsController =
