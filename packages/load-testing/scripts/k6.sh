fe3a757af60df9fa7839356bacb20c1e3faae6b6
Timothee Legros <timothee@ualberta.ca>
2024-07-09 00:47:32 +0200

    working k6 v0.52.0 via Docker on any env

--- packages/load-testing/scripts/k6.sh
+++ packages/load-testing/scripts/k6.sh
@@ -15,34 +15,23 @@ run_docker_command() {
     fi
 }
 
-# Determine the OS
-OS=$(uname -s)
-
-K6_BINARY="./k6"
-
-build_k6_binary() {
-    # Execute the appropriate command based on the OS
-    if [ "$OS" == "Linux" ]; then
-        # -u should normally be set to "$(id -u):$(id -g)" but this produces a permission error in some systems
-        run_docker_command run --rm -u root -v "${PWD}:/xk6" grafana/xk6 build --with github.com/grafana/xk6-sql --with github.com/grafana/xk6-ts
-    elif [ "$OS" == "Darwin" ]; then
-        # -u should normally be set to "$(id -u):$(id -g)" but this produces a permission error in some systems
-        run_docker_command run --rm -e GOOS=darwin -u root -v "${PWD}:/xk6" grafana/xk6 build --with github.com/grafana/xk6-sql --with github.com/grafana/xk6-ts
-    else
-        echo "Unsupported OS: $OS"
-        exit 1
-    fi
-}
-
-if [ ! -f "$K6_BINARY" ]; then
-    # Build k6 binary with Docker if it does not exist
-    build_k6_binary
-fi
-
-# If k6 binary exists, require a filepath to a .ts file
 if [ -z "$1" ]; then
-    echo "Usage: pnpm test test/<path_to_file>.spec.ts"
+    echo "Usage: pnpm test-load test/<path_to_file>.spec.ts"
     exit 1
 fi
 
-$K6_BINARY run "$1"
+
+ENVIRONMENT=${2:-local}
+
+if [[ "$ENVIRONMENT" != "local" && "$ENVIRONMENT" != "frick" && "$ENVIRONMENT" != "frack" && "$ENVIRONMENT" != "beta" ]]; then
+    echo "Error: Invalid argument. Accepted values are: local, frick, frack, or beta."
+    exit 1
+fi
+
+if [ "$ENVIRONMENT" == "local" ]; then
+    SERVER_URL='http://host.docker.internal:8080'
+else
+    SERVER_URL=$(heroku config:get SERVER_URL -a commonwealth-"$ENVIRONMENT")
+fi
+
+docker run --rm -v "${PWD}"/test:/test -i grafana/k6:0.52.0-with-browser run -e SERVER_URL="$SERVER_URL" --compatibility-mode=experimental_enhanced "$1"
