d353a5afb00e97dc1d5078dd9b10764efdb891b4
Timothee Legros <62490329+timolegros@users.noreply.github.com>
2024-07-10 01:09:32 +0200

    Remove xk6 (#8403)

--- packages/load-testing/test/util/config.ts
+++ packages/load-testing/test/util/config.ts
@@ -1,4 +1,5 @@
-export const SERVER_URL = __ENV.SERVER_URL ?? 'http://localhost:8080';
+export const SERVER_URL =
+  __ENV.SERVER_URL ?? 'http://host.docker.internal:8080';
 
 export const TRPC_API_URL = __ENV.TRPC_API_URL ?? `${SERVER_URL}/api/v1/rest`;
 
