38b607f535a5e2c456691a8121d40c790ca9155c
kassad <kurtis@commonwealth.im>
2024-07-11 05:55:34 -0700

    Fixed issue with failing tests

--- libs/model/src/utils/denormalizedCountUtils.ts
+++ libs/model/src/utils/denormalizedCountUtils.ts
@@ -1,8 +1,8 @@
 import { Transaction } from 'sequelize';
-import { ModelStatic } from 'sequelize/types/model';
+import Model, { ModelStatic } from 'sequelize/types/model';
 
 export const incrementProfileCount = async (
-  models: ModelStatic<?>,
+  models: ModelStatic<Model>,
   community_id: string,
   user_id: number,
   transaction: Transaction,
@@ -26,7 +26,7 @@ export const incrementProfileCount = async (
 };
 
 export const decrementProfileCount = async (
-  models: ModelStatic<?>,
+  models: ModelStatic<Model>,
   community_id: string,
   user_id: number,
   transaction: Transaction,
