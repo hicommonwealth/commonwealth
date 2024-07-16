b32ef12c9333af744e0eabcd82b800f2447c0fa9
Raymond Z <raykyri@gmail.com>
2024-07-16 02:39:41 -0400

    WIP construct SignedMessage

--- libs/shared/src/canvas/runtime/node.ts
+++ libs/shared/src/canvas/runtime/node.ts
@@ -1,4 +1,6 @@
 import { Canvas } from '@canvas-js/core';
+import { ed25519 } from '@canvas-js/signatures';
+
 import { getSessionSigners } from '../signers';
 import { CanvasSignedData } from '../types';
 import { contract, topic } from './contract';
@@ -29,17 +31,38 @@ export const applyCanvasSignedData = async (
   app: Canvas,
   data: CanvasSignedData,
 ) => {
+  console.log(data);
+
+  ed25519.verify(data.sessionMessageSignature, data.sessionMessage);
+  ed25519.verify(data.actionMessageSignature, data.actionMessage);
+  console.log('verified using scheme directly');
+
+  app.messageLog.verifySignature(
+    data.sessionMessageSignature,
+    data.sessionMessage,
+  );
+  app.messageLog.verifySignature(
+    data.actionMessageSignature,
+    data.actionMessage,
+  );
+  console.log('verified using log');
+
   try {
     const { id: idSession } = await app.insert(
       data.sessionMessageSignature,
       data.actionMessage,
     );
+    console.log('applied canvas session:', idSession);
+  } catch (err) {
+    console.log('could not apply canvas session:', err);
+  }
+  try {
     const { id: idAction } = await app.insert(
       data.actionMessageSignature,
       data.actionMessage,
     );
-    console.log('applied canvas action/session:', idSession, idAction);
+    console.log('applied canvas action:', idAction);
   } catch (err) {
-    console.log('could not apply canvas action/session:', err);
+    console.log('could not apply canvas action:', err);
   }
 };
