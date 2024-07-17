890ccd118a38f6bbe36533fcdfa651ea0484cf7f
rotorsoft <rotorsoft@outlook.com>
2024-07-17 17:07:48 -0400

    add libs eslint rules

--- libs/eslint-plugin/index.cjs
+++ libs/eslint-plugin/index.cjs
@@ -2,6 +2,32 @@ module.exports = {
   name: 'eslint-plugin-common',
   version: '1.0.0',
   rules: {
+    'no-self-imports': {
+      meta: {
+        type: 'problem',
+        docs: {
+          description: 'Prevents self importing libraries',
+          recommended: true,
+        },
+      },
+      create(context) {
+        return {
+          ImportDeclaration(node) {
+            const path = context.getFilename();
+            const lib = path.match(/\/libs\/(.*?)\/src\//);
+            if (lib) {
+              const selfimport = `@hicommonwealth/${lib[1]}`;
+              if (selfimport === node.source.value)
+                context.report({
+                  node,
+                  message: `Avoid self importing '${node.source.value}'`,
+                });
+            }
+          },
+        };
+      },
+    },
+
     'no-server-imports': {
       meta: {
         type: 'problem',
