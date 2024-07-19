bb9785e1568242b7173c1d935d41c04ff7ad9e17
Raymond Z <raykyri@gmail.com>
2024-07-19 01:45:17 -0400

    dids for author

--- libs/shared/src/canvas/runtime/contract.ts
+++ libs/shared/src/canvas/runtime/contract.ts
@@ -41,10 +41,10 @@ export const contract = {
     },
   },
   actions: {
-    thread(db, { community, title, body, link, topic }, { address, id, timestamp }) {
+    thread(db, { community, title, body, link, topic }, { did, id, timestamp }) {
       db.set("threads", {
         id: id,
-        author: address,
+        author: did,
         community,
         title,
         body,
@@ -54,21 +54,21 @@ export const contract = {
       });
     },
     // TODO: not implemented (packages/commonwealth/server/routes/threads/update_thread_handler.ts)
-    async updateThread(db, { thread_id, title, body }, { address, id }) {
+    async updateThread(db, { thread_id, title, body }, { did, id }) {
       const t = await db.get("threads", thread_id);
       if (!t || !t.id) throw new Error("invalid thread");
       db.set('threads', { id: t.id, title, body });
     },
     // TODO: signed on client, not verified on server (packages/commonwealth/server/routes/threads/delete_thread_handler.ts)
-    async deleteThread(db, { thread_id }, { address, id }) {
+    async deleteThread(db, { thread_id }, { did, id }) {
       const t = await db.get("threads", thread_id);
       if (!t || !t.id) throw new Error("invalid thread");
       db.delete("threads", t.id.toString());
     },
-    comment(db, { thread_id, body, parent_comment_id }, { address, id, timestamp }) {
+    comment(db, { thread_id, body, parent_comment_id }, { did, id, timestamp }) {
       db.set("comments", {
         id: id,
-        author: address,
+        author: did,
         thread_id, // TODO: this should be the thread's canvas_hash/cid
         body,
         parent_comment_id,
@@ -76,53 +76,53 @@ export const contract = {
       });
     },
     // TODO: not implemented (packages/commonwealth/server/routes/comments/update_comment_handler.ts)
-    async updateComment(db, { comment_id, body }, { address, id }) {
+    async updateComment(db, { comment_id, body }, { did, id }) {
       const c = await db.get("comments", comment_id);
       if (!c || !c.id) throw new Error("invalid comment");
       db.set("comments", { id: c.id.toString(), body });
     },
     // TODO: signed on client, not verified on server (packages/commonwealth/server/routes/comments/delete_comment_handler.ts)
-    async deleteComment(db, { comment_id }, { address, id }) {
+    async deleteComment(db, { comment_id }, { did, id }) {
       const c = await db.get("comments", comment_id);
       if (!c || !c.id) throw new Error("invalid comment");
       db.delete("comments", c.id.toString());
     },
-    reactThread(db, { thread_id, value }, { address, id }) {
+    reactThread(db, { thread_id, value }, { did, id }) {
       if (value !== 'like' && value !== 'dislike') {
         throw new Error('Invalid reaction');
       }
       db.set("thread_reactions", {
-        id: `${thread_id}/${address}`,
-        author: address,
+        id: `${thread_id}/${did}`,
+        author: did,
         thread_id,
         value,
       });
     },
     // TODO: signed on client, not verified on server (packages/commonwealth/server/routes/threads/delete_thread_reaction_handler.ts)
-    unreactThread(db, { thread_id, value }, { address, id }) {
+    unreactThread(db, { thread_id, value }, { did, id }) {
       db.set("thread_reactions", {
-        id: `${thread_id}/${address}`,
-        author: address,
+        id: `${thread_id}/${did}`,
+        author: did,
         thread_id,
         value: null,
       });
     },
-    reactComment(db, { comment_id, value }, { address, id }) {
+    reactComment(db, { comment_id, value }, { did, id }) {
       if (value !== 'like' && value !== 'dislike') {
         throw new Error('Invalid reaction');
       }
       db.set("comment_reactions", {
-        id: `${comment_id}/${address}`,
-        author: address,
+        id: `${comment_id}/${did}`,
+        author: did,
         comment_id,
         value,
       });
     },
     // TODO: signed on client, not verified on server (packages/commonwealth/server/routes/comments/delete_comment_reaction_handler.ts)
-    unreactComment(db, { comment_id, value }, { address, id }) {
+    unreactComment(db, { comment_id, value }, { did, id }) {
       db.set("comment_reactions", {
-        id: `${comment_id}/${address}`,
-        author: address,
+        id: `${comment_id}/${did}`,
+        author: did,
         comment_id,
         value: null,
       });
