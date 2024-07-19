07214af0c0cabe722f6bee879b8612a7c9ea2638
Malik Zulqurnain <51641047+mzparacha@users.noreply.github.com>
2024-07-19 12:19:55 -0400

    Migrated `/viewCount` calls to react query (#8464)
    * Migrated `/viewCount` calls to react query
    
    * Updated import path

--- packages/commonwealth/client/scripts/state/api/config.ts
+++ packages/commonwealth/client/scripts/state/api/config.ts
@@ -37,6 +37,7 @@ export const ApiEndpoints = {
   searchProfiles: (searchTerm: string) => `/profiles?search=${searchTerm}`,
   searchChains: (searchTerm: string) => `/communities?search=${searchTerm}`,
   REMOVE_DISCORD_BOT_CONFIG: '/removeDiscordBotConfig',
+  VIEW_COUNT: '/viewCount',
   FETCH_USER_ACTIVITY: '/viewUserActivity',
   FETCH_GLOBAL_ACTIVITY: '/viewGlobalActivity',
 };
