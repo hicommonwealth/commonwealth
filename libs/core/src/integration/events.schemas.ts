939b7b42401a0fdbbcdc47b2d4f7a99193796df9
Ryan Bennett <126922418+rbennettcw@users.noreply.github.com>
2024-07-19 11:29:41 -0700

    Add contest metadata to events (#8370)
    * add contest metadata to events
    
    * only include contest address
    
    * move to seperate query
    
    * added ended condition
    
    * fix query result type
    
    * fix db import
    
    * fix import
    
    * move helper to utils
    
    * fix test
    
    * fix test
    
    ---------
    
    Co-authored-by: rotorsoft <rotorsoft@outlook.com>

--- libs/core/src/integration/events.schemas.ts
+++ libs/core/src/integration/events.schemas.ts
@@ -9,8 +9,12 @@ import {
 } from '@hicommonwealth/schemas';
 import { z } from 'zod';
 
-export const ThreadCreated = Thread;
-export const ThreadUpvoted = Reaction;
+export const ThreadCreated = Thread.extend({
+  contestManagers: z.array(z.object({ contest_address: z.string() })).nullish(),
+});
+export const ThreadUpvoted = Reaction.extend({
+  contestManagers: z.array(z.object({ contest_address: z.string() })).nullish(),
+});
 export const CommentCreated = Comment;
 export const GroupCreated = z.object({
   groupId: z.string(),
