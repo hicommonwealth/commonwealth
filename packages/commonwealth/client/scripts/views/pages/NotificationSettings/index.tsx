3561a10665225f98371f22a42d9bdae6ab59d2da
Kevin Burton <burtonator@gmail.com>
2024-07-24 09:49:04 -0700

    only enable push notification on Safari with the PWA enabled.

--- packages/commonwealth/client/scripts/views/pages/NotificationSettings/index.tsx
+++ packages/commonwealth/client/scripts/views/pages/NotificationSettings/index.tsx
@@ -12,7 +12,7 @@ import {
 import { PageNotFound } from 'views/pages/404';
 import { CommunityEntry } from 'views/pages/NotificationSettings/CommunityEntry';
 import { PushNotificationsToggle } from 'views/pages/NotificationSettings/PushNotificationsToggle';
-import { supportsPushNotifications } from 'views/pages/NotificationSettings/supportsPushNotifications';
+import { useSupportsPushNotifications } from 'views/pages/NotificationSettings/useSupportsPushNotifications';
 import { useThreadSubscriptions } from 'views/pages/NotificationSettings/useThreadSubscriptions';
 import { z } from 'zod';
 import { CWText } from '../../components/component_kit/cw_text';
@@ -23,6 +23,7 @@ import './index.scss';
 type NotificationSection = 'community-alerts' | 'subscriptions';
 
 const NotificationSettings = () => {
+  const supportsPushNotifications = useSupportsPushNotifications();
   const threadSubscriptions = useThreadSubscriptions();
   const communityAlerts = useCommunityAlertsQuery();
   const enableKnockPushNotifications = useFlag('knockPushNotifications');
@@ -62,7 +63,7 @@ const NotificationSettings = () => {
           Manage the emails and alerts you receive about your activity
         </CWText>
 
-        {enableKnockPushNotifications && supportsPushNotifications() && (
+        {enableKnockPushNotifications && supportsPushNotifications && (
           <div>
             <CWText type="h5">Push Notifications</CWText>
 
