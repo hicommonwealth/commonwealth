a5f79f3940b2f749160e2fbe63d936a22c8bd69d
Timothee Legros <timothee@ualberta.ca>
2024-07-25 01:58:03 +0200

    TODOs

--- packages/commonwealth/server/scripts/releasePhaseEnvCheck.ts
+++ packages/commonwealth/server/scripts/releasePhaseEnvCheck.ts
@@ -6,6 +6,8 @@ const log = logger(__filename);
 if (import.meta.url.endsWith(process.argv[1])) {
   import('../config')
     .then(() => {
+      // TODO: Add checks for unused env var
+      // TODO: Add warning for manually set client FLAGs when APP_ENV !== 'local'/'CI'
       log.info('Environment variables are properly configured');
       // eslint-disable-next-line @typescript-eslint/no-floating-promises
       dispose()('EXIT', true);
