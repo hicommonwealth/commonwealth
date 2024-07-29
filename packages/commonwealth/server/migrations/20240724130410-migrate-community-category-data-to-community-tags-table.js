57769bb54f68512681453632becb43d9a2baa15c
Malik Zulqurnain <malikzulqurnain2121@gmail.com>
2024-07-29 16:19:33 +0500

    Simplified logic for communty->category to community_tags->tag_id migration

--- packages/commonwealth/server/migrations/20240724130410-migrate-community-category-data-to-community-tags-table.js
+++ packages/commonwealth/server/migrations/20240724130410-migrate-community-category-data-to-community-tags-table.js
@@ -4,49 +4,26 @@
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.sequelize.transaction(async (t) => {
-      // Find tags ids for `DAO` and `DeFi`
-      const tags = await queryInterface.sequelize.query(
-        `SELECT id, name FROM "Tags" WHERE name IN ('DAO', 'DeFi');`,
+      // Insert tag_id of each community category ('DAO' or 'DeFi') into CommunityTags
+      await queryInterface.sequelize.query(
+        `
+          INSERT INTO "CommunityTags" (community_id, tag_id, created_at, updated_at)
+          SELECT c.id, t.id, NOW(), NOW()
+            FROM "Communities" c
+            JOIN "Tags" t
+            ON (t.name = 'DAO' AND EXISTS (
+                SELECT 1
+                FROM jsonb_array_elements_text(c.category) AS elem
+                WHERE elem::text LIKE '%DAO%'
+              ))
+            OR (t.name = 'DeFi' AND EXISTS (
+                SELECT 1
+                FROM jsonb_array_elements_text(c.category) AS elem
+                WHERE elem::text LIKE '%DeFi%'
+              ))
+            WHERE c.category IS NOT NULL;
+        `,
         { transaction: t },
-      );
-      const DAO = tags[0].find((tag) => tag.name === 'DAO');
-      const DeFi = tags[0].find((tag) => tag.name === 'DeFi');
-
-      // Find all communities which have a category
-      const communities = await queryInterface.sequelize.query(
-        `SELECT id, category FROM "Communities" WHERE category IS NOT NULL;`,
-      );
-
-      // Insert each category ('DAO' or 'DeFi') into CommunityTags
-      await Promise.all(
-        communities[0].map(async (community) => {
-          const { id, category } = community;
-
-          if (category) {
-            if (category.includes('DAO')) {
-              await queryInterface.sequelize.query(
-                `INSERT INTO "CommunityTags" 
-                (community_id, tag_id, created_at, updated_at) 
-                VALUES 
-                ($1, $2, NOW(), NOW());`,
-                {
-                  bind: [id, DAO.id],
-                },
-              );
-            }
-            if (category.includes('DeFi')) {
-              await queryInterface.sequelize.query(
-                `INSERT INTO "CommunityTags" 
-                (community_id, tag_id, created_at, updated_at) 
-                VALUES 
-                ($1, $2, NOW(), NOW());`,
-                {
-                  bind: [id, DeFi.id],
-                },
-              );
-            }
-          }
-        }),
       );
     });
   },
