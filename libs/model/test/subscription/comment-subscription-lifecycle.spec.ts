3da1c926c3e7eaa3cf7c110206ce30d34b28fd37
Kevin Burton <burtonator@gmail.com>
2024-07-29 16:13:51 -0700

    revert this test.

--- libs/model/test/subscription/comment-subscription-lifecycle.spec.ts
+++ libs/model/test/subscription/comment-subscription-lifecycle.spec.ts
@@ -6,7 +6,6 @@ import { bootstrap_testing, seed } from 'model/src/tester';
 import { afterAll, afterEach, beforeAll, describe, test } from 'vitest';
 import z from 'zod';
 import { models } from '../../src/database';
-import { CommentSubscriptionInstance } from '../../src/models/comment_subscriptions';
 import {
   CreateCommentSubscription,
   DeleteCommentSubscription,
@@ -17,24 +16,19 @@ describe('Comment subscription lifecycle', () => {
   let actor: Actor;
   let commentOne: z.infer<typeof schemas.Comment> | undefined;
   let commentTwo: z.infer<typeof schemas.Comment> | undefined;
-  let community: z.infer<typeof schemas.Community> | undefined;
-  let thread: z.infer<typeof schemas.Thread> | undefined;
-  let address: z.infer<typeof schemas.Address> | undefined;
-
   beforeAll(async () => {
     await bootstrap_testing(true);
     const [user] = await seed('User', {
       isAdmin: false,
       selected_community_id: null,
     });
-
     const [node] = await seed('ChainNode', {
       url: 'https://ethereum-sepolia.publicnode.com',
       name: 'Sepolia Testnet',
       eth_chain_id: 11155111,
       balance_type: BalanceType.Ethereum,
     });
-    [community] = await seed('Community', {
+    const [community] = await seed('Community', {
       chain_node_id: node?.id,
       Addresses: [
         {
@@ -44,9 +38,7 @@ describe('Comment subscription lifecycle', () => {
       ],
     });
 
-    address = (community?.Addresses)![0];
-
-    [thread] = await seed('Thread', {
+    const [thread] = await seed('Thread', {
       address_id: community?.Addresses?.at(0)?.id,
       community_id: community?.id,
       topic_id: community?.topics?.at(0)?.id,
@@ -104,41 +96,9 @@ describe('Comment subscription lifecycle', () => {
       actor,
       payload: {},
     });
-
-    expect(thread).to.not.be.null;
-    // expect(address).to.not.be.null;
-
-    function updateStructure(
-      sub: CommentSubscriptionInstance,
-      comment: z.infer<typeof schemas.Comment>,
-    ) {
-      return {
-        ...sub.toJSON(),
-        Comment: {
-          ...comment,
-          // Thread: {
-          //   ...thread,
-          //   // Address: {
-          //   //   ...address
-          //   // }
-          // },
-          Thread: null,
-        },
-      };
-    }
-
-    console.log(
-      'FIXME: commentSubOne: ',
-      JSON.stringify(updateStructure(commentSubOne, commentOne!), null, 2),
-    );
-    console.log('FIXME: res: ', JSON.stringify(res![0], null, 2));
-
-    // This s giving me non-deterministic results. Sometimes the 'res' will have
-    // Thread, sometimes it is null.
-
     expect(res).to.have.deep.members([
-      updateStructure(commentSubOne, commentOne!),
-      updateStructure(commentSubTwo, commentTwo!),
+      commentSubOne.toJSON(),
+      commentSubTwo.toJSON(),
     ]);
   });
 
