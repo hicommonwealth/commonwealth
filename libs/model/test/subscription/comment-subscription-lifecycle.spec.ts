1489ff87a5425e5a3471e952a0db69b68cc80ae6
Kevin Burton <burtonator@gmail.com>
2024-07-29 11:47:18 -0700

    ...

--- libs/model/test/subscription/comment-subscription-lifecycle.spec.ts
+++ libs/model/test/subscription/comment-subscription-lifecycle.spec.ts
@@ -6,6 +6,7 @@ import { bootstrap_testing, seed } from 'model/src/tester';
 import { afterAll, afterEach, beforeAll, describe, test } from 'vitest';
 import z from 'zod';
 import { models } from '../../src/database';
+import { CommentSubscriptionInstance } from '../../src/models/comment_subscriptions';
 import {
   CreateCommentSubscription,
   DeleteCommentSubscription,
@@ -16,19 +17,26 @@ describe('Comment subscription lifecycle', () => {
   let actor: Actor;
   let commentOne: z.infer<typeof schemas.Comment> | undefined;
   let commentTwo: z.infer<typeof schemas.Comment> | undefined;
+  let community: z.infer<typeof schemas.Community> | undefined;
+  let thread: z.infer<typeof schemas.Thread> | undefined;
+  let address: z.infer<typeof schemas.Address> | undefined;
+
   beforeAll(async () => {
     await bootstrap_testing(true);
     const [user] = await seed('User', {
       isAdmin: false,
       selected_community_id: null,
     });
+
+    address = community?.Addresses?.[0]!;
+
     const [node] = await seed('ChainNode', {
       url: 'https://ethereum-sepolia.publicnode.com',
       name: 'Sepolia Testnet',
       eth_chain_id: 11155111,
       balance_type: BalanceType.Ethereum,
     });
-    const [community] = await seed('Community', {
+    [community] = await seed('Community', {
       chain_node_id: node?.id,
       Addresses: [
         {
@@ -38,7 +46,7 @@ describe('Comment subscription lifecycle', () => {
       ],
     });
 
-    const [thread] = await seed('Thread', {
+    [thread] = await seed('Thread', {
       address_id: community?.Addresses?.at(0)?.id,
       community_id: community?.id,
       topic_id: community?.topics?.at(0)?.id,
@@ -96,9 +104,29 @@ describe('Comment subscription lifecycle', () => {
       actor,
       payload: {},
     });
+
+    expect(thread).to.not.be.null;
+    expect(address).to.not.be.null;
+
+    function updateStructure(comment: CommentSubscriptionInstance) {
+      return {
+        ...comment.toJSON(),
+        Comment: {
+          ...commentOne,
+          Thread: null,
+        },
+      };
+    }
+
+    console.log(
+      'FIXME: commentSubOne: ',
+      JSON.stringify(updateStructure(commentSubOne), null, 2),
+    );
+    console.log('FIXME: res: ', JSON.stringify(res![0], null, 2));
+
     expect(res).to.have.deep.members([
-      commentSubOne.toJSON(),
-      commentSubTwo.toJSON(),
+      updateStructure(commentSubOne),
+      updateStructure(commentSubTwo),
     ]);
   });
 
