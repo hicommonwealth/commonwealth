b1981c5387683f279ad501e072542ecedc3d6e23
Timothee Legros <timothee@ualberta.ca>
2024-08-02 00:18:15 +0200

    Create combined EvmEventSignatures object

--- libs/core/src/integration/events.ts
+++ libs/core/src/integration/events.ts
@@ -27,12 +27,14 @@ export enum EventNames {
   SubscriptionPreferencesUpdated = 'SubscriptionPreferencesUpdated',
 }
 
+// TODO: move all signatures to events.utils.ts
+
 export const EvmNamespaceFactoryEventSignatures = {
   NewContest:
     '0x990f533044dbc89b838acde9cd2c72c400999871cf8f792d731edcae15ead693',
   NewNamespace:
     '0x8870ba2202802ce285ce6bead5ac915b6dc2d35c8a9d6f96fa56de9de12829d5',
-};
+} as const;
 
 export const EvmRecurringContestEventSignatures = {
   ContestStarted:
@@ -41,7 +43,7 @@ export const EvmRecurringContestEventSignatures = {
     '0x2f0d66b98c7708890a982e2194479b066a117a6f9a8f418f7f14c6001965b78b',
   VoterVoted:
     '0x68d40dd5e34d499a209946f8e381c1258bdeff6dea4e96e9ab921da385c03667',
-};
+} as const;
 
 export const EvmSingleContestEventSignatures = {
   ContestStarted:
@@ -50,8 +52,19 @@ export const EvmSingleContestEventSignatures = {
     '0x2f0d66b98c7708890a982e2194479b066a117a6f9a8f418f7f14c6001965b78b',
   VoterVoted:
     '0xba2ce2b4fab99c4186fd3e0a8e93ffb61e332d0c4709bd01d01e7ac60631437a',
-};
+} as const;
 
 export const EvmCommunityStakingEventSignatures = {
   trade: '0xfc13c9a8a9a619ac78b803aecb26abdd009182411d51a986090f82519d88a89e',
-};
+} as const;
+
+export const EvmEventSignatures = {
+  NamespaceFactory: EvmNamespaceFactoryEventSignatures,
+  RecurringContest: EvmRecurringContestEventSignatures,
+  SingleContest: EvmSingleContestEventSignatures,
+  CommunityStake: EvmCommunityStakingEventSignatures,
+} as const;
+
+type Values<T> = T[keyof T];
+type NestedValues<T> = Values<{ [K in keyof T]: Values<T[K]> }>;
+export type EvmEventSignature = NestedValues<typeof EvmEventSignatures>;
