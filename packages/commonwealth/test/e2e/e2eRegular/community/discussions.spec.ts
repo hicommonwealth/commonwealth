d3ba493056c18b15e02e14f7d51c0245a6e7662b
Malik Zulqurnain <malikzulqurnain2121@gmail.com>
2024-08-01 19:28:05 +0500

    Refactored community discussions test

--- packages/commonwealth/test/e2e/e2eRegular/community/discussions.spec.ts
+++ packages/commonwealth/test/e2e/e2eRegular/community/discussions.spec.ts
@@ -1,5 +1,20 @@
+import { config } from '@hicommonwealth/core';
 import { test } from '@playwright/test';
+import { e2eSeeder, type E2E_Seeder } from '../../utils/e2eUtils';
 import { testPageCrash } from '../common/testPageCrash';
+import { discussionTests } from './discussionsTest';
+
+let seeder: E2E_Seeder;
+
+test.beforeAll(async () => {
+  seeder = await e2eSeeder();
+});
+
+test.beforeEach(async ({ page }) => {
+  await page.goto(
+    `${config.SERVER_URL}/${seeder.testChains[0].id}/discussions`,
+  );
+});
 
 test.describe('Test community discussions page', () => {
   testPageCrash('http://localhost:8080/dydx/proposal/discussion/:identifier');
@@ -11,4 +26,6 @@ test.describe('Test community discussions page', () => {
   testPageCrash('http://localhost:8080/dydx/discussion/:identifier');
 
   testPageCrash('http://localhost:8080/discussion/:identifier');
+
+  discussionTests(test);
 });
