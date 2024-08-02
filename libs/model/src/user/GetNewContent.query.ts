ee174ab93113be46edcb5f099f991518536e0688
Malik Zulqurnain <malikzulqurnain2121@gmail.com>
2024-08-02 21:39:04 +0500

    Optimized get content query

--- libs/model/src/user/GetNewContent.query.ts
+++ libs/model/src/user/GetNewContent.query.ts
@@ -1,7 +1,7 @@
 import { Query } from '@hicommonwealth/core';
 import * as schemas from '@hicommonwealth/schemas';
-import { Op, QueryTypes } from 'sequelize';
-import { models, sequelize } from '../database';
+import { QueryTypes } from 'sequelize';
+import { sequelize } from '../database';
 
 export function GetNewContent(): Query<typeof schemas.GetNewContent> {
   return {
@@ -10,63 +10,63 @@ export function GetNewContent(): Query<typeof schemas.GetNewContent> {
     body: async ({ actor }) => {
       const joinedCommunityIdsWithNewContent: string[] = [];
 
-      // get addresses for auth user
-      const addresses = await models.Address.findAll({
-        where: {
-          user_id: actor.user.id!,
-          verified: { [Op.not]: null },
-          last_active: { [Op.not]: null },
-        },
-      });
-
-      // find if there is new content for each community per user address
-      for (let i = 0; i < addresses.length; i++) {
-        const { community_id, last_active } = addresses[i];
-
-        // goto next address if current community address is already in content array
-        if (
-          !community_id ||
-          !last_active ||
-          joinedCommunityIdsWithNewContent.includes(community_id)
-        ) {
-          continue;
-        }
-
-        // get counts of threads and comments for current community address
-        const response = await sequelize.query<{ count: string }>(
-          `
-            SELECT (
+      // get ids of communities where user has membership and that community got new threads/comments
+      // since user's last active session per address (the address that user used to join that community)
+      const response = await sequelize.query<{ community_id: string }>(
+        `
+          WITH CommunityContentContent AS (
+            SELECT 
+              a.community_id,
               (
-                SELECT COUNT(*) 
-                  FROM "Threads" 
-                  WHERE "community_id" = :community_id 
-                  AND "created_at" > :last_active 
-                  AND "deleted_at" IS NULL
-              )
-              +
-              (
-                SELECT COUNT(*) 
-                FROM "Comments" 
-                WHERE "community_id" = :community_id 
-                AND "created_at" > :last_active 
-                AND "deleted_at" IS NULL
-              )
-            ) AS count;
-          `,
-          {
-            raw: true,
-            type: QueryTypes.SELECT,
-            replacements: {
-              community_id,
-              last_active: new Date(last_active)?.toISOString(),
-            },
+                (
+                  SELECT 
+                    COUNT(*) 
+                  FROM 
+                    "Threads" 
+                  WHERE 
+                    "community_id" = a.community_id 
+                    AND "created_at" > a.last_active 
+                    AND "deleted_at" IS NULL
+                )
+                +
+                (
+                  SELECT 
+                    COUNT(*) 
+                  FROM 
+                    "Comments" 
+                  WHERE 
+                    "community_id" = a.community_id 
+                    AND "created_at" > a.last_active 
+                    AND "deleted_at" IS NULL
+                )
+              ) AS content_content
+            FROM 
+              "Addresses" a
+            WHERE 
+              a.verified IS NOT NULL 
+              AND a.last_active IS NOT NULL 
+              AND a.user_id = :user_id
+          )
+          SELECT
+            community_id
+          FROM 
+            CommunityContentContent
+          WHERE
+            content_content > 0;
+        `,
+        {
+          raw: true,
+          type: QueryTypes.SELECT,
+          replacements: {
+            user_id: actor.user.id!,
           },
-        );
-        const count = parseInt(response[0].count); // query returns `count` as a string, convert it to int
+        },
+      );
 
-        // only add community id's to new content array
-        if (count > 0) joinedCommunityIdsWithNewContent.push(community_id);
-      }
+      // add unique community ids to content array
+      joinedCommunityIdsWithNewContent.push(
+        ...new Set(response.map((x) => x.community_id)),
+      );
 
       return { joinedCommunityIdsWithNewContent };
     },
