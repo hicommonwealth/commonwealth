17469ab07a7f130c689dfb6ac74243379abfb628
Ryan Bennett <ryan@common.xyz>
2024-08-09 08:35:57 -0700

    bulk create topics

--- libs/model/src/services/discourseImport/topics.ts
+++ libs/model/src/services/discourseImport/topics.ts
@@ -1,9 +1,7 @@
-import { models } from '@hicommonwealth/model';
-import { Topic } from '@hicommonwealth/schemas';
-import { QueryTypes, Sequelize, Transaction } from 'sequelize';
-import { z } from 'zod';
+import { models, TopicAttributes } from '@hicommonwealth/model';
+import { Op, QueryTypes, Sequelize, Transaction } from 'sequelize';
 
-export type CWTopicWithDiscourseId = z.infer<typeof Topic> & {
+export type CWTopicWithDiscourseId = TopicAttributes & {
   discourseCategoryId: number;
   created: boolean;
 };
@@ -27,32 +25,63 @@ class DiscourseQueries {
 }
 
 class CWQueries {
-  static createOrFindTopic = async (
-    name: string,
-    description: string,
-    communityId: string,
-    discourseCategoryId: number,
+  static bulkCreateTopics = async (
+    entries: {
+      name: string;
+      description: string;
+      communityId: string;
+      discourseCategoryId: number;
+    }[],
     { transaction }: { transaction: Transaction | null },
-  ): Promise<CWTopicWithDiscourseId> => {
-    const [topic, created] = await models.Topic.findOrCreate({
-      where: {
-        name,
-        community_id: communityId,
-      },
-      defaults: {
+  ): Promise<Array<CWTopicWithDiscourseId>> => {
+    const topicsToCreate: TopicAttributes[] = entries.map(
+      ({ name, description, communityId }) => ({
         name,
         description: description || '',
         community_id: communityId,
         featured_in_sidebar: true,
         featured_in_new_post: false,
+      }),
+    );
+
+    const existingTopics = await models.Topic.findAll({
+      where: {
+        [Op.or]: topicsToCreate.map((t) => ({
+          name: t.name,
+          community_id: t.community_id,
+        })),
       },
-      transaction,
     });
-    return {
-      ...(topic.get({ plain: true }) as z.infer<typeof Topic>),
-      discourseCategoryId,
-      created,
-    };
+
+    const filteredAddressesToCreate = topicsToCreate.filter(
+      (t) =>
+        !existingTopics.find(
+          (et) => t.name === et.name && t.community_id === et.community_id,
+        ),
+    );
+
+    const createdTopics = await models.Topic.bulkCreate(
+      filteredAddressesToCreate,
+      {
+        transaction,
+      },
+    );
+
+    return [
+      ...existingTopics.map((a) => ({
+        ...a.get({ plain: true }),
+        created: false,
+      })),
+      ...createdTopics.map((a) => ({
+        ...a.get({ plain: true }),
+        created: true,
+      })),
+    ].map((topic) => ({
+      ...topic,
+      discourseCategoryId: entries.find(
+        (e) => e.name === topic.name && e.communityId === topic.community_id,
+      )!.discourseCategoryId,
+    }));
   };
 }
 
@@ -61,18 +90,14 @@ export const createAllTopicsInCW = async (
   { communityId }: { communityId: string },
   { transaction }: { transaction: Transaction | null },
 ): Promise<Array<CWTopicWithDiscourseId>> => {
-  const discourseTopics = await DiscourseQueries.fetchCategories(
+  const discourseCategories = await DiscourseQueries.fetchCategories(
     discourseConnection,
   );
-  const promises = discourseTopics.map((discourseTopic) =>
-    CWQueries.createOrFindTopic(
-      // eslint-disable-next-line no-useless-escape
-      discourseTopic.name.replace(/[\/\\]/g, ' '),
-      discourseTopic.description,
-      communityId,
-      discourseTopic.id,
-      { transaction },
-    ),
-  );
-  return Promise.all(promises);
+  const entries = discourseCategories.map((discourseTopic) => ({
+    name: discourseTopic.name.replace(/[\/\\]/g, ' '),
+    description: discourseTopic.description,
+    communityId,
+    discourseCategoryId: discourseTopic.id,
+  }));
+  return CWQueries.bulkCreateTopics(entries, { transaction });
 };
