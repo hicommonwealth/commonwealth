e0b35f1f3b7596d17eb61401437457d1fe447e75
rotorsoft <rotorsoft@outlook.com>
2024-08-12 13:26:42 -0400

    clean schemas

--- libs/schemas/src/entities/reaction.schemas.ts
+++ libs/schemas/src/entities/reaction.schemas.ts
@@ -1,9 +1,9 @@
 import { z } from 'zod';
-import { PG_INT, zDate } from '../utils';
+import { PG_INT } from '../utils';
 
 // TODO: use this as single source of truth for model?
 export const Reaction = z.object({
-  id: PG_INT.nullish(),
+  id: PG_INT.optional(),
   address_id: PG_INT,
   reaction: z.enum(['like']),
   thread_id: PG_INT.nullish(),
@@ -12,6 +12,6 @@ export const Reaction = z.object({
   calculated_voting_weight: PG_INT.nullish(),
   canvas_signed_data: z.any().nullish(),
   canvas_hash: z.string().max(255).nullish(),
-  created_at: zDate.nullish(),
-  updated_at: zDate.nullish(),
+  created_at: z.coerce.date().optional(),
+  updated_at: z.coerce.date().optional(),
 });
