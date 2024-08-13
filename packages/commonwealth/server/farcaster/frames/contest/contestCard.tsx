8675cb10350918e9bbe40250e61a307a8611fcfa
Marcin <maslankam92@gmail.com>
2024-08-13 15:25:22 +0200

    Add comment info

--- packages/commonwealth/server/farcaster/frames/contest/contestCard.tsx
+++ packages/commonwealth/server/farcaster/frames/contest/contestCard.tsx
@@ -4,6 +4,10 @@ import { frames } from '../../config';
 import { CardWithText } from '../../utils';
 
 export const contestCard = frames(async () => {
+  // here we would need to be able to fetch data related to contest eg
+  // image, title, description, prizes
+  // check designs https://www.figma.com/design/NNqlhNPHvn0O96TCBIi6WU/Contests?node-id=960-3689&t=8ogN11dhaRqJP8ET-1
+
   return {
     image: CardWithText({ text: 'Contest' }),
     buttons: [
