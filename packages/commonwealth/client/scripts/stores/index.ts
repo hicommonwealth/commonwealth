6f008181463c35d7f4ac93937af6e86cf5b10360
Timothee Legros <timothee@ualberta.ca>
2024-08-20 17:40:07 +0300

    remove notifications store

--- packages/commonwealth/client/scripts/stores/index.ts
+++ packages/commonwealth/client/scripts/stores/index.ts
@@ -2,7 +2,6 @@ export { default as AccountsStore } from './AccountsStore';
 export { default as ChainStore } from './ChainStore';
 export { default as IdStore } from './IdStore';
 export { default as NewProfileStore } from './NewProfileStore';
-export { default as NotificationStore } from './NotificationStore';
 export { default as PersistentStore } from './PersistentStore';
 export { default as ProposalStore } from './ProposalStore';
 export { default as Store } from './Store';
