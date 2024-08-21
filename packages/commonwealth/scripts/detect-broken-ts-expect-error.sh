4090ce8e83431b2af214ec3701fa32c248f101c7
Kevin Burton <burtonator@gmail.com>
2024-08-21 11:53:19 -0700

    ...

--- packages/commonwealth/scripts/detect-broken-ts-expect-error.sh
+++ packages/commonwealth/scripts/detect-broken-ts-expect-error.sh
@@ -9,4 +9,4 @@ set -e
 
 # find . -type f \( -name "*.ts" -o -name "*.tsx" \) -exec sed -i '' 's/\/\/ @ts-expect-error .*//g' '{}' ';'
 
-find . -type f \( -name "*.ts" -o -name "*.tsx" \) -exec sed -i '' 's/\/\* @ts-expect-error .*//g' '{}' ';'
+find . -type f \( -name "*.ts" -o -name "*.tsx" \) -exec sed -i '' 's/{\/\* @ts-expect-error .*//g' '{}' ';'
