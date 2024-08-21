3dee6e8bd0238fd437cdf8645e9f82070bddd93d
Kevin Burton <burtonator@gmail.com>
2024-08-21 12:03:42 -0700

    ...

--- packages/commonwealth/scripts/detect-broken-ts-expect-error.sh
+++ packages/commonwealth/scripts/detect-broken-ts-expect-error.sh
@@ -13,4 +13,7 @@ find . -type f \( -name "*.ts" -o -name "*.tsx" \) -exec sed -i '' 's/\/\/ @ts-e
 # the tsx files have a slightly different syntax for JSX
 find . -type f \( -name "*.ts" -o -name "*.tsx" \) -exec sed -i '' 's/{\/\* @ts-expect-error .*//g' '{}' ';'
 
-# now turn off strict null checks
+# now turn off StrictNullChecks
+sed -i '' 's/"strictNullChecks": true/"strictNullChecks": false/g' tsconfig.json
+
+pnpm run build
