56e76ae74e70e2c9d0fd777131c42ebfa5d17824
Timothee Legros <timothee@ualberta.ca>
2024-08-27 00:00:55 +0300

    update thread emitNotif removal

--- packages/commonwealth/server/controllers/server_threads_methods/update_thread.ts
+++ packages/commonwealth/server/controllers/server_threads_methods/update_thread.ts
@@ -8,7 +8,6 @@ import {
   ThreadInstance,
   UserInstance,
 } from '@hicommonwealth/model';
-import { NotificationCategories, ProposalType } from '@hicommonwealth/shared';
 import _ from 'lodash';
 import {
   Op,
@@ -27,7 +26,6 @@ import {
 import { findAllRoles } from '../../util/roles';
 import { addVersionHistory } from '../../util/versioning';
 import { TrackOptions } from '../server_analytics_controller';
-import { EmitOptions } from '../server_notifications_methods/emit';
 import { ServerThreadsController } from '../server_threads_controller';
 
 export const Errors = {
@@ -69,11 +67,7 @@ export type UpdateThreadOptions = {
   discordMeta?: any;
 };
 
-export type UpdateThreadResult = [
-  ThreadAttributes,
-  EmitOptions[],
-  TrackOptions[],
-];
+export type UpdateThreadResult = [ThreadAttributes, TrackOptions[]];
 
 export async function __updateThread(
   this: ServerThreadsController,
@@ -395,31 +389,6 @@ export async function __updateThread(
     ],
   });
 
-  const now = new Date();
-  // build notifications
-  const allNotificationOptions: EmitOptions[] = [];
-
-  allNotificationOptions.push({
-    notification: {
-      categoryId: NotificationCategories.ThreadEdit,
-      data: {
-        created_at: now,
-        // @ts-expect-error StrictNullChecks
-        thread_id: +finalThread.id,
-        root_type: ProposalType.Thread,
-        // @ts-expect-error StrictNullChecks
-        root_title: finalThread.title,
-        // @ts-expect-error StrictNullChecks
-        community_id: finalThread.community_id,
-        // @ts-expect-error StrictNullChecks
-        author_address: finalThread.Address.address,
-        // @ts-expect-error StrictNullChecks
-        author_community_id: finalThread.Address.community_id,
-      },
-    },
-    excludeAddresses: [address.address],
-  });
-
   const updatedThreadWithComments = {
     // @ts-expect-error StrictNullChecks
     ...finalThread.toJSON(),
@@ -442,11 +411,7 @@ export async function __updateThread(
 
   delete updatedThreadWithComments.Comments;
 
-  return [
-    updatedThreadWithComments,
-    allNotificationOptions,
-    allAnalyticsOptions,
-  ];
+  return [updatedThreadWithComments, allAnalyticsOptions];
 }
 
 // -----
