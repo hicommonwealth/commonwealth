aa950af332c58a961cb5e311f78cd0e6daf15755
Kevin Burton <burtonator@gmail.com>
2024-08-26 13:55:43 -0700

    use the correct icons.

--- packages/commonwealth/client/scripts/views/pages/Editor/Editor.tsx
+++ packages/commonwealth/client/scripts/views/pages/Editor/Editor.tsx
@@ -1,9 +1,11 @@
 import {
   codeBlockPlugin,
   codeMirrorPlugin,
+  defaultSvgIcons,
   diffSourcePlugin,
   frontmatterPlugin,
   headingsPlugin,
+  IconKey,
   imagePlugin,
   linkDialogPlugin,
   linkPlugin,
@@ -27,6 +29,7 @@ import {
   ListChecks,
   ListDashes,
   ListNumbers,
+  Table,
   TextB,
   TextItalic,
   TextStrikethrough,
@@ -72,6 +75,53 @@ type EditorMode = 'desktop' | 'mobile';
 type EditorProps = {
   readonly mode?: EditorMode;
   readonly placeholder?: string;
+};
+
+const DEFAULT_ICON_SIZE = 22;
+
+const iconComponentFor = (name: IconKey) => {
+  // 'undo' | 'redo' | 'format_bold' | 'format_italic' |
+  // 'format_underlined' | 'code' | 'strikeThrough' | 'superscript' |
+  // 'subscript' | 'format_list_bulleted' | 'format_list_numbered' |
+  // 'format_list_checked' | 'link' | 'add_photo' | 'table' |
+  // 'horizontal_rule' | 'frontmatter' | 'frame_source' |
+  // 'arrow_drop_down' | 'admonition' | 'sandpack' | 'rich_text' |
+  // 'difference' | 'markdown' | 'open_in_new' | 'link_off' | 'edit' |
+  // 'content_copy' | 'more_horiz' | 'more_vert' | 'close' | 'settings' |
+  // 'delete_big' | 'delete_small' | 'format_align_center' |
+  // 'format_align_left' | 'format_align_right' | 'add_row' | 'add_column'
+  // | 'insert_col_left' | 'insert_row_above' | 'insert_row_below' |
+  // 'insert_col_right' | 'check';
+
+  switch (name) {
+    case 'format_bold':
+      return <TextB size={DEFAULT_ICON_SIZE} />;
+    case 'format_italic':
+      return <TextItalic size={DEFAULT_ICON_SIZE} />;
+    case 'format_underlined':
+      return <TextUnderline size={DEFAULT_ICON_SIZE} />;
+    case 'strikeThrough':
+      return <TextStrikethrough size={DEFAULT_ICON_SIZE} />;
+    case 'superscript':
+      return <TextSuperscript size={DEFAULT_ICON_SIZE} />;
+    case 'subscript':
+      return <TextSubscript size={DEFAULT_ICON_SIZE} />;
+    case 'format_list_bulleted':
+      return <ListDashes size={DEFAULT_ICON_SIZE} />;
+    case 'format_list_numbered':
+      return <ListNumbers size={DEFAULT_ICON_SIZE} />;
+    case 'format_list_checked':
+      return <ListChecks size={DEFAULT_ICON_SIZE} />;
+    case 'frame_source':
+    case 'code':
+      console.log('FIXME: code');
+      return <Code size={DEFAULT_ICON_SIZE} />;
+    case 'table':
+      return <Table size={DEFAULT_ICON_SIZE + 2} />;
+
+    default:
+      return defaultSvgIcons[name];
+  }
 };
 
 export const Editor = (props: EditorProps) => {
@@ -133,48 +183,7 @@ export const Editor = (props: EditorProps) => {
         ref={mdxEditorRef}
         markdown={supported}
         placeholder={placeholder}
-        // 'undo' | 'redo' | 'format_bold' | 'format_italic' |
-        // 'format_underlined' | 'code' | 'strikeThrough' | 'superscript' |
-        // 'subscript' | 'format_list_bulleted' | 'format_list_numbered' |
-        // 'format_list_checked' | 'link' | 'add_photo' | 'table' |
-        // 'horizontal_rule' | 'frontmatter' | 'frame_source' |
-        // 'arrow_drop_down' | 'admonition' | 'sandpack' | 'rich_text' |
-        // 'difference' | 'markdown' | 'open_in_new' | 'link_off' | 'edit' |
-        // 'content_copy' | 'more_horiz' | 'more_vert' | 'close' | 'settings' |
-        // 'delete_big' | 'delete_small' | 'format_align_center' |
-        // 'format_align_left' | 'format_align_right' | 'add_row' | 'add_column'
-        // | 'insert_col_left' | 'insert_row_above' | 'insert_row_below' |
-        // 'insert_col_right' | 'check';
-        iconComponentFor={(name) => {
-          switch (name) {
-            case 'format_bold':
-              return <TextB size={24} />;
-            case 'format_italic':
-              return <TextItalic size={24} />;
-            case 'format_underlined':
-              return <TextUnderline size={24} />;
-            case 'strikeThrough':
-              return <TextStrikethrough size={24} />;
-            case 'superscript':
-              return <TextSuperscript size={24} />;
-            case 'subscript':
-              return <TextSubscript size={24} />;
-            case 'format_list_bulleted':
-              return <ListDashes size={24} />;
-            case 'format_list_numbered':
-              return <ListNumbers size={24} />;
-            case 'format_list_checked':
-              return <ListChecks size={24} />;
-            case 'code':
-              console.log('FIXME: code');
-              return <Code size={24} />;
-
-            default:
-              break;
-          }
-
-          return <div />;
-        }}
+        iconComponentFor={iconComponentFor}
         translation={(key, defaultValue, interpolations) => {
           switch (key) {
             case 'toolbar.blockTypeSelect.placeholder':
