430fbe4401efd886d9fe18a8bd4a49752a979fa8
Kevin Burton <burtonator@gmail.com>
2024-08-26 14:49:06 -0700

    moved to dedicated files.

--- packages/commonwealth/client/scripts/views/pages/Editor/Editor.tsx
+++ packages/commonwealth/client/scripts/views/pages/Editor/Editor.tsx
@@ -1,11 +1,9 @@
 import {
   codeBlockPlugin,
   codeMirrorPlugin,
-  defaultSvgIcons,
   diffSourcePlugin,
   frontmatterPlugin,
   headingsPlugin,
-  IconKey,
   imagePlugin,
   linkDialogPlugin,
   linkPlugin,
@@ -22,27 +20,14 @@ import React, { useCallback } from 'react';
 
 import './Editor.scss';
 
-import 'commonwealth-mdxeditor/style.css';
-
-import {
-  Code,
-  ListChecks,
-  ListDashes,
-  ListNumbers,
-  Table,
-  TextB,
-  TextItalic,
-  TextStrikethrough,
-  TextSubscript,
-  TextSuperscript,
-  TextUnderline,
-} from '@phosphor-icons/react';
 import clsx from 'clsx';
+import 'commonwealth-mdxeditor/style.css';
 import { SERVER_URL } from 'state/api/config';
 import useUserStore from 'state/ui/user';
 import { uploadFileToS3 } from 'views/components/react_quill_editor/utils';
 import { codeBlockLanguages } from 'views/pages/Editor/codeBlockLanguages';
 import { fileToText } from 'views/pages/Editor/fileToText';
+import { iconComponentFor } from 'views/pages/Editor/iconComponentFor';
 import { ToolbarForDesktop } from 'views/pages/Editor/ToolbarForDesktop';
 import { ToolbarForMobile } from 'views/pages/Editor/ToolbarForMobile';
 import supported from './supported.md?raw';
@@ -76,53 +61,6 @@ type EditorMode = 'desktop' | 'mobile';
 type EditorProps = {
   readonly mode?: EditorMode;
   readonly placeholder?: string;
-};
-
-const DEFAULT_ICON_SIZE = 22;
-
-const iconComponentFor = (name: IconKey) => {
-  // 'undo' | 'redo' | 'format_bold' | 'format_italic' |
-  // 'format_underlined' | 'code' | 'strikeThrough' | 'superscript' |
-  // 'subscript' | 'format_list_bulleted' | 'format_list_numbered' |
-  // 'format_list_checked' | 'link' | 'add_photo' | 'table' |
-  // 'horizontal_rule' | 'frontmatter' | 'frame_source' |
-  // 'arrow_drop_down' | 'admonition' | 'sandpack' | 'rich_text' |
-  // 'difference' | 'markdown' | 'open_in_new' | 'link_off' | 'edit' |
-  // 'content_copy' | 'more_horiz' | 'more_vert' | 'close' | 'settings' |
-  // 'delete_big' | 'delete_small' | 'format_align_center' |
-  // 'format_align_left' | 'format_align_right' | 'add_row' | 'add_column'
-  // | 'insert_col_left' | 'insert_row_above' | 'insert_row_below' |
-  // 'insert_col_right' | 'check';
-
-  switch (name) {
-    case 'format_bold':
-      return <TextB size={DEFAULT_ICON_SIZE} />;
-    case 'format_italic':
-      return <TextItalic size={DEFAULT_ICON_SIZE} />;
-    case 'format_underlined':
-      return <TextUnderline size={DEFAULT_ICON_SIZE} />;
-    case 'strikeThrough':
-      return <TextStrikethrough size={DEFAULT_ICON_SIZE} />;
-    case 'superscript':
-      return <TextSuperscript size={DEFAULT_ICON_SIZE} />;
-    case 'subscript':
-      return <TextSubscript size={DEFAULT_ICON_SIZE} />;
-    case 'format_list_bulleted':
-      return <ListDashes size={DEFAULT_ICON_SIZE} />;
-    case 'format_list_numbered':
-      return <ListNumbers size={DEFAULT_ICON_SIZE} />;
-    case 'format_list_checked':
-      return <ListChecks size={DEFAULT_ICON_SIZE} />;
-    case 'frame_source':
-    case 'code':
-      console.log('FIXME: code');
-      return <Code size={DEFAULT_ICON_SIZE} />;
-    case 'table':
-      return <Table size={DEFAULT_ICON_SIZE + 2} />;
-
-    default:
-      return defaultSvgIcons[name];
-  }
 };
 
 export const Editor = (props: EditorProps) => {
---
+++ packages/commonwealth/client/scripts/views/pages/Editor/iconComponentFor.tsx
@@ -0,0 +1,62 @@
+import {
+  Code,
+  ListChecks,
+  ListDashes,
+  ListNumbers,
+  Table,
+  TextB,
+  TextItalic,
+  TextStrikethrough,
+  TextSubscript,
+  TextSuperscript,
+  TextUnderline,
+} from '@phosphor-icons/react';
+import { defaultSvgIcons, IconKey } from 'commonwealth-mdxeditor';
+import React from 'react';
+
+const DEFAULT_ICON_SIZE = 22;
+
+export const iconComponentFor = (name: IconKey) => {
+  // 'undo' | 'redo' | 'format_bold' | 'format_italic' |
+  // 'format_underlined' | 'code' | 'strikeThrough' | 'superscript' |
+  // 'subscript' | 'format_list_bulleted' | 'format_list_numbered' |
+  // 'format_list_checked' | 'link' | 'add_photo' | 'table' |
+  // 'horizontal_rule' | 'frontmatter' | 'frame_source' |
+  // 'arrow_drop_down' | 'admonition' | 'sandpack' | 'rich_text' |
+  // 'difference' | 'markdown' | 'open_in_new' | 'link_off' | 'edit' |
+  // 'content_copy' | 'more_horiz' | 'more_vert' | 'close' | 'settings' |
+  // 'delete_big' | 'delete_small' | 'format_align_center' |
+  // 'format_align_left' | 'format_align_right' | 'add_row' | 'add_column'
+  // | 'insert_col_left' | 'insert_row_above' | 'insert_row_below' |
+  // 'insert_col_right' | 'check';
+
+  switch (name) {
+    case 'format_bold':
+      return <TextB size={DEFAULT_ICON_SIZE} />;
+    case 'format_italic':
+      return <TextItalic size={DEFAULT_ICON_SIZE} />;
+    case 'format_underlined':
+      return <TextUnderline size={DEFAULT_ICON_SIZE} />;
+    case 'strikeThrough':
+      return <TextStrikethrough size={DEFAULT_ICON_SIZE} />;
+    case 'superscript':
+      return <TextSuperscript size={DEFAULT_ICON_SIZE} />;
+    case 'subscript':
+      return <TextSubscript size={DEFAULT_ICON_SIZE} />;
+    case 'format_list_bulleted':
+      return <ListDashes size={DEFAULT_ICON_SIZE} />;
+    case 'format_list_numbered':
+      return <ListNumbers size={DEFAULT_ICON_SIZE} />;
+    case 'format_list_checked':
+      return <ListChecks size={DEFAULT_ICON_SIZE} />;
+    case 'frame_source':
+    case 'code':
+      console.log('FIXME: code');
+      return <Code size={DEFAULT_ICON_SIZE} />;
+    case 'table':
+      return <Table size={DEFAULT_ICON_SIZE + 2} />;
+
+    default:
+      return defaultSvgIcons[name];
+  }
+};
