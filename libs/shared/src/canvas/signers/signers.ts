8f0800ac690a56b03cce6a284597d8e0035698c0
Raymond Z <raykyri@gmail.com>
2024-08-27 15:11:55 -0400

    fix client side polkadot signer

--- libs/shared/src/canvas/signers/signers.ts
+++ libs/shared/src/canvas/signers/signers.ts
@@ -4,11 +4,15 @@ import { SolanaSigner } from '@canvas-js/chain-solana';
 import { SubstrateSigner } from '@canvas-js/chain-substrate';
 import {
   AbstractSessionData,
+  Action,
   DidIdentifier,
   Session,
+  Signer,
 } from '@canvas-js/interfaces';
 import { fromBech32, toBech32 } from '@cosmjs/encoding';
 import { addressSwapper } from '@hicommonwealth/shared';
+import * as json from '@ipld/dag-json';
+import { KeypairType } from '@polkadot/util-crypto/types';
 
 export const getSessionSigners = () => {
   return [
@@ -27,16 +31,21 @@ export const getSessionSignerForDid = (address: string) => {
 };
 
 /**
- * In Canvas, the default behaviour is that a SessionSigner saves a session key in localStorage for
- * each combination of chain base (e.g. "cosmos", "ethereum", "substrate"), chain id and address.
+ * In Canvas, the default behaviour is that a SessionSigner saves a
+ * session key in localStorage for each combination of chain base
+ * (e.g. "cosmos", "ethereum", "substrate"), chain id and address.
+ * For Commonwealth, the chain id is not always known, so we want to
+ * always use `cosmos` as the chain ID for cosmos, and `42` for
+ * substrate, etc.
  *
- * This is not ideal for Commonwealth, where the chain id is not always known, e.g. with Cosmos.
- *
- * In these cases we want to override the default behaviour and store one session for all chains.
- * We can do this by overriding the `getAddress` method of the session signer class (e.g. CosmosSigner).
+ * In these cases we want to override the default behaviour and store
+ * one session for all chains.  We can do this by overriding the
+ * `getDid` method of the session signer class (e.g. CosmosSigner).
  */
 
-function parseAddress(address: string): [chain: string, walletAddress: string] {
+function parseCosmosAddress(
+  address: string,
+): [chain: string, walletAddress: string] {
   const addressPattern = /^did:pkh:cosmos:([0-9a-z\-_]+):([a-zA-Fa-f0-9]+)$/;
   const result = addressPattern.exec(address);
   if (result === null) {
@@ -51,7 +60,7 @@ function parseAddress(address: string): [chain: string, walletAddress: string] {
 }
 
 /**
- * Use addresses of the form `cosmos:cosmoshub-1:osmo1...`
+ * Use addresses of the form `did:pkh:cosmos:cosmoshub-1:osmo1...`
  * since we don't know the chain ID, but also don't want to reencode the chain address
  */
 export class CosmosSignerCW extends CosmosSigner {
@@ -72,7 +81,7 @@ export class CosmosSignerCW extends CosmosSigner {
       publicKey,
       context: { timestamp, duration },
     } = data;
-    const [chainId, walletAddress] = parseAddress(did);
+    const [chainId, walletAddress] = parseCosmosAddress(did);
 
     const issuedAt = new Date(timestamp);
     const message = {
@@ -112,6 +121,30 @@ export class CosmosSignerCW extends CosmosSigner {
   }
 }
 
+/**
+ * Use addresses of the form `did:pkh:polkadot:42:mL6f72...`.
+ * Substrate and Polkadot chains are even more complicated;
+ * they also reencode the *address* differently for each chain.
+ */
+
+export type SubstrateMessage = {
+  topic: string;
+  address: string;
+  chainId: string;
+  uri: string;
+  issuedAt: string;
+  expirationTime: string | null;
+};
+
+export type SubstrateSessionData = {
+  signatureResult: {
+    signature: Uint8Array;
+    nonce: Uint8Array;
+  };
+  substrateKeyType: KeypairType;
+  data: SubstrateMessage;
+};
+
 export class SubstrateSignerCW extends SubstrateSigner {
   // override SubstrateSigner to always use 42 as the ss58 id
   // TODO: Could we pass the ss58prefix to this._signer.getAddress()
@@ -125,7 +158,53 @@ export class SubstrateSignerCW extends SubstrateSigner {
     return `did:pkh:polkadot:42:${finalAddress}`;
   }
 
-  // override AbstractSessionSigner to use ss58 id 42 in hasSession
+  // override AbstractSessionSigner to use ss58 id 42
+  public async getSession(
+    topic: string,
+    options: { did?: string; address?: string } = {},
+  ): Promise<{
+    payload: Session<SubstrateSessionData>;
+    signer: Signer<Action | Session<SubstrateSessionData>>;
+  } | null> {
+    let did;
+    if (options.address) {
+      const dids = this.listSessions(topic).filter((did) =>
+        did.endsWith(':' + options.address),
+      );
+      if (dids.length === 0) return null;
+      did = dids[0];
+    } else {
+      did = await Promise.resolve(options.did ?? this.getDid());
+    }
+    const didParts = did.split(':');
+    const walletAddress = didParts[4];
+    const finalAddress = addressSwapper({
+      currentPrefix: 42,
+      address: walletAddress,
+    });
+    did = `did:pkh:polkadot:42:${finalAddress}`;
+    const key = `canvas/${topic}/${did}`;
+
+    this.log('getting session for topic %s and DID %s', topic, did);
+
+    const value = this.target.get(key);
+    if (value !== null) {
+      this.log('found session and signer in storage');
+      const entry = json.parse<{
+        type: string;
+        privateKey: Uint8Array;
+        session: Session;
+      }>(value);
+      const { type, privateKey, session } = entry;
+
+      const signer = this.scheme.create({ type, privateKey });
+      return { payload: session, signer };
+    }
+
+    this.log('session and signer not found');
+    return null;
+  }
+
   hasSession(topic: string, address: string) {
     // TODO: we should have a utility function to access parts of DIDs
     const addressParts = address.split(':');
