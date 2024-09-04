acb31bd8fd60ef3dcd23a914bc76a228f145ee60
Kevin Burton <burtonator@gmail.com>
2024-09-04 12:55:52 -0700

    note that we need this to not upload to S3..

--- packages/commonwealth/client/scripts/views/pages/Editor/useImageUploadHandlerLocal.ts
+++ packages/commonwealth/client/scripts/views/pages/Editor/useImageUploadHandlerLocal.ts
@@ -3,6 +3,9 @@ import { useCallback } from 'react';
 
 /**
  * Just a basic local image handler that uses a file URL.
+ *
+ * This should remain and not be removed, otherwise, in dev mode, we'll
+ * constantly be uploading to S3.
  */
 export function useImageUploadHandlerLocal() {
   return useCallback(async (file: File) => {
