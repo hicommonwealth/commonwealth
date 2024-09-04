c6f116ebb08b8649ecf98bf1453b5c1a1b4bcf90
Kevin Burton <burtonator@gmail.com>
2024-09-04 12:36:34 -0700

    cleanup for marcin.

--- packages/commonwealth/client/scripts/views/pages/Editor/iconComponentFor.tsx
+++ packages/commonwealth/client/scripts/views/pages/Editor/iconComponentFor.tsx
@@ -17,19 +17,8 @@ import React from 'react';
 const DEFAULT_ICON_SIZE = 22;
 
 export const iconComponentFor = (name: IconKey) => {
-  // 'undo' | 'redo' | 'format_bold' | 'format_italic' |
-  // 'format_underlined' | 'code' | 'strikeThrough' | 'superscript' |
-  // 'subscript' | 'format_list_bulleted' | 'format_list_numbered' |
-  // 'format_list_checked' | 'link' | 'add_photo' | 'table' |
-  // 'horizontal_rule' | 'frontmatter' | 'frame_source' |
-  // 'arrow_drop_down' | 'admonition' | 'sandpack' | 'rich_text' |
-  // 'difference' | 'markdown' | 'open_in_new' | 'link_off' | 'edit' |
-  // 'content_copy' | 'more_horiz' | 'more_vert' | 'close' | 'settings' |
-  // 'delete_big' | 'delete_small' | 'format_align_center' |
-  // 'format_align_left' | 'format_align_right' | 'add_row' | 'add_column'
-  // | 'insert_col_left' | 'insert_row_above' | 'insert_row_below' |
-  // 'insert_col_right' | 'check';
-
+  // to add new custom icons, just jump to the IconKey symbol name, then
+  // implement one of the icons below.
   switch (name) {
     case 'format_bold':
       return <TextB size={DEFAULT_ICON_SIZE} />;
