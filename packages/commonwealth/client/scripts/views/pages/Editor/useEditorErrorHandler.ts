1a2621f5c5882fce96f0289a8a24ad4fdbd86039
Kevin Burton <burtonator@gmail.com>
2024-09-04 13:09:59 -0700

    docs about how to change it to a snackbar.

--- packages/commonwealth/client/scripts/views/pages/Editor/useEditorErrorHandler.ts
+++ packages/commonwealth/client/scripts/views/pages/Editor/useEditorErrorHandler.ts
@@ -4,6 +4,10 @@ type ErrorPayload = Readonly<{
 }>;
 
 export function useEditorErrorHandler(): (err: ErrorPayload) => void {
+  // I want to keep this as a hook so we can *make* it a hook later if we want.
+  // I think the errors should probably be tied into snackbar errors which will
+  // probably need to be a hook.  Right now we just console.log it but we're
+  // going to change it to use a snackbar for errors.
   return (err) => {
     console.error('Encountered error with editor: ', err);
   };
