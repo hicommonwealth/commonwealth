cf4a65b6a3ba67df03828a6a660463c65eaeeed4
Kevin Burton <burtonator@gmail.com>
2024-09-05 14:25:49 -0700

    Fixed the upload indicators now...

--- packages/commonwealth/client/scripts/views/pages/Editor/indicators/UploadIndicator.tsx
+++ packages/commonwealth/client/scripts/views/pages/Editor/indicators/UploadIndicator.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
 import './UploadIndicator.scss';
+import CWCircleRingSpinner
+  from 'views/components/component_kit/new_designs/CWCircleRingSpinner';
 
 export const UploadIndicator = () => {
-  return <div className="UploadIndicator"></div>;
+  return (
+    <div className="UploadIndicator">
+      <div className="inner">
+        <CWCircleRingSpinner/>
+      </div>
+    </div>
+  );
 };
