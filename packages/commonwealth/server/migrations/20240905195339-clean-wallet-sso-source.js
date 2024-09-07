f41f10f8d0d90341eedd3c2c776a9c12d72da1c0
Timothee Legros <timothee@ualberta.ca>
2024-09-07 05:56:52 +0300

    remove WalletSsoSource from address model

--- packages/commonwealth/server/migrations/20240905195339-clean-wallet-sso-source.js
+++ packages/commonwealth/server/migrations/20240905195339-clean-wallet-sso-source.js
@@ -3,11 +3,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.sequelize.query(`
-      UPDATE "Addresses"
-      SET wallet_sso_source = NULL
-      WHERE wallet_sso_source = '';
-    `);
+    await queryInterface.removeColumn('Addresses', 'wallet_sso_source');
   },
 
   async down(queryInterface, Sequelize) {},
