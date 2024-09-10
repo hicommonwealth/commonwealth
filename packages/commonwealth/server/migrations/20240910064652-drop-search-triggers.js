f5ed869ad29ddf3df8d0e9333e11cc1d4847155e
Timothee Legros <timothee@ualberta.ca>
2024-09-10 12:28:30 +0300

    add txn to migration

--- packages/commonwealth/server/migrations/20240910064652-drop-search-triggers.js
+++ packages/commonwealth/server/migrations/20240910064652-drop-search-triggers.js
@@ -4,14 +4,24 @@
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.sequelize.transaction(async (transaction) => {
-      await queryInterface.sequelize.query(`
+      await queryInterface.sequelize.query(
+        `
       DROP TRIGGER IF EXISTS "OffchainThreads_vector_update" ON "Threads";
-    `);
-      await queryInterface.renameColumn('Threads', '_search', 'search');
-      await queryInterface.sequelize.query(`
+    `,
+        { transaction },
+      );
+      await queryInterface.renameColumn('Threads', '_search', 'search', {
+        transaction,
+      });
+      await queryInterface.sequelize.query(
+        `
       DROP TRIGGER IF EXISTS "OffchainComments_vector_update" ON "Comments";
-    `);
-      await queryInterface.renameColumn('Comments', '_search', 'search');
+    `,
+        { transaction },
+      );
+      await queryInterface.renameColumn('Comments', '_search', 'search', {
+        transaction,
+      });
     });
   },
 
