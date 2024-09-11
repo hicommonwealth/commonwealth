76602b7b7cc31391a19b1c70caa8efcae80eef56
rotorsoft <rotorsoft@outlook.com>
2024-09-11 12:34:54 -0400

    removing unused stuff

--- libs/model/src/community/UpdateCommunity.command.ts
+++ libs/model/src/community/UpdateCommunity.command.ts
@@ -8,11 +8,7 @@ import { checkSnapshotObjectExists, commonProtocol } from '../services';
 
 export const UpdateCommunityErrors = {
   ReservedId: 'The id is reserved and cannot be used',
-  CantChangeCustomDomain: 'Custom domain change not permitted',
-  CustomDomainIsTaken: 'Custom domain is taken by another community',
-  CantChangeNetwork: 'Cannot change community network',
   NotAdmin: 'Not an admin',
-  InvalidCustomDomain: 'Custom domain may not include "commonwealth"',
   SnapshotOnlyOnEthereum:
     'Snapshot data may only be added to chains with Ethereum base',
   InvalidDefaultPage: 'Default page does not exist',
@@ -30,8 +26,6 @@ export function UpdateCommunity(): Command<
     body: async ({ actor, payload }) => {
       const {
         id,
-        network,
-        custom_domain,
         snapshot,
         name,
         description,
@@ -55,8 +49,6 @@ export function UpdateCommunity(): Command<
 
       if (id === ALL_COMMUNITIES)
         throw new InvalidInput(UpdateCommunityErrors.ReservedId);
-      if (network)
-        throw new InvalidInput(UpdateCommunityErrors.CantChangeNetwork);
 
       const community = await models.Community.findOne({
         where: { id },
@@ -68,23 +60,6 @@ export function UpdateCommunity(): Command<
         ],
       });
       mustExist('Community', community); // if authorized as admin, community is always found
-
-      // Only permit site admins to update custom domain field on communities, as it requires
-      // external configuration (via heroku + whitelists).
-      // Currently does not permit unsetting the custom domain; must be done manually.
-      if (custom_domain && community.custom_domain !== custom_domain) {
-        if (!actor.user.isAdmin)
-          throw new InvalidInput(UpdateCommunityErrors.CantChangeCustomDomain);
-        if (custom_domain.includes('commonwealth'))
-          throw new InvalidInput(UpdateCommunityErrors.InvalidCustomDomain);
-
-        // verify if this custom domain is taken by another community
-        const found = await models.Community.findOne({
-          where: { custom_domain },
-        });
-        if (found)
-          throw new InvalidInput(UpdateCommunityErrors.CustomDomainIsTaken);
-      }
 
       // Handle single string case and undefined case
       const snapshots = !snapshot
@@ -124,7 +99,6 @@ export function UpdateCommunity(): Command<
           );
       }
 
-      custom_domain && (community.custom_domain = custom_domain);
       default_page && (community.default_page = default_page);
       community.snapshot_spaces = snapshots;
       name && (community.name = name);
@@ -148,13 +122,6 @@ export function UpdateCommunity(): Command<
       custom_stages && (community.custom_stages = custom_stages);
 
       await community.save();
-
-      // Suggested solution for serializing BigInts
-      // https://github.com/GoogleChromeLabs/jsbi/issues/30#issuecomment-1006086291
-      (BigInt.prototype as unknown as { toJSON: () => string }).toJSON =
-        function () {
-          return this.toString();
-        };
       return community.toJSON();
     },
   };
