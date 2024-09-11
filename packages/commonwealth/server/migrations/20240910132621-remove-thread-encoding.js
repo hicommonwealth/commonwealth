734cbff8102fda0701fb36aedbc3d01312f82818
Timothee Legros <timothee@ualberta.ca>
2024-09-11 07:15:53 +0300

    update migration

--- packages/commonwealth/server/migrations/20240910132621-remove-thread-encoding.js
+++ packages/commonwealth/server/migrations/20240910132621-remove-thread-encoding.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const { QueryTypes } = require('sequelize');
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
@@ -13,6 +15,45 @@ module.exports = {
       await queryInterface.renameColumn('Threads', 'plaintext', 'body', {
         transaction,
       });
+
+      // date filter since confirmed on previous execution of this query in
+      // production that no older records exist -> significantly improves query
+      // performance which is important to speed up the migration
+      const encodedThreads = await queryInterface.sequelize.query(
+        `
+        SELECT id, body
+        FROM "Threads"
+        WHERE body_backup = body AND body ~ '%[0-9A-Fa-f]{2}' AND created_at > '2024-09-09';
+      `,
+        { transaction, type: QueryTypes.SELECT },
+      );
+
+      if (encodedThreads.length > 0) {
+        let query = ``;
+        const replacements = [];
+        for (const thread of encodedThreads) {
+          if (replacements.length > 0) query += ',\n';
+          try {
+            const decodedBody = decodeURIComponent(thread.body);
+            query += 'WHEN id = ? THEN ?';
+            replacements.push([thread.id, decodedBody]);
+          } catch (e) {
+            query += 'WHEN id = ? THEN ?';
+            replacements.push([thread.id, thread.body]);
+          }
+        }
+
+        await queryInterface.sequelize.query(
+          `
+          UPDATE "Threads"
+          SET body = CASE
+                          ${query}
+                      END
+          WHERE body_backup = body AND body ~ '%[0-9A-Fa-f]{2}';
+      `,
+          { transaction, replacements },
+        );
+      }
     });
   },
 
