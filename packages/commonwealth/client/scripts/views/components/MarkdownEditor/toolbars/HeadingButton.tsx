65631d94854a5fb1bb8436cfa775b550683461d8
Kevin Burton <burtonator@gmail.com>
2024-09-12 14:08:57 -0700

    Fixed lint issue.

--- packages/commonwealth/client/scripts/views/components/MarkdownEditor/toolbars/HeadingButton.tsx
+++ packages/commonwealth/client/scripts/views/components/MarkdownEditor/toolbars/HeadingButton.tsx
@@ -3,9 +3,9 @@ import { BlockButton } from 'views/components/MarkdownEditor/toolbars/BlockButto
 import { DEFAULT_ICON_SIZE } from 'views/components/MarkdownEditor/utils/iconComponentFor';
 import { CWIcon } from 'views/components/component_kit/cw_icons/cw_icon';
 
-export type HeadingButtonProps = {
+export type HeadingButtonProps = Readonly<{
   headingTag: 'h1' | 'h2' | 'h3';
-};
+}>;
 
 export const HeadingButton = (props: HeadingButtonProps) => {
   const { headingTag } = props;
