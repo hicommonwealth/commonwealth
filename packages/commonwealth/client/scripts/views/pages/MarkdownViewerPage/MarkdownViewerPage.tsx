3843cdc1e3d7ab482c727937f1715a07542f2f05
Kevin Burton <burtonator@gmail.com>
2024-09-12 13:00:38 -0700

    Fixed import path.

--- packages/commonwealth/client/scripts/views/pages/MarkdownViewerPage/MarkdownViewerPage.tsx
+++ packages/commonwealth/client/scripts/views/pages/MarkdownViewerPage/MarkdownViewerPage.tsx
@@ -3,9 +3,8 @@ import React from 'react';
 import supported from 'views/components/MarkdownEditor/markdown/supported.md?raw';
 import MarkdownViewer from 'views/components/MarkdownViewer';
 
-import "./MarkdownViewerPage.scss"
-import "../../../../styles/index.scss"
-
+import '../../../../styles/index.scss';
+import './MarkdownViewerPage.scss';
 
 /**
  * Basic demo page that allows us to use either mode and to log the markdown.
