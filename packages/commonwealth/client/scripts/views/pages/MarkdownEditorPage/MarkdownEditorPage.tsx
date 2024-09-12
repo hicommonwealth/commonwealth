af1099af5474f85e7e34ff89c5da2eca2653614c
Kevin Burton <burtonator@gmail.com>
2024-09-12 12:09:38 -0700

    improved layout

--- packages/commonwealth/client/scripts/views/pages/MarkdownEditorPage/MarkdownEditorPage.tsx
+++ packages/commonwealth/client/scripts/views/pages/MarkdownEditorPage/MarkdownEditorPage.tsx
@@ -3,6 +3,7 @@ import { useSearchParams } from 'react-router-dom';
 import MarkdownEditor from 'views/components/MarkdownEditor';
 import { MarkdownEditorMode } from 'views/components/MarkdownEditor/MarkdownEditor';
 
+import overview from 'views/components/MarkdownEditor/markdown/editor_overview.md?raw';
 import supported from 'views/components/MarkdownEditor/markdown/supported.md?raw';
 
 function useParams() {
@@ -21,7 +22,7 @@ export const MarkdownEditorPage = () => {
 
   return (
     <MarkdownEditor
-      markdown={supported}
+      markdown={overview + '\n' + supported}
       mode={mode}
       imageHandler="local"
       onSubmit={(markdown) => console.log('markdown: \n' + markdown)}
