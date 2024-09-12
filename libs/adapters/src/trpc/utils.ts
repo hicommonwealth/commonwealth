5df46ea213189666d377e6968d05ab73e5769ad3
Timothee Legros <timothee@ualberta.ca>
2024-09-12 09:25:56 +0300

    simplify fingerprint logic

--- libs/adapters/src/trpc/utils.ts
+++ libs/adapters/src/trpc/utils.ts
@@ -17,14 +17,12 @@ const logError = (path: string | undefined, error: TRPCError) => {
   const errorName = error.cause?.name ?? error.name;
   const errorMessage = error.cause?.message ?? error.message;
   const msg = `${error.code}: [${errorName}] ${path}: ${errorMessage}`;
-  let fingerprint = `${error.code}-${errorName}-${path}`;
-  if (
-    error.cause &&
-    'issues' in error.cause &&
-    Array.isArray(error.cause.issues)
-  ) {
-    error.cause.issues.forEach((e) => (fingerprint += `-${e.code}`));
-  }
+  const issues =
+    error.cause && 'issues' in error.cause && Array.isArray(error.cause.issues)
+      ? error.cause.issues.map((i) => i.code)
+      : [];
+  const fingerprint = [error.code, errorName, path, ...issues].join('-');
+
   error.code === 'INTERNAL_SERVER_ERROR'
     ? log.error(msg, error.cause, { fingerprint })
     : log.warn(msg, { fingerprint });
