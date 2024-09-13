57fccc6ea281fd0fe6a5bcb23527c116cf696b92
Ryan Bennett <ryan@common.xyz>
2024-09-13 09:32:15 -0700

    topics are staked by default

--- packages/commonwealth/server/controllers/server_topics_methods/create_topic.ts
+++ packages/commonwealth/server/controllers/server_topics_methods/create_topic.ts
@@ -5,6 +5,7 @@ import {
   UserInstance,
   sanitizeQuillText,
 } from '@hicommonwealth/model';
+import { TopicWeightedVoting } from '@hicommonwealth/schemas';
 import { MixpanelCommunityInteractionEvent } from '../../../shared/analytics/types';
 import { validateOwner } from '../../util/validateOwner';
 import { TrackOptions } from '../server_analytics_controller';
@@ -76,12 +77,29 @@ export async function __createTopic(
     featured_in_new_post,
     default_offchain_template: default_offchain_template || '',
     community_id: community.id,
-    weighted_voting: body.weighted_voting,
-    chain_node_id: body.chain_node_id,
-    token_address: body.token_address,
-    token_symbol: body.token_symbol,
-    vote_weight_multiplier: body.vote_weight_multiplier,
   };
+
+  // by default, if stake is enabled, then make the topic staked
+  // TODO: REMOVE THIS WHEN THE UI IS WIRED UP TO ALLOW SELECTION
+  const stake = await this.models.CommunityStake.findOne({
+    where: {
+      community_id: community.id,
+    },
+  });
+  if (stake) {
+    options.weighted_voting = TopicWeightedVoting.Stake;
+  }
+
+  // if (body.weighted_voting) {
+  //   options = {
+  //     ...options,
+  //     weighted_voting: body.weighted_voting,
+  //     chain_node_id: body.chain_node_id,
+  //     token_address: body.token_address,
+  //     token_symbol: body.token_symbol,
+  //     vote_weight_multiplier: body.vote_weight_multiplier,
+  //   };
+  // }
 
   const [newTopic] = await this.models.Topic.findOrCreate({
     where: {
