476faff9c78652a3aa6673b4b1a182af63cb76a1
kassad <kurtis@commonwealth.im>
2024-09-13 02:14:35 -0700

    temp

--- packages/commonwealth/server/scripts/releasePhaseEnvCheck.js
+++ packages/commonwealth/server/scripts/releasePhaseEnvCheck.js
@@ -8,12 +8,10 @@ if (import.meta.url.endsWith(process.argv[1])) {
       // TODO: Add checks for unused env var
       // TODO: Add warning for manually set client FLAGs when APP_ENV !== 'local'/'CI'
       log.info('Environment variables are properly configured');
-      // eslint-disable-next-line @typescript-eslint/no-floating-promises
       dispose()('EXIT', true);
     })
     .catch((err) => {
       log.fatal('Environment variables not properly configured!', err);
-      // eslint-disable-next-line @typescript-eslint/no-floating-promises
       dispose()('ERROR', true);
     });
 }
