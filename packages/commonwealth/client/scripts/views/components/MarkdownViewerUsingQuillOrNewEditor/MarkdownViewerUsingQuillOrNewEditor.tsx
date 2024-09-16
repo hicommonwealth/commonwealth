1633006bfd9915a4988c50ec426e8b30d075443e
Kevin Burton <burtonator@gmail.com>
2024-09-16 12:44:01 -0700

    - killed off another two uses...

--- packages/commonwealth/client/scripts/views/components/MarkdownViewerUsingQuillOrNewEditor/MarkdownViewerUsingQuillOrNewEditor.tsx
+++ packages/commonwealth/client/scripts/views/components/MarkdownViewerUsingQuillOrNewEditor/MarkdownViewerUsingQuillOrNewEditor.tsx
@@ -1,11 +1,12 @@
 import { useFlag } from 'hooks/useFlag';
-import React from 'react';
+import React, { ReactNode } from 'react';
 import MarkdownViewer from 'views/components/MarkdownViewer';
 import { QuillRenderer } from 'views/components/react_quill_editor/quill_renderer';
 
 type MarkdownViewerUsingQuillOrNewEditor = {
   readonly markdown: string | undefined;
   readonly cutoffLines?: number;
+  readonly customShowMoreButton?: ReactNode;
 };
 
 /**
@@ -14,13 +15,25 @@ type MarkdownViewerUsingQuillOrNewEditor = {
 export const MarkdownViewerUsingQuillOrNewEditor = (
   props: MarkdownViewerUsingQuillOrNewEditor,
 ) => {
-  const { markdown, cutoffLines } = props;
+  const { markdown, cutoffLines, customShowMoreButton } = props;
 
   const newEditor = useFlag('newEditor');
 
   if (newEditor) {
-    return <MarkdownViewer markdown={markdown} cutoffLines={cutoffLines} />;
+    return (
+      <MarkdownViewer
+        markdown={markdown}
+        cutoffLines={cutoffLines}
+        customShowMoreButton={customShowMoreButton}
+      />
+    );
   }
 
-  return <QuillRenderer doc={markdown ?? ''} cutoffLines={cutoffLines} />;
+  return (
+    <QuillRenderer
+      doc={markdown ?? ''}
+      cutoffLines={cutoffLines}
+      customShowMoreButton={customShowMoreButton}
+    />
+  );
 };
--- packages/commonwealth/client/scripts/views/pages/MarkdownViewerPage/MarkdownViewerPage.tsx
+++ packages/commonwealth/client/scripts/views/pages/MarkdownViewerPage/MarkdownViewerPage.tsx
@@ -12,17 +12,19 @@ function useParams() {
   const [searchParams] = useSearchParams();
   const cutoffLines = searchParams.get('cutoffLines');
   const quill = searchParams.get('quill') === 'true';
+  const searchTerm = searchParams.get('searchTerm') ?? undefined;
 
   return {
     cutoffLines: cutoffLines ? parseInt(cutoffLines) : undefined,
     quill,
+    searchTerm,
   };
 }
 /**
  * Basic demo page that allows us to use either mode and to log the markdown.
  */
 export const MarkdownViewerPage = () => {
-  const { cutoffLines, quill } = useParams();
+  const { cutoffLines, quill, searchTerm } = useParams();
 
   return (
     <section className="MarkdownViewerPage">
@@ -34,7 +36,11 @@ export const MarkdownViewerPage = () => {
 
       {quill && (
         <div className="inner">
-          <QuillRenderer doc={supported} cutoffLines={cutoffLines} />
+          <QuillRenderer
+            doc={supported}
+            cutoffLines={cutoffLines}
+            searchTerm={searchTerm}
+          />
         </div>
       )}
     </section>
