5e8b6e751843ba5c2e511c99fb3603ef5732a998
kassad <kurtis@commonwealth.im>
2024-09-17 13:55:15 -0700

    temp

--- packages/commonwealth/scripts/docker-start.sh
+++ packages/commonwealth/scripts/docker-start.sh
@@ -1,16 +1,14 @@
-# start.sh
-
-if [[ $DYNO == "web"* ]]; then
+if [ "${DYNO}" == "web"* ]; then
   NODE_ENV=production node --import=extensionless/register --enable-source-maps ./build/server.js
-elif [[ $DYNO == "consumer"* ]]; then
+elif [ "${DYNO}" == "consumer"* ]; then
   node --import=extensionless/register build/server/workers/commonwealthConsumer/commonwealthConsumer.js
-elif [[ $DYNO == "evm-ce"* ]]; then
+elif [ "${DYNO}" == "evm-ce"* ]; then
   node --import=extensionless/register build/server/workers/evmChainEvents/startEvmPolling.js
-elif [[ $DYNO == "knock"* ]]; then
+elif [ "${DYNO}" == "knock"* ]; then
   node --import=extensionless/register build/server/workers/knock/knockWorker.js
-elif [[ $DYNO == "message-relayer"* ]]; then
+elif [ "${DYNO}" == "message-relayer"* ]; then
   node --import=extensionless/register build/server/workers/messageRelayer/messageRelayer.js
-elif [[ $DYNO == "release"* ]]; then
+elif [ "${DYNO}" == "release"* ]; then
   node --import=extensionless/register build/server/scripts/releasePhaseEnvCheck.js && \
   npx sequelize-cli db:migrate --config server/sequelize.json && \
   node --import=extensionless/register build/server/scripts/purgeCloudflareCache.js
