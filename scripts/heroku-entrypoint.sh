99af693ca54015b0720adb4f634416e33c0b2c4a
kassad <kurtis@commonwealth.im>
2024-09-18 09:11:20 -0700

    Added datadog

--- scripts/heroku-entrypoint.sh
+++ scripts/heroku-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 datadog-agent run &
-/opt/datadog-agent/embedded/bin/trace-agent run &
-/opt/datadog-agent/embedded/bin/process-agent run &
+/opt/datadog-agent/embedded/bin/trace-agent --config=/etc/datadog-agent/datadog.yaml &
+/opt/datadog-agent/embedded/bin/process-agent --config=/etc/datadog-agent/datadog.yaml &
 NODE_ENV=production node --import=extensionless/register --enable-source-maps ./build/server.js
