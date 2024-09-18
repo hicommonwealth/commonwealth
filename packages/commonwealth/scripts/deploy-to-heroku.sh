1731d10ace57bf33c08200edf3b53928b74be900
kassad <kurtis@commonwealth.im>
2024-09-18 02:46:31 -0700

    temp

---
+++ packages/commonwealth/scripts/deploy-to-heroku.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [ "$#" -ne 4 ]; then
+    echo "Usage: $0 <email> <api_key> <app_name> <heroku_app_name>"
+    exit 1
+fi
+
+email=$1
+api_key=$2
+app_name=$3
+heroku_app_name=$4
+
+cat >~/.netrc <<EOF
+machine api.heroku.com
+    login ${email}
+    password ${api_key}
+machine git.heroku.com
+    login ${email}
+    password ${api_key}
+EOF`
+
+heroku git:remote --app ${app_name}
+
+heroku container:push web consumer evm_ce knock message_relayer --recursive -a ${heroku_app_name}
+
+heroku container:release web consumer evm_ce knock message_relayer --recursive -a ${heroku_app_name}
