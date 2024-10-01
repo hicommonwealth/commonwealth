d6888b5e5a9f6eed26e52641551994c6096b190f
kassad <kurtis@commonwealth.im>
2024-10-01 10:28:13 -0700

    Added test log

--- libs/model/src/database.ts
+++ libs/model/src/database.ts
@@ -12,6 +12,16 @@ console.log(
   config.DB.NO_SSL,
 );
 
+console.log(
+  'dialect options are',
+  config.NODE_ENV !== 'production' || config.DB.NO_SSL
+    ? { requestTimeout: 40000 }
+    : config.DB.URI ===
+        'postgresql://commonwealth:edgeware@localhost/commonwealth'
+      ? { requestTimeout: 40000, ssl: false }
+      : { requestTimeout: 40000, ssl: { rejectUnauthorized: false } },
+);
+
 export const sequelize = new Sequelize(config.DB.URI, {
   // disable string operators (https://github.com/sequelize/sequelize/issues/8417)
   // operatorsAliases: false,
