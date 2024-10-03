dbf4baec53b2e4fe323f7c166c3ffe1a15428bcc
Timothee Legros <timothee@ualberta.ca>
2024-10-03 21:46:30 +0300

    delete discord bot integration routes

--- packages/commonwealth/server/api/integration-router.ts
+++ packages/commonwealth/server/api/integration-router.ts
@@ -1,52 +1,11 @@
 import { express } from '@hicommonwealth/adapters';
-import { ChainEvents, Comment, Thread, models } from '@hicommonwealth/model';
-import { RequestHandler, Router, raw } from 'express';
+import { ChainEvents } from '@hicommonwealth/model';
+import { Router, raw } from 'express';
 import DatabaseValidationService from 'server/middleware/databaseValidationService';
 
 const PATH = '/api/integration';
 
-function withThreadId(req, _, next) {
-  const message_id = req.params.message_id;
-  void models.Thread.findOne({
-    where: {
-      discord_meta: { message_id },
-      deleted_at: null,
-    },
-    attributes: ['id'],
-  })
-    .then((thread) => {
-      if (!thread)
-        throw new Error(`Thread not found for message ${message_id}`);
-      req.body.thread_id = thread.id;
-      next();
-    })
-    .catch(next);
-}
-
-function withCommentId(req, _, next) {
-  const message_id = req.params.message_id;
-  void models.Comment.findOne({
-    where: {
-      discord_meta: { message_id },
-      deleted_at: null,
-    },
-    attributes: ['id'],
-  })
-    .then((comment) => {
-      if (!comment)
-        throw new Error(`Comment not found for message ${message_id}`);
-      req.body.comment_id = comment.id;
-      next();
-    })
-    .catch(next);
-}
-
 function build(validator: DatabaseValidationService) {
-  // Async middleware wrappers
-  const isBotUser: RequestHandler = (req, res, next) => {
-    validator.validateBotUser(req, res, next).catch(next);
-  };
-
   const router = Router();
   router.use(express.statsMiddleware);
 
@@ -64,48 +23,6 @@ function build(validator: DatabaseValidationService) {
       next();
     },
     express.command(ChainEvents.ChainEventCreated()),
-  );
-
-  // Discord BOT integration
-  router.post(
-    '/bot/threads',
-    isBotUser,
-    express.command(Thread.CreateThread()),
-  );
-
-  router.patch(
-    '/bot/threads/:message_id',
-    isBotUser,
-    withThreadId,
-    express.command(Thread.UpdateThread()),
-  );
-
-  router.delete(
-    '/bot/threads/:message_id',
-    isBotUser,
-    withThreadId,
-    express.command(Thread.DeleteThread()),
-  );
-
-  router.post(
-    '/bot/threads/:message_id/comments',
-    isBotUser,
-    withThreadId,
-    express.command(Comment.CreateComment()),
-  );
-
-  router.patch(
-    '/bot/comments/:message_id',
-    isBotUser,
-    withCommentId,
-    express.command(Comment.UpdateComment()),
-  );
-
-  router.delete(
-    '/bot/comments/:message_id',
-    isBotUser,
-    withCommentId,
-    express.command(Comment.DeleteComment()),
   );
 
   return router;
