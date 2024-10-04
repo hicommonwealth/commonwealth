3e1f0906117572c8303036237264033ade143faa
Timothee Legros <timothee@ualberta.ca>
2024-10-04 18:00:02 +0300

    combine devnet tests

--- packages/commonwealth/test/devnet/evm/evmChainEvents/logProcessing.spec.ts
+++ packages/commonwealth/test/devnet/evm/evmChainEvents/logProcessing.spec.ts
@@ -1,22 +1,34 @@
 import { Log } from '@ethersproject/providers';
-import { dispose } from '@hicommonwealth/core';
+import { ChainEventCreated, dispose, EventNames } from '@hicommonwealth/core';
 import {
   CommunityStake,
-  NamespaceFactory,
   communityStakesAbi,
   getAnvil,
+  NamespaceFactory,
   namespaceFactoryAbi,
 } from '@hicommonwealth/evm-testing';
-import { equalEvmAddresses } from '@hicommonwealth/model';
-import { AbiType, commonProtocol } from '@hicommonwealth/shared';
+import {
+  ChainNodeInstance,
+  equalEvmAddresses,
+  hashAbi,
+  models,
+} from '@hicommonwealth/model';
+import {
+  AbiType,
+  BalanceType,
+  commonProtocol,
+  delay,
+} from '@hicommonwealth/shared';
 import { Anvil } from '@viem/anvil';
 import { afterAll, beforeAll, describe, expect, test } from 'vitest';
+import { z } from 'zod';
 import {
   getEvents,
   getLogs,
   getProvider,
   parseLogs,
 } from '../../../../server/workers/evmChainEvents/logProcessing';
+import { startEvmPolling } from '../../../../server/workers/evmChainEvents/startEvmPolling';
 import {
   ContractSources,
   EvmSource,
@@ -57,7 +69,7 @@ const communityStakeTradeSignature =
   '0xfc13c9a8a9a619ac78b803aecb26abdd009182411d51a986090f82519d88a89e';
 const communityStake = new CommunityStake();
 
-describe('EVM Chain Events Log Processing Tests', () => {
+describe('EVM Chain Events Devnet Tests', () => {
   let namespaceDeployedBlock: number;
   let communityStakeBuyBlock: number;
   let communityStakeSellBlock: number;
@@ -163,6 +175,7 @@ describe('EVM Chain Events Log Processing Tests', () => {
     );
   });
 
+  // TODO: move to parallel unit tests
   describe('parsing logs', () => {
     test('should not throw if an invalid ABI is given for a contract address', async () => {
       const evmSource: EvmSource = {
@@ -314,5 +327,105 @@ describe('EVM Chain Events Log Processing Tests', () => {
         ),
       ).toBeTruthy();
     });
+  });
+  describe('EVM Chain Events End to End Tests', () => {
+    let chainNode: ChainNodeInstance;
+
+    beforeAll(async () => {
+      chainNode = await models.ChainNode.create({
+        url: localRpc,
+        balance_type: BalanceType.Ethereum,
+        name: 'Local Base Sepolia',
+        eth_chain_id: commonProtocol.ValidChains.SepoliaBase,
+        max_ce_block_range: -1,
+      });
+      const namespaceAbiInstance = await models.ContractAbi.create({
+        abi: namespaceFactoryAbi,
+        nickname: 'NamespaceFactory',
+        abi_hash: hashAbi(namespaceFactoryAbi),
+      });
+      const stakesAbiInstance = await models.ContractAbi.create({
+        abi: communityStakesAbi,
+        nickname: 'CommunityStakes',
+        abi_hash: hashAbi(communityStakesAbi),
+      });
+      await models.EvmEventSource.bulkCreate([
+        {
+          chain_node_id: chainNode.id!,
+          contract_address:
+            commonProtocol.factoryContracts[
+              commonProtocol.ValidChains.SepoliaBase
+            ].factory.toLowerCase(),
+          event_signature: namespaceDeployedSignature,
+          kind: 'DeployedNamespace',
+          abi_id: namespaceAbiInstance.id!,
+        },
+        {
+          chain_node_id: chainNode.id!,
+          contract_address:
+            commonProtocol.factoryContracts[
+              commonProtocol.ValidChains.SepoliaBase
+            ].communityStake.toLowerCase(),
+          event_signature: communityStakeTradeSignature,
+          kind: 'Trade',
+          abi_id: stakesAbiInstance.id!,
+        },
+      ]);
+    });
+
+    test(
+      'should insert events into the outbox',
+      { timeout: 80_000 },
+      async () => {
+        expect(await models.Outbox.count()).to.equal(0);
+        let lastProcessedBlockNumber =
+          await models.LastProcessedEvmBlock.findOne({
+            where: {
+              chain_node_id: chainNode.id!,
+            },
+          });
+        expect(lastProcessedBlockNumber).toBeFalsy();
+
+        const intervalId = await startEvmPolling(10_000);
+        clearInterval(intervalId);
+
+        await delay(5000);
+
+        lastProcessedBlockNumber = await models.LastProcessedEvmBlock.findOne({
+          where: {
+            chain_node_id: chainNode.id!,
+          },
+        });
+        // should stop before the current block to minimize chain re-org impact
+        expect(lastProcessedBlockNumber?.block_number).to.be.greaterThanOrEqual(
+          communityStakeSellBlock - 1,
+        );
+
+        const events = (await models.Outbox.findAll()) as unknown as Array<{
+          event_name: EventNames.ChainEventCreated;
+          event_payload: z.infer<typeof ChainEventCreated>;
+        }>;
+        expect(events.length).to.equal(3);
+        for (const { event_name } of events) {
+          expect(event_name).to.equal(EventNames.ChainEventCreated);
+        }
+
+        expect(events[0].event_payload.eventSource).to.deep.equal({
+          kind: 'DeployedNamespace',
+          chainNodeId: chainNode.id!,
+          eventSignature: namespaceDeployedSignature,
+        });
+        expect(events[1].event_payload.eventSource).to.deep.equal({
+          kind: 'Trade',
+          chainNodeId: chainNode.id!,
+          eventSignature: communityStakeTradeSignature,
+        });
+        expect(events[2].event_payload.eventSource).to.deep.equal({
+          kind: 'Trade',
+          chainNodeId: chainNode.id!,
+          eventSignature: communityStakeTradeSignature,
+        });
+      },
+    );
   });
 });
