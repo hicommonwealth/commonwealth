99be79dd097965176e6e81af9b42fdea7b386019
kassad <kurtis@commonwealth.im>
2024-10-07 06:29:06 -0700

    Run datadog if production

--- scripts/datadog-entrypoint.sh
+++ scripts/datadog-entrypoint.sh
@@ -4,7 +4,7 @@ datadog-agent run &
 if [ "$APP_ENV" = "production" ]; then
   /opt/datadog-agent/embedded/bin/trace-agent --config=/etc/datadog-agent/datadog.yaml &
   /opt/datadog-agent/embedded/bin/process-agent --config=/etc/datadog-agent/datadog.yaml &
-done
+fi
 
 # Check if a command is provided
 if [ -z "$1" ]; then
