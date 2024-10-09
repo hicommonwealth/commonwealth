a86968a23b078203d2440fd004b90c20a3d9cc8f
Kevin Burton <burtonator@gmail.com>
2024-10-09 11:29:47 -0700

    this roughly works now...

--- packages/commonwealth/client/scripts/views/pages/MarkdownEditorPage/NewToolbar.tsx
+++ packages/commonwealth/client/scripts/views/pages/MarkdownEditorPage/NewToolbar.tsx
@@ -1,25 +1,42 @@
 import React from 'react';
 import CWIconButton from 'views/components/component_kit/new_designs/CWIconButton';
 
+import CWPopover, {
+  usePopover,
+} from 'views/components/component_kit/new_designs/CWPopover';
 import { CWTooltip } from 'views/components/component_kit/new_designs/CWTooltip';
 import './NewToolbar.scss';
 
 export const NewToolbar = () => {
+  const formattingPopoverProps = usePopover();
+
   return (
     <div className="NewToolbar">
-      <button>
-        <CWTooltip
-          content="Bold"
-          renderTrigger={(handleInteraction) => (
-            <CWIconButton
-              buttonSize="lg"
-              iconName="bold"
-              onMouseEnter={handleInteraction}
-              onMouseLeave={handleInteraction}
-            />
-          )}
-        ></CWTooltip>
+      <button onClick={formattingPopoverProps.handleInteraction}>
+        headings
       </button>
+
+      <CWPopover
+        className="FormattingPopover"
+        body={
+          <div>
+            <button>
+              <CWTooltip
+                content="Bold"
+                renderTrigger={(handleInteraction) => (
+                  <CWIconButton
+                    buttonSize="lg"
+                    iconName="bold"
+                    onMouseEnter={handleInteraction}
+                    onMouseLeave={handleInteraction}
+                  />
+                )}
+              ></CWTooltip>
+            </button>
+          </div>
+        }
+        {...formattingPopoverProps}
+      />
     </div>
   );
 };
