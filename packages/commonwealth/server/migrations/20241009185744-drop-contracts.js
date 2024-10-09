89e0a098ed6235fe93ef20f48fc1b0dc0b862c8e
Timothee Legros <timothee@ualberta.ca>
2024-10-09 22:18:30 +0300

    fix migration

--- packages/commonwealth/server/migrations/20241009185744-drop-contracts.js
+++ packages/commonwealth/server/migrations/20241009185744-drop-contracts.js
@@ -4,8 +4,8 @@
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.sequelize.transaction(async (transaction) => {
-      await queryInterface.dropTable('Contracts', { transaction });
       await queryInterface.dropTable('CommunityContracts', { transaction });
+      await queryInterface.dropTable('Contracts', { transaction });
     });
   },
 
