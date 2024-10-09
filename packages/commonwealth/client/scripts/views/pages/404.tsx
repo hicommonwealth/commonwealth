6f2c556ba16e92e153a0459ae944e1b5d3d4a636
israellund <israelmlund@gmail.com>
2024-10-09 18:36:51 -0400

    updated 404/PageNotFound

--- packages/commonwealth/client/scripts/views/pages/404.tsx
+++ packages/commonwealth/client/scripts/views/pages/404.tsx
@@ -1,21 +1,44 @@
 import React from 'react';
+import useUserStore from 'state/ui/user';
+import { useAuthModalStore } from '../../state/ui/modals';
 import { CWEmptyState } from '../components/component_kit/cw_empty_state';
+import { CWButton } from '../components/component_kit/new_designs/CWButton';
+import { AuthModal, AuthModalType } from '../modals/AuthModal';
+import './404.scss';
 
 type PageNotFoundProps = { title?: string; message?: string };
 
 export const PageNotFound = (props: PageNotFoundProps) => {
   const { message } = props;
 
+  const user = useUserStore();
+
+  const { authModalType, setAuthModalType } = useAuthModalStore();
+
   return (
-    <CWEmptyState
-      iconName="cautionCircle"
-      content={
-        message ||
-        `
-            This page may not be visible to the public.
-            If it belongs to a private thread or community, try logging in.
+    <div className="PageNotFound">
+      <CWEmptyState
+        iconName="cautionCircle"
+        content={
+          message ||
+          `
+            This page is private.
+            Please Sign in to view or join the conversation.
             `
-      }
-    />
+        }
+      />
+      {!user.isLoggedIn && (
+        <CWButton
+          buttonType="primary"
+          label="Sign in"
+          onClick={() => setAuthModalType(AuthModalType.SignIn)}
+        />
+      )}
+      <AuthModal
+        type={AuthModalType.SignIn}
+        onClose={() => setAuthModalType(undefined)}
+        isOpen={!!authModalType}
+      />
+    </div>
   );
 };
