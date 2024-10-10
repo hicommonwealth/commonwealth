ece7ba99b82f5ff2d2a32526377ccb3c53713bed
rotorsoft <rotorsoft@outlook.com>
2024-10-10 12:36:20 -0400

    refactor feed query

--- libs/model/src/feed/index.ts
+++ libs/model/src/feed/index.ts
@@ -1,1 +1,2 @@
 export * from './GetGlobalActivity.query';
+export * from './GetUserFeed.query';
--- libs/schemas/src/queries/feed.schemas.ts
+++ libs/schemas/src/queries/feed.schemas.ts
@@ -1,4 +1,5 @@
 import { z } from 'zod';
+import { Thread } from '../entities';
 import { PG_INT } from '../utils';
 
 export const ThreadFeedRecord = z.object({
@@ -38,4 +39,52 @@ export const ChainFeedRecord = z.object({
 export const ChainFeed = {
   input: z.object({}),
   output: z.array(ChainFeedRecord),
+};
+
+export const MappedReaction = z.object({
+  id: z.number(),
+  type: z.literal('like'),
+  address: z.string(),
+  updated_at: z.date(),
+  voting_weight: z.number(),
+  profile_name: z.string().optional(),
+  avatar_url: z.string().optional(),
+  last_active: z.date().optional(),
+});
+
+export const MappedThread = Thread.extend({
+  associatedReactions: z.array(MappedReaction),
+});
+
+export const GetUserFeedStatus = z.enum(['active', 'pastWinners', 'all']);
+export const GetUserFeedOrderBy = z.enum([
+  'newest',
+  'oldest',
+  'mostLikes',
+  'mostComments',
+  'latestActivity',
+]);
+
+export const GetUserFeed = {
+  input: z.object({
+    community_id: z.string(),
+    page: z.number().optional(),
+    limit: z.number().optional(),
+    stage: z.string().optional(),
+    topic_id: PG_INT.optional(),
+    includePinnedThreads: z.boolean().optional(),
+    order_by: GetUserFeedOrderBy.optional(),
+    from_date: z.string().optional(),
+    to_date: z.string().optional(),
+    archived: z.boolean().optional(),
+    contestAddress: z.string().optional(),
+    status: GetUserFeedStatus.optional(),
+    withXRecentComments: z.number().optional(),
+  }),
+  output: z.object({
+    page: z.number(),
+    limit: z.number(),
+    numVotingThreads: z.number(),
+    threads: z.array(MappedThread),
+  }),
 };
