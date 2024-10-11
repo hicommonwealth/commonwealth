5324e39896fc840a43f352965f45bf2666e4526f
Timothee Legros <timothee@ualberta.ca>
2024-10-12 02:42:28 +0300

    update publish step

--- libs/api-client/scripts/publish.sh
+++ libs/api-client/scripts/publish.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+
+set -e
+
 # check if version number is different from production
 # if they are different then generate the new OpenAPI spec
 CURRENT_VERSION=$(pnpm info @commonxyz/api-client version)
@@ -6,5 +10,6 @@ LOCAL_VERSION=$(grep -m1 '"version":' package.json | sed -E 's/[^0-9]*([0-9]+\.[
 if [ "$CURRENT_VERSION" != "$LOCAL_VERSION" ]; then
   pnpm install -g fern-api@latest
   pnpm generate-client
-  pnpm publish --dry-run
+  # without --no-git-checks pnpm doesn't want to publish the uncommited/generated src/ files
+  pnpm publish --dry-run --no-git-checks
 fi
