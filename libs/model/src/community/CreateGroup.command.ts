b193e6d303a772670a08c2c194f2c1d9c6582ed9
Malik Zulqurnain <malikzulqurnain2121@gmail.com>
2024-10-16 01:08:20 +0500

    Revert bulk group create

--- libs/model/src/community/CreateGroup.command.ts
+++ libs/model/src/community/CreateGroup.command.ts
@@ -13,8 +13,6 @@ export const CreateGroupErrors = {
   InvalidTopics: 'Invalid topics',
 };
 
-type GroupsPayload = { group_id: number; topic_id: number; allowed_actions: schemas.PermissionEnum[] };
-
 export function CreateGroup(): Command<
   typeof schemas.CreateGroup,
   AuthContext
@@ -74,16 +72,20 @@ export function CreateGroup(): Command<
             );
 
             // add topic level interaction permissions for current group
-            const groupPermissions = (payload.topics || []).map((t) => {
-              if (group.id) {
-                return {
-                  group_id: group.id,
-                  topic_id: t.id,
-                  allowed_actions: t.permissions,
-                };
-              }
-            }).filter(Boolean) as GroupsPayload[];
-            await models.GroupPermission.bulkCreate(groupPermissions, { transaction });
+            await Promise.all(
+              (payload.topics || [])?.map(async (t) => {
+                if (group.id) {
+                  await models.GroupPermission.create(
+                    {
+                      group_id: group.id,
+                      topic_id: t.id,
+                      allowed_actions: t.permissions,
+                    },
+                    { transaction },
+                  );
+                }
+              }),
+            );
           }
           return group.toJSON();
         },
