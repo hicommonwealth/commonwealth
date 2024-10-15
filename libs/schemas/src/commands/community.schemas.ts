d39f3569f81e90b50b72855aaf4d2d596d793dff
Timothee Legros <timothee@ualberta.ca>
2024-10-15 18:55:49 +0300

    small API update

--- libs/schemas/src/commands/community.schemas.ts
+++ libs/schemas/src/commands/community.schemas.ts
@@ -53,6 +53,7 @@ export const CreateCommunity = {
     telegram: z.string().url().startsWith('https://t.me/').optional(),
     element: z.string().url().startsWith('https://matrix.to/').optional(),
     discord: z.string().url().startsWith('https://discord.com/').optional(),
+    random: z.string().nullish(),
   }),
   output: z.object({
     community: Community,
