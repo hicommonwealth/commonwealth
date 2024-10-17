0d0285d84cc9604d57b1edd18be85ab162a8fe5f
Ryan Bennett <ryan@common.xyz>
2024-10-17 12:59:03 -0700

    lint

--- packages/commonwealth/client/scripts/views/pages/ComponentsShowcase/components/Upvotes.showcase.tsx
+++ packages/commonwealth/client/scripts/views/pages/ComponentsShowcase/components/Upvotes.showcase.tsx
@@ -8,15 +8,15 @@ const UpvotesShowcase = () => {
     <>
       <CWText type="h5">Regular</CWText>
       <div className="flex-row">
-        <CWUpvote voteCount={87} />
-        <CWUpvote voteCount={8887} active />
-        <CWUpvote voteCount={99999} disabled />
+        <CWUpvote voteCount="87" />
+        <CWUpvote voteCount="8887" active />
+        <CWUpvote voteCount="99999" disabled />
       </div>
 
       <CWText type="h5">Small</CWText>
       <div className="flex-row">
         <CWUpvoteSmall
-          voteCount={'87'}
+          voteCount="87"
           disabled={false}
           selected={false}
           // @ts-expect-error <StrictNullChecks/>
@@ -24,7 +24,7 @@ const UpvotesShowcase = () => {
           popoverContent={<div>Upvoters List</div>}
         />
         <CWUpvoteSmall
-          voteCount={'8887'}
+          voteCount="8887"
           disabled={false}
           selected={true}
           // @ts-expect-error <StrictNullChecks/>
@@ -32,7 +32,7 @@ const UpvotesShowcase = () => {
           popoverContent={<div>Upvoters List</div>}
         />
         <CWUpvoteSmall
-          voteCount={'99999'}
+          voteCount="99999"
           disabled={true}
           selected={false}
           // @ts-expect-error <StrictNullChecks/>
