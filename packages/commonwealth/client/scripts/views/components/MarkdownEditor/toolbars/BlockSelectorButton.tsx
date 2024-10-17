d6492f33990bc033135debe09cb9442656aeb08e
Kevin Burton <burtonator@gmail.com>
2024-10-17 12:14:14 -0700

    implemented click away listener now.

--- packages/commonwealth/client/scripts/views/components/MarkdownEditor/toolbars/BlockSelectorButton.tsx
+++ packages/commonwealth/client/scripts/views/components/MarkdownEditor/toolbars/BlockSelectorButton.tsx
@@ -1,3 +1,4 @@
+import ClickAwayListener from '@mui/base/ClickAwayListener';
 import { currentBlockType$, useCellValue } from 'commonwealth-mdxeditor';
 import React, { useCallback } from 'react';
 import { CWHeadingButton } from 'views/components/MarkdownEditor/toolbars/CWHeadingButton';
@@ -34,36 +35,52 @@ export const BlockSelectorButton = (props: BlockSelectorButtonProps) => {
     [popoverProps],
   );
 
-  // FIXME use click away listener...
+  const handleClickAway = useCallback(() => {
+    popoverProps.dispose();
+  }, [popoverProps]);
+
   // FIXME what about list items that don't have an icon when you place the
   // cursor in the wrong block. No icon will be picked.
   // FIXME: the spacing for the P icon is wrong
-  //
 
   const iconName = blockTypeToIconName(currentBlockType);
 
   return (
-    <div className="BlockSelectorButton">
-      <button onClick={handleClick}>
-        {iconName && <CWIcon iconName={iconName} />}
-      </button>
+    <ClickAwayListener onClickAway={handleClickAway}>
+      <div className="BlockSelectorButton">
+        <button onClick={handleClick}>
+          {iconName && <CWIcon iconName={iconName} />}
+        </button>
 
-      <CWPopover
-        className="FormattingPopover"
-        body={
-          <div onMouseLeave={popoverProps.handleInteraction}>
-            <CWHeadingButton blockType="p" onClick={handleFormatButtonClick} />
-            <CWHeadingButton blockType="h1" onClick={handleFormatButtonClick} />
-            <CWHeadingButton blockType="h2" onClick={handleFormatButtonClick} />
-            <CWHeadingButton blockType="h3" onClick={handleFormatButtonClick} />
-            <CWHeadingButton
-              blockType="quote"
-              onClick={handleFormatButtonClick}
-            />
-          </div>
-        }
-        {...popoverProps}
-      />
-    </div>
+        <CWPopover
+          className="FormattingPopover"
+          body={
+            <div onMouseLeave={popoverProps.handleInteraction}>
+              <CWHeadingButton
+                blockType="p"
+                onClick={handleFormatButtonClick}
+              />
+              <CWHeadingButton
+                blockType="h1"
+                onClick={handleFormatButtonClick}
+              />
+              <CWHeadingButton
+                blockType="h2"
+                onClick={handleFormatButtonClick}
+              />
+              <CWHeadingButton
+                blockType="h3"
+                onClick={handleFormatButtonClick}
+              />
+              <CWHeadingButton
+                blockType="quote"
+                onClick={handleFormatButtonClick}
+              />
+            </div>
+          }
+          {...popoverProps}
+        />
+      </div>
+    </ClickAwayListener>
   );
 };
