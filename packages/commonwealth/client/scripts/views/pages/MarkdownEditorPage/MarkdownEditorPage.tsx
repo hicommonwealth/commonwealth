dcab04728ad474854e0bc5c7d2fc69690ff517ed
Kevin Burton <burtonator@gmail.com>
2024-10-18 09:33:29 -0700

    cleanup

--- packages/commonwealth/client/scripts/views/pages/MarkdownEditorPage/MarkdownEditorPage.tsx
+++ packages/commonwealth/client/scripts/views/pages/MarkdownEditorPage/MarkdownEditorPage.tsx
@@ -59,8 +59,6 @@ const Inner = (props: Pick<MarkdownEditorProps, 'mode'>) => {
         {...props}
         markdown={`${overview}\n${supported}`}
         imageHandler="local"
-        // disabled={true}
-        // tooltip="this is a tooltip"
         SubmitButton={SubmitButton}
       />
     </>
