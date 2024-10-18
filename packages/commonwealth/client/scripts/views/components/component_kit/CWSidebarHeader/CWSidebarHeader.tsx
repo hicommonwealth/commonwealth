9dea61abbda784fe2d5eab8862a5453e7f54c9e7
israellund <israelmlund@gmail.com>
2024-10-18 16:47:13 -0400

    truncated long community names in sidebar header

--- packages/commonwealth/client/scripts/views/components/component_kit/CWSidebarHeader/CWSidebarHeader.tsx
+++ packages/commonwealth/client/scripts/views/components/component_kit/CWSidebarHeader/CWSidebarHeader.tsx
@@ -5,9 +5,12 @@ import { navigateToCommunity, useCommonNavigate } from 'navigation/helpers';
 import app from 'state';
 import { useGetCommunityByIdQuery } from 'state/api/communities';
 import CollapsableSidebarButton from 'views/components/sidebar/CollapsableSidebarButton';
+import { handleMouseEnter, handleMouseLeave } from 'views/menus/utils';
+import { smartTrim } from '../../../../../../shared/utils';
 import { Skeleton } from '../../Skeleton';
 import { CWCommunityAvatar } from '../../component_kit/cw_community_avatar';
 import { CWText } from '../../component_kit/cw_text';
+import { CWTooltip } from '../../component_kit/new_designs/CWTooltip';
 
 const SidebarHeader = ({
   isInsideCommunity,
@@ -23,7 +26,7 @@ const SidebarHeader = ({
     id: communityId,
     enabled: !!communityId,
   });
-
+  console.log('community', community?.name);
   return (
     <div className="SidebarHeader">
       <CWCommunityAvatar
@@ -37,18 +40,30 @@ const SidebarHeader = ({
           navigateToCommunity({ navigate, path: '', chain: app.chain.id })
         }
       />
-
-      <CWText
-        className="header"
-        type="h5"
-        onClick={() =>
-          app.chain.id &&
-          navigateToCommunity({ navigate, path: '', chain: app.chain.id })
+      <CWTooltip
+        content={
+          community?.name && community.name.length > 17 ? community.name : null
         }
-      >
-        {community?.name || <Skeleton width="70%" />}
-      </CWText>
-
+        placement="top"
+        renderTrigger={(handleInteraction, isTooltipOpen) => (
+          <CWText
+            className="header"
+            type="h5"
+            onClick={() =>
+              app.chain.id &&
+              navigateToCommunity({ navigate, path: '', chain: app.chain.id })
+            }
+            onMouseEnter={(e) => {
+              handleMouseEnter({ e, isTooltipOpen, handleInteraction });
+            }}
+            onMouseLeave={(e) => {
+              handleMouseLeave({ e, isTooltipOpen, handleInteraction });
+            }}
+          >
+            {smartTrim(community?.name, 17) || <Skeleton width="70%" />}
+          </CWText>
+        )}
+      />
       {isInsideCommunity && (
         <CollapsableSidebarButton
           isInsideCommunity={isInsideCommunity}
