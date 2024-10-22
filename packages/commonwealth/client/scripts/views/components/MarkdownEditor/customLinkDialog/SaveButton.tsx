40cdb0b595c681c4e00f54cca7ff43fc5ded48f0
Kevin Burton <burtonator@gmail.com>
2024-10-22 13:38:55 -0700

    Fixed save button.

--- packages/commonwealth/client/scripts/views/components/MarkdownEditor/customLinkDialog/SaveButton.tsx
+++ packages/commonwealth/client/scripts/views/components/MarkdownEditor/customLinkDialog/SaveButton.tsx
@@ -1,23 +1,13 @@
 import React from 'react';
-import { EditorTooltip } from 'views/components/MarkdownEditor/toolbars/EditorTooltip';
-import { CWIconButton } from 'views/components/component_kit/cw_icon_button';
+import { IconButtonWithTooltip } from 'views/components/MarkdownEditor/customLinkDialog/IconButtonWithTooltip';
 
 interface SaveButtonProps {
   readonly onClick: () => void;
 }
 
 export const SaveButton = (props: SaveButtonProps) => {
+  const { onClick } = props;
   return (
-    <EditorTooltip
-      content="Save"
-      renderTrigger={(handleInteraction) => (
-        <CWIconButton
-          iconName="check"
-          onClick={props.onClick}
-          onMouseEnter={handleInteraction}
-          onMouseLeave={handleInteraction}
-        />
-      )}
-    />
+    <IconButtonWithTooltip onClick={onClick} tooltip="Save" iconName="check" />
   );
 };
