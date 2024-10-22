e4683123b6d5a46ff49fa7d234077e1cd3f6e892
Kevin Burton <burtonator@gmail.com>
2024-10-22 09:20:56 -0700

    the editing of links works on mobile... now time for desktop...

--- packages/commonwealth/client/scripts/views/components/MarkdownEditor/CustomLinkEdit.tsx
+++ packages/commonwealth/client/scripts/views/components/MarkdownEditor/CustomLinkEdit.tsx
@@ -1,37 +1,51 @@
 import {
+  cancelLinkEdit$,
   linkDialogState$,
   removeLink$,
-  switchFromPreviewToLinkEdit$,
   updateLink$,
   useCellValues,
   usePublisher,
 } from 'commonwealth-mdxeditor';
-import React from 'react';
+import React, { useState } from 'react';
 import { CWIconButton } from 'views/components/component_kit/cw_icon_button';
+import './CustomLinkEdit.scss';
 
 export const CustomLinkEdit = () => {
   const [linkDialogState] = useCellValues(linkDialogState$);
   const removeLink = usePublisher(removeLink$);
   const updateLink = usePublisher(updateLink$);
 
-  const switchFromPreviewToLinkEdit = usePublisher(
-    switchFromPreviewToLinkEdit$,
+  const [link, setLink] = useState(
+    linkDialogState.type == 'edit' ? linkDialogState.url : '',
   );
+
+  const cancelLinkEdit = usePublisher(cancelLinkEdit$);
 
   if (linkDialogState.type !== 'edit') {
     return null;
   }
-
-  // FIXME use SCSS
   return (
-    <div style={{ display: 'flex', gap: 4 }}>
-      <input type="text" value={linkDialogState.url} style={{ flexGrow: 1 }} />
+    <div className="CustomLinkEdit">
+      <input
+        type="text"
+        value={link}
+        style={{ flexGrow: 1 }}
+        autoFocus={true}
+        placeholder="Enter link"
+        onChange={(event) => setLink(event.currentTarget.value)}
+      />
 
       <CWIconButton
-        iconName="linkBreak"
+        iconName="check"
         onClick={() => {
-          console.log('FIXME: removing link');
-          removeLink();
+          updateLink({ url: link, title: link });
+        }}
+      />
+
+      <CWIconButton
+        iconName="close"
+        onClick={() => {
+          cancelLinkEdit();
         }}
       />
     </div>
