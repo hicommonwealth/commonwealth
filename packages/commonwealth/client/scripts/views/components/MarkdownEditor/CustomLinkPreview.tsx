71177ed9fa0ae5ad500f3f1b368d1c7c62a0a4a4
Kevin Burton <burtonator@gmail.com>
2024-10-22 08:42:33 -0700

    switch to edit mode works now.

--- packages/commonwealth/client/scripts/views/components/MarkdownEditor/CustomLinkPreview.tsx
+++ packages/commonwealth/client/scripts/views/components/MarkdownEditor/CustomLinkPreview.tsx
@@ -1,10 +1,19 @@
-import { linkDialogState$, useCellValues } from 'commonwealth-mdxeditor';
+import {
+  linkDialogState$,
+  switchFromPreviewToLinkEdit$,
+  useCellValues,
+  usePublisher,
+} from 'commonwealth-mdxeditor';
 import React from 'react';
+import { CWIconButton } from 'views/components/component_kit/cw_icon_button';
 import { CWIcon } from 'views/components/component_kit/cw_icons/cw_icon';
 import './CustomLinkPreview.scss';
 
 export const CustomLinkPreview = () => {
   const [linkDialogState] = useCellValues(linkDialogState$);
+  const switchFromPreviewToLinkEdit = usePublisher(
+    switchFromPreviewToLinkEdit$,
+  );
 
   if (linkDialogState.type !== 'preview') {
     return null;
@@ -24,6 +33,11 @@ export const CustomLinkPreview = () => {
       <a href={linkDialogState.url} target="_blank" rel="noreferrer">
         <CWIcon iconName="arrowSquareOut" />
       </a>
+
+      <CWIconButton
+        iconName="pencil"
+        onClick={() => switchFromPreviewToLinkEdit()}
+      />
     </div>
   );
 };
