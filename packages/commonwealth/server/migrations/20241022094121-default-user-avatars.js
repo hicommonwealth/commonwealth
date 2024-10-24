7f72da0e035e5037a0f5de38a806bac6529bc7d3
Timothee Legros <timothee@ualberta.ca>
2024-10-24 19:55:55 +0200

    update URLs to production urls

--- packages/commonwealth/server/migrations/20241022094121-default-user-avatars.js
+++ packages/commonwealth/server/migrations/20241022094121-default-user-avatars.js
@@ -8,12 +8,12 @@ module.exports = {
         `
             WITH random_avatars AS (
                 SELECT id, (ARRAY[
-                    'https://s3.us-east-1.amazonaws.com/local.assets/fb3289b0-38cb-4883-908b-7af0c1626ece.png',
-                    'https://s3.us-east-1.amazonaws.com/local.assets/794bb7a3-17d7-407a-b52e-2987501221b5.png',
-                    'https://s3.us-east-1.amazonaws.com/local.assets/181e25ad-ce08-427d-8d3a-d290af3be44b.png',
-                    'https://s3.us-east-1.amazonaws.com/local.assets/9f40b221-e2c7-4052-a7de-e580222baaa9.png',
-                    'https://s3.us-east-1.amazonaws.com/local.assets/ef919936-8554-42e5-8590-118e8cb68101.png',
-                    'https://s3.us-east-1.amazonaws.com/local.assets/0847e7f5-4d96-4406-8f30-c3082fa2f27c.png'
+                    'https://assets.commonwealth.im/fb3289b0-38cb-4883-908b-7af0c1626ece.png',
+                    'https://assets.commonwealth.im/794bb7a3-17d7-407a-b52e-2987501221b5.png',
+                    'https://assets.commonwealth.im/181e25ad-ce08-427d-8d3a-d290af3be44b.png',
+                    'https://assets.commonwealth.im/9f40b221-e2c7-4052-a7de-e580222baaa9.png',
+                    'https://assets.commonwealth.im/ef919936-8554-42e5-8590-118e8cb68101.png',
+                    'https://assets.commonwealth.im/0847e7f5-4d96-4406-8f30-c3082fa2f27c.png'
                     ])[floor(random() * 6 + 1)] AS avatar_url
                 FROM "Users"
                 WHERE ("profile" ->> 'avatar_url') IS NULL
