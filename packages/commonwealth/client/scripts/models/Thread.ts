aced8a6330bdfbcd904d933417f00321cfb6de0a
israellund <israelmlund@gmail.com>
2024-10-25 17:28:25 -0400

    deleted community_id from Thread to pass tests

--- packages/commonwealth/client/scripts/models/Thread.ts
+++ packages/commonwealth/client/scripts/models/Thread.ts
@@ -432,7 +432,6 @@ export class Thread implements IUniqueId {
       (rc) =>
         new Comment({
           authorChain: this.authorCommunity,
-          community_id: this.authorCommunity,
           id: rc?.id,
           thread_id: id,
           author: rc?.address,
