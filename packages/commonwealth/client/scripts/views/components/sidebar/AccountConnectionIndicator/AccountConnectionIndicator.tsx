4ce8a507fd4488ea2cc1040a5ebef161ec391a4f
KaleemNeslit <kaleem.neslit@gmail.com>
2024-10-30 20:23:38 +0500

    fixed the issue for tooltip and dropdown

--- packages/commonwealth/client/scripts/views/components/sidebar/AccountConnectionIndicator/AccountConnectionIndicator.tsx
+++ packages/commonwealth/client/scripts/views/components/sidebar/AccountConnectionIndicator/AccountConnectionIndicator.tsx
@@ -1,11 +1,10 @@
+import { saveToClipboard } from 'client/scripts/utils/clipboard';
 import clsx from 'clsx';
 import React from 'react';
-import { CWText } from 'views/components/component_kit/cw_text';
-import { CWIdentificationTag } from 'views/components/component_kit/new_designs/CWIdentificationTag';
-
-import { saveToClipboard } from 'client/scripts/utils/clipboard';
 import useJoinCommunity from 'views/components/SublayoutHeader/useJoinCommunity';
+import { CWText } from 'views/components/component_kit/cw_text';
 import { CWButton } from 'views/components/component_kit/new_designs/CWButton';
+import { CWIdentificationTag } from 'views/components/component_kit/new_designs/CWIdentificationTag';
 import CWIconButton from '../../component_kit/new_designs/CWIconButton';
 import { CWTooltip } from '../../component_kit/new_designs/CWTooltip';
 import './AccountConnectionIndicator.scss';
@@ -35,15 +34,19 @@ const AccountConnectionIndicator = ({
               <CWTooltip
                 placement="top"
                 content="address copied!"
-                renderTrigger={(handleInteraction) => {
+                renderTrigger={(handleInteraction, isTooltipOpen) => {
                   return (
                     <CWIconButton
                       iconName="copySimple"
-                      onMouseDown={(event) => {
+                      onClick={(event) => {
                         saveToClipboard(address).catch(console.error);
                         handleInteraction(event);
                       }}
-                      onMouseLeave={handleInteraction}
+                      onMouseLeave={(e) => {
+                        if (isTooltipOpen) {
+                          handleInteraction(e);
+                        }
+                      }}
                       className="copy-icon"
                     />
                   );
