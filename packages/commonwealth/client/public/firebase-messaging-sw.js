e44c5234b58b96929208e526965c9a4ff6da964e
Kevin Burton <burtonator@gmail.com>
2024-10-31 12:50:51 -0700

    Fixes issues with knock... other than the markdown issues.

--- packages/commonwealth/client/public/firebase-messaging-sw.js
+++ packages/commonwealth/client/public/firebase-messaging-sw.js
@@ -19,23 +19,48 @@ self.addEventListener('push', function (event) {
 
   const img = '/static/img/branding/common.png';
 
+  // NOTE that the raw event can't be called because it has a custom toJSON
+  // implementation and all you can see is "isTrusted": true.
+
   const data = event.data.json();
+
+  // we're keeping this JSON stringify here because setting breakpoints in service
+  // workers is inconsistent and this is the only way to see the REAL JSON sent
+  // from Knock. Note that this is different from what we see in their logs
+  // and there are different properties here including data.notification which
+  // is not actually shown in the Knock console.
+  console.log(
+    'Received event data from Knock: ',
+    JSON.stringify(data, null, 2),
+  );
+
+  // we MUST handle body, title, and url computation here... for all workflow
+  // message types including comment-created,
+
+  const title = data.notification.title || data.title || 'No title';
+  const body =
+    data.notification.body || data.body || data.comment_body || 'No body';
+  const url =
+    data.notification.url ||
+    data.url ||
+    data.data.comment_url ||
+    data.data.object_url ||
+    'https://common.xyz';
+
   const options = {
-    body: data.body || data.comment_body || 'No body',
+    body,
     image: img,
     icon: img,
     badge: img,
     // tag: 'notification-tag', // Optional: A tag for the notification (useful for stacking notifications)
     data: {
-      url: data.url || data.comment_url || 'https://common.xyz',
+      url,
     },
   };
 
   // Display the notification
   // https://developer.mozilla.org/en-US/docs/Web/API/ServiceWorkerRegistration/showNotification
-  event.waitUntil(
-    self.registration.showNotification(data.title || 'No title', options),
-  );
+  event.waitUntil(self.registration.showNotification(title, options));
 });
 
 // Listen for 'notificationclick' events to handle notification interactions
