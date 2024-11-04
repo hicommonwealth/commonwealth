3c6e745f5c7c51deb3c8aa210f7496355aac2bb4
kassad <kurtis@commonwealth.im>
2024-11-04 08:30:50 -0800

    Fixed PR comments

--- libs/model/src/database.ts
+++ libs/model/src/database.ts
@@ -5,19 +5,15 @@ import { buildDb } from './models';
 
 const log = logger(import.meta);
 
-const dbURI =
-  config.NODE_ENV === 'test'
-    ? 'postgresql://commonwealth:edgeware@localhost/common_test'
-    : config.DB.URI;
-
-export const sequelize = new Sequelize(dbURI, {
+export const sequelize = new Sequelize(config.DB.URI, {
   // disable string operators (https://github.com/sequelize/sequelize/issues/8417)
   // operatorsAliases: false,
   logging: config.DB.TRACE ? (msg) => log.trace(msg) : false,
   dialectOptions:
     config.NODE_ENV !== 'production' || config.DB.NO_SSL
       ? { requestTimeout: 40000 }
-      : dbURI === 'postgresql://commonwealth:edgeware@localhost/commonwealth'
+      : config.DB.URI ===
+          'postgresql://commonwealth:edgeware@localhost/commonwealth'
         ? { requestTimeout: 40000, ssl: false }
         : { requestTimeout: 40000, ssl: { rejectUnauthorized: false } },
   pool: {
