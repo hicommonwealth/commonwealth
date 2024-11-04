7ad1f6836aad68aaad5073a5d736dfdf02f6a379
Kevin Burton <burtonator@gmail.com>
2024-11-04 11:10:20 -0800

    disable safari...

--- packages/commonwealth/client/scripts/views/pages/NotificationSettings/useSupportsPushNotifications.ts
+++ packages/commonwealth/client/scripts/views/pages/NotificationSettings/useSupportsPushNotifications.ts
@@ -1,6 +1,12 @@
 import { getBrowserType } from 'helpers/browser';
 import useAppStatus from 'hooks/useAppStatus';
 
+// enable/disable safari support. This doesn't really need to be a feature flag
+// because when we enable Safari, we're going to have so work on a larger patch
+// *anyway* due to the fact that Knock doesn't support push notifications with
+// Safari in the browser.
+const SAFARI_ENABLED = false;
+
 export const useSupportsPushNotifications = () => {
   const { isAddedToHomeScreen } = useAppStatus();
 
@@ -8,7 +14,7 @@ export const useSupportsPushNotifications = () => {
 
   if (browserType === 'safari' && isAddedToHomeScreen) {
     // Safari only works if we've added it as a PWA
-    return true;
+    return SAFARI_ENABLED;
   }
 
   return browserType === 'chrome';
