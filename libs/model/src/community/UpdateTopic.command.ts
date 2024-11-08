b02485e1e2821785c4b4f780c9e09943f3130d57
Timothee Legros <timothee@ualberta.ca>
2024-11-09 01:20:58 +0200

    prevent updating archived topics in UpdateTopic.command

--- libs/model/src/community/UpdateTopic.command.ts
+++ libs/model/src/community/UpdateTopic.command.ts
@@ -23,6 +23,10 @@ export function UpdateTopic(): Command<
       const topic = await models.Topic.findByPk(topic_id!);
       mustExist('Topic', topic);
 
+      if (topic.archived_at) {
+        throw new InvalidState('Cannot update archived topic');
+      }
+
       const {
         name,
         description,
