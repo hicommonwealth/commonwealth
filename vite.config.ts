060b6d51a5b69d06c518fce251ee750c2f3d813d
rotorsoft <rotorsoft@outlook.com>
2024-11-26 10:18:40 -0500

    roll back parallel

--- vite.config.ts
+++ vite.config.ts
@@ -12,14 +12,14 @@ export default defineConfig({
     setupFiles: [path.resolve(__dirname, './libs/model/src/vitest.setup.ts')],
     poolMatchGlobs: [
       // lifecycle tests in forks pool (uses node:child_process)
-      ['**/libs/model/**/*-lifecycle.spec.ts', 'forks'],
+      // ['**/libs/model/**/*-lifecycle.spec.ts', 'forks'],
       // everything else runs in threads pool
     ],
     poolOptions: {
       threads: { minThreads: 1, maxThreads: 1 },
-      forks: { minForks: 1, maxForks: 10 },
+      // forks: { minForks: 1, maxForks: 10 },
     },
-    fileParallelism: process.env.npm_package_name === '@hicommonwealth/model',
+    fileParallelism: false, // process.env.npm_package_name === '@hicommonwealth/model',
     sequence: { concurrent: false },
     coverage: {
       include: ['src/**/*.ts'],
