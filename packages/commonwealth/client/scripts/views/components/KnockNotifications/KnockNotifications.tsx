1e07eb88ad143cd3eb9c545704281165770fb148
KaleemNeslit <kaleem.neslit@gmail.com>
2024-12-06 18:49:26 +0500

    update the ui for notification

--- packages/commonwealth/client/scripts/views/components/KnockNotifications/KnockNotifications.tsx
+++ packages/commonwealth/client/scripts/views/components/KnockNotifications/KnockNotifications.tsx
@@ -1,15 +1,18 @@
 import Knock from '@knocklabs/client';
 import {
+  Avatar,
   KnockFeedProvider,
   KnockProvider,
   NotificationFeedPopover,
   NotificationIconButton,
 } from '@knocklabs/react';
 import '@knocklabs/react-notification-feed/dist/index.css';
+import moment from 'moment';
 import React, { memo, useEffect, useRef, useState } from 'react';
+import { smartTrim } from 'shared/utils';
 import useUserStore from 'state/ui/user';
+import { CWText } from '../component_kit/cw_text';
 import './KnockNotifications.scss';
-
 const KNOCK_PUBLIC_API_KEY =
   process.env.KNOCK_PUBLIC_API_KEY ||
   'pk_test_Hd4ZpzlVcz9bqepJQoo9BvZHokgEqvj4T79fPdKqpYM';
@@ -79,6 +82,45 @@ export const KnockNotifications = memo(function KnockNotifications() {
               buttonRef={notifButtonRef}
               isVisible={isVisible}
               onClose={() => setIsVisible(false)}
+              renderItem={({ item }) => {
+                const author = item?.data?.author || '';
+                const commentBody = item?.data?.comment_body || '';
+                const title = item?.data?.title || '';
+                const communityName = item?.data?.community_name || '';
+                const type = item?.data?.type || '';
+                const createdAt = item?.inserted_at || '';
+                return (
+                  <div className="container">
+                    <div className="avatar">
+                      <Avatar name={author} />
+                    </div>
+                    <div className="content-container">
+                      <div className="title-container">
+                        <CWText fontWeight="semiBold" type="h5">
+                          {author} &nbsp;
+                          {type}
+                        </CWText>
+                        <CWText fontWeight="regular" type="b1">
+                          &nbsp;{title}
+                        </CWText>
+                        <CWText fontWeight="semiBold" type="h5">
+                          {communityName}
+                        </CWText>
+                      </div>
+                      <div className="content">
+                        <CWText fontWeight="regular" type="b2">
+                          {smartTrim(commentBody, 100)}
+                        </CWText>
+                      </div>
+                      <div className="content">
+                        <CWText fontWeight="regular" type="b2">
+                          {moment(createdAt).fromNow()}
+                        </CWText>
+                      </div>
+                    </div>
+                  </div>
+                );
+              }}
             />
           </div>
         </KnockFeedProvider>
--- packages/commonwealth/client/scripts/views/pages/notifications/KnockNotificationsContent.tsx
+++ packages/commonwealth/client/scripts/views/pages/notifications/KnockNotificationsContent.tsx
@@ -1,10 +1,14 @@
 import {
+  Avatar,
   KnockFeedProvider,
   KnockProvider,
   NotificationFeed,
 } from '@knocklabs/react';
+import moment from 'moment';
 import React from 'react';
+import { smartTrim } from 'shared/utils';
 import useUserStore from 'state/ui/user';
+import { CWText } from '../../components/component_kit/cw_text';
 
 const KNOCK_PUBLIC_API_KEY = process.env.KNOCK_PUBLIC_API_KEY;
 const KNOCK_IN_APP_FEED_ID = process.env.KNOCK_IN_APP_FEED_ID;
@@ -13,14 +17,57 @@ export const KnockNotificationsContent = () => {
   const user = useUserStore();
 
   return (
-    <KnockProvider
-      apiKey={KNOCK_PUBLIC_API_KEY!}
-      userId={`${user.id}`}
-      userToken={user.knockJWT}
-    >
-      <KnockFeedProvider feedId={KNOCK_IN_APP_FEED_ID!} colorMode="light">
-        <NotificationFeed />
-      </KnockFeedProvider>
-    </KnockProvider>
+    <div className="KnockNotifications">
+      <KnockProvider
+        apiKey={KNOCK_PUBLIC_API_KEY!}
+        userId={`${user.id}`}
+        userToken={user.knockJWT}
+      >
+        <KnockFeedProvider feedId={KNOCK_IN_APP_FEED_ID!} colorMode="light">
+          <NotificationFeed
+            renderItem={({ item }) => {
+              const author = item?.data?.author || '';
+              const commentBody = item?.data?.comment_body || '';
+              const title = item?.data?.title || '';
+              const communityName = item?.data?.community_name || '';
+              const type = item?.data?.type || '';
+              const createdAt = item?.inserted_at || '';
+
+              return (
+                <div className="container">
+                  <div className="avatar">
+                    <Avatar name={author} />
+                  </div>
+                  <div className="content-container">
+                    <div className="title-container">
+                      <CWText fontWeight="semiBold" type="h5">
+                        {author} &nbsp;
+                        {type}
+                      </CWText>
+                      <CWText fontWeight="regular" type="b1">
+                        &nbsp; {title}
+                      </CWText>
+                      <CWText fontWeight="medium" type="h5">
+                        {communityName}
+                      </CWText>
+                    </div>
+                    <div className="content">
+                      <CWText fontWeight="regular" type="b2">
+                        {smartTrim(commentBody, 100)}
+                      </CWText>
+                    </div>
+                    <div className="content">
+                      <CWText fontWeight="regular" type="b2">
+                        {moment(createdAt).fromNow()}
+                      </CWText>
+                    </div>
+                  </div>
+                </div>
+              );
+            }}
+          />
+        </KnockFeedProvider>
+      </KnockProvider>
+    </div>
   );
 };
