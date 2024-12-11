6f6ebb0caaa41f65fd5b14bbeeb23b162023484d
kassad <kurtis@commonwealth.im>
2024-12-11 12:21:12 -0800

    Fixed pr comments

--- packages/commonwealth/test/devnet/integration/integrationUtils/mainSetup.ts
+++ packages/commonwealth/test/devnet/integration/integrationUtils/mainSetup.ts
@@ -1,8 +1,8 @@
 import { commonProtocol as cp } from '@hicommonwealth/evm-protocols';
 import { models } from '@hicommonwealth/model';
 import { bootstrapBindings } from '../../../../server/bindings/bootstrap';
-import { up as up1 } from '../../../../server/migrations/20240319234133-create-outbox-channel-trigger.js';
-import { up as up2 } from '../../../../server/migrations/20240620213513-fix-pg-notify-trigger.js';
+import { up as outboxTriggerMigration } from '../../../../server/migrations/20240319234133-create-outbox-channel-trigger.js';
+import { up as outboxTriggerMigrationFix } from '../../../../server/migrations/20240620213513-fix-pg-notify-trigger.js';
 import { startMessageRelayer } from '../../../../server/workers/messageRelayer/messageRelayer';
 import { mineBlocks, setupAnvil } from './process-setup/setupAnvil';
 import { setupEvmCe } from './process-setup/setupEvmCe';
@@ -12,8 +12,14 @@ import { anvilAccounts, setupWeb3 } from './process-setup/setupWeb3';
 export async function setupCommonwealthE2E() {
   // setup outbox notifications
 
-  await up1(models.sequelize.getQueryInterface(), models.sequelize);
-  await up2(models.sequelize.getQueryInterface(), models.sequelize);
+  await outboxTriggerMigration(
+    models.sequelize.getQueryInterface(),
+    models.sequelize,
+  );
+  await outboxTriggerMigrationFix(
+    models.sequelize.getQueryInterface(),
+    models.sequelize,
+  );
 
   // need to set up anvil before we can run evmCE.
   // need to set up rmq before running consumer
