c8b8898c49b439a3b87fb801df0353174f2ad2fe
Ryan Bennett <ryan@common.xyz>
2024-12-11 11:41:00 -0800

    fix test

--- libs/evm-protocols/src/common-protocol/utils.ts
+++ libs/evm-protocols/src/common-protocol/utils.ts
@@ -1,9 +1,12 @@
 export const calculateVoteWeight = (
   balance: string, // should be in wei
   voteWeight: number = 0,
+  precision: number = 10 ** 18, // precision factor for multiplying
 ): bigint | null => {
   if (!balance || voteWeight <= 0) return null;
-  return BigInt(balance) * BigInt(voteWeight);
+  // solution to multiply BigInt with fractional vote weight
+  const scaledVoteWeight = BigInt(Math.round(voteWeight * precision));
+  return (BigInt(balance) * scaledVoteWeight) / BigInt(precision);
 };
 
 export enum Denominations {
