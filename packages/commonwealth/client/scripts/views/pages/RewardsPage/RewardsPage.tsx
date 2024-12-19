7497896729497e0cb4f4754145af4c303875e30a
Marcin <maslankam92@gmail.com>
2024-12-19 17:49:54 +0100

    Rewards tabs

--- packages/commonwealth/client/scripts/views/pages/RewardsPage/RewardsPage.tsx
+++ packages/commonwealth/client/scripts/views/pages/RewardsPage/RewardsPage.tsx
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { CWText } from '../../components/component_kit/cw_text';
 import CWPageLayout from '../../components/component_kit/new_designs/CWPageLayout';
 import { PageNotFound } from '../404';
 import RewardsCard from './RewardsCard';
+import RewardsTab from './RewardsTab';
 
 import { useFlag } from 'hooks/useFlag';
 import useUserStore from 'state/ui/user';
 
 import './RewardsPage.scss';
 
+enum RewardsTabType {
+  Referrals = 'referrals',
+  WalletBalance = 'walletBalance',
+  Quests = 'quests',
+}
+
 const RewardsPage = () => {
   const user = useUserStore();
   const rewardsEnabled = useFlag('rewardsPage');
+
+  const [activeTab, setActiveTab] = useState<RewardsTabType>(
+    RewardsTabType.Referrals,
+  );
 
   if (!user.isLoggedIn || !rewardsEnabled) {
     return <PageNotFound />;
@@ -24,6 +35,28 @@ const RewardsPage = () => {
         <CWText type="h2" className="header">
           Rewards
         </CWText>
+
+        <div className="rewards-button-tabs">
+          <RewardsTab
+            icon="userSwitch"
+            title="Referral Earnings"
+            isActive={activeTab === RewardsTabType.Referrals}
+            onClick={() => setActiveTab(RewardsTabType.Referrals)}
+          />
+          <RewardsTab
+            icon="cardholder"
+            title="Wallet Balance"
+            isActive={activeTab === RewardsTabType.WalletBalance}
+            onClick={() => setActiveTab(RewardsTabType.WalletBalance)}
+          />
+          <RewardsTab
+            icon="trophy"
+            title="Quests"
+            isActive={activeTab === RewardsTabType.Quests}
+            onClick={() => setActiveTab(RewardsTabType.Quests)}
+          />
+        </div>
+
         <div className="rewards-card-container">
           <RewardsCard
             title="Referrals"
