f710ca6d730a3c5928ac9e4685867fe4712ab95c
Marcin <maslankam92@gmail.com>
2024-12-19 14:02:21 +0100

    Styling

--- packages/commonwealth/client/scripts/views/pages/RewardsPage/RewardsPage.tsx
+++ packages/commonwealth/client/scripts/views/pages/RewardsPage/RewardsPage.tsx
@@ -1,13 +1,27 @@
 import React from 'react';
 
+import { CWText } from '../../components/component_kit/cw_text';
 import CWPageLayout from '../../components/component_kit/new_designs/CWPageLayout';
+import { PageNotFound } from '../404';
+
+import useUserStore from 'state/ui/user';
 
 import './RewardsPage.scss';
 
 const RewardsPage = () => {
+  const user = useUserStore();
+
+  if (!user.isLoggedIn) {
+    return <PageNotFound />;
+  }
+
   return (
     <CWPageLayout>
-      <div className="RewardsPage"></div>
+      <section className="RewardsPage">
+        <CWText type="h2" className="header">
+          Rewards
+        </CWText>
+      </section>
     </CWPageLayout>
   );
 };
