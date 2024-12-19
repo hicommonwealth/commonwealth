bbfc594541c30d3d5c3cea163bb127d7c49bbdf4
Marcin <maslankam92@gmail.com>
2024-12-19 13:53:39 +0100

    Add rewards page

---
+++ packages/commonwealth/client/scripts/views/pages/RewardsPage/RewardsPage.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import CWPageLayout from '../../components/component_kit/new_designs/CWPageLayout';
+
+import './RewardsPage.scss';
+
+const RewardsPage = () => {
+  return (
+    <CWPageLayout>
+      <div className="RewardsPage"></div>
+    </CWPageLayout>
+  );
+};
+
+export default RewardsPage;
