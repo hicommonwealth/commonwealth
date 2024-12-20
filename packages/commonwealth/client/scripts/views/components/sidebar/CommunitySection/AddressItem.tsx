d0adc5e9350c7823714b17968c63ad77c24df605
Salman <muhsalman.neslit@gmail.com>
2024-12-21 00:58:43 +0500

    copy-btn

--- packages/commonwealth/client/scripts/views/components/sidebar/CommunitySection/AddressItem.tsx
+++ packages/commonwealth/client/scripts/views/components/sidebar/CommunitySection/AddressItem.tsx
@@ -14,6 +14,7 @@ import { CWIcon } from '../../component_kit/cw_icons/cw_icon';
 import { CWText } from '../../component_kit/cw_text';
 import { CWTooltip } from '../../component_kit/new_designs/CWTooltip';
 
+import { saveToClipboard } from 'client/scripts/utils/clipboard';
 import './AddressItem.scss';
 
 type AddressItemProps = {
@@ -31,52 +32,79 @@ const AddressItem = (props: AddressItemProps) => {
 
   return (
     <div className="AddressItem">
-      <div className="address">
-        <CWText className="address-label" type="b2" fontWeight="regular">
-          {formatAddressShort(address, 6)}
-        </CWText>
-      </div>
-      {isSelected && <CWIcon iconName="checkCircleFilled" />}
-      {walletId === WalletId.Magic && (
+      <div className="address-section">
+        <div className="address">
+          <CWText className="address-label" type="b2" fontWeight="regular">
+            {formatAddressShort(address, 6)}
+          </CWText>
+        </div>
         <CWTooltip
           placement="top"
-          content="Open wallet"
-          renderTrigger={(handleInteraction, isTooltipOpen) => (
-            <CWIconButton
-              iconName="arrowSquareOut"
-              onClick={() => {
-                openMagicWallet().catch(console.error);
-              }}
-              onMouseEnter={(e) => {
-                handleMouseEnter({
-                  e,
-                  isTooltipOpen,
-                  handleInteraction,
-                });
-              }}
-              onMouseLeave={(e) => {
-                handleMouseLeave({
-                  e,
-                  isTooltipOpen,
-                  handleInteraction,
-                });
-              }}
-              className="open-wallet-icon"
-            />
+          content="address copied!"
+          renderTrigger={(handleInteraction, isTooltipOpen) => {
+            return (
+              <CWIconButton
+                iconName="copySimple"
+                onClick={(event) => {
+                  saveToClipboard(address).catch(console.error);
+                  handleInteraction(event);
+                }}
+                onMouseLeave={(e) => {
+                  if (isTooltipOpen) {
+                    handleInteraction(e);
+                  }
+                }}
+                className="copy-icon"
+              />
+            );
+          }}
+        />
+        {isSelected && (
+          <CWIcon iconName="checkCircleFilled" className="check-mark-icon" />
+        )}
+        {walletId === WalletId.Magic && (
+          <CWTooltip
+            placement="top"
+            content="Open wallet"
+            renderTrigger={(handleInteraction, isTooltipOpen) => (
+              <CWIconButton
+                iconName="arrowSquareOut"
+                onClick={() => {
+                  openMagicWallet().catch(console.error);
+                }}
+                onMouseEnter={(e) => {
+                  handleMouseEnter({
+                    e,
+                    isTooltipOpen,
+                    handleInteraction,
+                  });
+                }}
+                onMouseLeave={(e) => {
+                  handleMouseLeave({
+                    e,
+                    isTooltipOpen,
+                    handleInteraction,
+                  });
+                }}
+                className="open-wallet-icon"
+              />
+            )}
+          />
+        )}
+      </div>
+      <div className="popover-section">
+        <PopoverMenu
+          menuItems={[
+            {
+              label: `Remove Address`,
+              onClick: () => toggleRemoveModal(true, addressInfo),
+            },
+          ]}
+          renderTrigger={(onClick) => (
+            <CWIconButton iconName="dotsHorizontal" onClick={onClick} />
           )}
         />
-      )}
-      <PopoverMenu
-        menuItems={[
-          {
-            label: `Remove Address`,
-            onClick: () => toggleRemoveModal(true, addressInfo),
-          },
-        ]}
-        renderTrigger={(onClick) => (
-          <CWIconButton iconName="dotsHorizontal" onClick={onClick} />
-        )}
-      />
+      </div>
     </div>
   );
 };
