f6587c2ece2ddd1093f74f83f32d0d5b72f4f720
Malik Zulqurnain <malikzulqurnain2121@gmail.com>
2024-12-26 15:21:26 +0500

    Fixed pagination for get comments query

--- libs/model/src/comment/GetComments.query.ts
+++ libs/model/src/comment/GetComments.query.ts
@@ -14,76 +14,77 @@ export function GetComments(): Query<typeof schemas.GetComments> {
     body: async ({ payload }) => {
       const { thread_id, comment_id, include_reactions, limit, cursor } =
         payload;
+      const offset = (cursor - 1) * limit;
 
       const sql = `
- SELECT
-    C.id,
-    C.body,
-    C.created_at,
-    C.updated_at,
-    C.deleted_at,
-    C.marked_as_spam_at,
-    C.reaction_count,
-    CA.address,
-    CA.last_active,
-    CU.id AS "user_id",
-    CU.profile->>'name' AS "profile_name",
-    CU.profile->>'avatar_url' AS "avatar_url",
-    ${
-      include_reactions
-        ? `
-    json_agg(json_strip_nulls(json_build_object(
-        'id', R.id,
-        'address_id', R.address_id,
-        'reaction', R.reaction,
-        'created_at', R.created_at::text,
-        'updated_at', R.updated_at::text,
-        'calculated_voting_weight', R.calculated_voting_weight::text,
-        'address', RA.address,
-        'last_active', RA.last_active::text,
-        'profile_name', RU.profile->>'name',
-        'avatar_url', RU.profile->>'avatar_url'
-      ))) AS "reactions",
+        SELECT
+            C.id,
+            C.body,
+            C.created_at,
+            C.updated_at,
+            C.deleted_at,
+            C.marked_as_spam_at,
+            C.reaction_count,
+            CA.address,
+            CA.last_active,
+            CU.id AS "user_id",
+            CU.profile->>'name' AS "profile_name",
+            CU.profile->>'avatar_url' AS "avatar_url",
+            ${
+              include_reactions
+                ? `
+            json_agg(json_build_object(
+                'id', R.id,
+                'address_id', R.address_id,
+                'reaction', R.reaction,
+                'created_at', R.created_at::text,
+                'updated_at', R.updated_at::text,
+                'calculated_voting_weight', R.calculated_voting_weight::text,
+                'address', RA.address,
+                'last_active', RA.last_active::text,
+                'profile_name', RU.profile->>'name',
+                'avatar_url', RU.profile->>'avatar_url'
+              )) AS "reactions",
+                `
+                : ''
+            }
+            COUNT(*) OVER() AS total_count
+        FROM
+            "Comments" AS C
+            JOIN "Addresses" AS CA ON C."address_id" = CA."id"
+            JOIN "Users" AS CU ON CA."user_id" = CU."id"
+            ${
+              include_reactions
+                ? `
+            LEFT JOIN "Reactions" AS R ON C."id" = R."comment_id"
+            LEFT JOIN "Addresses" AS RA ON R."address_id" = RA."id"
+            LEFT JOIN "Users" AS RU ON RA."user_id" = RU."id"
+            `
+                : ''
+            }
+        WHERE
+            C."thread_id" = :thread_id
+            ${comment_id ? ' AND C."id" = :comment_id' : ''}
+        ${
+          include_reactions
+            ? `
+        GROUP BY
+            C.id,
+            C.created_at,
+            C.updated_at,
+            C.deleted_at,
+            C.marked_as_spam_at,
+            CA.address,
+            CA.last_active,
+            CU.id,
+            CU.profile->>'name',
+            CU.profile->>'avatar_url'
         `
-        : ''
-    }
-    COUNT(*) OVER() AS total_count
-FROM
-    "Comments" AS C
-    JOIN "Addresses" AS CA ON C."address_id" = CA."id"
-    JOIN "Users" AS CU ON CA."user_id" = CU."id"
-    ${
-      include_reactions
-        ? `
-    LEFT JOIN "Reactions" AS R ON C."id" = R."comment_id"
-    LEFT JOIN "Addresses" AS RA ON R."address_id" = RA."id"
-    LEFT JOIN "Users" AS RU ON RA."user_id" = RU."id"
-    `
-        : ''
-    }
-WHERE
-    C."thread_id" = :thread_id
-    ${comment_id ? ' AND C."id" = :comment_id' : ''}
-${
-  include_reactions
-    ? `
-GROUP BY
-    C.id,
-    C.created_at,
-    C.updated_at,
-    C.deleted_at,
-    C.marked_as_spam_at,
-    CA.address,
-    CA.last_active,
-    CU.id,
-    CU.profile->>'name',
-    CU.profile->>'avatar_url'
-`
-    : ''
-}
-ORDER BY
-    C."created_at"
-LIMIT :limit OFFSET :offset;      
+            : ''
+        }
+        ORDER BY
+            C."created_at"
+        LIMIT :limit OFFSET :offset;      
       `;
 
       const comments = await models.sequelize.query<
@@ -95,7 +96,7 @@ LIMIT :limit OFFSET :offset;
           thread_id,
           comment_id,
           limit,
-          offset: (cursor - 1) * limit,
+          offset,
         },
         type: QueryTypes.SELECT,
       });
@@ -112,8 +113,8 @@ LIMIT :limit OFFSET :offset;
 
       return schemas.buildPaginatedResponse(
         sanitizedComments,
-        comments?.length ? comments!.at(0)!.total_count : 0,
-        payload,
+        comments?.length ? parseInt(`${comments!.at(0)!.total_count}`) : 0,
+        { ...payload, offset },
       );
     },
   };
