4f22ff5583d98d6cf28453a27ec1c2dba48da4e7
Timothee Legros <timothee@ualberta.ca>
2025-01-06 18:29:32 +0100

    fix userMentioned get thread

--- packages/commonwealth/server/workers/knock/eventHandlers/userMentioned.ts
+++ packages/commonwealth/server/workers/knock/eventHandlers/userMentioned.ts
@@ -100,9 +100,8 @@ export const processUserMentioned: EventHandler<
     );
 
     let thread = payload.thread;
-    if (!thread) {
+    if (!thread || !thread.id) {
       const threadInstance = await models.Thread.findOne({
-        attributes: ['title'],
         where: {
           id: payload.comment!.thread_id,
         },
@@ -110,7 +109,6 @@ export const processUserMentioned: EventHandler<
       if (!threadInstance) throw new Error('Thread not found');
       thread = threadInstance.get({ plain: true });
     }
-    if (!thread.id) throw new Error('Missing thread id');
 
     await provider.triggerWorkflow({
       key: WorkflowKeys.Webhooks,
@@ -144,7 +142,7 @@ export const processUserMentioned: EventHandler<
             ? payload.thread!.content_url
             : payload.comment!.content_url,
         content_type: 'thread' in payload ? 'thread' : 'comment',
-        thread_id: thread.id,
+        thread_id: thread.id!,
         comment_id: payload.comment?.id,
         author_user_id: user.id!,
       },
