8dd6824ba78dcad62dd4fba8f151f3219dccba03
Kevin Burton <burtonator@gmail.com>
2025-01-07 14:34:55 -0800

    allow the client to enable/disable push notifications now...

--- packages/commonwealth/client/scripts/views/pages/NotificationSettings/computeChannelTypeFromBrowserType.ts
+++ packages/commonwealth/client/scripts/views/pages/NotificationSettings/computeChannelTypeFromBrowserType.ts
@@ -2,6 +2,8 @@ import { BrowserType } from 'helpers/browser';
 
 const SAFARI_USES_FCM = true;
 
+const isReactNative = !!window.ReactNativeWebView;
+
 /**
  * Compute the channel for Knock notifications.  Firebase cloud messaging or
  * Apple.
@@ -9,6 +11,10 @@ const SAFARI_USES_FCM = true;
 export function computeChannelTypeFromBrowserType(
   browserType: BrowserType | undefined,
 ): 'FCM' | 'APNS' | undefined {
+  if (isReactNative) {
+    return undefined;
+  }
+
   switch (browserType) {
     case 'safari':
       return SAFARI_USES_FCM ? 'FCM' : 'APNS';
