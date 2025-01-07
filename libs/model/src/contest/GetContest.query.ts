836f9c495a64ca20142f84fab166e881d8a0685f
Marcin <maslankam92@gmail.com>
2025-01-07 13:02:30 +0100

    add contests

--- libs/model/src/contest/GetContest.query.ts
+++ libs/model/src/contest/GetContest.query.ts
@@ -1,5 +1,6 @@
 import { Query } from '@hicommonwealth/core';
 import * as schemas from '@hicommonwealth/schemas';
+import { Order } from 'sequelize';
 import { models } from '../database';
 
 export function GetContest(): Query<typeof schemas.GetContest> {
@@ -8,24 +9,41 @@ export function GetContest(): Query<typeof schemas.GetContest> {
     auth: [],
     secure: false,
     body: async ({ payload }) => {
-      const include = payload.with_chain_node
-        ? [
+      const include = [];
+
+      if (payload.with_chain_node) {
+        include.push({
+          model: models.Community,
+          include: [
             {
-              model: models.Community,
-              include: [
-                {
-                  model: models.ChainNode.scope('withPrivateData'),
-                },
-              ],
+              model: models.ChainNode.scope('withPrivateData'),
             },
-          ]
-        : [];
+          ],
+        });
+      }
+
+      if (payload.with_contests) {
+        include.push({
+          model: models.Contest,
+          as: 'contests',
+          order: [['contest_id', 'ASC']] as Order,
+        });
+      }
+
       const contestManager = await models.ContestManager.findOne({
         where: {
           contest_address: payload.contest_address,
         },
         include,
       });
+
+      if (contestManager?.contests) {
+        contestManager.contests.forEach((contest: any) => {
+          contest.start_time = new Date(contest.start_time);
+          contest.end_time = new Date(contest.end_time);
+        });
+      }
+
       return contestManager;
     },
   };
