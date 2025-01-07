0f159952b95302e71dee09aa615ab2e9164cc344
Kevin Burton <burtonator@gmail.com>
2025-01-07 15:00:18 -0800

    see if this fixes the white buffer ...

--- packages/commonwealth/client/scripts/views/components/MobileNavigation/MobileScrollBuffer.tsx
+++ packages/commonwealth/client/scripts/views/components/MobileNavigation/MobileScrollBuffer.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { useQuickPostButtonActivated } from 'views/components/MobileNavigation/useQuickPostButtonActivated';
 import './MobileScrollBuffer.scss';
 
+const DISABLED = true;
+
 export const MobileScrollBuffer = () => {
   const activated = useQuickPostButtonActivated();
 
-  if (!activated) {
+  if (DISABLED || !activated) {
     return null;
   }
 
