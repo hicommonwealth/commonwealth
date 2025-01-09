02f7fde764e4d1db965cda7d0925f1c01f8fb8e0
Malik Zulqurnain <malikzulqurnain2121@gmail.com>
2025-01-09 21:21:18 +0500

    Added XP table

--- packages/commonwealth/client/scripts/views/pages/Leaderboard/Leaderboard.tsx
+++ packages/commonwealth/client/scripts/views/pages/Leaderboard/Leaderboard.tsx
@@ -1,19 +1,26 @@
 import React from 'react';
 import CWPageLayout from 'views/components/component_kit/new_designs/CWPageLayout';
+import { CWText } from '../../components/component_kit/cw_text';
 import './Leaderboard.scss';
 import QuestsExplorer from './QuestsExplorer';
 import TelegramBotExplorer from './TelegramBotExplorer';
 import XPExplainerCard from './XPExplainerCard';
+import XPTable from './XPTable';
 
 const Leaderboard = () => {
   return (
     <CWPageLayout>
       <section className="Leaderboard">
-        <section className="left">{/* TODO: add XP table */}</section>
-        <section className="right">
-          <XPExplainerCard />
-          <TelegramBotExplorer />
-          <QuestsExplorer />
+        <CWText type="h2">XP Leaderboard</CWText>
+        <section className="body">
+          <section className="left">
+            <XPTable />
+          </section>
+          <section className="right">
+            <XPExplainerCard />
+            <TelegramBotExplorer />
+            <QuestsExplorer />
+          </section>
         </section>
       </section>
     </CWPageLayout>
---
+++ packages/commonwealth/client/scripts/views/pages/Leaderboard/XPTable/XPTable.tsx
@@ -0,0 +1,97 @@
+import { APIOrderDirection } from 'helpers/constants';
+import React from 'react';
+import { CWIcon } from 'views/components/component_kit/cw_icons/cw_icon';
+import { CWText } from 'views/components/component_kit/cw_text';
+import { CWTable } from 'views/components/component_kit/new_designs/CWTable';
+import { CWTableColumnInfo } from 'views/components/component_kit/new_designs/CWTable/CWTable';
+import { useCWTableState } from 'views/components/component_kit/new_designs/CWTable/useCWTableState';
+import './XPTable.scss';
+
+const columns: CWTableColumnInfo[] = [
+  {
+    key: 'rank',
+    header: 'Rank',
+    numeric: true,
+    sortable: true,
+  },
+  {
+    key: 'username',
+    header: 'Username',
+    numeric: false,
+    sortable: true,
+  },
+  {
+    key: 'xp',
+    header: () => (
+      <CWText className="table-header">
+        <CWIcon iconName="help" iconSize="regular" /> XP
+      </CWText>
+    ),
+    numeric: false,
+    sortable: true,
+  },
+];
+
+const sampleData = [
+  {
+    rank: 1,
+    username: 'Commoner.eth',
+    xp: 126,
+  },
+  {
+    rank: 2,
+    username: 'Commoner.eth',
+    xp: 125,
+  },
+  {
+    rank: 3,
+    username: 'Commoner.eth',
+    xp: 124,
+  },
+  {
+    rank: 4,
+    username: 'Commoner.eth',
+    xp: 123,
+  },
+  {
+    rank: 5,
+    username: 'Commoner.eth',
+    xp: 122,
+  },
+  {
+    rank: 6,
+    username: 'Commoner.eth',
+    xp: 121,
+  },
+  {
+    rank: 7,
+    username: 'Commoner.eth',
+    xp: 120,
+  },
+  {
+    rank: 8,
+    username: 'Commoner.eth',
+    xp: 119,
+  },
+];
+
+const XPTable = () => {
+  const tableState = useCWTableState({
+    columns,
+    initialSortColumn: 'rank',
+    initialSortDirection: APIOrderDirection.Asc,
+  });
+
+  return (
+    <section className="XPTable">
+      <CWTable
+        columnInfo={tableState.columns}
+        sortingState={tableState.sorting}
+        setSortingState={tableState.setSorting}
+        rowData={sampleData}
+      />
+    </section>
+  );
+};
+
+export default XPTable;
