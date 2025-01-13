b76dcf48d1ca18bdc3bff33ff97da5780b0cf9e9
Marcin <maslankam92@gmail.com>
2025-01-13 14:25:58 +0100

    fetch referrals data

--- packages/commonwealth/client/scripts/state/api/user/index.ts
+++ packages/commonwealth/client/scripts/state/api/user/index.ts
@@ -2,17 +2,18 @@ import { useCreateApiKeyMutation } from './createApiKey';
 import { useDeleteApiKeyMutation } from './deleteApiKey';
 import { useGetApiKeyQuery } from './getApiKey';
 import useGetNewContent from './getNewContent';
+import useGetUserReferralsQuery from './getUserReferrals';
 import useUpdateUserActiveCommunityMutation from './updateActiveCommunity';
 import useUpdateUserEmailMutation from './updateEmail';
 import useUpdateUserEmailSettingsMutation from './updateEmailSettings';
 import useUpdateUserMutation from './updateUser';
 import { useSignIn } from './useSignIn';
-
 export {
   useCreateApiKeyMutation,
   useDeleteApiKeyMutation,
   useGetApiKeyQuery,
   useGetNewContent,
+  useGetUserReferralsQuery,
   useSignIn,
   useUpdateUserActiveCommunityMutation,
   useUpdateUserEmailMutation,
--- packages/commonwealth/client/scripts/views/pages/RewardsPage/tables/ReferralTable/ReferralTable.tsx
+++ packages/commonwealth/client/scripts/views/pages/RewardsPage/tables/ReferralTable/ReferralTable.tsx
@@ -1,63 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import { S3_ASSET_BUCKET_CDN } from '@hicommonwealth/shared';
 import { APIOrderDirection } from 'helpers/constants';
+import { useGetUserReferralsQuery } from 'state/api/user';
+import useUserStore from 'state/ui/user';
 import { Avatar } from 'views/components/Avatar';
 import { CWText } from 'views/components/component_kit/cw_text';
+import CWCircleMultiplySpinner from 'views/components/component_kit/new_designs/CWCircleMultiplySpinner';
 import { CWTable } from 'views/components/component_kit/new_designs/CWTable';
-import { CWTableColumnInfo } from 'views/components/component_kit/new_designs/CWTable/CWTable';
 import { useCWTableState } from 'views/components/component_kit/new_designs/CWTable/useCWTableState';
 
+import { columns } from './columns';
 import './ReferralTable.scss';
 
-const fakeData = [
-  {
-    user: {
-      name: 'cambell',
-      avatarUrl: `https://${S3_ASSET_BUCKET_CDN}/794bb7a3-17d7-407a-b52e-2987501221b5.png`,
-      userId: '128606',
-      address: 'address1',
-    },
-    earnings: '5.3',
-  },
-  {
-    user: {
-      name: 'adam',
-      avatarUrl: `https://${S3_ASSET_BUCKET_CDN}/0847e7f5-4d96-4406-8f30-c3082fa2f27c.png`,
-      userId: '135099',
-      address: 'address2',
-    },
-    earnings: '1.9',
-  },
-  {
-    user: {
-      name: 'mike',
-      avatarUrl: `https://${S3_ASSET_BUCKET_CDN}/181e25ad-ce08-427d-8d3a-d290af3be44b.png`,
-      userId: '158139',
-      address: 'address3',
-    },
-    earnings: '0.1',
-  },
-];
-
-const columns: CWTableColumnInfo[] = [
-  {
-    key: 'member',
-    header: 'Member',
-    numeric: false,
-    sortable: true,
-  },
-
-  {
-    key: 'earnings',
-    header: 'Earnings',
-    numeric: true,
-    sortable: true,
-  },
-];
-
 export const ReferralTable = () => {
+  const user = useUserStore();
+
+  const { data: referrals, isLoading } = useGetUserReferralsQuery({
+    userId: user?.id,
+    apiCallEnabled: !!user?.id,
+  });
+
   const tableState = useCWTableState({
     columns,
     initialSortColumn: 'earnings',
@@ -67,36 +30,52 @@ export const ReferralTable = () => {
   return (
     <>
       <div className="ReferralTable">
-        {fakeData.length > 0 ? (
+        {isLoading ? (
+          <div className="loading-state">
+            <CWCircleMultiplySpinner />
+          </div>
+        ) : referrals && referrals.length > 0 ? (
           <CWTable
             columnInfo={tableState.columns}
             sortingState={tableState.sorting}
             setSortingState={tableState.setSorting}
-            rowData={fakeData.map((item) => ({
+            rowData={referrals.map((item, index) => ({
               ...item,
-              member: {
-                sortValue: item.user.name.toLowerCase(),
+              rank: {
+                sortValue: index + 1,
+                customElement: <div className="table-cell">{index + 1}</div>,
+              },
+              username: {
+                sortValue: item.referee_profile?.name?.toLowerCase(),
                 customElement: (
                   <div className="table-cell">
                     <Link
-                      to={`/profile/id/${item.user.userId}`}
+                      to={`/profile/id/${item.referee_user_id}`}
                       className="user-info"
                     >
                       <Avatar
-                        url={item.user.avatarUrl ?? ''}
+                        url={item.referee_profile?.avatar_url ?? ''}
                         size={24}
-                        address={+item.user.address}
+                        address={+item.referee_address}
                       />
-                      <p>{item.user.name}</p>
+                      <p>{item.referee_profile?.name}</p>
                     </Link>
                   </div>
                 ),
               },
+              address: {
+                sortValue: item.referee_address,
+                customElement: (
+                  <div className="table-cell">{item.referee_address}</div>
+                ),
+              },
               earnings: {
-                sortValue: item.earnings,
+                sortValue: parseFloat(
+                  String(item.referrer_received_eth_amount),
+                ),
                 customElement: (
                   <div className="table-cell text-right">
-                    USD {item.earnings}
+                    USD {parseFloat(String(item.referrer_received_eth_amount))}
                   </div>
                 ),
               },
