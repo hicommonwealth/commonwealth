47faab7e784fa0f4ca5345ac0f08452b1272e408
Malik Zulqurnain <malikzulqurnain2121@gmail.com>
2025-01-16 19:05:37 +0500

    Correctly refresh market cap progress and tokens list after each trade

--- packages/commonwealth/client/scripts/state/api/tokens/createTokenTrade.ts
+++ packages/commonwealth/client/scripts/state/api/tokens/createTokenTrade.ts
@@ -1,7 +1,16 @@
 import { trpc } from 'utils/trpcClient';
 
 const useCreateTokenTradeMutation = () => {
-  return trpc.launchpadToken.createTrade.useMutation();
+  const utils = trpc.useUtils();
+
+  return trpc.launchpadToken.createTrade.useMutation({
+    onSuccess: async () => {
+      await utils.launchpadToken.getTokens.invalidate();
+      await utils.launchpadToken.getTokens.refetch();
+      await utils.launchpadToken.getToken.invalidate();
+      await utils.launchpadToken.getToken.refetch();
+    },
+  });
 };
 
 export default useCreateTokenTradeMutation;
