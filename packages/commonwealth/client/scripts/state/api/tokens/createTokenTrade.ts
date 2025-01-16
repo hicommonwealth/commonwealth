ff9da05746e912df57cfe28a07e4297cf6c30504
Malik Zulqurnain <malikzulqurnain2121@gmail.com>
2025-01-16 19:10:53 +0500

    Removed extra refetch

--- packages/commonwealth/client/scripts/state/api/tokens/createTokenTrade.ts
+++ packages/commonwealth/client/scripts/state/api/tokens/createTokenTrade.ts
@@ -6,9 +6,7 @@ const useCreateTokenTradeMutation = () => {
   return trpc.launchpadToken.createTrade.useMutation({
     onSuccess: async () => {
       await utils.launchpadToken.getTokens.invalidate();
-      await utils.launchpadToken.getTokens.refetch();
       await utils.launchpadToken.getToken.invalidate();
-      await utils.launchpadToken.getToken.refetch();
     },
   });
 };
