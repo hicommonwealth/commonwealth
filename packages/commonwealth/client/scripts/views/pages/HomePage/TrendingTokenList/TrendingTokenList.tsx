d4225f1844d4a960bc8e6496e7b3d88f191e9c03
Salman <muhsalman.neslit@gmail.com>
2025-01-31 18:21:14 +0500

    es-lint

--- packages/commonwealth/client/scripts/views/pages/HomePage/TrendingTokenList/TrendingTokenList.tsx
+++ packages/commonwealth/client/scripts/views/pages/HomePage/TrendingTokenList/TrendingTokenList.tsx
@@ -66,6 +66,7 @@ const TrendingTokensList = ({
       ) {
         return communitySortOptionsLabelToKeysMap[
           filters.withCommunitySortBy
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
         ] as any;
       }
 
