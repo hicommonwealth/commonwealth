b8253ce72c067d02f7cce1e4554aabe5ba98c0be
Salman <muhsalman.neslit@gmail.com>
2025-01-31 15:22:07 +0500

    contest-link

--- packages/commonwealth/client/scripts/views/pages/HomePage/ActiveContestList/ActiveContestList.tsx
+++ packages/commonwealth/client/scripts/views/pages/HomePage/ActiveContestList/ActiveContestList.tsx
@@ -4,6 +4,8 @@ import { Skeleton } from 'views/components/Skeleton';
 import { CWText } from 'views/components/component_kit/cw_text';
 import useCommunityContests from '../../CommunityManagement/Contests/useCommunityContests';
 
+import { CWIcon } from 'client/scripts/views/components/component_kit/cw_icons/cw_icon';
+import { Link } from 'react-router-dom';
 import ActiveContestCard from '../ActiveContestCard/ActiveContestCard';
 import './ActiveContestList.scss';
 
@@ -40,7 +42,15 @@ const ActiveContestList = () => {
 
   return (
     <div className="ActiveContestList">
-      <CWText type="h2">Contests</CWText>
+      <div className="heading-container">
+        <CWText type="h2">Contests</CWText>
+        <Link to="/explore">
+          <div className="link-right">
+            <CWText className="link">See all contests</CWText>
+            <CWIcon iconName="arrowRightPhosphor" className="blue-icon" />
+          </div>
+        </Link>
+      </div>
       <>
         {!isContestDataLoading && activeContests.length === 0 && (
           <CWText type="h2" className="empty-contests">
