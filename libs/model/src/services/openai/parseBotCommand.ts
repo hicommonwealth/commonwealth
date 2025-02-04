525b92a7ec9c980b2229b2fc59e1c5370da98b11
Ryan Bennett <ryan@common.xyz>
2025-02-04 13:10:16 -0800

    support updated test cases for contest bot

--- libs/model/src/services/openai/parseBotCommand.ts
+++ libs/model/src/services/openai/parseBotCommand.ts
@@ -5,6 +5,12 @@ import {
 } from 'openai/resources/index.mjs';
 import { config } from '../../config';
 
+export const DEFAULT_CONTEST_BOT_PARAMS = {
+  payoutStructure: [40, 30, 20],
+  voterShare: 10,
+  image_url: 'https://placehold.co/600x400/EEE/31343C',
+};
+
 export type ContestMetadataResponse = {
   contestName: string;
   payoutStructure: number[];
@@ -18,29 +24,49 @@ export const DEFAULT_VOTER_SHARE = 15;
 const system_prompt: ChatCompletionMessage = {
   role: 'assistant',
   content: `
-    You are a data extraction system to understand intents of the following style of message: \n
-    "hey @contestbot create a contest with the token 0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913,
-    with 20% of prize amount allocated to voters and the rest going to two winners.
-    Winner #1 should have 75% while winner #2 has 25%.
-    The contest title is “Submit your best artwork for our token”.
-    Use the following image https://test.com/test.png" \n
-    This message should result in the following parameters: \n
+    You are a data extraction system that extracts information
+    from the user.
+
+    The user will ask to "launch", "start", "create", etc. That means they want to launch a contest.
+
+    Extract the name of the contest as well as the token address from the user prompt.
+    contestName is the name of the contest.
+    tokenAddress is the ethereum address of the funding token.
+
+    Example: "Hey @contestbot, create a Big Donut with 0xc204af95b0307162118f7bc36a91c9717490ab69"
+    Expected Output:
     {
-      "contestName": "Submit your best artwork for our token",
-      "payoutStructure": [75, 25],
-      "voterShare": 20,
-      "image_url": "https://test.com/test.png",
-      "tokenAddress": "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913"
+      contestName: "Big Donut",
+      tokenAddress: "0xc204af95b0307162118f7bc36a91c9717490ab69"
     }
-    \n
-    \n
-    The length of payoutStructure array is the number of winners.
-    payoutStructure represents the percentage alloted to each winner and its values must all add up to 100.
-    Any mention of dividing equally refers to payoutStructure, all values being equal.
-    Any mention of shares, allocation or distribution to voters should be assigned to the voterShare
-    which is independent of payoutStructure.
-    If the vote share is not mentioned, then it should be 0 by default.
-    If it is said that X% goes to one winner, then payoutStructure is [X] because the sole winner gets X%.
+
+    Example: "@commonbot, ignite the engine! Happy Monday funded via 0x833589fcd6edb6e08f4c7c32d4f71b54bda02913"
+    Expected Output:
+    {
+      contestName: "Happy Monday",
+      tokenAddress: "0x833589fcd6edb6e08f4c7c32d4f71b54bda02913"
+    }
+
+    Example: "@commonbot, create a Good Dogs Only dog battle. Use 0x0555E30da8f98308EdB960aa94C0Db47230d2B9c."
+    Expected Output:
+    {
+      contestName: "Good Dogs Only",
+      tokenAddress: "0x0555E30da8f98308EdB960aa94C0Db47230d2B9c"
+    }
+
+    Example: "@commonbot, rev up the meme machine! Launch 0xcbb7c0000ab88b473b1f5afd9ef808440eed33bf."
+    Expected Output:
+    {
+      contestName: "meme machine",
+      tokenAddress: "0xcbb7c0000ab88b473b1f5afd9ef808440eed33bf"
+    }
+
+    Example: "@contestbot, let's go! Diamond Handz funded by 0x820c137fa70c8691f0e44dc420a5e53c168921dc"
+    Expected Output:
+    {
+      contestName: "Diamond Handz",
+      tokenAddress: "0x820c137fa70c8691f0e44dc420a5e53c168921dc"
+    }
     `,
 };
 
@@ -53,9 +79,6 @@ const tools: ChatCompletionTool[] = [
         type: 'object',
         properties: {
           contestName: { type: 'string' },
-          payoutStructure: { type: 'array', items: { type: 'number' } },
-          voterShare: { type: 'number' },
-          image_url: { type: 'string' },
           tokenAddress: { type: 'string' },
         },
       },
@@ -118,41 +141,13 @@ export const parseBotCommand = async (
     throw new ParseBotCommandError('NoResponse');
   }
 
-  if (!data.payoutStructure.length) {
-    throw new ParseBotCommandError('InvalidParams');
-  }
-
-  const payoutStructure: Array<number> = data.payoutStructure.map((n: number) =>
-    Math.floor(n),
-  );
-
-  // first pass: ensure that there are always 3 slots
-  // if there's only 1 split and sum is less than 100
-  const sumA = payoutStructure.reduce((p, acc) => acc + p, 0);
-  if (payoutStructure.length === 1 && sumA < 100) {
-    const remainder = 100 - sumA;
-    const splitRemainder = Math.floor(remainder / 2);
-    payoutStructure.push(splitRemainder);
-    payoutStructure.push(splitRemainder);
-  }
-
-  // if payout structure has any remainder under 100,
-  // give to winner
-  const sumB = payoutStructure.reduce((p, acc) => acc + p, 0);
-  if (sumB < 100) {
-    const remainder = 100 - sumB;
-    payoutStructure[0] += remainder;
-  }
-
-  if (!data.contestName || !data.image_url || !data.tokenAddress) {
+  if (!data.contestName || !data.tokenAddress) {
     throw new ParseBotCommandError('InvalidParams');
   }
 
   return {
     contestName: data.contestName,
-    payoutStructure,
-    voterShare: data.voterShare || DEFAULT_VOTER_SHARE,
-    image_url: data.image_url,
     tokenAddress: data.tokenAddress,
+    ...DEFAULT_CONTEST_BOT_PARAMS,
   };
 };
