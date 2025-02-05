76e14408164bb3ae980bd910868425c0c60ef560
Malik Zulqurnain <malikzulqurnain2121@gmail.com>
2025-02-05 23:13:07 +0500

    Updated wallet card UI

--- packages/commonwealth/client/scripts/views/pages/RewardsPage/cards/WalletCard/WalletCard.tsx
+++ packages/commonwealth/client/scripts/views/pages/RewardsPage/cards/WalletCard/WalletCard.tsx
@@ -1,11 +1,62 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import FractionalValue from 'views/components/FractionalValue';
+import { CWText } from 'views/components/component_kit/cw_text';
+import {
+  CWTab,
+  CWTabsRow,
+} from 'views/components/component_kit/new_designs/CWTabs';
 import RewardsCard from '../../RewardsCard';
-
 import './WalletCard.scss';
 
+enum WalletBalanceTabs {
+  Tokens = 'Tokens',
+  Staked = 'Staked',
+}
+
+const sampleTokens = [
+  {
+    name: 'Common',
+    price: 1231,
+  },
+  {
+    name: 'ETH',
+    price: 50,
+  },
+  {
+    name: 'Sushi',
+    price: 32,
+  },
+];
+
 const WalletCard = () => {
-  return <RewardsCard title="Wallet Balance" icon="cardholder" />;
+  const [activeTab, setActiveTab] = useState<WalletBalanceTabs>(
+    WalletBalanceTabs.Tokens,
+  );
+
+  return (
+    <RewardsCard title="Wallet Balance" icon="cardholder">
+      <div className="WalletCard">
+        <CWTabsRow>
+          {Object.values(WalletBalanceTabs).map((tab) => (
+            <CWTab
+              key={tab}
+              label={tab}
+              isSelected={activeTab === tab}
+              onClick={() => setActiveTab(tab)}
+            />
+          ))}
+        </CWTabsRow>
+        <div className="list">
+          {sampleTokens.map((token) => (
+            <div className="balance-row" key={token.name}>
+              <CWText>{token.name}</CWText>
+              <FractionalValue value={token.price} />
+            </div>
+          ))}
+        </div>
+      </div>
+    </RewardsCard>
+  );
 };
 
 export default WalletCard;
