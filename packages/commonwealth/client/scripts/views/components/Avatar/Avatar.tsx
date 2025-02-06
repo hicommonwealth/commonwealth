49731518b36318dcd8afe0df4e714f5b34b56bef
israellund <israelmlund@gmail.com>
2025-02-06 13:15:25 -0500

    removed unused library and component

--- packages/commonwealth/client/scripts/views/components/Avatar/Avatar.tsx
+++ packages/commonwealth/client/scripts/views/components/Avatar/Avatar.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import {
   CWAvatar,
   CWAvatarSkeleton,
-  CWJdenticon,
 } from 'views/components/component_kit/cw_avatar';
 
 interface AvatarProps {
@@ -21,11 +20,6 @@ export const Avatar = ({ url, size, address, showSkeleton }: AvatarProps) => {
   if (url) {
     // @ts-expect-error StrictNullChecks
     return <CWAvatar avatarUrl={url} size={size} />;
-  }
-
-  if (address) {
-    // @ts-expect-error StrictNullChecks
-    return <CWJdenticon address={String(address)} size={size} />;
   }
 
   return null;
