f18209acdf3db1d5e56cf1e44dd29c0447bbab37
Marcin <maslankam92@gmail.com>
2025-02-07 17:54:38 +0100

    Refactor ContestPage into separate NewContestPage component

--- packages/commonwealth/client/scripts/views/pages/ContestPage/ContestPage.tsx
+++ packages/commonwealth/client/scripts/views/pages/ContestPage/ContestPage.tsx
@@ -14,13 +14,8 @@ import { PageNotFound } from 'views/pages/404';
 import ContestCard from 'views/pages/CommunityManagement/Contests/ContestsList/ContestCard';
 import useCommunityContests from 'views/pages/CommunityManagement/Contests/useCommunityContests';
 
-import { CWButton } from '../../components/component_kit/new_designs/CWButton';
-import CWGrid from '../../components/component_kit/new_designs/CWGrid';
-import { CWMobileTab } from '../../components/component_kit/new_designs/CWMobileTab';
 import FundContestDrawer from '../CommunityManagement/Contests/FundContestDrawer';
-import EntriesTab from './tabs/Entries';
-import PriceChartTab from './tabs/PriceChart';
-import TokenSwapTab from './tabs/TokenSwap';
+import NewContestPage from './NewContestPage';
 
 import './ContestPage.scss';
 
@@ -68,10 +63,6 @@ const ContestPage = ({ contestAddress }: ContestPageProps) => {
       selectedSort,
     });
 
-  const [selectedMobileTab, setSelectedMobileTab] = useState<MobileTabType>(
-    MobileTabType.Entries,
-  );
-
   if (!isContestDataLoading && !contest) {
     return <PageNotFound />;
   }
@@ -79,109 +70,7 @@ const ContestPage = ({ contestAddress }: ContestPageProps) => {
   const { end_time } = contest?.contests[0] || {};
 
   if (newContestPageEnabled) {
-    return (
-      <CWPageLayout>
-        <div className="NewContestPage">
-          <div className="top-container">
-            {contest && (
-              <ContestCard
-                key={contest?.contest_address}
-                isAdmin={false}
-                address={contest?.contest_address}
-                name={contest?.name}
-                imageUrl={contest?.image_url || ''}
-                topics={contest?.topics}
-                decimals={contest?.decimals}
-                ticker={contest?.ticker}
-                finishDate={end_time ? moment(end_time).toISOString() : ''}
-                isCancelled={!!contest?.cancelled}
-                isRecurring={!contest?.funding_token_address}
-                isHorizontal
-                showShareButton={false}
-                showLeaderboardButton={false}
-                payoutStructure={contest?.payout_structure}
-                isFarcaster={contest?.is_farcaster_contest}
-                onFund={() => setFundDrawerContest(contest)}
-              />
-            )}
-
-            <div className="navigation-buttons">
-              <CWButton
-                buttonType="secondary"
-                iconLeft="arrowLeftPhosphor"
-                label="Previous Contest"
-                onClick={() => {
-                  console.log('previous contest');
-                }}
-                containerClassName="previous-btn"
-              />
-              <CWButton
-                label={contest?.name}
-                containerClassName="contest-name"
-              />
-              <CWButton
-                buttonType="secondary"
-                label="Next Contest"
-                iconRight="arrowRightPhosphor"
-                onClick={() => {
-                  console.log('next contest');
-                }}
-                containerClassName="next-btn"
-              />
-            </div>
-          </div>
-
-          <div className="mobile-tabs">
-            <CWMobileTab
-              label={MobileTabType.Entries}
-              icon="trophy"
-              isActive={selectedMobileTab === MobileTabType.Entries}
-              onClick={() => setSelectedMobileTab(MobileTabType.Entries)}
-            />
-            <CWMobileTab
-              label={MobileTabType.PriceChart}
-              icon="chartLineUp"
-              isActive={selectedMobileTab === MobileTabType.PriceChart}
-              onClick={() => setSelectedMobileTab(MobileTabType.PriceChart)}
-            />
-            <CWMobileTab
-              label={MobileTabType.TokenSwap}
-              icon="arrowClockwise"
-              isActive={selectedMobileTab === MobileTabType.TokenSwap}
-              onClick={() => setSelectedMobileTab(MobileTabType.TokenSwap)}
-            />
-          </div>
-
-          <div className="mobile-tab-content">
-            {selectedMobileTab === MobileTabType.Entries && <EntriesTab />}
-            {selectedMobileTab === MobileTabType.PriceChart && (
-              <PriceChartTab />
-            )}
-            {selectedMobileTab === MobileTabType.TokenSwap && <TokenSwapTab />}
-          </div>
-
-          <div className="desktop-view">
-            <CWGrid>
-              <div>
-                <EntriesTab />
-              </div>
-              <div>
-                <TokenSwapTab />
-                <PriceChartTab />
-              </div>
-            </CWGrid>
-          </div>
-        </div>
-
-        <FundContestDrawer
-          onClose={() => setFundDrawerContest(undefined)}
-          isOpen={!!fundDrawerContest}
-          contestAddress={fundDrawerContest?.contest_address || ''}
-          fundingTokenAddress={fundDrawerContest?.funding_token_address || ''}
-          fundingTokenTicker={fundDrawerContest?.ticker || 'ETH'}
-        />
-      </CWPageLayout>
-    );
+    return <NewContestPage contestAddress={contestAddress} />;
   }
 
   return (
