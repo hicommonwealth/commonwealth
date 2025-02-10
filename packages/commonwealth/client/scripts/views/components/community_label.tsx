37aca0675781c47464e584d64e23f6181a2c2e41
israellund <israelmlund@gmail.com>
2025-02-10 15:06:33 -0500

    changed ellipsis to css and added tooltip

--- packages/commonwealth/client/scripts/views/components/community_label.tsx
+++ packages/commonwealth/client/scripts/views/components/community_label.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
-
-import { smartTrim } from '../../../../shared/utils';
+import { handleMouseEnter, handleMouseLeave } from 'views/menus/utils';
+import { CWTooltip } from '../components/component_kit/new_designs/CWTooltip';
 import './community_label.scss';
 import { CWCommunityAvatar } from './component_kit/cw_community_avatar';
 import type { IconSize } from './component_kit/cw_icons/types';
-
 import { CWText } from './component_kit/cw_text';
 
 type CommunityLabelProps = {
@@ -27,9 +26,27 @@ export const CommunityLabel = ({
         }}
         size={size}
       />
-      <CWText noWrap type="b1" fontWeight="medium" title={name}>
-        {smartTrim(name, 25)}
-      </CWText>
+      <CWTooltip
+        content={name && name.length > 17 ? name : null}
+        placement="top"
+        renderTrigger={(handleInteraction, isTooltipOpen) => (
+          <CWText
+            className="community-name"
+            noWrap
+            type="b1"
+            fontWeight="medium"
+            title={name}
+            onMouseEnter={(e) => {
+              handleMouseEnter({ e, isTooltipOpen, handleInteraction });
+            }}
+            onMouseLeave={(e) => {
+              handleMouseLeave({ e, isTooltipOpen, handleInteraction });
+            }}
+          >
+            {name}
+          </CWText>
+        )}
+      />
     </div>
   );
 };
