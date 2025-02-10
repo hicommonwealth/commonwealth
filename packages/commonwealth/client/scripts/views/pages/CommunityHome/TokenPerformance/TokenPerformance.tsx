3c1a81179d40247637da717dd271948223af0ec3
Salman <muhsalman.neslit@gmail.com>
2025-02-10 21:25:36 +0500

    geko-uniswap

--- packages/commonwealth/client/scripts/views/pages/CommunityHome/TokenPerformance/TokenPerformance.tsx
+++ packages/commonwealth/client/scripts/views/pages/CommunityHome/TokenPerformance/TokenPerformance.tsx
@@ -1,12 +1,47 @@
-import React from 'react';
-import { CWText } from 'views/components/component_kit/cw_text';
-
+import { ChainBase } from '@hicommonwealth/shared';
 import GeckoTerminalChart from 'client/scripts/views/components/GekoTerminalChart/GekoTerminalChart';
 import { CWIcon } from 'client/scripts/views/components/component_kit/cw_icons/cw_icon';
+import { useTokenTradeWidget } from 'client/scripts/views/components/sidebar/CommunitySection/TokenTradeWidget/useTokenTradeWidget';
+import {
+  TradingConfig,
+  TradingMode,
+} from 'client/scripts/views/modals/TradeTokenModel';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { CWText } from 'views/components/component_kit/cw_text';
+import CommonTrade from './CommonTrade/CommonTrade';
 import './TokenPerformance.scss';
+import UniswapTrade from './UniswapTrade/UniswapTrade';
 
 const TokenPerformance = () => {
+  const [tokenLaunchModalConfig, setTokenLaunchModalConfig] = useState<{
+    tradeConfig?: TradingConfig;
+  }>({ tradeConfig: undefined });
+
+  const { communityToken, isLoadingToken, isPinnedToken } =
+    useTokenTradeWidget();
+
+  useEffect(() => {
+    if (!communityToken) return;
+
+    setTokenLaunchModalConfig({
+      tradeConfig: {
+        mode: isPinnedToken ? TradingMode.Swap : TradingMode.Buy,
+        token: communityToken,
+        addressType: ChainBase.Ethereum,
+      } as TradingConfig,
+    });
+  }, [communityToken]);
+
+  if (isLoadingToken || !communityToken) return;
+
+  const chain = isPinnedToken ? 'base' : 'base-sepolia';
+  const address = isPinnedToken
+    ? // @ts-ignore Strict-Null Check
+      communityToken.contract_address
+    : // @ts-ignore Strict-Null Check
+      communityToken.token_address;
+
   return (
     <div className="TokenPerformance">
       <div className="heading-container">
@@ -18,16 +53,29 @@ const TokenPerformance = () => {
           </div>
         </Link>
       </div>
-      <>
+      <div className="performance-content">
         <GeckoTerminalChart
-          chain="base"
+          chain={chain}
           width="650px"
           height="500px"
-          poolAddress="0x7d49a065d17d6d4a55dc13649901fdbb98b2afba"
+          poolAddress={address}
           info={false}
           swaps={false}
         />
-      </>
+        {isPinnedToken
+          ? tokenLaunchModalConfig.tradeConfig && (
+              <UniswapTrade
+                // eslint-disable-next-line @typescript-eslint/no-explicit-any
+                tradeConfig={tokenLaunchModalConfig.tradeConfig as any}
+              />
+            )
+          : tokenLaunchModalConfig.tradeConfig && (
+              <CommonTrade
+                // eslint-disable-next-line @typescript-eslint/no-explicit-any
+                tradeConfig={tokenLaunchModalConfig.tradeConfig as any}
+              />
+            )}
+      </div>
     </div>
   );
 };
