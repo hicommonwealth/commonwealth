6991f5095fc0627b07959337a52fb794188c6999
Timothee Legros <timothee@ualberta.ca>
2025-02-11 16:30:20 +0100

    create community + pinnedToken + todos in mombot policy

--- libs/model/src/policies/MomBot.policy.ts
+++ libs/model/src/policies/MomBot.policy.ts
@@ -1,6 +1,14 @@
-import { logger, Policy } from '@hicommonwealth/core';
-import { parseMomBotMention } from '@hicommonwealth/model';
+import { command, logger, Policy } from '@hicommonwealth/core';
+import { commonProtocol as cp } from '@hicommonwealth/evm-protocols';
+import { config } from '@hicommonwealth/model';
 import { events } from '@hicommonwealth/schemas';
+import { ChainBase, ChainType } from '@hicommonwealth/shared';
+import _ from 'lodash';
+import { CreateCommunity } from '../community';
+import { models } from '../database';
+import { systemActor } from '../middleware';
+import { mustExist } from '../middleware/guards';
+import { parseMomBotMention } from '../services/openai';
 
 const log = logger(import.meta);
 
@@ -31,11 +39,62 @@ export function MomBotPolicy(): Policy<typeof inputs> {
         // TODO: Launch token -> Ian TBD
         const tokenAddress = '';
 
-        // Create community
-        // -> who do we make admin of the community?
+        // TODO: pregenerate a wallet for the twitter user (unless the provide an address?)
         // -> https://magic.link/docs/wallets/features/wallet-pregen
         // -> https://magic.link/posts/wallet-pregeneration
-        // -> pregenerate a wallet for the twitter user (unless the provide an address?)
+        const creatorAddress = '';
+
+        const communityName = `${payload.username}'s Dating Pool`;
+
+        const existingCommunity = await models.Community.findOne({
+          where: {
+            id: _.kebabCase(communityName.toLowerCase()),
+          },
+        });
+        if (existingCommunity) {
+          // TODO: do create a random id or can each user only have 1 dating pool?4
+          // TODO: how can we tell if this is just a retry of the same tweet processing vs new tweet
+        }
+
+        const chainNode = await models.ChainNode.findOne({
+          where: {
+            eth_chain_id:
+              config.APP_ENV === 'production'
+                ? cp.ValidChains.Base
+                : cp.ValidChains.SepoliaBase,
+          },
+        });
+        mustExist('Chain Node', chainNode);
+
+        // TODO: default symbol, name, tags, token_name
+        // TODO: ideally this should be a in transaction with pinned token
+        //  should we modify CreateCommunity to allow passing a pinned token on creation?
+        const community = await command(CreateCommunity(), {
+          actor: systemActor({ address: creatorAddress }),
+          payload: {
+            id: _.kebabCase(communityName.toLowerCase()),
+            name: communityName,
+            chain_node_id: chainNode.id!,
+            description: `A community for ${payload.username} to find his date!`,
+            social_links: [`https://x.com/${payload.username}`],
+            tags: ['mombot'],
+            directory_page_enabled: false,
+            type: ChainType.Offchain,
+            base: ChainBase.Ethereum,
+            token_name: payload.username,
+            default_symbol: '',
+          },
+        });
+        if (!community) {
+          // TODO: why would this be undefined?
+          return;
+        }
+
+        const pinnedToken = await models.PinnedToken.create({
+          community_id: community.community.id!,
+          contract_address: tokenAddress,
+          chain_node_id: chainNode.id!,
+        });
 
         // Reply to user with link to community + token info
 
