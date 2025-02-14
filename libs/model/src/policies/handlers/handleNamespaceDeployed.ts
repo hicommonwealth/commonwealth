2c9ffc18c8e482b1a8a8d7b12654782db6473213
Timothee Legros <timothee@ualberta.ca>
2025-02-14 22:11:02 +0200

    lint

--- libs/model/src/policies/handlers/handleNamespaceDeployed.ts
+++ libs/model/src/policies/handlers/handleNamespaceDeployed.ts
@@ -6,6 +6,7 @@ const log = logger(import.meta);
 export const handleNamespaceDeployed: EventHandler<
   'NamespaceDeployed',
   ZodUndefined
+  // eslint-disable-next-line @typescript-eslint/require-await
 > = async ({ payload: _ }) => {
   log.info('NamespaceDeployed event implementation not defined');
 };
