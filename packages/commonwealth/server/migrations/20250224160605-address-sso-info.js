3b490dd184c66265758d695fa75cae23221c5e7c
Timothee Legros <timothee@ualberta.ca>
2025-02-24 18:33:40 +0200

    migration

---
+++ packages/commonwealth/server/migrations/20250224160605-address-sso-info.js
@@ -0,0 +1,38 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.sequelize.transaction(async (transaction) => {
+      await queryInterface.addColumn(
+        'Addresses',
+        'oauth_provider',
+        {
+          type: Sequelize.STRING,
+          allowNull: true,
+        },
+        { transaction },
+      );
+      await queryInterface.addColumn(
+        'Addresses',
+        'oauth_email',
+        {
+          type: Sequelize.STRING,
+          allowNull: true,
+        },
+        { transaction },
+      );
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.sequelize.transaction(async (transaction) => {
+      await queryInterface.removeColumn('Addresses', 'oauth_provider', {
+        transaction,
+      });
+      await queryInterface.removeColumn('Addresses', 'oauth_email', {
+        transaction,
+      });
+    });
+  },
+};
