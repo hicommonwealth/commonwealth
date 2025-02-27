e3ca0768a7e8bc1da03fc263cb58c761a3edcb59
Ryan Bennett <ryan@common.xyz>
2025-02-27 13:46:04 -0800

    do not remove frame hashes on contest end

--- libs/model/src/contest/SetContestEnded.command.ts
+++ libs/model/src/contest/SetContestEnded.command.ts
@@ -1,4 +1,4 @@
-import { command, Command, logger } from '@hicommonwealth/core';
+import { Command, logger } from '@hicommonwealth/core';
 import {
   getContestScore,
   rollOverContest,
@@ -6,10 +6,8 @@ import {
 import * as schemas from '@hicommonwealth/schemas';
 import { config } from '../config';
 import { models } from '../database';
-import { systemActor } from '../middleware';
 import { mustExist } from '../middleware/guards';
 import { emitEvent, getChainNodeUrl } from '../utils/utils';
-import { UpdateContestManagerFrameHashes } from './UpdateContestManagerFrameHashes.command';
 
 const log = logger(import.meta);
 
@@ -57,23 +55,6 @@ export function SetContestEnded(): Command<typeof schemas.SetContestEnded> {
         },
       );
       mustExist('Contest Manager', contestManager);
-
-      if (contestManager.farcaster_frame_hashes?.length) {
-        try {
-          await command(UpdateContestManagerFrameHashes(), {
-            actor: systemActor({}),
-            payload: {
-              contest_address,
-              frames_to_remove: contestManager.farcaster_frame_hashes,
-            },
-          });
-        } catch (err) {
-          log.error(
-            'Failed to update contest manager frame hashes',
-            err as Error,
-          );
-        }
-      }
 
       await models.sequelize.transaction(async (transaction) => {
         // update final score
