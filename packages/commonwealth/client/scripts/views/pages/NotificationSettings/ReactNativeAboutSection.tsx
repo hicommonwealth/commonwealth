f9866dabffb1c0fd624c3e9a9e45682b56dc57fa
Kevin Burton <burtonator@gmail.com>
2025-03-04 10:34:41 -0800

    ...

--- packages/commonwealth/client/scripts/views/pages/NotificationSettings/ReactNativeAboutSection.tsx
+++ packages/commonwealth/client/scripts/views/pages/NotificationSettings/ReactNativeAboutSection.tsx
@@ -20,36 +20,39 @@ export const ReactNativeAboutSection = () => {
    * trying to debug production issues. This could be members of the Common team
    * that aren't deeply technical or don't have ready access to debug tools.
    */
-  const logoutAndClearSession = useCallback(async () => {
-    function clearCookies() {
-      document.cookie.split(';').forEach(function (c) {
-        document.cookie =
-          c.trim().split('=')[0] +
-          '=;expires=' +
-          new Date(0).toUTCString() +
-          ';path=/';
-      });
-    }
-
-    function clearLocalStorage() {
-      localStorage.clear();
-    }
-
-    async function clearIndexedDB() {
-      const databases = await indexedDB.databases();
-      for (const db of databases) {
-        if (!db.name) {
-          throw new Error('No database name');
-        }
-        indexedDB.deleteDatabase(db.name);
+  const logoutAndClearSession = useCallback(() => {
+    async function doAsync() {
+      function clearCookies() {
+        document.cookie.split(';').forEach(function (c) {
+          document.cookie =
+            c.trim().split('=')[0] +
+            '=;expires=' +
+            new Date(0).toUTCString() +
+            ';path=/';
+        });
       }
+
+      function clearLocalStorage() {
+        localStorage.clear();
+      }
+
+      async function clearIndexedDB() {
+        const databases = await indexedDB.databases();
+        for (const db of databases) {
+          if (!db.name) {
+            throw new Error('No database name');
+          }
+          indexedDB.deleteDatabase(db.name);
+        }
+      }
+
+      clearCookies();
+      clearLocalStorage();
+      await clearIndexedDB();
+
+      document.location.href = '/logout';
     }
-
-    clearCookies();
-    clearLocalStorage();
-    await clearIndexedDB();
-
-    document.location.href = '/logout';
+    doAsync().catch(console.error);
   }, []);
 
   if (!reactNativeWebView) {
