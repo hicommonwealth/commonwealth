21e3563be031a17d26036026c10751ed2d390305
Kevin Burton <burtonator@gmail.com>
2025-03-04 09:45:33 -0800

    somewhat ugly but this isn't bad.

--- packages/commonwealth/client/scripts/views/pages/NotificationSettings/ReactNativeAboutSection.tsx
+++ packages/commonwealth/client/scripts/views/pages/NotificationSettings/ReactNativeAboutSection.tsx
@@ -1,6 +1,5 @@
 import { useReactNativeWebView } from 'hooks/useReactNativeWebView';
-import React from 'react';
-import useUserStore from 'state/ui/user';
+import React, { useCallback } from 'react';
 import { CWButton } from 'views/components/component_kit/new_designs/CWButton';
 
 /**
@@ -8,17 +7,44 @@ import { CWButton } from 'views/components/component_kit/new_designs/CWButton';
  */
 export const ReactNativeAboutSection = () => {
   const reactNativeWebView = useReactNativeWebView();
-  const user = useUserStore();
 
-  function handleClick() {
+  const handleClick = useCallback(() => {
     if (reactNativeWebView) {
       reactNativeWebView.postMessage(JSON.stringify({ type: 'about' }));
     }
-  }
+  }, [reactNativeWebView]);
 
-  if (!user.email.endsWith('@common.xyz')) {
-    return null;
-  }
+  const logoutAndClearSession = useCallback(async () => {
+    function clearCookies() {
+      document.cookie.split(';').forEach(function (c) {
+        document.cookie =
+          c.trim().split('=')[0] +
+          '=;expires=' +
+          new Date(0).toUTCString() +
+          ';path=/';
+      });
+    }
+
+    function clearLocalStorage() {
+      localStorage.clear();
+    }
+
+    async function clearIndexedDB() {
+      const databases = await indexedDB.databases();
+      for (const db of databases) {
+        if (!db.name) {
+          throw new Error('No database name');
+        }
+        indexedDB.deleteDatabase(db.name);
+      }
+    }
+
+    clearCookies();
+    clearLocalStorage();
+    await clearIndexedDB();
+
+    document.location.href = '/logout';
+  }, []);
 
   if (!reactNativeWebView) {
     return null;
@@ -27,6 +53,11 @@ export const ReactNativeAboutSection = () => {
   return (
     <div>
       <CWButton label="About Mobile App" onClick={handleClick} />
+
+      <CWButton
+        label="Logout and Clear Session"
+        onClick={logoutAndClearSession}
+      />
     </div>
   );
 };
