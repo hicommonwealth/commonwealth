d01fe93b2bcc9c3e3bdca460de1cfe1b1b622329
Dillon Chen <5334557+dillchen@users.noreply.github.com>
2025-03-06 14:40:37 -0500

    copy update

--- packages/commonwealth/client/scripts/views/modals/InviteLinkModal/InviteLinkModal.tsx
+++ packages/commonwealth/client/scripts/views/modals/InviteLinkModal/InviteLinkModal.tsx
@@ -64,8 +64,7 @@ const InviteLinkModal = ({ onModalClose }: InviteLinkModalProps) => {
           <CWText>
             {communityId
               ? 'Get more voting power in your communities when people join with your referral link.'
-              : `When you refer your friends to Common, you'll get XP and a portion of any unchain 
-               fees from trades, swaps, and transactions they make on Common.`}
+              : `For every referral, you'll soon get offchain and onchain rewards, like fees from trades, swaps, and transactions they make on Common.`}
           </CWText>
           <CWText type="b2">
             Fees are Base only for now, more networks coming soon!
