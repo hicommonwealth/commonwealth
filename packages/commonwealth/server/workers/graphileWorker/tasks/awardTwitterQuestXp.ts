34a2a2deb40421777b8cabe7e14e4bf831b9280e
Timothee Legros <timothee@ualberta.ca>
2025-03-10 13:41:09 +0200

    lint

--- packages/commonwealth/server/workers/graphileWorker/tasks/awardTwitterQuestXp.ts
+++ packages/commonwealth/server/workers/graphileWorker/tasks/awardTwitterQuestXp.ts
@@ -7,6 +7,7 @@ import { z } from 'zod';
 const awardTwitterQuestX = async (
   payload: z.infer<typeof TaskPayloads.AwardTwitterQuestXp>,
 ) => {
+  await Promise.resolve();
   console.log(payload);
 };
 
