3b6378cef8a69d55de877518ce49309a77dbfcbe
Kevin Burton <burtonator@gmail.com>
2025-03-11 13:21:04 -0700

    removed the add to home screen prompt...

--- packages/commonwealth/client/scripts/App.tsx
+++ packages/commonwealth/client/scripts/App.tsx
@@ -14,9 +14,7 @@ import { ReactNativeLogForwarder } from 'views/components/ReactNativeBridge/Reac
 import { ReactNativeScrollToTopListener } from 'views/components/ReactNativeBridge/ReactNativeScrollToTopListener';
 import { Splash } from './Splash';
 import { openFeatureProvider } from './helpers/feature-flags';
-import useAppStatus from './hooks/useAppStatus';
 import { trpc, trpcClient } from './utils/trpcClient';
-import { AddToHomeScreenPrompt } from './views/components/AddToHomeScreenPrompt';
 import FarcasterFrameProvider from './views/components/FarcasterProvider';
 import { Mava } from './views/components/Mava';
 import OnBoardingWrapperForMobile from './views/pages/OnBoarding/OnBoardingWrapperForMobile';
@@ -25,9 +23,6 @@ OpenFeature.setProvider(openFeatureProvider);
 
 const App = () => {
   const { isLoading } = useInitApp();
-  const { isAddedToHomeScreen, isMarketingPage, isIOS, isAndroid } =
-    useAppStatus();
-
   return (
     <StrictMode>
       <HelmetProvider>
@@ -46,13 +41,6 @@ const App = () => {
                       <ReactNativeLogForwarder />
                       <ReactNativeScrollToTopListener />
                       <RouterProvider router={router()} />
-                      {isAddedToHomeScreen || isMarketingPage ? null : (
-                        <AddToHomeScreenPrompt
-                          isIOS={isIOS}
-                          isAndroid={isAndroid}
-                          displayDelayMilliseconds={1000}
-                        />
-                      )}
                     </OnBoardingWrapperForMobile>
                   </>
                 )}
--- packages/commonwealth/client/scripts/views/components/AddToHomeScreenPrompt/constants.ts
+++
@@ -1,3 +0,0 @@
-export const HIDE_PROMPT_TIME = 'hidePromptTime';
-export const HIDE_PROMPT_DAYS = 'hidePromptDays';
-export const HIDE_PROMPT = 'hidePrompt';
