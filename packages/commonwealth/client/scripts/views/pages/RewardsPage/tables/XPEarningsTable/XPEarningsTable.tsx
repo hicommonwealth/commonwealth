bc77734fea8b7f266b313135c46db61fa5177ee5
Malik Zulqurnain <malikzulqurnain2121@gmail.com>
2025-03-11 22:57:59 +0500

    CR

--- packages/commonwealth/client/scripts/views/pages/RewardsPage/tables/XPEarningsTable/XPEarningsTable.tsx
+++ packages/commonwealth/client/scripts/views/pages/RewardsPage/tables/XPEarningsTable/XPEarningsTable.tsx
@@ -16,12 +16,8 @@ import {
 } from 'views/components/component_kit/new_designs/CWTag';
 import { withTooltip } from 'views/components/component_kit/new_designs/CWTooltip';
 import { QuestAction } from '../../../CreateQuest/QuestForm/QuestActionSubForm';
-import {
-  doesActionRequireRewardShare,
-  doesActionRewardShareForCreator,
-  doesActionRewardShareForReferrer,
-} from '../../../CreateQuest/QuestForm/QuestActionSubForm/helpers';
 import './XPEarningsTable.scss';
+import { getTagConfigForRewardType } from './helpers';
 
 const columns: CWTableColumnInfo[] = [
   {
@@ -55,34 +51,6 @@ const columns: CWTableColumnInfo[] = [
     sortable: false,
   },
 ];
-
-const getTagConfigForRewardType = ({
-  action,
-  auth_user_id,
-  log,
-}: {
-  action: QuestAction;
-  auth_user_id: number;
-  log: { user_id: number; creator_id };
-}) => {
-  if (doesActionRequireRewardShare(action)) {
-    if (
-      doesActionRewardShareForCreator(action) &&
-      auth_user_id === log.creator_id
-    ) {
-      return { type: 'proposal', copy: `Creator Bonus` };
-    }
-
-    if (
-      doesActionRewardShareForReferrer(action) &&
-      auth_user_id === log.creator_id
-    ) {
-      return { type: 'trending', copy: `Referrer Bonus` };
-    }
-  }
-
-  return { type: 'new', copy: `Task Completion` };
-};
 
 export const XPEarningsTable = () => {
   const tableState = useCWTableState({
