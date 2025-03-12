f1228ba72a3bbd192b90c8b4ee028c60999f65be
Ryan Bennett <ryan@common.xyz>
2025-03-12 14:54:21 -0700

    home feed WIP

---
+++ libs/model/src/policies/HomeFeed.policy.ts
@@ -0,0 +1,108 @@
+import { Policy } from '@hicommonwealth/core';
+import { events } from '@hicommonwealth/schemas';
+import { Mutex } from 'async-mutex';
+import { z, ZodUndefined } from 'zod';
+
+const FEED_WINDOW_SIZE = 50;
+
+const homeFeedInputs = {
+  ContestStarted: events.ContestStarted,
+  ContestEnding: events.ContestEnding,
+  ContestEnded: events.ContestEnded,
+  CommunityCreated: events.CommunityCreated,
+  ThreadCreated: events.ThreadCreated,
+  // TODO: add token/XP/stake events
+};
+
+type FeedItem = {
+  type: keyof typeof homeFeedInputs;
+  data: any;
+  url: string;
+};
+
+type FeedMappers = {
+  [K in keyof typeof homeFeedInputs]: (
+    payload: z.infer<(typeof homeFeedInputs)[K]>,
+  ) => Promise<FeedItem>;
+};
+
+const feedMappers: FeedMappers = {
+  ContestStarted: async (payload) => {
+    return {
+      type: 'ContestStarted',
+      data: payload,
+      url: '',
+    };
+  },
+  ContestEnding: async (payload) => {
+    return {
+      type: 'ContestEnding',
+      data: payload,
+      url: '',
+    };
+  },
+  ContestEnded: async (payload) => {
+    return {
+      type: 'ContestEnded',
+      data: payload,
+      url: '',
+    };
+  },
+  CommunityCreated: async (payload) => {
+    return {
+      type: 'CommunityCreated',
+      data: payload,
+      url: '',
+    };
+  },
+  ThreadCreated: async (payload) => {
+    return {
+      type: 'ThreadCreated',
+      data: payload,
+      url: '',
+    };
+  },
+};
+
+const getFeed = async (): Promise<FeedItem[]> => {
+  return [];
+};
+
+const setFeed = async (feed: FeedItem[]) => {};
+
+const feedMutex = new Mutex();
+
+const addToHomeFeed = async (eventToAdd: FeedItem) => {
+  await feedMutex.runExclusive(async () => {
+    const oldFeed = await getFeed();
+    const newFeed = [...oldFeed, eventToAdd];
+    // if the feed exceeds the window size, remove the oldest items
+    if (newFeed.length > FEED_WINDOW_SIZE) {
+      return newFeed.slice(newFeed.length - FEED_WINDOW_SIZE);
+    }
+    await setFeed(newFeed);
+  });
+};
+
+export function HomeFeedPolicy(): Policy<typeof homeFeedInputs, ZodUndefined> {
+  return {
+    inputs: homeFeedInputs,
+    body: {
+      ContestStarted: async ({ payload }) => {
+        await addToHomeFeed(await feedMappers.ContestStarted(payload));
+      },
+      ContestEnding: async ({ payload }) => {
+        await addToHomeFeed(await feedMappers.ContestEnding(payload));
+      },
+      ContestEnded: async ({ payload }) => {
+        await addToHomeFeed(await feedMappers.ContestEnded(payload));
+      },
+      CommunityCreated: async ({ payload }) => {
+        await addToHomeFeed(await feedMappers.CommunityCreated(payload));
+      },
+      ThreadCreated: async ({ payload }) => {
+        await addToHomeFeed(await feedMappers.ThreadCreated(payload));
+      },
+    },
+  };
+}
