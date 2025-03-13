fe9da3121f529338590240a084dd73f98dc0c1bc
Timothee Legros <timothee@ualberta.ca>
2025-03-13 16:34:54 +0200

    Parse id

--- packages/commonwealth/server/routes/communities/get_namespace_metadata.ts
+++ packages/commonwealth/server/routes/communities/get_namespace_metadata.ts
@@ -2,15 +2,25 @@ import { AppError } from '@hicommonwealth/core';
 import { DB } from '@hicommonwealth/model';
 import { TypedRequestParams } from 'server/types';
 
+const HEX_ID_REGEX = /^[0-9a-f]{64}$/;
+
 export const getNamespaceMetadata = async (
   models: DB,
   req: TypedRequestParams<{ namespace: string; stake_id: string }>,
   res: any,
 ) => {
+  if (!HEX_ID_REGEX.test(req.params.stake_id)) {
+    throw new AppError('Invalid id');
+  }
+
+  if (!req.params.namespace) {
+    throw new AppError('Invalid namespace');
+  }
+
   //stake_id will be a 32 byte hex string, convert to number
-  const decodedId = parseInt(req.params.stake_id, 16);
+  const decodedId = parseInt('0x' + req.params.stake_id, 16);
   if (isNaN(decodedId)) {
-    throw new AppError('Invalid stake_id');
+    throw new AppError('Invalid id');
   }
   const metadata = await models.Community.findOne({
     where: {
