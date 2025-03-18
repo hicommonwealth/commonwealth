f6c6b9ec70127d51df8a26cf4b08ad2a4e0f6a5a
Marcin <maslankam92@gmail.com>
2025-03-18 15:43:53 +0100

    Add useFarcasterFrameRedirect hook for handling Farcaster frame URL redirection in Sublayout

---
+++ packages/commonwealth/client/scripts/hooks/useFarcasterFrameRedirect.ts
@@ -0,0 +1,47 @@
+import { useEffect, useState } from 'react';
+
+import { useCommonNavigate } from '../navigation/helpers';
+import useFarcasterStore from '../state/ui/farcaster';
+
+const parsePath = (path: string) => {
+  const pathParts = path.split('/').filter(Boolean);
+  return '/' + pathParts.join('/');
+};
+
+// Farcaster frame V2 opens root URL, so we need to redirect to the relative path
+// which will be the contest page URL (e.g. /dydx/contests/0x123...)
+const useFarcasterFrameRedirect = () => {
+  const [redirected, setRedirected] = useState(false);
+  const { farcasterContext } = useFarcasterStore();
+  const navigate = useCommonNavigate();
+
+  useEffect(() => {
+    const openEmbedUrl = async () => {
+      if (
+        farcasterContext?.location &&
+        'embed' in farcasterContext.location &&
+        !redirected
+      ) {
+        try {
+          const fullUrl = farcasterContext.location.embed as string;
+          const url = new URL(fullUrl);
+
+          const relativePath = parsePath(url.pathname);
+
+          if (relativePath.length > 0) {
+            navigate(relativePath, {}, null);
+            setRedirected(true);
+          } else {
+            console.error('No valid path segments found in URL:', fullUrl);
+          }
+        } catch (error) {
+          console.error('Error parsing or navigating to URL :', error);
+        }
+      }
+    };
+
+    openEmbedUrl();
+  }, [farcasterContext, navigate, redirected]);
+};
+
+export default useFarcasterFrameRedirect;
