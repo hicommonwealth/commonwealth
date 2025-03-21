759f7dc697ee672697adcf1a9e8c39f9c7c9b30a
ianrowan <i.rowan299@gmail.com>
2025-03-21 14:00:16 -0500

    enable solflare and backpack for solana

---
+++ packages/commonwealth/client/scripts/controllers/app/webWallets/solflare_web_wallet.ts
@@ -0,0 +1,85 @@
+declare let window: any;
+
+import { SolanaSigner } from '@canvas-js/chain-solana';
+import {
+  ChainBase,
+  ChainNetwork,
+  SOLANA_MAINNET_CHAIN_ID,
+  WalletId,
+} from '@hicommonwealth/shared';
+import IWebWallet from '../../../models/IWebWallet';
+
+class SolflareWebWalletController implements IWebWallet<string> {
+  // GETTERS/SETTERS
+  private _enabled = false;
+  private _enabling = false;
+  private _accounts: string[];
+
+  public readonly name = WalletId.Solflare;
+  public readonly label = 'Solflare';
+  public readonly chain = ChainBase.Solana;
+  public readonly defaultNetwork = ChainNetwork.Solana;
+
+  public get available() {
+    return window.solflare;
+  }
+
+  public get enabled() {
+    return this.available && this._enabled;
+  }
+
+  public get enabling() {
+    return this._enabling;
+  }
+
+  public get accounts() {
+    return this._accounts || [];
+  }
+
+  public getChainId() {
+    // 5ey... is the solana mainnet genesis hash
+    return SOLANA_MAINNET_CHAIN_ID;
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  public async getRecentBlock(chainIdentifier: string) {
+    return null;
+  }
+
+  public getSessionSigner() {
+    return new SolanaSigner({
+      signer: window.solflare,
+      chainId: this.getChainId(),
+    });
+  }
+
+  // ACTIONS
+  public async enable() {
+    console.log('Attempting to enable Phantom');
+    this._enabling = true;
+    if (!this.available) {
+      this._enabling = false;
+      throw new Error('Phantom wallet not installed!');
+    }
+    try {
+      const resp = await window.solflare.connect();
+      const key =
+        typeof resp.publicKey === 'function'
+          ? (await resp.publicKey()).toString()
+          : resp.publicKey.toString();
+      this._accounts = [key];
+      this._enabling = false;
+      this._enabled = true;
+    } catch (err) {
+      this._enabling = false;
+      if (!window.solana.isConnected) {
+        throw new Error(
+          'No Phantom accounts found! Please setup your Phantom wallet and try again.',
+        );
+      }
+      throw new Error('Could not connect to Phantom wallet!');
+    }
+  }
+}
+
+export default SolflareWebWalletController;
