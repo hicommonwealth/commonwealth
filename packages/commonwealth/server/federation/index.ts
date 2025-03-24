b6d8069c9df5788f60ce21cefc0389fd133b7606
kassad <kurtisassad@gmail.com>
2025-03-24 06:02:35 -0700

    Fixed linter

--- packages/commonwealth/server/federation/index.ts
+++ packages/commonwealth/server/federation/index.ts
@@ -1,7 +1,7 @@
-import { logger } from '@hicommonwealth/core';
+// import { logger } from '@hicommonwealth/core';
 // import { CanvasSignedData, startCanvasNode } from '@hicommonwealth/shared';
 
-const log = logger(import.meta);
+// const log = logger(import.meta);
 // export const { app: canvas, libp2p } = await startCanvasNode(config);
 
 // if (libp2p) {
@@ -14,51 +14,51 @@ const log = logger(import.meta);
 //   );
 // }
 
-export const applyCanvasSignedData = async (
-  path: string,
-  canvas_signed_data?: string,
-) => {
-  // if (!canvas_signed_data) return;
-  // const data = parse(canvas_signed_data) as CanvasSignedData;
-  //
-  // let appliedSessionId: string | null = null;
-  // let appliedActionId: string | null = null;
-  //
-  // log.trace('applying canvas signed data', {
-  //   path,
-  //   publicKey: data.sessionMessage.payload.publicKey,
-  // });
-  // try {
-  //   const encodedSessionMessage = canvas.messageLog.encode(
-  //     data.sessionMessageSignature,
-  //     data.sessionMessage,
-  //   );
-  //   if (!(await canvas.messageLog.has(encodedSessionMessage.id))) {
-  //     const { id: idSession } = await canvas.insert(
-  //       data.sessionMessageSignature,
-  //       data.sessionMessage,
-  //     );
-  //     appliedSessionId = idSession;
-  //   }
-  // } catch (err) {
-  //   log.warn(`could not apply canvas session: ${err.stack}`);
-  // }
-  //
-  // try {
-  //   const encodedActionMessage = canvas.messageLog.encode(
-  //     data.actionMessageSignature,
-  //     data.actionMessage,
-  //   );
-  //   if (!(await canvas.messageLog.has(encodedActionMessage.id))) {
-  //     const { id: idAction } = await canvas.insert(
-  //       data.actionMessageSignature,
-  //       data.actionMessage,
-  //     );
-  //     appliedActionId = idAction;
-  //   }
-  // } catch (err) {
-  //   log.warn(`could not apply canvas action: ${err.stack}`);
-  // }
-  //
-  // return { session: appliedSessionId, action: appliedActionId };
-};
+// export const applyCanvasSignedData = async (
+//   path: string,
+//   canvas_signed_data?: string,
+// ) => {
+// if (!canvas_signed_data) return;
+// const data = parse(canvas_signed_data) as CanvasSignedData;
+//
+// let appliedSessionId: string | null = null;
+// let appliedActionId: string | null = null;
+//
+// log.trace('applying canvas signed data', {
+//   path,
+//   publicKey: data.sessionMessage.payload.publicKey,
+// });
+// try {
+//   const encodedSessionMessage = canvas.messageLog.encode(
+//     data.sessionMessageSignature,
+//     data.sessionMessage,
+//   );
+//   if (!(await canvas.messageLog.has(encodedSessionMessage.id))) {
+//     const { id: idSession } = await canvas.insert(
+//       data.sessionMessageSignature,
+//       data.sessionMessage,
+//     );
+//     appliedSessionId = idSession;
+//   }
+// } catch (err) {
+//   log.warn(`could not apply canvas session: ${err.stack}`);
+// }
+//
+// try {
+//   const encodedActionMessage = canvas.messageLog.encode(
+//     data.actionMessageSignature,
+//     data.actionMessage,
+//   );
+//   if (!(await canvas.messageLog.has(encodedActionMessage.id))) {
+//     const { id: idAction } = await canvas.insert(
+//       data.actionMessageSignature,
+//       data.actionMessage,
+//     );
+//     appliedActionId = idAction;
+//   }
+// } catch (err) {
+//   log.warn(`could not apply canvas action: ${err.stack}`);
+// }
+//
+// return { session: appliedSessionId, action: appliedActionId };
+// };
--- packages/commonwealth/server/routes/canvas/get_canvas_clock_handler.ts
+++ packages/commonwealth/server/routes/canvas/get_canvas_clock_handler.ts
@@ -1,19 +1,22 @@
 import { AppError } from '@hicommonwealth/core';
-import { ServerControllers } from '../../routing/router';
-import { TypedRequestQuery, TypedResponse } from '../../types';
+// import { ServerControllers } from '../../routing/router';
+// import { TypedRequestQuery, TypedResponse } from '../../types';
 
-type GetStatsResponse = {
-  clock: number;
-  heads: string[];
-};
+// type GetStatsResponse = {
+//   clock: number;
+//   heads: string[];
+// };
 
-export const getCanvasClockHandler = async (
-  controllers: ServerControllers,
-  req: TypedRequestQuery<{}>,
-  res: TypedResponse<GetStatsResponse>,
-) => {
+export const getCanvasClockHandler = () => {
   throw new AppError('Not implemented');
-  // const [clock, heads] = await canvas.messageLog.getClock();
-  //
-  // return success(res, { clock, heads });
 };
+
+// export const getCanvasClockHandler = async (
+//   controllers: ServerControllers,
+//   req: TypedRequestQuery<{}>,
+//   res: TypedResponse<GetStatsResponse>,
+// ) => {
+//   const [clock, heads] = await canvas.messageLog.getClock();
+//
+//   return success(res, { clock, heads });
+// };
