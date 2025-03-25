54eeb69c239cf715ec93fb7ea61f8dba56a3f544
ianrowan <i.rowan299@gmail.com>
2025-03-25 11:54:14 -0500

    fix warnings

--- packages/commonwealth/client/scripts/views/components/component_kit/cw_icons/cw_custom_icons.tsx
+++ packages/commonwealth/client/scripts/views/components/component_kit/cw_icons/cw_custom_icons.tsx
@@ -580,11 +580,13 @@ export const CWSolflare = (props: CustomIconProps) => {
   const { componentType, iconSize, ...otherProps } = props;
   return (
     <svg
+      className={getClasses<CustomIconStyleProps>({ iconSize }, componentType)}
       xmlns="http://www.w3.org/2000/svg"
       width="32"
       height="32"
       viewBox="0 0 290 290"
       fill="none"
+      {...otherProps}
     >
       <g clipPath="url(#clip0_146_299)">
         <path
