611871ec51ed6115f33d663fa5edffc8cf1d1df5
Malik Zulqurnain <malikzulqurnain2121@gmail.com>
2025-03-25 21:43:21 +0500

    Display tweet engagement quest action in quest details page

---
+++ packages/commonwealth/client/scripts/views/pages/QuestDetails/QuestActionCard/helpers.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+
+export const actionCopies = {
+  title: {
+    ['CommunityCreated']: 'Create a community',
+    ['CommunityJoined']: 'Join a community',
+    ['ThreadCreated']: 'Create a thread',
+    ['ThreadUpvoted']: 'Upvote a thread',
+    ['CommentCreated']: 'Create a comment',
+    ['CommentUpvoted']: 'Upvote a comment',
+    ['WalletLinked']: 'Link a new wallet',
+    ['SSOLinked']: 'Link a new social (SSO)',
+    ['TweetEngagement']: 'Engage on Tweet',
+  },
+  pre_reqs: {
+    ['CommunityCreated']: () => '',
+    ['CommunityJoined']: () => '',
+    ['ThreadCreated']: () => '',
+    ['ThreadUpvoted']: () => '',
+    ['CommentCreated']: () => '',
+    ['CommentUpvoted']: () => '',
+    ['WalletLinked']: () => '',
+    ['SSOLinked']: () => '',
+    // TODO: display this same copy in quest creation form for this action
+    ['TweetEngagement']: () => `Requires Twitter/X linked to profile.`,
+  },
+  explainer: {
+    ['CommunityCreated']: () => '',
+    ['CommunityJoined']: () => '',
+    ['ThreadCreated']: () => '',
+    ['ThreadUpvoted']: () => '',
+    ['CommentCreated']: () => '',
+    ['CommentUpvoted']: () => '',
+    ['WalletLinked']: () => '',
+    ['SSOLinked']: () => '',
+    ['TweetEngagement']: (likes: number, retweets: number, replies: number) => (
+      <>
+        XP rewarded to participants after any of these tweet metrics are met.
+        <br />
+        {likes} Likes, {retweets} Retweets or {replies} Replies.
+      </>
+    ),
+  },
+  shares: {
+    ['CommunityCreated']: 'referrer',
+    ['CommunityJoined']: 'referrer',
+    ['ThreadCreated']: '',
+    ['ThreadUpvoted']: '',
+    ['CommentCreated']: '',
+    ['CommentUpvoted']: 'comment creator',
+    ['WalletLinked']: '',
+    ['SSOLinked']: '',
+    ['UserMentioned']: '',
+    ['TweetEngagement']: '',
+  },
+};
