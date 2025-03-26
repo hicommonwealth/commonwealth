c011e0aed46a4f46f2c9a1eb1603d643b72befd2
Kevin Burton <burtonator@gmail.com>
2025-03-26 12:00:09 -0700

    ... text almost done... I just need to build the link now ...

--- packages/commonwealth/client/scripts/views/components/DownloadMobileApp/DownloadMobileAppContent.tsx
+++ packages/commonwealth/client/scripts/views/components/DownloadMobileApp/DownloadMobileAppContent.tsx
@@ -5,6 +5,16 @@ import './DownloadMobileAppContent.scss';
 export const DownloadMobileAppContent = () => {
   return (
     <div className="DownloadMobileAppContent">
+      <div className="Background">
+        <p>
+          <i>Scan QR Code on your device to go to the App Store.</i>
+        </p>
+
+        <p>Common now has an app in the App Store!</p>
+
+        <p>Android coming soon!</p>
+      </div>
+
       <ShareSection permalink="https://common.xyz/download-mobile-app" />
     </div>
   );
