c502549dd82acb87e965e58e63fb31b228037cc5
Kevin Burton <burtonator@gmail.com>
2025-03-26 12:07:21 -0700

    need to add the tooltip...

--- packages/commonwealth/client/scripts/views/components/DownloadMobileApp/DownloadMobileAppContent.tsx
+++ packages/commonwealth/client/scripts/views/components/DownloadMobileApp/DownloadMobileAppContent.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { CWIcon } from 'views/components/component_kit/cw_icons/cw_icon';
+import { CWTextInput } from 'views/components/component_kit/new_designs/CWTextInput';
 import ShareSection from '../ShareSection';
 import './DownloadMobileAppContent.scss';
 
@@ -13,6 +15,17 @@ export const DownloadMobileAppContent = () => {
         <p>Common now has an app in the App Store!</p>
 
         <p>Android coming soon!</p>
+      </div>
+
+      <div className="Copy">
+        <CWTextInput
+          fullWidth
+          type="text"
+          value="FIXME"
+          readOnly
+          onClick={() => {}}
+          iconRight={<CWIcon iconName="linkPhosphor" />}
+        />
       </div>
 
       <ShareSection permalink="https://common.xyz/download-mobile-app" />
