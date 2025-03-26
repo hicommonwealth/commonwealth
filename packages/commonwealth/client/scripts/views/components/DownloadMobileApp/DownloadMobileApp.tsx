cb45899889fcf7d7f4cf64fd84bc2f008f91dc68
Kevin Burton <burtonator@gmail.com>
2025-03-26 12:36:28 -0700

    fix the border at the top...

--- packages/commonwealth/client/scripts/views/components/DownloadMobileApp/DownloadMobileApp.tsx
+++ packages/commonwealth/client/scripts/views/components/DownloadMobileApp/DownloadMobileApp.tsx
@@ -2,6 +2,7 @@ import { isMobileApp } from 'hooks/useReactNativeWebView';
 import React, { useState } from 'react';
 import { DownloadMobileAppModal } from 'views/components/DownloadMobileApp/DownloadMobileAppModal';
 import { CWButton } from '../component_kit/new_designs/CWButton';
+import './DownloadMobileApp.scss';
 
 export const DownloadMobileApp = () => {
   const [modalActive, setModalActive] = useState(false);
@@ -13,12 +14,14 @@ export const DownloadMobileApp = () => {
 
   return (
     <>
-      <CWButton
-        buttonType="secondary"
-        buttonHeight="sm"
-        label="Download Mobile App"
-        onClick={() => setModalActive(true)}
-      />
+      <div className="DownloadMobileAppButton">
+        <CWButton
+          buttonType="secondary"
+          buttonHeight="sm"
+          label="Download Mobile App"
+          onClick={() => setModalActive(true)}
+        />
+      </div>
 
       {modalActive && (
         <DownloadMobileAppModal onClose={() => setModalActive(false)} />
