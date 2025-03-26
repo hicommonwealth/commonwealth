4cdd4c2b57b533a7b85ed5bb09f15f69c61f26ee
Kevin Burton <burtonator@gmail.com>
2025-03-26 12:40:04 -0700

    ...

--- packages/commonwealth/client/scripts/views/components/DownloadMobileApp/DownloadMobileApp.tsx
+++ packages/commonwealth/client/scripts/views/components/DownloadMobileApp/DownloadMobileApp.tsx
@@ -1,14 +1,21 @@
 import { isMobileApp } from 'hooks/useReactNativeWebView';
 import React, { useState } from 'react';
 import { DownloadMobileAppModal } from 'views/components/DownloadMobileApp/DownloadMobileAppModal';
+import { useDeviceProfile } from 'views/components/MarkdownEditor/useDeviceProfile';
 import { CWButton } from '../component_kit/new_designs/CWButton';
 import './DownloadMobileApp.scss';
 
 export const DownloadMobileApp = () => {
   const [modalActive, setModalActive] = useState(false);
 
+  const deviceProfile = useDeviceProfile();
+
   if (isMobileApp()) {
     // we're already the mobile app, so we're done.
+    return null;
+  }
+
+  if (deviceProfile !== 'desktop') {
     return null;
   }
 
@@ -19,6 +26,7 @@ export const DownloadMobileApp = () => {
           buttonType="secondary"
           buttonHeight="sm"
           label="Download Mobile App"
+          iconLeft="download"
           onClick={() => setModalActive(true)}
         />
       </div>
