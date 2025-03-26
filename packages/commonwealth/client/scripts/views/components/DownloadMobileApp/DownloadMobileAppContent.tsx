0d758febfa1937864f04cfbeb6133932f670dc77
Kevin Burton <burtonator@gmail.com>
2025-03-26 10:54:41 -0700

    Fix CSS

--- packages/commonwealth/client/scripts/views/components/DownloadMobileApp/DownloadMobileAppContent.tsx
+++ packages/commonwealth/client/scripts/views/components/DownloadMobileApp/DownloadMobileAppContent.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ShareSection from '../ShareSection';
+import './DownloadMobileAppContent.scss';
 
 export const DownloadMobileAppContent = () => {
   return (
-    <>
+    <div className="DownloadMobileAppContent">
       <ShareSection permalink="https://common.xyz/download-mobile-app" />
-    </>
+    </div>
   );
 };
