ded7c3e42d88fbaf4909fc477acaaf9837c284ad
israellund <israelmlund@gmail.com>
2025-03-31 13:27:32 -0400

    made requested changes from tim

--- libs/model/src/aggregates/poll/createPollVote.command.ts
+++ libs/model/src/aggregates/poll/createPollVote.command.ts
@@ -37,6 +37,10 @@ export function CreatePollVote(): Command<typeof schemas.CreatePollVote> {
         where: {
           poll_id: payload.poll_id,
           address: address.address,
+        },
+        defaults: {
+          poll_id: payload.poll_id,
+          address: address.address,
           author_community_id: address.community_id,
           community_id: poll.community_id,
           option: payload.option,
