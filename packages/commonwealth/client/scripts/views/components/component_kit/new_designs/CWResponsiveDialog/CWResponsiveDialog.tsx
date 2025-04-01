a5db1e49cb395729edae719a89cc7ec276fb5ce4
Kevin Burton <burtonator@gmail.com>
2025-04-01 10:52:06 -0700

    Fixed bug ...

--- packages/commonwealth/client/scripts/views/components/component_kit/new_designs/CWResponsiveDialog/CWResponsiveDialog.tsx
+++ packages/commonwealth/client/scripts/views/components/component_kit/new_designs/CWResponsiveDialog/CWResponsiveDialog.tsx
@@ -2,7 +2,7 @@ import useBrowserWindow from 'hooks/useBrowserWindow';
 import React, { ReactNode, useState } from 'react';
 import CWDrawer from 'views/components/component_kit/new_designs/CWDrawer';
 import { CWModal } from 'views/components/component_kit/new_designs/CWModal';
-import './ResponsiveDialog.scss';
+import './CWResponsiveDialog.scss';
 
 type CWResponsiveDialogProps = {
   children: ReactNode;
