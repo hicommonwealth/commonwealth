a53207f1a9fc63be924b95d92465fc65e9b02919
Kevin Burton <burtonator@gmail.com>
2025-04-02 14:41:03 -0700

    ... I think I'm going to revert this and move this logic into a shared hook...

--- packages/commonwealth/client/scripts/views/modals/InviteLinkModal/InviteLinkModal.tsx
+++ packages/commonwealth/client/scripts/views/modals/InviteLinkModal/InviteLinkModal.tsx
@@ -1,21 +1,12 @@
-import { uniqBy } from 'lodash';
-import React, { useState } from 'react';
+import React, { useCallback } from 'react';
 
-import { formatAddressShort } from 'helpers';
-import app from 'state';
-import useUserStore from 'state/ui/user';
-import { saveToClipboard } from 'utils/clipboard';
 import ShareSection from 'views/components/ShareSection';
-import { generateTextAndLink } from 'views/modals/InviteLinkModal/utils';
-import { CWIcon } from '../../components/component_kit/cw_icons/cw_icon';
 import { CWText } from '../../components/component_kit/cw_text';
 import {
   CWModalBody,
   CWModalFooter,
   CWModalHeader,
 } from '../../components/component_kit/new_designs/CWModal';
-import { CWSelectList } from '../../components/component_kit/new_designs/CWSelectList';
-import { CWTextInput } from '../../components/component_kit/new_designs/CWTextInput';
 import './InviteLinkModal.scss';
 
 interface InviteLinkModalProps {
@@ -23,41 +14,30 @@ interface InviteLinkModalProps {
 }
 
 const InviteLinkModal = ({ onModalClose }: InviteLinkModalProps) => {
-  const user = useUserStore();
-  const hasJoinedCommunity = !!user.activeAccount;
-  const communityId = hasJoinedCommunity ? app.activeChainId() : '';
+  const url = useCallback((communityId: string | undefined) => {
+    const currentUrl = window.location.origin;
 
-  const availableAddresses = uniqBy(user.addresses, 'address');
+    if (currentUrl) {
+      return `${currentUrl}/${communityId}/discussions`;
+    } else {
+      return `${currentUrl}/dashboard`;
+    }
+  }, []);
 
-  const addressOptions = availableAddresses.map((addressInfo) => ({
-    value: addressInfo.address,
-    label: formatAddressShort(addressInfo.address, 6),
-  }));
+  const text = () =>
+    useCallback((communityId: string | undefined) => {
+      const isInsideCommunity = !!communityId;
 
-  const refAddress = communityId
-    ? user.activeAccount?.address
-    : addressOptions?.[0]?.value;
-
-  const [refCode, setRefCode] = useState(refAddress);
-
-  const currentUrl = window.location.origin;
-
-  const inviteLink = `${currentUrl}${
-    communityId ? `/${communityId}/discussions` : '/dashboard'
-  }?refcode=${refCode}`;
-
-  const textAndLink = generateTextAndLink(!!communityId, inviteLink);
-
-  const handleCopy = () => {
-    saveToClipboard(inviteLink, true).catch(console.error);
-  };
+      if (isInsideCommunity) {
+        return 'Join my Community on Common using my link!';
+      } else {
+        return 'Join me on Common using my link!';
+      }
+    }, []);
 
   return (
     <div className="InviteLinkModal">
-      <CWModalHeader
-        label={communityId ? 'Community invite link' : 'Common invite link'}
-        onModalClose={onModalClose}
-      />
+      <CWModalHeader label="Common invite link" onModalClose={onModalClose} />
       <CWModalBody>
         <div className="content">
           <CWText>
@@ -70,27 +50,6 @@ const InviteLinkModal = ({ onModalClose }: InviteLinkModalProps) => {
             Fees are Base only for now, more networks coming soon!
           </CWText>
           <>
-            <CWSelectList
-              label="Select Address"
-              placeholder="Select a wallet"
-              isClearable={false}
-              isSearchable={false}
-              value={addressOptions.find((option) => option.value === refCode)}
-              defaultValue={addressOptions[0]}
-              options={addressOptions}
-              onChange={(option) => setRefCode(option?.value)}
-            />
-
-            <CWTextInput
-              inputClassName="invite-link-input"
-              fullWidth
-              type="text"
-              value={inviteLink}
-              readOnly
-              onClick={handleCopy}
-              iconRight={<CWIcon iconName="copy" />}
-            />
-
             <ShareSection url={textAndLink.link} text={textAndLink.text} />
           </>
         </div>
