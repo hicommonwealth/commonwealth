6d275d445ebc2ed767ee10b6336b2575b6f2e2e3
kassad <kurtisassad@gmail.com>
2025-04-02 08:26:10 -0700

    Generalized script

---
+++ packages/commonwealth/scripts/async-delete-spam-threads.ts
@@ -0,0 +1,122 @@
+import { models, ThreadAttributes } from '@hicommonwealth/model';
+import { QueryTypes } from 'sequelize';
+
+import { parseArgs } from 'node:util';
+import { z } from 'zod';
+
+const argSchema = z.object({
+  communityId: z.string().min(1),
+  deleteAfter: z.string().optional(),
+  deleteFromTopicId: z.coerce.number().optional(),
+});
+
+const args = parseArgs({
+  options: {
+    communityId: { type: 'string' },
+    deleteAfter: { type: 'string' },
+    deleteFromTopicId: { type: 'string' },
+  },
+  strict: true,
+});
+
+const parsedArgs = argSchema.parse(args.values);
+
+async function asyncDeleteThreads() {
+  let deletedCount;
+  const { communityId, deleteAfter, deleteFromTopicId } = parsedArgs;
+  const whereClausesAND: string[] = [];
+  const vietnameseClause = `(t.title ~ '[ÀÁÂÃÈÉÊÌÍÒÓÔÕÙÚĂĐĨŨƠàáâãèéêìíòóôõùúăđĩũơƯĂẸỆỈỐỒỔỖỘỚỜỞỠỢỤỦỨỪỬỮỰỳỵỷỹÝ]' 
+        OR t.body ~ '[ÀÁÂÃÈÉÊÌÍÒÓÔÕÙÚĂĐĨŨƠàáâãèéêìíòóôõùúăđĩũơƯĂẸỆỈỐỒỔỖỘỚỜỞỠỢỤỦỨỪỬỮỰỳỵỷỹÝ]')`;
+
+  const replacements = { communityId };
+
+  if (deleteAfter) {
+    whereClausesAND.push(`t.created_at > :deleteAfter`);
+    replacements['deleteAfter'] = deleteAfter;
+  }
+
+  if (deleteFromTopicId) {
+    whereClausesAND.push(`topic.id = :deleteFromTopicId`);
+    replacements['deleteFromTopicId'] = deleteFromTopicId;
+  }
+
+  const threadSelectCommand = `
+      SELECT DISTINCT t.id
+      FROM "Threads" t
+               JOIN "Topics" topic ON topic.community_id = t.community_id
+      WHERE (t.community_id = :communityId AND ${whereClausesAND.join(' AND ')})
+          OR (t.community_id = :communityId AND ${vietnameseClause})
+          LIMIT 10;
+  `;
+  console.log('running command', threadSelectCommand);
+
+  do {
+    const deletedThreadIds = await models.sequelize.transaction(
+      async (transaction) => {
+        const threads = await models.sequelize.query<ThreadAttributes>(
+          threadSelectCommand,
+          { type: QueryTypes.SELECT, replacements, transaction },
+        );
+
+        if (threads.length === 0) {
+          console.log('No more threads found, returning');
+        }
+
+        const threadIds = threads.map((t) => t.id);
+
+        await models.sequelize.query(
+          `DELETE
+           FROM "Reactions"
+           WHERE thread_id IN (:threadIds)
+              OR comment_id IN (SELECT id FROM "Comments" WHERE thread_id IN (:threadIds));`,
+          { replacements: { threadIds }, transaction },
+        );
+
+        await models.sequelize.query(
+          `DELETE
+           FROM "CommentVersionHistories"
+           WHERE comment_id IN
+                 (SELECT id FROM "Comments" WHERE thread_id IN (:threadIds));`,
+          { replacements: { threadIds }, transaction },
+        );
+
+        await models.sequelize.query(
+          `DELETE
+           FROM "Comments"
+           WHERE thread_id IN (:threadIds);`,
+          { replacements: { threadIds }, transaction },
+        );
+
+        await models.sequelize.query(
+          `DELETE
+           FROM "ThreadVersionHistories"
+           WHERE thread_id IN (:threadIds);`,
+          { replacements: { threadIds }, transaction },
+        );
+
+        await models.sequelize.query(
+          `DELETE
+           FROM "Collaborations"
+           WHERE thread_id IN (:threadIds);`,
+          { replacements: { threadIds }, transaction },
+        );
+
+        await models.sequelize.query(
+          `DELETE
+           FROM "Threads"
+           WHERE id IN (:threadIds);`,
+          { replacements: { threadIds }, transaction },
+        );
+
+        return threadIds;
+      },
+    );
+
+    deletedCount = deletedThreadIds?.length || 0;
+    console.log(`Deleted threads with ids: ${deletedThreadIds}`);
+  } while (deletedCount === 10);
+
+  console.log('Thread deletion process completed.');
+}
+
+asyncDeleteThreads().catch(console.error);
