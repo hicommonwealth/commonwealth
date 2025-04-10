996e1427996e0a4cd29c49ce5c42b5faf699b95c
Timothee Legros <timothee@ualberta.ca>
2025-04-10 23:47:33 +0200

    update search indexes

--- packages/commonwealth/server/migrations/20250410194649-spam-indexes.js
+++ packages/commonwealth/server/migrations/20250410194649-spam-indexes.js
@@ -4,13 +4,51 @@
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.sequelize.transaction(async (transaction) => {
-      await queryInterface.removeIndex('Threads', 'threads_title_trgm_idx', {
-        transaction,
-      });
+      // Update Threads.title index
       await queryInterface.sequelize.query(
         `
+        DROP INDEX IF EXISTS threads_title_trgm_idx;
+
         CREATE INDEX threads_title_trgm_idx
         ON "Threads" USING gin (title gin_trgm_ops)
+        WHERE marked_as_spam_at IS NULL;
+      `,
+        { transaction },
+      );
+
+      // Update Threads.search column + index
+      await queryInterface.sequelize.query(
+        `
+        DROP INDEX IF EXISTS OffchainThreads_search;
+
+        ALTER TABLE "Threads"
+        ALTER COLUMN search DROP NOT NULL;
+
+        UPDATE "Threads"
+        SET search = null
+        WHERE marked_as_spam_at IS NULL;
+
+        CREATE INDEX threads_search
+        ON "Threads" USING gin(search)
+        WHERE marked_as_spam_at IS NULL;
+      `,
+        { transaction },
+      );
+
+      // Update Comments.search column + index
+      await queryInterface.sequelize.query(
+        `
+        DROP INDEX IF EXISTS comments_search;
+
+        ALTER TABLE "Comments"
+        ALTER COLUMN search DROP NOT NULL;
+
+        UPDATE "Comments"
+        SET search = null
+        WHERE marked_as_spam_at IS NULL;
+
+        CREATE INDEX comments_search
+        ON "Comments" USING gin(search)
         WHERE marked_as_spam_at IS NULL;
       `,
         { transaction },
