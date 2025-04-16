7803eef787839fd0db1c7daa8066a1a0e6defe47
israellund <israelmlund@gmail.com>
2025-04-16 10:37:46 -0400

    increased max sommunity_selected_ids length and deleted comments

--- libs/model/src/utils/updateCommunityDirectoryTags.ts
+++ libs/model/src/utils/updateCommunityDirectoryTags.ts
@@ -2,7 +2,7 @@ import { AppError } from '@hicommonwealth/core';
 import { Transaction } from 'sequelize';
 import { models } from '../database';
 
-export const MAX_SELECTED_COMMUNITIES = 20;
+export const MAX_SELECTED_COMMUNITIES = 1000;
 
 export const updateCommunityDirectoryTags = async (
   tag_names: string[],
@@ -10,14 +10,12 @@ export const updateCommunityDirectoryTags = async (
   selected_community_ids: string[],
   transaction?: Transaction,
 ) => {
-  // Check for maximum number of selected communities
   if (selected_community_ids.length > MAX_SELECTED_COMMUNITIES) {
     throw new AppError(
       `Cannot select more than ${MAX_SELECTED_COMMUNITIES} communities`,
     );
   }
 
-  // Get tag IDs from names
   const tags = await models.Tags.findAll({
     where: { name: tag_names },
     attributes: ['id', 'name'],
@@ -26,7 +24,6 @@ export const updateCommunityDirectoryTags = async (
 
   const tagNameToId = new Map(tags.map((tag) => [tag.name, tag.id]));
 
-  // Verify all tag names exist
   if (tag_names.length > 0) {
     const tagCount = tags.length;
     const allTagsFound = tagCount === tag_names.length;
@@ -36,7 +33,6 @@ export const updateCommunityDirectoryTags = async (
     }
   }
 
-  // Delete existing entries
   await models.CommunityDirectoryTags.destroy({
     where: { community_id },
     transaction,
@@ -44,7 +40,6 @@ export const updateCommunityDirectoryTags = async (
 
   const entries = [];
 
-  // Create entries for tags (with null selected_community_id)
   for (const tag_name of tag_names) {
     const tag_id = tagNameToId.get(tag_name);
     if (tag_id !== undefined) {
@@ -58,7 +53,6 @@ export const updateCommunityDirectoryTags = async (
     }
   }
 
-  // Create entries for selected communities (with null tag_id)
   for (const selected_community_id of selected_community_ids) {
     entries.push({
       community_id,
@@ -69,7 +63,6 @@ export const updateCommunityDirectoryTags = async (
     });
   }
 
-  // Insert all entries if we have any
   if (entries.length > 0) {
     const [status, newRows] = await models.CommunityDirectoryTags.bulkCreate(
       entries,
