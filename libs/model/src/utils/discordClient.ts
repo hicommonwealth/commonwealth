5116ceeac088fe1bad359b219ba308ab9cc70fba
Timothee Legros <timothee@ualberta.ca>
2025-04-17 20:00:28 +0200

    simplify getDiscordClient

--- libs/model/src/utils/discordClient.ts
+++ libs/model/src/utils/discordClient.ts
@@ -1,28 +1,29 @@
+import { logger } from '@hicommonwealth/core';
 import { Client, GatewayIntentBits } from 'discord.js';
 import { config } from '../config';
+
+const log = logger(import.meta);
 
 let client: Client;
 
 export async function getDiscordClient(): Promise<Client<true>> {
-  if (client && client.isReady() && client.token) return client;
-
-  if (!client) {
-    client = new Client({
-      intents: [GatewayIntentBits.Guilds],
-    });
+  if (client && client.isReady() && client.token) {
+    log.trace('Using existing discord client');
+    return client;
   }
 
-  return new Promise(async (resolve) => {
-    if (client.isReady()) {
-      resolve(client);
-    } else {
+  return new Promise((resolve) => {
+    if (!client) {
+      client = new Client({
+        intents: [GatewayIntentBits.Guilds],
+      });
       client.once('ready', () => {
+        log.trace('Discord client ready');
         resolve(client as Client<true>);
       });
     }
 
-    if (!client.token) {
-      await client.login(config.DISCORD.BOT_TOKEN);
-    }
+    log.trace('Logging in to discord');
+    client.login(config.DISCORD.BOT_TOKEN);
   });
 }
