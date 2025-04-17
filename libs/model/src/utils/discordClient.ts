ad257692c0af1bc1048a4d3df60e519439b7ae2d
Timothee Legros <timothee@ualberta.ca>
2025-04-17 20:05:23 +0200

    handle timeout + error

--- libs/model/src/utils/discordClient.ts
+++ libs/model/src/utils/discordClient.ts
@@ -12,7 +12,11 @@ export async function getDiscordClient(): Promise<Client<true>> {
     return client;
   }
 
-  return new Promise((resolve) => {
+  return new Promise((resolve, reject) => {
+    const timeout = setTimeout(() => {
+      reject(new Error('Discord client connection timed out after 5 seconds'));
+    }, 5_000); // 5 second timeout
+
     if (!client) {
       client = new Client({
         intents: [GatewayIntentBits.Guilds],
@@ -21,9 +25,17 @@ export async function getDiscordClient(): Promise<Client<true>> {
         log.trace('Discord client ready');
         resolve(client as Client<true>);
       });
+      client.once('error', (e) => {
+        log.error('Discord client error');
+        clearTimeout(timeout);
+        reject(e);
+      });
     }
 
     log.trace('Logging in to discord');
-    client.login(config.DISCORD.BOT_TOKEN);
+    client.login(config.DISCORD.BOT_TOKEN).catch((e) => {
+      clearTimeout(timeout);
+      reject(e);
+    });
   });
 }
