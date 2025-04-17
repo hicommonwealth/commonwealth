317d1a8cdea028be39228992d52c2afac65def6d
Salman <muhsalman.neslit@gmail.com>
2025-04-17 19:31:14 +0500

    holder-tab

--- packages/commonwealth/client/scripts/views/components/UpvoteDrawer/ViewUpvotesDrawer/Tabs/HoldersTab.tsx
+++ packages/commonwealth/client/scripts/views/components/UpvoteDrawer/ViewUpvotesDrawer/Tabs/HoldersTab.tsx
@@ -1,10 +1,41 @@
+import { APIOrderDirection } from 'client/scripts/helpers/constants';
 import React from 'react';
-import { CWText } from '../../../component_kit/cw_text';
+import { CWTable } from '../../../component_kit/new_designs/CWTable';
+import { CWTableColumnInfo } from '../../../component_kit/new_designs/CWTable/CWTable';
+import { useCWTableState } from '../../../component_kit/new_designs/CWTable/useCWTableState';
 
 export const HoldersTab = () => {
+  const columns: CWTableColumnInfo[] = [
+    {
+      key: 'holder',
+      header: 'User',
+      numeric: false,
+      sortable: true,
+    },
+    {
+      key: 'amount',
+      header: 'Amount',
+      numeric: true,
+      sortable: true,
+    },
+  ];
+
+  const tableState = useCWTableState({
+    columns,
+    initialSortColumn: 'amount',
+    initialSortDirection: APIOrderDirection.Desc,
+  });
+
+  const rowData = [];
+
   return (
     <div className="holders-tab">
-      <CWText type="b1">Holders information will be displayed here</CWText>
+      <CWTable
+        columnInfo={tableState.columns}
+        sortingState={tableState.sorting}
+        setSortingState={tableState.setSorting}
+        rowData={rowData}
+      />
     </div>
   );
 };
