a59378fd68cd29ef5fefa025ccc484145a125635
israellund <israelmlund@gmail.com>
2025-04-18 17:55:28 -0400

    yep

--- libs/model/src/models/community_directory_tags.ts
+++ libs/model/src/models/community_directory_tags.ts
@@ -61,12 +61,6 @@ export default (sequelize: Sequelize.Sequelize) => {
             exclude: ['created_at', 'updated_at'],
           },
         },
-        indexes: [
-          {
-            unique: true,
-            fields: ['community_id', 'tag_id', 'selected_community_id'],
-          },
-        ],
       },
     )
   );
