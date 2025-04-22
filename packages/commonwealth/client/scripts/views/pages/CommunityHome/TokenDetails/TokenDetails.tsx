c67ef62719b8b99ba9b6ca9505aaa8a22592ba66
Dillon <dillon@common.xyz>
2025-04-22 01:40:04 -0400

    update token details display

--- packages/commonwealth/client/scripts/views/pages/CommunityHome/TokenDetails/TokenDetails.tsx
+++ packages/commonwealth/client/scripts/views/pages/CommunityHome/TokenDetails/TokenDetails.tsx
@@ -1,4 +1,5 @@
 import { TokenView } from '@hicommonwealth/schemas';
+import BigNumber from 'bignumber.js';
 import { formatAddressShort } from 'client/scripts/helpers';
 import { calculateTokenPricing } from 'client/scripts/helpers/launchpad';
 import { useFetchTokenUsdRateQuery } from 'client/scripts/state/api/communityStake';
@@ -10,9 +11,12 @@ import { CWText } from 'client/scripts/views/components/component_kit/cw_text';
 import { CWTooltip } from 'client/scripts/views/components/component_kit/new_designs/CWTooltip';
 import { LaunchpadToken } from 'client/scripts/views/modals/TradeTokenModel/CommonTradeModal/types';
 import { ExternalToken } from 'client/scripts/views/modals/TradeTokenModel/UniswapTradeModal/types';
+import { currencyNameToSymbolMap, SupportedCurrencies } from 'helpers/currency';
+import numeral from 'numeral';
 import React from 'react';
 import { useTokenTradeWidget } from 'views/components/sidebar/CommunitySection/TokenTradeWidget/useTokenTradeWidget';
 import { z } from 'zod';
+import FormattedDisplayNumber from '../../../components/FormattedDisplayNumber/FormattedDisplayNumber';
 import SocialLinks from './SocialLinks/SocialLinks';
 import './TokenDetails.scss';
 
@@ -59,11 +63,34 @@ const TokenDetails = ({
       : (communityToken as LaunchpadToken).icon_url
     : undefined;
 
-  const marketCap = communityToken
-    ? isPinnedToken
-      ? 'N/A'
-      : (communityToken as LaunchpadToken).eth_market_cap_target
-    : undefined;
+  const formattedMarketCap =
+    communityToken &&
+    tokenPricing &&
+    typeof tokenPricing.marketCapCurrent === 'number'
+      ? `${currencyNameToSymbolMap[SupportedCurrencies.USD]}${numeral(tokenPricing.marketCapCurrent).format('0.00a')}`
+      : 'N/A';
+
+  // Set Total Supply to 1B for community tokens, null otherwise
+  const totalSupply = !isPinnedToken ? '1000000000' : null;
+  const formattedTotalSupply = totalSupply
+    ? numeral(totalSupply).format('0.0a')
+    : 'N/A';
+
+  // Re-added Sanity Check Log
+  if (
+    tokenPricing &&
+    totalSupply &&
+    typeof tokenPricing.currentPrice === 'number'
+  ) {
+    const calculatedMarketCap = new BigNumber(tokenPricing.currentPrice).times(
+      new BigNumber(totalSupply),
+    );
+    console.log('>>> TokenDetails Sanity Check:');
+    console.log('   Price:', tokenPricing.currentPrice);
+    console.log('   Supply:', totalSupply);
+    console.log('   Calculated MCAP:', calculatedMarketCap.toString());
+    console.log('   Reported MCAP:', tokenPricing.marketCapCurrent);
+  }
 
   return (
     <div className="token-details">
@@ -159,8 +186,45 @@ const TokenDetails = ({
           <CWText type="b1" className="faded">
             Market Cap
           </CWText>
-          {communityToken ? <CWText>{marketCap}</CWText> : <CWText>N/A</CWText>}
+          <CWText type="b1" fontWeight="medium">
+            {formattedMarketCap}
+          </CWText>
         </div>
+        <div className="stat-item">
+          <CWText type="b1" className="faded">
+            Price
+          </CWText>
+          {/* Use FormattedDisplayNumber for price */}
+          {communityToken &&
+          tokenPricing &&
+          typeof tokenPricing.currentPrice === 'number' ? (
+            <FormattedDisplayNumber
+              value={tokenPricing.currentPrice}
+              options={{
+                style: 'currency',
+                currency: 'USD',
+                maximumFractionDigits: 4,
+              }}
+              type="b1"
+              fontWeight="medium"
+            />
+          ) : (
+            <CWText type="b1" fontWeight="medium">
+              N/A
+            </CWText>
+          )}
+        </div>
+        {/* Conditionally render Total Supply Stat Item only for Launchpad Tokens */}
+        {!isPinnedToken && (
+          <div className="stat-item">
+            <CWText type="b1" className="faded">
+              Total Supply
+            </CWText>
+            <CWText type="b1" fontWeight="medium">
+              {formattedTotalSupply}
+            </CWText>
+          </div>
+        )}
         <div className="token-footer">
           <CWText type="b1" className="faded">
             <CWIcon iconName="users" iconSize="small" className="footer-icon" />{' '}
