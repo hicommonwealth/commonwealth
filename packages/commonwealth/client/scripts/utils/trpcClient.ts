1ee55d8ccdcdc538467c91b182de1ba3dc246763
Timothee Legros <timothee@ualberta.ca>
2025-04-25 02:42:53 +0200

    dry trpcClient

--- packages/commonwealth/client/scripts/utils/trpcClient.ts
+++ packages/commonwealth/client/scripts/utils/trpcClient.ts
@@ -9,36 +9,29 @@ export const BASE_API_PATH = '/api/internal/trpc';
 
 export const trpcClient = trpc.createClient({
   links: [
-    ['production', 'beta'].includes(process.env.APP_ENV || 'local') ||
-    process.env.ENABLE_TRPC_BATCHING === 'true'
-      ? httpBatchLink({
-          url: BASE_API_PATH,
-          headers() {
-            const user = userStore.getState();
-            return {
-              authorization: user.jwt || '',
-              isPWA: user.isOnPWA?.toString(),
-              address:
-                user.addressSelectorSelectedAddress ??
-                user.activeAccount?.address ??
-                user.addresses?.at(0)?.address,
-            };
-          },
-        })
-      : httpLink({
-          url: BASE_API_PATH,
-          headers() {
-            const user = userStore.getState();
-            return {
-              authorization: user.jwt || '',
-              isPWA: user.isOnPWA?.toString(),
-              address:
-                user.addressSelectorSelectedAddress ??
-                user.activeAccount?.address ??
-                user.addresses?.at(0)?.address,
-            };
-          },
-        }),
+    (() => {
+      const getHeaders = () => {
+        const user = userStore.getState();
+        return {
+          authorization: user.jwt || '',
+          isPWA: user.isOnPWA?.toString(),
+          address:
+            user.addressSelectorSelectedAddress ??
+            user.activeAccount?.address ??
+            user.addresses?.at(0)?.address,
+        };
+      };
+      return ['production', 'beta'].includes(process.env.APP_ENV || 'local') ||
+        process.env.ENABLE_TRPC_BATCHING === 'true'
+        ? httpBatchLink({
+            url: BASE_API_PATH,
+            headers: getHeaders,
+          })
+        : httpLink({
+            url: BASE_API_PATH,
+            headers: getHeaders,
+          });
+    })(),
   ],
   transformer: undefined,
 });
