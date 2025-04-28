f3a153b84d54f0a3f6a0ef0b12be026bc3944107
Kevin Burton <burtonator@gmail.com>
2025-04-28 12:12:42 -0700

    Fixed bug with privy not loading.

--- packages/commonwealth/client/scripts/views/components/DefaultPrivyProvider/DefaultPrivyProvider.tsx
+++ packages/commonwealth/client/scripts/views/components/DefaultPrivyProvider/DefaultPrivyProvider.tsx
@@ -1,4 +1,3 @@
-import { useFlag } from 'hooks/useFlag';
 import React, { memo } from 'react';
 import { LoadPrivy } from './LoadPrivy';
 import { WaitForPrivy } from './WaitForPrivy';
@@ -12,12 +11,8 @@ export const DefaultPrivyProvider = memo(function DefaultPrivyProvider(
 ) {
   const { children } = props;
 
-  const privyEnabled = useFlag('privy');
-
-  if (!privyEnabled) {
-    return children;
-  }
-
+  // NOTE normally, we would NOT mount the privy components, if the privy
+  // feature flag was false, however, Privy won't work with that configuration.
   return (
     <LoadPrivy>
       <WaitForPrivy>{children}</WaitForPrivy>
