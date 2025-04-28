0cf5ce18f0815e2046b6543a11f4ef7c1ce95596
Timothee Legros <timothee@ualberta.ca>
2025-04-29 02:55:58 +0300

    fix type

--- libs/model/src/aggregates/thread/UpdateThread.command.ts
+++ libs/model/src/aggregates/thread/UpdateThread.command.ts
@@ -328,83 +328,81 @@ export function UpdateThread(): Command<typeof schemas.UpdateThread> {
 
       // TODO: should we make a query out of this, or do we have one already?
       return {
-        ...(
-          await models.Thread.findOne({
-            where: { id: thread_id },
-            include: [
-              {
-                model: models.Address,
-                as: 'Address',
-                include: [
-                  {
-                    model: models.User,
-                    required: true,
-                    attributes: ['id', 'profile', 'tier'],
-                  },
-                ],
-              },
-              {
-                model: models.Address,
-                as: 'collaborators',
-                include: [
-                  {
-                    model: models.User,
-                    required: true,
-                    attributes: ['id', 'profile', 'tier'],
-                  },
-                ],
-              },
-              { model: models.Topic, as: 'topic' },
-              {
-                model: models.Reaction,
-                as: 'reactions',
-                include: [
-                  {
-                    model: models.Address,
-                    required: true,
-                    include: [
-                      {
-                        model: models.User,
-                        required: true,
-                        attributes: ['id', 'profile', 'tier'],
-                      },
-                    ],
-                  },
-                ],
-              },
-              {
-                model: models.Comment,
-                limit: 3, // This could me made configurable, atm we are using 3 recent comments with threads in frontend.
-                order: [['created_at', 'DESC']],
-                attributes: [
-                  'id',
-                  'address_id',
-                  'body',
-                  'created_at',
-                  'updated_at',
-                  'deleted_at',
-                  'marked_as_spam_at',
-                  'discord_meta',
-                ],
-                include: [
-                  {
-                    model: models.Address,
-                    attributes: ['address'],
-                    include: [
-                      {
-                        model: models.User,
-                        attributes: ['profile', 'tier'],
-                      },
-                    ],
-                  },
-                ],
-              },
-              {
-                model: models.ThreadVersionHistory,
-              },
-            ],
-          })
-        )?.toJSON(),
+        ...(await models.Thread.findOne({
+          where: { id: thread_id },
+          include: [
+            {
+              model: models.Address,
+              as: 'Address',
+              include: [
+                {
+                  model: models.User,
+                  required: true,
+                  attributes: ['id', 'profile', 'tier'],
+                },
+              ],
+            },
+            {
+              model: models.Address,
+              as: 'collaborators',
+              include: [
+                {
+                  model: models.User,
+                  required: true,
+                  attributes: ['id', 'profile', 'tier'],
+                },
+              ],
+            },
+            { model: models.Topic, as: 'topic' },
+            {
+              model: models.Reaction,
+              as: 'reactions',
+              include: [
+                {
+                  model: models.Address,
+                  required: true,
+                  include: [
+                    {
+                      model: models.User,
+                      required: true,
+                      attributes: ['id', 'profile', 'tier'],
+                    },
+                  ],
+                },
+              ],
+            },
+            {
+              model: models.Comment,
+              limit: 3, // This could me made configurable, atm we are using 3 recent comments with threads in frontend.
+              order: [['created_at', 'DESC']],
+              attributes: [
+                'id',
+                'address_id',
+                'body',
+                'created_at',
+                'updated_at',
+                'deleted_at',
+                'marked_as_spam_at',
+                'discord_meta',
+              ],
+              include: [
+                {
+                  model: models.Address,
+                  attributes: ['address'],
+                  include: [
+                    {
+                      model: models.User,
+                      attributes: ['profile', 'tier'],
+                    },
+                  ],
+                },
+              ],
+            },
+            {
+              model: models.ThreadVersionHistory,
+            },
+          ],
+        }))!.toJSON(),
         spam_toggled: spamToggled,
       };
     },
