23c098088a4c8d84d78d142394cf7084cc67cb30
Salman <muhsalman.neslit@gmail.com>
2025-04-29 07:41:59 +0500

    Community Modal Refactor

--- packages/commonwealth/client/scripts/views/components/EditProfile/UserTrustLevel/CommunitySelectionModal.tsx
+++ packages/commonwealth/client/scripts/views/components/EditProfile/UserTrustLevel/CommunitySelectionModal.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import { CWText } from 'views/components/component_kit/cw_text';
+import { CWButton } from 'views/components/component_kit/new_designs/CWButton';
 import {
   CWModal,
   CWModalBody,
   CWModalHeader,
 } from 'views/components/component_kit/new_designs/CWModal';
 import { CWSelectList } from 'views/components/component_kit/new_designs/CWSelectList';
+import { CWDivider } from '../../component_kit/cw_divider';
+import './CommunitySelectionModal.scss';
 
 interface UserCommunity {
   id: string;
@@ -38,18 +42,36 @@ const CommunitySelectionModal = ({
       onClose={onClose}
       content={
         <>
-          <CWModalHeader label="Select a Community" onModalClose={onClose} />
+          <CWModalHeader label="Verify Community" onModalClose={onClose} />
           <CWModalBody>
-            <CWSelectList
-              options={communityOptions}
-              onChange={(option) => {
-                if (option) {
-                  navigate(`/${option.value}`);
-                  onClose();
-                }
-              }}
-              placeholder="Select a community..."
-            />
+            <div className="CommunitySectionModal">
+              <CWText type="b1" className="description">
+                Choose an existing community or create a new one to verify your
+                trust level.
+              </CWText>
+              <CWSelectList
+                options={communityOptions}
+                onChange={(option) => {
+                  if (option) {
+                    navigate(`/${option.value}`);
+                    onClose();
+                  }
+                }}
+                placeholder="Select a community..."
+              />
+              <CWDivider />
+              <div className="create-community-button">
+                <CWButton
+                  label="Create New Community"
+                  buttonType="primary"
+                  buttonHeight="lg"
+                  onClick={() => {
+                    navigate('/createCommunity');
+                    onClose();
+                  }}
+                />
+              </div>
+            </div>
           </CWModalBody>
         </>
       }
