7c1267f4061f3fd2b68956502794ce38344d1287
Kevin Burton <burtonator@gmail.com>
2025-05-05 14:10:08 -0700

    ok this should be all that we need now...

--- packages/commonwealth/client/scripts/views/components/PrivyMobile/DebugPrivyMobile.tsx
+++ packages/commonwealth/client/scripts/views/components/PrivyMobile/DebugPrivyMobile.tsx
@@ -1,14 +1,31 @@
 import React from 'react';
 import usePrivyMobileAuthStatusStore from 'views/components/PrivyMobile/usePrivyMobileAuthStatusStore';
+import { usePrivyMobileSignMessage } from 'views/components/PrivyMobile/usePrivyMobileSignMessage';
 
 export const DebugPrivyMobile = () => {
   const { status: privyMobileAuthStatus } = usePrivyMobileAuthStatusStore();
+
+  const signMessage = usePrivyMobileSignMessage();
+
+  const handleSignMessage = () => {
+    async function doAsync() {
+      const result = await signMessage('hello');
+      console.log(result);
+    }
+
+    doAsync().catch(console.error);
+  };
 
   return (
     <div>
       <div>
         <b>privyMobileAuthStatus:</b>
       </div>
+
+      <div>
+        <button onClick={handleSignMessage}>sign message</button>
+      </div>
+
       <div>{JSON.stringify(privyMobileAuthStatus, null, 2)}</div>
     </div>
   );
--- packages/commonwealth/client/scripts/views/components/PrivyMobile/usePrivyMobileSignMessage.ts
+++ packages/commonwealth/client/scripts/views/components/PrivyMobile/usePrivyMobileSignMessage.ts
@@ -1,19 +1,15 @@
 import { execWithinMobileApp } from 'hooks/useReactNativeWebView';
 import { useCallback } from 'react';
 
-type Opts = {
-  message: string;
-};
-
 /**
  * Get privy to sign a message, in react-native, then return the message into
  * the browser.
  */
 export function usePrivyMobileSignMessage() {
-  return useCallback(async (opts: Opts) => {
+  return useCallback(async (message: string) => {
     return await execWithinMobileApp({
       type: 'privy.sign_message',
-      data: opts,
+      data: { message },
     });
   }, []);
 }
