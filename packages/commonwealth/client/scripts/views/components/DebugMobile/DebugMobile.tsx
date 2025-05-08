e83da9f389d16c1fc27f7e9fdfd6ebd75b834be8
Kevin Burton <burtonator@gmail.com>
2025-05-08 13:11:39 -0700

    ...

--- packages/commonwealth/client/scripts/views/components/DebugMobile/DebugMobile.tsx
+++ packages/commonwealth/client/scripts/views/components/DebugMobile/DebugMobile.tsx
@@ -1,5 +1,6 @@
 import React, { memo, useState } from 'react';
 import { DebugPostMessage } from 'views/components/PrivyMobile/DebugPostMessage';
+import { useNotificationsGetPermissionsAsyncReceiver } from 'views/components/PrivyMobile/useNotificationsGetPermissionsAsyncReceiver';
 import { usePrivyEthereumWalletRequest } from 'views/components/PrivyMobile/usePrivyEthereumWalletRequest';
 import usePrivyMobileAuthStatusStore from 'views/components/PrivyMobile/usePrivyMobileAuthStatusStore';
 import { usePrivyMobileLogout } from 'views/components/PrivyMobile/usePrivyMobileLogout';
@@ -14,10 +15,16 @@ export const DebugMobile = memo(function DebugMobile() {
   const [signature, setSignature] = useState<string | undefined>();
 
   const [accounts, setAccounts] = useState<any | undefined>();
+  const [notificationPermissions, setNotificationPermissions] = useState<
+    string | undefined
+  >();
 
   const signMessage = usePrivyMobileSignMessage();
   const logout = usePrivyMobileLogout();
   const ethereumWalletRequest = usePrivyEthereumWalletRequest();
+
+  const getNotificationsPermissions =
+    useNotificationsGetPermissionsAsyncReceiver();
 
   const handleSignMessage = () => {
     async function doAsync() {
@@ -47,6 +54,15 @@ export const DebugMobile = memo(function DebugMobile() {
     doAsync().catch(console.error);
   };
 
+  const handleGetNotificationsPermissions = () => {
+    async function doAsync() {
+      const { status } = await getNotificationsPermissions({});
+      setNotificationPermissions(status);
+    }
+
+    doAsync().catch(console.error);
+  };
+
   return (
     <DebugPostMessage>
       <div>
@@ -68,9 +84,19 @@ export const DebugMobile = memo(function DebugMobile() {
           </button>
         </div>
 
+        <div style={{ margin: '8px' }}>
+          <button onClick={handleGetNotificationsPermissions}>
+            Get Notification Permissions
+          </button>
+        </div>
+
         {signature && <div>signature: {signature}</div>}
 
         {accounts && <div>accounts: {JSON.stringify(accounts, null, 2)}</div>}
+
+        {notificationPermissions && (
+          <div>notificationPermissions: {notificationPermissions}</div>
+        )}
 
         <div>{JSON.stringify(privyMobileAuthStatus, null, 2)}</div>
       </div>
