68d2771aef020fc878e5348caae76add9bcf5112
kassad <kurtisassad@gmail.com>
2025-05-08 07:01:29 -0700

    temp

--- scripts/deploy-to-heroku.sh
+++ scripts/deploy-to-heroku.sh
@@ -9,6 +9,15 @@ if [ "$#" -ne 3 ]; then
     exit 1
 fi
 
+start_step() {
+  STEP_START=$(date +%s)
+}
+
+end_step() {
+  STEP_END=$(date +%s)
+  echo "[$1] took $((STEP_END - STEP_START)) seconds"
+}
+
 email=$1
 api_key=$2
 app_name=$3
@@ -22,9 +31,20 @@ machine git.heroku.com
     password ${api_key}
 EOF
 
+
+echo "===> heroku container:login"
+start_step
+
 heroku container:login
 
+end_step "heroku container:login"
+
+echo "===> heroku git:remote --app ${app_name}"
+start_step
+
 heroku git:remote --app ${app_name}
+
+end_step "heroku git:remote"
 
 env_path=./packages/commonwealth/deploy/environments/.env.public.${app_name}
 if [ ! -f ${env_path} ]; then
@@ -34,8 +54,14 @@ fi
 
 cp ${env_path} .env
 
+
+echo "===> docker build -f Dockerfile.datadog"
+start_step
+
 # Needed for commonwealth_base
 docker build -f Dockerfile.datadog -t datadog-base .
+
+end_step "docker build datadog-base"
 
 deploy_heroku_app() {
   local app_path=$1
@@ -47,18 +73,35 @@ deploy_heroku_app() {
 
      heroku_tag=registry.heroku.com/${app_name}/${base_name}
 
+     echo "===> docker build ${dockerfile}"
+     start_step
+
      docker build -f ${dockerfile} -t ${heroku_tag}:latest .
 
+     end_step "docker build ${base_name}"
+
+
+     echo "===> docker push ${heroku_tag}"
+     start_step
      echo docker image ls
 
      docker push ${heroku_tag}:latest
 
+     end_step "docker push ${base_name}"
+
      process_types="${process_types} ${base_name}"
   done
 
+  echo "===> heroku container:release ${process_types} -a ${app_name}"
+  start_step
   process_types=$(echo $process_types | xargs)
   heroku container:release ${process_types} -a ${app_name}
+  end_step "heroku release"
 }
 
+echo "===> docker build commonwealth"
+start_step
 docker build . --target commonwealth -t commonwealth -f Dockerfile.commonwealth_base
+end_step "docker build commonwealth"
+
 deploy_heroku_app "./packages/commonwealth/deploy/dockerfiles" ${app_name}
