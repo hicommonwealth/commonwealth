51c7b0060e7c3fc59c498d6954437113fbb27cdb
Timothee Legros <timothee@ualberta.ca>
2025-05-09 16:57:18 +0200

    remove eslint testing code

--- libs/evm-protocols/src/common-protocol/contractHelpers/launchpad.ts
+++ libs/evm-protocols/src/common-protocol/contractHelpers/launchpad.ts
@@ -236,10 +236,6 @@ export async function getLaunchpadTokenCreatedTransaction({
     deployedNamespaceLog.data!.toString(),
   );
 
-  async function test() {
-    return 10;
-  }
-
   const tokenRegisteredLog = txReceipt.logs.find((l) => {
     if (l.topics && l.topics.length > 0) {
       return (
