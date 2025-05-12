a88c07e9d003b6d1a50240c14530acb4e0f94531
rotorsoft <rotorsoft@outlook.com>
2025-05-12 10:39:20 -0400

    fix test

--- libs/model/test/thread/thread-lifecycle.spec.ts
+++ libs/model/test/thread/thread-lifecycle.spec.ts
@@ -1326,13 +1326,11 @@ describe('Thread lifecycle', () => {
           order_direction: 'DESC',
         },
       });
-      expect(comments!.results).to.have.length(5);
-      expect(comments!.results[0].id).to.equal(1);
-      expect(comments!.results[1].id).to.equal(2);
+      expect(comments!.results).to.have.length(1);
       expect(comments!.limit).to.equal(5);
-      expect(comments!.page).to.equal(2);
-      expect(comments!.totalPages).to.equal(3);
-      expect(comments!.totalResults).to.equal(11);
+      expect(comments!.page).to.equal(1);
+      expect(comments!.totalPages).to.equal(1);
+      expect(comments!.totalResults).to.equal(1);
     });
   });
 });
