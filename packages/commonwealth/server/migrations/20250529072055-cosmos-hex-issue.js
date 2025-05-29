ce6e8795401ebdc1a335eafbd1f8dc66d942dc29
Timothee Legros <timothee@ualberta.ca>
2025-05-29 13:06:00 +0300

    migration v1

---
+++ packages/commonwealth/server/migrations/20250529072055-cosmos-hex-issue.js
@@ -0,0 +1,155 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.sequelize.transaction(async (transaction) => {
+      await queryInterface.sequelize.query(
+        `
+        CREATE TEMP TABLE duplicate_hexes AS
+          SELECT hex
+          FROM "Addresses"
+          WHERE hex IS NOT NULL
+            AND user_id IS NOT NULL
+          GROUP BY hex
+          HAVING COUNT(DISTINCT user_id) > 1;
+      `,
+        { transaction },
+      );
+      console.log('duplicate_hexes temp table created');
+
+      // format: { user_data: { [user_id]: {bio, name, total_addresses, last_active} } }
+      const [users] = await queryInterface.sequelize.query(
+        `
+          WITH user_stats AS (SELECT U.id,
+                                     U.profile,
+                                     COUNT(*)         as total_addresses,
+                                     MAX(last_active) as last_active
+                              FROM "Addresses" A
+                                     JOIN duplicate_hexes dh ON dh.hex = A.hex
+                                     JOIN "Users" U ON U.id = A.user_id
+                              GROUP BY U.id, U.profile)
+          SELECT json_object_agg(
+                   id::text,
+                   json_build_object(
+                     'id', id,
+                     'bio', profile ->> 'bio',
+                     'name', profile ->> 'name',
+                     'total_addresses', total_addresses,
+                     'last_active', last_active
+                   )
+                 ) as user_data
+          FROM user_stats;
+        `,
+        { transaction, type: queryInterface.sequelize.QueryTypes, raw: true },
+      );
+      if (
+        !users ||
+        !users['user_data'] ||
+        !Object.keys(users['user_data']).length
+      )
+        return;
+      const userData = users['user_data'];
+      console.log(`fetched ${Object.keys(userData).length} users`);
+
+      const hexes = (
+        await queryInterface.sequelize.query(
+          `
+          SELECT *
+          FROM duplicate_hexes;
+        `,
+          {
+            transaction,
+            type: queryInterface.sequelize.QueryTypes.SELECT,
+            raw: true,
+          },
+        )
+      ).map((h) => h['hex']);
+      console.log(`fetched ${hexes.length} hexes`);
+
+      for (const hex of hexes) {
+        console.log(`processing hex: ${hex}`);
+        const hexUserIds = (
+          await queryInterface.sequelize.query(
+            `
+            SELECT DISTINCT user_id
+            FROM "Addresses"
+            WHERE hex = :hex
+              AND user_id IS NOT NULL;
+          `,
+            {
+              transaction,
+              type: queryInterface.sequelize.QueryTypes.SELECT,
+              raw: true,
+              replacements: { hex },
+            },
+          )
+        ).map((h) => h['user_id']);
+
+        // Happens if an address was transferred from another user already
+        if (hexUserIds.length === 1) continue;
+
+        console.log(`Users found for hex: ${hexUserIds}`);
+
+        // Determine the user that should be owner of all the addresses.
+        // Picks the user with the most connected address or if all have the same
+        // number it picks the user with a bio or name set. If no name or bio
+        // picks the user that was most recently active.
+        let topUser = userData[hexUserIds[0]];
+        for (let i = 1; i > hexUserIds.length; i++) {
+          const hexUser = userData[hexUserIds[i]];
+          if (hexUser['total_addresses'] > topUser['total_addresses']) {
+            topUser = hexUser;
+          } else if (
+            hexUser['total_addresses'] === topUser['total_addresses']
+          ) {
+            if (
+              (hexUser['bio'] && !topUser['bio']) ||
+              (hexUser['name'] && !topUser['name']) ||
+              new Date(hexUser['last_active']) >
+                new Date(topUser['last_active'])
+            ) {
+              topUser = hexUser;
+            }
+          }
+        }
+
+        // transfer addresses
+        const fromUserIds = hexUserIds.filter((id) => id != topUser['id']);
+        console.log(
+          `transferring addresses from users: ${fromUserIds}, to: ${topUser['id']}`,
+        );
+        await queryInterface.sequelize.query(
+          `
+            UPDATE "Addresses"
+            SET user_id = ${topUser['id']}
+            WHERE user_id IN (:fromUserIds)
+          `,
+          { transaction, replacements: { fromUserIds } },
+        );
+      }
+
+      // await queryInterface.sequelize.query(
+      //   `
+      //     CREATE UNIQUE INDEX idx_addresses_hex_user_id_unique
+      //       ON "Addresses" (hex, user_id)
+      //       WHERE hex IS NOT NULL AND user_id IS NOT NULL;
+      //   `,
+      //   { transaction },
+      // );
+    });
+
+    // TODO: need another migration to delete all orphaned users
+    // TODO: need to check all columns that reference user_id to see if they
+    //  should be referencing address_id instead given that address can change ownership
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  },
+};
