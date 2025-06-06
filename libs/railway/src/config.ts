da79ded5a81d6db022b0b6ad34ef9f7c483ebeaa
Timothee Legros <timothee@ualberta.ca>
2025-06-06 23:41:39 +0300

    simplify config

--- libs/railway/src/config.ts
+++ libs/railway/src/config.ts
@@ -6,28 +6,10 @@ import { z } from 'zod';
 const { RAILWAY_TOKEN, RAILWAY_PROJECT_ID } = process.env;
 
 export const config = configure(
-  fwoek,
   [target],
   {
     RAILWAY_TOKEN,
     RAILWAY_PROJECT_ID,
   },
-  z.object({
-    RAILWAY_TOKEN: z
-      .string()
-      .optional()
-      .refine(
-        (data) => !(target.IS_CI && !data),
-        'RAILWAY_TOKEN is required in CI',
-      )
-      .describe('A Railway personal/team token (NOT environment specific)'),
-    RAIlWAY_PROJECT_ID: z
-      .string()
-      .optional()
-      .refine(
-        (data) => !(target.IS_CI && !data),
-        'RAILWAY_PROJECT_ID is required in CI',
-      )
-      .describe('A Railway project id in which all operations will take place'),
-  }),
+  z.object({}),
 );
