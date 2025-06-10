bbc2cf6c514edccc4905bcb93e08c0a0168f3140
Malik Zulqurnain <malikzulqurnain2121@gmail.com>
2025-06-10 22:53:31 +0500

    Added search filters to contest list

--- packages/commonwealth/client/scripts/views/pages/ExplorePage/ExploreContestList/ExploreContestList.tsx
+++ packages/commonwealth/client/scripts/views/pages/ExplorePage/ExploreContestList/ExploreContestList.tsx
@@ -12,7 +12,6 @@ import './ExploreContestList.scss';
 type ExploreContestListProps = {
   hideHeader?: boolean;
   hideFilters?: boolean;
-  hideSeeMore?: boolean;
   searchText?: string;
   onClearSearch?: () => void;
   hideSearchTag?: boolean;
@@ -21,7 +20,6 @@ type ExploreContestListProps = {
 const ExploreContestList = ({
   hideHeader,
   hideFilters,
-  hideSeeMore,
   searchText,
   onClearSearch,
   hideSearchTag,
@@ -31,6 +29,7 @@ const ExploreContestList = ({
     isContestDataLoading,
   } = useCommunityContests({
     fetchAll: true,
+    search: searchText?.trim(),
   });
 
   const communityIds = [
@@ -60,61 +59,61 @@ const ExploreContestList = ({
   return (
     <div className="ExploreContestList">
       {!hideHeader && <CWText type="h2">Contests</CWText>}
-      <div className="filters">
-        {!hideSearchTag && searchText?.trim() && (
+      {!hideFilters && searchText?.trim() && (
+        <div className="filters">
           <CWTag
             label={`Search: ${searchText?.trim()}`}
             type="filter"
             onCloseClick={onClearSearch}
           />
-        )}
-        {!isContestDataLoading && activeContests.length === 0 && (
-          <CWText type="h2" className="empty-contests">
-            No active contests found
-          </CWText>
-        )}
-        {isContestDataLoading ? (
-          <div className="content">
-            <>
-              <Skeleton height="300px" />
-              <Skeleton height="300px" />
-            </>
-          </div>
-        ) : (
-          <div className="content">
-            {activeContests.map((contest) => {
-              const sortedContests = (contest?.contests || []).toSorted(
-                (a, b) => (moment(a.end_time).isBefore(b.end_time) ? -1 : 1),
-              );
+        </div>
+      )}
+      {!isContestDataLoading && activeContests.length === 0 && (
+        <CWText type="h2" className="empty-contests">
+          No active contests found
+        </CWText>
+      )}
+      {isContestDataLoading ? (
+        <div className="content">
+          <>
+            <Skeleton height="300px" />
+            <Skeleton height="300px" />
+          </>
+        </div>
+      ) : (
+        <div className="content">
+          {activeContests.map((contest) => {
+            const sortedContests = (contest?.contests || []).toSorted((a, b) =>
+              moment(a.end_time).isBefore(b.end_time) ? -1 : 1,
+            );
 
-              const { end_time, score } =
-                sortedContests[sortedContests.length - 1] || {};
+            const { end_time, score } =
+              sortedContests[sortedContests.length - 1] || {};
 
-              return (
-                <ContestCard
-                  key={contest.contest_address}
-                  isAdmin={false}
-                  address={contest.contest_address || ''}
-                  name={contest.name || ''}
-                  imageUrl={contest.image_url || ''}
-                  topics={contest.topics || []}
-                  decimals={contest.decimals}
-                  ticker={contest.ticker}
-                  prizePercentage={contest.prize_percentage || undefined}
-                  finishDate={end_time ? moment(end_time).toISOString() : ''}
-                  isCancelled={contest.cancelled}
-                  isRecurring={!contest.funding_token_address}
-                  payoutStructure={contest.payout_structure}
-                  score={score || []}
-                  isFarcaster={contest.is_farcaster_contest}
-                  community={community[contest.community_id as string]}
-                  hideWhenNoPrizes={true}
-                />
-              );
-            })}
-          </div>
-        )}
-      </div>
+            return (
+              <ContestCard
+                key={contest.contest_address}
+                isAdmin={false}
+                address={contest.contest_address || ''}
+                name={contest.name || ''}
+                imageUrl={contest.image_url || ''}
+                topics={contest.topics || []}
+                decimals={contest.decimals}
+                ticker={contest.ticker}
+                prizePercentage={contest.prize_percentage || undefined}
+                finishDate={end_time ? moment(end_time).toISOString() : ''}
+                isCancelled={contest.cancelled}
+                isRecurring={!contest.funding_token_address}
+                payoutStructure={contest.payout_structure}
+                score={score || []}
+                isFarcaster={contest.is_farcaster_contest}
+                community={community[contest.community_id as string]}
+                hideWhenNoPrizes={true}
+              />
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 };
