f1fa3bbb755857caee2012ec462d905bad7ce4e2
Salman <muhsalman.neslit@gmail.com>
2025-06-12 07:04:35 +0500

    ES Lint Warnings

--- libs/model/src/aggregates/token/GetTokenStats.query.ts
+++ libs/model/src/aggregates/token/GetTokenStats.query.ts
@@ -13,7 +13,10 @@ export function GetTokenStats(): Query<typeof schemas.GetTokenStats> {
         COUNT(DISTINCT trader_address) AS holder_count,
         COALESCE(SUM(CASE WHEN timestamp >= EXTRACT(EPOCH FROM CURRENT_TIMESTAMP - INTERVAL '24 hours') THEN price * community_token_amount ELSE 0 END),0) AS volume_24h
         FROM "LaunchpadTrades" WHERE token_address = :token_address`;
-      const [row] = await models.sequelize.query<{ holder_count: number; volume_24h: string }>(sql, {
+      const [row] = await models.sequelize.query<{
+        holder_count: number;
+        volume_24h: string;
+      }>(sql, {
         replacements: { token_address },
         type: QueryTypes.SELECT,
       });
