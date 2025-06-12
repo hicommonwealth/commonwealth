5d0f04578edd45662d2ba880b9ef99d84f1fb00c
Salman <muhsalman.neslit@gmail.com>
2025-06-12 07:31:30 +0500

    Lint Fixed

--- libs/model/src/aggregates/token/GetTokenStats.query.ts
+++ libs/model/src/aggregates/token/GetTokenStats.query.ts
@@ -9,10 +9,22 @@ export function GetTokenStats(): Query<typeof schemas.GetTokenStats> {
     auth: [],
     async body({ payload }) {
       const { token_address } = payload;
-      const sql = `SELECT
-        COUNT(DISTINCT trader_address) AS holder_count,
-        COALESCE(SUM(CASE WHEN timestamp >= EXTRACT(EPOCH FROM CURRENT_TIMESTAMP - INTERVAL '24 hours') THEN price * community_token_amount ELSE 0 END),0) AS volume_24h
-        FROM "LaunchpadTrades" WHERE token_address = :token_address`;
+      const sql = `
+        SELECT
+          COUNT(DISTINCT trader_address) AS holder_count,
+          COALESCE(
+            SUM(
+              CASE 
+                WHEN timestamp >= EXTRACT(EPOCH FROM CURRENT_TIMESTAMP - INTERVAL '24 hours')
+                THEN price * community_token_amount 
+                ELSE 0 
+              END
+            ),
+            0
+          ) AS volume_24h
+        FROM "LaunchpadTrades" 
+        WHERE token_address = :token_address
+      `;
       const [row] = await models.sequelize.query<{
         holder_count: number;
         volume_24h: string;
