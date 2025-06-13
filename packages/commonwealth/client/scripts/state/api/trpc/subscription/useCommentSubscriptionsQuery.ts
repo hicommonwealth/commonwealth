444e5cfc4d768f4821a3db94489bbc54abc166ad
Marcin <maslankam92@gmail.com>
2025-06-13 17:12:57 +0200

    Add stale time configuration to useCommentSubscriptionsQuery for improved data freshness management.

--- packages/commonwealth/client/scripts/state/api/trpc/subscription/useCommentSubscriptionsQuery.ts
+++ packages/commonwealth/client/scripts/state/api/trpc/subscription/useCommentSubscriptionsQuery.ts
@@ -1,5 +1,12 @@
 import { trpc } from 'utils/trpcClient';
 
+const SUBSCRIPTIONS_STALE_TIME = 30 * 1_000; // 30s
+
 export function useCommentSubscriptionsQuery() {
-  return trpc.subscriptions.getCommentSubscriptions.useQuery({});
+  return trpc.subscriptions.getCommentSubscriptions.useQuery(
+    {},
+    {
+      staleTime: SUBSCRIPTIONS_STALE_TIME,
+    },
+  );
 }
