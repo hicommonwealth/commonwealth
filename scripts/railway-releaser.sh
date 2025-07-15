0f2ecaeaf02125b0f14da084827149cd7f36017f
timolegros <timothee@ualberta.ca>
2025-07-15 21:39:35 +0300

    fix migration command

--- scripts/railway-releaser.sh
+++ scripts/railway-releaser.sh
@@ -5,7 +5,7 @@ set -e
 # Apply migration to DB
 echo "Migrating DB..."
 echo ""
-npx -y sequelize@^6.32.1 sequelize-cli@^6.2.0 db:migrate --config packages/commonwealth/server/sequelize.json
+npx -y --package=sequelize@^6.32.1 --package=sequelize-cli@^6.2.0 sequelize-cli db:migrate --config packages/commonwealth/server/sequelize.json
 echo ""
 
 # Purge the Cloudflare Cache
