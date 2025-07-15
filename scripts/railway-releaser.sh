a135883fc4f4b3b62bb23016bb3d017e66e68455
timolegros <timothee@ualberta.ca>
2025-07-15 21:17:11 +0300

    revert back to npx

--- scripts/railway-releaser.sh
+++ scripts/railway-releaser.sh
@@ -5,10 +5,10 @@ set -e
 # Apply migration to DB
 echo "Migrating DB..."
 echo ""
-pnpm dlx sequelize@^6.32.1 sequelize-cli@^6.2.0 db:migrate --config packages/commonwealth/server/sequelize.json
+npx -y sequelize@^6.32.1 sequelize-cli@^6.2.0 db:migrate --config packages/commonwealth/server/sequelize.json
 echo ""
 
 # Purge the Cloudflare Cache
 echo "Purging Cloudflare Cache..."
 echo ""
-pnpm dlx tsx@^4.7.2 packages/commonwealth/server/scripts/purgeCloudflareCache.ts
+npx -y tsx@^4.7.2 packages/commonwealth/server/scripts/purgeCloudflareCache.ts
