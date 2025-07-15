e76ffa8223fc509a4abd2cc9412b39d577c980b7
Salman <muhsalman.neslit@gmail.com>
2025-07-15 19:26:44 +0500

    Fixed Es Lint

--- packages/commonwealth/client/scripts/state/api/groups/fetchGroupById.ts
+++ packages/commonwealth/client/scripts/state/api/groups/fetchGroupById.ts
@@ -1,6 +1,6 @@
 import { trpcClient } from 'client/scripts/utils/trpcClient';
 
-export const fetchGroupById = async (groupId: number) => {
+export const fetchGroupById = (groupId: number) => {
   return trpcClient.community.getGroups.query({
     group_id: groupId,
     include_topics: true,
