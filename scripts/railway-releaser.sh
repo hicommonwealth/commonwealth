82dc2c15af2abbf8e831abe418699ec7835388d7
timolegros <timothee@ualberta.ca>
2025-07-15 23:03:35 +0300

    added pg

--- scripts/railway-releaser.sh
+++ scripts/railway-releaser.sh
@@ -5,7 +5,7 @@ set -e
 # Apply migration to DB
 echo "Migrating DB..."
 echo ""
-npx -y --package=sequelize@^6.32.1 --package=sequelize-cli@^6.2.0 sequelize-cli db:migrate --config packages/commonwealth/server/sequelize.json
+npx -y --package=sequelize@^6.32.1 --package=sequelize-cli@^6.2.0 --package=pg@^8.11.3 sequelize-cli db:migrate --config packages/commonwealth/server/sequelize.json
 echo ""
 
 # Purge the Cloudflare Cache
