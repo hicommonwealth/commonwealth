dc171fb2513b68ca5760322989f6b1c51dfb24ad
timolegros <timothee@ualberta.ca>
2025-07-15 21:15:25 +0300

    Use pnpm dlx instead of npx

--- scripts/railway-releaser.sh
+++ scripts/railway-releaser.sh
@@ -5,10 +5,10 @@ set -e
 # Apply migration to DB
 echo "Migrating DB..."
 echo ""
-npx -y sequelize-cli db:migrate --config packages/commonwealth/server/sequelize.json
+pnpm dlx sequelize@^6.32.1 sequelize-cli@^6.2.0 db:migrate --config packages/commonwealth/server/sequelize.json
 echo ""
 
 # Purge the Cloudflare Cache
 echo "Purging Cloudflare Cache..."
 echo ""
-npx -y tsx packages/commonwealth/server/scripts/purgeCloudflareCache.ts
+pnpm dlx tsx@^4.7.2 packages/commonwealth/server/scripts/purgeCloudflareCache.ts
