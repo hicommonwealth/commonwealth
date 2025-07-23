565b116cc0c6c5581d8e799eb9d4339fc92286a7
timolegros <timothee@ualberta.ca>
2025-07-23 17:53:06 +0200

    fix migration export

--- libs/model/migrations/20250721170241-token-allocations.js
+++ libs/model/migrations/20250721170241-token-allocations.js
@@ -1,7 +1,7 @@
 'use strict';
 
 /** @type {import('sequelize-cli').Migration} */
-module.exports = {
+export default {
   async up(queryInterface, Sequelize) {
     await queryInterface.sequelize.transaction(async (transaction) => {
       await queryInterface.createTable(
