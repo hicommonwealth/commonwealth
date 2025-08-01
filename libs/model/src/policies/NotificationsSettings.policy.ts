f2489e3fa6d5fb8c9703bf17cdf47738839974d1
timolegros <timothee@ualberta.ca>
2025-08-01 18:34:20 +0200

    update Knock email on user email update

--- libs/model/src/policies/NotificationsSettings.policy.ts
+++ libs/model/src/policies/NotificationsSettings.policy.ts
@@ -1,9 +1,11 @@
-import { Policy } from '@hicommonwealth/core';
+import { notificationsProvider, Policy } from '@hicommonwealth/core';
 import { events } from '@hicommonwealth/schemas';
 import { processSubscriptionPreferencesUpdated } from '../services/knock/subscriptionPreferencesUpdated';
 
 const notificationSettingsInputs = {
   SubscriptionPreferencesUpdated: events.SubscriptionPreferencesUpdated,
+  UserCreated: events.UserCreated,
+  UserUpdated: events.UserUpdated,
 };
 
 export function NotificationsSettingsPolicy(): Policy<
@@ -14,6 +16,28 @@ export function NotificationsSettingsPolicy(): Policy<
     body: {
       SubscriptionPreferencesUpdated: async (event) => {
         await processSubscriptionPreferencesUpdated(event);
+      },
+      UserCreated: async ({ payload }) => {
+        if (payload.user.email) {
+          await notificationsProvider().identifyUser({
+            user_id: `${payload.user_id}`,
+            user_properties: {
+              email: payload.user.email,
+            },
+          });
+        }
+      },
+      UserUpdated: async ({ payload }) => {
+        const oldEmail = payload.old_user.email;
+        const newEmail = payload.new_user.email;
+        if (newEmail && newEmail !== oldEmail) {
+          await notificationsProvider().identifyUser({
+            user_id: `${payload.new_user.id!}`,
+            user_properties: {
+              email: newEmail,
+            },
+          });
+        }
       },
     },
   };
