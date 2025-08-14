f9d99730e94eb8ef8e99c31722daf4fed8d82efb
rotorsoft <rotorsoft@outlook.com>
2025-08-14 09:48:22 -0400

    simplify sync

--- packages/commonwealth/server/workers/graphileWorker/tasks/magnaSync.ts
+++ packages/commonwealth/server/workers/graphileWorker/tasks/magnaSync.ts
@@ -1,9 +1,5 @@
 import { logger } from '@hicommonwealth/core';
-import {
-  TokenAllocationSyncArgs,
-  TokenAllocationSyncResponse,
-  magnaSync,
-} from '@hicommonwealth/model';
+import { TokenAllocationSyncArgs, magnaSync } from '@hicommonwealth/model';
 import { GraphileTask, TaskPayloads } from '@hicommonwealth/model/services';
 import { config } from '../../../config';
 import * as magnaApi from '../../magna/api';
@@ -11,42 +7,36 @@ import * as magnaApi from '../../magna/api';
 const log = logger(import.meta);
 
 async function createMagnaAllocation({
+  key,
   category,
   description,
   user_name,
   user_email,
   wallet_address,
   token_allocation,
-}: TokenAllocationSyncArgs): Promise<TokenAllocationSyncResponse> {
-  try {
-    const response = await magnaApi.createAllocation(
-      config.MAGNA!.API_URL,
-      config.MAGNA!.API_KEY,
-      {
-        key: `A-${category}-${wallet_address}`,
-        category,
-        description,
-        contractId: config.MAGNA!.CONTRACT_ID,
-        tokenId: config.MAGNA!.TOKEN_ID,
-        amount: token_allocation,
-        walletAddress: wallet_address,
-        stakeholder: user_email
-          ? {
-              name: user_name,
-              email: user_email,
-            }
-          : { name: user_name },
-        unlockScheduleId: config.MAGNA!.UNLOCK_SCHEDULE_ID,
-        unlockStartAt: config.MAGNA!.UNLOCK_START_AT.toISOString(),
-      },
-    );
-    if (response.isProcessed) {
-      if (response.result.id) return { id: response.result.id };
-      else return { error: 'Allocation ID not found in reponse' };
-    } else return { error: JSON.stringify(response) };
-  } catch (error) {
-    return { error: error.message };
-  }
+}: TokenAllocationSyncArgs): Promise<boolean> {
+  const response = await magnaApi.createAllocation(
+    config.MAGNA!.API_URL,
+    config.MAGNA!.API_KEY,
+    {
+      key,
+      category,
+      description,
+      contractId: config.MAGNA!.CONTRACT_ID,
+      tokenId: config.MAGNA!.TOKEN_ID,
+      amount: token_allocation,
+      walletAddress: wallet_address,
+      stakeholder: user_email
+        ? {
+            name: user_name,
+            email: user_email,
+          }
+        : { name: user_name },
+      unlockScheduleId: config.MAGNA!.UNLOCK_SCHEDULE_ID,
+      unlockStartAt: config.MAGNA!.UNLOCK_START_AT.toISOString(),
+    },
+  );
+  return response.isProcessed;
 }
 
 export const magnaSyncTask: GraphileTask<typeof TaskPayloads.MagnaSync> = {
