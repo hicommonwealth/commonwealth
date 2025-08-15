4ec9f5c92533d58c1a1da6d4ac10cf130f0bb808
Salman <muhsalman.neslit@gmail.com>
2025-08-15 05:09:43 +0500

    Incremental Reties

--- libs/model/src/aggregates/thread/CreateThreadToken.command.ts
+++ libs/model/src/aggregates/thread/CreateThreadToken.command.ts
@@ -28,10 +28,12 @@ export function CreateThreadToken(): Command<typeof schemas.CreateThreadToken> {
         transport: http(rpc),
       });
 
-      const receipt = await withRetries(() =>
-        client.getTransactionReceipt({
-          hash: transaction_hash as Hash,
-        }),
+      const receipt = await withRetries(
+        () =>
+          client.getTransactionReceipt({
+            hash: transaction_hash as Hash,
+          }),
+        15000,
       );
 
       const parsedLogs = parseEventLogs({
@@ -81,7 +83,7 @@ export function CreateThreadToken(): Command<typeof schemas.CreateThreadToken> {
             launchpad_liquidity: BigInt(launchpadLiquidity).toString(),
             eth_market_cap_target: protocols.getTargetMarketCap(),
             creator_address: receipt.from,
-            thread_id: threadId.toString(),
+            thread_id: Number(threadId),
             created_at: date,
             updated_at: date,
           },
