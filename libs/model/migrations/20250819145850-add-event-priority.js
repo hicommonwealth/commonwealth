85fb3e84415f5ff2e76f00d6fe78f436498a9026
timolegros <timothee@ualberta.ca>
2025-08-19 17:52:44 +0200

    add index to outbox_unrelayed

--- libs/model/migrations/20250819145850-add-event-priority.js
+++ libs/model/migrations/20250819145850-add-event-priority.js
@@ -3,14 +3,36 @@
 /** @type {import('sequelize-cli').Migration} */
 export default {
   async up(queryInterface, Sequelize) {
-    await queryInterface.addColumn('Outbox', 'priority', {
-      type: Sequelize.INTEGER,
-      allowNull: true,
-      defaultValue: 0,
+    await queryInterface.sequelize.transaction(async (transaction) => {
+      await queryInterface.addColumn(
+        'Outbox',
+        'priority',
+        {
+          type: Sequelize.INTEGER,
+          allowNull: true,
+          defaultValue: 0,
+        },
+        { transaction },
+      );
+      await queryInterface.sequelize.query(
+        `
+        CREATE INDEX idx_outbox_unrelayed_priority_created_at
+          ON public.outbox_unrelayed (priority DESC NULLS FIRST, created_at ASC);
+      `,
+        { transaction },
+      );
     });
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.removeColumn('Outbox', 'priority');
+    await queryInterface.sequelize.transaction(async (transaction) => {
+      await queryInterface.removeColumn('Outbox', 'priority', { transaction });
+      await queryInterface.sequelize.query(
+        `
+        DROP INDEX IF EXISTS idx_outbox_unrelayed_priority_created_at;
+      `,
+        { transaction },
+      );
+    });
   },
 };
