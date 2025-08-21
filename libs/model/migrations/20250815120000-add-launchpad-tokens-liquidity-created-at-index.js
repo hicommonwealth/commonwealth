7267969e810b87f0e63eb6c6f39222868e8608b6
Dillon Chen <5334557+dillchen@users.noreply.github.com>
2025-08-20 21:19:52 -0400

    chore: add index migration

---
+++ libs/model/migrations/20250815120000-add-launchpad-tokens-liquidity-created-at-index.js
@@ -0,0 +1,28 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+export default {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.sequelize.transaction(async (transaction) => {
+      await queryInterface.addIndex(
+        'LaunchpadTokens',
+        ['liquidity_transferred', 'created_at'],
+        {
+          name: 'LaunchpadTokens_liquidity_transferred_created_at',
+          transaction,
+        },
+      );
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.sequelize.transaction(async (transaction) => {
+      await queryInterface.removeIndex(
+        'LaunchpadTokens',
+        'LaunchpadTokens_liquidity_transferred_created_at',
+        { transaction },
+      );
+    });
+  },
+};
+
