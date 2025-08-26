a6f4adb42d15624612dec79352c1bc2fb29de79f
Malik Zulqurnain <malikzulqurnain2121@gmail.com>
2025-08-26 05:01:14 +0500

    fix types

--- packages/commonwealth/client/scripts/views/pages/CommunityGroupsAndMembers/Groups/common/GroupForm/validations.ts
+++ packages/commonwealth/client/scripts/views/pages/CommunityGroupsAndMembers/Groups/common/GroupForm/validations.ts
@@ -10,22 +10,22 @@ export const VALIDATION_MESSAGES = {
 export const requirementSubFormValidationSchema = z.object({
   requirementType: z
     .string({ invalid_type_error: VALIDATION_MESSAGES.NO_INPUT })
-    .nonempty({ message: VALIDATION_MESSAGES.NO_INPUT }),
+    .min(1, { message: VALIDATION_MESSAGES.NO_INPUT }),
   requirementChain: z
     .string({ invalid_type_error: VALIDATION_MESSAGES.NO_INPUT })
-    .nonempty({ message: VALIDATION_MESSAGES.NO_INPUT }),
+    .min(1, { message: VALIDATION_MESSAGES.NO_INPUT }),
   requirementTrustLevel: z
     .string({ invalid_type_error: VALIDATION_MESSAGES.NO_INPUT })
-    .nonempty({ message: VALIDATION_MESSAGES.NO_INPUT }),
+    .min(1, { message: VALIDATION_MESSAGES.NO_INPUT }),
   requirementContractAddress: z
     .string({ invalid_type_error: VALIDATION_MESSAGES.NO_INPUT })
-    .nonempty({ message: VALIDATION_MESSAGES.NO_INPUT }),
+    .min(1, { message: VALIDATION_MESSAGES.NO_INPUT }),
   requirementCondition: z
     .string({ invalid_type_error: VALIDATION_MESSAGES.NO_INPUT })
-    .nonempty({ message: VALIDATION_MESSAGES.NO_INPUT }),
+    .min(1, { message: VALIDATION_MESSAGES.NO_INPUT }),
   requirementAmount: z
     .string({ invalid_type_error: VALIDATION_MESSAGES.NO_INPUT })
-    .nonempty({ message: VALIDATION_MESSAGES.NO_INPUT })
+    .min(1, { message: VALIDATION_MESSAGES.NO_INPUT })
     .refine(
       (value) => {
         return !isNaN(Number(value));
@@ -34,7 +34,7 @@ export const requirementSubFormValidationSchema = z.object({
     ),
   requirementTokenId: z
     .string({ invalid_type_error: VALIDATION_MESSAGES.NO_INPUT })
-    .nonempty({ message: VALIDATION_MESSAGES.NO_INPUT })
+    .min(1, { message: VALIDATION_MESSAGES.NO_INPUT })
     .refine(
       (value) => {
         return !isNaN(Number(value));
@@ -46,7 +46,7 @@ export const requirementSubFormValidationSchema = z.object({
 export const groupValidationSchema = z.object({
   groupName: z
     .string({ invalid_type_error: VALIDATION_MESSAGES.NO_INPUT })
-    .nonempty({ message: VALIDATION_MESSAGES.NO_INPUT })
+    .min(1, { message: VALIDATION_MESSAGES.NO_INPUT })
     .max(40, { message: VALIDATION_MESSAGES.MAX_CHAR_LIMIT_REACHED }),
   groupDescription: z
     .string({ invalid_type_error: VALIDATION_MESSAGES.NO_INPUT })
@@ -62,7 +62,7 @@ export const groupValidationSchema = z.object({
     .optional(),
   requirementsToFulfill: z
     .string({ invalid_type_error: VALIDATION_MESSAGES.NO_INPUT })
-    .nonempty({ message: VALIDATION_MESSAGES.NO_INPUT }),
+    .min(1, { message: VALIDATION_MESSAGES.NO_INPUT }),
   topics: z.union([
     z
       .array(
