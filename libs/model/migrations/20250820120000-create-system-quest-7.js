b3e73b3c6a528c454a2aa0cac90def3b214b91b5
Dillon Chen <5334557+dillchen@users.noreply.github.com>
2025-08-26 17:26:35 -0400

    feat: add system quest 7 migration

---
+++ libs/model/migrations/20250820120000-create-system-quest-7.js
@@ -0,0 +1,149 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+export default {
+  async up(queryInterface) {
+    await queryInterface.sequelize.transaction(async (transaction) => {
+      // Create Quest -7 (System Quest 7) - copy of quest -5 "Welcome to Common"
+      await queryInterface.sequelize.query(
+        `
+        INSERT INTO "Quests" (
+          "id",
+          "name",
+          "description",
+          "quest_type",
+          "image_url",
+          "start_date",
+          "end_date",
+          "xp_awarded",
+          "max_xp_to_end",
+          "created_at",
+          "updated_at"
+        )
+        VALUES (
+          -7,                                     -- id
+          'Welcome to Common',                    -- name (same as quest -5)
+          'Onboard to Common and gain XP',       -- description (same as quest -5)
+          'common',                               -- quest_type
+          'https://assets.commonwealth.im/fab3f073-9bf1-4ac3-8625-8b2ee258b5a8.png', -- image_url (same as quest -5)
+          now(),                                  -- start_date (now)
+          now() + interval '4 months',            -- end_date (4 months from now)
+          0,                                      -- xp_awarded
+          2000000,                                -- max_xp_to_end (same as quest -5)
+          now(),                                  -- created_at
+          now()                                   -- updated_at
+        );
+        `,
+        { transaction },
+      );
+
+      // Create Action Metas for Quest -7 (same as quest -5 but with new IDs)
+      await queryInterface.sequelize.query(
+        `
+        -- Action Meta: SignUpFlowCompleted (equivalent to ID -11 from quest -5)
+        INSERT INTO "QuestActionMetas" (
+          "id",
+          "quest_id",
+          "event_name",
+          "reward_amount",
+          "creator_reward_weight",
+          "participation_limit",
+          "created_at",
+          "updated_at"
+        )
+        VALUES (
+          -15,                    -- id (new, starting at -15)
+          -7,                     -- quest_id
+          'SignUpFlowCompleted',  -- event_name
+          10,                     -- reward_amount (same as quest -5)
+          0.2,                    -- creator_reward_weight (same as quest -5)
+          'once_per_quest',       -- participation_limit
+          now(),                  -- created_at
+          now()                   -- updated_at
+        );
+
+        -- Action Meta: WalletLinked (equivalent to ID -12 from quest -5)
+        INSERT INTO "QuestActionMetas" (
+          "id",
+          "quest_id",
+          "event_name",
+          "reward_amount",
+          "creator_reward_weight",
+          "participation_limit",
+          "created_at",
+          "updated_at"
+        )
+        VALUES (
+          -16,                    -- id (new, continuing sequence from -15)
+          -7,                     -- quest_id
+          'WalletLinked',         -- event_name
+          5,                      -- reward_amount (same as quest -5)
+          0,                      -- creator_reward_weight
+          'once_per_quest',       -- participation_limit
+          now(),                  -- created_at
+          now()                   -- updated_at
+        );
+
+        -- Action Meta: SSOLinked (equivalent to ID -13 from quest -5)
+        INSERT INTO "QuestActionMetas" (
+          "id",
+          "quest_id",
+          "event_name",
+          "reward_amount",
+          "creator_reward_weight",
+          "participation_limit",
+          "created_at",
+          "updated_at"
+        )
+        VALUES (
+          -17,                    -- id (new, continuing sequence from -16)
+          -7,                     -- quest_id
+          'SSOLinked',            -- event_name
+          5,                      -- reward_amount (same as quest -5)
+          0,                      -- creator_reward_weight
+          'once_per_quest',       -- participation_limit
+          now(),                  -- created_at
+          now()                   -- updated_at
+        );
+
+        -- Action Meta: XpAwarded for manual awards (equivalent to ID -14 from quest -5)
+        INSERT INTO "QuestActionMetas" (
+          "id",
+          "quest_id",
+          "event_name",
+          "reward_amount",
+          "creator_reward_weight",
+          "participation_limit",
+          "participation_period",
+          "created_at",
+          "updated_at"
+        )
+        VALUES (
+          -18,                    -- id (new, continuing sequence from -17)
+          -7,                     -- quest_id
+          'XpAwarded',            -- event_name
+          0,                      -- reward_amount
+          0,                      -- creator_reward_weight
+          'once_per_period',      -- participation_limit
+          'daily',                -- participation_period
+          now(),                  -- created_at
+          now()                   -- updated_at
+        );
+        `,
+        { transaction },
+      );
+    });
+  },
+
+  async down(queryInterface) {
+    await queryInterface.sequelize.transaction(async (transaction) => {
+      await queryInterface.sequelize.query(
+        `
+        DELETE FROM "QuestActionMetas" WHERE "quest_id" = -7;
+        DELETE FROM "Quests" WHERE "id" = -7;
+        `,
+        { transaction },
+      );
+    });
+  },
+};
