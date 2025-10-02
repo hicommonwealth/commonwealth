c74211e1c752e0e1653d29c411be813df7e9231d
Salman <muhsalman.neslit@gmail.com>
2025-10-02 15:22:32 +0500

    Proposal Empty State

---
+++ packages/commonwealth/client/scripts/views/pages/GovernancePage/GovernanceCards/GovernanceProposal/ProposalsEmptyState.tsx
@@ -0,0 +1,35 @@
+import { CWText } from 'client/scripts/views/components/component_kit/cw_text';
+import { CWButton } from 'client/scripts/views/components/component_kit/new_designs/CWButton';
+import { useCommonNavigate } from 'navigation/helpers';
+import React from 'react';
+import './ProposalsEmptyState.scss';
+
+const ProposalsEmptyState = () => {
+  const navigate = useCommonNavigate();
+
+  const handleConnectProposals = () => {
+    navigate('/community/integrations');
+  };
+
+  return (
+    <div className="ProposalsEmptyState">
+      <CWText fontWeight="medium" type="h5">
+        Proposals
+      </CWText>
+      <CWText type="caption" className="empty-description">
+        There are no proposal tools
+      </CWText>
+      <CWText type="caption" className="empty-description">
+        linked to this community.
+      </CWText>
+      <CWButton
+        buttonType="primary"
+        label="Connect Proposals"
+        buttonWidth="full"
+        onClick={handleConnectProposals}
+      />
+    </div>
+  );
+};
+
+export default ProposalsEmptyState;
