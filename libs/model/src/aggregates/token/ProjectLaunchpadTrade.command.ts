e03e2a9e5cfc566ab58e8bb8bba665b66fbfd8b5
Kurtis Assad <kurtisassad@gmail.com>
2025-10-09 09:55:26 -0700

    force redeploy

--- libs/model/src/aggregates/token/ProjectLaunchpadTrade.command.ts
+++ libs/model/src/aggregates/token/ProjectLaunchpadTrade.command.ts
@@ -8,6 +8,7 @@ import { chainNodeMustExist } from '../../policies/utils/utils';
 import { setUserTier } from '../../utils/tiers';
 import { handleCapReached } from './utils';
 
+// force redeploy
 export function ProjectLaunchpadTrade(): Command<
   typeof schemas.ProjectLaunchpadTrade
 > {
