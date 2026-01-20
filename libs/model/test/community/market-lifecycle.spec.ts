435899f57234cb934951513b53a564b3bfc5e007
Malik Zulqurnain <malikzulqurnain2121@gmail.com>
2026-01-20 22:05:44 +0500

    fix tests

--- libs/model/test/community/market-lifecycle.spec.ts
+++ libs/model/test/community/market-lifecycle.spec.ts
@@ -44,16 +44,20 @@ describe('Market lifecycle', () => {
       actor: adminActor,
       payload: {
         community_id: community_id!,
+        limit: 20,
+        cursor: 1,
       },
     });
-    expect(markets.length).toBe(1);
-    expect(markets[0].slug).toBe('polymarket-usdt-eth');
-    expect(markets[0].provider).toBe('polymarket');
-    expect(markets[0].question).toBe('What is the price of USDT in ETH?');
-    expect(markets[0].category).toBe('cryptocurrency');
-    expect(markets[0].start_time).toBeInstanceOf(Date);
-    expect(markets[0].end_time).toBeInstanceOf(Date);
-    expect(markets[0].status).toBe('open');
+    expect(markets).toBeDefined();
+    expect(markets.results).toBeDefined();
+    expect(markets.results.length).toBe(1);
+    expect(markets.results[0].slug).toBe('polymarket-usdt-eth');
+    expect(markets.results[0].provider).toBe('polymarket');
+    expect(markets.results[0].question).toBe('What is the price of USDT in ETH?');
+    expect(markets.results[0].category).toBe('cryptocurrency');
+    expect(markets.results[0].start_time).toBeInstanceOf(Date);
+    expect(markets.results[0].end_time).toBeInstanceOf(Date);
+    expect(markets.results[0].status).toBe('open');
   });
 
   it('should unsubscribe from a market', async () => {
@@ -68,8 +72,12 @@ describe('Market lifecycle', () => {
       actor: adminActor,
       payload: {
         community_id: community_id!,
+        limit: 20,
+        cursor: 1,
       },
     });
-    expect(markets.length).toBe(0);
+    expect(markets).toBeDefined();
+    expect(markets.results).toBeDefined();
+    expect(markets.results.length).toBe(0);
   });
 });
