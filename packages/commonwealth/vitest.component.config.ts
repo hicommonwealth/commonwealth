70d10d3e6220a4c53dd8891ebcbdb55ffe960850
Malik Zulqurnain <51641047+mzparacha@users.noreply.github.com>
2026-02-19 07:59:08 -0800

    fix broken form validations (#13183)

--- packages/commonwealth/vitest.component.config.ts
+++ packages/commonwealth/vitest.component.config.ts
@@ -1,7 +1,7 @@
 /// <reference types="vitest" />
 import path from 'path';
-import { defineConfig } from 'vitest/config';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
